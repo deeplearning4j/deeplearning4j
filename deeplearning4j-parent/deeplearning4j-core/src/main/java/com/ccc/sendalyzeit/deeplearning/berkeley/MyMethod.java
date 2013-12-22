022686845545fdb6af61c370cb1fe0adb4ebfaed
agibsonccc <agibson@clevercloudcomputing.com>
2013-12-21 21:50:16 -0800

    Lots of hackery in here right now; does distributed a lot better now (intend to add things like zookeeper service discovery and other clustering capabilities; this is based on a pub-sub model ala rabbitmq;other message brokers. Cleaned up, I believe this to be a reasonable approach to clustering a distributed akka cluster

--- deeplearning4j-parent/deeplearning4j-core/src/main/java/com/ccc/sendalyzeit/deeplearning/berkeley/MyMethod.java
+++
@@ -1,8 +0,0 @@
-package com.ccc.sendalyzeit.deeplearning.berkeley;
-/**
- * A function wrapping interface.
- * @author John DeNero
- */
-public interface MyMethod<I, O> {
-	public O call(I obj);
-}
--- deeplearning4j-parent/deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/com/ccc/sendalyzeit/textanalytics/algorithms/deeplearning/sda/matrix/jblas/iterativereduce/actor/EpochDoneListener.java
+++
@@ -1,8 +0,0 @@
-package com.ccc.sendalyzeit.textanalytics.algorithms.deeplearning.sda.matrix.jblas.iterativereduce.actor;
-
-import com.ccc.sendalyzeit.textanalytics.ml.scaleout.iterativereduce.jblas.UpdateableMatrix;
-
-public interface EpochDoneListener {
-
-	void epochComplete(UpdateableMatrix result);
-}
