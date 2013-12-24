b155fe51a9027bd9442988126fdb34f722f682d4
agibsonccc <agibson@clevercloudcomputing.com>
2013-12-23 21:48:13 -0800

    documentation and clean up of rbms
    Former-commit-id: 3108a0f7ec71193a82af4443f929c684153dbd13

--- deeplearning4j-parent/deeplearning4j-core/src/main/java/com/ccc/sendalyzeit/textanalytics/algorithms/deeplearning/rbm/matrix/jblas/RBM.java
+++ deeplearning4j-parent/deeplearning4j-core/src/main/java/com/ccc/sendalyzeit/textanalytics/algorithms/deeplearning/rbm/matrix/jblas/RBM.java
@@ -14,6 +14,11 @@ import com.ccc.sendalyzeit.textanalytics.util.MatrixUtil;
 
 /**
  * Restricted Boltzmann Machine
+ * 
+ * 
+ * Based on Hinton et al.'s work 
+ * 
+ * 
  * @author Adam Gibson
  *
  */
@@ -42,42 +47,65 @@ public class RBM extends BaseNeuralNetwork {
 	public void contrastiveDivergence(double learningRate,int k,DoubleMatrix input) {
 		if(input != null)
 			this.input = input;
+		
+		/*
+		 * Cost and updates dictionary.
+		 * This is the update rules for weights and biases
+		 */
 		Pair<DoubleMatrix,DoubleMatrix> probHidden = this.sampleHiddenGivenVisible(this.input);
         
+		/*
+		 * Start the gibbs sampling.
+		 */
 		DoubleMatrix chainStart = probHidden.getSecond();
+		
+		
 		Pair<Pair<DoubleMatrix,DoubleMatrix>,Pair<DoubleMatrix,DoubleMatrix>> matrices = null;
+		//negative visble means or expected values
 		DoubleMatrix nvMeans = null;
+		//negative value samples
 		DoubleMatrix nvSamples = null;
+		//negative hidden means or expected values
 		DoubleMatrix nhMeans = null;
+		//negative hidden samples
 		DoubleMatrix nhSamples = null;
+		
+		/*
+		 * K steps of gibbs sampling. THis is the positive phase of contrastive divergence.
+		 */
+		
 		for(int i = 0; i < k; i++) {
 
-
+			
 			if(i == 0) 
 				matrices = gibbhVh(chainStart);
 			else
 				matrices = gibbhVh(nhSamples);
-
+			//get the cost updates for sampling in the chain after k iterations
 			nvMeans = matrices.getFirst().getFirst();
 			nvSamples = matrices.getFirst().getSecond();
 			nhMeans = matrices.getSecond().getFirst();
 			nhSamples = matrices.getSecond().getSecond();
 		}
 
+		
+		//input times the positive hidden sample
 		DoubleMatrix inputTimesPhSample =  this.input.transpose().mmul(probHidden.getSecond());
+		//negative samples times the negative hidden means
 		DoubleMatrix nvSamplesTTimesNhMeans = nvSamples.transpose().mmul(nhMeans);
+		//delta: input times the positive hidden sample - negative visible samples * negative hidden means
 		DoubleMatrix diff = inputTimesPhSample.sub(nvSamplesTTimesNhMeans);
 		DoubleMatrix wAdd = diff.mul(learningRate);
-
+		//update rule
 		W = W.add(wAdd);
 
-
+		//update rule: the expected values of the input - the negative samples adjusted by the learning rate
 		DoubleMatrix  vBiasAdd = MatrixUtil.mean(this.input.sub(nvSamples), 0);
 		vBias = vBiasAdd.mul(learningRate);
 
 
+		//update rule: the expected values of the hidden input - the negative hideen  means adjusted by the learning reate
 		DoubleMatrix hBiasAdd = MatrixUtil.mean(probHidden.getSecond().sub(nhMeans), 0);
-
 
 		hBiasAdd = hBiasAdd.mul(learningRate);
 
@@ -108,13 +136,12 @@ public class RBM extends BaseNeuralNetwork {
 	}
 
 
-	public DoubleMatrix reconstruct(DoubleMatrix v) {
-		DoubleMatrix h = MatrixUtil.sigmoid(v.mmul(W).addRowVector(hBias));
-		DoubleMatrix reconstructedV = MatrixUtil.sigmoid(h.mmul(W.transpose()).addRowVector(vBias));
-		return reconstructedV;
-	}
 
-
+	/**
+	 * Binomial sampling of the hidden values given visible
+	 * @param v the visible values
+	 * @return a binomial distribution containing the expected values and the samples
+	 */
 	public Pair<DoubleMatrix,DoubleMatrix> sampleHiddenGivenVisible(DoubleMatrix v) {
 		DoubleMatrix h1Mean = propUp(v);
 		DoubleMatrix h1Sample = MatrixUtil.binomial(h1Mean, 1, rng);
@@ -122,6 +149,11 @@ public class RBM extends BaseNeuralNetwork {
 
 	}
 
+	/**
+	 * Gibbs sampling step: hidden ---> visible ---> hidden
+	 * @param h
+	 * @return
+	 */
 	public Pair<Pair<DoubleMatrix,DoubleMatrix>,Pair<DoubleMatrix,DoubleMatrix>> gibbhVh(DoubleMatrix h) {
 		Pair<DoubleMatrix,DoubleMatrix> v1MeanAndSample = this.sampleVGivenH(h);
 		DoubleMatrix vSample = v1MeanAndSample.getSecond();
@@ -130,7 +162,11 @@ public class RBM extends BaseNeuralNetwork {
 	}
 
 
-
+	/**
+	 * Guess the visible values given the hidden
+	 * @param h
+	 * @return
+	 */
 	public Pair<DoubleMatrix,DoubleMatrix> sampleVGivenH(DoubleMatrix h) {
 		DoubleMatrix v1Mean = propDown(h);
 		DoubleMatrix v1Sample = MatrixUtil.binomial(v1Mean, 1, rng);
@@ -144,13 +180,30 @@ public class RBM extends BaseNeuralNetwork {
 		return MatrixUtil.sigmoid(preSig);
 
 	}
-
+	
+	/**
+	 * Propagates hidden down to visible
+	 * @param h the hidden layer
+	 * @return the approximated output of the hidden layer
+	 */
 	public DoubleMatrix propDown(DoubleMatrix h) {
 		DoubleMatrix preSig = h.mmul(W.transpose()).addRowVector(vBias);
 		return MatrixUtil.sigmoid(preSig);
 
 	}
 
+	/**
+	 * Reconstructs the visible input.
+	 * A reconstruction is a propdown of the reconstructed hidden input.
+	 * @param v the visible input
+	 * @return
+	 */
+	public DoubleMatrix reconstruct(DoubleMatrix v) {
+		//sigmoid(visible * weights + hidden bias
+		DoubleMatrix h = MatrixUtil.sigmoid(v.mmul(W).addRowVector(hBias));
+		//reconstructed: sigmoid(hidden * Weights + the visible bias
+		return propDown(h);
+	}
 
 	public static class Builder extends BaseNeuralNetwork.Builder<RBM> {
 		public Builder() {
