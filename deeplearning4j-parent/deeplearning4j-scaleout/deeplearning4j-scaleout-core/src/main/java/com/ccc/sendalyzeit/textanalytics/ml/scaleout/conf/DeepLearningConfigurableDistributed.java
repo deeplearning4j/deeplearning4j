6d3d59234cfab21d0a34097916221df4cfa517d7
agibsonccc <agibson@clevercloudcomputing.com>
2013-12-26 09:35:40 -0800

    fix reconstruction error measure

--- deeplearning4j-parent/deeplearning4j-scaleout/deeplearning4j-scaleout-core/src/main/java/com/ccc/sendalyzeit/textanalytics/ml/scaleout/conf/DeepLearningConfigurableDistributed.java
+++
@@ -1,7 +0,0 @@
-package com.ccc.sendalyzeit.textanalytics.ml.scaleout.conf;
-
-public interface DeepLearningConfigurableDistributed extends DeepLearningConfigurable {
-
-	/* A master url for service discovery*/
-	public final static String MASTER_URL = "masterurl";
-}
