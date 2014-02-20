ff98eaa5c4278255fb0acb10423b975961bc7366
agibsonccc <agibson@clevercloudcomputing.com>
2014-02-19 17:04:58 -0800

    cleanup

--- deeplearning4j-parent/deeplearning4j-scaleout/deeplearning4j-scaleout-core/src/main/java/com/ccc/deeplearning/scaleout/core/conf/DeepLearningConfigurableDistributed.java
+++
@@ -1,9 +0,0 @@
-package com.ccc.deeplearning.scaleout.core.conf;
-
-import com.ccc.deeplearning.scaleout.conf.DeepLearningConfigurable;
-
-public interface DeepLearningConfigurableDistributed extends DeepLearningConfigurable {
-
-	/* A master url for service discovery*/
-	public final static String MASTER_URL = "masterurl";
-}
