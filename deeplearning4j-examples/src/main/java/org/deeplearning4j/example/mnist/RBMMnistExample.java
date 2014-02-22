b0785587bc628d922ab5b90e30b84c7f581118a5
agibsonccc <agibson@clevercloudcomputing.com>
2014-02-22 15:53:56 -0800

    added examples
    Former-commit-id: cfe65e9ee643a499f2752bbe160880a454bcfce7

---
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/mnist/RBMMnistExample.java
@@ -0,0 +1,56 @@
+package org.deeplearning4j.example.mnist;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.datasets.mnist.draw.DrawMnistGreyScale;
+import org.deeplearning4j.rbm.RBM;
+import org.deeplearning4j.util.MatrixUtil;
+import org.jblas.DoubleMatrix;
+
+public class RBMMnistExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		RBM r = new RBM.Builder().numberOfVisible(784).numHidden(500).build();
+		//batches of 10, 60000 examples total
+		DataSetIterator iter = new MnistDataSetIterator(10,60000);
+
+		while(iter.hasNext()) {
+			DataSet next = iter.next();
+			//train with k = 1 0.01 learning rate and 1000 epochs
+			r.trainTillConvergence(next.getFirst(), 0.01, new Object[]{1,0.01,1000});
+		}
+
+		iter.reset();
+
+		//Iterate over the data set after done training and show the 2 side by side (you have to drag the test image over to the right)
+		while(iter.hasNext()) {
+			DataSet first = iter.next();
+			DoubleMatrix reconstruct = r.reconstruct(first.getFirst());
+			for(int j = 0; j < first.numExamples(); j++) {
+
+				DoubleMatrix draw1 = first.get(j).getFirst().mul(255);
+				DoubleMatrix reconstructed2 = reconstruct.getRow(j);
+				DoubleMatrix draw2 = MatrixUtil.binomial(reconstructed2,1,new MersenneTwister(123)).mul(255);
+
+				DrawMnistGreyScale d = new DrawMnistGreyScale(draw1);
+				d.title = "REAL";
+				d.draw();
+				DrawMnistGreyScale d2 = new DrawMnistGreyScale(draw2,100,100);
+				d2.title = "TEST";
+				d2.draw();
+				Thread.sleep(10000);
+				d.frame.dispose();
+				d2.frame.dispose();
+			}
+
+
+		}
+
+	}
+
+}
