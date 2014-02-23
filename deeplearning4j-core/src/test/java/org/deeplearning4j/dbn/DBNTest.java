5b3fb1efba820ce7f4f66187d571bd7773b95fdb
agibsonccc <agibson@clevercloudcomputing.com>
2014-02-22 17:12:14 -0800

    updated

--- deeplearning4j-core/src/test/java/org/deeplearning4j/dbn/DBNTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/dbn/DBNTest.java
@@ -43,8 +43,7 @@ public class DBNTest  extends DeepLearningTest {
 		int fineTuneEpochs = 1000;
 
 		DBN dbn = new DBN.Builder()
-		.transformWeightsAt(0, new MultiplyScalar(1000))
-		.transformWeightsAt(1, new MultiplyScalar(100)).hiddenLayerSizes(hiddenLayerSizes)
+    .hiddenLayerSizes(hiddenLayerSizes)
 		.numberOfInputs(nIns).renderWeights(0)
 		.useRegularization(false).withMomentum(0).withDist(new NormalDistribution(0,0.001))
 		.numberOfOutPuts(nOuts).withRng(rng).build();
