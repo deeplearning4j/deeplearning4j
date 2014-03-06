e371bfd1de66dfccd6373c7a37e60f9372ac2f60
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-05 18:42:53 -0800

    update

---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/org/deeplearning4j/iterativereduce/actor/core/NeedsModelMessage.java
@@ -0,0 +1,13 @@
+package org.deeplearning4j.iterativereduce.actor.core;
+
+import java.io.Serializable;
+
+public class NeedsModelMessage  implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1282144180750694842L;
+
+	
+}
