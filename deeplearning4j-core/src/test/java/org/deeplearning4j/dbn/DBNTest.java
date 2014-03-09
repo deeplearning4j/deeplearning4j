70109d0e5da8451fe4367ae350587e4a5b275c19
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-08 20:44:26 -0800

    added done message which will tell the master when the iterator is done, this hook allows the master to respond better

--- deeplearning4j-core/src/test/java/org/deeplearning4j/dbn/DBNTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/dbn/DBNTest.java
@@ -1,12 +1,18 @@
 package org.deeplearning4j.dbn;
+import static org.junit.Assert.*;
+import java.io.IOException;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.random.MersenneTwister;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
 import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.activation.HardTanh;
+import org.deeplearning4j.transformation.MultiplyScalar;
 import org.deeplearning4j.util.MatrixUtil;
 import org.jblas.DoubleMatrix;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,32 +25,27 @@ public class DBNTest {
 
 
 	@Test
+	@Ignore
 	public void testDBN() {
 
-		int n = 10;
-		DataSet d = MatrixUtil.xorData(n);
+		int n = 50;
+		DataSet d = MatrixUtil.xorData(n,500);
 		DoubleMatrix x = d.getFirst();
 		DoubleMatrix y = d.getSecond();
 
 
-
-
-
-		RandomGenerator rng = new MersenneTwister(123);
-
-		double preTrainLr = 0.001;
+		double preTrainLr = 0.0001;
 		int preTrainEpochs = 1000;
 		int k = 1;
-		int nIns = 2,nOuts = 2;
-		int[] hiddenLayerSizes = new int[] {2,2,2};
-		double fineTuneLr = 0.001;
+		int[] hiddenLayerSizes = new int[] {400,250,100};
+		double fineTuneLr = 0.0001;
 		int fineTuneEpochs = 1000;
 
-		DBN dbn = new DBN.Builder()
-    .hiddenLayerSizes(hiddenLayerSizes)
-		.numberOfInputs(nIns).renderWeights(0)
-		.useRegularization(false).withMomentum(0).withDist(new NormalDistribution(0,0.001))
-		.numberOfOutPuts(nOuts).withRng(rng).build();
+		DBN dbn = new DBN.Builder().forceEpochs()
+				.hiddenLayerSizes(hiddenLayerSizes)
+				.numberOfInputs(d.numInputs())
+				.useRegularization(false)
+				.numberOfOutPuts(d.numOutcomes()).build();
 
 		dbn.pretrain(x,k, preTrainLr, preTrainEpochs);
 		dbn.finetune(y,fineTuneLr, fineTuneEpochs);
@@ -69,6 +70,7 @@ public class DBNTest {
 	}
 
 	@Test
+	@Ignore
 	public void testCDBN() {
 		DoubleMatrix x = new DoubleMatrix( new double[][] 
 				{{0.4, 0.5, 0.5, 0.,  0.,  0.},
@@ -88,18 +90,18 @@ public class DBNTest {
 
 		RandomGenerator rng = new MersenneTwister(123);
 
-		double preTrainLr = 0.01;
+		double preTrainLr = 0.001;
 		int preTrainEpochs = 1000;
 		int k = 1;
 		int nIns = 6,nOuts = 2;
-		int[] hiddenLayerSizes = new int[] {5,5};
-		double fineTuneLr = 0.01;
+		int[] hiddenLayerSizes = new int[] {20,10,5};
+		double fineTuneLr = 0.001;
 		int fineTuneEpochs = 200;
 
 		CDBN dbn = new CDBN.Builder()
 		.numberOfInputs(nIns).numberOfOutPuts(nOuts)
 		.hiddenLayerSizes(hiddenLayerSizes).useRegularization(false)
-		.withRng(rng).withL2(0.1).renderWeights(1000)
+		.withRng(rng)
 		.build();
 		dbn.pretrain(x,k, preTrainLr, preTrainEpochs);
 		dbn.finetune(y,fineTuneLr, fineTuneEpochs);
@@ -110,8 +112,31 @@ public class DBNTest {
 				{0., 0., 0., 0.5, 0.5, 0.},
 				{0.5, 0.5, 0.5, 0.5, 0.5, 0.}});
 
-		log.info(dbn.predict(testX).toString());
 
+		Evaluation eval = new Evaluation();
+		eval.eval(y, dbn.predict(x));
+		log.info(eval.stats());
+
+	}
+
+
+
+	@Test
+	public void testMnist() throws IOException {
+		MnistDataFetcher fetcher = new MnistDataFetcher();
+		fetcher.fetch(20);
+		DataSet d = fetcher.next();
+		assertEquals(20,d.numExamples());
+
+		DBN dbn = new DBN.Builder().hiddenLayerSizes(new int[]{500,250,100}).withActivation(new HardTanh())
+				.numberOfInputs(784).numberOfOutPuts(10).useRegularization(false).build();
+		dbn.pretrain(d.getFirst(), 1, 0.0001, 30000);
+		dbn.finetune(d.getSecond(), 0.0001, 10000);
+
+		Evaluation eval = new Evaluation();
+		DoubleMatrix predict = dbn.predict(d.getFirst());
+		eval.eval(d.getSecond(), predict);
+		log.info(eval.stats());
 	}
 
 
