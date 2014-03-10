a854579cf41dc74ef12d705020dee68a21a6e7d9
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-10 14:06:23 -0700

    fixed cluster communincation in network runner
    Former-commit-id: e79751a66fba92d8db55ab7a442299b8b5befea6

--- deeplearning4j-examples/bin/run-worker.sh
+++ deeplearning4j-examples/bin/run-worker.sh
@@ -3,10 +3,11 @@
 if [ -z "$1" ]
   then
     echo "Please specify a host"
+    exit 1
 fi
 
 MAX_HEAP="5g"
 MIN_HEAP="5g"
 
 
-java -cp "lib/*"   -Xmx$MAX_HEAP -Xms$MIN_HEAP -server -XX:+UseTLAB   -Dakka.remote.netty.tcp.hostname=train2 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:MaxTenuringThreshold=0 -XX:CMSInitiatingOccupancyFraction=60  -XX:+CMSParallelRemarkEnabled -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled com.blix.deeplearning.akka.scaleout.ActorNetworkRunnerApp  -h $1 -t worker
+java -cp "lib/*"   -Xmx$MAX_HEAP -Xms$MIN_HEAP -server -XX:+UseTLAB   -Dakka.remote.netty.tcp.hostname=train2 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:MaxTenuringThreshold=0 -XX:CMSInitiatingOccupancyFraction=60  -XX:+CMSParallelRemarkEnabled -XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled org.deeplearning4j.iterativereduce.actor.multilayer.ActorNetworkRunnerApp  -h $1 -t worker
