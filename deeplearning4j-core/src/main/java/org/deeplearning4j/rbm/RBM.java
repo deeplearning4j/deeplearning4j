40e247a2bc36a1ae76cabc18ffd1c0fdb320014b
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-12 13:25:07 -0700

    found adagrad trick
    Former-commit-id: 3c239c825595e00919ede0e617a63bc21436b04e

--- deeplearning4j-core/src/main/java/org/deeplearning4j/rbm/RBM.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/rbm/RBM.java
@@ -79,9 +79,9 @@ public class RBM extends BaseNeuralNetwork {
 		if(input != null)
 			this.input = input;
 		NeuralNetworkGradient gradient = getGradient(new Object[]{k});
-		W.subi(gradient.getwGradient());
-		hBias.subi(gradient.gethBiasGradient());
-		vBias.subi(gradient.getvBiasGradient());
+		W.addi(gradient.getwGradient());
+		hBias.addi(gradient.gethBiasGradient());
+		vBias.addi(gradient.getvBiasGradient());
 
 	}
 
