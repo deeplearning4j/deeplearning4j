e6bc75a4687bacb2bb64be0df40d1aab61ecd457
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-14 10:01:52 -0700

    forgot to merge back reconstruction entropy change
    Former-commit-id: 78f8dc30e885db1dba00162b418fc6d812ea644b

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/gradient/GradientAverager.java
@@ -0,0 +1,23 @@
+package org.deeplearning4j.gradient;
+
+import org.deeplearning4j.nn.gradient.NeuralNetworkGradient;
+
+/**
+ * Calculates the change in the gradients over time.
+ * @author Adam Gibson
+ *
+ */
+public class GradientAverager implements NeuralNetworkGradientListener {
+
+	
+	
+	
+	@Override
+	public void onGradient(NeuralNetworkGradient gradient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ deeplearning4j-instrumentation/src/main/java/org/deeplearning4j/instrumentation/gradient/ParallelGradientStatistics.java
@@ -0,0 +1,37 @@
+package org.deeplearning4j.instrumentation.gradient;
+
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.RawMnistDataSetIterator;
+import org.deeplearning4j.dbn.DBN;
+import org.deeplearning4j.iterativereduce.actor.multilayer.ActorNetworkRunner;
+import org.deeplearning4j.scaleout.conf.Conf;
+
+public class ParallelGradientStatistics {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		//batches of 10, 60000 examples total
+				DataSetIterator iter = new RawMnistDataSetIterator(80,60000);
+				
+				
+				Conf c = new Conf();
+				c.setFinetuneEpochs(10000);
+				c.setFinetuneLearningRate(0.01);
+				c.setLayerSizes(new int[]{500,400,250});
+				c.setnIn(784);
+				c.setUseAdaGrad(false);
+				//c.setRenderWeightEpochs(1000);
+				c.setnOut(10);
+				c.setSplit(10);
+				c.setMultiLayerClazz(DBN.class);
+				c.setUseRegularization(false);
+				c.setDeepLearningParams(new Object[]{1,0.01,1000});
+				c.setRenderWeightEpochs(1000);
+				ActorNetworkRunner runner = new ActorNetworkRunner("master",iter);
+				runner.setup(c);
+				runner.train();
+	}
+
+}
