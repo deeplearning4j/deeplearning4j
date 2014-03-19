42e87b22505720e0d29e867c27c0cfe02da9af93
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-19 12:52:42 -0700

    update

---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/topicmodeling/CountVectorizer.java
@@ -0,0 +1,94 @@
+package org.deeplearning4j.topicmodeling;
+
+import org.deeplearning4j.berkeley.Counter;
+import org.deeplearning4j.word2vec.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.word2vec.tokenizer.Tokenizer;
+import org.deeplearning4j.word2vec.tokenizer.TokenizerFactory;
+import org.deeplearning4j.word2vec.viterbi.Index;
+import org.jblas.DoubleMatrix;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Given a {@link SentenceIterator} and a {@link TokenizerFactory}
+ * and an {@link Index} constructs a word count vector
+ * @author Adam Gibson
+ *
+ */
+public class CountVectorizer {
+
+	private SentenceIterator iter;
+	private TokenizerFactory tokenizerFactory;
+	private Index wordsToCount;
+	private static Logger log = LoggerFactory.getLogger(CountVectorizer.class);
+	
+	public CountVectorizer(SentenceIterator iter,
+			TokenizerFactory tokenizerFactory, Index wordsToCount) {
+		super();
+		this.iter = iter;
+		this.tokenizerFactory = tokenizerFactory;
+		this.wordsToCount = wordsToCount;
+	}
+	
+
+	public CountVectorizer(SentenceIterator iter,
+			TokenizerFactory tokenizerFactory) {
+		super();
+		this.iter = iter;
+		this.tokenizerFactory = tokenizerFactory;
+		this.wordsToCount = new Index();
+	}
+	
+	
+	public DoubleMatrix toVector() {
+		DoubleMatrix d = new DoubleMatrix(1,wordsToCount.size());
+		Counter<String> wordFrequencies = new Counter<String>();
+		while(iter.hasNext()) {
+			String sentence = iter.nextSentence();
+			if(sentence == null)
+				continue;
+			Tokenizer t = tokenizerFactory.create(sentence);
+			while(t.hasMoreTokens()) {
+				String token = t.nextToken();
+				log.info("Token " + token);
+				if(wordsToCount.indexOf(token) >= 0 || wordsToCount.size() < 1) 
+					wordFrequencies.incrementCount(token, 1.0);
+					
+			}
+		}
+		
+		for(int i = 0; i < wordsToCount.size(); i++) {
+			d.put(i,wordFrequencies.getCount(wordsToCount.get(i).toString()));
+		}
+		
+		return d;
+	}
+	
+	public DoubleMatrix toBinaryVector() {
+		DoubleMatrix d = new DoubleMatrix(1,wordsToCount.size());
+		Counter<String> wordFrequencies = new Counter<String>();
+		while(iter.hasNext()) {
+			String sentence = iter.nextSentence();
+			if(sentence == null)
+				continue;
+			Tokenizer t = tokenizerFactory.create(sentence);
+			while(t.hasMoreTokens()) {
+				String token = t.nextToken();
+				log.info("Token " + token);
+				if(wordsToCount.indexOf(token) >= 0 || wordsToCount.size() < 1) 
+					wordFrequencies.incrementCount(token, 1.0);
+					
+			}
+		}
+		
+		for(int i = 0; i < wordsToCount.size(); i++) {
+			double count = wordFrequencies.getCount(wordsToCount.get(i).toString());
+			d.put(i,count > 0 ? 1 : 0);
+		}
+		
+		return d;
+	}
+	
+	
+	
+}
---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/topicmodeling/TopicModelingDataSetIterator.java
@@ -0,0 +1,204 @@
+package org.deeplearning4j.topicmodeling;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.iterativereduce.actor.core.SerializableFileIter;
+import org.deeplearning4j.util.MatrixUtil;
+import org.deeplearning4j.word2vec.viterbi.Index;
+import org.jblas.DoubleMatrix;
+
+
+public class TopicModelingDataSetIterator implements DataSetIterator {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5137504154841977611L;
+	private List<String> labels;
+	private Index vocab = new Index();
+	private VocabCreator vocabCreator;
+	private SerializableFileIter files;
+	private int batchSize = 1;
+
+	/**
+	 * 
+	 * @param rootDir the root directory to get data from
+	 * @param numWords the number of words to set as vocab
+	 * @param batchSize the batch size to train on
+	 */
+	@SuppressWarnings("unchecked")
+	public TopicModelingDataSetIterator(File rootDir,int numWords,int batchSize) {
+		File[] subs = rootDir.listFiles();
+		if(subs == null)
+			throw new IllegalArgumentException("Root directory has no files");
+		
+		
+		labels = new ArrayList<String>(subs.length);
+		for(File f : subs) {
+			labels.add(f.getName());
+		}
+		
+
+		this.vocabCreator = new VocabCreator(rootDir);
+		vocab = vocabCreator.createVocab(numWords);
+		Iterator<File> iter = FileUtils.iterateFiles(rootDir, null, true);
+		List<File> list = new ArrayList<File>();
+		while(iter.hasNext()) 
+			list.add(iter.next());
+
+		
+		Collections.shuffle(list);
+		files = new SerializableFileIter(list);
+
+
+	}
+
+	
+	/**
+	 * This assumes a previously persisted vocab creator was used.
+	 * @param rootDir the root directory to train on
+	 * @param batchSize the batch size, or number of rows
+	 * in the data set
+	 * @param vocabCreator the vocab creator 
+	 */
+	@SuppressWarnings("unchecked")
+	public TopicModelingDataSetIterator(File rootDir,int batchSize,VocabCreator vocabCreator) {
+		File[] subs = rootDir.listFiles();
+		if(subs == null)
+			throw new IllegalArgumentException("Root directory has no files");
+		
+		
+		labels = new ArrayList<String>(subs.length);
+		this.batchSize = batchSize;
+		for(File f : subs) {
+			labels.add(f.getName());
+		}
+		
+
+		this.vocabCreator = vocabCreator;
+		vocab = vocabCreator.getCurrVocab();
+		Iterator<File> iter = FileUtils.iterateFiles(rootDir, null, true);
+		List<File> list = new ArrayList<File>();
+		while(iter.hasNext()) 
+			list.add(iter.next());
+
+		
+		Collections.shuffle(list);
+		files = new SerializableFileIter(list);
+
+
+	}
+
+	@Override
+	public  boolean hasNext() {
+		return files.hasNext();
+
+
+	}
+
+	@Override
+	public DataSet next() {
+		int curr = 0;
+		if(!hasNext())
+			throw new IllegalStateException("Unable to get next; no more data found");
+
+		List<DataSet> d = new ArrayList<DataSet>();
+		while(files.hasNext() && curr  < batchSize) {
+			File next = files.next();
+			DoubleMatrix input = vocabCreator.getScoreMatrix(next);
+			if(input.sum() != 0) {
+				String label = next.getParentFile().getName();
+				DoubleMatrix y = MatrixUtil.toOutcomeVector(labels.indexOf(label), labels.size());
+				d.add(new DataSet(input,y));
+				curr++;
+			}
+
+
+		}
+
+		return DataSet.merge(d);
+
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int totalExamples() {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public int inputColumns() {
+		return vocab.size();
+	}
+
+	@Override
+	public int totalOutcomes() {
+		return this.labels.size();
+	}
+
+	@Override
+	public void reset() {
+		files.setCurr(0);
+	}
+
+	@Override
+	public int batch() {
+		return batchSize;
+	}
+
+	@Override
+	public int cursor() {
+		throw new UnsupportedOperationException();
+
+	}
+
+	@Override
+	public int numExamples() {
+		throw new UnsupportedOperationException();
+
+	}
+
+
+	@Override
+	public DataSet next(int num) {
+		int curr = 0;
+		if(!hasNext())
+			throw new IllegalStateException("Unable to get next; no more data found");
+
+		List<DataSet> d = new ArrayList<DataSet>();
+		while(files.hasNext() && curr  < num) {
+			File next = files.next();
+			DoubleMatrix input = vocabCreator.getScoreMatrix(next);
+			if(input.sum() != 0) {
+				String label = next.getParentFile().getName();
+				DoubleMatrix y = MatrixUtil.toOutcomeVector(labels.indexOf(label), labels.size());
+				d.add(new DataSet(input,y));
+				curr++;
+			}
+
+
+		}
+
+		return DataSet.merge(d);
+
+	}
+
+	
+
+
+
+
+
+}
