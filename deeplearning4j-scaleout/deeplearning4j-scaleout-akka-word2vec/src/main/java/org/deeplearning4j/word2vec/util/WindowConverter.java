a78d785b64dd5022516e5e41184e56aa7f706ee9
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-20 17:48:43 -0700

    changed rerouting logic
    Former-commit-id: fba7198121819b94f9f22fca2578cd68bf077320

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/util/WindowConverter.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/util/WindowConverter.java
@@ -7,9 +7,29 @@ import org.deeplearning4j.word2vec.ner.InputHomogenization;
 import org.jblas.DoubleMatrix;
 
 
-
+/**
+ * Util methods for converting windows to 
+ * training examples
+ * @author Adam Gibson    
+ *
+ */
 public class WindowConverter {
 	
+	/**
+	 * Converts a window (each word in the window)
+	 * 
+	 * in to a vector.
+	 * 
+	 * Keep in mind each window is a multi word context.
+	 * 
+	 * From there, each word uses the passed in model
+	 * as a lookup table to get what vectors are relevant
+	 * to the passed in windows
+	 * @param window the window to take in.
+	 * @param vec the model to use as a lookup table
+	 * @return a concacneated 1 row array
+	 * containing all of the numbers for each word in the window
+	 */
 	public static double[] asExample(Window window,Word2Vec vec) {
 		int length = vec.getLayerSize();
 		List<String> words = window.getWords();
