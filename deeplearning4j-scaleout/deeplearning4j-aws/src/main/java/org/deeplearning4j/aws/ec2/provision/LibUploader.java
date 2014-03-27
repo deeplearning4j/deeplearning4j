3cc5f75b1b9f17868ab18b0b6cd72ed3cb82ec93
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-26 21:42:57 -0700

    close channels and some more touchups for remote deployment
    Former-commit-id: 25f626861a288d2814247f62c554170614d780ad [formerly 6a6f4c5da4d966d7de008daa695369af2c2c1177]
    Former-commit-id: 095230873bc6b571fbc46eb08e2bcac1cdfcc99a

---
+++ deeplearning4j-scaleout/deeplearning4j-aws/src/main/java/org/deeplearning4j/aws/ec2/provision/LibUploader.java
@@ -0,0 +1,245 @@
+package org.deeplearning4j.aws.ec2.provision;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserInfo;
+/**
+ * Meant for uploading files to remote servers
+ * @author Adam Gibson
+ *
+ */
+public class LibUploader implements UserInfo {
+
+	private String host;
+	private JSch jsch;
+	private String user;
+	private int port = 22;
+	private String password;
+	private static Logger log = LoggerFactory.getLogger(LibUploader.class);
+
+	/**
+	 * 
+	 * @param host host to connect to (public facing dns)
+	 * @param user the user to connect with (default root otherwise)
+	 * @param password the password to use if any
+	 * @param port the port to connect to(default 22)
+	 */
+	public LibUploader(String host,String user,String password,int port) {
+		super();
+		this.host = host;
+		this.user = user;
+		this.port = port;
+		this.password = password;
+		jsch = new JSch();
+
+	}
+
+	/**
+	 * Connects to port 22
+	 * @param host host to connect to (public facing dns)
+	 * @param user the user to connect with (default root otherwise)
+	 * @param password the password to use if any
+	 */
+	public LibUploader(String host,String user,String password) {
+		this(host,user,password,22);
+	}
+	/**
+	 * Connects to port 22
+	 * @param host host to connect to (public facing dns)
+	 * @param user the user to connect with (default root otherwise)
+	 * @param password the password to use if any
+	 */
+	public LibUploader(String host,String user) {
+		this(host,user,"",22);
+	}
+
+	/**
+	 * Connects to port 22, user root, with no password
+	 * @param host host to connect to (public facing dns)
+	 */
+	public LibUploader(String host) {
+		this(host,"root","",22);
+	}
+
+
+	public void uploadAndRun(String script,String rootDir) throws Exception {
+		String remoteName = rootDir.isEmpty() ? new File(script).getName() : rootDir + "/" + script;
+		upload(new File(script),remoteName);
+		Session session = getSession();
+		session.connect();
+		ChannelExec channel = (ChannelExec) session.openChannel("exec");
+		
+		String remoteCommand = remoteName.charAt(0) != '/' ? "./" + remoteName : remoteName;
+		
+		channel.setCommand("chmod +x " + remoteCommand + " && " + remoteCommand);
+		channel.setErrStream(System.err);
+		channel.setPty(true);
+
+		channel.setOutputStream(System.out);
+		channel.connect();
+		channel.start();
+		InputStream input = channel.getInputStream();
+		//start reading the input from the executed commands on the shell
+		byte[] tmp = new byte[1024];
+		while (true) {
+			while (input.available() > 0) {
+				int i = input.read(tmp, 0, 1024);
+				if (i < 0) break;
+				log.info(new String(tmp, 0, i));
+			}
+			if (channel.isClosed()){
+				log.info("exit-status: " + channel.getExitStatus());
+				break;
+			}
+			Thread.sleep(1000);
+		}
+		
+		channel.disconnect();
+		session.disconnect();
+
+	}
+
+
+	private Session getSession() throws Exception {
+		Session session = jsch.getSession(user, host, port);
+		session.setUserInfo(this);
+		return session;
+	}
+
+	/**
+	 * Creates the directory for the file
+	 * @param from the directory to upload from
+	 * @param to the destination directory on the remote server
+	 * @throws Exception
+	 */
+	public void uploadForDeployment(String from,String to) throws Exception {
+		mkDir(to);
+		upload(from,to);
+
+
+	}
+
+	public void addKeyFile(String keyFile) throws Exception {
+		jsch.addIdentity(keyFile);
+	}
+
+	//creates the directory to upload to
+	private void mkDir(String dir) throws Exception {
+		Session session = getSession();
+		session.connect();
+		Channel channel = session.openChannel("sftp");
+		channel.connect();
+
+		ChannelSftp c = (ChannelSftp) channel;
+		c.mkdir(dir);
+		c.exit();
+		session.disconnect();
+	}
+
+	//uploads the file
+	private void upload(String rootDir,String uploadRootDir) throws Exception {
+		upload(Arrays.asList(new File(rootDir).listFiles()),uploadRootDir);
+	}
+
+	private void upload(Collection<File> files,String rootDir) throws Exception {
+		Session session = getSession();
+		session.connect();
+		Channel channel = session.openChannel("sftp");
+		channel.connect();
+
+		ChannelSftp c = (ChannelSftp) channel;
+		for(File f : files) {
+			if(f.isDirectory()) {
+				log.warn("Skipping " + f.getName());
+				continue;
+			}
+			BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+			c.put(bis, rootDir + "/" + f.getName());
+			bis.close();
+
+		}
+
+		channel.disconnect();
+		session.disconnect();
+
+
+	}
+
+	private void upload(File f,String remoteFile) throws Exception {
+		Session session = getSession();
+		int numRetries = 0;
+		while(numRetries < 3 && !session.isConnected()) {
+			try {
+				session.connect();
+				Thread.sleep(10000);
+			}catch(Exception e) {
+				numRetries++;
+			}
+		}
+		
+		
+		Channel channel = session.openChannel("sftp");
+		channel.connect();
+
+		ChannelSftp c = (ChannelSftp) channel;
+
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+		c.put(bis,remoteFile);
+		bis.close();
+		c.exit();
+		session.disconnect();
+	}
+
+
+
+	@Override
+	public String getPassphrase() {
+		return this.password;
+	}
+
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+
+	@Override
+	public boolean promptPassphrase(String arg0) {
+		return true;
+	}
+
+
+	@Override
+	public boolean promptPassword(String arg0) {
+		return true;
+
+	}
+
+
+	@Override
+	public boolean promptYesNo(String arg0) {
+		return true;
+
+	}
+
+
+	@Override
+	public void showMessage(String arg0) {
+		log.info(arg0);
+	}
+
+}
