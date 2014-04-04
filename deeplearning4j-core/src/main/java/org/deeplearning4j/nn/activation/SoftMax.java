9612662f7d989fbd1a35b0551f93624d21cb2283
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-04 10:18:44 -0700

    added some missing classes

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/activation/SoftMax.java
@@ -0,0 +1,30 @@
+package org.deeplearning4j.nn.activation;
+
+import static org.deeplearning4j.util.MatrixUtil.oneMinus;
+import static org.deeplearning4j.util.MatrixUtil.softmax;
+
+import org.jblas.DoubleMatrix;
+/**
+ * Softmax function 
+ * @author Adam Gibson
+ *
+ */
+public class SoftMax implements ActivationFunction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3407472284248637360L;
+
+	@Override
+	public DoubleMatrix apply(DoubleMatrix input) {
+		return softmax(input);
+	}
+
+	@Override
+	public DoubleMatrix applyDerivative(DoubleMatrix input) {
+		return softmax(input).mul(oneMinus(softmax(input)));
+
+	}
+
+}
