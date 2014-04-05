9788737696874c71ddf8bf9dbf41c15e0bdea29f
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-05 13:40:03 -0700

    added cursory tfidf vectorizer still a few more design decisions to make
    Former-commit-id: 7329a2816b3e32148ec9deeec436bc4e568dc82c [formerly bf7be7879c1164a90b8c25853224749e7608eefd]
    Former-commit-id: e339f6c7d49a38d7860a02e0c096c29e9f71609c

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/vectorizer/TfidfVectorizer.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/vectorizer/TfidfVectorizer.java
@@ -1,7 +1,18 @@
 package org.deeplearning4j.word2vec.vectorizer;
 
+import org.deeplearning4j.berkeley.Counter;
 import org.deeplearning4j.datasets.DataSet;
 import org.deeplearning4j.datasets.vectorizer.Vectorizer;
+import org.deeplearning4j.util.MathUtils;
+import org.deeplearning4j.util.SetUtils;
+import org.deeplearning4j.word2vec.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.word2vec.tokenizer.DefaultTokenizerFactory;
+import org.deeplearning4j.word2vec.tokenizer.Tokenizer;
+import org.deeplearning4j.word2vec.tokenizer.TokenizerFactory;
+import org.deeplearning4j.word2vec.viterbi.Index;
+
+import java.util.List;
+import java.util.Collection;
 
 /**
  * Turns a set of documents in to a tfidf bag of words
@@ -9,10 +20,98 @@ import org.deeplearning4j.datasets.vectorizer.Vectorizer;
  */
 public class TfidfVectorizer implements Vectorizer {
 
+    private int minDf;
+    private Index vocab = new Index();
+    private SentenceIterator sentenceIterator;
+    private TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
+    private List<String> labels;
+    private Counter<String> tf = new Counter<>();
+    private Counter<String> idf = new Counter<>();
+    private int numDocs = 0;
+
+    /**
+     *
+     * @param sentenceIterator the document iterator
+     * @param tokenizerFactory the tokenizer for individual tokens
+     * @param labels the possible labels
+     */
+    public TfidfVectorizer(SentenceIterator sentenceIterator,TokenizerFactory tokenizerFactory,List<String> labels) {
+        this.sentenceIterator = sentenceIterator;
+        this.tokenizerFactory = tokenizerFactory;
+        this.labels = labels;
+
+
+    }
+
+    private Collection<String> allWords() {
+        return SetUtils.union(tf.keySet(),idf.keySet());
+    }
+
+
+    private Counter<String> tfIdfWeights(int top) {
+        Counter<String> ret = new Counter<String>();
+        for(String word  : allWords())
+            ret.setMinCount(word,tfidfWord(word));
+        ret.keepTopNKeys(top);
+        return ret;
+    }
+
+
+    private Counter<String> tfIdfWeights() {
+        Counter<String> ret = new Counter<String>();
+        for(String word  : allWords())
+            ret.setMinCount(word,tfidfWord(word));
+        return ret;
+    }
+
+    private void initIndexFromTfIdf() {
+        Counter<String> tfidf = tfIdfWeights();
+        for(String key : tfidf.keySet())
+            this.vocab.add(key);
+    }
+
+
+
+    private double tfidfWord(String word) {
+        return MathUtils.tfidf(tfForWord(word),idfForWord(word));
+    }
+
+
+    private double tfForWord(String word) {
+        return MathUtils.tf((int) tf.getCount(word));
+    }
+
+    private double idfForWord(String word) {
+        return MathUtils.idf(numDocs,idf.getCount(word));
+    }
+
+    private void process() {
+        while(sentenceIterator.hasNext()) {
+            Tokenizer tokenizer = tokenizerFactory.create(sentenceIterator.nextSentence());
+            Counter<String> runningTotal = new Counter<>();
+            Counter<String> documentOccurrences = new Counter<>();
+            numDocs++;
+            for(String token : tokenizer.getTokens()) {
+                runningTotal.incrementCount(token,1.0);
+                //idf
+                if(!documentOccurrences.containsKey(token))
+                    documentOccurrences.setMinCount(token,1.0);
+            }
+
+            idf.incrementAll(documentOccurrences);
+            tf.incrementAll(runningTotal);
+        }
+    }
 
 
     @Override
     public DataSet vectorize() {
+        process();
+        initIndexFromTfIdf();
+        
+        Counter<String> tfIdfWeights = tfIdfWeights();
+
+
         return null;
     }
 }
