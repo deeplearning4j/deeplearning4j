066f08d125358231e4ecfe52c7ab258d097f6811
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-05 15:54:41 -0700

    added label aware uima sentence iterator
    Former-commit-id: d486a1e88045d064d0fb50b7c868f985bfcb4e63

---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/vectorizer/TextVectorizer.java
@@ -0,0 +1,37 @@
+package org.deeplearning4j.word2vec.vectorizer;
+
+import java.io.InputStream;
+import java.io.File;
+
+import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.vectorizer.Vectorizer;
+
+/**
+ * Vectorizes text
+ * @author Adam Gibson
+ */
+public interface TextVectorizer extends Vectorizer {
+    /**
+     * Text coming from an input stream considered as one document
+     * @param is the input stream to read from
+     * @param label the label to assign
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    DataSet vectorize(InputStream is,String label);
+
+    /**
+     * Vectorizes the passed in text treating it as one document
+     * @param text the text to vectorize
+     * @param label the label of the text
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    DataSet vectorize(String text,String label);
+
+    /**
+     *
+     * @param input the text to vectorize
+     * @param label the label of the text
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    DataSet vectorize(File input,String label);
+}
