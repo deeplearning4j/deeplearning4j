5adae7c6ab55fc003c8b69388baeb08ef8deca14
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-05 09:58:40 -0700

    updated temp dir to be user home
    Former-commit-id: b2ac0b8482472bc942add5feff59b7d2f517f883

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/RawMnistDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/RawMnistDataSetIterator.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
 import org.deeplearning4j.datasets.iterator.BaseDatasetIterator;
 
+/**
+ * Mnist data with scaled pixels
+ */
 public class RawMnistDataSetIterator extends BaseDatasetIterator {
 
 	public RawMnistDataSetIterator(int batch, int numExamples) throws IOException {
