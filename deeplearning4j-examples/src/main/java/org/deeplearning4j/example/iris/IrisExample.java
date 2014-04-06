387719c61e994e1d8f9eff4d197e4b7e801e0061
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-06 12:33:50 -0700

    added gzip support for extracting training archive datasets
    Former-commit-id: 26c19b87871828cc7ba3664ecf89b8b1621128a8 [formerly 4680bf1d82691cb9debf9406e5f936f3a88a4a96]
    Former-commit-id: 3ddfa170ee81a58db99d535e289acfa657e92f58

--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/iris/IrisExample.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/iris/IrisExample.java
@@ -5,6 +5,7 @@ import org.deeplearning4j.datasets.DataSet;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.datasets.iterator.SamplingDataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
 import org.deeplearning4j.dbn.GaussianRectifiedLinearDBN;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.NeuralNetwork;
@@ -23,38 +24,36 @@ public class IrisExample {
     public static void main(String[] args) {
         DataSetIterator irisData = new IrisDataSetIterator(150,150);
         DataSet next = irisData.next();
-        next.normalizeZeroMeanZeroUnitVariance();
-
-        Pair<DataSet,DataSet> trainTest = next.splitTestAndTrain(140);
-
-        DataSet train = trainTest.getFirst();
-        DataSet test = trainTest.getSecond();
 
 
-        int   numExamples = train.numExamples();
+        int   numExamples = irisData.numExamples();
         log.info("Training on " + numExamples);
-        DataSetIterator sampling = new SamplingDataSetIterator(train,10,300000);
+        DataSetIterator sampling = new ListDataSetIterator(next.asList(),150);
 
 
         GaussianRectifiedLinearDBN cdbn1 = new GaussianRectifiedLinearDBN.Builder()
-                .hiddenLayerSizes(new int[]{3, 2, 3}).withOptimizationAlgorithm(NeuralNetwork.OptimizationAlgorithm.CONJUGATE_GRADIENT)
-                .normalizeByInputRows(true).numberOfInputs(4).numberOfOutPuts(3).withLossFunction(NeuralNetwork.LossFunction.RECONSTRUCTION_CROSSENTROPY)
-                .useAdaGrad(false).useHiddenActivationsForwardProp(true).withMomentum(0.5)
-                .useRegularization(false).withActivation(Activations.tanh())
+                .hiddenLayerSizes(new int[]{4,3})
+                .withOptimizationAlgorithm(NeuralNetwork.OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .normalizeByInputRows(true).numberOfInputs(4).numberOfOutPuts(3)
+                .withLossFunction(NeuralNetwork.LossFunction.RECONSTRUCTION_CROSSENTROPY)
+                .useAdaGrad(true).useHiddenActivationsForwardProp(true)
+                .useRegularization(false).withActivation(Activations.hardTanh())
                 .build();
 
         while(sampling.hasNext()) {
             DataSet sample = sampling.next();
-            cdbn1.pretrain(sample.getFirst(), 1, 1e-3, 1000);
+            cdbn1.pretrain(sample.getFirst(), 1, 1e-5, 30000);
 
         }
 
         sampling.reset();
 
+
+
         while(sampling.hasNext()) {
             DataSet sample = sampling.next();
             cdbn1.feedForward(sample.getFirst());
-            cdbn1.finetune(sample.getSecond(), 1e-3, 1000);
+            cdbn1.finetune(sample.getSecond(), 1e-3, 30000);
 
         }
 
@@ -62,8 +61,8 @@ public class IrisExample {
 
         Evaluation eval = new Evaluation();
 
-        DoubleMatrix predicted = cdbn1.predict(test.getFirst());
-        eval.eval(test.getSecond(),predicted);
+        DoubleMatrix predicted = cdbn1.predict(next.getFirst());
+        eval.eval(next.getSecond(),predicted);
 
 
 
