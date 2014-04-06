4bdaede97b5c97f82ea9479f6a0f8a6245cddcde
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-05 18:33:43 -0700

    added cursory tfidf vectorizer still a few more design decisions to make

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/vectorizer/BagOfWordsVectorizer.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka-word2vec/src/main/java/org/deeplearning4j/word2vec/vectorizer/BagOfWordsVectorizer.java
@@ -1,67 +1,133 @@
 package org.deeplearning4j.word2vec.vectorizer;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.util.FileUtils;
+import org.deeplearning4j.berkeley.Counter;
 import org.deeplearning4j.datasets.DataSet;
-import org.deeplearning4j.datasets.vectorizer.Vectorizer;
-import org.deeplearning4j.topicmodeling.CountVectorizer;
+import org.deeplearning4j.stopwords.StopWords;
 import org.deeplearning4j.util.MatrixUtil;
-import org.deeplearning4j.word2vec.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.word2vec.sentenceiterator.labelaware.LabelAwareSentenceIterator;
+import org.deeplearning4j.word2vec.tokenizer.Tokenizer;
 import org.deeplearning4j.word2vec.tokenizer.TokenizerFactory;
 import org.deeplearning4j.word2vec.viterbi.Index;
 import org.jblas.DoubleMatrix;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Bag of words vectorizer.
  * Transforms a document in to a bag of words
  * @author Adam Gibson
  *
  */
-public class BagOfWordsVectorizer implements Vectorizer {
+public class BagOfWordsVectorizer implements TextVectorizer {
 
-	private SentenceIterator sentenceIter;
-	private TokenizerFactory tokenizerFactory;
-	private int numLabels;
-	private int label;
-	private Index vocab;
-	
-	/**
-	 * Converts a document in to a bag of words
-	 * @param iter the sentence iterator to use
-	 * This handles segmenting the document in to 
-	 * whole segments
-	 * @param tokenizer the tokenizer to use
-	 * @param label the label of this document
-	 * @param numLabels the total number of labels for the problem
-	 * @param index a word vocab
-	 */
-	public BagOfWordsVectorizer(SentenceIterator iter,TokenizerFactory tokenizer,int label,int numLabels,Index index) {
-		this.sentenceIter = iter;
-		this.tokenizerFactory = tokenizer;
-		this.numLabels = numLabels;
-		this.label = label;
-		this.vocab = index;
-	}
+    private LabelAwareSentenceIterator sentenceIter;
+    private TokenizerFactory tokenizerFactory;
+    private Counter<String> wordCounts;
+    private Index vocab;
+    private int vocabSize;
+    private List<String> stopWords;
+    private List<String> labels;
+    /**
+     * Converts a document in to a bag of words
+     * @param sentenceIterator the sentence iterator to use
+     * This handles segmenting the document in to
+     * whole segments
+     * @param tokenizerFactory the tokenizer to use
+     * @param vocab
+     * @param labels the possible labels for each document
+     * @param vocabSize the max size of vocab
+     */
+    public BagOfWordsVectorizer(LabelAwareSentenceIterator sentenceIterator, TokenizerFactory tokenizerFactory, Index vocab, List<String> labels, int vocabSize) {
+        this.sentenceIter = sentenceIterator;
+        this.tokenizerFactory = tokenizerFactory;
+        this.vocab = vocab;
+        this.labels = labels;
+        this.vocabSize = vocabSize;
+        wordCounts = new Counter<>();
+        stopWords = StopWords.getStopWords();
+    }
 
-	/**
-	 * Creates a bag of words vectorizer with no vocab.
-	 * Note that this will count all words in the document.
-	 * @param iter the iterator used to segment the document
-	 * @param tokenizer the tokenizer to use for word frequencies
-	 * @param label the label of this document
-	 * @param numLabels the number of labels for the domain
-	 */
-	public BagOfWordsVectorizer(SentenceIterator iter,TokenizerFactory tokenizer,int label,int numLabels) {
-		this.sentenceIter = iter;
-		this.tokenizerFactory = tokenizer;
-		this.numLabels = numLabels;
-		this.label = label;
-		this.vocab = new Index();
-	}
-	
-	@Override
-	public DataSet vectorize() {
-		CountVectorizer count = new CountVectorizer(sentenceIter, tokenizerFactory, vocab);
-		DoubleMatrix input = count.toVector();
-		DoubleMatrix d = MatrixUtil.toOutcomeVector(label, numLabels);
-		return new DataSet(input,d);
-	}
+
+
+    /**
+     * Text coming from an input stream considered as one document
+     *
+     * @param is    the input stream to read from
+     * @param label the label to assign
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    @Override
+    public DataSet vectorize(InputStream is, String label) {
+        try {
+            String inputString = IOUtils.toString(is);
+            return vectorize(inputString,label);
+
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * Vectorizes the passed in text treating it as one document
+     *
+     * @param text  the text to vectorize
+     * @param label the label of the text
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    @Override
+    public DataSet vectorize(String text, String label) {
+        Tokenizer tokenizer = tokenizerFactory.create(text);
+        List<String> tokens = tokenizer.getTokens();
+        DoubleMatrix input = new DoubleMatrix(1,vocab.size());
+        for(int i = 0; i < tokens.size(); i++) {
+            if(vocab.indexOf(tokens.get(i)) >= 0)
+                input.put(i,wordCounts.getCount(tokens.get(i)));
+        }
+
+        DoubleMatrix labelMatrix = MatrixUtil.toOutcomeVector(labels.indexOf(label),labels.size());
+        return new DataSet(input,labelMatrix);
+    }
+
+    private void process() {
+        while(sentenceIter.hasNext()) {
+            Tokenizer tokenizer = tokenizerFactory.create(sentenceIter.nextSentence());
+            List<String> tokens = tokenizer.getTokens();
+            for(String token  : tokens)
+                wordCounts.incrementCount(token,1.0);
+        }
+    }
+
+
+    /**
+     * @param input the text to vectorize
+     * @param label the label of the text
+     * @return a dataset with a set of weights(relative to impl; could be word counts or tfidf scores)
+     */
+    @Override
+    public DataSet vectorize(File input, String label) {
+        try {
+            String text = FileUtils.file2String(input);
+            return vectorize(text, label);
+        }catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public DataSet vectorize() {
+        process();
+        sentenceIter.reset();
+        List<DataSet> ret = new ArrayList<>();
+        while(sentenceIter.hasNext()) {
+            ret.add(vectorize(sentenceIter.nextSentence(),sentenceIter.currentLabel()));
+        }
+        return DataSet.merge(ret);
+    }
 
 }
