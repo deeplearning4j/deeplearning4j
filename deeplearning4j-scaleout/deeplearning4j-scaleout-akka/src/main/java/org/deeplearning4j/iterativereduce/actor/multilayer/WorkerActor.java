caec24ccb8972c2ef275f5086f167a34f3a5e83a
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-10 11:04:07 -0700

    added config for normalize mean and zero unit variance
    Former-commit-id: 5b33bd4b96f8561bf5efc8ded7f52364e1b41637 [formerly 7799b2dba0061a225641ec00b7a0dc8dbb279d45]
    Former-commit-id: cda0b07ff5eb7b321fcb374e60675bab89dcb25d

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/org/deeplearning4j/iterativereduce/actor/multilayer/WorkerActor.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/org/deeplearning4j/iterativereduce/actor/multilayer/WorkerActor.java
@@ -38,85 +38,85 @@ import akka.dispatch.Futures;
  *
  */
 public class WorkerActor extends org.deeplearning4j.iterativereduce.actor.core.actor.WorkerActor<UpdateableImpl> {
-	protected BaseMultiLayerNetwork network;
-	protected DoubleMatrix combinedInput;
+    protected BaseMultiLayerNetwork network;
+    protected DoubleMatrix combinedInput;
 
-	protected UpdateableImpl workerUpdateable;
-	protected ActorRef mediator = DistributedPubSubExtension.get(getContext().system()).mediator();
-	protected Cancellable heartbeat;
-	protected static Logger log = LoggerFactory.getLogger(WorkerActor.class);
+    protected UpdateableImpl workerUpdateable;
+    protected ActorRef mediator = DistributedPubSubExtension.get(getContext().system()).mediator();
+    protected Cancellable heartbeat;
+    protected static Logger log = LoggerFactory.getLogger(WorkerActor.class);
     protected boolean isWorking = false;
 
-	public WorkerActor(Conf conf,StateTracker<UpdateableImpl> tracker) {
-		super(conf,tracker);
-		setup(conf);
-		//subscribe to broadcasts from workers (location agnostic)
-		mediator.tell(new Put(getSelf()), getSelf());
+    public WorkerActor(Conf conf,StateTracker<UpdateableImpl> tracker) {
+        super(conf,tracker);
+        setup(conf);
+        //subscribe to broadcasts from workers (location agnostic)
+        mediator.tell(new Put(getSelf()), getSelf());
 
-		//subscribe to broadcasts from master (location agnostic)
-		mediator.tell(new DistributedPubSubMediator.Subscribe(MasterActor.BROADCAST, getSelf()), getSelf());
-
-
-		//subscribe to broadcasts from master (location agnostic)
-		mediator.tell(new DistributedPubSubMediator.Subscribe(id, getSelf()), getSelf());
-
-		heartbeat();
-
-		tracker.addWorker(id);
-
-	}
-
-	public WorkerActor(ActorRef clusterClient,Conf conf,StateTracker<UpdateableImpl> tracker) {
-		super(conf,clusterClient,tracker);
-		setup(conf);
-		//subscribe to broadcasts from workers (location agnostic)
-		mediator.tell(new Put(getSelf()), getSelf());
-
-		//subscribe to broadcasts from master (location agnostic)
-		mediator.tell(new DistributedPubSubMediator.Subscribe(MasterActor.BROADCAST, getSelf()), getSelf());
+        //subscribe to broadcasts from master (location agnostic)
+        mediator.tell(new DistributedPubSubMediator.Subscribe(MasterActor.BROADCAST, getSelf()), getSelf());
 
 
-		tracker.addWorker(id);
-		//subscribe to broadcasts from master (location agnostic)
-		mediator.tell(new DistributedPubSubMediator.Subscribe(id, getSelf()), getSelf());
+        //subscribe to broadcasts from master (location agnostic)
+        mediator.tell(new DistributedPubSubMediator.Subscribe(id, getSelf()), getSelf());
 
-		heartbeat();
+        heartbeat();
 
-	}
+        tracker.addWorker(id);
+
+    }
+
+    public WorkerActor(ActorRef clusterClient,Conf conf,StateTracker<UpdateableImpl> tracker) {
+        super(conf,clusterClient,tracker);
+        setup(conf);
+        //subscribe to broadcasts from workers (location agnostic)
+        mediator.tell(new Put(getSelf()), getSelf());
+
+        //subscribe to broadcasts from master (location agnostic)
+        mediator.tell(new DistributedPubSubMediator.Subscribe(MasterActor.BROADCAST, getSelf()), getSelf());
 
 
+        tracker.addWorker(id);
+        //subscribe to broadcasts from master (location agnostic)
+        mediator.tell(new DistributedPubSubMediator.Subscribe(id, getSelf()), getSelf());
 
-	public static Props propsFor(ActorRef actor,Conf conf,StateTracker<UpdateableImpl> tracker) {
-		return Props.create(WorkerActor.class,actor,conf,tracker);
-	}
+        heartbeat();
 
-	public static Props propsFor(Conf conf,HazelCastStateTracker stateTracker) {
-		return Props.create(WorkerActor.class,conf,stateTracker);
-	}
-
-	protected void confirmWorking() {
-		Job j = tracker.jobFor(id);
-
-		//reply
-		if(j != null)
-			mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
-					j), getSelf());
-
-		else
-			log.warn("Not confirming work when none to be found");
-	}
+    }
 
 
 
-	protected void heartbeat() {
-		heartbeat = context().system().scheduler().schedule(Duration.apply(10, TimeUnit.SECONDS), Duration.apply(10, TimeUnit.SECONDS), new Runnable() {
+    public static Props propsFor(ActorRef actor,Conf conf,StateTracker<UpdateableImpl> tracker) {
+        return Props.create(WorkerActor.class,actor,conf,tracker);
+    }
 
-			@Override
-			public void run() {
-				log.info("Sending heartbeat to master");
-				mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
-						register()), getSelf());
-				tracker.addWorker(id);
+    public static Props propsFor(Conf conf,HazelCastStateTracker stateTracker) {
+        return Props.create(WorkerActor.class,conf,stateTracker);
+    }
+
+    protected void confirmWorking() {
+        Job j = tracker.jobFor(id);
+
+        //reply
+        if(j != null)
+            mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
+                    j), getSelf());
+
+        else
+            log.warn("Not confirming work when none to be found");
+    }
+
+
+
+    protected void heartbeat() {
+        heartbeat = context().system().scheduler().schedule(Duration.apply(10, TimeUnit.SECONDS), Duration.apply(10, TimeUnit.SECONDS), new Runnable() {
+
+            @Override
+            public void run() {
+                log.info("Sending heartbeat to master");
+                mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
+                        register()), getSelf());
+                tracker.addWorker(id);
                 //heart beat to ensure race conditions dont happen
                 Job j = tracker.jobFor(id);
                 try {
@@ -128,287 +128,291 @@ public class WorkerActor extends org.deeplearning4j.iterativereduce.actor.core.a
 
 
 
-			}
+            }
 
-		}, context().dispatcher());
+        }, context().dispatcher());
 
-	}
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void onReceive(Object message) throws Exception {
-		if (message instanceof DistributedPubSubMediator.SubscribeAck || message instanceof DistributedPubSubMediator.UnsubscribeAck) {
-			DistributedPubSubMediator.SubscribeAck ack = (DistributedPubSubMediator.SubscribeAck) message;
-			//reply
-			mediator.tell(new DistributedPubSubMediator.Publish(ClusterListener.TOPICS,
-					message), getSelf());
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof DistributedPubSubMediator.SubscribeAck || message instanceof DistributedPubSubMediator.UnsubscribeAck) {
+            DistributedPubSubMediator.SubscribeAck ack = (DistributedPubSubMediator.SubscribeAck) message;
+            //reply
+            mediator.tell(new DistributedPubSubMediator.Publish(ClusterListener.TOPICS,
+                    message), getSelf());
 
-			log.info("Subscribed to " + ack.toString());
-		}
+            log.info("Subscribed to " + ack.toString());
+        }
 
-		else if(message instanceof Job) {
-			Job j = (Job) message;
-			Job trackerJob = tracker.jobFor(id);
-			if(trackerJob == null) {
-				tracker.addJobToCurrent(j);
-				log.info("Confirmation from " + j.getWorkerId() + " on work");
-				List<DataSet> input = (List<DataSet>) j.getWork();
-				confirmWorking();
-				updateTraining(input);
+        else if(message instanceof Job) {
+            Job j = (Job) message;
+            Job trackerJob = tracker.jobFor(id);
+            if(trackerJob == null) {
+                tracker.addJobToCurrent(j);
+                log.info("Confirmation from " + j.getWorkerId() + " on work");
+                List<DataSet> input = (List<DataSet>) j.getWork();
+                confirmWorking();
+                updateTraining(input);
 
-			}
+            }
 
-			else {
-				//block till there's an available worker
+            else {
+                //block till there's an available worker
 
-				Job j2 = null;
-				boolean redist = false;
-
-
-				while(!redist) {
-					List<String> ids = tracker.workers();
-
-					for(String s : ids) {
-						if(tracker.jobFor(s) == null) {
-
-							//wrap in a job for additional metadata
-							j2 = j;
-							j2.setWorkerId(s);
-							//replicate the network
-							mediator.tell(new DistributedPubSubMediator.Publish(s,
-									j2), getSelf());
-							log.info("Delegated work to worker " + s);
-
-							redist = true;
-							break;
-
-						}
-					}
-				}
+                Job j2 = null;
+                boolean redist = false;
 
 
-			}
+                while(!redist) {
+                    List<String> ids = tracker.workers();
+
+                    for(String s : ids) {
+                        if(tracker.jobFor(s) == null) {
+
+                            //wrap in a job for additional metadata
+                            j2 = j;
+                            j2.setWorkerId(s);
+                            //replicate the network
+                            mediator.tell(new DistributedPubSubMediator.Publish(s,
+                                    j2), getSelf());
+                            log.info("Delegated work to worker " + s);
+
+                            redist = true;
+                            break;
+
+                        }
+                    }
+                }
 
 
-		}
-
-		else if(message instanceof BaseMultiLayerNetwork) {
-			setNetwork((BaseMultiLayerNetwork) message);
-			log.info("Set network");
-		}
-
-		else if(message instanceof Ack) {
-			log.info("Ack from master on worker " + id);
-		}
+            }
 
 
+        }
 
+        else if(message instanceof BaseMultiLayerNetwork) {
+            setNetwork((BaseMultiLayerNetwork) message);
+            log.info("Set network");
+        }
 
-		else if(message instanceof Updateable) {
-			final UpdateableImpl m = (UpdateableImpl) message;
-			Future<Void> f = Futures.future(new Callable<Void>() {
-
-				@Override
-				public Void call() throws Exception {
-
-					if(m.get() == null) {
-						setNetwork(tracker.getCurrent().get());
-
-					}
-
-					else {
-						setWorkerUpdateable(m.clone());
-						setNetwork(m.get());
-
-					}
-
-					return null;
-				}
-
-			},context().dispatcher());
-
-			ActorRefUtils.throwExceptionIfExists(f, context().dispatcher());
-
-
-		}
-		else
-			unhandled(message);
-	}
-
-	protected  void updateTraining(List<DataSet> list) {
-		DoubleMatrix newInput = new DoubleMatrix(list.size(),list.get(0).getFirst().columns);
-		DoubleMatrix newOutput = new DoubleMatrix(list.size(),list.get(0).getSecond().columns);
+        else if(message instanceof Ack) {
+            log.info("Ack from master on worker " + id);
+        }
 
 
 
 
+        else if(message instanceof Updateable) {
+            final UpdateableImpl m = (UpdateableImpl) message;
+            Future<Void> f = Futures.future(new Callable<Void>() {
+
+                @Override
+                public Void call() throws Exception {
+
+                    if(m.get() == null) {
+                        setNetwork(tracker.getCurrent().get());
+
+                    }
+
+                    else {
+                        setWorkerUpdateable(m.clone());
+                        setNetwork(m.get());
+
+                    }
+
+                    return null;
+                }
+
+            },context().dispatcher());
+
+            ActorRefUtils.throwExceptionIfExists(f, context().dispatcher());
 
 
-		for(int i = 0; i < list.size(); i++) {
-			newInput.putRow(i,list.get(i).getFirst());
-			newOutput.putRow(i,list.get(i).getSecond());
-		}
+        }
+        else
+            unhandled(message);
+    }
 
-
-		Future<UpdateableImpl> f = Futures.future(new Callable<UpdateableImpl>() {
-
-			@Override
-			public UpdateableImpl call() throws Exception {
-
-
-				UpdateableImpl work = compute();
-
-				if(work != null) {
-					log.info("Updating parent actor...");
-					//update parameters in master param server
-					mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
-							work), getSelf());
-
-				}
-
-				else {
-					//ensure next iteration happens by decrementing number of required batches
-					mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
-							NoJobFound.getInstance()), getSelf());
-					log.info("No job found; unlocking worker "  + id);
-				}
-
-				return work;
-			}
-
-		}, getContext().dispatcher());
-
-		ActorRefUtils.throwExceptionIfExists(f, context().dispatcher());
-	}
+    protected  void updateTraining(List<DataSet> list) {
+        DoubleMatrix newInput = new DoubleMatrix(list.size(),list.get(0).getFirst().columns);
+        DoubleMatrix newOutput = new DoubleMatrix(list.size(),list.get(0).getSecond().columns);
 
 
 
 
-	@Override
-	public  UpdateableImpl compute(List<UpdateableImpl> records) {
-		return compute();
-	}
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public  UpdateableImpl compute() {
-		log.info("Training network");
-		BaseMultiLayerNetwork network = this.getNetwork();
-		isWorking = true;
+
+        for(int i = 0; i < list.size(); i++) {
+            newInput.putRow(i,list.get(i).getFirst());
+            newOutput.putRow(i,list.get(i).getSecond());
+        }
+
+
+        Future<UpdateableImpl> f = Futures.future(new Callable<UpdateableImpl>() {
+
+            @Override
+            public UpdateableImpl call() throws Exception {
+
+
+                UpdateableImpl work = compute();
+
+                if(work != null) {
+                    log.info("Updating parent actor...");
+                    //update parameters in master param server
+                    mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
+                            work), getSelf());
+
+                }
+
+                else {
+                    //ensure next iteration happens by decrementing number of required batches
+                    mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
+                            NoJobFound.getInstance()), getSelf());
+                    log.info("No job found; unlocking worker "  + id);
+                }
+
+                return work;
+            }
+
+        }, getContext().dispatcher());
+
+        ActorRefUtils.throwExceptionIfExists(f, context().dispatcher());
+    }
+
+
+
+
+    @Override
+    public  UpdateableImpl compute(List<UpdateableImpl> records) {
+        return compute();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public  UpdateableImpl compute() {
+        log.info("Training network");
+        BaseMultiLayerNetwork network = this.getNetwork();
+        isWorking = true;
         while(network == null) {
-			try {
-				network = tracker.getCurrent().get();
-				this.network = network;
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
+            try {
+                network = tracker.getCurrent().get();
+                this.network = network;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
 
 
-		DataSet d = null;
-		Job j = tracker.jobFor(id);
+        DataSet d = null;
+        Job j = tracker.jobFor(id);
 
-		if(j != null) {
-			log.info("Found job for worker " + id);
-			if(j.getWork() instanceof List) {
-				List<DataSet> l = (List<DataSet>) j.getWork();
-				d = DataSet.merge(l);
-			}
+        if(j != null) {
+            log.info("Found job for worker " + id);
+            if(j.getWork() instanceof List) {
+                List<DataSet> l = (List<DataSet>) j.getWork();
+                d = DataSet.merge(l);
+            }
 
-			else
-				d = (DataSet) j.getWork();
-		}
-
-
-
-		if(j == null)
-			return null;
-
-		if(d == null) {
-			throw new IllegalStateException("No job found for worker " + id);
-		}
+            else
+                d = (DataSet) j.getWork();
+        }
 
 
-		if(tracker.isPretrain()) {
-			log.info("Worker " + id + " pretraining");
-			network.pretrain(d.getFirst(), extraParams);
-		}
 
-		else {
-			network.setInput(d.getFirst());
-			log.info("Worker " + id + " finetune");
+        if(j == null)
+            return null;
+
+        if(d == null) {
+            throw new IllegalStateException("No job found for worker " + id);
+        }
+
+
+        if(tracker.isPretrain()) {
+            log.info("Worker " + id + " pretraining");
+            if(isNormalizeZeroMeanAndUnitVariance)
+                d.normalizeZeroMeanZeroUnitVariance();;
+            network.pretrain(d.getFirst(), extraParams);
+        }
+
+        else {
+            if(isNormalizeZeroMeanAndUnitVariance)
+                d.normalizeZeroMeanZeroUnitVariance();;
+            network.setInput(d.getFirst());
+            log.info("Worker " + id + " finetune");
             network.feedForward(d.getFirst());
-			network.finetune(d.getSecond(), learningRate, fineTuneEpochs);
-		}
+            network.finetune(d.getSecond(), learningRate, fineTuneEpochs);
+        }
 
 
-		try {
-			if(j != null)
-				tracker.clearJob(j);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+        try {
+            if(j != null)
+                tracker.clearJob(j);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
         isWorking = false;
-		return new UpdateableImpl(network);
-	}
+        return new UpdateableImpl(network);
+    }
 
-	@Override
-	public boolean incrementIteration() {
-		return false;
-	}
+    @Override
+    public boolean incrementIteration() {
+        return false;
+    }
 
-	@Override
-	public void setup(Conf conf) {
-		super.setup(conf);
-	}
-
-
-
-	@Override
-	public void aroundPostStop() {
-		super.aroundPostStop();
-		//replicate the network
-		mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
-				new ClearWorker(id)), getSelf());
-		heartbeat.cancel();
-	}
+    @Override
+    public void setup(Conf conf) {
+        super.setup(conf);
+    }
 
 
 
-	@Override
-	public  UpdateableImpl getResults() {
-		return workerUpdateable;
-	}
-
-	@Override
-	public  void update(UpdateableImpl t) {
-		this.workerUpdateable = t;
-	}
-
-
-	public  synchronized BaseMultiLayerNetwork getNetwork() {
-		return network;
-	}
-
-
-	public  void setNetwork(BaseMultiLayerNetwork network) {
-		this.network = network;
-	}
+    @Override
+    public void aroundPostStop() {
+        super.aroundPostStop();
+        //replicate the network
+        mediator.tell(new DistributedPubSubMediator.Publish(MasterActor.MASTER,
+                new ClearWorker(id)), getSelf());
+        heartbeat.cancel();
+    }
 
 
 
+    @Override
+    public  UpdateableImpl getResults() {
+        return workerUpdateable;
+    }
+
+    @Override
+    public  void update(UpdateableImpl t) {
+        this.workerUpdateable = t;
+    }
 
 
-	public  UpdateableImpl getWorkerUpdateable() {
-		return workerUpdateable;
-	}
+    public  synchronized BaseMultiLayerNetwork getNetwork() {
+        return network;
+    }
 
 
-	public  void setWorkerUpdateable(UpdateableImpl workerUpdateable) {
-		this.workerUpdateable = workerUpdateable;
-	}
+    public  void setNetwork(BaseMultiLayerNetwork network) {
+        this.network = network;
+    }
+
+
+
+
+
+    public  UpdateableImpl getWorkerUpdateable() {
+        return workerUpdateable;
+    }
+
+
+    public  void setWorkerUpdateable(UpdateableImpl workerUpdateable) {
+        this.workerUpdateable = workerUpdateable;
+    }
 
 
 
