3dc8972579a17b11da5b6e6e48abc83b308dd222
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-14 09:24:53 -0700

    added opinion mining example

--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/text/TweetOpinionMining.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/text/TweetOpinionMining.java
@@ -2,6 +2,10 @@ package org.deeplearning4j.example.text;
 
 import org.apache.commons.io.IOUtils;
 import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.dbn.GaussianRectifiedLinearDBN;
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.text.tokenizerfactory.UimaTokenizerFactory;
 import org.deeplearning4j.word2vec.sentenceiterator.labelaware.LabelAwareListSentenceIterator;
 import org.deeplearning4j.word2vec.tokenizer.TokenizerFactory;
@@ -31,11 +35,31 @@ public class TweetOpinionMining {
 
         LabelAwareListSentenceIterator iterator = new LabelAwareListSentenceIterator(is);
         TokenizerFactory tokenizerFactory = new UimaTokenizerFactory();
-        TextVectorizer vectorizor = new TfidfVectorizer(iterator,tokenizerFactory,Arrays.asList("0","1","2"));
+        TextVectorizer vectorizor = new TfidfVectorizer(iterator,tokenizerFactory,Arrays.asList("0","1","2"),1000cd );
         DataSet data = vectorizor.vectorize();
+        DataSetIterator iter = new ListDataSetIterator(data.asList(),10);
+
+        GaussianRectifiedLinearDBN dbn = new GaussianRectifiedLinearDBN.Builder().useAdaGrad(true).useRegularization(true)
+                .hiddenLayerSizes(new int[]{iter.inputColumns() / 2,iter.inputColumns() / 2,iter.inputColumns() / 6}).normalizeByInputRows(true)
+                .numberOfInputs(iter.inputColumns()).numberOfOutPuts(iter.totalOutcomes())
+                .build();
+
+        while(iter.hasNext()) {
+            DataSet next = iter.next();
+            dbn.pretrain(next.getFirst(), 1, 1e-3, 10000);
+        }
+
+        iter.reset();
+        while(iter.hasNext()) {
+            DataSet next = iter.next();
+            dbn.setInput(next.getFirst());
+            dbn.finetune(next.getSecond(), 1e-3, 10000);
+        }
 
 
-
+        Evaluation eval = new Evaluation();
+        eval.eval(data.getSecond(),dbn.predict(data.getFirst()));
+        log.info(eval.stats());
         log.info("Example tweets " + data.numExamples());
 
 
