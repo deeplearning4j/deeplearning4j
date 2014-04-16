c0a5eff7fa0aed52831b316357548526cccbd7ec
agibsonccc <agibson@clevercloudcomputing.com>
2014-04-16 09:29:14 -0700

    added toMatrix() method
    Former-commit-id: 97b75e1a341627fc77fdbeb12cd41c69f38dcded [formerly 2bb6a5535d1dde3895cee4009137c5045b8667bb]
    Former-commit-id: fcebbb38bd28bdb330cbb5500a6a117fb92d6182

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/learning/TensorTests.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/learning/TensorTests.java
@@ -4,6 +4,8 @@ import org.apache.commons.math3.random.RandomGenerator;
 import org.deeplearning4j.distributions.Distributions;
 import org.deeplearning4j.nn.Tensor;
 import static org.junit.Assert.*;
+
+import org.jblas.DoubleMatrix;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,16 +25,24 @@ public class TensorTests {
     }
 
     @Test
+    public void testAsMatrix() {
+        Tensor t = Tensor.rand(2,3,3,4.0,6.0);
+        DoubleMatrix matrix = t.toMatrix();
+        assertEquals(true,matrix.rows == 6);
+        assertEquals(true, matrix.columns == 3);
+    }
+
+    @Test
     public void tensorTests() {
         RandomGenerator rng = new MersenneTwister(123);
         Tensor t = Tensor.rand(2,3,3,4.0,6.0);
         log.info("Original tensor " + t);
-        assertEquals(t,t.permute(new int[]{1,2,3}));
+        assertEquals(t, t.permute(new int[]{1, 2, 3}));
         assertEquals(t.transpose(),t.permute(new int[]{2,1,3}));
         log.info("T " + t.permute(new int[]{3, 2, 1}));
-        assertEquals(2,t.permute(new int[]{2,3,1}).slices());
-        log.info("T " + t.permute(new int[]{2,3,1}));
-        log.info("T 2 " + t.permute(new int[]{1,3,2}));
+        assertEquals(2, t.permute(new int[]{2, 3, 1}).slices());
+        log.info("T " + t.permute(new int[]{2, 3, 1}));
+        log.info("T 2 " + t.permute(new int[]{1, 3, 2}));
         log.info("T 2 " + t.permute(new int[]{3,1,2}));
 
 
