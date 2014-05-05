52285134f797fdeb7a8d555ac10a1b35cb078568
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-04 19:38:21 -0700

    updated down sampling layer
    Former-commit-id: 05e59c3692f4c66e3aaa9b73480c426c23b4f454

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/DownSamplingLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/DownSamplingLayer.java
@@ -1,12 +1,136 @@
 package org.deeplearning4j.nn;
 
-import java.io.Serializable;
+import org.apache.commons.math3.distribution.RealDistribution;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.deeplearning4j.nn.activation.ActivationFunction;
+import org.jblas.DoubleMatrix;
+
 
 /**
- * Created by agibsonccc on 5/3/14.
+ * Down sampling hidden layer
  */
-public class DownSamplingLayer implements Serializable {
+public class DownSamplingLayer extends HiddenLayer {
+
+    private int[] stride;
+    private int numFeatureMaps;
+    private DoubleMatrix fmSize;
 
 
+    private DownSamplingLayer() {}
+
+
+    public int[] getStride() {
+        return stride;
+    }
+
+    public void setStride(int[] stride) {
+        this.stride = stride;
+    }
+
+    public int getNumFeatureMaps() {
+        return numFeatureMaps;
+    }
+
+    public void setNumFeatureMaps(int numFeatureMaps) {
+        this.numFeatureMaps = numFeatureMaps;
+    }
+
+
+    public DoubleMatrix getFmSize() {
+        return fmSize;
+    }
+
+    public void setFmSize(DoubleMatrix fmSize) {
+        this.fmSize = fmSize;
+    }
+
+
+    public static class Builder extends HiddenLayer.Builder {
+
+        private int[] stride;
+        private DoubleMatrix fmSize;
+        private int numFeatureMaps;
+
+
+
+
+        public Builder numFeatureMaps(int numFeatureMaps) {
+            this.numFeatureMaps = numFeatureMaps;
+            return this;
+
+        }
+        public Builder withStride(int[] stride) {
+            this.stride = stride;
+            return this;
+        }
+
+        public Builder withFmSize(DoubleMatrix fmSize) {
+            this.fmSize = fmSize;
+            return this;
+        }
+
+
+        @Override
+        public Builder dist(RealDistribution dist) {
+            super.dist(dist);
+            return this;
+        }
+
+        @Override
+        public Builder nIn(int nIn) {
+            super.nIn(nIn);
+            return this;
+        }
+
+        @Override
+        public Builder nOut(int nOut) {
+            super.nOut(nOut);
+            return this;
+        }
+
+        @Override
+        public Builder withWeights(DoubleMatrix W) {
+            super.withWeights(W);
+            return this;
+        }
+
+        @Override
+        public Builder withRng(RandomGenerator gen) {
+            super.withRng(gen);
+            return this;
+        }
+
+        @Override
+        public Builder withActivation(ActivationFunction function) {
+            super.withActivation(function);
+            return this;
+        }
+
+        @Override
+        public Builder withBias(DoubleMatrix b) {
+            super.withBias(b);
+            return this;
+        }
+
+        @Override
+        public HiddenLayer.Builder withInput(DoubleMatrix input) {
+            return super.withInput(input);
+        }
+
+        @Override
+        public DownSamplingLayer build() {
+            DownSamplingLayer layer = new DownSamplingLayer();
+            layer.fmSize = fmSize;
+            layer.numFeatureMaps = numFeatureMaps;
+            layer.stride = stride;
+            layer.activationFunction = activationFunction;
+            layer.b = b;
+            layer.rng = rng;
+            layer.W = W;
+            layer.input = input;
+            layer.dist = dist;
+            return layer;
+        }
+    }
 
 }
