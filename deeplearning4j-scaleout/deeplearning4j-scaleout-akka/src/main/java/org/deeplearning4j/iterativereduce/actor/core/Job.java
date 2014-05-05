05e59c3692f4c66e3aaa9b73480c426c23b4f454
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-04 19:38:21 -0700

    updated down sampling layer

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/org/deeplearning4j/iterativereduce/actor/core/Job.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/main/java/org/deeplearning4j/iterativereduce/actor/core/Job.java
@@ -14,7 +14,6 @@ public class Job implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 6762101539446662016L;
-	private boolean done;
 	private String workerId;
 	private Serializable work;
 
@@ -28,58 +27,16 @@ public class Job implements Serializable {
 
 
     public Job(Job job) {
-        this.done = job.done;
         this.workerId = job.workerId;
         this.work = job.work;
     }
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Job)) return false;
-
-        Job job = (Job) o;
-
-        if (done != job.done) return false;
-        if (work != null ? !work.equals(job.work) : job.work != null) return false;
-        if (workerId != null ? !workerId.equals(job.workerId) : job.workerId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (done ? 1 : 0);
-        result = 31 * result + (workerId != null ? workerId.hashCode() : 0);
-        result = 31 * result + (work != null ? work.hashCode() : 0);
-        return result;
-    }
 
     @Override
     public Job clone() {
         return new Job(this);
     }
-	
-	
-	public boolean isDone() {
-		return done;
-	}
-	public void setDone(boolean done) {
-		this.done = done;
-	}
-	public String getWorkerId() {
-		return workerId;
-	}
-	public void setWorkerId(String workerId) {
-		this.workerId = workerId;
-	}
-	public Serializable getWork() {
-		return work;
-	}
-	public void setWork(Serializable work) {
-		this.work = work;
-	}
 
     /**
      * Returns a string representation of the object. In general, the
@@ -104,6 +61,43 @@ public class Job implements Serializable {
      */
     @Override
     public String toString() {
-        return super.toString();
+        return "Job{" +
+                "workerId='" + workerId + '\'' +
+                ", work=" + work +
+                '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+
+        Job job = (Job) o;
+
+        if (work != null ? !work.equals(job.work) : job.work != null) return false;
+        if (workerId != null ? !workerId.equals(job.workerId) : job.workerId != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = workerId != null ? workerId.hashCode() : 0;
+        result = 31 * result + (work != null ? work.hashCode() : 0);
+        return result;
+    }
+
+    public String getWorkerId() {
+		return workerId;
+	}
+	public void setWorkerId(String workerId) {
+		this.workerId = workerId;
+	}
+	public Serializable getWork() {
+		return work;
+	}
+	public void setWork(Serializable work) {
+		this.work = work;
+	}
+
 }
