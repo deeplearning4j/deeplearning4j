f0c2cd4426cb7c58628afc2ec167c0c3cfb1d163
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-12 14:59:20 -0700

    added tensor and fourdtensor adagrad

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/learning/AdaGrad.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/learning/AdaGrad.java
@@ -24,41 +24,55 @@ public class AdaGrad implements Serializable {
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = -4754127927704099888L;
-	private double masterStepSize = 1e-3; // default for masterStepSize (this is the numerator)
-	//private double squaredGradientSum = 0;
+	protected static final long serialVersionUID = -4754127927704099888L;
+	protected double masterStepSize = 1e-3; // default for masterStepSize (this is the numerator)
+	//protected double squaredGradientSum = 0;
 	public DoubleMatrix historicalGradient;
 	public DoubleMatrix adjustedGradient;
 	public double fudgeFactor = 1e-6;
 	public DoubleMatrix gradient;
 	public int rows;
 	public int cols;
-	private int numIterations = 0;
-	private double lrDecay = 0.95;
-	private boolean decayLr;
-	private double minLearningRate = 1e-4;
+	protected int numIterations = 0;
+	protected double lrDecay = 0.95;
+	protected boolean decayLr;
+	protected double minLearningRate = 1e-4;
 
 	public AdaGrad( int rows, int cols, double gamma) {
 		this.rows = rows;
 		this.cols = cols;
-		this.adjustedGradient = new DoubleMatrix(rows, cols);
-		this.historicalGradient = new DoubleMatrix(rows, cols);
+        createHistoricalGradient();
+        createAdjustedGradient();
 		this.masterStepSize = gamma;
 		this.decayLr = false;
 
 
 	}
 
+    /**
+     * Initializes adagrad with a gamma of 1e-2
+     * @param rows the rows for the gradients
+     * @param cols the number of columns for the gradient
+     */
 	public AdaGrad( int rows, int cols) {
 		this(rows,cols,0.01);
 
 	}
 
+    protected void createHistoricalGradient() {
+        this.historicalGradient = new DoubleMatrix(rows, cols);
+
+    }
+    protected void createAdjustedGradient() {
+        this.adjustedGradient = new DoubleMatrix(rows, cols);
+    }
 
 
 
 
-	/**
+
+
+    /**
 	 * Gets feature specific learning rates
 	 * Adagrad keeps a history of gradients being passed in.
 	 * Note that each gradient passed in becomes adapted over time, hence
---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/learning/FourDTensorAdaGrad.java
@@ -0,0 +1,75 @@
+package org.deeplearning4j.nn.learning;
+
+import org.deeplearning4j.nn.FourDTensor;
+import org.deeplearning4j.nn.Tensor;
+import org.jblas.DoubleMatrix;
+
+import static org.jblas.MatrixFunctions.abs;
+import static org.jblas.MatrixFunctions.pow;
+import static org.jblas.MatrixFunctions.sqrt;
+
+/**
+ * Four dimensional tensor ada grad.
+ * This allows for tensor and tensor by slice wise learning rates
+ */
+public class FourDTensorAdaGrad extends TensorAdaGrad {
+
+    protected int tensors;
+
+    public FourDTensorAdaGrad(int rows, int cols, int slices,int tensors, double gamma) {
+        super(rows, cols, slices, gamma);
+        this.tensors = tensors;
+    }
+
+    /**
+     * Initializes the tensor adad grad with a gamma
+     * of 1e-2
+     *
+     * @param rows   number of rows for the gradients
+     * @param cols   the number of columns for the gradient
+     * @param slices the number of slices for the gradient
+     * @param tensors the number of tensors to use
+     */
+    public FourDTensorAdaGrad(int rows, int cols, int slices,int tensors) {
+        super(rows, cols, slices);
+        this.tensors = tensors;
+    }
+
+
+
+    /**
+     * Tensor/Slice wise learning rates
+     * @param tensor the tensor to use
+     * @param slice the slice to use
+     * @param gradient the gradient slice
+     * @return the slice wise learning rates
+     */
+    public DoubleMatrix getLearningRates(int tensor,int slice,DoubleMatrix gradient) {
+        this.gradient = gradient.dup();
+        FourDTensor histGrad = (FourDTensor) this.historicalGradient;
+        FourDTensor grad = (FourDTensor) this.gradient;
+        FourDTensor adjustedGrad = (FourDTensor) adjustedGradient;
+        DoubleMatrix squaredGradient = pow(grad.getSliceOfTensor(tensor,slice),2);
+        if(this.historicalGradient.length != this.gradient.length)
+            this.historicalGradient = FourDTensor.zeros(grad.rows(),this.gradient.columns,grad.slices());
+        histGrad.put(tensor,slice,histGrad.getSlice(slice).add(squaredGradient));
+        this.historicalGradient.addi(squaredGradient);
+        numIterations++;
+        DoubleMatrix sqrtGradient = sqrt(squaredGradient).add(fudgeFactor);
+        DoubleMatrix div = abs(gradient).div(sqrtGradient);
+        adjustedGrad.put(tensor,slice,div.mul(masterStepSize));
+        //ensure no zeros
+        return adjustedGrad.getSliceOfTensor(tensor,slice);
+    }
+
+    @Override
+    protected void createHistoricalGradient() {
+       this.historicalGradient = new FourDTensor(rows,cols,slices,tensors);
+    }
+
+    @Override
+    protected void createAdjustedGradient() {
+        this.adjustedGradient = new FourDTensor(rows,cols,slices,tensors);
+
+    }
+}
