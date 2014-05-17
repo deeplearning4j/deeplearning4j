b9ef543d1870a29f01f6e631257f336b1b61c1eb
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-16 23:52:27 -0700

    added gaussian hidden units for RBMs
    Former-commit-id: f1e9d6a2c9655b99993b554616dac3bca6c73a80

--- deeplearning4j-core/src/test/java/org/deeplearning4j/autoencoder/DeepAutoEncoderTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/autoencoder/DeepAutoEncoderTest.java
@@ -1,12 +1,19 @@
 package org.deeplearning4j.autoencoder;
 import org.apache.commons.lang3.time.StopWatch;
+import org.apache.commons.math3.random.MersenneTwister;
 import org.deeplearning4j.datasets.DataSet;
 import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.datasets.mnist.draw.DrawMnistGreyScale;
 import org.deeplearning4j.dbn.DBN;
 import org.deeplearning4j.nn.activation.Activations;
+import org.deeplearning4j.rbm.RBM;
+import org.deeplearning4j.util.MatrixUtil;
+import org.jblas.DoubleMatrix;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
 
 /**
  * Created by agibsonccc on 5/11/14.
@@ -18,23 +25,41 @@ public class DeepAutoEncoderTest {
     public void testWithMnist() throws Exception {
         MnistDataFetcher fetcher = new MnistDataFetcher(true);
         fetcher.fetch(100);
-        DataSet d = fetcher.next();
-        d.filterAndStrip(new int[]{0,1});
-        log.info("Training on " + d.numExamples());
+        DataSet data = fetcher.next();
+        data.filterAndStrip(new int[]{0, 1});
+        log.info("Training on " + data.numExamples());
         StopWatch watch = new StopWatch();
 
-
+        data = DataSet.merge(data.asList().subList(0,10));
         DBN dbn = new DBN.Builder()
-                .hiddenLayerSizes(new int[]{1000,500,250,30})
-                .withMomentum(0.5).renderWeights(100)
+                .withHiddenUnitsByLayer(Collections.singletonMap(3, RBM.HiddenUnit.GAUSSIAN))
+               .hiddenLayerSizes(new int[]{1000, 500, 250, 30})
                 .numberOfInputs(784)
                 .numberOfOutPuts(2)
                 .build();
 
+        dbn.pretrain(data.getFirst(),new Object[]{1,1e-2,10000});
+
         DeepAutoEncoder encoder = new DeepAutoEncoder(dbn,new Object[]{1,1e-2,10000});
-        encoder.train(d.getFirst(),1e-2,1);
+        encoder.finetune(data.getFirst(),1e-2,1000);
 
+        DoubleMatrix reconstruct = encoder.reconstruct(data.getFirst());
+        for(int j = 0; j < data.numExamples(); j++) {
 
+            DoubleMatrix draw1 = data.get(j).getFirst().mul(255);
+            DoubleMatrix reconstructed2 = reconstruct.getRow(j);
+            DoubleMatrix draw2 = reconstructed2.mul(255);
+
+            DrawMnistGreyScale d = new DrawMnistGreyScale(draw1);
+            d.title = "REAL";
+            d.draw();
+            DrawMnistGreyScale d2 = new DrawMnistGreyScale(draw2);
+            d2.title = "TEST";
+            d2.draw();
+            Thread.sleep(10000);
+            d.frame.dispose();
+            d2.frame.dispose();
+        }
 
 
 
