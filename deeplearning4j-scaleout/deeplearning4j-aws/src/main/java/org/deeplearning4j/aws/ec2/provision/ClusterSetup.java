ad3485cf6a40d9334e1b66572d3c8aa39aa3113d
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-20 08:22:04 -0700

    change hostname when provisioniong worker
    Former-commit-id: 09662401933f9ea322f7598b7eebff4895aaad96 [formerly 0535beff94c15411ddbcedd15afab3b9c7510bea]
    Former-commit-id: 3521cc69ae8b47a35f6439d16cfb8ca2c00e3116

--- deeplearning4j-scaleout/deeplearning4j-aws/src/main/java/org/deeplearning4j/aws/ec2/provision/ClusterSetup.java
+++ deeplearning4j-scaleout/deeplearning4j-aws/src/main/java/org/deeplearning4j/aws/ec2/provision/ClusterSetup.java
@@ -2,13 +2,17 @@ package org.deeplearning4j.aws.ec2.provision;
 
 import java.io.File;
 import java.util.List;
+import java.util.concurrent.Callable;
 
+import akka.actor.ActorSystem;
+import akka.dispatch.Futures;
 import org.deeplearning4j.aws.ec2.Ec2BoxCreator;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.concurrent.Future;
 
 /**
  * Sets up a DL4J cluster
@@ -20,27 +24,22 @@ public class ClusterSetup {
 	@Option(name = "-w",usage = "Number of workers")
 	private int numWorkers = 1;
 	@Option(name = "-ami",usage = "Amazon machine image: default, amazon linux (only works with RHEL right now")
-	private String ami = "ami-bba18dd2";
+	private String ami = "ami-fb8e9292";
 	@Option(name = "-s",usage = "size of instance: default m1.medium")
-	private String size = "m1.medium";
+	private String size = "m3.xlarge";
 	@Option(name = "-sg",usage = "security group, this needs to be set")
 	private String securityGroupName;
 	@Option(name = "-kp",usage = "key pair name, also needs to be set.")
 	private String keyPairName;
 	@Option(name = "-kpath",usage = "path to private key - needs to be set, this is used to login to amazon.")
 	private String pathToPrivateKey;
-
-	@Option(name = "-wscript", usage = "path to worker script to run, this will allow customization of dependencies")
+    @Option(name = "-wscript", usage = "path to worker script to run, this will allow customization of dependencies")
 	private String workerSetupScriptPath;
 	@Option(name = "-mscript", usage = "path to master script to run this will allow customization of the dependencies")
 	private String masterSetupScriptPath;
-	@Option(name = "-lib",usage = "path to lib directory, this could be a default dl4j distribution or your own custom dependencies")
-	private String libDirPath;
-	@Option(name = "-datapath", usage = "path to serialized dataset")
-	private String dataSetPath;
-	@Option(name = "-uploddeps",usage = "whether to upload deps: default true")
-	private boolean uploadDeps = true;
-	
+
+    private ActorSystem as;
+
 	private static Logger log = LoggerFactory.getLogger(ClusterSetup.class);
 
 
@@ -62,8 +61,8 @@ public class ClusterSetup {
 		boxCreator.create();
 		boxCreator.blockTillAllRunning();
 		List<String> hosts = boxCreator.getHosts();
-		provisionMaster(hosts.get(0));
-		//provisionWorkers(hosts.subList(1, hosts.size()));
+		//provisionMaster(hosts.get(0));
+		provisionWorkers(hosts);
 
 
 	}
@@ -71,32 +70,32 @@ public class ClusterSetup {
 
 
 
-	private void provisionMaster(String host) {
-		try {
-			HostProvisioner uploader = new HostProvisioner(host, "ec2-user");
-			uploader.addKeyFile(pathToPrivateKey);
 
-			uploader.uploadForDeployment(libDirPath, "lib");
-			if(dataSetPath != null)
-				uploader.uploadForDeployment(dataSetPath, "");
-			String sshPath = "/home/ec2-user/.ssh/";
-			uploader.uploadForDeployment(pathToPrivateKey, sshPath + new File(pathToPrivateKey).getName());
-			uploader.runRemoteCommand("chmod 0400 " + sshPath + "*");
-
-			uploader.uploadAndRun(masterSetupScriptPath, "");
-		}catch(Exception e) {
-			log.error("Error ",e);
-		}
-	}
 
 	private void provisionWorkers(List<String> workers) {
-		for(String workerHost : workers) {
+		as = ActorSystem.create("Workers");
+        for(final String workerHost : workers) {
 			try {
-				HostProvisioner uploader = new HostProvisioner(workerHost, "ec2-user");
-				uploader.addKeyFile(pathToPrivateKey);
+                Future<Void> f = Futures.future(new Callable<Void>(){
 
-				uploader.uploadForDeployment(libDirPath, "lib");
-				uploader.uploadAndRun(workerSetupScriptPath, "");
+                    /**
+                     * Computes a result, or throws an exception if unable to do so.
+                     *
+                     * @return computed result
+                     * @throws Exception if unable to compute a result
+                     */
+                    @Override
+                    public Void call() throws Exception {
+
+                        HostProvisioner uploader = new HostProvisioner(workerHost, "ec2-user");
+                        uploader.addKeyFile(pathToPrivateKey);
+                        uploader.runRemoteCommand("sudo hostname " + workerHost);
+                        uploader.uploadAndRun(workerSetupScriptPath, "");
+                        return null;
+                    }
+                },as.dispatcher());
+
+
 			}catch(Exception e) {
 				log.error("Error ",e);
 			}
