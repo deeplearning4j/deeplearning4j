ae9f31b854a33c4b6a8510303b701abbb62101ab
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-29 10:07:01 -0700

    working deep autoencoder demo, very simple 2 label approach
    Former-commit-id: 73cac9edb65674ac2cb0c3fd9e8659807b33688a

--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderExample.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderExample.java
@@ -28,7 +28,7 @@ public class DeepAutoEncoderExample {
     private static Logger log = LoggerFactory.getLogger(DeepAutoEncoderExample.class);
 
     public static void main(String[] args) throws Exception {
-        DataSetIterator iter = new MnistDataSetIterator(10,10);
+        DataSetIterator iter = new MnistDataSetIterator(80,80);
 
         Map<Integer,Boolean> activationForLayer = new HashMap<>();
 
@@ -38,17 +38,18 @@ public class DeepAutoEncoderExample {
         DBN dbn = new DBN.Builder()
                 .withHiddenUnitsByLayer(Collections.singletonMap(3, RBM.HiddenUnit.GAUSSIAN))
                 .withLossFunction(NeuralNetwork.LossFunction.RECONSTRUCTION_CROSSENTROPY)
-                .hiddenLayerSizes(new int[]{1000, 500, 250, 28})
-                .lossFunctionByLayer(Collections.singletonMap(3, NeuralNetwork.LossFunction.SQUARED_LOSS))
+                .hiddenLayerSizes(new int[]{1000, 500, 250, 28}).withMomentum(0.9)
+               // .lossFunctionByLayer(Collections.singletonMap(3, NeuralNetwork.LossFunction.SQUARED_LOSS))
                 .withOptimizationAlgorithm(NeuralNetwork.OptimizationAlgorithm.CONJUGATE_GRADIENT)
                 .sampleOrActivateByLayer(activationForLayer).activateForLayer(Collections.singletonMap(3,Activations.sigmoid()))
-                .learningRateForLayer(Collections.singletonMap(3, 1e-2)).withDropOut(0.5)
+                .learningRateForLayer(Collections.singletonMap(3, 1e-1))
                 .numberOfInputs(784)
                 .numberOfOutPuts(2)
                 .build();
 
         while(iter.hasNext()) {
             DataSet data = iter.next();
+            data.filterAndStrip(new int[]{0,1});
             dbn.pretrain(data.getFirst(), new Object[]{1, 1e-1, 100});
 
 
@@ -68,15 +69,22 @@ public class DeepAutoEncoderExample {
 
 
         DeepAutoEncoder encoder = new DeepAutoEncoder(dbn);
-        encoder.setVisibleUnit(RBM.VisibleUnit.BINARY);
+        encoder.setVisibleUnit(RBM.VisibleUnit.GAUSSIAN);
         encoder.setHiddenUnit(RBM.HiddenUnit.BINARY);
         while (iter.hasNext()) {
             DataSet next = iter.next();
-            encoder.finetune(next.getFirst(),1e-2,1000);
+            next.filterAndStrip(new int[]{0,1});
+            log.info("Fine tune " + next.labelDistribution());
+            encoder.finetune(next.getFirst(),1e-1,1000);
 
         }
+
+
+        iter.reset();
+
         while (iter.hasNext()) {
             DataSet data = iter.next();
+            data.filterAndStrip(new int[]{0,1});
             DoubleMatrix reconstruct = encoder.reconstruct(data.getFirst());
             for(int j = 0; j < data.numExamples(); j++) {
 
