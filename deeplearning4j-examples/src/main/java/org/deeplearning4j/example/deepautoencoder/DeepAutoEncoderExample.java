6cf7f1149c924a5959ed5c37ace8ab9f7f311566
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-29 08:38:15 -0700

    added loss function by layer

--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderExample.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderExample.java
@@ -5,6 +5,7 @@ import org.deeplearning4j.datasets.DataSet;
 import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.deeplearning4j.datasets.iterator.impl.ListDataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.datasets.mnist.draw.DrawReconstruction;
 import org.deeplearning4j.dbn.DBN;
 import org.deeplearning4j.nn.NeuralNetwork;
@@ -27,30 +28,32 @@ public class DeepAutoEncoderExample {
     private static Logger log = LoggerFactory.getLogger(DeepAutoEncoderExample.class);
 
     public static void main(String[] args) throws Exception {
-        MnistDataFetcher fetcher = new MnistDataFetcher(true);
-        fetcher.fetch(50);
-        DataSet data = fetcher.next();
-        data.filterAndStrip(new int[]{0,1});
-        log.info("Training on " + data.numExamples());
+        DataSetIterator iter = new MnistDataSetIterator(10,80);
+
         Map<Integer,Boolean> activationForLayer = new HashMap<>();
 
-       activationForLayer.put(3,true);
+        activationForLayer.put(3,true);
 
 
         DBN dbn = new DBN.Builder()
-                .withHiddenUnitsByLayer(Collections.singletonMap(3,RBM.HiddenUnit.GAUSSIAN))
+                .withHiddenUnitsByLayer(Collections.singletonMap(3, RBM.HiddenUnit.GAUSSIAN))
                 .withLossFunction(NeuralNetwork.LossFunction.RECONSTRUCTION_CROSSENTROPY)
                 .hiddenLayerSizes(new int[]{1000, 500, 250, 28})
+                .lossFunctionByLayer(Collections.singletonMap(3, NeuralNetwork.LossFunction.NEGATIVELOGLIKELIHOOD))
                 .withOptimizationAlgorithm(NeuralNetwork.OptimizationAlgorithm.CONJUGATE_GRADIENT)
-                .sampleOrActivateByLayer(activationForLayer).activateForLayer(Collections.singletonMap(3,Activations.linear()))
-                .learningRateForLayer(Collections.singletonMap(3, 1e-2))
+                .sampleOrActivateByLayer(activationForLayer).activateForLayer(Collections.singletonMap(3,Activations.sigmoid()))
+                .learningRateForLayer(Collections.singletonMap(3, 1e-2)).withDropOut(0.5)
                 .numberOfInputs(784)
                 .numberOfOutPuts(2)
                 .build();
 
-        DataSetIterator iter = new ListDataSetIterator(data.asList(),data.numExamples());
-        while(iter.hasNext())
-            dbn.pretrain(iter.next().getFirst(), new Object[]{1, 1e-1, 10000});
+        while(iter.hasNext()) {
+            DataSet data = iter.next();
+            dbn.pretrain(data.getFirst(), new Object[]{1, 1e-1, 10000});
+
+
+
+        }
 
 
 
@@ -65,32 +68,35 @@ public class DeepAutoEncoderExample {
 
 
         DeepAutoEncoder encoder = new DeepAutoEncoder(dbn);
-        encoder.setVisibleUnit(RBM.VisibleUnit.GAUSSIAN);
+        encoder.setVisibleUnit(RBM.VisibleUnit.BINARY);
         encoder.setHiddenUnit(RBM.HiddenUnit.BINARY);
-         while (iter.hasNext()) {
-             DataSet next = iter.next();
-             encoder.finetune(next.getFirst(),1e-2,1000);
+        while (iter.hasNext()) {
+            DataSet next = iter.next();
+            encoder.finetune(next.getFirst(),1e-2,1000);
 
-         }
-
-        DoubleMatrix reconstruct = encoder.reconstruct(data.getFirst());
-        for(int j = 0; j < data.numExamples(); j++) {
-
-            DoubleMatrix draw1 = data.get(j).getFirst().mul(255);
-            DoubleMatrix reconstructed2 = reconstruct.getRow(j);
-            DoubleMatrix draw2 = reconstructed2.mul(255);
-
-           DrawReconstruction d = new DrawReconstruction(draw1);
-            d.title = "REAL";
-            d.draw();
-            DrawReconstruction d2 = new DrawReconstruction(draw2);
-            d2.title = "TEST";
-            d2.draw();
-            Thread.sleep(10000);
-            d.frame.dispose();
-            d2.frame.dispose();
         }
+        while (iter.hasNext()) {
+            DataSet data = iter.next();
+            DoubleMatrix reconstruct = encoder.reconstruct(data.getFirst());
+            for(int j = 0; j < data.numExamples(); j++) {
 
+                DoubleMatrix draw1 = data.get(j).getFirst().mul(255);
+                DoubleMatrix reconstructed2 = reconstruct.getRow(j);
+                DoubleMatrix draw2 = reconstructed2.mul(255);
+
+                DrawReconstruction d = new DrawReconstruction(draw1);
+                d.title = "REAL";
+                d.draw();
+                DrawReconstruction d2 = new DrawReconstruction(draw2);
+                d2.title = "TEST";
+                d2.draw();
+                Thread.sleep(10000);
+                d.frame.dispose();
+                d2.frame.dispose();
+            }
+
+
+        }
 
 
     }
