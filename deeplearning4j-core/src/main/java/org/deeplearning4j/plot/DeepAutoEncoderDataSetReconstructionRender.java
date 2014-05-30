ddd7d4d8ed5cfa56893e589a5c9301924bad9f9e
agibsonccc <agibson@clevercloudcomputing.com>
2014-05-30 07:55:17 -0700

    added objects for rendering reconstructions from neural networks,multi layer and deep autoencoders
    Former-commit-id: 69568728b1b83ebbb4f648b34ca5b1606dc144d9 [formerly ec43d0fbbeb4337a89f9e2413a95b9825e204af2]
    Former-commit-id: b60d770fc1a96e9c4baffdf32a6b3cecc5cc8415

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/plot/DeepAutoEncoderDataSetReconstructionRender.java
@@ -0,0 +1,46 @@
+package org.deeplearning4j.plot;
+
+import org.deeplearning4j.autoencoder.DeepAutoEncoder;
+import org.deeplearning4j.datasets.DataSet;
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.mnist.draw.DrawReconstruction;
+import org.jblas.DoubleMatrix;
+
+/**
+ * Iterates through a dataset and draws reconstructions
+ */
+public class DeepAutoEncoderDataSetReconstructionRender {
+    private DataSetIterator iter;
+    private DeepAutoEncoder encoder;
+
+    public DeepAutoEncoderDataSetReconstructionRender(DataSetIterator iter, DeepAutoEncoder encoder) {
+        this.iter = iter;
+        this.encoder = encoder;
+    }
+
+    public void draw() throws InterruptedException {
+        while(iter.hasNext()) {
+            DataSet first = iter.next();
+            DoubleMatrix reconstruct = encoder.reconstruct(first.getFirst());
+            for(int j = 0; j < first.numExamples(); j++) {
+
+                DoubleMatrix draw1 = first.get(j).getFirst().mul(255);
+                DoubleMatrix reconstructed2 = reconstruct.getRow(j);
+                DoubleMatrix draw2 = reconstructed2.mul(255);
+
+                DrawReconstruction d = new DrawReconstruction(draw1);
+                d.title = "REAL";
+                d.draw();
+                DrawReconstruction d2 = new DrawReconstruction(draw2,1000,1000);
+                d2.title = "TEST";
+                d2.draw();
+                Thread.sleep(10000);
+                d.frame.dispose();
+                d2.frame.dispose();
+            }
+
+
+        }
+    }
+
+}
--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderFromFile.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/deepautoencoder/DeepAutoEncoderFromFile.java
@@ -25,33 +25,46 @@ public class DeepAutoEncoderFromFile {
 
     public static void main(String[] args) throws Exception {
         //batches of 10, 60000 examples total
-        DataSetIterator iter = new MnistDataSetIterator(10,60000);
+        DataSetIterator iter = new MnistDataSetIterator(100,60000);
 
 
         DBN dbn = SerializationUtils.readObject(new File(args[0]));
-        RBM r = (RBM) dbn.getLayers()[dbn.getnLayers() - 1];
-        r.setVisibleType(RBM.VisibleUnit.BINARY);
-        r.setHiddenType(RBM.HiddenUnit.GAUSSIAN);
-        dbn.setLayerLearningRates(Collections.singletonMap(dbn.getnLayers() - 1,1e-3));
-        dbn.getSigmoidLayers()[dbn.getnLayers() - 1].setActivationFunction(Activations.sigmoid());
+
+
+
         DeepAutoEncoder encoder = new DeepAutoEncoder(dbn);
 
-
-        encoder.setVisibleUnit(RBM.VisibleUnit.GAUSSIAN);
-        encoder.setHiddenUnit(RBM.HiddenUnit.BINARY);
 
         while(iter.hasNext()) {
             DataSet next = iter.next();
             if(next == null)
                 break;
-            dbn.setInput(next.getFirst());
-            dbn.pretrain(1,1e-2,1000);
             log.info("Training on " + next.numExamples());
-            List<DoubleMatrix> activations = dbn.feedForward();
-            log.info("Activation sum was " + activations.get(activations.size() - 2));
-            encoder.finetune(next.getFirst(),1e-1,1000);
+            encoder.finetune(next.getFirst(),1e-3,1000);
+
+            //Iterate over the data set after done training and show the 2 side by side (you have to drag the test image over to the right)
+            while(iter.hasNext()) {
+                DataSet first = iter.next();
+                DoubleMatrix reconstruct = encoder.reconstruct(first.getFirst());
+                for(int j = 0; j < first.numExamples(); j++) {
+
+                    DoubleMatrix draw1 = first.get(j).getFirst().mul(255);
+                    DoubleMatrix reconstructed2 = reconstruct.getRow(j);
+                    DoubleMatrix draw2 = reconstructed2.mul(255);
+
+                    DrawReconstruction d = new DrawReconstruction(draw1);
+                    d.title = "REAL";
+                    d.draw();
+                    DrawReconstruction d2 = new DrawReconstruction(draw2,1000,1000);
+                    d2.title = "TEST";
+                    d2.draw();
+                    Thread.sleep(10000);
+                    d.frame.dispose();
+                    d2.frame.dispose();
+                }
 
 
+            }
 
 
         }
