21201d8ca3eb75378f79078253896fdc6a8a1bd3
agibsonccc <agibson@clevercloudcomputing.com>
2014-07-02 07:30:38 -0700

    fixed error from latest commit on the parser example

--- deeplearning4j-examples/src/main/java/org/deeplearning4j/example/text/ParserExample.java
+++ deeplearning4j-examples/src/main/java/org/deeplearning4j/example/text/ParserExample.java
@@ -1,9 +1,12 @@
 package org.deeplearning4j.example.text;
 
 import org.cleartk.syntax.constituent.type.TopTreebankNode;
+import org.deeplearning4j.rntn.Tree;
 import org.deeplearning4j.text.treeparser.TreeParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * Created by agibsonccc on 6/30/14.
@@ -15,8 +18,7 @@ public class ParserExample {
     public static void main(String[] args) throws Exception {
 
         TreeParser parser = new TreeParser();
-        TopTreebankNode node = parser.getTree("This is a tree.");
-        log.info(node.getTreebankParse());
+        List<Tree> node = parser.getTrees("This is a tree.");
 
     }
 
