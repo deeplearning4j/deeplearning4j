3dc2a44652c95ec9b8a21ea0729e0fb12366fbdc
agibsonccc <agibson@clevercloudcomputing.com>
2014-07-02 19:42:30 -0700

    added basic csv data set iterator
    Former-commit-id: f61f853e35b7bf57ae657d68b791b07336cd794d [formerly 082929367e7dad7c7369098ad2cabb4266ee9480]
    Former-commit-id: 2aedc6b99a0301d52ab269cfa292c42dad3dd47d

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/CSVDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/CSVDataSetIterator.java
@@ -12,11 +12,52 @@ import java.io.InputStream;
  * @author Adam Gibson
  */
 public class CSVDataSetIterator extends BaseDatasetIterator {
+
+    /**
+     *
+     * @param batch the mini batch size
+     * @param numExamples the number of examples
+     * @param is the input stream to read from
+     * @param labelColumn the index (0 based) of the label
+     */
     public CSVDataSetIterator(int batch, int numExamples,InputStream is,int labelColumn) {
-        super(batch, numExamples, new CSVDataFetcher(is,labelColumn));
+        super(batch, numExamples, new CSVDataFetcher(is,labelColumn,0));
     }
 
+    /**
+     *
+     * @param batch the mini batch size
+     * @param numExamples the number of examples
+     * @param f the file to read from
+     * @param labelColumn the index (0 based) of the label
+     * @throws IOException
+     */
     public CSVDataSetIterator(int batch, int numExamples,File f,int labelColumn) throws IOException {
-        super(batch, numExamples, new CSVDataFetcher(f,labelColumn));
+        super(batch, numExamples, new CSVDataFetcher(f,labelColumn,0));
+    }
+
+    /**
+     *
+     * @param batch the mini batch size
+     * @param numExamples the number of examples
+     * @param is the input stream to read from
+     * @param labelColumn the index (0 based) of the label
+     * @param skipLines the number of lines to skip
+     */
+    public CSVDataSetIterator(int batch, int numExamples,InputStream is,int labelColumn,int skipLines) {
+        super(batch, numExamples, new CSVDataFetcher(is,labelColumn,skipLines));
+    }
+
+    /**
+     *
+     * @param batch the mini batch size
+     * @param numExamples the number of examples
+     * @param f the file to read from
+     * @param labelColumn the index (0 based) of the label
+     * @param skipLines the number of lines to skip
+     * @throws IOException
+     */
+    public CSVDataSetIterator(int batch, int numExamples,File f,int labelColumn,int skipLines) throws IOException {
+        super(batch, numExamples, new CSVDataFetcher(f,labelColumn,skipLines));
     }
 }
---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/datasets/iterator/CSVDataSetIteratorTest.java
@@ -0,0 +1,19 @@
+package org.deeplearning4j.datasets.iterator;
+
+import org.deeplearning4j.datasets.DataSet;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * CSV Test
+ * @author Adam Gibson
+ */
+public class CSVDataSetIteratorTest {
+    @Test
+    public void testCSV() throws Exception {
+       DataSetIterator iter = new CSVDataSetIterator(10,10,new ClassPathResource("csv-example.csv").getFile(),1,1);
+        DataSet next = iter.next();
+    }
+
+
+}
