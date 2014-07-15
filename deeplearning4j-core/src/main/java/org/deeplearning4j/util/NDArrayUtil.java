37ba3c432c2c2f02510a5e1c384160c88acfc46e
agibsonccc <agibson@clevercloudcomputing.com>
2014-07-15 12:09:08 -0700

    added putSlice and aggregate operations

--- deeplearning4j-core/src/main/java/org/deeplearning4j/util/NDArrayUtil.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/util/NDArrayUtil.java
@@ -3,14 +3,82 @@ package org.deeplearning4j.util;
 import org.deeplearning4j.nn.NDArray;
 
 /**
- * Created by agibsonccc on 7/14/14.
+ *  Basic NDArray ops
+ *
+ *  @author Adam Gibson
  */
 public class NDArrayUtil {
     public static enum ScalarOp {
         SUM,MEAN,PROD,MAX,MIN,ARG_MAX,ARG_MIN
     }
 
+    public static enum MatrixOp {
+        COLUMN_MIN,
+        COLUMN_MAX,
+        COLUMN_SUM,
+        COLUMN_MEAN,
+        ROW_MIN,
+        ROW_MAX,
+        ROW_SUM,
+        ROW_MEAN
+    }
 
+    private static boolean isRowOp(MatrixOp op) {
+        return
+                op == MatrixOp.ROW_MIN ||
+                op == MatrixOp.ROW_MAX ||
+                op == MatrixOp.ROW_SUM ||
+                op == MatrixOp.ROW_MEAN;
+    }
+
+
+    private static boolean isColumnOp(MatrixOp op) {
+        return
+                op == MatrixOp.COLUMN_MIN ||
+                        op == MatrixOp.COLUMN_MAX ||
+                        op == MatrixOp.COLUMN_SUM ||
+                        op == MatrixOp.COLUMN_MEAN;
+    }
+
+
+    public static NDArray doSliceWise(MatrixOp op,NDArray arr) {
+        int columns = isColumnOp(op) ? arr.columns() : arr.rows();
+        int[] shape = {arr.slices(),columns};
+
+        NDArray ret = new NDArray(shape);
+
+        for(int i = 0; i < arr.slices(); i++) {
+            switch(op) {
+               case COLUMN_MIN:
+                   ret.putSlice(i,arr.slice(i).columnMins());
+                   break;
+               case COLUMN_MAX:
+                   ret.putSlice(i,arr.slice(i).columnMaxs());
+                   break;
+               case COLUMN_SUM:
+                   ret.putSlice(i,arr.slice(i).columnSums());
+                   break;
+               case COLUMN_MEAN:
+                   ret.putSlice(i,arr.slice(i).columnMeans());
+                   break;
+               case ROW_MIN:
+                   ret.putSlice(i,arr.slice(i).rowMins());
+                   break;
+               case ROW_MAX:
+                   ret.putSlice(i,arr.slice(i).rowMaxs());
+                   break;
+               case ROW_SUM:
+                   ret.putSlice(i,arr.slice(i).rowSums());
+                  break;
+               case ROW_MEAN:
+                   ret.putSlice(i,arr.slice(i).rowMeans());
+                   break;
+            }
+        }
+
+
+        return arr;
+    }
 
 
 
