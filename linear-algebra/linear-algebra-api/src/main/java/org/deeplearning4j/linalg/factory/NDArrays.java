56236d84e5b7464817fd2137969c16d1aaec4298
agibsonccc <agibson@clevercloudcomputing.com>
2014-08-13 10:54:39 -0700

    added baseline factory method implementations for jblas

--- linear-algebra/linear-algebra-api/src/main/java/org/deeplearning4j/linalg/factory/NDArrays.java
+++ linear-algebra/linear-algebra-api/src/main/java/org/deeplearning4j/linalg/factory/NDArrays.java
@@ -1,6 +1,11 @@
 package org.deeplearning4j.linalg.factory;
 
+import org.deeplearning4j.linalg.api.complex.IComplexDouble;
+import org.deeplearning4j.linalg.api.complex.IComplexFloat;
+import org.deeplearning4j.linalg.api.complex.IComplexNDArray;
+import org.deeplearning4j.linalg.api.complex.IComplexNumber;
 import org.deeplearning4j.linalg.api.ndarray.INDArray;
+import org.deeplearning4j.linalg.util.ArrayUtil;
 import org.springframework.core.io.ClassPathResource;
 
 import java.lang.reflect.Constructor;
@@ -16,18 +21,316 @@ import java.util.Properties;
 public class NDArrays {
 
     private static Class<? extends INDArray> clazz;
+    private static Class<? extends IComplexNDArray> complexClazz;
     public final static String LINALG_PROPS = "/dl4j-linalg.properties";
+    public final static String REAL_CLASS_PROP = "real.class";
+    public final static String COMPLEX_CLASS_PROP = "complex.class";
+    public final static String DTYPE = "dtype";
+    public static String dtype;
+    private static Properties props = new Properties();
 
 
     static {
         try {
             ClassPathResource c = new ClassPathResource(LINALG_PROPS);
-            Properties props = new Properties();
             props.load(c.getInputStream());
-
+            String realType = props.get(REAL_CLASS_PROP + "." + props.get(DTYPE)).toString();
+            String complexType = props.get(COMPLEX_CLASS_PROP + "." + props.get(DTYPE)).toString();
+            dtype = props.get(DTYPE).toString();
+            clazz = (Class<? extends INDArray>) Class.forName(realType);
+            complexClazz = (Class<? extends IComplexNDArray>) Class.forName(complexType);
         }catch(Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param data the data to use with the ndarray
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(float[] data,int rows,int columns,int[] stride,int offset) {
+        return createComplex(data,new int[]{rows,columns},stride,offset);
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param data  the data to use with the ndarray
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(float[] data,int rows,int columns,int[] stride,int offset) {
+        return create(data,new int[]{rows,columns},stride,offset);
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param data the data to use with the ndarray
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset  the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(float[] data,int[] shape,int[] stride,int offset) {
+        try {
+            Constructor c = complexClazz.getConstructor(float[].class,int[].class,int[].class,int.class);
+            return (IComplexNDArray) c.newInstance(data,shape,stride,offset);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(float[] data,int[] shape,int[] stride,int offset) {
+        try {
+            Constructor c = clazz.getConstructor(float[].class,int[].class,int[].class,int.class);
+            return (INDArray) c.newInstance(data,shape,stride,offset);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(double[] data,int rows,int columns,int[] stride,int offset) {
+        return createComplex(data,new int[]{rows,columns},stride,offset);
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param data the data to use with tne ndarray
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(double[] data,int rows,int columns,int[] stride,int offset) {
+        return create(data,new int[]{rows,columns},stride,offset);
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset  the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(double[] data,int[] shape,int[] stride,int offset) {
+        try {
+            Constructor c = complexClazz.getConstructor(double[].class,int[].class,int[].class,int.class);
+            return (IComplexNDArray) c.newInstance(data,shape,stride,offset);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(double[] data,int[] shape,int[] stride,int offset) {
+        try {
+            Constructor c = clazz.getConstructor(double[].class,int[].class,int[].class,int.class);
+            return (INDArray) c.newInstance(data,shape,stride,offset);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int rows,int columns,int[] stride,int offset) {
+        if(dtype.equals("double"))
+            return createComplex(new double[rows * columns * 2],new int[]{rows,columns},stride,offset);
+        if(dtype.equals("float"))
+            return createComplex(new float[rows * columns * 2],new int[]{rows,columns},stride,offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(int rows,int columns,int[] stride,int offset) {
+        if(dtype.equals("double"))
+            return create(new double[rows * columns],new int[]{rows,columns},stride,offset);
+        if(dtype.equals("float"))
+            return create(new float[rows * columns],new int[]{rows,columns},stride,offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset  the offset of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int[] shape,int[] stride,int offset) {
+        if(dtype.equals("double"))
+            return createComplex(new double[ArrayUtil.prod(shape) * 2],shape,stride,offset);
+        if(dtype.equals("float"))
+            return createComplex(new float[ArrayUtil.prod(shape) * 2],shape,stride,offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @param offset the offset of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(int[] shape,int[] stride,int offset) {
+        if(dtype.equals("double"))
+            return create(new double[ArrayUtil.prod(shape)],shape,stride,offset);
+        if(dtype.equals("float"))
+            return create(new float[ArrayUtil.prod(shape)],shape,stride,offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+
+    }
+
+
+
+
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int rows,int columns,int[] stride) {
+        return createComplex(new int[]{rows,columns},stride);
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @param stride the stride for the ndarray
+     * @return the instance
+     */
+    public static INDArray create(int rows,int columns,int[] stride) {
+        return create(new int[]{rows,columns},stride);
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int[] shape,int[] stride) {
+        return createComplex(shape,stride,0);
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @param stride the stride for the ndarray
+     * @return the instance
+     */
+    public static INDArray create(int[] shape,int[] stride) {
+        return create(shape,stride,0);
+    }
+
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int rows,int columns) {
+        return createComplex(new int[]{rows, columns});
+    }
+
+
+    /**
+     * Creates an ndarray with the specified shape
+     * @param rows the rows of the ndarray
+     * @param columns the columns of the ndarray
+     * @return the instance
+     */
+    public static INDArray create(int rows,int columns) {
+        return create(new int[]{rows,columns});
+    }
+
+
+
+    /**
+     * Creates a complex ndarray with the specified shape
+     * @param shape the shape of the ndarray
+     * @return the instance
+     */
+    public static IComplexNDArray createComplex(int[] shape) {
+        return createComplex(shape, ArrayUtil.calcStrides(shape,2),0);
     }
 
 
@@ -37,13 +340,81 @@ public class NDArrays {
      * @return the instance
      */
     public static INDArray create(int[] shape) {
-        try {
-            Constructor c = clazz.getConstructor(int[].class);
-            return (INDArray) c.newInstance(shape);
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return create(shape,ArrayUtil.calcStrides(shape),0);
     }
+
+
+    /**
+     * Create a scalar ndarray with the specified offset
+     * @param value the value to initialize the scalar with
+     * @param offset the offset of the ndarray
+     * @return the created ndarray
+     */
+    public static INDArray scalar(Number value,int offset) {
+        if(dtype.equals("double"))
+            return scalar(value.doubleValue(),offset);
+        if(dtype.equals("float"))
+            return scalar(value.floatValue(),offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+    }
+
+    /**
+     * Create a scalar nd array with the specified value and offset
+     * @param value the value of the scalar
+     * @param offset the offset of the ndarray
+     * @return the scalar nd array
+     */
+    public static INDArray scalar(float value,int offset) {
+        return create(new float[]{value},new int[]{1},new int[]{1},offset);
+    }
+
+    /**
+     * Create a scalar nd array with the specified value and offset
+     * @param value the value of the scalar
+     * @param offset the offset of the ndarray
+     * @return the scalar nd array
+     */
+    public static INDArray scalar(double value,int offset) {
+        return create(new double[]{value},new int[]{1},new int[]{1},offset);
+
+    }
+
+
+    /**
+     * Create a scalar ndarray with the specified offset
+     * @param value the value to initialize the scalar with
+     * @param offset the offset of the ndarray
+     * @return the created ndarray
+     */
+    public static IComplexNDArray scalar(IComplexNumber value,int offset) {
+        if(dtype.equals("double"))
+            return scalar(value.asDouble(),offset);
+        if(dtype.equals("float"))
+            return scalar(value.asFloat(),offset);
+        throw new IllegalStateException("Illegal data type " + dtype);
+    }
+
+    /**
+     * Create a scalar nd array with the specified value and offset
+     * @param value the value of the scalar
+     * @param offset the offset of the ndarray
+     * @return the scalar nd array
+     */
+    public static IComplexNDArray scalar(IComplexFloat value,int offset) {
+        return createComplex(new float[]{value.realComponent(),value.imaginaryComponent()},new int[]{1},new int[]{1},offset);
+    }
+
+    /**
+     * Create a scalar nd array with the specified value and offset
+     * @param value the value of the scalar
+     * @param offset the offset of the ndarray
+     * @return the scalar nd array
+     */
+    public static IComplexNDArray scalar(IComplexDouble value,int offset) {
+        return createComplex(new double[]{value.realComponent(),value.imaginaryComponent()},new int[]{1},new int[]{1},offset);
+
+    }
+
 
 
 }
