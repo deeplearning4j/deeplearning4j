14c4031e44eb229edd654992344abf5851619f79
MJK <mjk@emmjaykay.org>
2014-08-14 00:32:11 -0700

    fix compile error, get jcublas thing going
    Former-commit-id: 596bba350a691837e87178e239572dfd92233828 [formerly 5dd6370d6f09432ebfaa288216eea6275a9ad544]
    Former-commit-id: 002c7afaa0469b5ccf83a23bd48b4cd8df4fad6d

---
+++ linear-algebra/linear-algebra-jcublas/src/main/java/org/deeplearning4j/linalg/api/ndarray/JCUBlasNDArray.java
@@ -0,0 +1,11 @@
+package org.deeplearning4j.linalg.api.ndarray;
+
+import org.deeplearning4j.linalg.factory.NDArrays;
+
+/**
+ * Created by mjk on 8/12/14.
+ */
+public class JCUBlasNDArray extends NDArrays /*implements INDArray */ {
+
+
+}
