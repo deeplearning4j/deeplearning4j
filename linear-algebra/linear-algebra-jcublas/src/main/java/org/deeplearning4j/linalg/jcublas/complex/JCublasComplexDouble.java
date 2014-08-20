4d82ffb5f72be800ded0a4ce1498766400584de1
MJK <mjk@emmjaykay.org>
2014-08-20 01:28:28 -0700

    Change up complex classes to get JCublasNDArray tests to run

---
+++ linear-algebra/linear-algebra-jcublas/src/main/java/org/deeplearning4j/linalg/jcublas/complex/JCublasComplexDouble.java
@@ -0,0 +1,234 @@
+package org.deeplearning4j.linalg.jcublas.complex;
+
+import org.deeplearning4j.linalg.api.complex.IComplexDouble;
+import org.deeplearning4j.linalg.api.complex.IComplexFloat;
+import org.deeplearning4j.linalg.api.complex.IComplexNumber;
+/**
+ * Created by mjk on 8/20/14.
+ */
+public class JCublasComplexDouble implements IComplexDouble  {
+    @Override
+    public IComplexNumber set(Number real, Number imag) {
+        return null;
+    }
+
+    @Override
+    public Double realComponent() {
+        return null;
+    }
+
+    @Override
+    public Double imaginaryComponent() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber dup() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber copy(IComplexNumber other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber addi(IComplexNumber c, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber addi(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber add(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber addi(Number a, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber addi(Number c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber add(Number c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber subi(IComplexNumber c, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber subi(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber sub(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber subi(Number a, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber subi(Number a) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber sub(Number r) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber muli(IComplexNumber c, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber muli(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber mul(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber mul(Number v) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber muli(Number v, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber muli(Number v) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber div(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber divi(IComplexNumber c, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber divi(IComplexNumber c) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber divi(Number v, IComplexNumber result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber divi(Number v) {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber div(Number v) {
+        return null;
+    }
+
+    @Override
+    public Number absoluteValue() {
+        return null;
+    }
+
+    @Override
+    public Number complexArgument() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber invi() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber inv() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber neg() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber negi() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber conji() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber conj() {
+        return null;
+    }
+
+    @Override
+    public IComplexNumber sqrt() {
+        return null;
+    }
+
+    @Override
+    public boolean eq(IComplexNumber c) {
+        return false;
+    }
+
+    @Override
+    public boolean ne(IComplexNumber c) {
+        return false;
+    }
+
+    @Override
+    public boolean isZero() {
+        return false;
+    }
+
+    @Override
+    public boolean isReal() {
+        return false;
+    }
+
+    @Override
+    public boolean isImag() {
+        return false;
+    }
+
+    @Override
+    public IComplexFloat asFloat() {
+        return null;
+    }
+
+    @Override
+    public IComplexDouble asDouble() {
+        return null;
+    }
+}
---
+++ linear-algebra/linear-algebra-jcublas/src/main/java/org/deeplearning4j/linalg/jcublas/complex/JCublasComplexNDArray.java
@@ -0,0 +1,537 @@
+package org.deeplearning4j.linalg.jcublas.complex;
+
+import org.deeplearning4j.linalg.api.complex.IComplexNDArray;
+import org.deeplearning4j.linalg.api.ndarray.INDArray;
+import org.deeplearning4j.linalg.api.ndarray.SliceOp;
+import org.deeplearning4j.linalg.ops.reduceops.Ops;
+
+/**
+ * Created by mjk on 8/20/14.
+ */
+public class JCublasComplexNDArray implements IComplexNDArray {
+
+    @Override
+    public INDArray getColumns(int[] columns) {
+        return null;
+    }
+
+    @Override
+    public INDArray getRows(int[] rows) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray min(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray max(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray put(int i, int j, INDArray element) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray put(int[] indices, INDArray element) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray putSlice(int slice, INDArray put) {
+        return null;
+    }
+
+    @Override
+    public void iterateOverDimension(int dimension, SliceOp op, boolean modify) {
+
+    }
+
+    @Override
+    public IComplexNDArray reduce(Ops.DimensionOp op, int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray getScalar(int... indexes) {
+        return null;
+    }
+
+    @Override
+    public void checkDimensions(INDArray other) {
+
+    }
+
+    @Override
+    public int[] endsForSlices() {
+        return new int[0];
+    }
+
+    @Override
+    public IComplexNDArray assign(Number value) {
+        return null;
+    }
+
+    @Override
+    public int linearIndex(int i) {
+        return 0;
+    }
+
+    @Override
+    public void iterateOverAllRows(SliceOp op) {
+
+    }
+
+    @Override
+    public IComplexNDArray rdiv(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rdivi(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rdiv(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rdivi(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rsub(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rsub(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rsubi(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray rsubi(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray hermitian() {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray conj() {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray conji() {
+        return null;
+    }
+
+    @Override
+    public INDArray getReal() {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray repmat(int[] shape) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray putRow(int row, INDArray toPut) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray putColumn(int column, INDArray toPut) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray getScalar(int row, int column) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray getScalar(int i) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray put(int i, INDArray element) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray diviColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray divColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray diviRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray divRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray muliColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mulColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray muliRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mulRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subiColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subiRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addiColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addColumnVector(INDArray columnVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addiRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addRowVector(INDArray rowVector) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mmul(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mmul(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray div(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray div(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mul(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mul(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray sub(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray sub(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray add(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray add(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mmuli(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mmuli(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray divi(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray divi(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray muli(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray muli(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subi(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray subi(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addi(INDArray other) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray addi(INDArray other, INDArray result) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray normmax(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray norm2(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray norm1(int dimension) {
+        return null;
+    }
+
+    @Override
+    public INDArray std(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray prod(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray mean(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray sum(int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray get(int[] indices) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray dup() {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray ravel() {
+        return null;
+    }
+
+    @Override
+    public int slices() {
+        return 0;
+    }
+
+    @Override
+    public IComplexNDArray slice(int i, int dimension) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray slice(int i) {
+        return null;
+    }
+
+    @Override
+    public int offset() {
+        return 0;
+    }
+
+    @Override
+    public IComplexNDArray reshape(int[] newShape) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray transpose() {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray swapAxes(int dimension, int with) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray permute(int[] rearrange) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray getColumn(int i) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray getRow(int i) {
+        return null;
+    }
+
+    @Override
+    public int columns() {
+        return 0;
+    }
+
+    @Override
+    public int rows() {
+        return 0;
+    }
+
+    @Override
+    public boolean isColumnVector() {
+        return false;
+    }
+
+    @Override
+    public boolean isRowVector() {
+        return false;
+    }
+
+    @Override
+    public boolean isVector() {
+        return false;
+    }
+
+    @Override
+    public boolean isMatrix() {
+        return false;
+    }
+
+    @Override
+    public boolean isScalar() {
+        return false;
+    }
+
+    @Override
+    public int[] shape() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] stride() {
+        return new int[0];
+    }
+
+    @Override
+    public int size(int dimension) {
+        return 0;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public IComplexNDArray broadcast(int[] shape) {
+        return null;
+    }
+
+    @Override
+    public IComplexNDArray broadcasti(int[] shape) {
+        return null;
+    }
+
+    @Override
+    public Object element() {
+        return null;
+    }
+
+    @Override
+    public double[] data() {
+        return new double[0];
+    }
+
+    @Override
+    public float[] floatData() {
+        return new float[0];
+    }
+}
