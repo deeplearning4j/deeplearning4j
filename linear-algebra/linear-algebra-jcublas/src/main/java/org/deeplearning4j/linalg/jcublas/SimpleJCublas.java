66584f979b90b41dd8622a15550ea150997731fe
MJK <mjk@emmjaykay.org>
2014-08-20 17:43:34 -0700

    towards simplejcublas

---
+++ linear-algebra/linear-algebra-jcublas/src/main/java/org/deeplearning4j/linalg/jcublas/SimpleJCublas.java
@@ -0,0 +1,13 @@
+package org.deeplearning4j.linalg.jcublas;
+
+/**
+ * Created by mjk on 8/20/14.
+ */
+public class SimpleJCublas {
+    public static JCublasNDArray gemv(JCublasNDArray A, JCublasNDArray B) {
+        JCublasNDArray C;
+        C = new JCublasNDArray(1);
+
+        return C;
+    }
+}
