be7e1f45d9ac15a3da8a92d0c5007249c89fe4ee
agibsonccc <agibson@clevercloudcomputing.com>
2014-08-24 23:26:30 -0700

    one minor fix to mmul on complex left
    Former-commit-id: 5e66d87f6b111f091079572e2af42dd385b3304a [formerly 0b8feb378ee0015fb12c276b5519134ba313493e]
    Former-commit-id: e7092e3418f6f91bdb8858912e4c4ce2d3439495

---
+++ linear-algebra/linear-algebra-api/src/main/java/org/deeplearning4j/linalg/ops/reduceops/complex/Norm1.java
@@ -0,0 +1,22 @@
+package org.deeplearning4j.linalg.ops.reduceops.complex;
+
+import org.deeplearning4j.linalg.api.complex.IComplexNDArray;
+import org.deeplearning4j.linalg.api.complex.IComplexNumber;
+import org.deeplearning4j.linalg.factory.NDArrays;
+
+/**
+ *
+ * Overall norm1 of an ndarray
+ *
+ * @author Adam Gibson
+ */
+public class Norm1 extends BaseScalarOp {
+    public Norm1() {
+        super(NDArrays.createDouble(0,0));
+    }
+
+    @Override
+    public IComplexNumber accumulate(IComplexNDArray arr, int i, IComplexNumber soFar) {
+        return soFar.add(arr.getComplex(i).absoluteValue());
+    }
+}
