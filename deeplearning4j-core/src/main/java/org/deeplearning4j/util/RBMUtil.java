0a868cfaf7f6d0eccf91bfe3a5235fb2d517f7da
agibsonccc <agibson@clevercloudcomputing.com>
2014-08-26 23:15:04 -0700

    weird compilation errors with enums
    Former-commit-id: 7fc2892637414a19f78d9a495e17c96da093c979 [formerly 8246bd2a022dc3816964912a5b2604d7527bd85d]
    Former-commit-id: 5c8bff617fc590e25383bf5a0e83aa85f67cb1dd

--- deeplearning4j-core/src/main/java/org/deeplearning4j/util/RBMUtil.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/util/RBMUtil.java
@@ -4,6 +4,9 @@ import org.deeplearning4j.models.featuredetectors.autoencoder.DeepAutoEncoder;
 import org.deeplearning4j.models.classifiers.dbn.DBN;
 import org.deeplearning4j.models.featuredetectors.rbm.RBM;
 
+import static  org.deeplearning4j.models.featuredetectors.rbm.RBM.HiddenUnit;
+import static  org.deeplearning4j.models.featuredetectors.rbm.RBM.VisibleUnit;
+
 /**
  * Handles various functions for RBM specific functions
  * @author Adam Gibson
@@ -60,28 +63,28 @@ public class RBMUtil {
     }
 
 
-    public static RBM.VisibleUnit inverse(RBM.HiddenUnit visible) {
+    public static RBM.VisibleUnit inverse(HiddenUnit visible) {
         switch(visible) {
             case BINARY:
-                return RBM.VisibleUnit.BINARY;
+                return  VisibleUnit.BINARY;
             case GAUSSIAN:
-                return RBM.VisibleUnit.GAUSSIAN;
+                return  VisibleUnit.GAUSSIAN;
             case SOFTMAX:
-                return RBM.VisibleUnit.SOFTMAX;
+                return  VisibleUnit.SOFTMAX;
             default:
                 return null;
 
         }
     }
 
-    public static RBM.HiddenUnit inverse(RBM.VisibleUnit hidden) {
+    public static RBM.HiddenUnit inverse( VisibleUnit hidden) {
         switch(hidden) {
             case BINARY:
-                return RBM.HiddenUnit.BINARY;
+                return   HiddenUnit.BINARY;
             case GAUSSIAN:
-                return RBM.HiddenUnit.GAUSSIAN;
+                return  HiddenUnit.GAUSSIAN;
             case SOFTMAX:
-                return RBM.HiddenUnit.SOFTMAX;
+                return  HiddenUnit.SOFTMAX;
         }
 
         return null;
