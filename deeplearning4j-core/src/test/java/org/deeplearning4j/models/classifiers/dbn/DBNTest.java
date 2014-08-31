1ae978ef1009a50a00c8c169670d2eda1f8ad504
agibsonccc <agibson@clevercloudcomputing.com>
2014-08-30 17:39:13 -0700

    added model tests
    Former-commit-id: 0b4bd09ede552668643470efd3bb24196ac43365

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/models/classifiers/dbn/DBNTest.java
@@ -0,0 +1,47 @@
+package org.deeplearning4j.models.classifiers.dbn;
+
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.linalg.api.activation.Activations;
+import org.deeplearning4j.linalg.api.ndarray.INDArray;
+import org.deeplearning4j.linalg.dataset.DataSet;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by agibsonccc on 8/28/14.
+ */
+public class DBNTest {
+
+    private static Logger log = LoggerFactory.getLogger(DBNTest.class);
+
+
+    @Test
+    public void testDbn() throws IOException {
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .learningRate(1e-1f).nIn(784).nOut(10).build();
+
+
+
+        DBN d = new DBN.Builder().configure(conf)
+                .hiddenLayerSizes(new int[]{500,250})
+                .build();
+        d.getOutputLayer().conf().setActivationFunction(Activations.softMaxRows());
+        MnistDataFetcher fetcher = new MnistDataFetcher(true);
+        fetcher.fetch(100);
+        DataSet d2 = fetcher.next();
+        d2.filterAndStrip(new int[]{0, 1});
+        d.fit(d2);
+        int[] predict = d.predict(d2.getFeatureMatrix());
+        log.info("Predict " + Arrays.toString(predict));
+
+    }
+
+}
---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/models/featuredetectors/rbm/RBMTests.java
@@ -0,0 +1,117 @@
+package org.deeplearning4j.models.featuredetectors.rbm;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.linalg.api.ndarray.INDArray;
+import org.deeplearning4j.linalg.dataset.DataSet;
+import org.deeplearning4j.linalg.factory.NDArrays;
+import org.deeplearning4j.linalg.lossfunctions.LossFunctions;
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.gradient.NeuralNetworkGradient;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by agibsonccc on 8/27/14.
+ */
+public class RBMTests {
+    private static Logger log = LoggerFactory.getLogger(RBMTests.class);
+
+
+    @Test
+    public void testBasic() {
+        float[][] data = new float[][]
+                {
+                        {1,1,1,0,0,0},
+                        {1,0,1,0,0,0},
+                        {1,1,1,0,0,0},
+                        {0,0,1,1,1,0},
+                        {0,0,1,1,0,0},
+                        {0,0,1,1,1,0},
+                        {0,0,1,1,1,0}
+                };
+
+
+        INDArray input = NDArrays.create(data);
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .learningRate(1e-1f).nIn(6).nOut(4).build();
+        Model rbm = new RBM.Builder().configure(conf).withInput(input).build();
+        rbm.fit(input);
+
+
+
+    }
+
+    @Test
+    public void testMnist() throws Exception {
+        MnistDataFetcher fetcher = new MnistDataFetcher(true);
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT).rng(new MersenneTwister(123))
+                .learningRate(1e-1f).nIn(784).nOut(600).build();
+
+        fetcher.fetch(10);
+        DataSet d2 = fetcher.next();
+
+        INDArray input = d2.getFeatureMatrix();
+
+        Model rbm = new RBM.Builder().configure(conf).withInput(input).build();
+        rbm.fit(input);
+
+    }
+
+
+    @Test
+    public void testSetGetParams() {
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .learningRate(1e-1f).nIn(6).nOut(4).build();
+        Model rbm = new RBM.Builder().configure(conf).build();
+        INDArray rand2 = NDArrays.rand(new int[]{1, rbm.numParams()});
+        rbm.setParams(rand2);
+        INDArray getParams = rbm.params();
+        assertEquals(rand2,getParams);
+    }
+
+    @Test
+    public void testGradient() {
+        float[][] data = new float[][]
+                {
+                        {1,1,1,0,0,0},
+                        {1,0,1,0,0,0},
+                        {1,1,1,0,0,0},
+                        {0,0,1,1,1,0},
+                        {0,0,1,1,0,0},
+                        {0,0,1,1,1,0},
+                        {0,0,1,1,1,0}
+                };
+
+
+        INDArray input = NDArrays.create(data);
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .learningRate(1e-1f).nIn(6).nOut(4).build();
+        RBM rbm = new RBM.Builder().configure(conf).withInput(input).build();
+        rbm.setInput(input);
+        float value = rbm.score();
+
+        for(int i = 0; i < 1000; i++) {
+            rbm.contrastiveDivergence(1e-1f,1,input);
+            value = rbm.score();
+
+        }
+        NeuralNetworkGradient grad2 = rbm.getGradient(new Object[]{1});
+        rbm.fit(input);
+
+    }
+
+
+
+}
