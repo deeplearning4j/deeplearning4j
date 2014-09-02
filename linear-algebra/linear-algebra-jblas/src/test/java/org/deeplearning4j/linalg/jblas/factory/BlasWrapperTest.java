ec4e8fb5dedf1a7aa442f9efc2b24b1a11665baf
agibsonccc <agibson@clevercloudcomputing.com>
2014-09-01 22:16:06 -0700

    iamax unit test/fix
    Former-commit-id: acb59820e3140d1f872f745aa2cc71639d1043e9 [formerly a662ce193f1cfdb48cfcd6b87ad59d9c348dc983]
    Former-commit-id: 6f5915923145a8d544c0ea2b9e5ed3ad09fe973a

---
+++ linear-algebra/linear-algebra-jblas/src/test/java/org/deeplearning4j/linalg/jblas/factory/BlasWrapperTest.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.linalg.jblas.factory;
+
+/**
+ * Created by agibsonccc on 9/1/14.
+ */
+public class BlasWrapperTest extends org.deeplearning4j.linalg.factory.BlasWrapperTest {
+}
