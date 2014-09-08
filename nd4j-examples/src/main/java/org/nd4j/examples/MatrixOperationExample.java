1125b0578e617f5636118ee09467047978ea8f06
ChrisN <chrisvnicholson@gmail.com>
2014-09-07 19:18:58 -0700

    Update and rename VectorOperationExample.java to MatrixOperationExample.java

--- nd4j-examples/src/main/java/org/nd4j/examples/MatrixOperationExample.java
+++ nd4j-examples/src/main/java/org/nd4j/examples/MatrixOperationExample.java
@@ -7,7 +7,7 @@ import org.nd4j.linalg.factory.Nd4j;
  * Created by cvn on 9/7/14.
  */
 
-public class VectorOperationExample {
+public class MatrixOperationExample {
 
     public static void main(String[] args) {
 
