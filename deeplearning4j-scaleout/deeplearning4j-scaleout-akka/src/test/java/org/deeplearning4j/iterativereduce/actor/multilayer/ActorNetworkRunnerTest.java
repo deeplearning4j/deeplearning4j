f3b26ae6a30814d292531b5e1f61802b246d2bab
agibsonccc <agibson@clevercloudcomputing.com>
2014-09-20 15:38:29 -0700

    maven clean up and some of the distributed integration

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/iterativereduce/actor/multilayer/ActorNetworkRunnerTest.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/iterativereduce/actor/multilayer/ActorNetworkRunnerTest.java
@@ -3,6 +3,7 @@ package org.deeplearning4j.iterativereduce.actor.multilayer;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.datasets.test.TestDataSetIterator;
 import org.deeplearning4j.models.classifiers.dbn.DBN;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.scaleout.conf.Conf;
 import org.junit.Test;
 import org.nd4j.linalg.transformation.MatrixTransform;
@@ -20,7 +21,12 @@ public class ActorNetworkRunnerTest {
         MnistDataSetIterator mnist = new MnistDataSetIterator(20, 100);
         TestDataSetIterator iter = new TestDataSetIterator(mnist);
         ActorNetworkRunner runner = new ActorNetworkRunner(iter);
+
+        NeuralNetConfiguration conf2 = new NeuralNetConfiguration.Builder()
+                .nIn(784).nOut(10).build();
+
         Conf conf = new Conf();
+        conf.setConf(conf2);
         conf.getConf().setFinetuneEpochs(1000);
         conf.getConf().setPretrainLearningRate(0.0001f);
         conf.setLayerSizes(new int[]{500,250,100});
@@ -48,6 +54,12 @@ public class ActorNetworkRunnerTest {
         MnistDataSetIterator mnist = new MnistDataSetIterator(5, 5);
         TestDataSetIterator iter = new TestDataSetIterator(mnist);
         ActorNetworkRunner runner = new ActorNetworkRunner(iter);
+
+        NeuralNetConfiguration conf2 = new NeuralNetConfiguration.Builder()
+                .nIn(784).nOut(10).build();
+
+
+
         Conf conf = new Conf();
         conf.getConf().setFinetuneEpochs(1);
         conf.getConf().setPretrainEpochs(1);
