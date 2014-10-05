bc2c6a1588d47617586b01c6dcfb9d53daf1c6bb
agibsonccc <agibson@clevercloudcomputing.com>
2014-10-05 00:59:05 -0700

    add tsne optimizations
    Former-commit-id: 2ffab41b6d3b0fb348cac1b0fbe7a51ef618afdc

--- deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneCalculationTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneCalculationTest.java
@@ -1,16 +1,11 @@
 package org.deeplearning4j.plot;
 
-import static org.junit.Assert.*;
-
-import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
-import java.util.Arrays;
 
 /**
  * Created by agibsonccc on 10/1/14.
@@ -19,12 +14,12 @@ public class TsneCalculationTest {
 
     @Test
     public void testTsne() throws Exception {
-        TsneCalculation calculation = new TsneCalculation.Builder().normallize(true)
+        TsneCalculation calculation = new TsneCalculation.Builder().normalize(false)
                 .build();
         ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
         File f = resource.getFile();
         INDArray data = Nd4j.readTxt(f.getAbsolutePath(),"   ");
-        data = data.getRow(0);
+        //data = data.getRow(0);
         //assertTrue(Arrays.equals(new int[]{2500,784},data.shape()));
         calculation.calculate(data,2,50,20);
     }
