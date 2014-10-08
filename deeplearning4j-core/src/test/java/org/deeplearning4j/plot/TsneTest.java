0727576f7c871016c86e06522beb6b47eb9b90e0
agibsonccc <0@blix.io>
2014-10-08 13:51:53 -0700

    update

--- deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneTest.java
@@ -1,11 +1,16 @@
 package org.deeplearning4j.plot;
 
+import com.google.common.reflect.ClassPath;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by agibsonccc on 10/1/14.
@@ -15,14 +20,15 @@ public class TsneTest {
     @Test
     public void testTsne() throws Exception {
         Tsne calculation = new Tsne.Builder().setMaxIter(10000)
-                .normalize(true).useAdaGrad(true).learningRate(1e-1f)
+                .normalize(true).useAdaGrad(true).learningRate(500)
                 .build();
         ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
         File f = resource.getFile();
         INDArray data = Nd4j.readTxt(f.getAbsolutePath(),"   ");
-        data = data.getRows(new int[]{0,5});
-        //assertTrue(Arrays.equals(new int[]{2500,784},data.shape()));
-        calculation.calculate(data,2,20);
+        ClassPathResource labels = new ClassPathResource("mnist2500_labels.txt");
+        List<String> labelsList = IOUtils.readLines(labels.getInputStream());
+
+        calculation.plot(data,2,28, labelsList);
     }
 
 
