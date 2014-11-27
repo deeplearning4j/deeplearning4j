d687327161619256bfc7d228f4c32279f8102748
agibsonccc <0@blix.io>
2014-11-27 13:51:31 -0800

    factor in visible bias

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/scaleout/perform/NeuralNetWorkPerformerTest.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/scaleout/perform/NeuralNetWorkPerformerTest.java
@@ -1,10 +1,13 @@
 package org.deeplearning4j.scaleout.perform;
 
+import org.deeplearning4j.datasets.fetchers.IrisDataFetcher;
 import org.deeplearning4j.models.featuredetectors.rbm.RBM;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.scaleout.conf.Configuration;
 import org.deeplearning4j.scaleout.conf.DeepLearningConfigurable;
+import org.deeplearning4j.scaleout.job.Job;
 import org.junit.Test;
+import org.nd4j.linalg.dataset.DataSet;
 
 /**
  * Created by agibsonccc on 11/27/14.
@@ -20,6 +23,11 @@ public class NeuralNetWorkPerformerTest extends BaseWorkPerformerTest {
         conf2.set(DeepLearningConfigurable.CLASS,RBM.class.getName());
         WorkerPerformer performer = new NeuralNetWorkPerformer();
         performer.setup(conf2);
+        IrisDataFetcher fetcher = new IrisDataFetcher();
+        fetcher.fetch(10);
+        DataSet d = fetcher.next();
+        Job j = new Job(d,"1");
+        assumeJobResultNotNull(performer,j);
     }
 
 
