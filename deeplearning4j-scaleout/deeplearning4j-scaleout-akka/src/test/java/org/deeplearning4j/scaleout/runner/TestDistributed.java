8e1da51144282afe118f270734b9adc472d1e008
agibsonccc <0@blix.io>
2014-11-27 20:16:11 -0800

    minor work in master
    Former-commit-id: e7c942ea9eb83efbb6ab8317c617560ab6b370f4

---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/scaleout/runner/TestDistributed.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.scaleout.runner;
+
+/**
+ * Created by agibsonccc on 11/27/14.
+ */
+public class TestDistributed {
+}
---
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-api/src/main/java/org/deeplearning4j/scaleout/job/collection/CollectionJobIterator.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.scaleout.job.collection;
+
+/**
+ * Created by agibsonccc on 11/27/14.
+ */
+public class CollectionJobIterator {
+}
