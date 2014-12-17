fc3f6982e8fa00d2603e6ce625cb1837bf437036
agibsonccc <0@blix.io>
2014-12-17 13:34:19 -0800

    split nd4j in to different cuda versions

---
+++ nd4j-jcublas-6.0/src/test/java/org/nd4j/linalg/jcublas/factory/JcublasBlasWrapperTests.java
@@ -0,0 +1,9 @@
+package org.nd4j.linalg.jcublas.factory;
+
+import org.nd4j.linalg.factory.BlasWrapperTests;
+
+/**
+ * Created by agibsonccc on 9/11/14.
+ */
+public class JcublasBlasWrapperTests extends BlasWrapperTests {
+}
