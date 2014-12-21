4fc42b048a403c311e2355cced7027545da20cc1
agibsonccc <0@blix.io>
2014-12-21 10:25:45 -0800

    add common dep for osx

--- nd4j-jcublas-osx-parent/nd4j-jcublas-osx-common/src/test/java/org/nd4j/linalg/jcublas/factory/JcublasBlasWrapperTests.java
+++
@@ -1,9 +0,0 @@
-package org.nd4j.linalg.jcublas.factory;
-
-import org.nd4j.linalg.factory.BlasWrapperTests;
-
-/**
- * Created by agibsonccc on 9/11/14.
- */
-public class JcublasBlasWrapperTests extends BlasWrapperTests {
-}
