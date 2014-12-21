63b888f29842edb2bcde9d5a7bb4a1257733cfcc
agibsonccc <0@blix.io>
2014-12-21 08:26:48 -0800

    make multi dimensional map serializable

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/models/classifiers/lstm/LSTM.java
@@ -0,0 +1,13 @@
+package org.deeplearning4j.models.classifiers.lstm;
+
+import java.io.Serializable;
+
+/**
+ * LSTM recurrent net
+ * @author Adam Gibson
+ */
+public class LSTM implements Serializable {
+
+
+
+}
