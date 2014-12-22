969321bd822411c8fe6d0893c201b320f3919a6a
agibsonccc <0@blix.io>
2014-12-22 15:24:01 -0800

    add tests

---
+++ nd4j-jcublas-osx-parent/nd4j-jcublas-osx-6.5/src/test/java/org/nd4j/linalg/jcublas/complex/JCublasComplexNDArrayTests.java
@@ -0,0 +1,28 @@
+package org.nd4j.linalg.jcublas.complex;
+
+import org.junit.Test;
+
+import org.nd4j.linalg.api.complex.IComplexNDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * Tests for a complex ndarray
+ *
+ * @author Adam Gibson
+ */
+public class JCublasComplexNDArrayTests extends org.nd4j.linalg.api.test.ComplexNDArrayTests {
+
+    private static Logger log = LoggerFactory.getLogger(JCublasComplexNDArrayTests.class);
+
+
+
+
+
+
+
+
+}
