911a475041e3ec00c1f00a19b53dfce111af5d08
Delip Rao <delip@ravellaw.com>
2014-12-29 15:11:00 -0800

    More low risk code cleanups

--- deeplearning4j-core/src/test/java/org/deeplearning4j/util/MovingWindowMatrixTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/util/MovingWindowMatrixTest.java
@@ -2,10 +2,7 @@ package org.deeplearning4j.util;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.List;
@@ -14,8 +11,6 @@ import java.util.List;
  * Created by agibsonccc on 6/11/14.
  */
 public class MovingWindowMatrixTest {
-    private static Logger log = LoggerFactory.getLogger(MovingWindowMatrixTest.class);
-
     @Test
     public void testMovingWindow() {
         INDArray ones = Nd4j.ones(4, 4);
@@ -25,10 +20,5 @@ public class MovingWindowMatrixTest {
         MovingWindowMatrix m2 = new MovingWindowMatrix(ones,2,2,true);
         List<INDArray> windowsRotate  = m2.windows();
         assertEquals(16,windowsRotate.size());
-
-
     }
-
-
-
 }
