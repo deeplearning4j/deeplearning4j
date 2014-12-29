29b848a03e325c14145b8f233f3446bcf74ed962
Delip Rao <delip@ravellaw.com>
2014-12-29 10:02:48 -0800

    Make testCSV actually test something
    Former-commit-id: 6c322211e6d46e37dd3f558ce15ad5b84062bd22 [formerly 28e06f42df7e5c5a6fb0c7dcf4351fc582f1ed9a]
    Former-commit-id: 732d9c72cdd0102c75f82d28c12e1a1ed82660eb

--- deeplearning4j-core/src/test/java/org/deeplearning4j/datasets/iterator/CSVDataSetIteratorTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/datasets/iterator/CSVDataSetIteratorTest.java
@@ -3,6 +3,7 @@ package org.deeplearning4j.datasets.iterator;
 import org.nd4j.linalg.dataset.DataSet;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
+import static org.junit.Assert.*;
 
 /**
  * CSV Test
@@ -11,8 +12,11 @@ import org.springframework.core.io.ClassPathResource;
 public class CSVDataSetIteratorTest {
     @Test
     public void testCSV() throws Exception {
-       DataSetIterator iter = new CSVDataSetIterator(10,10,new ClassPathResource("csv-example.csv").getFile(),1,1);
-        DataSet next = iter.next();
+       DataSetIterator iter =
+           new CSVDataSetIterator(10, 10, new ClassPathResource("csv-example.csv").getFile(), 1, 1);
+       DataSet next = iter.next();
+       assertEquals("", 10, next.numExamples());
+       assertEquals("", 479, next.numInputs());
     }
 
 
