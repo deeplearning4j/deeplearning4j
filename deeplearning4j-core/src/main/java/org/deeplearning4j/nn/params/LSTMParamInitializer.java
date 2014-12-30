2bc113cdad0c56f453c42ee5bb7aceb733f160a1
agibsonccc <0@blix.io>
2014-12-30 15:44:32 -0800

    runnable recurrent net test

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/params/LSTMParamInitializer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/params/LSTMParamInitializer.java
@@ -9,7 +9,9 @@ import org.nd4j.linalg.factory.Nd4j;
 import java.util.Map;
 
 /**
- * LSTM Parameters
+ * LSTM Parameters.
+ * Recurrent weights represent the all of the related parameters for the recurrent net.
+ * The decoder weights are used for predictions.
  * @author Adam Gibson
  */
 public class LSTMParamInitializer implements ParamInitializer {
@@ -19,9 +21,13 @@ public class LSTMParamInitializer implements ParamInitializer {
 
     @Override
     public void init(Map<String, INDArray> params, NeuralNetConfiguration conf) {
-        params.put(RECURRENT_WEIGHTS,WeightInitUtil.initWeights(conf.getnIn() + conf.getnOut() + 1, conf.getnOut() * 4, conf.getWeightInit(), conf.getActivationFunction(), conf.getDist()));
-        params.put(DECODER_WEIGHTS,WeightInitUtil.initWeights(conf.getnOut(),conf.getRecurrentOutput(),conf.getWeightInit(),conf.getActivationFunction(),conf.getDist()));
-        params.put(DECODER_BIAS, Nd4j.zeros(conf.getRecurrentOutput()));
+        int inputSize = conf.getnIn();
+        int hiddenSize = conf.getnIn();
+        int outputSize = conf.getnOut();
+
+        params.put(RECURRENT_WEIGHTS,WeightInitUtil.initWeights(inputSize + hiddenSize + 1, 4 * hiddenSize, conf.getWeightInit(), conf.getActivationFunction(), conf.getDist()));
+        params.put(DECODER_WEIGHTS,WeightInitUtil.initWeights(hiddenSize,outputSize,conf.getWeightInit(),conf.getActivationFunction(),conf.getDist()));
+        params.put(DECODER_BIAS, Nd4j.zeros(outputSize));
 
     }
 }
