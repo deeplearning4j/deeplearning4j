0f4d330e9fc9f135d200f8107372a6347cefa492
Josh Patterson <jpatterson@floe.tv>
2015-01-03 09:34:09 -0500

    add yarn port wip
    Former-commit-id: d75f14ffa27b5411e55493f0fc98861a5e418944 [formerly 45c335047bc6cc2196574bd07c5ced97b9a45498]
    Former-commit-id: b17f01239e5756dc0f76b167d2e7b5035f23b206

---
+++ deeplearning4j-scaleout/hadoop-yarn/cdh4/src/main/java/org/deeplearning4j/iterativereduce/runtime/io/SVMLightVectorNoLabelException.java
@@ -0,0 +1,11 @@
+package org.deeplearning4j.iterativereduce.runtime.io;
+
+public class SVMLightVectorNoLabelException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
