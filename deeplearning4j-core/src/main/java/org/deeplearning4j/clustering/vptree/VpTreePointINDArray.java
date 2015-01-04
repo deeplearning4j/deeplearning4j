babacc42584ae065c0946cb8f94edd4e852e1c32
agibsonccc <0@blix.io>
2015-01-04 11:36:58 -0800

    more tsne filled out; bit more to go
    Former-commit-id: dc12a0ca263e05a7fb4dd9a1bdb27195fe9e79ff

--- deeplearning4j-core/src/main/java/org/deeplearning4j/clustering/vptree/VpTreePointINDArray.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/clustering/vptree/VpTreePointINDArray.java
@@ -8,6 +8,7 @@ import org.nd4j.linalg.distancefunction.EuclideanDistance;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * VPTree point for ndarrays
@@ -16,6 +17,21 @@ import java.util.List;
 public class VpTreePointINDArray implements VpTreePoint<VpTreePointINDArray> {
     private INDArray data;
     private DistanceFunction distanceFunction;
+    private int index = 0;
+
+
+    /**
+     * Takes in the data. This will throw an IllegalArgumentException if the data isn't a vector
+     * @param data the data
+     */
+    public VpTreePointINDArray(INDArray data,int index) {
+        if(!data.isVector())
+            throw new IllegalArgumentException("Data should only be a vector");
+        this.data = data;
+        distanceFunction = new EuclideanDistance(data);
+        this.index = index;
+    }
+
 
     /**
      * Takes in the data. This will throw an IllegalArgumentException if the data isn't a vector
@@ -52,6 +68,7 @@ public class VpTreePointINDArray implements VpTreePoint<VpTreePointINDArray> {
      */
     public static List<VpTreePointINDArray> dataPoints(INDArray matrix) {
         final List<VpTreePointINDArray> ret = new ArrayList<>();
+        final AtomicInteger count = new AtomicInteger(0);
         matrix.iterateOverAllRows(new SliceOp() {
             @Override
             public void operate(DimensionSlice nd) {
@@ -60,7 +77,8 @@ public class VpTreePointINDArray implements VpTreePoint<VpTreePointINDArray> {
 
             @Override
             public void operate(INDArray nd) {
-                 ret.add(new VpTreePointINDArray(nd));
+                 ret.add(new VpTreePointINDArray(nd,count.get()));
+                 count.incrementAndGet();
             }
         });
 
@@ -77,5 +95,29 @@ public class VpTreePointINDArray implements VpTreePoint<VpTreePointINDArray> {
     @Override
     public String toString() {
         return data != null ? data.toString() : "[]";
+    }
+
+    public INDArray getData() {
+        return data;
+    }
+
+    public void setData(INDArray data) {
+        this.data = data;
+    }
+
+    public DistanceFunction getDistanceFunction() {
+        return distanceFunction;
+    }
+
+    public void setDistanceFunction(DistanceFunction distanceFunction) {
+        this.distanceFunction = distanceFunction;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 }
