a0e7ef88ad2994797400c6c3eb19768ac3d34ddf
Josh Patterson <jpatterson@floe.tv>
2015-01-06 00:18:58 -0500

    various WIP updates to support the SVMLightHDFSFetcher work for the Yarn IR Worker
    Former-commit-id: 5b6b0bd2836611f73797e85568b6a97abd2f93d7

--- deeplearning4j-scaleout/hadoop-yarn/cdh4/src/main/java/org/deeplearning4j/iterativereduce/runtime/io/SVMLightHDFSDataSetIterator.java
+++ deeplearning4j-scaleout/hadoop-yarn/cdh4/src/main/java/org/deeplearning4j/iterativereduce/runtime/io/SVMLightHDFSDataSetIterator.java
@@ -1,5 +1,21 @@
 package org.deeplearning4j.iterativereduce.runtime.io;
 
-public class SVMLightHDFSDataSetIterator {
+import java.io.IOException;
 
+import org.deeplearning4j.datasets.iterator.BaseDatasetIterator;
+
+
+public class SVMLightHDFSDataSetIterator extends BaseDatasetIterator {
+
+	public SVMLightHDFSDataSetIterator( int batch,int numExamples, TextRecordParser hdfsLineParser, int maxFeatureCount, int numberClasses ) throws IOException {
+		
+		super( batch, numExamples, new SVMLightDataFetcher( hdfsLineParser, maxFeatureCount, numberClasses ) );
+		
+		
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return fetcher.hasMore();
+	}
 }
