7a08f598f3faa223089fbc7be0afeda8f1ede977
agibsonccc <0@blix.io>
2015-01-10 18:22:39 -0800

    add some more tests for the configuration loading
    Former-commit-id: 06842ec808f50ebe2f640136f484ea276a9120d1 [formerly 2eda691fbddd8da224d5a96ca18c9ab27782bc50]
    Former-commit-id: bbecc85a39d1beb099f3f9d4c5906db7f84c17e9

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/MultiLayerNeuralNetConfigurationTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/MultiLayerNeuralNetConfigurationTest.java
@@ -1,7 +1,12 @@
 package org.deeplearning4j.nn.conf;
 
 import static org.junit.Assert.*;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.*;
+import java.util.Properties;
 
 /**
  * Created by agibsonccc on 11/27/14.
@@ -9,12 +14,33 @@ import org.junit.Test;
 public class MultiLayerNeuralNetConfigurationTest {
 
     @Test
-    public void testJson() {
+    public void testJson() throws Exception {
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .list(4).hiddenLayerSizes(new int[]{3,2,2}).build();
         String json = conf.toJson();
         MultiLayerConfiguration from = MultiLayerConfiguration.fromJson(json);
         assertEquals(conf,from);
+
+        Properties props = new Properties();
+        props.put("json",json);
+        String key = props.getProperty("json");
+        assertEquals(json,key);
+        File f = new File("props");
+        f.deleteOnExit();
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(f));
+        props.store(bos,"");
+        bos.flush();
+        bos.close();
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+        Properties props2 = new Properties();
+        props2.load(bis);
+        bis.close();
+        assertEquals(props2.getProperty("json"),props.getProperty("json"));
+
+        MultiLayerConfiguration conf3 = MultiLayerConfiguration.fromJson(props2.getProperty("json"));
+        assertEquals(conf,conf3);
+
+
     }
 
 
