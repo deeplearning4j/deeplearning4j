ad625c4192982e848154c094a1f7ba64aa4c94a5
agibsonccc <0@blix.io>
2015-01-19 15:31:28 -0800

    add single layer spark worker
    Former-commit-id: 72f04358c828314a19da833372f0b49f609dacad [formerly ead15085a39146f718bc4a23cd93157d1493cc53]
    Former-commit-id: 9fb4f446a10d16adb9c957cbf8192c7ac0beadce

--- deeplearning4j-scaleout/spark/spark-cdh5/src/main/java/org/deeplearning4j/spark/impl/layer/DL4jWorker.java
+++ deeplearning4j-scaleout/spark/spark-cdh5/src/main/java/org/deeplearning4j/spark/impl/layer/DL4jWorker.java
@@ -1,8 +1,11 @@
-package org.deeplearning4j.spark.impl.multilayer;
+package org.deeplearning4j.spark.impl.layer;
 
 import org.apache.spark.api.java.function.Function;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.LayerFactory;
 import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
@@ -22,19 +25,22 @@ public class DL4jWorker implements Function<DataSet, INDArray> {
     private final Model network;
 
     public DL4jWorker(String json,INDArray params) {
-        this.network = new MultiLayerNetwork(MultiLayerConfiguration.fromJson(json));
-        MultiLayerNetwork thisNetwork = (MultiLayerNetwork) this.network;
-        thisNetwork.init();
+        NeuralNetConfiguration conf = NeuralNetConfiguration.fromJson(json);
+        LayerFactory layerFactory = conf.getLayerFactory();
+        if(layerFactory == null)
+            throw new IllegalStateException("Please specify a layer factory");
+        this.network = layerFactory.create(conf);
         int numParams = this.network.numParams();
         if(numParams != params.length())
             throw new IllegalStateException("Number of params for configured network was " + numParams + " while the specified parameter vector length was " + params.length());
-        thisNetwork.setParameters(params);
+        Layer network = (Layer) this.network;
+        network.setParameters(params);
     }
 
     @Override
     public INDArray call(DataSet v1) throws Exception {
-        MultiLayerNetwork thisNetwork = (MultiLayerNetwork) this.network;
-        thisNetwork.fit(v1);
+        Layer network = (Layer) this.network;
+        network.fit(v1.getFeatureMatrix());
         return network.params();
 
     }
