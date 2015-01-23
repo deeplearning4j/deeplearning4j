0da69256c6d7faf5c46be81e7505b772c1e00c97
agibsonccc <0@blix.io>
2015-01-23 11:30:02 -0800

    condense spark
    Former-commit-id: 2d9eadd10b7766f254ed0382e0e5fb23982f0d14 [formerly 6a7c576fb0346d9c1ac799e75a28fd101b4b48c5]
    Former-commit-id: 6a612ab0c1f37b69f9f2b8e85ff8ac2a520a9db4

---
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/test/java/org/deeplearning4j/spark/BaseSparkTest.java
@@ -0,0 +1,42 @@
+package org.deeplearning4j.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Created by agibsonccc on 1/23/15.
+ */
+public abstract class BaseSparkTest {
+    protected JavaSparkContext sc;
+
+    @Before
+    public void before() {
+        sc = getContext();
+    }
+    @After
+    public void after() {
+        sc.close();
+        sc = null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public JavaSparkContext getContext() {
+        if(sc != null)
+            return sc;
+        // set to test mode
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("local")
+                .setAppName("sparktest");
+
+
+        sc = new JavaSparkContext(sparkConf);
+        return sc;
+
+    }
+
+}
