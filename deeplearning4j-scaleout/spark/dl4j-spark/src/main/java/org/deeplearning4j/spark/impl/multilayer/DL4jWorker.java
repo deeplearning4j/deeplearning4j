c4d98c75ae82b9c10a71f39fa58d43c086f14a77
agibsonccc <0@blix.io>
2015-01-23 09:55:27 -0800

    cleanup spark modules
    Former-commit-id: 6a1f15b01146c92c44ded74dfadd34fa49e35ab4 [formerly baabc14e9e0ff260ec8c77248144e3ae19a5f852]
    Former-commit-id: 7e80986a2af5bcae30ad98186b49137de63039d4

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/multilayer/DL4jWorker.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/multilayer/DL4jWorker.java
@@ -1,14 +1,13 @@
-package org.deeplearning4j.spark.impl.layer;
+package org.deeplearning4j.spark.impl.multilayer;
 
 import org.apache.spark.api.java.function.Function;
-import org.deeplearning4j.nn.api.Layer;
-import org.deeplearning4j.nn.api.LayerFactory;
 import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This is considered the "Worker"
@@ -23,24 +22,21 @@ import org.nd4j.linalg.dataset.DataSet;
 public class DL4jWorker implements Function<DataSet, INDArray> {
 
     private final Model network;
-
+    private static Logger log = LoggerFactory.getLogger(DL4jWorker.class);
     public DL4jWorker(String json,INDArray params) {
-        NeuralNetConfiguration conf = NeuralNetConfiguration.fromJson(json);
-        LayerFactory layerFactory = conf.getLayerFactory();
-        if(layerFactory == null)
-            throw new IllegalStateException("Please specify a layer factory");
-        this.network = layerFactory.create(conf);
+        this.network = new MultiLayerNetwork(MultiLayerConfiguration.fromJson(json));
+        MultiLayerNetwork thisNetwork = (MultiLayerNetwork) this.network;
+        thisNetwork.init();
         int numParams = this.network.numParams();
         if(numParams != params.length())
             throw new IllegalStateException("Number of params for configured network was " + numParams + " while the specified parameter vector length was " + params.length());
-        Layer network = (Layer) this.network;
-        network.setParameters(params);
+        thisNetwork.setParameters(params);
     }
 
     @Override
     public INDArray call(DataSet v1) throws Exception {
-        Layer network = (Layer) this.network;
-        network.fit(v1.getFeatureMatrix());
+        MultiLayerNetwork thisNetwork = (MultiLayerNetwork) this.network;
+        thisNetwork.fit(v1);
         return network.params();
 
     }
