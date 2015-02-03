bd462555967f8357508e822a5a5e46763ad9833a
agibsonccc <0@blix.io>
2015-02-02 19:03:46 -0800

    serializable iteration listeners
    Former-commit-id: 2dde8fa0161678dc069f39b324ee9d504f78af71 [formerly 825bde4c3e2669d3cab3fc0bfbccb9ecaf07f05e]
    Former-commit-id: 18cae5c82832ca333f041d97e966cf19f1898acf

--- deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/api/IterationListener.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/api/IterationListener.java
@@ -3,12 +3,14 @@ package org.deeplearning4j.optimize.api;
 
 import org.deeplearning4j.nn.api.Model;
 
+import java.io.Serializable;
+
 /**
  * Each epoch the listener is called, mainly used for debugging or visualizations
  * @author Adam Gibson
  *
  */
-public interface IterationListener {
+public interface IterationListener extends Serializable {
 	/**
 	 * Event listener for each iteration
 	 * @param iteration the iteration
