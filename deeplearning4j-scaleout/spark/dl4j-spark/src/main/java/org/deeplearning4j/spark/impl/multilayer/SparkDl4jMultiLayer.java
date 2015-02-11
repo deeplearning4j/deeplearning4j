0b7add1d128d67b5fc8da799c55a88aaf4de0a5e
agibsonccc <0@blix.io>
2015-02-11 08:24:44 -0800

    fix null constructor
    Former-commit-id: b983f61ce88f8b61a715b8075bb7d9d697ba45fb

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/multilayer/SparkDl4jMultiLayer.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/multilayer/SparkDl4jMultiLayer.java
@@ -48,7 +48,7 @@ public class SparkDl4jMultiLayer implements Serializable {
     public SparkDl4jMultiLayer(SparkContext sparkContext, MultiLayerNetwork network) {
         this.sparkContext = sparkContext;
         this.averageEachIteration = sparkContext.conf().getBoolean(AVERAGE_EACH_ITERATION,false);
-        this.conf = conf.clone();
+        this.conf = this.network.getLayerWiseConfigurations().clone();
         sc = new JavaSparkContext(this.sparkContext);
         this.network = network;
     }
