2ead007c5157a1739abb7bc17dcf8595a86d8211
sonaliii <sona8242@gmail.com>
2015-02-16 14:44:03 -0800

    merge
    Former-commit-id: 6d32c558bb1bd93624caf3790d63a64f40e8e153 [formerly 73e56853994320305d165942cacccc802896d7f0]
    Former-commit-id: 1543b301d649f60933bb910ca9550184ae38561e

---
+++ deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/subcommands/Test.java
@@ -0,0 +1,32 @@
+package org.deeplearning4j.cli.subcommands;
+
+import org.deeplearning4j.cli.api.SubCommand;
+import org.kohsuke.args4j.Option;
+
+/**
+ * Subcommand for testing model
+ *
+ * Options:
+ *      Required:
+ *          --input: input data file for model
+ *          --model: json configuration for model
+ *
+ * Created by sonali on 2/10/15.
+ */
+public class Test extends BaseSubCommand {
+
+    @Option(name = "--input", usage = "input data",aliases = "-i", required = true)
+    private String input = "input.txt";
+
+    @Option(name = "--model", usage = "model for prediction", aliases = "-m", required = true)
+    private String model = "model.json";
+
+    public Test(String[] args) {
+        super(args);
+    }
+
+    @Override
+    public void exec() {
+
+    }
+}
