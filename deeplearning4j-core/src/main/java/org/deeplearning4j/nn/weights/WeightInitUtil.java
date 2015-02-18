3ab1a28575ba126fcc0a030c9dc61a3e7e623642
Suneel Marthi <suneel.marthi@gmail.com>
2015-02-18 03:11:41 -0500

    First pass at fixing some of the broken tests
    Former-commit-id: f62081243b86f55ef8645d0ae8cc98a8dec5194d

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/weights/WeightInitUtil.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/weights/WeightInitUtil.java
@@ -11,95 +11,102 @@ import org.nd4j.linalg.factory.Nd4j;
 
 /**
  * Weight initialization utility
+ *
  * @author Adam Gibson
  */
 public class WeightInitUtil {
 
 
-    /**
-     * Normalized weight init
-     * @param shape shape
-     * @param nIn number of inputs
-     * @return the weights
-     */
-    public static INDArray normalized(int[] shape,int nIn) {
-        return Nd4j.rand(shape).subi(0.5).divi((double) nIn);
-    }
+  /**
+   * Normalized weight init
+   *
+   * @param shape shape
+   * @param nIn   number of inputs
+   * @return the weights
+   */
+  public static INDArray normalized(int[] shape, int nIn) {
+    return Nd4j.rand(shape).subi(0.5).divi((double) nIn);
+  }
 
-    /**
-     * Generate a random matrix with respect to the number of inputs and outputs.
-     * This is a bound uniform distribution with the specified minimum and maximum
-     * @param shape the shape of the matrix
-     * @param nIn the number of inputs
-     * @param nOut the number of outputs
-     * @return
-     */
-    public static INDArray uniformBasedOnInAndOut(int[] shape,int nIn,int nOut) {
-        double min = -4.0 * Math.sqrt(6.0 / (double) (nOut + nIn));
-        double max = 4.0 * Math.sqrt(6.0 / (double) (nOut + nIn));
-        return Nd4j.rand(shape, Distributions.uniform(new MersenneTwister(123),min,max));
-    }
+  /**
+   * Generate a random matrix with respect to the number of inputs and outputs.
+   * This is a bound uniform distribution with the specified minimum and maximum
+   *
+   * @param shape the shape of the matrix
+   * @param nIn   the number of inputs
+   * @param nOut  the number of outputs
+   * @return {@link INDArray}
+   */
+  public static INDArray uniformBasedOnInAndOut(int[] shape, int nIn, int nOut) {
+    double min = -4.0 * Math.sqrt(6.0 / (double) (nOut + nIn));
+    double max = 4.0 * Math.sqrt(6.0 / (double) (nOut + nIn));
+    return Nd4j.rand(shape, Distributions.uniform(new MersenneTwister(123), min, max));
+  }
 
-    public static INDArray initWeights(int[] shape,float min,float max) {
-        return Nd4j.rand(shape,min,max,new MersenneTwister(123));
-    }
-
-
-    /**
-     * Initializes a matrix with the given weight initialization scheme
-     * @param shape the shape of the matrix
-     * @param initScheme the scheme to use
-     * @return a matrix of the specified dimensions with the specified
-     * distribution based on the initialization scheme
-     */
-    public static INDArray initWeights(int[] shape,WeightInit initScheme,ActivationFunction act,RealDistribution dist) {
-        INDArray ret;
-        switch(initScheme) {
-            case NORMALIZED:
-                ret = Nd4j.rand(shape);
-                return ret.subi(0.5).divi(shape[0]);
-            case UNIFORM:
-                double a = 1 / shape[0];
-                return Nd4j.rand(shape,-a,a,new MersenneTwister(123));
-
-            case  VI:
-                ret = Nd4j.rand(shape);
-                int len = 0;
-                for(int i = 0; i < shape.length; i++)
-                    len += shape[i];
-                double r = Math.sqrt(6) / Math.sqrt(len + 1);
-                ret.muli(2).muli(r).subi(r);
-                return ret;
-
-            case DISTRIBUTION:
-                ret = Nd4j.rand(shape);
-                for(int i = 0; i < ret.slices(); i++) {
-                    ret.putSlice(i,Nd4j.create(dist.sample(ret.columns())));
-                }
-                return ret;
-            case SIZE:
-                return uniformBasedOnInAndOut(shape,shape[0],shape[1]);
-            case ZERO:
-                return Nd4j.create(shape);
+  public static INDArray initWeights(int[] shape, float min, float max) {
+    return Nd4j.rand(shape, min, max, new MersenneTwister(123));
+  }
 
 
+  /**
+   * Initializes a matrix with the given weight initialization scheme
+   *
+   * @param shape      the shape of the matrix
+   * @param initScheme the scheme to use
+   * @return a matrix of the specified dimensions with the specified
+   * distribution based on the initialization scheme
+   */
+  public static INDArray initWeights(int[] shape, WeightInit initScheme,
+                                     RealDistribution dist) {
+    INDArray ret;
+    switch (initScheme) {
+      case NORMALIZED:
+        ret = Nd4j.rand(shape);
+        return ret.subi(0.5).divi(shape[0]);
+      case UNIFORM:
+        double a = 1 / shape[0];
+        return Nd4j.rand(shape, -a, a, new MersenneTwister(123));
 
+      case VI:
+        ret = Nd4j.rand(shape);
+        int len = 0;
+        for (int aShape : shape) {
+          len += aShape;
         }
+        double r = Math.sqrt(6) / Math.sqrt(len + 1);
+        ret.muli(2).muli(r).subi(r);
+        return ret;
 
-        throw new IllegalStateException("Illegal weight init value");
+      case DISTRIBUTION:
+        ret = Nd4j.rand(shape);
+        for (int i = 0; i < ret.slices(); i++) {
+          ret.putSlice(i, Nd4j.create(dist.sample(ret.columns())));
+        }
+        return ret;
+      case SIZE:
+        return uniformBasedOnInAndOut(shape, shape[0], shape[1]);
+      case ZERO:
+        return Nd4j.create(shape);
+
+
     }
 
-    /**
-     * Initializes a matrix with the given weight initialization scheme
-     * @param nIn the number of rows in the matrix
-     * @param nOut the number of columns in the matrix
-     * @param initScheme the scheme to use
-     * @return a matrix of the specified dimensions with the specified
-     * distribution based on the initialization scheme
-     */
-    public static INDArray initWeights(int nIn,int nOut,WeightInit initScheme,ActivationFunction act,RealDistribution dist) {
-        return initWeights(new int[]{nIn,nOut},initScheme,act,dist);
-    }
+    throw new IllegalStateException("Illegal weight init value");
+  }
+
+  /**
+   * Initializes a matrix with the given weight initialization scheme
+   *
+   * @param nIn        the number of rows in the matrix
+   * @param nOut       the number of columns in the matrix
+   * @param initScheme the scheme to use
+   * @return a matrix of the specified dimensions with the specified
+   * distribution based on the initialization scheme
+   */
+  public static INDArray initWeights(int nIn, int nOut, WeightInit initScheme,
+                                     ActivationFunction act, RealDistribution dist) {
+    return initWeights(new int[]{nIn, nOut}, initScheme, dist);
+  }
 
 
 }
