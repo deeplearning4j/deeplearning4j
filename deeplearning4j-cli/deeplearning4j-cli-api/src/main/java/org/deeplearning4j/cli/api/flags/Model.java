ba96be1e93e878465fc7081879ba060c6930d86d
sonaliii <sona8242@gmail.com>
2015-03-02 15:05:05 -0800

    Adds model flag for CLI and associated tests, including integration tests with train subcommand

--- deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/api/flags/Model.java
+++ deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/api/flags/Model.java
@@ -1,6 +1,8 @@
 package org.deeplearning4j.cli.api.flags;
 
 import org.deeplearning4j.cli.subcommands.SubCommand;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 
 /**
  * Model flag for setting model configurations
@@ -10,11 +12,26 @@ import org.deeplearning4j.cli.subcommands.SubCommand;
 public class Model implements Flag {
     /**
      * JSON model configuration passed in
+     * If you are entering a MultiLayerConfiguration JSON,
+     * your file name MUST end with '_multi.json'.
+     * Otherwise, it will be processed as a regular
+     * NeuralNetConfiguration
      *
      */
     @Override
     public <E> E value(String value) throws Exception {
-        return null;
+        //take in JSON
+        //figure out if it's multilayer or normal
+        //return a configuration
+
+        Boolean isMultiLayer = value.contains("_multi");
+
+        if (isMultiLayer) {
+            return (E) MultiLayerConfiguration.fromJson(value);
+        }
+        else {
+            return (E) NeuralNetConfiguration.fromJson(value);
+        }
     }
 
 }
