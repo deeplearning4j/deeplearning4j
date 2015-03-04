09fba48027c450c72393f77a5575eea54bde2fe2
sonaliii <sona8242@gmail.com>
2015-03-03 17:54:25 -0800

    Fixes bug in model flag and associated model configuration tests
    Former-commit-id: 88da99beb99cbe8b8567dcbde20e7787f57d4440

--- deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/api/flags/Model.java
+++ deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/api/flags/Model.java
@@ -1,8 +1,11 @@
 package org.deeplearning4j.cli.api.flags;
 
+import org.apache.commons.io.FileUtils;
 import org.deeplearning4j.cli.subcommands.SubCommand;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+
+import java.io.File;
 
 /**
  * Model flag for setting model configurations
@@ -13,24 +16,26 @@ public class Model implements Flag {
     /**
      * JSON model configuration passed in
      * If you are entering a MultiLayerConfiguration JSON,
-     * your file name MUST end with '_multi.json'.
+     * your file name MUST contain '_multi'.
      * Otherwise, it will be processed as a regular
      * NeuralNetConfiguration
+     *
+     * Takes in JSON file path
+     * Checks file path for indication of MultiLayer
+     * Reads JSON file to string
+     * Creates neural net configuration from string config
      *
      */
     @Override
     public <E> E value(String value) throws Exception {
-        //take in JSON
-        //figure out if it's multilayer or normal
-        //return a configuration
-
         Boolean isMultiLayer = value.contains("_multi");
+        String json = FileUtils.readFileToString(new File(value));
 
         if (isMultiLayer) {
-            return (E) MultiLayerConfiguration.fromJson(value);
+            return (E) MultiLayerConfiguration.fromJson(json);
         }
         else {
-            return (E) NeuralNetConfiguration.fromJson(value);
+            return (E) NeuralNetConfiguration.fromJson(json);
         }
     }
 
