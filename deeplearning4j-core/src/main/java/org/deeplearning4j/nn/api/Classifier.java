9c18a5e30430a12e4b38f3178a2780ed4dcb0dd4
Adam Gibson <adam@skymind.io>
2015-03-25 02:51:31 -0400

    change to nd4j datasetiterator

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/api/Classifier.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/api/Classifier.java
@@ -16,9 +16,9 @@
 
 package org.deeplearning4j.nn.api;
 
-import org.deeplearning4j.datasets.iterator.DataSetIterator;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 
 /**
