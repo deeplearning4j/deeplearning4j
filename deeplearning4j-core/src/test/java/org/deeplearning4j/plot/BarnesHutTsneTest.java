e6e7e3622298d7b580d4c691e250d008645fbdd0
Adam Gibson <adam@skymind.io>
2015-04-06 14:00:40 -0400

    add input pre processors

--- deeplearning4j-core/src/test/java/org/deeplearning4j/plot/BarnesHutTsneTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/plot/BarnesHutTsneTest.java
@@ -16,6 +16,7 @@
 
 package org.deeplearning4j.plot;
 
+import org.apache.commons.io.EndianUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -23,7 +24,10 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -36,13 +40,34 @@ public class BarnesHutTsneTest {
         Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
         Nd4j.MAX_ELEMENTS_PER_SLICE = Integer.MAX_VALUE;
         Nd4j.MAX_SLICES_TO_PRINT = Integer.MAX_VALUE;
-
+        Nd4j.getRandom().setSeed(123);
         BarnesHutTsne b = new BarnesHutTsne.Builder().stopLyingIteration(250)
-                .theta(0.5).learningRate(200).useAdaGrad(false)
+                .theta(0.5).learningRate(200).useAdaGrad(true)
                 .build();
+
+        BufferedOutputStream fos = new BufferedOutputStream(new FileOutputStream(new File("/home/agibsonccc/code/barneshut/data.dat"),false));
+        DataOutputStream dos = new DataOutputStream(fos);
         ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
         File f = resource.getFile();
         INDArray data = Nd4j.readTxt(f.getAbsolutePath(),"   ");
+
+
+        EndianUtils.writeSwappedInteger(dos,data.rows());
+        EndianUtils.writeSwappedInteger(dos, data.columns());
+        EndianUtils.writeSwappedDouble(dos, 0.5);
+        EndianUtils.writeSwappedDouble(dos, 30);
+        EndianUtils.writeSwappedInteger(dos, 2);
+        for(int i = 0; i < data.rows(); i++) {
+            for(int j = 0; j < data.columns(); j++)
+                EndianUtils.writeSwappedDouble(dos,data.getDouble(i,j));
+        }
+
+        EndianUtils.writeSwappedInteger(dos,123);
+
+        dos.flush();
+        fos.flush();
+        dos.close();
+
         ClassPathResource labels = new ClassPathResource("mnist2500_labels.txt");
         List<String> labelsList = IOUtils.readLines(labels.getInputStream());
         b.fit(data);
