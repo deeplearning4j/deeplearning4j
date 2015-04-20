4067be24b5b634eeb29c4958feff8de15af62e86
Adam Gibson <adam@skymind.io>
2015-04-21 08:06:48 +0900

    updated word2vec and glove on spark

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/embeddings/glove/GloveChange.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/embeddings/glove/GloveChange.java
@@ -12,51 +12,85 @@ import java.util.Map;
  * @author Adam Gibson
  */
 public class GloveChange implements Serializable {
-    private Map<Integer,INDArray> changed = new HashMap<>();
-    private Map<Integer,INDArray> weightAdaGrad = new HashMap<>();
-    private Map<Integer,Double> biasChange = new HashMap<>();
-    private Map<Integer,Double> biasAdaGradChange = new HashMap<>();
     private VocabWord w1,w2;
+    private INDArray w1Update,w2Update;
+    private double w1BiasUpdate,w2BiasUpdate;
+    private double error;
 
-
-
-    public GloveChange(VocabWord w1, VocabWord w2,GloveWeightLookupTable table) {
+    public GloveChange(VocabWord w1, VocabWord w2, INDArray w1Update, INDArray w2Update, double w1BiasUpdate, double w2BiasUpdate,double error) {
         this.w1 = w1;
         this.w2 = w2;
-        changed.put(w1.getIndex(),table.getSyn0().slice(w1.getIndex()));
-        changed.put(w2.getIndex(),table.getSyn0().slice(w2.getIndex()));
-        biasChange.put(w1.getIndex(),table.getBias().getDouble(w1.getIndex()));
-        biasChange.put(w2.getIndex(),table.getBias().getDouble(w2.getIndex()));
-        biasAdaGradChange.put(w1.getIndex(),table.getBiasAdaGrad().getHistoricalGradient().getDouble(w1.getIndex()));
-        biasAdaGradChange.put(w2.getIndex(),table.getBiasAdaGrad().getHistoricalGradient().getDouble(w2.getIndex()));
-        weightAdaGrad.put(w1.getIndex(),table.getWeightAdaGrad().getHistoricalGradient().slice(w1.getIndex()));
-        weightAdaGrad.put(w2.getIndex(),table.getWeightAdaGrad().getHistoricalGradient().slice(w2.getIndex()));
-
+        this.w1Update = w1Update;
+        this.w2Update = w2Update;
+        this.w1BiasUpdate = w1BiasUpdate;
+        this.w2BiasUpdate = w2BiasUpdate;
+        this.error = error;
     }
 
-
-    private double getDelta(double sub,double delta) {
-        return sub - (sub + delta);
-    }
-
-    private void addDelta(INDArray add,INDArray update) {
-        add.addi(update.sub(add));
-    }
     /**
      * Apply the changes to the table
      * @param table
      */
     public void apply(GloveWeightLookupTable table) {
-        //only apply the delta (not the original weights)
-        for(VocabWord word : new VocabWord[]{w1,w2}) {
-            table.getBias().putScalar(word.getIndex(),getDelta(table.getBias().getDouble(word.getIndex()),biasChange.get(word.getIndex())));
-            table.getBiasAdaGrad().getHistoricalGradient().putScalar(word.getIndex(),getDelta(table.getBiasAdaGrad().getHistoricalGradient().getDouble(word.getIndex()), biasAdaGradChange.get(word.getIndex())));
-            addDelta(table.getWeightAdaGrad().getHistoricalGradient().slice(word.getIndex()), changed.get(word.getIndex()));
-            addDelta( table.getSyn0().slice(word.getIndex()),changed.get(word.getIndex()));
-        }
+        table.getBias().putScalar(w1.getIndex(), table.getBias().getDouble(w1.getIndex()) - w1BiasUpdate);
+        table.getBias().putScalar(w2.getIndex(),table.getBias().getDouble(w2.getIndex()) - w2BiasUpdate);
+        table.getSyn0().slice(w1.getIndex()).subi(w1Update);
+        table.getSyn0().slice(w2.getIndex()).sub(w2Update);
     }
 
+    public VocabWord getW1() {
+        return w1;
+    }
 
+    public void setW1(VocabWord w1) {
+        this.w1 = w1;
+    }
 
+    public VocabWord getW2() {
+        return w2;
+    }
 
+    public void setW2(VocabWord w2) {
+        this.w2 = w2;
+    }
+
+    public INDArray getW1Update() {
+        return w1Update;
+    }
+
+    public void setW1Update(INDArray w1Update) {
+        this.w1Update = w1Update;
+    }
+
+    public INDArray getW2Update() {
+        return w2Update;
+    }
+
+    public void setW2Update(INDArray w2Update) {
+        this.w2Update = w2Update;
+    }
+
+    public double getW1BiasUpdate() {
+        return w1BiasUpdate;
+    }
+
+    public void setW1BiasUpdate(double w1BiasUpdate) {
+        this.w1BiasUpdate = w1BiasUpdate;
+    }
+
+    public double getW2BiasUpdate() {
+        return w2BiasUpdate;
+    }
+
+    public void setW2BiasUpdate(double w2BiasUpdate) {
+        this.w2BiasUpdate = w2BiasUpdate;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public void setError(double error) {
+        this.error = error;
+    }
 }
