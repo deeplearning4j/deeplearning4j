307c90b8bf4529032dd24eb779ee55ef28e7c316
Bam4d <chris.bam4d@gmail.com>
2015-04-21 18:18:47 +0100

    fix the test and add an IllegalArgumentException if max < min

--- nd4j-jcublas-parent/nd4j-jcublas-7.0/src/test/java/jcublas/rng/distribution/RngKernelTest.java
+++ nd4j-jcublas-parent/nd4j-jcublas-7.0/src/test/java/jcublas/rng/distribution/RngKernelTest.java
@@ -23,7 +23,7 @@ public class RngKernelTest {
 	public void testUniformSample() {
 		Nd4j.dtype = DataBuffer.FLOAT;
 		for(int x = 0; x<100; x++) {
-			INDArray arr = Nd4j.rand(200,200,max,min,Nd4j.getRandom());
+			INDArray arr = Nd4j.rand(200,200,min,max,Nd4j.getRandom());
 			
 			// Assert the values are within range
 			assertMaxMin(arr);
