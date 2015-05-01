7f229f30cd0abf93c80940fb52216f3c90cfbfe2
Eron Wright <ewright@live.com>
2015-05-01 08:40:56 -0700

    fix use of ConfOverride to specify the layer index

--- deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/scaleout/perform/MultiLayerWorkPerformerTests.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-akka/src/test/java/org/deeplearning4j/scaleout/perform/MultiLayerWorkPerformerTests.java
@@ -44,7 +44,7 @@ public class MultiLayerWorkPerformerTests extends NeuralNetWorkPerformerTest {
                 .momentum(9e-1f).weightInit(WeightInit.DISTRIBUTION).dist(Nd4j.getDistributions().createNormal(1e-1,1))
                 .lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY).iterations(10)
                 .learningRate(1e-1f).nIn(4).nOut(3).layerFactory(layerFactory)
-                .list(2).hiddenLayerSizes(new int[]{3}).override(new ConfOverride() {
+                .list(2).hiddenLayerSizes(new int[]{3}).override(1, new ConfOverride() {
                     @Override
                     public void overrideLayer(int i, NeuralNetConfiguration.Builder builder) {
 
