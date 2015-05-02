f23baf07380da4261aa433235d0f557d73a8a9b0
Eron Wright <ewright@live.com>
2015-05-02 07:53:16 -0700

    updated comments
    Former-commit-id: 81089834e089f130e83a2f81a660fd4c52446176 [formerly 9f01f5e108960054ce2d4fe12e4c2f1ad4adc413]
    Former-commit-id: 1aa00c6fe5127d26b912cfdf46d67fdb1666b42e

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/distribution/UniformDistribution.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/distribution/UniformDistribution.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright 2015 Skymind,Inc.
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
+
 package org.deeplearning4j.nn.conf.distribution;
 
 import org.apache.commons.math3.exception.NumberIsTooLargeException;
@@ -6,6 +24,10 @@ import org.apache.commons.math3.exception.util.LocalizedFormats;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * A uniform distribution.
+ *
+ */
 public class UniformDistribution extends Distribution {
 
     private static final long serialVersionUID = 7006579116682205405L;
