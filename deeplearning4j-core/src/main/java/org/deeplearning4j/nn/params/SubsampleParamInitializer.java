c397964d60bb9da3538f2f62019eb29a6fc3a0bb
Adam Gibson <adam@skymind.io>
2015-05-03 18:23:18 -0700

    WIP: improved multilayer support
    Former-commit-id: 5d9ceab71cf50f22680a234841b2bc1cd2772809

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/params/SubsampleParamInitializer.java
@@ -0,0 +1,23 @@
+package org.deeplearning4j.nn.params;
+
+import org.canova.api.conf.Configuration;
+import org.deeplearning4j.nn.api.ParamInitializer;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import java.util.Map;
+
+/**
+ * @author Adam Gibson
+ */
+public class SubsampleParamInitializer implements ParamInitializer {
+    @Override
+    public void init(Map<String, INDArray> params, NeuralNetConfiguration conf) {
+
+    }
+
+    @Override
+    public void init(Map<String, INDArray> params, NeuralNetConfiguration conf, Configuration extraConf) {
+
+    }
+}
