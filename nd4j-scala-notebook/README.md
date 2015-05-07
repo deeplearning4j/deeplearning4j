827093cc863a37ef675719a36091d2358dafa7fe
Adam Gibson <adam@skymind.io>
2015-05-07 14:28:05 -0700

    add sources for scala

--- nd4j-scala-notebook/README.md
+++
@@ -1,7 +0,0 @@
-IScala Notebook
-======================
-
-Derivative work of the ISpark notebook.
-
-https://github.com/tribbloid/ISpark
-
--- nd4j-scala-notebook/macros/src/main/scala/org/tribbloid/ispark/macros/Utils.scala
+++
@@ -1,8 +0,0 @@
-package org.tribbloid.ispark.macros
-
-object Utils {
-    def snakify(name: String): String =
-        name.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2")
-            .replaceAll("([a-z\\d])([A-Z])", "$1_$2")
-            .toLowerCase
-}
