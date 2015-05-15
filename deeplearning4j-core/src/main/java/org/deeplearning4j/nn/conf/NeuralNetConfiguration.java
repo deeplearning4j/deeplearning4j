bffae0e4675a1536bea6ea1f9a7dda4b79415b1e
nyghtowl <warrick.melanie@gmail.com>
2015-05-14 18:48:55 -0700

    Pulled error message because its a conflict for convolutional nets structure.
    Former-commit-id: 0b7713fb0933c389a42d5590fbc196deebd4d5e0 [formerly 2be4181f0dd4c7501ead0115541fe89c7fae6bf8]
    Former-commit-id: 219c2d288f3e909f08e00ffa440cfa525f57df02

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/NeuralNetConfiguration.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/NeuralNetConfiguration.java
@@ -788,8 +788,6 @@ public class NeuralNetConfiguration implements Serializable,Cloneable {
         }
 
         public MultiLayerConfiguration build() {
-            if(layerwise.size() != hiddenLayerSizes.length + 1)
-                throw new IllegalStateException("Number of hidden layers mut be equal to hidden layer sizes + 1");
 
             List<NeuralNetConfiguration> list = new ArrayList<>();
             for(int i = 0; i < layerwise.size(); i++) {
