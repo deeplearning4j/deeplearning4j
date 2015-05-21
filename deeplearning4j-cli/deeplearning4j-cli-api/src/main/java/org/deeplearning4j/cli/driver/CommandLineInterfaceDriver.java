7f252cf6261d886499e7a24370516409ce51a7d1
Adam Gibson <adam@skymind.io>
2015-05-20 22:29:45 -0700

    update train command
    Former-commit-id: 8425860b5e0d01bcc63e3b13cbec72c5426b4927 [formerly dde7464caae768ca51013a1349a6fa31cde711fb]
    Former-commit-id: ef4a95dbeb4551dc24434ba32dc3aebb70ecba98

--- deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/driver/CommandLineInterfaceDriver.java
+++ deeplearning4j-cli/deeplearning4j-cli-api/src/main/java/org/deeplearning4j/cli/driver/CommandLineInterfaceDriver.java
@@ -24,9 +24,12 @@ public class CommandLineInterfaceDriver {
 
 	@Argument(required=true,index=0,metaVar="action",usage="subcommands, e.g., {train|test|predict}",handler=SubCommandHandler.class)
 	@SubCommands({
-			@SubCommand(name="vectorize",impl=Train.class)
+			@SubCommand(name="train",impl=Train.class)
 	})
 	private org.deeplearning4j.cli.subcommands.SubCommand subCommand;
+
+
+
     /**
      * Print the usage for the command.
      */
