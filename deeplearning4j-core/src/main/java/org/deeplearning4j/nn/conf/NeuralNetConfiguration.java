deeplearning4j/deeplearning4j

commit 42133b02fcafa5b6eeafb7f37f56643f950b6e83

rng is transient
Former-commit-id: d27f640264a650a4b59c50a53de64c0e50fb0eee

deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/NeuralNetConfiguration.java
@@ -79,7 +79,7 @@ public class NeuralNetConfiguration implements Serializable,Cloneable {
     //whether to constrain the gradient to unit norm or not
     protected boolean constrainGradientToUnitNorm = false;
     /* RNG for sampling. */
-    protected Random rng;
+    protected transient Random rng;
     //weight initialization
     protected Distribution dist;
     protected StepFunction stepFunction = new GradientStepFunction();

commit a2a0fa81b3626278ceb41ee40b2ec8549b2232a1

undo regression from 42133b02fcafa5b6eeafb7f37f56643f950b6e83 [formerly d27f640264a650a4b59c50a53de64c0e50fb0eee]
Former-commit-id: 989b7bb1abe6e6d58aee20e45c53fdeebf8a21ec

deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/NeuralNetConfiguration.java
@@ -80,7 +80,7 @@ public class NeuralNetConfiguration implements Serializable,Cloneable {
     //whether to constrain the gradient to unit norm or not
     protected boolean constrainGradientToUnitNorm = false;
     /* RNG for sampling. */
-    protected transient Random rng;
+    protected Random rng;
     //weight initialization
     protected Distribution dist;
     protected StepFunction stepFunction = new GradientStepFunction();