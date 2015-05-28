88ff2ae0254675a08361695f0b6f196293d01dc5
Adam Gibson <adam@skymind.io>
2015-05-28 11:51:11 -0700

    change array listin test to be thread safe

--- deeplearning4j-core/src/test/java/org/deeplearning4j/berkeley/CounterMapTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/berkeley/CounterMapTest.java
@@ -25,6 +25,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -38,7 +39,7 @@ public class CounterMapTest {
 
     @Test
     public void testParallel() {
-        List<Integer> items = new CopyOnWriteArrayList<>();
+        List<Integer> items =  Collections.synchronizedList(new ArrayList<Integer>());
         for(int i  = 0; i < 1e6; i++)
             items.add(i);
 
