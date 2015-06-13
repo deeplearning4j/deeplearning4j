142e8a6d4d54f45aede1811fb761ac7fc5a7f510
nyghtowl <warrick.melanie@gmail.com>
2015-06-12 17:41:19 -0700

    Verfied that direct call to plotNetworkGraph does work. Fixed parameter needed for Listner in NeuralNetPlotter. Streamlined and confirmed how graphs are shown and saved. PIL is the only one not saving completely.
    Former-commit-id: b66972cd8492d2e1ed1f61bd8ce0868c3d1edf14 [formerly 88c8e7c2ec17dd2759f7bcbc56f2f7695ee4b8ba]
    Former-commit-id: f4c87ea7e67a3251422407b0e075529e5a2f19e7

--- deeplearning4j-core/src/main/resources/scripts/plot.py
+++ deeplearning4j-core/src/main/resources/scripts/plot.py
@@ -3,8 +3,9 @@ from matplotlib.pyplot import hist, title, subplot, scatter
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-import seaborn
+import seaborn # improves matplotlib look and feel
 import sys
+import time
 
 '''
 Optimization Methods Visualalization
@@ -38,13 +39,17 @@ def render_plot(values, plot_type='histogram', chart_title=''):
     title(chart_title)
 
 
-def render_activation_probability(dataPath):
+def render_activation_probability(dataPath, filename):
     hidden_mean = load_file(dataPath)
     # Should sigmoid be moved earlier?
-    Image.fromarray(sigmoid(hidden_mean) * 256).show()
+    img = Image.fromarray(sigmoid(hidden_mean) * 256)
+    img.save(filename, 'PNG')
+    time.sleep(15)
+    img.show()
+    time.sleep(15)
+    img.close()
 
-
-def plot_matrices(orig_path, plot_type=''):
+def plot_matrices(orig_path, plot_type, filename):
     paths = orig_path.split(',')
 
     for idx, path in enumerate(paths):
@@ -56,12 +61,14 @@ def plot_matrices(orig_path, plot_type=''):
             render_plot(matrix, plot_type, chart_title=title)
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig(filename, format='png')
+    plt.show(block=False)
+    time.sleep(15)
+    plt.close()
+
 
 # TODO Finish adapting. Code still does not fully run through.
-# FilterRender produces a result for now
-# Goal is to consolidate these into a common script
-def render_filter(data_path, n_rows, n_cols):
+def render_filter(data_path, n_rows, n_cols, filename):
     weight_data = load_file(data_path).reshape((n_rows, n_cols))
     patch_width = weight_data.shape[1]
     patch_height = 1
@@ -75,27 +82,30 @@ def render_filter(data_path, n_rows, n_cols):
             norm_patch = ((patch - patch.min()) / (patch.max() - patch.min() + 1e-6))
             filter_frame[row * patch_width: row * patch_width + patch_width,
                   col * patch_height:col * patch_height + patch_height] = norm_patch * 255
-    Image.fromarray(filter_frame).show()
+    img = Image.fromarray(filter_frame)
+    img.savefig(filename)
+    img.show()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
        print 'Please specify a command: One of hbias,weights,plot and a file path'
        sys.exit(1)
     plot_type = sys.argv[1]
     path = sys.argv[2]
+    filename = sys.argv[3]
 
     if plot_type == 'activations':
-        render_activation_probability(path)
+        render_activation_probability(path, filename)
     elif plot_type == 'single_matrix':
         render_plot(path)
     elif plot_type == 'histogram':
-        plot_matrices(path, plot_type)
+        plot_matrices(path, plot_type, filename)
     elif plot_type == 'scatter':
-        plot_matrices(path, plot_type)
+        plot_matrices(path, plot_type, filename)
     elif sys.argv[1] == 'filter':
-        n_rows = int(sys.argv[3])
-        n_cols = int(sys.argv[4])
-        length = int(sys.argv[5])
+        n_rows = int(sys.argv[4])
+        n_cols = int(sys.argv[5])
+        length = int(sys.argv[6])
         print 'Rendering ' + sys.argv[3] + ' x ' + sys.argv[4] + ' matrix'
-        render_filter(path, n_rows, n_cols, length)
+        render_filter(path, n_rows, n_cols, length, filename)
