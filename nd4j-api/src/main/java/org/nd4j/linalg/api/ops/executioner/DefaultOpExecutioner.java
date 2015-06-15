34cd755ff9dca6e77a2c62426755d7c6be5cc215
Adam Gibson <adam@skymind.io>
2015-06-14 18:39:17 -0700

    fix exec and return on transform op along a dimension

--- nd4j-api/src/main/java/org/nd4j/linalg/api/ops/executioner/DefaultOpExecutioner.java
+++ nd4j-api/src/main/java/org/nd4j/linalg/api/ops/executioner/DefaultOpExecutioner.java
@@ -349,7 +349,7 @@ public class DefaultOpExecutioner implements OpExecutioner {
         for (int i = 0; i < op.x().vectorsAlongDimension(dimension); i++) {
             Op op2 = op.opForDimension(i, dimension);
             exec(op2);
-            op.z().vectorAlongDimension(i, dimension).assign(op.z());
+            op.z().vectorAlongDimension(i, dimension).assign(op2.z());
         }
         return op.z();
     }
