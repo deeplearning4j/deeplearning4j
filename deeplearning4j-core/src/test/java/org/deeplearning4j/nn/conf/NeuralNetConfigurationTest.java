e333834cc9072e245352ae093f7929b5a65d85fd
nyghtowl <warrick.melanie@gmail.com>
2015-06-15 11:49:23 -0700

    Added tests for rng and setSeed. Rng currently failing because of logged issue. Also JsonMappipngException causing json test to fail.
    Former-commit-id: 9911da68aaef74b166c760332b13b9ba57722a15 [formerly 45329601c1aabdbaff8a5cc94ea7c36049a24c69]
    Former-commit-id: 90f0bcb7b9cc68f5b15b483735c2747b79fd72a5

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/NeuralNetConfigurationTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/NeuralNetConfigurationTest.java
@@ -20,24 +20,65 @@ package org.deeplearning4j.nn.conf;
 
 import static org.junit.Assert.*;
 
+import org.deeplearning4j.nn.conf.rng.DefaultRandom;
+import org.nd4j.linalg.factory.Nd4j;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
 import org.deeplearning4j.nn.conf.layers.RBM;
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.nn.layers.factory.LayerFactories;
+import org.deeplearning4j.nn.params.DefaultParamInitializer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.junit.Test;
-import org.nd4j.linalg.factory.Nd4j;
 
 /**
  * Created by agibsonccc on 11/27/14.
  */
 public class NeuralNetConfigurationTest {
+
+    final DataSet trainingSet = createData();
+
+    public DataSet createData() {
+        int numFeatures = 40;
+
+        INDArray input = Nd4j.create(2, numFeatures); // have to be at least two or else output layer gradient is a scalar and cause exception
+        INDArray labels = Nd4j.create(2, 2);
+
+        INDArray row0 = Nd4j.create(1, numFeatures);
+        row0.assign(0.1);
+        input.putRow(0, row0);
+        labels.put(0, 1, 1); // set the 4th column
+
+        INDArray row1 = Nd4j.create(1, numFeatures);
+        row1.assign(0.2);
+
+        input.putRow(1, row1);
+        labels.put(1, 0, 1); // set the 2nd column
+
+        return new DataSet(input, labels);
+    }
+
+
     @Test
     public void testJson() {
         NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
-                .dist(new NormalDistribution(1,1))
-                .layer(new RBM()).useAdaGrad(false)
+                .layer(new RBM())
+                .nIn(trainingSet.numInputs())
+                .nOut(trainingSet.numOutcomes())
+                .weightInit(WeightInit.SIZE)
+                .iterations(3)
+                .useAdaGrad(false)
+                .regularization(false)
+                .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
+                .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
                 .build();
 
-        assertFalse(conf.isUseAdaGrad());
+        assertFalse(conf.useRegularization);
         String json = conf.toJson();
         NeuralNetConfiguration read = NeuralNetConfiguration.fromJson(json);
 
@@ -47,12 +88,76 @@ public class NeuralNetConfigurationTest {
     @Test
     public void testCopyConstructor() {
         NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
-                .dist(new NormalDistribution(1,1))
+                .dist(new NormalDistribution(1, 1))
                 .layer(new RBM()).useAdaGrad(false)
                 .build();
 
         NeuralNetConfiguration conf2 = new NeuralNetConfiguration(conf);
         assertEquals(conf,conf2);
+    }
+
+    @Test
+    public void testRNG() {
+//        Nd4j.getRandom().setSeed (123);
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .layer(new RBM())
+                .nIn(trainingSet.numInputs())
+                .nOut(trainingSet.numOutcomes())
+                .weightInit(WeightInit.SIZE)
+                .constrainGradientToUnitNorm(true)
+                .rng(new DefaultRandom(123))
+                .iterations(3)
+                .activationFunction("tanh")
+                .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
+                .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .build();
+        Layer model = LayerFactories.getFactory(conf).create(conf);
+
+        INDArray modelWeights = model.getParam(DefaultParamInitializer.WEIGHT_KEY);
+
+        float[][] weights = new float[][]{
+                {1.5115644931793213f, -1.294399261474609f}
+                ,{0.28141850233078003f, 0.6243384480476379f}
+                ,{0.9196174144744873f, -0.7969362735748291f}
+        };
+
+        INDArray exceptedWeights = Nd4j.create(weights);
+        assertEquals(modelWeights, exceptedWeights);
+    }
+
+    @Test
+    public void testSetSeed() {
+        Nd4j.getRandom().setSeed (123);
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .layer(new RBM())
+                .nIn(trainingSet.numInputs())
+                .nOut(trainingSet.numOutcomes())
+                .weightInit(WeightInit.SIZE)
+                .constrainGradientToUnitNorm(true)
+                .rng(new DefaultRandom(123))
+                .iterations(3)
+                .activationFunction("tanh")
+                .visibleUnit(RBM.VisibleUnit.GAUSSIAN)
+                .hiddenUnit(RBM.HiddenUnit.RECTIFIED)
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .build();
+        Layer model = LayerFactories.getFactory(conf).create(conf);
+
+        INDArray modelWeights = model.getParam(DefaultParamInitializer.WEIGHT_KEY);
+
+        float[][] weights = new float[][]{
+                {0.5940669775009155f, 0.3747984766960144f}
+                ,{-0.6466538906097412f, -1.1622607707977295f}
+                ,{-0.8259235620498657f, -0.5524767637252808f}
+        };
+
+        INDArray exceptedWeights = Nd4j.create(weights);
+        assertEquals(modelWeights, exceptedWeights);
+
     }
 
 
