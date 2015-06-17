978200aca0551e3d243dd1fff8c0c2abf877f715
Eron Wright <ewright@live.com>
2015-06-16 21:56:23 -0700

    more assertions for unit test

--- deeplearning4j-scaleout/spark/dl4j-spark-ml/src/test/scala/org/deeplearning4j/spark/ml/reconstruction/NeuralNetworkReconstructionTest.scala
+++ deeplearning4j-scaleout/spark/dl4j-spark-ml/src/test/scala/org/deeplearning4j/spark/ml/reconstruction/NeuralNetworkReconstructionTest.scala
@@ -9,10 +9,9 @@ import org.deeplearning4j.spark.sql.sources.iris.DefaultSource
 import org.deeplearning4j.spark.util.TestSparkContext
 import org.junit.runner.RunWith
 import org.nd4j.linalg.lossfunctions.LossFunctions
-import org.scalatest.FunSuite
+import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 import org.springframework.core.io.ClassPathResource
-import org.scalatest._
 
 /**
  * Test reconstruction.
