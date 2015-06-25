7b56035a4fe00c1bfaf1d86c5d047c3f218ab701
nyghtowl <warrick.melanie@gmail.com>
2015-06-25 16:12:44 -0700

    Fixed the gradient calcs. Activations needed the first input to be included because its part of the equation however derivatives needed to have a placeholder so the loop through it would match up to activations. Investigation confirmed original GradientAdjustment was doing a direct update to gradients and shifted that back. Adjusted names for more clarity. Still investigating test discrepency and trying out only applying divisuion of batchsize to gradient weights in the adjustment section.
    Former-commit-id: 2c963c56eb9cfd4c4b5637f3033e0a445f905275 [formerly a726c4585f48a7de77e3e0c3d606075027d8fb76]
    Former-commit-id: 074ce344212ad3eae88ee24b911318852c9e1115

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
@@ -175,7 +175,7 @@ public abstract class BaseLayer implements Layer {
     @Override
     public void update(INDArray gradient, String paramType) {
         if (paramType.contains("b"))
-            setParam(paramType, getParam(paramType).subi(gradient.mean(0)));
+            setParam(paramType, getParam(paramType).subi(gradient).mean(0));
         else
             setParam(paramType, getParam(paramType).subi(gradient));
     }
