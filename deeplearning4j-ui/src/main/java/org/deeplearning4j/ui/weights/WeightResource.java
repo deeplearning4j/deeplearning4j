9a33e5fa7a766e06843b6a0924572a62810b2d1c
Adam Gibson <adam@skymind.io>
2015-06-30 11:02:27 -0700

    add new rendering components
    Former-commit-id: aa176e3ddae14c3e873c0d9c7acbe6fbf1d33079

--- deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/weights/WeightResource.java
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/weights/WeightResource.java
@@ -19,23 +19,65 @@
 package org.deeplearning4j.ui.weights;
 
 import io.dropwizard.views.View;
+import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.ui.tsne.TsneView;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Created by agibsonccc on 10/8/14.
+ * Weight renderings
+ *
+ * @author Adam Gibson
  */
 @Path("/weights")
 @Produces(MediaType.TEXT_HTML)
 public class WeightResource {
-
+    private ModelAndGradient current;
+    private boolean updated = false;
     @GET
     public View get() {
-        return new TsneView();
+        return new WeightView();
     }
+
+    @GET
+    @Path("/updated")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updated() {
+        return Response.ok(Collections.singletonMap("message",updated)).build();
+    }
+
+    @GET
+    @Path("/data")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response data() {
+        if(current == null) {
+            current = new ModelAndGradient();
+            updated = true;
+        }
+        updated = false;
+        return Response.ok(current).build();
+    }
+
+
+    @POST
+    @Path("/update")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(ModelAndGradient modelAndGrad) {
+        this.current = modelAndGrad;
+        updated = true;
+        return Response.ok(Collections.singletonMap("status","ok")).build();
+    }
+
+
+
 
 }
