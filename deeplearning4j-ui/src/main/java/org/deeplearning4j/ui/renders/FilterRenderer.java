6cb588a776361f4db18dcf2dbd6d2c6cf500b48d
Adam Gibson <adam@skymind.io>
2015-07-02 10:35:05 -0700

    add new filter render code

---
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/renders/FilterRenderer.java
@@ -0,0 +1,76 @@
+package org.deeplearning4j.ui.renders;
+
+import com.google.common.primitives.Ints;
+import org.apache.commons.math.util.FastMath;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Based on the work by krizshevy et. al
+ *
+ * http://nbviewer.ipython.org/github/udibr/caffe/blob/dev/examples/filter_visualization.ipynb
+ *
+ * @author Adam Gibson
+ */
+public class FilterRenderer {
+    /**
+     * Render the given data
+     * as filters
+     * @param data the data to render
+     * @param padSize the padding size for the images
+     * @return the image to render
+     */
+    public INDArray render(INDArray data,int padSize) {
+        data.subi(data.mean(Integer.MAX_VALUE));
+        data.divi(data.max(Integer.MAX_VALUE));
+        // n = int(np.ceil(np.sqrt(data.shape[0])))
+        int n = (int) Math.ceil(FastMath.sqrt(data.size(0)));
+        int[][] padding = new int[4][];
+        double end = Math.pow( n,2) - data.size(0);
+        padding[0] = new int[]{0,(int) end};
+        padding[1] = new int[]{0,padSize};
+        padding[2] = new int[]{0,padSize};
+        List<double[]> list = new ArrayList<>();
+
+        data = Nd4j.pad(data,padding, Nd4j.PadMode.CONSTANT);
+        //# tile the filters into an image
+        int[] baseFilterShape = Ints.concat(new int[]{n,n}, Arrays.copyOfRange(data.shape(),1,data.shape().length));
+        data = data.reshape(baseFilterShape).permute(0,2,1,3);
+        data = data.reshape(n * data.size(1),n * data.size(3));
+        return data;
+    }
+
+
+    /**
+     * Render the given data
+     * as filters
+     * @param data the data to render
+     * @param padSize the padding size for the images
+     * @param padVal the value to pad with
+     * @return the image to render
+     */
+    public INDArray render(INDArray data,int padSize,double padVal) {
+        data.subi(data.mean(Integer.MAX_VALUE));
+        data.divi(data.max(Integer.MAX_VALUE));
+        // n = int(np.ceil(np.sqrt(data.shape[0])))
+        int n = (int) Math.ceil(FastMath.sqrt(data.size(0)));
+        int[][] padding = new int[4][];
+        double end = Math.pow( n,2) - data.size(0);
+        padding[0] = new int[]{0,(int) end};
+        padding[1] = new int[]{0,padSize};
+        padding[2] = new int[]{0,padSize};
+        List<double[]> list = new ArrayList<>();
+
+        data = Nd4j.pad(data,padding, Nd4j.PadMode.CONSTANT);
+        //# tile the filters into an image
+        int[] baseFilterShape = Ints.concat(new int[]{n,n}, Arrays.copyOfRange(data.shape(),1,data.shape().length));
+        data = data.reshape(baseFilterShape).permute(0,2,1,3);
+        data = data.reshape(n * data.size(1),n * data.size(3));
+        return data;
+    }
+
+}
