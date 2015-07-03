169831674fd75da51c37acac032b245cfbdd1f67
Adam Gibson <adam@skymind.io>
2015-07-02 23:18:53 -0700

    finish renders
    Former-commit-id: 516fd0606ea43e3e1676610681cd3424796dc1a9 [formerly c2c074060d5228e07f22a02aefd0722610926f7a]
    Former-commit-id: 76956d762886cace7bd8c92b508f8600fa535f56

--- deeplearning4j-core/src/test/java/org/deeplearning4j/plot/RenderTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/plot/RenderTest.java
@@ -1,10 +1,24 @@
 package org.deeplearning4j.plot;
 
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.layers.factory.LayerFactories;
+import org.deeplearning4j.nn.layers.feedforward.autoencoder.AutoEncoder;
+import org.deeplearning4j.nn.params.DefaultParamInitializer;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.plot.iterationlistener.PlotFiltersIterationListener;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Adam Gibson
@@ -16,6 +30,30 @@ public class RenderTest {
         PlotFilters render = new PlotFilters();
         INDArray rendered = render.render(test,1,1);
         assertArrayEquals(new int[]{7619, 95}, rendered.shape());
+    }
+
+
+    @Test
+    public void testPlotter() throws Exception {
+        PlotFiltersIterationListener listener = new PlotFiltersIterationListener(Arrays.asList(DefaultParamInitializer.WEIGHT_KEY));
+        MnistDataFetcher fetcher = new MnistDataFetcher(true);
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder().momentum(0.9f)
+                .optimizationAlgo(OptimizationAlgorithm.GRADIENT_DESCENT)
+                .corruptionLevel(0.6)
+                .iterations(1)
+                .lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY)
+                .learningRate(1e-1f).nIn(784).nOut(600)
+                .layer(new org.deeplearning4j.nn.conf.layers.AutoEncoder())
+                .build();
+
+
+        fetcher.fetch(100);
+        DataSet d2 = fetcher.next();
+
+        INDArray input = d2.getFeatureMatrix();
+        AutoEncoder da = LayerFactories.getFactory(conf.getLayer()).create(conf, Arrays.<IterationListener>asList(new ScoreIterationListener(1),listener));
+        da.fit(input);
+
     }
 
 }
