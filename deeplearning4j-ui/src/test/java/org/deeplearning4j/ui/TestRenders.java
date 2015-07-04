4a941cb17ab3117e7ed75a06e40723e0977bbde2
Adam Gibson <adam@skymind.io>
2015-07-04 13:47:53 -0700

    real time updates on graphs

--- deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/TestRenders.java
+++ deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/TestRenders.java
@@ -7,6 +7,7 @@ import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.layers.factory.LayerFactories;
 import org.deeplearning4j.nn.layers.feedforward.autoencoder.AutoEncoder;
 import org.deeplearning4j.nn.params.PretrainParamInitializer;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.renders.UpdateFilterIterationListener;
@@ -48,20 +49,22 @@ public class TestRenders extends BaseUiServerTest {
 
     @Test
     public void renderHistogram() throws Exception {
+        MnistDataFetcher fetcher = new MnistDataFetcher(true);
         NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder().momentum(0.9f)
                 .optimizationAlgo(OptimizationAlgorithm.ITERATION_GRADIENT_DESCENT)
                 .corruptionLevel(0.6)
-                .iterations(100).constrainGradientToUnitNorm(true).applySparsity(true).sparsity(0.5)
-                .lossFunction(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY)
-                .learningRate(1e-1f).nIn(4).nOut(3)
+                .iterations(100).weightInit(WeightInit.XAVIER)
+                .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                .learningRate(1e-1f).nIn(784).nOut(600)
                 .layer(new org.deeplearning4j.nn.conf.layers.AutoEncoder())
                 .build();
 
 
-        DataSet d2 = new IrisDataSetIterator(150,150).next();
+        fetcher.fetch(100);
+        DataSet d2 = fetcher.next();
 
         INDArray input = d2.getFeatureMatrix();
-        AutoEncoder da = LayerFactories.getFactory(conf.getLayer()).create(conf, Arrays.<IterationListener>asList(new ScoreIterationListener(1),new HistogramIterationListener(1)),0);
+        AutoEncoder da = LayerFactories.getFactory(conf.getLayer()).create(conf, Arrays.<IterationListener>asList(new ScoreIterationListener(1),new HistogramIterationListener(5)),0);
         da.setParams(da.params());
         da.fit(input);
     }
