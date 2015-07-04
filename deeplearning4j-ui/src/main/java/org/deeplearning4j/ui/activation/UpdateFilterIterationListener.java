3dfe1fc1582152ec86d1a6e9680e79a99ecca30a
Adam Gibson <adam@skymind.io>
2015-07-04 14:41:34 -0700

    refresh image
    Former-commit-id: 626bcbec05b297eb8d14eba03c0c1b0fc1e96dc0 [formerly acaa662cc330cf39e6c205263b5d6fab52cafcd6]
    Former-commit-id: c62ed7805d38289d34c2c1818519d537845c93de

---
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/activation/UpdateFilterIterationListener.java
@@ -0,0 +1,56 @@
+package org.deeplearning4j.ui.activation;
+
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.plot.iterationlistener.PlotFiltersIterationListener;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * @author Adam Gibson
+ */
+public class UpdateFilterIterationListener implements IterationListener {
+    private Client client = ClientBuilder.newClient();
+    private WebTarget target = client.target("http://localhost:8080").path("filters").path("update");
+    private PlotFiltersIterationListener listener;
+    private int iterations = 1;
+
+    /**
+     * Initializes with the variables to render filters for
+     * @param variables the variables ot render filters for
+     * @param iterations the number of iterations to update on
+     */
+    public UpdateFilterIterationListener(List<String> variables,int iterations) {
+        listener = new PlotFiltersIterationListener(variables);
+        this.iterations = iterations;
+    }
+
+    @Override
+    public boolean invoked() {
+        return false;
+    }
+
+    @Override
+    public void invoke() {
+
+    }
+
+    @Override
+    public void iterationDone(Model model, int iteration) {
+        if(iteration % iterations == 0) {
+            PathUpdate update = new PathUpdate();
+            //update the weights
+            listener.iterationDone(model, iteration);
+            //ensure path is set
+            update.setPath(listener.getOutputFile().getPath());
+            //ensure the server is hooked up with the path
+            target.request(MediaType.APPLICATION_JSON).post(Entity.entity(update, MediaType.APPLICATION_JSON));
+        }
+
+    }
+}
