6c3effc6637c0e58adfc453665655290ec1463c8
jyt109 <jeffrey.tang09@gmail.com>
2015-07-11 19:39:24 -0700

    done parsing caffemodel and test
    Former-commit-id: 05a727e24019cc754e2a9c0a5bcf8fb07aca81c4 [formerly 613c26658cbcae56da25a3d025012753cc05f4d1]
    Former-commit-id: cac57333501475f20b8ef135180c65f4a974f486

---
+++ dl4j-caffe/src/test/java/org/deeplearning4j/translate/CaffeTest.java
@@ -0,0 +1,30 @@
+package org.deeplearning4j.translate;
+
+import org.deeplearning4j.caffe.Caffe.NetParameter;
+import org.springframework.core.io.ClassPathResource;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by jeffreytang on 7/11/15.
+ */
+public class CaffeTest {
+
+    @Test
+    public void testCaffeModelToJavaClass() throws Exception {
+        // caffemodel downloaded from https://gist.github.com/mavenlin/d802a5849de39225bcc6
+        String imagenetCaffeModelPath = new ClassPathResource("nin_imagenet_conv.caffemodel").getURL().getFile();
+
+        NetParameter net = CaffeModelToJavaClass.readCaffeModel(imagenetCaffeModelPath, 1000);
+        assertEquals(net.getName(), "CaffeNet");
+        assertEquals(net.getLayersCount(), 31);
+        assertEquals(net.getLayers(0).getName(), "data");
+        assertEquals(net.getLayers(30).getName(), "loss");
+        assertEquals(net.getLayers(15).getBlobs(0).getData(0), -0.008252043f, 1e-1);
+
+    }
+
+}
