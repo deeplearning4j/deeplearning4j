ae3e533167324131c6619df63bbf829e1c064929
jyt109 <jeffrey.tang09@gmail.com>
2015-07-12 19:51:43 -0700

    all tests pass read textformat and binary
    Former-commit-id: cab30c9627b9bc2615e6c42f0568ba076c911c20 [formerly 395a957b2c65a635d20821caadbd5772fc3c195e]
    Former-commit-id: 6af1d1b20df22ee0f93915c708dbe0f57e2d2f1c

--- dl4j-caffe/src/main/java/org/deeplearning4j/translate/CaffeModelToJavaClass.java
+++ dl4j-caffe/src/main/java/org/deeplearning4j/translate/CaffeModelToJavaClass.java
@@ -1,6 +1,8 @@
 package org.deeplearning4j.translate;
 
 import com.google.protobuf.TextFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.deeplearning4j.caffe.Caffe.*;
 import com.google.protobuf.CodedInputStream;
 
@@ -18,7 +20,7 @@ public class CaffeModelToJavaClass {
      * @return NetParameter Java Class
      * @throws IOException
      */
-    public static NetParameter readBinaryCaffeModel(InputStream is, int sizeLimitMb) throws IOException {
+    protected static NetParameter readBinaryNet(InputStream is, int sizeLimitMb) throws IOException {
         CodedInputStream codeStream = CodedInputStream.newInstance(is);
         // Increase the limit when loading bigger caffemodels size
         int oldLimit = codeStream.setSizeLimit(sizeLimitMb * 1024 * 1024);
@@ -27,19 +29,19 @@ public class CaffeModelToJavaClass {
 
     /**
      *
-     * @param caffeModelPath Path of caffemodel
+     * @param binaryNetPath Path of caffemodel
      * @param sizeLimitMb Size limit of the CodedInputStream
      * @return NetParameter Java Class
      * @throws IOException
      */
-    public static NetParameter readBinaryCaffeModel(String caffeModelPath, int sizeLimitMb) throws IOException {
-        InputStream is = new BufferedInputStream(new FileInputStream(caffeModelPath));
-        return readBinaryCaffeModel(is, sizeLimitMb);
+    protected static NetParameter readBinaryNet(String binaryNetPath, int sizeLimitMb) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(binaryNetPath));
+        return readBinaryNet(is, sizeLimitMb);
     }
 
-    public static NetParameter readTextFormatNetProto(String caffeNetProtoPath) throws IOException{
+    protected static NetParameter readTextFormatNet(String textFormatNetPath) throws IOException{
 
-        InputStream is = new FileInputStream(caffeNetProtoPath);
+        InputStream is = new FileInputStream(textFormatNetPath);
         InputStreamReader isReader = new InputStreamReader(is, "ASCII");
 
         NetParameter.Builder builder = NetParameter.newBuilder();
@@ -47,9 +49,9 @@ public class CaffeModelToJavaClass {
         return builder.build();
     }
 
-    public static SolverParameter readTextFormatSolverProto(String caffeSolverProtoPath) throws IOException {
+    protected static SolverParameter readTextFormatSolver(String textFormatSolverPath) throws IOException {
 
-        InputStream is = new FileInputStream(caffeSolverProtoPath);
+        InputStream is = new FileInputStream(textFormatSolverPath);
         InputStreamReader isReader = new InputStreamReader(is, "ASCII");
 
         SolverParameter.Builder builder = SolverParameter.newBuilder();
@@ -57,8 +59,27 @@ public class CaffeModelToJavaClass {
         return builder.build();
     }
 
-//    public static NetParameter readCaffeWithWeights() {}
-//
-//    public static NetParameter readCaffeWithoutWeights() {}
+
+    // Define the wrapper class and methods to return the wrapper class
+    @AllArgsConstructor @Data
+    public static class CaffeSolverNetContainer {
+        NetParameter net;
+        SolverParameter solver;
+    }
+
+    public static CaffeSolverNetContainer readCaffeWithWeights(String binaryNetPath,
+                                                               String textFormatSolverPath,
+                                                               int sizeLimitMb) throws IOException {
+        NetParameter net = readBinaryNet(binaryNetPath, sizeLimitMb);
+        SolverParameter solver = readTextFormatSolver(textFormatSolverPath);
+        return new CaffeSolverNetContainer(net, solver);
+    }
+
+    public static CaffeSolverNetContainer readCaffeWithoutWeights(String textFormatNetPath,
+                                                                  String textFormatSolverPath) throws IOException {
+        NetParameter net = readTextFormatNet(textFormatNetPath);
+        SolverParameter solver = readTextFormatSolver(textFormatSolverPath);
+        return new CaffeSolverNetContainer(net, solver);
+    }
 
 }
