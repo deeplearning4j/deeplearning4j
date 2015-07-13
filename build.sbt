9f61d0a888d84787f691c260b81a6178af26b8ea
taisukeoe <oeuia.t@gmail.com>
2015-07-14 01:54:07 +0900

    moved nd4j-scala-api from nd4j repo to nd4s repo

---
+++ build.sbt
@@ -0,0 +1,13 @@
+lazy val root = (project in file(".")).settings(
+       scalaVersion := "2.10.5",
+       name := "ND4s",
+       organization := "org.deeplearning4s",
+       resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository", 
+       libraryDependencies ++= Seq( 
+              "org.scala-lang" % "scala-compiler" % "2.10.5",
+              "org.nd4j" % "nd4j-api" % "0.0.3.5.5.6-SNAPSHOT",  
+              "org.nd4j" % "nd4j-jblas" % "0.0.3.5.5.6-SNAPSHOT" % Test,
+              "org.scalatest" %% "scalatest" % "2.2.4" % Test
+       ),
+       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:implicitConversions", "-language:higherKinds")
+)
---
+++ src/main/scala/org/nd4j/api/linalg/DSL.scala
@@ -0,0 +1,43 @@
+/*
+ *
+ *  * Copyright 2015 Skymind,Inc.
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ *
+ */
+
+package org.nd4j.api.linalg
+
+import org.nd4j.linalg.api.ndarray.INDArray
+
+/**
+ * DSL for using [[INDArray]]s with more Scala-friendly syntax
+ *
+ * Usage example: {{{
+ *   // To use the DSL just use the following import
+ *   import org.nd4j.api.linalg.DSL._
+ *
+ *   // You can now use Scala syntax on the arrays
+ *   val a = Nd4j.create(Array[Float](1, 2), Array(2, 1))
+ *   val b = a + 10
+ * }}}
+ */
+object DSL {
+
+  /**
+   * Make [INDArray] more scala friendly
+   */
+  implicit def toRichNDArray(a: INDArray): RichNDArray = new RichNDArray(a)
+
+}
