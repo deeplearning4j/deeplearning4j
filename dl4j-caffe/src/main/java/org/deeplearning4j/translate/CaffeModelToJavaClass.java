1ab178f36fe21e362c6db5cdd7d179d45083f193
jyt109 <jeffrey.tang09@gmail.com>
2015-07-12 17:16:27 -0700

    text format 2
    Former-commit-id: 7495cc5ddb186e9d2df5dd68eea06c997ef3b150

--- dl4j-caffe/src/main/java/org/deeplearning4j/translate/CaffeModelToJavaClass.java
+++ dl4j-caffe/src/main/java/org/deeplearning4j/translate/CaffeModelToJavaClass.java
@@ -1,12 +1,10 @@
 package org.deeplearning4j.translate;
 
+import com.google.protobuf.TextFormat;
 import org.deeplearning4j.caffe.Caffe.*;
 import com.google.protobuf.CodedInputStream;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * @author jeffreytang
@@ -20,7 +18,7 @@ public class CaffeModelToJavaClass {
      * @return NetParameter Java Class
      * @throws IOException
      */
-    public static NetParameter readCaffeModel(InputStream is, int sizeLimitMb) throws IOException {
+    public static NetParameter readBinaryCaffeModel(InputStream is, int sizeLimitMb) throws IOException {
         CodedInputStream codeStream = CodedInputStream.newInstance(is);
         // Increase the limit when loading bigger caffemodels size
         int oldLimit = codeStream.setSizeLimit(sizeLimitMb * 1024 * 1024);
@@ -34,8 +32,33 @@ public class CaffeModelToJavaClass {
      * @return NetParameter Java Class
      * @throws IOException
      */
-    public static NetParameter readCaffeModel(String caffeModelPath, int sizeLimitMb) throws IOException {
+    public static NetParameter readBinaryCaffeModel(String caffeModelPath, int sizeLimitMb) throws IOException {
         InputStream is = new BufferedInputStream(new FileInputStream(caffeModelPath));
-        return readCaffeModel(is,sizeLimitMb);
+        return readBinaryCaffeModel(is, sizeLimitMb);
     }
+
+    public static NetParameter readTextFormatNetProto(String caffeNetProtoPath) throws IOException{
+
+        InputStream is = new FileInputStream(caffeNetProtoPath);
+        InputStreamReader isReader = new InputStreamReader(is, "ASCII");
+
+        NetParameter.Builder builder = NetParameter.newBuilder();
+        TextFormat.merge(isReader, builder);
+        return builder.build();
+    }
+
+    public static SolverParameter readTextFormatSolverProto(String caffeSolverProtoPath) throws IOException {
+
+        InputStream is = new FileInputStream(caffeSolverProtoPath);
+        InputStreamReader isReader = new InputStreamReader(is, "ASCII");
+
+        SolverParameter.Builder builder = SolverParameter.newBuilder();
+        TextFormat.merge(isReader, builder);
+        return builder.build();
+    }
+
+//    public static NetParameter readCaffeWithWeights() {}
+//
+//    public static NetParameter readCaffeWithoutWeights() {}
+
 }
