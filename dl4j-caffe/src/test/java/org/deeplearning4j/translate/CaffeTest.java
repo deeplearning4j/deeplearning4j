ae1c515743f079c997ec1cc161e3c6200ed389ac
jyt109 <jeffrey.tang09@gmail.com>
2015-07-12 17:16:27 -0700

    text format 2
    Former-commit-id: 1ab178f36fe21e362c6db5cdd7d179d45083f193 [formerly 7495cc5ddb186e9d2df5dd68eea06c997ef3b150]
    Former-commit-id: 87ac8cb2fdbc1f399bd0bd6692197bde73ecfcbf

--- dl4j-caffe/src/test/java/org/deeplearning4j/translate/CaffeTest.java
+++ dl4j-caffe/src/test/java/org/deeplearning4j/translate/CaffeTest.java
@@ -1,8 +1,12 @@
 package org.deeplearning4j.translate;
 
+import org.deeplearning4j.caffe.Caffe;
 import org.deeplearning4j.caffe.Caffe.NetParameter;
 import org.springframework.core.io.ClassPathResource;
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,18 +14,66 @@ import static org.junit.Assert.*;
  */
 public class CaffeTest {
 
+    /**
+     * Test reading the ImageNet in binary format into a Java Class
+     * This includes the network configuration and the pre-trained weights
+     * But not the Solver parameters, e.g. max_iterations, momentum, etc
+     * @throws Exception
+     */
     @Test
-    public void testCaffeModelToJavaClass() throws Exception {
+    public void testBinaryCaffeModelToJavaClass() throws Exception {
         // caffemodel downloaded from https://gist.github.com/mavenlin/d802a5849de39225bcc6
-        String imagenetCaffeModelPath = new ClassPathResource("nin_imagenet_conv.caffemodel").getURL().getFile();
+        String imagenetCaffeModelPath = new ClassPathResource("nin_imagenet/nin_imagenet_conv.caffemodel").getURL().getFile();
 
-        NetParameter net = CaffeModelToJavaClass.readCaffeModel(imagenetCaffeModelPath, 1000);
+        // Read the Binary File to a Java Class
+        NetParameter net = CaffeModelToJavaClass.readBinaryCaffeModel(imagenetCaffeModelPath, 1000);
+
+        // Test the binary file is read in correctly
         assertEquals(net.getName(), "CaffeNet");
         assertEquals(net.getLayersCount(), 31);
         assertEquals(net.getLayers(0).getName(), "data");
         assertEquals(net.getLayers(30).getName(), "loss");
-        assertEquals(net.getLayers(15).getBlobs(0).getData(0), -0.008252043f, 1e-1);
+        assertEquals(net.getLayers(15).getBlobs(0).getData(0), -0.008252043f, 1e-3);
+    }
 
+    /**
+     * Test reading the ImageNet Solver parameters in a proto-text (non binary format) file
+     * @throws IOException
+     */
+    @Test
+    public void testTextFormatSolverProtoToJavaClass() throws IOException {
+        // caffemodel downloaded from https://gist.github.com/mavenlin/d802a5849de39225bcc6
+        String imagenetSolverProtoPath = new ClassPathResource("nin_imagenet/solver.prototxt").getURL().getFile();
+
+        // Read the Solver proto-text File to a Java Class
+        Caffe.SolverParameter solver = CaffeModelToJavaClass.readTextFormatSolverProto(imagenetSolverProtoPath);
+
+        assertEquals(solver.getMaxIter(), 450000);
+        assertEquals(solver.getMomentum(), 0.9f, 1e-3);
+        assertEquals(solver.getBaseLr(), 0.01, 1e-3);
+        assertEquals(solver.getRegularizationType(), "L2");
+    }
+
+    /**
+     * Test reading the ImageNet Network parameters in a proto-text (non binary format) file
+     * Without the weights and Solver parameters
+     * Useful if you do not have the pre-trained weights in a .caffemodel binary file
+     * @throws IOException
+     */
+    @Test
+    public void testTextFormatNetProtoToJavaClass() throws IOException {
+        // caffemodel downloaded from https://gist.github.com/mavenlin/d802a5849de39225bcc6
+        String imagenetNetProtoPath = new ClassPathResource("nin_imagenet/train_val.prototxt").getURL().getFile();
+
+        // Read the Net proto-text File to a Java Class
+        Caffe.NetParameter net = CaffeModelToJavaClass.readTextFormatNetProto(imagenetNetProtoPath);
+
+        assertEquals(net.getName(), "nin_imagenet");
+        // Not 31 because there is an extra test data layer and an accuracy layer,
+        // otherwise same as the net loaded from the  binary .caffemodel file
+        assertEquals(net.getLayersCount(), 33);
+        assertEquals(net.getLayers(0).getName(), "data");
+        assertEquals(net.getLayers(32).getName(), "loss");
     }
 
 }
