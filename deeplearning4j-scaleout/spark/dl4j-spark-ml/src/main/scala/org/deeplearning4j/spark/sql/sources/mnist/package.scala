576dc6bdc25b1eef272aa7e863f35cce1fc8897f
lewuathe <lewuathe@me.com>
2015-07-16 20:44:20 +0900

    Mnist data relation

---
+++ deeplearning4j-scaleout/spark/dl4j-spark-ml/src/main/scala/org/deeplearning4j/spark/sql/sources/mnist/package.scala
@@ -0,0 +1,17 @@
+package org.deeplearning4j.spark.sql.sources
+
+import org.apache.spark.sql.SQLContext
+import org.deeplearning4j.spark.sql.sources.mnist.DefaultSource
+
+package object mnist {
+  /**
+   * Adds a method, `iris`, to SQLContext that allows reading the Iris dataset.
+   */
+  implicit class IrisContext(sqlContext: SQLContext) {
+    def mnist(imagesFile: String, labelsFile: String, numExamples: Int) = {
+      val parameters = Map("images_file" -> imagesFile,
+        "labels_file" -> labelsFile, "num_examples" -> numExamples.toString)
+      sqlContext.read.format(classOf[DefaultSource].getName).options(parameters).load()
+    }
+  }
+}
