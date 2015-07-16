d7c4457dd80154c2d73b26294af0058000b186db
nyghtowl <warrick.melanie@gmail.com>
2015-07-16 10:27:55 -0700

    Minor fix to work with Java7

--- deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/solvers/ConjugateGradient.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/solvers/ConjugateGradient.java
@@ -88,7 +88,7 @@ public class ConjugateGradient extends BaseOptimizer {
         // = ((grad(current)-grad(last)) \dot (grad(current))) / (grad(last) \dot grad(last))
         double dgg = Nd4j.getBlasWrapper().dot(gradient.sub(gLast),gradient);
         double gg = Nd4j.getBlasWrapper().dot(gLast, gLast);
-        double gamma = Double.max(dgg / gg, 0.0);
+        double gamma = Math.max(dgg / gg, 0.0);
         
         //Standard Polak-Ribiere does not guarantee that the search direction is a descent direction
         //But using max(gamma_Polak-Ribiere,0) does guarantee a descent direction. Hence the max above.
