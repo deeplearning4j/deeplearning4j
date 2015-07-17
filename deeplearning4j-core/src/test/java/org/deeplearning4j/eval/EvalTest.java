fb9b00df3e710f0df40e0246e7c370ad16c438d9
jyt109 <jeffrey.tang09@gmail.com>
2015-07-16 23:52:03 -0700

    accuracy and f1 score actually working. All tests passed

--- deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalTest.java
@@ -24,6 +24,9 @@ import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.util.FeatureUtil;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by agibsonccc on 12/22/14.
  */
@@ -31,26 +34,33 @@ public class EvalTest {
 
     @Test
     public void testEval() {
-        Evaluation eval = new Evaluation();
-        INDArray trueOutcome = FeatureUtil.toOutcomeVector(0,5);
-        INDArray test = FeatureUtil.toOutcomeVector(0,5);
-        eval.eval(trueOutcome,test);
-        assertEquals(1.0,eval.f1(),1e-1);
-        assertEquals(1,eval.classCount(0));
+        int classNum = 5;
+        Evaluation eval = new Evaluation(classNum);
 
-        INDArray one = FeatureUtil.toOutcomeVector(1,5);
-        INDArray miss = FeatureUtil.toOutcomeVector(0,5);
-        eval.eval(one,miss);
-        assertTrue(eval.f1() < 1);
-        assertEquals(1,eval.classCount(0));
-        assertEquals(1,eval.classCount(1));
+        // Testing the edge case when some classes do not have true positive
+        INDArray trueOutcome = FeatureUtil.toOutcomeVector(0, 5);
+        INDArray predictedOutcome = FeatureUtil.toOutcomeVector(0, 5);
+        eval.eval(trueOutcome, predictedOutcome);
+        assertEquals(1, eval.classCount(0));
+        assertEquals(1.0, eval.f1(), 1e-1);
 
-        assertEquals(2.0,eval.positive(),1e-1);
-        assertEquals(1.0,eval.negative(),1e-1);
-
-
-
+        // Testing more than one sample. eval() does not reset the Evaluation instance
+        INDArray trueOutcome2 = FeatureUtil.toOutcomeVector(1, 5);
+        INDArray predictedOutcome2 = FeatureUtil.toOutcomeVector(0, 5);
+        eval.eval(trueOutcome2, predictedOutcome2);
+        // Verified with sklearn in Python
+        // from sklearn.metrics import classification_report
+        // classification_report(['a', 'a'], ['a', 'b'], labels=['a', 'b', 'c', 'd', 'e'])
+        assertEquals(eval.f1(), 0.6, 1e-1);
+        // The first entry is 0 label
+        assertEquals(1, eval.classCount(0));
+        // The first entry is 1 label
+        assertEquals(1, eval.classCount(1));
+        // Two positives since two entries
+        assertEquals(2, eval.positive(), 0);
+        // The rest are negative
+        assertEquals(8, eval.negative(), 0);
+        // 2 rows and only the first is correct
+        assertEquals(eval.accuracy(), 0.5, 0);
     }
-
-
 }
