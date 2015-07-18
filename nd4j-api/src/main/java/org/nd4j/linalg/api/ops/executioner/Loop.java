b78fb6a6047c7100f14f283c8ec14961076cabcf
Adam Gibson <adam@skymind.io>
2015-07-17 18:27:24 -0700

    set daemon threads

--- nd4j-api/src/main/java/org/nd4j/linalg/api/ops/executioner/Loop.java
+++ nd4j-api/src/main/java/org/nd4j/linalg/api/ops/executioner/Loop.java
@@ -3,6 +3,7 @@ package org.nd4j.linalg.api.ops.executioner;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class Loop {
     public interface Each {
@@ -10,11 +11,18 @@ public class Loop {
     }
 
     private static final int CPUs = Runtime.getRuntime().availableProcessors();
+    static ExecutorService executor = Executors.newFixedThreadPool(CPUs, new ThreadFactory() {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            return t;
+        }
+    });
 
     public static void withIndex(int start, int stop, final Each body) {
         int chunksize = (stop - start + CPUs - 1) / CPUs;
         int loops = (stop - start + chunksize - 1) / chunksize;
-        ExecutorService executor = Executors.newFixedThreadPool(CPUs);
         final CountDownLatch latch = new CountDownLatch(loops);
         for (int i = start; i < stop; ) {
             final int lo = i;
@@ -32,6 +40,5 @@ public class Loop {
             latch.await();
         } catch (InterruptedException e) {
         }
-        executor.shutdown();
     }
 }
