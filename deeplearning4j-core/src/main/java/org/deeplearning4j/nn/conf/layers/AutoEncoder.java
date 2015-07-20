1b5a64a39a0ac0023e0556ec6c9241cab454395e
jyt109 <jeffrey.tang09@gmail.com>
2015-07-20 13:19:00 -0700

    done autoencoder

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/AutoEncoder.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/AutoEncoder.java
@@ -18,34 +18,58 @@
 
 package org.deeplearning4j.nn.conf.layers;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  *  Autoencoder.
  * Add Gaussian noise to input and learn
  * a reconstruction function.
  *
  */
+@Data
+@NoArgsConstructor
 public class AutoEncoder extends BasePretrainNetwork {
 
     private static final long serialVersionUID = -7624965662728637504L;
+    private double corruptionLevel;
+    private double dropOut;
+    private String activationFunction;
+    private double sparsity;
 
-    @Override
-    public int hashCode() {
-        return 0;
+    public AutoEncoder(int nIn, int nOut) {
+        this.nIn = nIn;
+        this.nOut = nOut;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        return true;
+    public AutoEncoder(int nIn, int nOut, double corruptionLevel) {
+        this.nIn = nIn;
+        this.nOut = nOut;
+        this.corruptionLevel = corruptionLevel;
     }
 
-    public String toString() {
-        return "AutoEncoder{" +
-                '}';
+    public AutoEncoder(int nIn, int nOut, double corruptionLevel, double dropOut) {
+        this.nIn = nIn;
+        this.nOut = nOut;
+        this.corruptionLevel = corruptionLevel;
+        this.dropOut = dropOut;
     }
+
+    public AutoEncoder(int nIn, int nOut, double corruptionLevel, double dropOut, String activationFunction) {
+        this.nIn = nIn;
+        this.nOut = nOut;
+        this.corruptionLevel = corruptionLevel;
+        this.dropOut = dropOut;
+        this.activationFunction = activationFunction;
+    }
+
+    public AutoEncoder(int nIn, int nOut, double corruptionLevel, double dropOut, String activationFunction, double sparsity) {
+        this.nIn = nIn;
+        this.nOut = nOut;
+        this.corruptionLevel = corruptionLevel;
+        this.dropOut = dropOut;
+        this.sparsity = sparsity;
+        this.activationFunction = activationFunction;
+    }
+
 }
