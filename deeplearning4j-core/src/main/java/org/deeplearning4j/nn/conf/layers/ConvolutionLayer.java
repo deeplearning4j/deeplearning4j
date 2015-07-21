af609f720f429fc68618a02c993842ce4146c23c
jyt109 <jeffrey.tang09@gmail.com>
2015-07-20 19:46:03 -0700

    removed weightint in conv layer
    Former-commit-id: 23216fa5a8a229494b623eaf36e09edd06101fca

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/ConvolutionLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/ConvolutionLayer.java
@@ -2,7 +2,6 @@ package org.deeplearning4j.nn.conf.layers;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.convolution.Convolution;
 
 /**
@@ -15,7 +14,6 @@ public class ConvolutionLayer extends Layer {
 
     private Convolution.Type convolutionType; // FULL / VALID / SAME
     private int[] filterSize; // Square filter
-    private WeightInit weightInit; // For initializing weights in kernel
 
 //    A stride of greater than (1, 1) will be implemented in the future.
 //    Stride decides the number of learnable filters
@@ -29,11 +27,5 @@ public class ConvolutionLayer extends Layer {
     public ConvolutionLayer(int[] filterSize, Convolution.Type convolutionType) {
         this.filterSize = filterSize;
         this.convolutionType = convolutionType;
-    }
-
-    public ConvolutionLayer(int[] filterSize, Convolution.Type convolutionType, WeightInit weightInit) {
-        this.filterSize = filterSize;
-        this.convolutionType = convolutionType;
-        this.weightInit = weightInit;
     }
 }
