322fee6a1b380f492a864f2ceb356d39a599985c
Eron Wright <ewright@live.com>
2015-07-21 00:44:04 -0700

    [Spark ML] move helper methods from SqlContext to DataFrameReader
    Former-commit-id: f61ef7d04accd7c9193b3bef4d876588cc4e61c6

--- deeplearning4j-scaleout/spark/dl4j-spark-ml/src/main/scala/org/deeplearning4j/spark/sql/sources/mnist/package.scala
+++ deeplearning4j-scaleout/spark/dl4j-spark-ml/src/main/scala/org/deeplearning4j/spark/sql/sources/mnist/package.scala
@@ -1,16 +1,27 @@
 package org.deeplearning4j.spark.sql.sources
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{DataFrameReader, SQLContext}
 import org.deeplearning4j.spark.sql.sources.mnist.DefaultSource._
 
 package object mnist {
   /**
    * Adds a method, `mnist`, to SQLContext that allows reading the Mnist dataset.
    */
-  implicit class IrisContext(sqlContext: SQLContext) {
+  implicit class MnistContext(sqlContext: SQLContext) {
+    @Deprecated
     def mnist(imagesPath: String, labelsPath: String) = {
       val parameters = Map(ImagesPath -> imagesPath, LabelsPath -> labelsPath)
       sqlContext.read.format(classOf[DefaultSource].getName).options(parameters).load()
+    }
+  }
+
+  /**
+   * Adds a method, `mnist`, to DataFrameReader that allows reading the Mnist dataset.
+   */
+  implicit class MnistDataFrameReader(read: DataFrameReader) {
+    def mnist(imagesPath: String, labelsPath: String) = {
+      val parameters = Map(ImagesPath -> imagesPath, LabelsPath -> labelsPath)
+      read.format(classOf[DefaultSource].getName).options(parameters).load()
     }
   }
 }
