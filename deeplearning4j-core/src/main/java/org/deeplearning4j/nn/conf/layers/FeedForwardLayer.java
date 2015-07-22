0593668875ae59235164aebc23e46c0f2a1ee3dc
jyt109 <jeffrey.tang09@gmail.com>
2015-07-21 19:18:24 -0700

    builder API working. All tests passed

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/FeedForwardLayer.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.nn.conf.layers;
+
+/**
+ * Created by jeffreytang on 7/21/15.
+ */
+public class FeedForwardLayer {
+}
---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/layerBuilderTest.java
@@ -0,0 +1,7 @@
+package org.deeplearning4j.nn.layers;
+
+/**
+ * Created by jeffreytang on 7/20/15.
+ */
+public class layerBuilderTest {
+}
