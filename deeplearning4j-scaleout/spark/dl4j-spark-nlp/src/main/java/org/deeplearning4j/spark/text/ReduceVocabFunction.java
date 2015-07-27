1155f544851eeeb83d79a019b46d1e4b9e6ba48d
jyt109 <jeffrey.tang09@gmail.com>
2015-07-26 17:40:32 -0700

    added class instead of headless function
    Former-commit-id: 15da1c2f6554076107b0dfdd90c0d087fa1d3cef [formerly 4ec13404e2b37b52e506700e92739a6b10fd209f]
    Former-commit-id: 7a0d514c49d9604bd3366eabcf93057c99ec8485

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/text/ReduceVocabFunction.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/text/ReduceVocabFunction.java
@@ -9,7 +9,7 @@ import org.deeplearning4j.models.word2vec.wordstore.inmemory.InMemoryLookupCache
 /**
  * @author Jeffrey Tang
  */
-class ReduceVocab implements Function2<Pair<VocabCache,Long>, Pair<VocabCache,Long>, Pair<VocabCache,Long>> {
+public class ReduceVocabFunction implements Function2<Pair<VocabCache,Long>, Pair<VocabCache,Long>, Pair<VocabCache,Long>> {
     // Function to computer the vocab to word count of each word in vocab
     public Pair<VocabCache,Long> call(Pair<VocabCache,Long> a, Pair<VocabCache,Long> b) {
         // Add InMemoryLookupCache
