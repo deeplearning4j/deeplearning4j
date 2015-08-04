1ce24f73be229a94832023e0f05743698fcfb3f7
Adam Gibson <adam@skymind.io>
2015-08-04 15:58:32 -0700

    relu derivative is itself

--- nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/RectifedLinear.java
+++ nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/RectifedLinear.java
@@ -23,6 +23,7 @@ import org.nd4j.linalg.api.complex.IComplexNumber;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.BaseTransformOp;
 import org.nd4j.linalg.api.ops.Op;
+import org.nd4j.linalg.api.ops.TransformOp;
 import org.nd4j.linalg.factory.Nd4j;
 
 /**
@@ -139,5 +140,10 @@ public class RectifedLinear extends BaseTransformOp {
         else
             return new Pow(xAlongDimension, z.tensorAlongDimension(index, dimension), xAlongDimension.length(), cutoff);
 
+    }
+
+    @Override
+    public TransformOp derivative() {
+        return this;
     }
 }
