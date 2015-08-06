1edcee20903030c7224a22ceebc93946033c49f3
nyghtowl <warrick.melanie@gmail.com>
2015-08-06 11:26:54 -0700

    Refactored processors and added back CNN to align with RNN.

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/preprocessor/CNNProcessorTest.java
@@ -0,0 +1,145 @@
+package org.deeplearning4j.nn.conf.preprocessor;
+
+import static org.junit.Assert.*;
+
+import org.deeplearning4j.berkeley.Pair;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.gradient.Gradient;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.convolution.Convolution;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+/**
+ **/
+
+public class CNNProcessorTest {
+    private static int rows = 28;
+    private static int cols = 28;
+    private static INDArray in2D = Nd4j.create(1, 784);
+    private static INDArray in3D = Nd4j.create(20, 784, 7);
+    private static INDArray in4D = Nd4j.create(20, 1, 28, 28);
+
+
+    @Test
+    public void testFeeForwardToCnnPreProcessor() {
+
+        FeedForwardToCnnPreProcessor convProcessor = new FeedForwardToCnnPreProcessor(rows, cols, 1);
+
+        INDArray check2to4 = convProcessor.preProcess(in2D);
+        int val2to4 = check2to4.shape().length;
+        assertTrue(val2to4 == 4);
+
+        INDArray result2 = convProcessor.preProcess(in4D);
+        int val4to4 = result2.shape().length;
+        assertTrue(val4to4 == 4);
+
+    }
+
+
+    @Test
+    public void testFeeForwardToCnnPreProcessorBackprop() {
+        FeedForwardToCnnPreProcessor convProcessor = new FeedForwardToCnnPreProcessor(rows, cols, 1);
+
+        INDArray check2to2 = convProcessor.backprop(in2D);
+        int val2to2 = check2to2.shape().length;
+        assertTrue(val2to2 == 2);
+
+        INDArray check3to2 = convProcessor.backprop(in3D);
+        int val3to2 = check3to2.shape().length;
+        assertTrue(val3to2 == 2);
+
+
+        INDArray check4to2 = convProcessor.backprop(in4D);
+        int val4to2 = check4to2.shape().length;
+        assertTrue(val4to2 == 2);
+
+    }
+
+    @Test
+    public void testCnnToFeeForwardProcessor() {
+        CnnToFeedForwardPreProcessor convProcessor = new CnnToFeedForwardPreProcessor(rows, cols, 1);
+
+        INDArray check2to4 = convProcessor.backprop(in2D);
+        int val2to4 = check2to4.shape().length;
+        assertTrue(val2to4 == 4);
+
+        INDArray result2 = convProcessor.backprop(in4D);
+        int val4to4 = result2.shape().length;
+        assertTrue(val4to4 == 4);
+
+    }
+
+    @Test
+    public void testCnnToFeeForwardPreProcessorBackprop() {
+        CnnToFeedForwardPreProcessor convProcessor = new CnnToFeedForwardPreProcessor(rows, cols, 1);
+
+        INDArray check2to2 = convProcessor.preProcess(in2D);
+        int val2to2 = check2to2.shape().length;
+        assertTrue(val2to2 == 2);
+
+        INDArray check3to2 = convProcessor.preProcess(in3D);
+        int val3to2 = check3to2.shape().length;
+        assertTrue(val3to2 == 2);
+
+        INDArray check4to2 = convProcessor.preProcess(in4D);
+        int val4to2 = check4to2.shape().length;
+        assertTrue(val4to2 == 2);
+
+    }
+    @Test
+    public void testCNNInputPreProcessorMnist() throws Exception {
+        int numSamples = 1;
+        int batchSize = 1;
+
+        DataSet mnistIter = new MnistDataSetIterator(batchSize, numSamples, true).next();
+        MultiLayerNetwork model = getCNNMnistConfig();
+        model.init();
+        model.fit(mnistIter);
+
+        int val2to4 = model.getLayer(0).input().shape().length;
+        assertTrue(val2to4 == 4);
+
+        int val4to4 = model.getLayer(1).input().shape().length;
+        assertTrue(val4to4 == 4);
+
+    }
+
+
+    public static MultiLayerNetwork getCNNMnistConfig()  {
+
+        Nd4j.ENFORCE_NUMERICAL_STABILITY = true;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(123)
+                .iterations(5)
+                .weightInit(WeightInit.XAVIER)
+                .activationFunction("relu")
+                .optimizationAlgo(OptimizationAlgorithm.LINE_GRADIENT_DESCENT)
+                .list(3)
+                .layer(0, new org.deeplearning4j.nn.conf.layers.ConvolutionLayer.Builder(new int[]{9, 9}, Convolution.Type.VALID)
+                        .nIn(1)
+                        .nOut(20)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{2, 2})
+                        .build())
+                .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nIn(20)
+                        .nOut(10)
+                        .activation("softmax")
+                        .build())
+                .inputPreProcessor(0, new FeedForwardToCnnPreProcessor(rows, cols, 1))
+                .inputPreProcessor(2, new CnnToFeedForwardPreProcessor(rows, cols, 1))
+        .build();
+        return new MultiLayerNetwork(conf);
+
+    }
+}
--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/preprocessor/ReshapePreProcessorTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/preprocessor/ReshapePreProcessorTest.java
@@ -1,22 +1,15 @@
-package org.deeplearning4j.nn.conf;
+package org.deeplearning4j.nn.conf.preprocessor;
 
 import static org.junit.Assert.*;
 
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.layers.RBM;
-import org.deeplearning4j.nn.conf.preprocessor.ReshapeProcessor;
-import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.junit.Test;
 
 /**
  * Created by merlin on 7/31/15.
  */
-public class ProcessorTest {
+public class ReshapePreProcessorTest {
 
         private static int rows = 28;
         private static int cols = 28;
@@ -27,7 +20,7 @@ public class ProcessorTest {
 
         @Test
         public void testPreProcessor2to4() {
-                ReshapeProcessor processor = new ReshapeProcessor(new int[]{1, 784}, new int[]{1, 1, 28, 28});
+                ReshapePreProcessor processor = new ReshapePreProcessor(new int[]{1, 784}, new int[]{1, 1, 28, 28});
 
                 INDArray check2to4 = processor.preProcess(in2D);
                 int val2to4 = check2to4.shape().length;
@@ -35,7 +28,7 @@ public class ProcessorTest {
         }
         @Test
         public void testPreProcessor4to2() {
-                ReshapeProcessor processor = new ReshapeProcessor(new int[] {20, 1, 28, 28}, new int[]{20, 784});
+                ReshapePreProcessor processor = new ReshapePreProcessor(new int[] {20, 1, 28, 28}, new int[]{20, 784});
                 INDArray result2 = processor.preProcess(in4D);
                 int val4to4 = result2.shape().length;
                 assertTrue(val4to4 == 2);
@@ -44,7 +37,7 @@ public class ProcessorTest {
 
         @Test
         public void testPreProcessorBackprop2to4() {
-                ReshapeProcessor processor = new ReshapeProcessor(new int[]{1, 784}, new int[]{1, 1, 28, 28});
+                ReshapePreProcessor processor = new ReshapePreProcessor(new int[]{1, 784}, new int[]{1, 1, 28, 28});
 
                 INDArray check2to2 = processor.backprop(in2D);
                 int val2to2 = check2to2.shape().length;
@@ -53,7 +46,7 @@ public class ProcessorTest {
 
         @Test
         public void testPreProcessorBackprop3to2() {
-                ReshapeProcessor processor = new ReshapeProcessor(new int[]{1, 784, 7}, new int[] {1, 5488});
+                ReshapePreProcessor processor = new ReshapePreProcessor(new int[]{1, 784, 7}, new int[] {1, 5488});
 
                 INDArray check3to2 = processor.backprop(in4D);
                 int val3to2 = check3to2.shape().length;
