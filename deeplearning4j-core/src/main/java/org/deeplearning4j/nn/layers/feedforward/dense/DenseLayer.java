52e2bed9ac81ef650eb4e0b53df3106d2379fb75
Alex Black <blacka101@gmail.com>
2015-08-07 12:55:27 +1000

    refactor DenseLayer (extends BaseLayer not AutoEncoder)
    Former-commit-id: a03a0d242419dbeb7a3669911e46bf84af124ede

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/feedforward/dense/DenseLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/feedforward/dense/DenseLayer.java
@@ -1,19 +1,13 @@
 package org.deeplearning4j.nn.layers.feedforward.dense;
 
-import org.deeplearning4j.berkeley.Pair;
-import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.gradient.DefaultGradient;
-import org.deeplearning4j.nn.gradient.Gradient;
-import org.deeplearning4j.nn.layers.feedforward.autoencoder.AutoEncoder;
-import org.deeplearning4j.nn.params.DefaultParamInitializer;
+import org.deeplearning4j.nn.layers.BaseLayer;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.factory.Nd4j;
 
 /**
  * @author Adam Gibson
  */
-public class DenseLayer extends AutoEncoder {
+public class DenseLayer extends BaseLayer {
     public DenseLayer(NeuralNetConfiguration conf) {
         super(conf);
     }
@@ -21,6 +15,4 @@ public class DenseLayer extends AutoEncoder {
     public DenseLayer(NeuralNetConfiguration conf, INDArray input) {
         super(conf, input);
     }
-
-
 }
