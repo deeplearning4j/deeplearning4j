01d5a151499ac8fbc13eba0237dbff2c5b3514b4
Adam Gibson <adam@skymind.io>
2015-08-08 12:18:17 -0700

    handle row vector with indexing zero

--- nd4j-api/src/main/java/org/nd4j/linalg/api/ndarray/BaseNDArray.java
+++ nd4j-api/src/main/java/org/nd4j/linalg/api/ndarray/BaseNDArray.java
@@ -3721,6 +3721,11 @@ public abstract class BaseNDArray implements INDArray {
      */
     @Override
     public INDArray get(NDArrayIndex... indexes) {
+        if(isRowVector()) {
+            if(indexes[0].offset() == 0)
+                return get(Arrays.copyOfRange(indexes,1,indexes.length));
+        }
+        
         ShapeOffsetResolution resolution = new ShapeOffsetResolution(this);
         resolution.exec(indexes);
         int[] shape = resolution.getShapes();
