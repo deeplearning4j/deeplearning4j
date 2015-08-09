a8d858f54d998b28166687c475524a3995777fd3
Alex Black <blacka101@gmail.com>
2015-08-09 12:00:53 +1000

    Add None/NoOp gradient updater
    Former-commit-id: 9a36f6f019ab1bd2bf2dcdacb86df7e7ad77e32a

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/updater/NoOpUpdater.java
@@ -0,0 +1,31 @@
+package org.deeplearning4j.nn.updater;
+
+import org.deeplearning4j.nn.api.Layer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.learning.GradientUpdater;
+
+public class NoOpUpdater extends BaseUpdater {
+	private NoOpGradientUpdater updater;
+	
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public GradientUpdater init(String variable, INDArray gradient, Layer layer) {
+		GradientUpdater updater = updaterForVariable.get(variable);
+        if (updater == null) {
+            updater = new NoOpGradientUpdater();
+            updaterForVariable.put(variable,updater);
+        }
+        return updater;
+	}
+
+	private static class NoOpGradientUpdater implements GradientUpdater {
+		@Override
+		public INDArray getGradient(INDArray gradient, int iteration) {
+			return gradient;
+		}
+	}
+}
