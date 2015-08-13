1be9b801df44927ee758ac54f7de6deaa93cf812
taisukeoe <oeuia.t@gmail.com>
2015-08-14 07:02:41 +0900

    fixes Fortran order indices bug in slicing to follow nd4j change

--- src/main/scala/org/nd4j/api/SliceableNDArray.scala
+++ src/main/scala/org/nd4j/api/SliceableNDArray.scala
@@ -1,17 +1,17 @@
 package org.nd4j.api
 
 import org.nd4j.api.Implicits._
-import org.nd4j.api.NDOrdering.Fortran
 import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.factory.Nd4j
+import org.slf4j.LoggerFactory
 
 import _root_.scala.annotation.tailrec
 
 trait SliceableNDArray {
+  lazy val log = LoggerFactory.getLogger(classOf[SliceableNDArray])
   val underlying: INDArray
 
   /*
- Extract subMatrix at given position.
+    Extract subMatrix at given position.
   */
   def subMatrix(target: IndexRange*): INDArray = {
     require(target.size <= underlying.shape().length, "Matrix dimension must be equal or larger than shape's dimension to extract.")
@@ -20,7 +20,7 @@ trait SliceableNDArray {
     else
       underlying.shape()
 
-    val originalTarget = if(underlying.isRowVector && target.size == 1)
+    val originalTarget = if (underlying.isRowVector && target.size == 1)
       IntRange(0) +: target
     else
       target
@@ -38,26 +38,32 @@ trait SliceableNDArray {
         modifyTargetIndices(t, i + 1, inr.asRange(originalShape(i)) :: acc)
 
       case Nil =>
-        if(underlying.ordering() == NDOrdering.Fortran.value)
-          acc
-        else
-          acc.reverse
+        acc.reverse
     }
 
     val modifiedTarget = modifyTargetIndices(originalTarget.toList, 0, Nil)
 
     val targetShape = modifiedTarget.map(_.length).toArray
 
-    def calcIndices(tgt: List[DRange], stride: List[Int]): List[Int] =
-      (tgt zip stride).collect {
+    def calcIndices(tgt: List[DRange], stride: List[Int]): List[Int] = {
+      val indicesOnAxis = (tgt zip stride).collect {
         case (range, st) => range.toList.map(_ * st)
-      }.reduceLeft[List[Int]] { case (l, r) => l.flatMap { i => r.map(_ + i)}}
+      }
+      indicesOnAxis.reduceLeft[List[Int]] { case (l, r) =>
+        if (underlying.ordering() == NDOrdering.C.value)
+          l.flatMap { i => r.map(_ + i)}
+        else
+          r.flatMap { i => l.map(_ + i)}
+      }
+    }
 
     val indices = calcIndices(modifiedTarget.toList, underlying.stride().toList)
+
+    log.trace(s"${target.mkString("[", ",", "]")} means $modifiedTarget at ${originalShape.mkString("[", "x", s"]${underlying.ordering}")} matrix with stride:${underlying.stride.mkString(",")}. Target shape:${targetShape.mkString("[", "x", s"]${underlying.ordering}")} indices:$indices")
 
     val lv = underlying.linearView()
     val filtered = indices.map { i => lv.getDouble(i)}
 
-    filtered.mkNDArray(targetShape, NDOrdering(underlying.ordering()),0)
+    filtered.mkNDArray(targetShape, NDOrdering(underlying.ordering()), 0)
   }
 }
