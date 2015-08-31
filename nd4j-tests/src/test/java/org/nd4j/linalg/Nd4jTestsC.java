51a84ae9e78381d9151186227dd7360f05290aba
Adam Gibson <adam@skymind.io>
2015-08-31 11:14:04 +0900

    also do copy when trailing or leading strides is unit

--- nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
+++ nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
@@ -132,6 +132,8 @@ public  class Nd4jTestsC extends BaseNd4jTest {
         }
     }
 
+
+
     @Test
     public void testGetDouble() {
         INDArray n2 = Nd4j.create(Nd4j.linspace(1, 30, 30).data(), new int[]{3, 5, 2});
@@ -796,7 +798,70 @@ public  class Nd4jTestsC extends BaseNd4jTest {
 
     }
 
+    @Test
+    public void testTADMMul(){
+        Nd4j.getRandom().setSeed(12345);
+        int[] shape = new int[]{4,5,7};
+        INDArray arr = Nd4j.rand(shape);
 
+        INDArray tad = arr.tensorAlongDimension(0,1,2);
+        assertArrayEquals(tad.shape(),new int[]{7,5});
+
+
+        INDArray copy = Nd4j.zeros(7,5);
+        for( int i=0; i<7; i++ ){
+            for( int j=0; j<5; j++ ){
+                copy.putScalar(new int[]{i,j},tad.getDouble(i,j));
+            }
+        }
+
+//        System.out.println(tad);
+//        System.out.println("\n");
+//        System.out.println(copy);
+
+        assertTrue(tad.equals(copy));
+
+        INDArray first = Nd4j.rand(new int[]{2,7});
+        INDArray mmul = first.mmul(tad);
+        INDArray mmulCopy = first.mmul(copy);
+
+        assertTrue(mmul.equals(mmulCopy));
+
+        INDArray mmul2 = tad.mmul(first);
+        INDArray mmul2copy = copy.mmul(first);
+        assertTrue(mmul2.equals(mmul2copy));
+    }
+
+    @Test
+    public void testTADMMulLeadingOne(){
+        Nd4j.getRandom().setSeed(12345);
+        int[] shape = new int[]{1,5,7};
+        INDArray arr = Nd4j.rand(shape);
+
+        INDArray tad = arr.tensorAlongDimension(0,1,2);
+        boolean order = Shape.cOrFortranOrder(tad.shape(),tad.stride(),tad.elementStride());
+        assertArrayEquals(tad.shape(),new int[]{7,5});
+
+
+        INDArray copy = Nd4j.zeros(7,5);
+        for( int i = 0; i < 7; i++ ){
+            for( int j = 0; j < 5; j++ ){
+                copy.putScalar(new int[]{i,j},tad.getDouble(i,j));
+            }
+        }
+
+        assertTrue(tad.equals(copy));
+
+        INDArray first = Nd4j.rand(new int[]{2,7});
+        INDArray mmul = first.mmul(tad);
+        INDArray mmulCopy = first.mmul(copy);
+
+        assertTrue(mmul.equals(mmulCopy));
+
+        INDArray mmul2 = tad.mmul(first);
+        INDArray mmul2copy = copy.mmul(first);
+        assertTrue(mmul2.equals(mmul2copy));
+    }
 
 
     @Test
