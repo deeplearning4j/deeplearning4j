bb345692ca8971fb485f5d67428070f918e5d4bb
nyghtowl <warrick.melanie@gmail.com>
2015-09-01 07:33:27 +0900

    Cleaned up pretrain and backprop tests for CNN. Clarified SeedTest for single layers that computeGradientAndScore.
    Former-commit-id: 803143e218e549fdb847c96a5a9fdf8de3aa4b89 [formerly f66984689d3ffdc10ba1bf2213bb0f4f2516cd24]
    Former-commit-id: bc79cd3c61493268a8421f40dfec753aecb704ce

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/SeedTest.java
@@ -0,0 +1,141 @@
+package org.deeplearning4j.nn.layers;
+
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.deeplearning4j.nn.api.*;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.override.ConfOverride;
+import org.deeplearning4j.nn.layers.factory.LayerFactories;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ */
+
+public class SeedTest {
+
+    private DataSetIterator irisIter = new IrisDataSetIterator(50,50);
+    private DataSet data = irisIter.next();
+
+    @Test
+    public void testRBMSeed() {
+        RBM layerType = new RBM.Builder()
+                .nIn(4)
+                .nOut(3)
+                .activation("tanh")
+                .build();
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .iterations(1)
+                .optimizationAlgo(OptimizationAlgorithm.LBFGS)
+                .layer(layerType)
+                .seed(123)
+                .build();
+
+        Layer layer = LayerFactories.getFactory(conf).create(conf);
+        layer.fit(data.getFeatureMatrix());
+
+        double score = layer.score();
+        INDArray parameters = layer.params();
+        layer.setParams(parameters);
+        layer.computeGradientAndScore();
+
+        double score2 = layer.score();
+        assertEquals(parameters, layer.params());
+        assertEquals(score, score2, 1e-4);
+    }
+
+    // TODO fix - seed in AutoEncoder so the score is consistent
+    @Test
+    public void testRecursiveAutoEncoderSeed() {
+        RecursiveAutoEncoder layerType = new RecursiveAutoEncoder.Builder()
+                .nIn(4)
+                .nOut(3)
+                .activation("sigmoid")
+                .build();
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .iterations(1)
+                .optimizationAlgo(OptimizationAlgorithm.LBFGS)
+                .layer(layerType)
+                .seed(123)
+                .build();
+
+        Layer layer = LayerFactories.getFactory(conf).create(conf);
+        layer.fit(data.getFeatureMatrix());
+
+        double score = layer.score();
+        INDArray parameters = layer.params();
+        layer.setParams(parameters);
+        layer.computeGradientAndScore();
+
+        double score2 = layer.score();
+        assertEquals(parameters, layer.params());
+        assertEquals(score, score2, 1e-4);
+    }
+
+    @Test
+    public void testDenseSeed() {
+        DenseLayer layerType = new DenseLayer.Builder()
+                .nIn(4)
+                .nOut(3)
+                .activation("sigmoid")
+                .build();
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .iterations(1)
+                .layer(layerType)
+                .seed(123)
+                .build();
+        Layer layer = LayerFactories.getFactory(conf).create(conf);
+        layer.fit(data.getFeatureMatrix());
+
+        double score = layer.score();
+        INDArray parameters = layer.params();
+        layer.setParams(parameters);
+        layer.computeGradientAndScore();
+
+        double score2 = layer.score();
+        assertEquals(parameters, layer.params());
+        assertEquals(score, score2, 1e-4);
+    }
+
+    @Test
+    public void testOutputSeed() {
+         OutputLayer layerType = new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                .nIn(10)
+                .nOut(3)
+                .build();
+
+        NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+                .iterations(1)
+                .layer(layerType)
+                .seed(123)
+                .build();
+        Layer layer = LayerFactories.getFactory(conf).create(conf);
+
+        layer.fit(data.getFeatureMatrix());
+
+        double score = layer.score();
+        INDArray parameters = layer.params();
+        layer.setParams(parameters);
+        layer.computeGradientAndScore();
+
+        double score2 = layer.score();
+        assertEquals(parameters, layer.params());
+        assertEquals(score, score2, 1e-4);
+    }
+
+
+}
