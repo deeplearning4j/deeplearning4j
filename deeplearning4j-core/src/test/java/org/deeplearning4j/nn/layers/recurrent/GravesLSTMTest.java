dd1144163e520eb4613252b84cc76f1cd624f383
Alex Black <blacka101@gmail.com>
2015-09-01 23:57:54 +1000

    Add unit test for GravesLSTM.activateHelper()
    Former-commit-id: 2dac34489db3c1b7d6f41f1b0172b42a6569aa3f

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/recurrent/GravesLSTMTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/recurrent/GravesLSTMTest.java
@@ -2,6 +2,9 @@ package org.deeplearning4j.nn.layers.recurrent;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
@@ -107,6 +110,54 @@ public class GravesLSTMTest {
 		//Check update:
 		for( String s : outGradient.gradientForVariable().keySet() ){
 			lstm.update(outGradient.getGradientFor(s), s);
+		}
+	}
+
+	@Test
+	public void testGravesLSTMForwardPassHelper() throws Exception {
+		//GravesLSTM.activateHelper() has different behaviour (due to optimizations) when forBackprop==true vs false
+		//But should otherwise provide identical activations
+		Nd4j.getRandom().setSeed(12345);
+
+		int nIn = 10;
+		int layerSize = 15;
+		int miniBatchSize = 4;
+		int timeSeriesLength = 7;
+
+		NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
+        .layer(new org.deeplearning4j.nn.conf.layers.GravesLSTM.Builder()
+				.nIn(nIn).nOut(layerSize)
+				.weightInit(WeightInit.DISTRIBUTION).dist(new UniformDistribution(0, 1))
+				.activation("tanh")
+				.build())
+		.build();
+
+		GravesLSTM lstm = LayerFactories.getFactory(conf.getLayer()).create(conf);
+		INDArray input = Nd4j.rand(new int[]{miniBatchSize,nIn,timeSeriesLength});
+		lstm.setInput(input);
+
+		Method actHelper = GravesLSTM.class.getDeclaredMethod("activateHelper",boolean.class,INDArray.class,INDArray.class,boolean.class);
+		actHelper.setAccessible(true);
+
+		//Call activateHelper with both forBackprop == true, and forBackprop == false and compare
+		Class<?> innerClass = Class.forName("org.deeplearning4j.nn.layers.recurrent.GravesLSTM$FwdPassReturn");
+
+		Object oFalse = actHelper.invoke(lstm, false,null,null,false);	//GravesLSTM.FwdPassReturn object; want fwdPassOutput INDArray
+		Object oTrue = actHelper.invoke(lstm, false,null,null,true);	//want fwdPassOutputAsArrays object
+
+		Field fwdPassOutput = innerClass.getDeclaredField("fwdPassOutput");
+		fwdPassOutput.setAccessible(true);
+
+		Field fwdPassOutputAsArrays = innerClass.getDeclaredField("fwdPassOutputAsArrays");
+		fwdPassOutputAsArrays.setAccessible(true);
+
+		INDArray fwdPassFalse = (INDArray)fwdPassOutput.get(oFalse);
+		INDArray[] fwdPassTrue = (INDArray[])fwdPassOutputAsArrays.get(oTrue);
+
+		for( int i=0; i<timeSeriesLength; i++ ){
+			INDArray sliceFalse = fwdPassFalse.tensorAlongDimension(i, 1,0);
+			INDArray sliceTrue = fwdPassTrue[i];
+			assertTrue(sliceFalse.equals(sliceTrue));
 		}
 	}
 }
