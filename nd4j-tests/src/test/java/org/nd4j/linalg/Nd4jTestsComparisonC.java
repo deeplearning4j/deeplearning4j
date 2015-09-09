55948fb3d88fa226f6b662f658a827182965f39e
Alex Black <blacka101@gmail.com>
2015-09-09 14:27:14 +1000

    Fix comparison tests to use correct index

--- nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsComparisonC.java
+++ nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsComparisonC.java
@@ -84,7 +84,7 @@ public  class Nd4jTestsComparisonC extends BaseNd4jTest {
     	for( int i = 0; i < first.size(); i++ ){
     		for( int j = 0; j < second.size(); j++ ){
     			Pair<INDArray,String> p1 = first.get(i);
-    			Pair<INDArray,String> p2 = second.get(i);
+    			Pair<INDArray,String> p2 = second.get(j);
     			String errorMsg = getTestWithOpsErrorMsg(i,j,"mmul",p1,p2);
     			assertTrue(errorMsg, CheckUtil.checkMmul(p1.getFirst(), p2.getFirst(), 1e-4, 1e-6));
     		}
@@ -98,7 +98,7 @@ public  class Nd4jTestsComparisonC extends BaseNd4jTest {
     	for( int i=0; i<first.size(); i++ ){
     		for( int j=0; j<second.size(); j++ ){
     			Pair<INDArray,String> p1 = first.get(i);
-    			Pair<INDArray,String> p2 = second.get(i);
+    			Pair<INDArray,String> p2 = second.get(j);
     			String errorMsg1 = getTestWithOpsErrorMsg(i,j,"add",p1,p2);
     			String errorMsg2 = getTestWithOpsErrorMsg(i,j,"sub",p1,p2);
     			assertTrue(errorMsg1,CheckUtil.checkAdd(p1.getFirst(), p2.getFirst(), 1e-4, 1e-6));
--- nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsComparisonFortran.java
+++ nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsComparisonFortran.java
@@ -85,7 +85,7 @@ public  class Nd4jTestsComparisonFortran extends BaseNd4jTest {
       	for( int i = 0; i < first.size(); i++ ){
     		for( int j = 0; j < second.size(); j++ ){
     			Pair<INDArray,String> p1 = first.get(i);
-    			Pair<INDArray,String> p2 = second.get(i);
+    			Pair<INDArray,String> p2 = second.get(j);
     			String errorMsg = getTestWithOpsErrorMsg(i,j,"mmul",p1,p2);
     			assertTrue(errorMsg, CheckUtil.checkMmul(p1.getFirst(), p2.getFirst(), 1e-4, 1e-6));
     		}
@@ -99,7 +99,7 @@ public  class Nd4jTestsComparisonFortran extends BaseNd4jTest {
     	for( int i = 0; i< first.size(); i++) {
     		for( int j = 0; j< second.size(); j++) {
     			Pair<INDArray,String> p1 = first.get(i);
-    			Pair<INDArray,String> p2 = second.get(i);
+    			Pair<INDArray,String> p2 = second.get(j);
     			String errorMsg1 = getTestWithOpsErrorMsg(i, j, "add", p1, p2);
     			String errorMsg2 = getTestWithOpsErrorMsg(i, j, "sub", p1, p2);
                 boolean addFail = CheckUtil.checkAdd(p1.getFirst(), p2.getFirst(), 1e-4, 1e-6);
