9711169f978a790c4a45ea620dfa68d101d3e778
Adam Gibson <adam@skymind.io>
2015-09-09 12:26:19 +0900

    merge

--- nd4j-api/src/main/java/org/nd4j/linalg/api/blas/params/GemmParams.java
+++ nd4j-api/src/main/java/org/nd4j/linalg/api/blas/params/GemmParams.java
@@ -36,76 +36,37 @@ public @Data class GemmParams {
         this.b = b;
         this.c = c;
 
-        this.m = a.ordering() == 'f' ? a.size(0) : a.size(1);
-        this.n = b.ordering() == 'f' ? b.size(1) : b.size(0);
-        this.k = c.ordering() == 'f' ? a.size(1) : a.size(0);
+        //multiple backends force us to be
+        //in fortran ordering only
+        this.a = a;
+        this.b = b;
+        this.c = c;
 
-
-
-        this.lda = a.ordering() == 'f' ? a.size(0) > 1 ? a.size(0) : 1 : a.size(1) > 1 ? a.size(1) : 1;
-        this.ldb = b.ordering() == 'f' ? b.size(0) > 1 ? b.size(0) : 1 : b.size(1) > 1 ? b.size(1) : 1;
-        this.ldc = c.size(0) > 1 ? c.size(0) : 1;
-
-
-        if(a.ordering() == 'c') {
-            aOrdering = 'T';
-
+        if(a.ordering() == 'f' && b.ordering() == 'f'){
+            this.m = a.rows();
+            this.n = b.columns();
+            this.k = a.columns();
+            this.lda = a.rows();
+            this.ldb = b.rows();
+            this.ldc = a.rows();
         }
-
-        if(b.ordering() == 'c') {
+        else if(a.ordering() == 'c' && b.ordering() == 'c') {
+            this.m = c.rows();
+            this.n = c.columns();
+            this.k = b.rows();
+            this.lda = a.columns();
+            this.ldb = b.columns();
+            this.ldc = c.rows();
+            aOrdering = 'T';
             bOrdering = 'T';
         }
-
-
+        else
+            throw new RuntimeException();
 
         ldc = c.size(0);
 
-
-        validate();
     }
 
-
-
-
-    private void validate() {
-     /*   if(aOrdering == 'N') {
-            if(a.columns() != k)
-                throw new IllegalStateException("When trans(a) == n a columns must be equal to k");
-            if(lda < Math.max(1,m))
-                throw new IllegalStateException("When trans(a) == n lda must be >= max(1,m)");
-
-        }
-        else {
-            if(a.columns() != m)
-                throw new IllegalStateException("When trans(a) == t a columns must be m");
-            if(lda < Math.max(1,k))
-                throw new IllegalStateException("When trans(a) == t lda must be >= max(1,k)");
-        }
-        if(bOrdering == 'N') {
-            if(b.columns() != n)
-                throw new IllegalStateException("When trans(b) == n b columns must be n");
-            if(ldb < Math.max(1,k))
-                throw new IllegalStateException("When trans(b) == n ldb must be >= max(1,k)");
-        }
-        else {
-            if(b.columns() != k)
-                throw new IllegalStateException("When trans(b) == t b columns must be k");
-            if(ldb < Math.max(1,n))
-                throw new IllegalStateException("When trans(b) == t ldb must be >= max(1,n)");
-        }
-
-
-        if(ldc < Math.max(1,m))
-            throw new IllegalStateException("ldc must be >= max(1,m)");
-
-        if(m < 0)
-            throw new IllegalStateException("M must be >= 0");
-        if(n < 0)
-            throw new IllegalStateException("N must be >= 0");
-        if(k < 0)
-            throw new IllegalStateException("K must be at least 0");*/
-
-    }
 
 
 
