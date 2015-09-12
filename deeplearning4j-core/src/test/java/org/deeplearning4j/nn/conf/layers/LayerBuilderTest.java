76bf7775f51508513d2a04cd11ca94f91d47006b
Alex Black <blacka101@gmail.com>
2015-09-13 09:19:06 +1000

    Add GravesLSTM forget gate bias initialization option for config

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/layers/LayerBuilderTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/layers/LayerBuilderTest.java
@@ -136,11 +136,14 @@ public class LayerBuilderTest {
     
     @Test
     public void testGravesLSTM() throws Exception {
-    	GravesLSTM glstm = new GravesLSTM.Builder().activation("tanh")
+    	GravesLSTM glstm = new GravesLSTM.Builder()
+                .forgetGateBiasInit(1.5)
+                .activation("tanh")
     			.nIn(numIn).nOut(numOut).build();
     	
     	checkSerialization(glstm);
-    	
+
+        assertEquals(glstm.getForgetGateBiasInit(),1.5,0.0);
     	assertEquals(glstm.nIn,numIn);
     	assertEquals(glstm.nOut,numOut);
     	assertEquals(glstm.activationFunction,"tanh");
