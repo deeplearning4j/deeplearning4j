270acfa95b18ffe5d8c65791164c7698bd5651e0
Adam Gibson <adam@skymind.io>
2015-09-27 11:15:40 -0700

    batchnorm WIP
    Former-commit-id: 372bf64f20a443f61b0d91be1df64dbce82938ae [formerly 2dd0469936d4fb23518cd77200f3218043fdcad9]
    Former-commit-id: e8e3921ad68e2b4538602970d5594901c60e26b3

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/BatchNormalization.java
@@ -0,0 +1,23 @@
+package org.deeplearning4j.nn.conf.layers;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.nd4j.linalg.factory.Nd4j;
+
+/**
+ * Created by agibsonccc on 9/27/15.
+ */
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class BatchNormalization extends Layer {
+    private int[] shape;
+    private double decay = 0.9;
+    private double eps = Nd4j.EPS_THRESHOLD;
+    private int size;
+
+
+}
