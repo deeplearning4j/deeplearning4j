6fbf8b11e12e775ff5c37c97f0e29030a5529ac3
Alex Black <blacka101@gmail.com>
2015-10-21 20:55:44 +1100

    SequenceRecordReaderDataSetIterator inputColumns() and totalOutcomes()
    Former-commit-id: 867550a0450f7783c673eea3db3c77d664c23ecb [formerly 431fe0cded0bb143575ac2bb194415a19e7b4599]
    Former-commit-id: d43eb81b87510a66ac101efb03fa7613896bef88

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/canova/SequenceRecordReaderDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/canova/SequenceRecordReaderDataSetIterator.java
@@ -11,7 +11,8 @@ import org.nd4j.linalg.util.FeatureUtil;
 
 import java.util.*;
 
-/**Sequence record reader data set iterator
+/**
+ * Sequence record reader data set iterator
  * Given a record reader (and optionally another record reader for the labels)
  * generate time series (sequence) data sets
  */
@@ -23,10 +24,16 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
     private int labelIndex = -1;
     private final int numPossibleLabels;
     private int cursor = 0;
+    private int inputColumns = -1;
+    private int totalOutcomes = -1;
+    private boolean useStored = false;
+    private DataSet stored = null;
 
-    /** Constructor where features and labels come from different RecordReaders (for example, different files) */
+    /**
+     * Constructor where features and labels come from different RecordReaders (for example, different files)
+     */
     public SequenceRecordReaderDataSetIterator(SequenceRecordReader featuresReader, SequenceRecordReader labels,
-                                               int miniBatchSize, int numPossibleLabels, boolean regression ){
+                                               int miniBatchSize, int numPossibleLabels, boolean regression) {
         this.recordReader = featuresReader;
         this.labelsReader = labels;
         this.miniBatchSize = miniBatchSize;
@@ -47,11 +54,17 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
 
     @Override
     public DataSet next(int num) {
-        if(!hasNext()) throw new NoSuchElementException();
+        if (useStored) {
+            useStored = false;
+            DataSet temp = stored;
+            stored = null;
+            return temp;
+        }
+        if (!hasNext()) throw new NoSuchElementException();
 
         List<INDArray> featureList = new ArrayList<>(num);
         List<INDArray> labelList = new ArrayList<>(num);
-        for( int i=0; i<num && hasNext(); i++ ){
+        for (int i = 0; i < num && hasNext(); i++) {
 
             Collection<Collection<Writable>> featureSequence = recordReader.sequenceRecord();
             Collection<Collection<Writable>> labelSequence = labelsReader.sequenceRecord();
@@ -76,13 +89,15 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
 
         INDArray featuresOut = Nd4j.create(featureShape);
         INDArray labelsOut = Nd4j.create(labelShape);
-        for( int i=0; i<featureList.size(); i++ ){
-            featuresOut.tensorAlongDimension(i,1,2).assign(featureList.get(i));
-            labelsOut.tensorAlongDimension(i,1,2).assign(labelList.get(i));
+        for (int i = 0; i < featureList.size(); i++) {
+            featuresOut.tensorAlongDimension(i, 1, 2).assign(featureList.get(i));
+            labelsOut.tensorAlongDimension(i, 1, 2).assign(labelList.get(i));
         }
 
         cursor += featureList.size();
-        return new DataSet(featuresOut,labelsOut);
+        if (inputColumns == -1) inputColumns = featuresOut.size(1);
+        if (totalOutcomes == -1) totalOutcomes = labelsOut.size(1);
+        return new DataSet(featuresOut, labelsOut);
     }
 
     @Override
@@ -92,17 +107,28 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
 
     @Override
     public int inputColumns() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (inputColumns != -1) return inputColumns;
+        preLoad();
+        return inputColumns;
     }
 
     @Override
     public int totalOutcomes() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (totalOutcomes != -1) return totalOutcomes;
+        preLoad();
+        return totalOutcomes;
+    }
+
+    private void preLoad() {
+        stored = next();
+        useStored = true;
+        inputColumns = stored.getFeatureMatrix().size(1);
+        totalOutcomes = stored.getLabels().size(1);
     }
 
     @Override
     public void reset() {
-        
+
     }
 
     @Override
@@ -130,7 +156,7 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
         throw new UnsupportedOperationException("Remove not supported for this iterator");
     }
 
-    private INDArray getFeatures(Collection<Collection<Writable>> features ){
+    private INDArray getFeatures(Collection<Collection<Writable>> features) {
 
         //Size of the record?
         int[] shape = new int[2]; //[timeSeriesLength,vectorSize]
@@ -138,40 +164,40 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
 
         Iterator<Collection<Writable>> iter = features.iterator();
 
-        int i=0;
+        int i = 0;
         INDArray out = null;
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             Collection<Writable> step = iter.next();
-            if(i == 0){
+            if (i == 0) {
                 shape[1] = step.size();
                 out = Nd4j.create(shape);
             }
 
             Iterator<Writable> timeStepIter = step.iterator();
-            int f=0;
-            while(timeStepIter.hasNext()){
+            int f = 0;
+            while (timeStepIter.hasNext()) {
                 Writable current = timeStepIter.next();
                 double value = Double.valueOf(current.toString());
-                out.put(i,f++,value);
+                out.put(i, f++, value);
             }
             i++;
         }
         return out;
     }
 
-    private INDArray getLabels(Collection<Collection<Writable>> labels ){
+    private INDArray getLabels(Collection<Collection<Writable>> labels) {
         //Size of the record?
         int[] shape = new int[2];   //[timeSeriesLength,vectorSize]
         shape[0] = labels.size();   //time series/sequence length
 
         Iterator<Collection<Writable>> iter = labels.iterator();
 
-        int i=0;
+        int i = 0;
         INDArray out = null;
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             Collection<Writable> step = iter.next();
-            if(i == 0){
-                if(regression){
+            if (i == 0) {
+                if (regression) {
                     shape[1] = step.size();
                 } else {
                     shape[1] = numPossibleLabels;
@@ -180,13 +206,13 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
             }
 
             Iterator<Writable> timeStepIter = step.iterator();
-            int f=0;
-            if(regression){
+            int f = 0;
+            if (regression) {
                 //Load all values
-                while(timeStepIter.hasNext()){
+                while (timeStepIter.hasNext()) {
                     Writable current = timeStepIter.next();
                     double value = Double.valueOf(current.toString());
-                    out.put(f++,i,value);
+                    out.put(f++, i, value);
                 }
             } else {
                 //Expect a single value (index) -> convert to one-hot vector
@@ -194,7 +220,6 @@ public class SequenceRecordReaderDataSetIterator implements DataSetIterator {
                 int idx = Double.valueOf(value.toString()).intValue();
                 INDArray line = FeatureUtil.toOutcomeVector(idx, numPossibleLabels);
                 out.getRow(i).assign(line);
-//                out.getColumn(i).assign(line);
             }
 
             i++;
