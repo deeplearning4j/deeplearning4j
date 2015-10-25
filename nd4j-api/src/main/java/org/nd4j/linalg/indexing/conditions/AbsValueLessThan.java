e38772cba71265e548c836c80f1a7696928f5245
Alex Black <blacka101@gmail.com>
2015-10-25 16:02:00 +1100

    Add boolean conditions: absolute value less than, abs value greater than

---
+++ nd4j-api/src/main/java/org/nd4j/linalg/indexing/conditions/AbsValueLessThan.java
@@ -0,0 +1,22 @@
+package org.nd4j.linalg.indexing.conditions;
+
+import org.apache.commons.math3.util.FastMath;
+import org.nd4j.linalg.api.complex.IComplexNumber;
+
+/**Boolean condition on absolute value: abs(x) < value
+ */
+public class AbsValueLessThan extends BaseCondition {
+    public AbsValueLessThan(Number value){
+        super(value);
+    }
+
+    @Override
+    public Boolean apply(Number input) {
+        return FastMath.abs(input.doubleValue()) < value.doubleValue();
+    }
+
+    @Override
+    public Boolean apply(IComplexNumber input) {
+        return input.absoluteValue().doubleValue() < value.doubleValue();
+    }
+}
