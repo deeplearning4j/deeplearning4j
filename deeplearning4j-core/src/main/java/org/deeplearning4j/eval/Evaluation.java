6c4f22ef42240711cc8fbca3e75b4e5f390b5358
Alex Black <blacka101@gmail.com>
2015-11-04 00:26:37 +1100

    fix Evaluation.incrementTrueNegatives()
    Former-commit-id: ceee492477e2a3e71778ce3533c88cd2dc021215

--- deeplearning4j-core/src/main/java/org/deeplearning4j/eval/Evaluation.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/eval/Evaluation.java
@@ -366,7 +366,7 @@ public class Evaluation<T extends Comparable<? super T>> implements Serializable
         truePositives.incrementCount(classLabel, 1.0);
     }
     public void incrementTrueNegatives(Integer classLabel) {
-        truePositives.incrementCount(classLabel, 1.0);
+        trueNegatives.incrementCount(classLabel, 1.0);
     }
     public void incrementFalseNegatives(Integer classLabel) {
         falseNegatives.incrementCount(classLabel, 1.0);
