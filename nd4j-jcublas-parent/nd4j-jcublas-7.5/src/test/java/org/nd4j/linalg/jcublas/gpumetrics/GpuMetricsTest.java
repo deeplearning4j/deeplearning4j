b4c2b586ed9e2d9491f5062bd2e1f5afee20d075
Adam Gibson <adam@skymind.io>
2015-11-09 17:21:59 +0900

    add gitmodules

---
+++ nd4j-jcublas-parent/nd4j-jcublas-7.5/src/test/java/org/nd4j/linalg/jcublas/gpumetrics/GpuMetricsTest.java
@@ -0,0 +1,19 @@
+package org.nd4j.linalg.jcublas.gpumetrics;
+
+import org.junit.Test;
+import org.nd4j.linalg.jcublas.context.ContextHolder;
+import org.nd4j.linalg.jcublas.kernel.KernelFunctionLoader;
+import org.nd4j.linalg.jcublas.kernel.KernelFunctions;
+
+/**
+ * Created by agibsonccc on 10/10/15.
+ */
+public class GpuMetricsTest {
+
+    @Test
+    public void testMetrics() {
+        GpuMetrics metrics = GpuMetrics.blockAndThreads("float",1000);
+        System.out.println(metrics);
+    }
+
+}
