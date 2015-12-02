89d091013fee3a1c14b0410cd08d4baf1abd4195
fartovii <raver119@gmail.com>
2015-12-02 12:47:24 +0300

    Huffman Tree sorting fixed, PrefetchingSentenceIterator fixed
    Huffman Tree sorting fixed, PrefetchingSentenceIterator fixed
    
    
    Former-commit-id: c4e4676e0abd8efb6b6493db2508c6e98d8fa182 [formerly 20847555471f4cab0445778bc61f475d662a8a44]
    Former-commit-id: f3584221e7311792ab3d216044f4d22769e41bb1

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/text/sentenceiterator/PrefetchingSentenceIteratorTest.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/text/sentenceiterator/PrefetchingSentenceIteratorTest.java
@@ -23,6 +23,7 @@ public class PrefetchingSentenceIteratorTest {
         BasicLineIterator iterator = new BasicLineIterator(file);
 
         PrefetchingSentenceIterator fetcher = new PrefetchingSentenceIterator.Builder(iterator)
+                .setFetchSize(1000)
                 .build();
 
         log.info("Phase 1 starting");
@@ -47,5 +48,31 @@ public class PrefetchingSentenceIteratorTest {
         }
 
         assertEquals(97162, cnt);
+    }
+
+    @Test
+    public void testLoadedIterator1() throws Exception {
+        ClassPathResource resource = new ClassPathResource("/big/raw_sentences.txt");
+        File file = resource.getFile();
+        BasicLineIterator iterator = new BasicLineIterator(file);
+
+        PrefetchingSentenceIterator fetcher = new PrefetchingSentenceIterator.Builder(iterator)
+                .setFetchSize(1000)
+                .build();
+
+        log.info("Phase 1 starting");
+
+        int cnt = 0;
+        while (fetcher.hasNext()) {
+            String line = fetcher.nextSentence();
+            // we'll imitate some workload in current thread by using ThreadSleep.
+            // there's no need to keep it enabled forever, just uncomment next line if you're going to test this iterator.
+            // otherwise this test will
+
+           //    Thread.sleep(0, 10);
+
+            cnt++;
+            if (cnt % 10000 == 0) log.info("Line processed: " + cnt);
+        }
     }
 }
