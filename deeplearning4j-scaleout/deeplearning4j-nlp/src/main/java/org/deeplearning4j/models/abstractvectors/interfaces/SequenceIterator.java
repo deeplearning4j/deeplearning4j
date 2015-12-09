98149549aa3edc779c5f1fc3b2ac4eab4965dc99
raver <raver119@gmail.com>
2015-12-09 15:44:59 +0300

    AbstractVectors drafting
    Former-commit-id: 09c14641f795d437489d4624d0310a1776359b88 [formerly d42da4e19e812520111dec91899c3dd0f4b2581a]
    Former-commit-id: eba1047e4da5cb9d7b03fbe5061aad7d401f071e

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/abstractvectors/interfaces/SequenceIterator.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/abstractvectors/interfaces/SequenceIterator.java
@@ -1,8 +1,17 @@
 package org.deeplearning4j.models.abstractvectors.interfaces;
 
+import org.deeplearning4j.models.abstractvectors.sequence.Sequence;
+import org.deeplearning4j.models.abstractvectors.sequence.SequenceElement;
+
 /**
  *
  * @author raver119@gmail.com
  */
-public interface SequenceIterator<T> {
+public interface SequenceIterator<T extends SequenceElement> {
+
+    boolean hasMoreSequences();
+
+    Sequence<T> nextSequence();
+
+    void reset();
 }
