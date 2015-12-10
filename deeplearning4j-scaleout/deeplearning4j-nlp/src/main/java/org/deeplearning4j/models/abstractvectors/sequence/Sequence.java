e27a1c6761a564ced392017f5a054b7b8e2b38c1
raver <raver119@gmail.com>
2015-12-10 18:26:09 +0300

    VocabConstructor abstraction
    Former-commit-id: f3dca172824efd154458b64233f1389b40f44200 [formerly fce88552211e0cf1b81ce2e50dba506b8674f6f9]
    Former-commit-id: 4228329336cbd33e3ca5773cfc78361bc5273110

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/abstractvectors/sequence/Sequence.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/abstractvectors/sequence/Sequence.java
@@ -2,8 +2,9 @@ package org.deeplearning4j.models.abstractvectors.sequence;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.collections.map.HashedMap;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Sequence for AbstractVectors is defined as limited set of SequenceElements. It can also contain label, if you're going to learn Sequence features as well.
@@ -11,6 +12,32 @@ import java.util.List;
  * @author raver119@gmail.com
  */
 public class Sequence<T extends SequenceElement> {
-    @Getter @Setter protected List<T> elements;
-    @Getter @Setter protected T label;
+    protected Map<String, T> elements = new HashMap<String, T>();
+    protected T label;
+
+    public Sequence() {
+
+    }
+
+    public Sequence(Collection<T> set) {
+        for (T element : set) {
+            this.elements.put(element.getLabel(), element);
+        }
+    }
+
+    public List<String> asLabels() {
+        List<String> labels = new ArrayList<>();
+        for(T element: getElements()) {
+            labels.add(element.getLabel());
+        }
+        return labels;
+    }
+
+    public T getElementByLabel(String label) {
+        return elements.get(label);
+    }
+
+    public Collection<T> getElements() {
+        return Collections.unmodifiableCollection(elements.values());
+    }
 }
