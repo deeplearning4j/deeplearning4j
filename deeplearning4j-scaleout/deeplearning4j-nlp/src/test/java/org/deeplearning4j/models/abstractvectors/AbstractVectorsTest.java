b6ad011f4929d6b18e0ffb120ee5a844bfef0cf5
fartovii <raver119@gmail.com>
2015-12-12 15:39:52 +0300

    AbstractVectors implemented

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/abstractvectors/AbstractVectorsTest.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/abstractvectors/AbstractVectorsTest.java
@@ -3,8 +3,11 @@ package org.deeplearning4j.models.abstractvectors;
 import org.canova.api.util.ClassPathResource;
 import org.deeplearning4j.models.abstractvectors.iterators.AbstractSequenceIterator;
 import org.deeplearning4j.models.abstractvectors.transformers.impl.SentenceTransformer;
+import org.deeplearning4j.models.embeddings.WeightLookupTable;
+import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
 import org.deeplearning4j.models.embeddings.loader.VectorsConfiguration;
 import org.deeplearning4j.models.word2vec.VocabWord;
+import org.deeplearning4j.models.word2vec.wordstore.VocabConstructor;
 import org.deeplearning4j.models.word2vec.wordstore.inmemory.AbstractCache;
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
@@ -13,6 +16,8 @@ import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFac
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -23,6 +28,8 @@ import static org.junit.Assert.*;
  * @author raver119@gmail.com
  */
 public class AbstractVectorsTest {
+
+    protected static final Logger logger = LoggerFactory.getLogger(AbstractVectorsTest.class);
 
     @Before
     public void setUp() throws Exception {
@@ -61,18 +68,56 @@ public class AbstractVectorsTest {
         AbstractSequenceIterator<VocabWord> sequenceIterator = new AbstractSequenceIterator.Builder<VocabWord>(transformer)
                 .build();
 
+
+        /*
+            Now we should build vocabulary out of sequence iterator
+        */
+        VocabConstructor<VocabWord> constructor = new VocabConstructor.Builder<VocabWord>()
+                .addSource(sequenceIterator, 5)
+                .useAdaGrad(false)
+                .setTargetVocabCache(vocabCache)
+                .build();
+
+        constructor.buildJointVocabulary(false, true);
+
+        assertEquals(242, vocabCache.numWords());
+
+        assertEquals(634303, vocabCache.totalWordOccurrences());
+
+
+        /*
+            Time to build WeightLookupTable instance for our new model
+        */
+
+        WeightLookupTable<VocabWord> lookupTable = new InMemoryLookupTable.Builder<VocabWord>()
+                .lr(0.025)
+                .vectorLength(150)
+                .useAdaGrad(false)
+                .cache(vocabCache)
+                .build();
+
+        lookupTable.resetWeights(true);
+
         /*
             Now we can build AbstractVectors model, that suits our needs
          */
         AbstractVectors<VocabWord> vectors = new AbstractVectors.Builder<VocabWord>(new VectorsConfiguration())
                 .minWordFrequency(3)
 
+                // WeightLookupTable
+                .setLookupTable(lookupTable)
+
                 // abstract iterator that covers training corpus
                 .iterate(sequenceIterator)
 
+                // vocabulary built prior to modelling
+                .setVocabCache(vocabCache)
+
                 // if set to true, vocabulary will be built from scratches internally
                 // otherwise externally provided vocab will be used
-                .resetModel(true)
+                .resetModel(false)
+
+
                 .build();
 
         /*
@@ -86,6 +131,7 @@ public class AbstractVectorsTest {
             objects/relations please take care of Labels uniqueness and meaning for yourself.
          */
         double sim = vectors.similarity("day", "night");
+        logger.info("Day/night similarity: " + sim);
         assertTrue(sim > 0.6d);
 
     }
