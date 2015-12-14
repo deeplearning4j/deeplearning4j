c070e55f111c0b598022a204e3cade6c43a9a11e
Alex Black <blacka101@gmail.com>
2015-12-14 14:18:00 +1100

    First pass on early stopping functionality

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/earlystopping/TestEarlyStopping.java
@@ -0,0 +1,19 @@
+package org.deeplearning4j.nn.earlystopping;
+
+import org.junit.Test;
+
+public class TestEarlyStopping {
+
+    @Test
+    public void testEarlyStoppingIris(){
+
+        test(new Double[0][0]);
+
+    }
+
+    public static void test(Object[][] in){
+
+    }
+
+
+}
