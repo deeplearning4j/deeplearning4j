8b0d4a6a946d13a769e23789fa5548d52b9f8e31
Alex Black <blacka101@gmail.com>
2015-12-16 13:37:41 +1100

    Improve MNIST data set fetcher (add test set downloading, shuffling, in-memory preloading)
    Former-commit-id: 20e547d66bb15ebc1ae67cd05141071811b11c7b

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/mnist/MnistManager.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/mnist/MnistManager.java
@@ -19,9 +19,12 @@
 package org.deeplearning4j.datasets.mnist;
 
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 
 /**
@@ -37,44 +40,24 @@ import java.io.IOException;
  * <pre>
  *  MnistManager m = new MnistManager("t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte");
  *  m.setCurrent(10); //index of the image that we are interested in
- *  int[][] image = m.readImage();
+ *  int[][] image = m.readImageUnsafe();
  *  System.out.println("Label:" + m.readLabel());
  *  MnistManager.writeImageToPpm(image, "10.ppm");
  * </pre>
  */
 public class MnistManager {
-    private MnistImageFile images;
+    MnistImageFile images;
     private MnistLabelFile labels;
 
-    /**
-     * Writes the given image in the given file using the PPM data format.
-     * 
-     * @param image
-     * @param ppmFileName
-     * @throws IOException
-     */
-    public static void writeImageToPpm(int[][] image, String ppmFileName) throws IOException {
-        try (BufferedWriter ppmOut = new BufferedWriter(new FileWriter(ppmFileName))) {
-            int rows = image.length;
-            int cols = image[0].length;
-            ppmOut.write("P3\n");
-            ppmOut.write("" + rows + " " + cols + " 255\n");
-            for (int i = 0; i < rows; i++) {
-                StringBuilder s = new StringBuilder();
-                for (int j = 0; j < cols; j++) {
-                    s.append(image[i][j] + " " + image[i][j] + " " + image[i][j] + "  ");
-                }
-                ppmOut.write(s.toString());
-            }
-        }
-
-    }
+    private byte[][] imagesArr;
+    private int[] labelsArr;
+    private static final int HEADER_SIZE = 8;
 
     /**
      * Constructs an instance managing the two given data files. Supports
      * <code>NULL</code> value for one of the arguments in case reading only one
      * of the files (images and labels) is required.
-     * 
+     *
      * @param imagesFile
      *            Can be <code>NULL</code>. In that case all future operations
      *            using that file will fail.
@@ -83,31 +66,27 @@ public class MnistManager {
      *            using that file will fail.
      * @throws IOException
      */
-    public MnistManager(String imagesFile, String labelsFile) throws IOException {
+    public MnistManager(String imagesFile, String labelsFile, boolean train) throws IOException {
         if (imagesFile != null) {
             images = new MnistImageFile(imagesFile, "r");
+            if(train) imagesArr = new MnistImageFile(imagesFile, "r").readImagesUnsafe(MnistDataFetcher.NUM_EXAMPLES);
+            else imagesArr = images.readImagesUnsafe(MnistDataFetcher.NUM_EXAMPLES_TEST);
         }
         if (labelsFile != null) {
             labels = new MnistLabelFile(labelsFile, "r");
+            if(train) labelsArr = labels.readLabels(MnistDataFetcher.NUM_EXAMPLES);
+            else labelsArr = labels.readLabels(MnistDataFetcher.NUM_EXAMPLES_TEST);
         }
+        System.out.println();
     }
 
-    /**
-     * Reads the current image.
-     * 
-     * @return matrix
-     * @throws IOException
-     */
-    public int[][] readImage() throws IOException {
-        if (images == null) {
-            throw new IllegalStateException("Images file not initialized.");
-        }
-        return images.readImage();
+    public byte[] readImageUnsafe(int i){
+        return imagesArr[i];
     }
 
     /**
      * Set the position to be read.
-     * 
+     *
      * @param index
      */
     public void setCurrent(int index) {
@@ -117,7 +96,7 @@ public class MnistManager {
 
     /**
      * Reads the current label.
-     * 
+     *
      * @return int
      * @throws IOException
      */
@@ -128,9 +107,13 @@ public class MnistManager {
         return labels.readLabel();
     }
 
+    public int readLabel(int i){
+        return labelsArr[i];
+    }
+
     /**
      * Get the underlying images file as {@link MnistImageFile}.
-     * 
+     *
      * @return {@link MnistImageFile}.
      */
     public MnistImageFile getImages() {
@@ -139,7 +122,7 @@ public class MnistManager {
 
     /**
      * Get the underlying labels file as {@link MnistLabelFile}.
-     * 
+     *
      * @return {@link MnistLabelFile}.
      */
     public MnistLabelFile getLabels() {
