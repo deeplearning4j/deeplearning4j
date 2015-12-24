451f091b0b46f0483f5e95ab46753a8cf901b9cb
raver <raver119@gmail.com>
2015-12-24 22:00:45 +0300

    BinaryMerger
    Former-commit-id: fc8ae50f5ff0c9fc80c2b879790fad40298a5289

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/glove/count/BinaryMerger.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/glove/count/BinaryMerger.java
@@ -1,10 +1,35 @@
 package org.deeplearning4j.models.glove.count;
 
+import lombok.NonNull;
+import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
+import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+
+import java.io.File;
+
 /**
  * Binary implementation of Merger interface, used to provide off-memory storage for cooccurrence maps generated for GloVe
  *
  * @author raver119@gmail.com
  */
-public class BinaryMerger implements Merger {
+public class BinaryMerger<T extends SequenceElement> implements Merger<T> {
+    private VocabCache<T> vocabCache;
 
+    public BinaryMerger(@NonNull File file, @NonNull VocabCache<T> vocabCache) {
+        this.vocabCache = vocabCache;
+    }
+
+    @Override
+    public boolean hasMoreObjects() {
+        return false;
+    }
+
+    @Override
+    public CoOccurrenceWeight<T> nextObject() {
+        return null;
+    }
+
+    @Override
+    public void writeObject(@NonNull CoOccurrenceWeight<T> object) {
+
+    }
 }
--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/glove/count/Merger.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/glove/count/Merger.java
@@ -1,18 +1,20 @@
 package org.deeplearning4j.models.glove.count;
 
+import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
+
 /**
  * Created by raver on 24.12.2015.
  */
-public interface Merger {
+public interface Merger<T extends SequenceElement> {
     /*
         Storage->Memory merging part
      */
     boolean hasMoreObjects();
 
 
-    Object nextObject();
+    CoOccurrenceWeight<T> nextObject();
     /*
         Memory -> Storage part
      */
-    void writeObject();
+    void writeObject(CoOccurrenceWeight<T> object);
 }
