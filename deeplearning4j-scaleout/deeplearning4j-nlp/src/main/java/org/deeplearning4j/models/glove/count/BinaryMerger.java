862ad1742b9028edb850ccc41a92303a23f16b00
raver <raver119@gmail.com>
2015-12-24 19:06:28 +0300

    BinaryMerger
    Former-commit-id: 0c9c634173accfc05219006324aac88f4bdae2fa

---
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/glove/count/BinaryMerger.java
@@ -0,0 +1,10 @@
+package org.deeplearning4j.models.glove.count;
+
+/**
+ * Binary implementation of Merger interface, used to provide off-memory storage for cooccurrence maps generated for GloVe
+ *
+ * @author raver119@gmail.com
+ */
+public class BinaryMerger implements Merger {
+
+}
