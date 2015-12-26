aac7bcd58d62c11a493f8384aaed42b9f5f3e5bd
fartovii <raver119@gmail.com>
2015-12-26 19:19:56 +0300

    final pre-merge tests

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/glove/count/BinaryCoOccurrenceReaderTest.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/glove/count/BinaryCoOccurrenceReaderTest.java
@@ -26,7 +26,7 @@ public class BinaryCoOccurrenceReaderTest {
     }
 
     @Test
-    public void testHasMoreObjects() throws Exception {
+    public void testHasMoreObjects1() throws Exception {
         File tempFile = File.createTempFile("tmp", "tmp");
         tempFile.deleteOnExit();
 
@@ -75,5 +75,64 @@ public class BinaryCoOccurrenceReaderTest {
         }
 
         assertEquals(2, cnt);
+    }
+
+    @Test
+    public void testHasMoreObjects2() throws Exception {
+        File tempFile = File.createTempFile("tmp", "tmp");
+        tempFile.deleteOnExit();
+
+        VocabCache<VocabWord> vocabCache = new AbstractCache.Builder<VocabWord>()
+                .build();
+
+        VocabWord word1 = new VocabWord(1.0, "human");
+        VocabWord word2 = new VocabWord(2.0, "animal");
+        VocabWord word3 = new VocabWord(3.0, "unknown");
+
+        vocabCache.addToken(word1);
+        vocabCache.addToken(word2);
+        vocabCache.addToken(word3);
+
+        Huffman huffman = new Huffman(vocabCache.vocabWords());
+        huffman.build();
+        huffman.applyIndexes(vocabCache);
+
+
+        BinaryCoOccurrenceWriter<VocabWord> writer = new BinaryCoOccurrenceWriter<>(tempFile);
+
+        CoOccurrenceWeight<VocabWord> object1 = new CoOccurrenceWeight<VocabWord>();
+        object1.setElement1(word1);
+        object1.setElement2(word2);
+        object1.setWeight(3.14159265);
+
+        writer.writeObject(object1);
+
+        CoOccurrenceWeight<VocabWord> object2 = new CoOccurrenceWeight<VocabWord>();
+        object2.setElement1(word2);
+        object2.setElement2(word3);
+        object2.setWeight(0.197);
+
+        writer.writeObject(object2);
+
+        CoOccurrenceWeight<VocabWord> object3 = new CoOccurrenceWeight<VocabWord>();
+        object3.setElement1(word1);
+        object3.setElement2(word3);
+        object3.setWeight(0.001);
+
+        writer.writeObject(object3);
+
+        writer.finish();
+
+        BinaryCoOccurrenceReader<VocabWord> reader = new BinaryCoOccurrenceReader<>(tempFile, vocabCache ,null);
+
+        int cnt = 0;
+        while (reader.hasMoreObjects()) {
+            CoOccurrenceWeight<VocabWord> r1 = reader.nextObject();
+            log.info("Object received: " + r1);
+            assertNotEquals(null, r1);
+            cnt++;
+        }
+
+        assertEquals(3, cnt);
     }
 }
