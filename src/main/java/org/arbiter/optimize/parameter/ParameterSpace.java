3d34b3da01c55dee7a88b802e9878c38ebf4da3c
Alex Black <blacka101@gmail.com>
2016-01-02 18:40:50 +1100

    Add licence/copyright to arbiter-deeplearning4j and -spark; remove duplicate code after refactoring

--- src/main/java/org/arbiter/optimize/parameter/ParameterSpace.java
+++
@@ -1,9 +0,0 @@
-package org.arbiter.optimize.parameter;
-
-/**ModelParameterSpace: defines the acceptable ranges of values a given parameter may take
- */
-public interface ParameterSpace<P> {
-
-    P randomValue();
-
-}
