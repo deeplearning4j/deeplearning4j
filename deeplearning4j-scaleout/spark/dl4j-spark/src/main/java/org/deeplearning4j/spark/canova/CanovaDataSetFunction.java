d047b51f6b6df68f3a0def4cc4baeccb74306f25
Alex Black <blacka101@gmail.com>
2016-01-06 16:51:25 +1100

    Add spark functions for canova writable rdds to dataset rdds
    Former-commit-id: 10e3603f0effcf084d0de4adfe10b324d2d44aff [formerly fe5cba2b89417c0987daa8d0adce37d01a780b7c]
    Former-commit-id: d8f4751aac4223f6f532b90b305cb7d34b3f916f

---
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/canova/CanovaDataSetFunction.java
@@ -0,0 +1,99 @@
+package org.deeplearning4j.spark.canova;
+
+import org.apache.spark.api.java.function.Function;
+import org.canova.api.io.WritableConverter;
+import org.canova.api.io.converters.WritableConverterException;
+import org.canova.api.writable.Writable;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.util.FeatureUtil;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**Map {@code Collection<Writable>} objects (out of a canova-spark record reader function) to DataSet objects for Spark training.
+ * Analogous to RecordReaderDataSetIterator, but in the context of Spark.
+ * @author Alex Black
+ */
+public class CanovaDataSetFunction implements Function<Collection<Writable>,DataSet>, Serializable {
+
+    private final int labelIndex;
+    private final int numPossibleLabels;
+    private final boolean regression;
+    private final DataSetPreProcessor preProcessor;
+    private final WritableConverter converter;
+
+    public CanovaDataSetFunction(int labelIndex, int numPossibleLabels, boolean regression){
+        this(labelIndex, numPossibleLabels, regression, null, null);
+    }
+
+    /**
+     * @param labelIndex Index of the label column
+     * @param numPossibleLabels Number of classes for classification  (not used if regression = true)
+     * @param regression False for classification, true for regression
+     * @param preProcessor DataSetPreprocessor (may be null)
+     * @param converter WritableConverter (may be null)
+     */
+    public CanovaDataSetFunction(int labelIndex, int numPossibleLabels, boolean regression,
+                                 DataSetPreProcessor preProcessor, WritableConverter converter){
+        this.labelIndex = labelIndex;
+        this.numPossibleLabels = numPossibleLabels;
+        this.regression = regression;
+        this.preProcessor = preProcessor;
+        this.converter = converter;
+    }
+
+
+    @Override
+    public DataSet call(Collection<Writable> writables) throws Exception {
+        List<Writable> list;
+        if(writables instanceof List) list = (List<Writable>)writables;
+        else list = new ArrayList<>(writables);
+
+        //allow people to specify label index as -1 and infer the last possible label
+        int labelIndex = this.labelIndex;
+        if (numPossibleLabels >= 1 && labelIndex < 0) {
+            labelIndex = list.size() - 1;
+        }
+
+        INDArray label = null;
+        INDArray featureVector = Nd4j.create(labelIndex >= 0 ? list.size() - 1 : list.size());
+        int featureCount = 0;
+        for (int j = 0; j < list.size(); j++) {
+            Writable current = list.get(j);
+            if (labelIndex >= 0 && j == labelIndex) {
+                //Current value is the label
+                if (converter != null) {
+                    try {
+                        current = converter.convert(current);
+                    } catch (WritableConverterException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (numPossibleLabels < 1)
+                    throw new IllegalStateException("Number of possible labels invalid, must be >= 1");
+
+                if (regression) {
+                    label = Nd4j.scalar(current.toDouble());
+                } else {
+                    //Convert to one-hot vector for
+                    int curr = current.toInt();
+                    if (curr >= numPossibleLabels) throw new IllegalStateException("Invalid input: class label is " + curr
+                            + " with numPossibleLables = " + numPossibleLabels + " (class label must be 0 <= labelIdx < numPossibleLabels)");
+                    label = FeatureUtil.toOutcomeVector(curr, numPossibleLabels);
+                }
+            } else {
+                //Current value is not the label
+                featureVector.putScalar(featureCount++, current.toDouble());
+            }
+        }
+
+        DataSet ds = new org.nd4j.linalg.dataset.DataSet(featureVector, (labelIndex >= 0 ? label : featureVector) );
+        if(preProcessor != null) preProcessor.preProcess(ds);
+        return ds;
+    }
+}
