6e0b71eb3b40c7865ef5b8271adef97002b7b50f
Adam Gibson <adam@skymind.io>
2016-01-07 10:48:38 -0800

    add bash script for building

---
+++ buildnativeoperations.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 1 ]; then
+    echo "Please specify an argument"
+else
+    command="$1"
+    echo "Running $1"
+    if [ "$1" == "clean" ]; then
+       rm -rf build
+       echo "Deleted build"
+    elif [ "$1" ==  "eclipse" ]; then
+            cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE .
+            python ./nsight-err-parse-patch.py ./project
+
+    else
+           mkdir build
+           cd build && cmake ..
+fi
+
+fi
