a1819d0c8e1d89e2cc8af6cfce74f72bec721478
Adam Gibson <adam@skymind.io>
2016-01-09 01:31:51 -0800

    add back tests; of note here: optimization level 3 seems to disable tests

--- main.cu
+++ main.cu
@@ -1,9 +1,9 @@
 #include <CppUTest/CommandLineTestRunner.h>
 #include <shapetests.h>
+#include <scalartests.h>
 #include <transformtests.h>
 #include <pairwise_transform_tests.h>
 #include <broadcaststests.h>
-#include <scalartests.h>
 #include <reducetests.h>
 #include <reduce3tests.h>
 #include <indexreducetests.h>
@@ -20,4 +20,3 @@ IMPORT_TEST_GROUP(Transform);
 IMPORT_TEST_GROUP(Reduce);
 IMPORT_TEST_GROUP(Reduce3);
 IMPORT_TEST_GROUP(IndexReduce);
-//IMPORT_TEST_GROUP(Buffer);
