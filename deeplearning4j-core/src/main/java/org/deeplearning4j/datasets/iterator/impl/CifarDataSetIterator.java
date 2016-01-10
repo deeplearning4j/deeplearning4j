5c90822e0b70dc4dd64638cdb6065a95317d4bf4
nyghtowl <warrick.melanie@gmail.com>
2016-01-10 09:22:50 -0800

    Completed CifarDataSetIterator. Loads bin files from loader and iterates. RecordReader was cut out of iterator because dataset created without it. Add back in if want to make more general.
    Former-commit-id: f78520fb2b643c14609b52b2b1c6c7ed474ad8cd [formerly 9d8f4deadabc8a625a072d630f6196969b50c346]
    Former-commit-id: ce5efe73fa801571f04eb4d77ca3ba59a28a3f7e

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/CifarDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/CifarDataSetIterator.java
@@ -2,6 +2,16 @@ package org.deeplearning4j.datasets.iterator.impl;
 
 import org.canova.image.loader.CifarLoader;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.util.FeatureUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by nyghtowl on 12/18/15.
@@ -11,6 +21,12 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
     protected static int width = 32;
     protected static int height = 32;
     protected static int channels = 3;
+    protected static int numPixels = 3073;
+    protected static CifarLoader loader;
+    protected static InputStream inputStream = null;
+    protected int totalExamples = CifarLoader.NUM_TRAIN_IMAGES + CifarLoader.NUM_TRAIN_IMAGES;
+    protected int numExamples = totalExamples;
+    protected int exampleCount = 0;
 
     /**
      * Create Cifar data specific iterator
@@ -18,8 +34,24 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
      * @param numExamples the overall number of examples
      * */
     public CifarDataSetIterator(int batchSize, int numExamples) {
-        super(new CifarLoader().getRecordReader(numExamples), batchSize, width * height * channels, CifarLoader.NUM_LABELS);
+        super(null, batchSize, width * height * channels, CifarLoader.NUM_LABELS);
+        this.loader = new CifarLoader();
+        this.inputStream  = loader.getInputStream();
+        this.numExamples = numExamples > totalExamples? totalExamples: numExamples;
     }
+
+    /**
+     * Create Cifar data specific iterator
+     * @param batchSize the the batch size of the examples
+     * @param numExamples the overall number of examples
+     * */
+    public CifarDataSetIterator(int batchSize, int numExamples, String version) {
+        super(null, batchSize, width * height * channels, CifarLoader.NUM_LABELS);
+        this.loader = new CifarLoader(version);
+        this.inputStream  = loader.getInputStream();
+        this.numExamples = numExamples > totalExamples? totalExamples: numExamples;
+    }
+
 
     /**
      * Create Cifar data specific iterator
@@ -28,7 +60,10 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
      * @param numExamples the overall number of examples
      * */
     public CifarDataSetIterator(int batchSize, int numExamples, int[] imgDim) {
-        super(new CifarLoader().getRecordReader(imgDim[0], imgDim[1], imgDim[2], numExamples), batchSize, imgDim[0] * imgDim[1] * imgDim[2], CifarLoader.NUM_LABELS);
+        super(null, batchSize, imgDim[0] * imgDim[1] * imgDim[2], CifarLoader.NUM_LABELS);
+        this.loader = new CifarLoader();
+        this.inputStream  = loader.getInputStream();
+        this.numExamples = numExamples > totalExamples? totalExamples: numExamples;
     }
 
     /**
@@ -38,7 +73,10 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
      * @param numExamples the overall number of examples
      * */
     public CifarDataSetIterator(int batchSize, int numExamples, int[] imgDim, int numCategories) {
-        super(new CifarLoader().getRecordReader(imgDim[0], imgDim[1], imgDim[2], numExamples), batchSize, imgDim[0] * imgDim[1] * imgDim[2], numCategories);
+        super(null, batchSize, imgDim[0] * imgDim[1] * imgDim[2], numCategories);
+        this.loader = new CifarLoader();
+        this.inputStream  = loader.getInputStream();
+        this.numExamples = numExamples > totalExamples? totalExamples: numExamples;
     }
 
     /**
@@ -48,7 +86,11 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
      * @param numCategories the overall number of labels
      * */
     public CifarDataSetIterator(int batchSize, int numExamples, int numCategories) {
-        super(new CifarLoader().getRecordReader(numExamples, numCategories), batchSize, width * height * channels, numCategories);
+        super(null, batchSize, width * height * channels, numCategories);
+        this.loader = new CifarLoader();
+        this.inputStream  = loader.getInputStream();
+        this.numExamples = numExamples > totalExamples? totalExamples: numExamples;
+
     }
 
     /**
@@ -57,7 +99,90 @@ public class CifarDataSetIterator extends RecordReaderDataSetIterator {
      * @param imgDim an array of width, height and channels
      */
     public CifarDataSetIterator(int batchSize, int[] imgDim)  {
-        super(new CifarLoader().getRecordReader(imgDim[0], imgDim[1], imgDim[2]), batchSize, imgDim[0] * imgDim[1] * imgDim[2], CifarLoader.NUM_LABELS);
+        super(null, batchSize, imgDim[0] * imgDim[1] * imgDim[2], CifarLoader.NUM_LABELS);
+        this.loader = new CifarLoader();
+        this.inputStream  = loader.getInputStream();
     }
+
+    @Override
+    public DataSet next(int num) {
+        if(useCurrent) {
+            useCurrent = false;
+            if(preProcessor != null) preProcessor.preProcess(last);
+            return last;
+        }
+
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        int batchNumCount = 0;
+        byte[] byteFeature = new byte[numPixels];
+        List<DataSet> dataSets = new ArrayList<>();
+        INDArray label = null; // first value in the 3073 byte array
+        INDArray featureVector = Nd4j.create(numPixels-1); // feature are 3072
+
+        try {
+            while((inputStream.read(byteFeature)) != -1 && batchNumCount != num) {
+                int featureCount = 0;
+                label = FeatureUtil.toOutcomeVector(byteFeature[0], numPossibleLabels);
+                for (int j = 1; j <= featureVector.length(); j++)
+                    featureVector.putScalar(featureCount++, byteFeature[j]);
+                dataSets.add(new DataSet(featureVector, label));
+                batchNumCount++;
+                byteFeature = new byte[numPixels];
+                featureVector = Nd4j.create(numPixels-1);
+            }
+            byteStream.close();
+            exampleCount += batchSize;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<INDArray> inputs = new ArrayList<>();
+        List<INDArray> labels = new ArrayList<>();
+
+        for (DataSet data : dataSets) {
+            inputs.add(data.getFeatureMatrix());
+            labels.add(data.getLabels());
+        }
+
+        if(inputs.isEmpty()) {
+            overshot = true;
+            return last;
+        }
+
+        DataSet ret =  new DataSet(Nd4j.vstack(inputs.toArray(new INDArray[0])), Nd4j.vstack(labels.toArray(new INDArray[0])));
+        last = ret;
+        if(preProcessor != null) preProcessor.preProcess(ret);
+        return ret;
+    }
+
+    @Override
+    public boolean hasNext() {
+        try {
+            return exampleCount < numExamples;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public int totalExamples() {
+        return totalExamples;
+    }
+
+    @Override
+    public void reset() {
+        try {
+            inputStream.reset();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<String> getLabels(){
+        return loader.getLabels();
+    }
+
 
 }
