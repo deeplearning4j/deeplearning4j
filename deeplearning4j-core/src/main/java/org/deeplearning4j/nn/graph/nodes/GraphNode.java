4210d788ded644832889ff8e50c651d3ce5495b2
Alex Black <blacka101@gmail.com>
2016-01-15 00:01:09 +1100

    Initial commit + blocking out for computation graph
    Former-commit-id: 58af90c21fff88cb6583438bc5ecfb107cdefbe7 [formerly 508f91ec40e54abe9b90947315dcc2a5424ac534]
    Former-commit-id: 0af0d67d038a74c0c9b8632fb72c16ca52803e4c

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/graph/nodes/GraphNode.java
@@ -0,0 +1,15 @@
+package org.deeplearning4j.nn.graph.nodes;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/** A GraphNode is a component of a ComputationGraph, that handles the interactions between layers
+ *  */
+public interface GraphNode {
+
+    INDArray[] forward(INDArray... activations);
+
+
+    INDArray[] backward(INDArray... epsilons);
+
+
+}
