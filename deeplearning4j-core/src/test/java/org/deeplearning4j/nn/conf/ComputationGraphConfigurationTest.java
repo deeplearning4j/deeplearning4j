8b9499353ae1ee1f8307e6a520a570208a5863e3
Alex Black <blacka101@gmail.com>
2016-01-17 00:13:50 +1100

    Add computation graph structure validation

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/ComputationGraphConfigurationTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/ComputationGraphConfigurationTest.java
@@ -9,6 +9,7 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
+import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.graph.nodes.ElementWiseNode;
 import org.deeplearning4j.nn.graph.nodes.MergeNode;
 import org.deeplearning4j.nn.graph.nodes.SubsetNode;
@@ -54,8 +55,8 @@ public class ComputationGraphConfigurationTest {
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .graphBuilder()
                 .addInputs("input")
-                .addLayer("cnn1", new ConvolutionLayer.Builder(2,2).stride(2,2).nIn(1).nOut(5).build(), "input")
-                .addLayer("cnn2", new ConvolutionLayer.Builder(2,2).stride(2,2).nIn(1).nOut(5).build(), "input")
+                .addLayer("cnn1", new ConvolutionLayer.Builder(2, 2).stride(2, 2).nIn(1).nOut(5).build(), "input")
+                .addLayer("cnn2", new ConvolutionLayer.Builder(2, 2).stride(2, 2).nIn(1).nOut(5).build(), "input")
                 .addLayer("max1", new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).build(), "cnn1", "cnn2")
                 .addLayer("dnn1", new DenseLayer.Builder().nOut(7).build(), "max1")
                 .addLayer("max2", new SubsamplingLayer.Builder().build(), "max1")
@@ -82,12 +83,14 @@ public class ComputationGraphConfigurationTest {
                 .graphBuilder()
                 .addInputs("input1", "input2")
                 .addLayer("cnn1", new ConvolutionLayer.Builder(2, 2).stride(2, 2).nIn(1).nOut(5).build(), "input1")
-                .addLayer("cnn2", new ConvolutionLayer.Builder(2,2).stride(2,2).nIn(1).nOut(5).build(), "input2")
+                .addLayer("cnn2", new ConvolutionLayer.Builder(2, 2).stride(2, 2).nIn(1).nOut(5).build(), "input2")
                 .addNode("merge1", new MergeNode(), "cnn1", "cnn2")
-                .addNode("subset1", new SubsetNode(0,1), "merge1")
+                .addNode("subset1", new SubsetNode(0, 1), "merge1")
                 .addLayer("dense1", new DenseLayer.Builder().nIn(20).nOut(5).build(), "subset1")
                 .addLayer("dense2", new DenseLayer.Builder().nIn(20).nOut(5).build(), "subset1")
                 .addNode("add", new ElementWiseNode(ElementWiseNode.Op.Add), "dense1", "dense2")
+                .addLayer("out", new OutputLayer.Builder().nIn(1).nOut(1).build(), "add")
+                .setOutputs("out")
                 .build();
 
         String json = conf.toJson();
@@ -97,5 +100,88 @@ public class ComputationGraphConfigurationTest {
 
         assertEquals(json,conf2.toJson());
         assertEquals(conf, conf2);
+    }
+
+    @Test
+    public void testInvalidConfigurations(){
+
+        //Test no inputs for a layer:
+        try{
+            new NeuralNetConfiguration.Builder()
+                    .graphBuilder()
+                    .addInputs("input1")
+                    .addLayer("dense1", new DenseLayer.Builder().nIn(2).nOut(2).build(), "input1")
+                    .addLayer("out", new OutputLayer.Builder().nIn(2).nOut(2).build())
+                    .setOutputs("out")
+                    .build();
+            fail("No exception thrown for invalid configuration");
+        }catch(IllegalStateException e){
+            //OK - exception is good
+            //e.printStackTrace();
+        }
+
+        //Test no network inputs
+        try{
+            new NeuralNetConfiguration.Builder()
+                    .graphBuilder()
+                    .addLayer("dense1", new DenseLayer.Builder().nIn(2).nOut(2).build(), "input1")
+                    .addLayer("out", new OutputLayer.Builder().nIn(2).nOut(2).build(), "dense1")
+                    .setOutputs("out")
+                    .build();
+            fail("No exception thrown for invalid configuration");
+        }catch(IllegalStateException e){
+            //OK - exception is good
+            //e.printStackTrace();
+        }
+
+        //Test no network outputs
+        try{
+            new NeuralNetConfiguration.Builder()
+                    .graphBuilder()
+                    .addInputs("input1")
+                    .addLayer("dense1", new DenseLayer.Builder().nIn(2).nOut(2).build(), "input1")
+                    .addLayer("out", new OutputLayer.Builder().nIn(2).nOut(2).build(), "dense1")
+                    .build();
+            fail("No exception thrown for invalid configuration");
+        }catch(IllegalStateException e){
+            //OK - exception is good
+            //e.printStackTrace();
+        }
+
+        //Test: invalid input
+        try{
+            new NeuralNetConfiguration.Builder()
+                    .graphBuilder()
+                    .addInputs("input1")
+                    .addLayer("dense1", new DenseLayer.Builder().nIn(2).nOut(2).build(), "input1")
+                    .addLayer("out", new OutputLayer.Builder().nIn(2).nOut(2).build(), "thisDoesntExist")
+                    .setOutputs("out")
+                    .build();
+            fail("No exception thrown for invalid configuration");
+        }catch(IllegalStateException e){
+            //OK - exception is good
+            //e.printStackTrace();
+        }
+
+        //Test: graph with cycles
+        try{
+            ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .graphBuilder()
+                    .addInputs("input1")
+                    .addLayer("dense1", new DenseLayer.Builder().nIn(2).nOut(2).build(), "input1", "dense3")
+                    .addLayer("dense2", new DenseLayer.Builder().nIn(2).nOut(2).build(), "dense1")
+                    .addLayer("dense3", new DenseLayer.Builder().nIn(2).nOut(2).build(), "dense2")
+                    .addLayer("out", new OutputLayer.Builder().nIn(2).nOut(2).build(), "dense1")
+                    .setOutputs("out")
+                    .build();
+            //Cycle detection happens in ComputationGraph.init()
+            ComputationGraph graph = new ComputationGraph(conf);
+            graph.init();
+
+            fail("No exception thrown for invalid configuration");
+        }catch(IllegalStateException e){
+            //OK - exception is good
+            //e.printStackTrace();
+        }
     }
 }
