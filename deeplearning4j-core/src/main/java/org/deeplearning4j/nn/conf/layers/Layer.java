f6e31edd91717fe04af9cc0124f78bb407b6f21d
nyghtowl <warrick.melanie@gmail.com>
2016-01-19 11:10:57 -0800

    Rework convoltuionLayerSetup
    Former-commit-id: 672d0bc2b552f330a0550377cd524418467dacf1 [formerly 82e37a9b6e698d4beca9bc66b8befac2296af49a]
    Former-commit-id: 9c1414568f09a50000e3fb99b14b99ed2daf5b39

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/Layer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/Layer.java
@@ -119,7 +119,7 @@ public abstract class Layer implements Serializable, Cloneable {
 
 
     public abstract static class Builder<T extends Builder<T>> {
-        protected String layerName = "genisys";
+        protected String layerName = null;
         protected String activationFunction = null;
         protected WeightInit weightInit = null;
         protected double biasInit = Double.NaN;
