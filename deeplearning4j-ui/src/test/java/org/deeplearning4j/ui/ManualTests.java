794992340b082e80bb20235eff43fcd59d4ac21c
raver119 <raver119@gmail.com>
2016-01-22 16:23:13 +0300

    weights ui p.1
    Former-commit-id: a65a9d1ccb0b8fc02fdec5015cd4c678a86ee7b9 [formerly adfd679b11a07fc0b4443fd8ab5bebdf4b7cdcce]
    Former-commit-id: b10a1b2a3600c58c7a95042836ee2b894bd6b13b

--- deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/ManualTests.java
+++ deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/ManualTests.java
@@ -1,13 +1,39 @@
 package org.deeplearning4j.ui;
 
+import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RBM;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.weights.HistogramIterationListener;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
+ * Test environment for building/debugging UI.
+ *
  * @author raver119@gmail.com
  */
 @Ignore
 public class ManualTests {
+
+    private static Logger log = LoggerFactory.getLogger(ManualTests.class);
 
     @Test
     public void testLaunch() throws Exception {
@@ -17,5 +43,74 @@ public class ManualTests {
         System.out.println("http://localhost:" + server.getPort()+ "/");
 
         Thread.sleep(10000000000L);
+
+        new ScoreIterationListener(100);
+    }
+
+    @Test
+    public void testHistograms() throws Exception {
+        final int numRows = 28;
+        final int numColumns = 28;
+        int outputNum = 10;
+        int numSamples = 60000;
+        int batchSize = 100;
+        int iterations = 10;
+        int seed = 123;
+        int listenerFreq = batchSize / 5;
+
+        log.info("Load data....");
+        DataSetIterator iter = new MnistDataSetIterator(batchSize,numSamples,true);
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
+                .gradientNormalizationThreshold(1.0)
+                .iterations(iterations)
+                .momentum(0.5)
+                .momentumAfter(Collections.singletonMap(3, 0.9))
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .list(4)
+                .layer(0, new RBM.Builder().nIn(numRows*numColumns).nOut(500)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(1, new RBM.Builder().nIn(500).nOut(250)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(2, new RBM.Builder().nIn(250).nOut(200)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).activation("softmax")
+                        .nIn(200).nOut(outputNum).build())
+                .pretrain(true).backprop(false)
+                .build();
+
+        ;
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq),(IterationListener) new HistogramIterationListener(listenerFreq)));
+
+        log.info("Train model....");
+        model.fit(iter); // achieves end to end pre-training
+
+        log.info("Evaluate model....");
+        Evaluation eval = new Evaluation(outputNum);
+
+        DataSetIterator testIter = new MnistDataSetIterator(100,10000);
+        while(testIter.hasNext()) {
+            DataSet testMnist = testIter.next();
+            INDArray predict2 = model.output(testMnist.getFeatureMatrix());
+            eval.eval(testMnist.getLabels(), predict2);
+        }
+
+        log.info(eval.stats());
+        log.info("****************Example finished********************");
     }
 }
