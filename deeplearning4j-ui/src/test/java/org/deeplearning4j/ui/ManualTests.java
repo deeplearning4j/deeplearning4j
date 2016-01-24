3eac62633574a1d22169676df71fe50eec9369f9
raver119 <raver119@gmail.com>
2016-01-24 13:58:52 +0300

    weights score scale impl
    Former-commit-id: 79bd182424242c8576509f60488b7a4f6770832a [formerly 81372080cdd89c6d30337c87c2fb836b165b887e]
    Former-commit-id: df3e66839219443b0b8010f0c3e178f85eb972a1

--- deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/ManualTests.java
+++ deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/ManualTests.java
@@ -13,6 +13,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.activation.UpdateActivationIterationListener;
 import org.deeplearning4j.ui.weights.HistogramIterationListener;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -96,6 +97,74 @@ public class ManualTests {
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
         model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq),(IterationListener) new HistogramIterationListener(listenerFreq)));
+
+        log.info("Train model....");
+        model.fit(iter); // achieves end to end pre-training
+
+        log.info("Evaluate model....");
+        Evaluation eval = new Evaluation(outputNum);
+
+        DataSetIterator testIter = new MnistDataSetIterator(100,10000);
+        while(testIter.hasNext()) {
+            DataSet testMnist = testIter.next();
+            INDArray predict2 = model.output(testMnist.getFeatureMatrix());
+            eval.eval(testMnist.getLabels(), predict2);
+        }
+
+        log.info(eval.stats());
+        log.info("****************Example finished********************");
+    }
+
+
+    @Test
+    public void testActivations() throws Exception {
+        final int numRows = 28;
+        final int numColumns = 28;
+        int outputNum = 10;
+        int numSamples = 60000;
+        int batchSize = 100;
+        int iterations = 10;
+        int seed = 123;
+        int listenerFreq = batchSize / 5;
+
+        log.info("Load data....");
+        DataSetIterator iter = new MnistDataSetIterator(batchSize,numSamples,true);
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
+                .gradientNormalizationThreshold(1.0)
+                .iterations(iterations)
+                .momentum(0.5)
+                .momentumAfter(Collections.singletonMap(3, 0.9))
+                .optimizationAlgo(OptimizationAlgorithm.CONJUGATE_GRADIENT)
+                .list(4)
+                .layer(0, new RBM.Builder().nIn(numRows*numColumns).nOut(500)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(1, new RBM.Builder().nIn(500).nOut(250)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(2, new RBM.Builder().nIn(250).nOut(200)
+                        .weightInit(WeightInit.XAVIER).lossFunction(LossFunctions.LossFunction.RMSE_XENT)
+                        .visibleUnit(RBM.VisibleUnit.BINARY)
+                        .hiddenUnit(RBM.HiddenUnit.BINARY)
+                        .build())
+                .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).activation("softmax")
+                        .nIn(200).nOut(outputNum).build())
+                .pretrain(true).backprop(false)
+                .build();
+
+        ;
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(Arrays.asList((IterationListener) new ScoreIterationListener(listenerFreq),(IterationListener) new UpdateActivationIterationListener(listenerFreq)));
 
         log.info("Train model....");
         model.fit(iter); // achieves end to end pre-training
