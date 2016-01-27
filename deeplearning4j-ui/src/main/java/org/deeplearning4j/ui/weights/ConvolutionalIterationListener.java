b6b0e4a6c7732f913cfbd9389d9773377c5fd43c
raver119 <raver119@gmail.com>
2016-01-27 21:28:17 +0300

    cnn visualization impl
    Former-commit-id: 1652c97a51fb06026279d121867a31abc491e019 [formerly 6c2c749540889e336fad2c26af2a3f3e2eb2a99e]
    Former-commit-id: e69730af86b4bae6ef57b4dae58b77942afa4ec6

--- deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/weights/ConvolutionalIterationListener.java
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/weights/ConvolutionalIterationListener.java
@@ -5,6 +5,7 @@ import org.canova.api.util.ClassPathResource;
 import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.layers.convolution.ConvolutionLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.ui.UiServer;
@@ -22,6 +23,7 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -107,13 +109,26 @@ public class ConvolutionalIterationListener implements IterationListener {
             int cnt = 0;
             Random rnd = new Random();
             MultiLayerNetwork l = (MultiLayerNetwork) model;
+            BufferedImage sourceImage = null;
             for (Layer layer: l.getLayers()) {
                 if (layer.type() == Layer.Type.CONVOLUTIONAL) {
                     INDArray output = layer.activate();
+                    int sampleDim = rnd.nextInt(output.shape()[0] - 1) + 1;
+                    if (cnt == 0) {
+                        INDArray inputs = ((ConvolutionLayer) layer).input();
+
+                        try {
+                            sourceImage = restoreRGBImage(inputs.tensorAlongDimension(sampleDim, 3, 2, 1));
+                            ImageIO.write( sourceImage,"png",new File("tmp/input_" + minibatchNum + ".png"));
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
 
 //                    log.info("Layer output shape: " + Arrays.toString(output.shape()));
 
-                    INDArray tad = output.tensorAlongDimension(rnd.nextInt(output.shape()[0] - 1) + 1, 3, 2, 1);
+                    INDArray tad = output.tensorAlongDimension(sampleDim, 3, 2, 1);
   //                  log.info("TAD(3,2,1) shape: " + Arrays.toString(tad.shape()));
 
                     tensors.add(tad);
@@ -121,7 +136,7 @@ public class ConvolutionalIterationListener implements IterationListener {
                     cnt++;
                 }
             }
-            BufferedImage render = rasterizeConvoLayers(tensors);
+            BufferedImage render = rasterizeConvoLayers(tensors, sourceImage);
             try {
                 File tempFile = File.createTempFile("cnn_activations",".png");
                 tempFile.deleteOnExit();
@@ -150,7 +165,7 @@ public class ConvolutionalIterationListener implements IterationListener {
      *
      * @param tensors3D list of tensors retrieved from convolution
      */
-    private BufferedImage rasterizeConvoLayers(@NonNull List<INDArray> tensors3D) {
+    private BufferedImage rasterizeConvoLayers(@NonNull List<INDArray> tensors3D, BufferedImage sourceImage) {
         int width = 0;
         int height = 0;
 
@@ -165,28 +180,49 @@ public class ConvolutionalIterationListener implements IterationListener {
         int numImages = shape[0];
         height = (shape[2]);
         width = (shape[1]);
-        log.info("Output image dimensions: {height: " + height + ", width: " + width + "}");
-        int maxHeight = (height + (border * 2 ) + padding_row) * numImages;
+//        log.info("Output image dimensions: {height: " + height + ", width: " + width + "}");
+        int maxHeight = 0; //(height + (border * 2 ) + padding_row) * numImages;
         int totalWidth = 0;
         int iOffset = 1;
 
-        Orientation orientation = Orientation.PORTRAIT;
+        Orientation orientation = Orientation.LANDSCAPE;
+        /*
+            for debug purposes we'll use portait only now
+         */
+        if (tensors3D.size() > 3) {
+            orientation = Orientation.PORTRAIT;
+        }
+
+
 
         List<BufferedImage> images = new ArrayList<>();
         for (int layer = 0; layer < tensors3D.size(); layer++) {
             INDArray tad = tensors3D.get(layer);
             int zoomed = 0;
 
-            BufferedImage image = renderMultipleImages(tad,maxHeight, width, height, orientation);
-            totalWidth += image.getWidth() + padding_col;
+            BufferedImage image = null;
+            if (orientation == Orientation.LANDSCAPE) {
+                maxHeight = (height + (border * 2 ) + padding_row) * numImages;
+                image = renderMultipleImagesLandscape(tad, maxHeight, width, height);
+                totalWidth += image.getWidth() + padding_col;
+            } else if (orientation == Orientation.PORTRAIT) {
+                totalWidth = (width + (border * 2) + padding_row) * numImages;
+                image = renderMultipleImagesPortrait(tad, totalWidth, width, height);
+                maxHeight += image.getHeight() + padding_col;
+            }
+
             images.add(image);
         }
 
+        if (orientation == Orientation.LANDSCAPE) {
+            // append some space for arrows
+            totalWidth += padding_col * 2;
+        } else if (orientation == Orientation.PORTRAIT) {
+            maxHeight += padding_col * 2;
+            maxHeight += sourceImage.getHeight() + (padding_col * 2);
+        }
 
-        // append some space for arrows
-        totalWidth += padding_col  * 2;
-
-        BufferedImage output = new BufferedImage(totalWidth, maxHeight, BufferedImage.TYPE_BYTE_GRAY);
+        BufferedImage output = new BufferedImage(totalWidth, maxHeight, BufferedImage.TYPE_INT_RGB);
         Graphics2D graphics2D = output.createGraphics();
 
         graphics2D.setPaint(bgColor);
@@ -199,18 +235,39 @@ public class ConvolutionalIterationListener implements IterationListener {
             We try to add nice flow arrow here
          */
         try {
-            ClassPathResource resource = new ClassPathResource("/arrow_sing.PNG");
-            ClassPathResource resource2 = new ClassPathResource("/arrow_mul.PNG");
 
-            singleArrow = ImageIO.read(resource.getInputStream());
-            multipleArrows = ImageIO.read(resource2.getInputStream());
-            if (orientation == Orientation.PORTRAIT) {
+            if (orientation == Orientation.LANDSCAPE) {
                 // if orientation is portrait, we should rotate arrows
-                singleArrow.createGraphics().rotate(90.0);
-                multipleArrows.createGraphics().rotate(90.0);
-            }
+                ClassPathResource resource = new ClassPathResource("/arrow_sing.PNG");
+                ClassPathResource resource2 = new ClassPathResource("/arrow_mul.PNG");
 
-                graphics2D.drawImage(singleArrow, (padding_col / 2) - (singleArrow.getWidth() / 2), (maxHeight / 2) - (singleArrow.getHeight() / 2), null);
+                singleArrow = ImageIO.read(resource.getInputStream());
+                multipleArrows = ImageIO.read(resource2.getInputStream());
+
+                graphics2D.drawImage(sourceImage, (padding_col / 2) - (sourceImage.getWidth() / 2),  (maxHeight / 2) - (sourceImage.getHeight() / 2), null );
+
+                graphics2D.setPaint(borderColor);
+                graphics2D.drawRect((padding_col / 2) - (sourceImage.getWidth() / 2), (maxHeight / 2) - (sourceImage.getHeight() / 2), sourceImage.getWidth(), sourceImage.getHeight());
+
+                iOffset += sourceImage.getWidth();
+
+                graphics2D.drawImage(singleArrow, iOffset + (padding_col / 2) - (singleArrow.getWidth() / 2), (maxHeight / 2) - (singleArrow.getHeight() / 2), null);
+            } else {
+                ClassPathResource resource = new ClassPathResource("/arrow_singi.PNG");
+                ClassPathResource resource2 = new ClassPathResource("/arrow_muli.PNG");
+
+                singleArrow = ImageIO.read(resource.getInputStream());
+                multipleArrows = ImageIO.read(resource2.getInputStream());
+
+                graphics2D.drawImage(sourceImage, (totalWidth / 2) - (sourceImage.getWidth() / 2),  (padding_col / 2) - (sourceImage.getHeight() / 2), null );
+
+                graphics2D.setPaint(borderColor);
+                graphics2D.drawRect((totalWidth / 2) - (sourceImage.getWidth() / 2), (padding_col / 2) - (sourceImage.getHeight() / 2), sourceImage.getWidth(), sourceImage.getHeight());
+
+                iOffset += sourceImage.getHeight();
+
+                graphics2D.drawImage(singleArrow,(totalWidth / 2) - (singleArrow.getWidth() / 2), iOffset + (padding_col / 2) - (singleArrow.getHeight() / 2), null);
+            }
             iOffset += padding_col;
         } catch (Exception e) {
             // if we can't load images - ignore them
@@ -237,21 +294,21 @@ public class ConvolutionalIterationListener implements IterationListener {
                         graphics2D.drawImage(multipleArrows, iOffset - (padding_col / 2) - (multipleArrows.getWidth() / 2), (maxHeight / 2) - (multipleArrows.getHeight() / 2), null);
                     } else {
                         // draw single arrow
-                        graphics2D.drawImage(singleArrow, iOffset - (padding_col / 2) - (singleArrow.getWidth() / 2), (maxHeight / 2) - (singleArrow.getHeight() / 2), null);
+                    //    graphics2D.drawImage(singleArrow, iOffset - (padding_col / 2) - (singleArrow.getWidth() / 2), (maxHeight / 2) - (singleArrow.getHeight() / 2), null);
                     }
                 }
             } else if (orientation == Orientation.PORTRAIT) {
                 // image grows from top to bottom
                 graphics2D.drawImage(curImage, 1, iOffset, null);
-                iOffset += curImage.getWidth() + padding_col;
+                iOffset += curImage.getHeight() + padding_col;
 
                 if (singleArrow != null && multipleArrows != null) {
                     if (i < images.size() - 1) {
                         // draw multiple arrows here
-                        graphics2D.drawImage(multipleArrows, iOffset - (padding_col / 2) - (multipleArrows.getWidth() / 2), (maxHeight / 2) - (multipleArrows.getHeight() / 2), null);
+                        graphics2D.drawImage(multipleArrows, (totalWidth / 2) - (multipleArrows.getWidth() / 2),  iOffset - (padding_col / 2) - (multipleArrows.getHeight() / 2) , null);
                     } else {
                         // draw single arrow
-                        graphics2D.drawImage(singleArrow, iOffset - (padding_col / 2) - (singleArrow.getWidth() / 2), (maxHeight / 2) - (singleArrow.getHeight() / 2), null);
+                     //   graphics2D.drawImage(singleArrow, (totalWidth / 2) - (singleArrow.getWidth() / 2),  iOffset - (padding_col / 2) - (singleArrow.getHeight() / 2) , null);
                     }
                 }
             }
@@ -260,12 +317,103 @@ public class ConvolutionalIterationListener implements IterationListener {
         return output;
     }
 
+
+    private BufferedImage renderMultipleImagesPortrait(INDArray tensor3D, int maxWidth, int zoomWidth, int zoomHeight) {
+        int border = 1;
+        int padding_row = 2;
+        int padding_col = 2;
+        int zoomPadding = 20;
+
+        int[] tShape = tensor3D.shape();
+
+        int numRows = tShape[0] / tShape[2];
+
+        int height = (numRows * (tShape[1] + border + padding_col)) + padding_col + zoomPadding + zoomWidth;
+
+        BufferedImage outputImage = new BufferedImage(maxWidth, height, BufferedImage.TYPE_BYTE_GRAY);
+        Graphics2D graphics2D = outputImage.createGraphics();
+
+        graphics2D.setPaint(bgColor);
+        graphics2D.fillRect(0, 0, outputImage.getWidth(), outputImage.getHeight());
+
+        int columnOffset = 0;
+        int rowOffset = 0;
+        int numZoomed = 0;
+        int limZoomed = 5;
+        int zoomSpan = maxWidth / limZoomed;
+
+        for (int z = 0; z < tensor3D.shape()[0]; z++) {
+
+            INDArray tad2D = tensor3D.tensorAlongDimension(z, 2, 1);
+
+            int rWidth = tad2D.shape()[0];
+            int rHeight = tad2D.shape()[1];
+
+            int loc_height = (rHeight) + (border * 2) + padding_row;
+            int loc_width = (rWidth) + (border * 2) + padding_col;
+
+
+
+            BufferedImage currentImage = renderImageGrayscale(tad2D);
+
+            /*
+                if resulting image doesn't fit into image, we should step to next columns
+             */
+            if (columnOffset + loc_width > maxWidth) {
+                rowOffset += loc_height;
+                columnOffset = 0;
+            }
+
+            /*
+                now we should place this image into output image
+            */
+
+            graphics2D.drawImage(currentImage, columnOffset+1, rowOffset + 1, null);
+
+
+            /*
+                draw borders around each image
+            */
+
+            graphics2D.setPaint(borderColor);
+            graphics2D.drawRect(columnOffset, rowOffset, tad2D.shape()[0], tad2D.shape()[1]);
+
+
+
+            /*
+                draw one of 3 zoomed images if we're not on first level
+            */
+
+            if (z % 7 == 0 && // zoom each 5th element
+                    z != 0 && // do not zoom 0 element
+                    numZoomed < limZoomed && // we want only few zoomed samples
+                    (rHeight != zoomHeight && rWidth != zoomWidth ) // do not zoom if dimensions match
+                    ) {
+
+                int cY = (zoomSpan * numZoomed) + (zoomHeight);
+                int cX = (zoomSpan * numZoomed) + (zoomWidth);
+
+                graphics2D.drawImage(currentImage, cX - 1 , height - zoomWidth - 1, zoomWidth, zoomHeight, null);
+                graphics2D.drawRect(cX - 2, height - zoomWidth - 2, zoomWidth, zoomHeight);
+
+                // draw line to connect this zoomed pic with its original
+                graphics2D.drawLine(columnOffset + rWidth, rowOffset + rHeight, cX - 2, height - zoomWidth - 2);
+                numZoomed++;
+
+            }
+
+            columnOffset += loc_width;
+        }
+
+        return outputImage;
+    }
+
     /**
      * This method renders 1 convolution layer as set of patches + multiple zoomed images
      * @param tensor3D
      * @return
      */
-    private BufferedImage renderMultipleImages(INDArray tensor3D, int maxHeight, int zoomWidth, int zoomHeight, Orientation orientation) {
+    private BufferedImage renderMultipleImagesLandscape(INDArray tensor3D, int maxHeight, int zoomWidth, int zoomHeight) {
         /*
             first we need to determine, weight of output image.
          */
@@ -276,13 +424,9 @@ public class ConvolutionalIterationListener implements IterationListener {
 
         int[] tShape = tensor3D.shape();
 
-        int numColumns = tShape[0] / tShape[2];
+        int numColumns = tShape[0] / tShape[1];
 
         int width = (numColumns * (tShape[1] + border + padding_col)) + padding_col + zoomPadding + zoomWidth;
-
-
-        List<Pair<Pair<Integer, Integer>, BufferedImage>> coords = new ArrayList<>();
-
 
         BufferedImage outputImage = new BufferedImage(width, maxHeight, BufferedImage.TYPE_BYTE_GRAY);
         Graphics2D graphics2D = outputImage.createGraphics();
@@ -323,10 +467,6 @@ public class ConvolutionalIterationListener implements IterationListener {
 
             graphics2D.drawImage(currentImage, columnOffset+1, rowOffset + 1, null);
 
-            /*
-                we save coords for each image, for use with zoom
-             */
-            coords.add(Pair.makePair(Pair.makePair(columnOffset, rowOffset), currentImage));
 
             /*
                 draw borders around each image
@@ -360,6 +500,29 @@ public class ConvolutionalIterationListener implements IterationListener {
             rowOffset += loc_height;
         }
         return outputImage;
+    }
+
+    /**
+     * Returns RGB image out of 3D tensor
+     *
+     * @param tensor3D
+     * @return
+     */
+    private BufferedImage restoreRGBImage(INDArray tensor3D) {
+
+        INDArray arrayR = tensor3D.tensorAlongDimension(2, 2, 1);
+        INDArray arrayG = tensor3D.tensorAlongDimension(1, 2, 1);
+        INDArray arrayB = tensor3D.tensorAlongDimension(0, 2, 1);
+
+        BufferedImage imageToRender = new BufferedImage(arrayR.columns(),arrayR.rows(),BufferedImage.TYPE_INT_RGB);
+        for( int x = 0; x < arrayR.columns(); x++ ){
+            for (int y = 0; y < arrayR.rows(); y++ ) {
+                Color pix = new Color((int) (255 * arrayR.getRow(y).getDouble(x)), (int) (255 * arrayG.getRow(y).getDouble(x)), (int) (255 * arrayB.getRow(y).getDouble(x)));
+                int rgb = pix.getRGB();
+                imageToRender.setRGB(x, y, rgb);
+            }
+        }
+        return imageToRender;
     }
 
     /**
