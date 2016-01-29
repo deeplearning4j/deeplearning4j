2cfccf0fd8e8372b59b56d1d315025fb0ba9ac8b
raver119 <raver119@gmail.com>
2016-01-30 00:21:34 +0300

    FlowIterationListener intro
    Former-commit-id: 5a708c540b27418b5bcf26f2dfdc2c43f15e49e5 [formerly cd080326c05b01b8b13dd3ad36a4fe01ccca82a9]
    Former-commit-id: 6fea5e260e32340439390b642e06496a512cfb59

---
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/weights/FlowIterationListener.java
@@ -0,0 +1,49 @@
+package org.deeplearning4j.ui.weights;
+
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.optimize.api.IterationListener;
+
+/**
+ * This IterationListener is suited for general model performance/architecture overview
+ *
+ * PLEASE NOTE: WORK IN PROGRESS, DO NOT USE IT
+ * @author raver119@gmail.com
+ */
+public class FlowIterationListener implements IterationListener {
+    /**
+     * Get if listener invoked
+     */
+    @Override
+    public boolean invoked() {
+        return false;
+    }
+
+    /**
+     * Change invoke to true
+     */
+    @Override
+    public void invoke() {
+
+    }
+
+    /**
+     * Event listener for each iteration
+     *
+     * @param model     the model iterating
+     * @param iteration the iteration
+     */
+    @Override
+    public void iterationDone(Model model, int iteration) {
+        /*
+            Basic plan:
+                1. We should detect, if that's CompGraph or MultilayerNetwork. However the actual difference will be limited to number of non-linear connections.
+                2. Network structure should be converted to JSON
+                3. Params for each node should be packed to JSON as well
+                4. For specific cases (like CNN) binary data should be wrapped into base64
+                5. For arrays/params gzip could be used (to be investigated)
+                ......
+                Later, on client side, this JSON should be parsed and rendered. So, proper object structure to be considered.
+         */
+
+    }
+}
