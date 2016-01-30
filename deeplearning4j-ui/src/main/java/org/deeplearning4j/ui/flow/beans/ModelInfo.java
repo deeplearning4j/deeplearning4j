0d5052b11b5910a96e0182dd4d09e536d4587aa2
raver119 <raver119@gmail.com>
2016-01-30 17:09:48 +0300

    flow vis p.0
    Former-commit-id: 7afaebcd642eeef49c6f4f010ad8ce7f766013fe

--- deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/flow/beans/ModelInfo.java
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/flow/beans/ModelInfo.java
@@ -7,24 +7,46 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * This bean works as holder for unbounded list of layers. Each layer has it's own place in model's virtual coordinate space
+ * This bean works as holder for unbounded list of layers. Each layer has it's own place in model's virtual coordinate space.
+ * For now, coordinate space is limited to 2 dimensions
  *
  * @author raver119@gmail.com
  */
 @Data
 public class ModelInfo implements Serializable {
     private long time;
+
+    // this should be table or map(pair(x,y)), but not a list
     private List<LayerInfo> layers;
 
+    /**
+     * This method maps given layer into model coordinate space
+     * @param layer
+     */
     public void addLayer(@NonNull LayerInfo layer) {
+        // TODO: implement 2D mapping here
         this.layers.add(layer);
     }
 
-    private LayerInfo getLayerInfoByName(String name) {
+    /**
+     * This method returns LayerInfo for specified layer name
+     * @param name
+     * @return
+     */
+    public LayerInfo getLayerInfoByName(String name) {
         for (LayerInfo layerInfo: layers) {
             if (layerInfo.getName().equalsIgnoreCase(name)) return layerInfo;
         }
+        return null;
+    }
 
+    /**
+     * This method returns LayerInfo for specified grid coordinates
+     * @param x
+     * @param y
+     * @return
+     */
+    public LayerInfo getLayerInfoByCoords(int x, int y) {
         return null;
     }
 }
---
+++ deeplearning4j-ui/src/main/java/org/deeplearning4j/ui/flow/beans/NodeReport.java
@@ -0,0 +1,17 @@
+package org.deeplearning4j.ui.flow.beans;
+
+import lombok.Data;
+
+/**
+ * Unified node state report: weights/gradients as distribution
+ *
+ * @author raver119@gmail.com
+ */
+@Data
+public class NodeReport {
+    /*
+        TODO: to be implemented
+
+        Basic idea: categorized distribution for weights/gradients built from INDArray, suitable for concurrent generation
+     */
+}
