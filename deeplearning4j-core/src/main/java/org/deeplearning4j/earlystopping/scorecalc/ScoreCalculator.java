83da338942ffdfa5518541de9c4c2796e346c82c
Alex Black <blacka101@gmail.com>
2016-01-31 22:03:59 +1100

    Refactor early stopping; add early stopping for ComputationGraph
    Former-commit-id: fc4661b3d57208d89601317905c5201e504cc2ab [formerly 5cc29641c435e1ebc1ff7c8821385eb408dade67]
    Former-commit-id: 0b7df8d9b070adf3465ce1cd2d483cdd82329b82

--- deeplearning4j-core/src/main/java/org/deeplearning4j/earlystopping/scorecalc/ScoreCalculator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/earlystopping/scorecalc/ScoreCalculator.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j.earlystopping.scorecalc;
 
+import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 
 import java.io.Serializable;
@@ -7,8 +8,8 @@ import java.io.Serializable;
 /** ScoreCalculator interface is used to calculate a score for a MultiLayerNetwork.
  * For example, the loss function, test set accuracy, F1, or some other (possibly custom) metric.
  */
-public interface ScoreCalculator extends Serializable {
+public interface ScoreCalculator<T extends Model> extends Serializable {
 
     /** Calculate the score for the given MultiLayerNetwork */
-    double calculateScore(MultiLayerNetwork network);
+    double calculateScore(T network);
 }
