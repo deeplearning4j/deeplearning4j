32587557e2c8f06142dd5fe29d52c84e0bf31903
nyghtowl <warrick.melanie@gmail.com>
2016-02-02 22:59:24 -0800

    Fixed forward and backprop and added tests. Need validation and review for improvements
    Former-commit-id: f1f6db7cabf065c7f5719ab154b4659bb4d59752 [formerly d83e0248bf6043a08fa0b019d0c6b577c42075a8]
    Former-commit-id: 9bac4dfc90982727fd58abb280aa609aa86a964a

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/MultiLayerTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/MultiLayerTest.java
@@ -37,6 +37,7 @@ import org.deeplearning4j.nn.conf.layers.BatchNormalization;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.ReshapePreProcessor;
+import org.deeplearning4j.nn.gradient.DefaultGradient;
 import org.deeplearning4j.nn.gradient.Gradient;
 import org.deeplearning4j.nn.layers.BaseOutputLayer;
 import org.deeplearning4j.nn.layers.factory.LayerFactories;
@@ -164,52 +165,6 @@ public class MultiLayerTest {
         network.fit(trainTest.getTrain());
 
     }
-
-    @Test
-    public void testBatchNormCNN() throws Exception {
-//        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-//                .optimizationAlgo(OptimizationAlgorithm.LINE_GRADIENT_DESCENT)
-//                .iterations(5)
-//                .seed(123)
-//                .list(4)
-//                .layer(0, new ConvolutionLayer.Builder().nIn(1).nOut(6).weightInit(WeightInit.XAVIER).activation("relu").build())
-//                .layer(1, new BatchNormalization.Builder().nIn(6).build())
-//                .layer(2, new DenseLayer.Builder().nIn(6).nOut(2).build())
-//                .layer(3, new org.deeplearning4j.nn.conf.layers.OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
-//                        .weightInit(WeightInit.XAVIER)
-//                        .activation("softmax")
-//                        .nIn(2).nOut(10).build())
-//                .backprop(true).pretrain(false)
-//                .cnnInputSize(28,28,1)
-//                .build();
-//        MultiLayerNetwork network = new MultiLayerNetwork(conf);
-//        network.init();
-//        network.fit(next);
-
-        BatchNormalization bN = new BatchNormalization.Builder().build();
-        NeuralNetConfiguration layerConf = new NeuralNetConfiguration.Builder()
-                .iterations(1).layer(bN).build();
-
-        Layer layer = LayerFactories.getFactory(layerConf).create(layerConf);
-
-        INDArray data = Nd4j.create(new double[] {
-                4.,4.,4.,4.,8.,8.,8.,8.,4.,4.,4.,4.,8.,8.,8.,8.,4.,4.
-                ,4.,4.,8.,8.,8.,8.,4.,4.,4.,4.,8.,8.,8.,8,
-                2.,2.,2.,2.,4.,4.,4.,4.,2.,2.,2.,2.,4.,4.,4.,4.,
-                2.,2.,2.,2.,4.,4.,4.,4.,2.,2.,2.,2.,4.,4.,4.,4.
-        },new int[]{2, 2, 4, 4});
-
-
-        INDArray t = data.var(0);
-
-        INDArray actualActivation = layer.preOutput(data);
-
-        DataSetIterator iter = new MnistDataSetIterator(5, 5);
-        DataSet next = iter.next();
-
-
-    }
-
 
     @Test
     public void testBackProp() {
