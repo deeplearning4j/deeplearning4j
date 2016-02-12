4dc4d65a8274cf213accd5bd366bb023d569844c
Alex Black <blacka101@gmail.com>
2016-02-12 23:43:58 +1100

    Fix x86 blas warning (warning previously always displayed, unless blas is forced)

--- nd4j-x86/src/main/java/org/nd4j/linalg/cpu/BlasWrapper.java
+++ nd4j-x86/src/main/java/org/nd4j/linalg/cpu/BlasWrapper.java
@@ -42,13 +42,11 @@ public class BlasWrapper extends BaseBlasWrapper {
     public final static String FORCE_NATIVE = "org.nd4j.linalg.cpu.force_native";
     static {
         String forceNative = System.getProperty(FORCE_NATIVE,"false");
-        boolean hasSetNative = false;
         if(Boolean.parseBoolean(forceNative)) {
             try {
                 Field blasInstance = BLAS.class.getDeclaredField("INSTANCE");
                 BLAS newInstance = (BLAS) Class.forName("com.github.fommil.netlib.NativeSystemBLAS").newInstance();
                 setFinalStatic(blasInstance, newInstance);
-                hasSetNative = true;
             } catch(ClassNotFoundException e) {
                 log.warn("Native BLAS not available on classpath");
             } catch (Exception e) {
@@ -56,12 +54,19 @@ public class BlasWrapper extends BaseBlasWrapper {
             }
         }
 
+        boolean usingNative = false;
+        try{
+            usingNative = BLAS.getInstance() instanceof com.github.fommil.netlib.NativeSystemBLAS;
+        }catch(Throwable t){
+            //May throw a NoClassDefFoundError
+        }
+
         //Check that native system blas is used:
-        if(!hasSetNative){
+        if(!usingNative){
             System.out.println("****************************************************************");
             System.out.println("WARNING: COULD NOT LOAD NATIVE SYSTEM BLAS");
-            System.out.println("ND4J performance WILL be reduced");
-            System.out.println("Please install native BLAS library such as OpenBLAS or IntelMKL");
+            System.out.println("ND4J computational performance WILL be reduced");
+            System.out.println("Please install native BLAS library such as OpenBLAS or Intel MKL");
             System.out.println("See http://nd4j.org/getstarted.html#open for further details");
             System.out.println("****************************************************************");
         }
