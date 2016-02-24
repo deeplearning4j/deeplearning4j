0d1145a79f14476cb0c2e005bf14464abbae4801
Alex Black <blacka101@gmail.com>
2016-02-24 22:43:53 +1100

    Switch step function warning log message from warn to debug

--- deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/solvers/BaseOptimizer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/optimize/solvers/BaseOptimizer.java
@@ -302,7 +302,7 @@ public abstract class BaseOptimizer implements ConvexOptimizer {
 
 
     public static StepFunction getDefaultStepFunctionForOptimizer( Class<? extends ConvexOptimizer> optimizerClass ){
-        log.warn("Objective function automatically set to minimize. Set stepFunction in neural net configuration to change default settings.");
+        log.debug("Objective function automatically set to minimize. Set stepFunction in neural net configuration to change default settings.");
         if( optimizerClass == StochasticGradientDescent.class ){
             return new NegativeGradientStepFunction();
         } else {
