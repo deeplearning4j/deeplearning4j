fadae33e4460a0a246c9db39f61c58f1f9360ecf
raver119 <raver119@gmail.com>
2016-02-25 18:11:51 +0300

    tsne p comp fixed

--- deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/plot/TsneTest.java
@@ -20,6 +20,7 @@ package org.deeplearning4j.plot;
 
 import org.apache.commons.io.IOUtils;
 import org.canova.api.util.ClassPathResource;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
@@ -36,19 +37,51 @@ public class TsneTest {
 
     @Test
     public void testTsne() throws Exception {
-        Tsne calculation = new Tsne.Builder().setMaxIter(1).usePca(false).setSwitchMomentumIteration(20)
-                .normalize(true).useAdaGrad(false).learningRate(500).perplexity(20).minGain(1e-1f)
+        Tsne calculation = new Tsne.Builder()
+                .setMaxIter(10)
+                .usePca(false)
+                .setSwitchMomentumIteration(20)
+                .normalize(false)
+                .useAdaGrad(false)
+                .learningRate(500)
+                .perplexity(20)
+                .minGain(1e-1f)
                 .build();
-        ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
-        File f = resource.getFile();
-        INDArray data = Nd4j.readNumpy(f.getAbsolutePath(),"   ").get(NDArrayIndex.interval(0,3));
+        //ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
+        File f = new File("/ext/mnist2500_X.txt");
+        INDArray data = Nd4j.readNumpy(f.getAbsolutePath(),"   ");//.get(NDArrayIndex.interval(0,600));
         ClassPathResource labels = new ClassPathResource("mnist2500_labels.txt");
         List<String> labelsList = IOUtils.readLines(labels.getInputStream()).subList(0,3);
 
-        calculation.plot(data,2, labelsList);
+        calculation.calculate(data, 2, 20);
 
 
     }
 
+    @Test
+    @Ignore
+    public void testExternalTsne() throws Exception {
+        ProperTsne calculation = new ProperTsne.Builder()
+                .setMaxIter(20)
+                .usePca(false)
+                .setSwitchMomentumIteration(20)
+                .normalize(false)
+                .useAdaGrad(false)
+                .learningRate(500)
+                .perplexity(20)
+                .minGain(1e-1f)
+                .perplexity(20)
+                .build();
+        //ClassPathResource resource = new ClassPathResource("/mnist2500_X.txt");
+        File f = new File("/ext/mnist2500_X.txt");
+        INDArray data = Nd4j.readNumpy(f.getAbsolutePath(),"   "); //.get(NDArrayIndex.interval(0,600));
+        ClassPathResource labels = new ClassPathResource("mnist2500_labels.txt");
+        List<String> labelsList = IOUtils.readLines(labels.getInputStream());//.subList(0,3);
+
+        //calculation.calculate(data, 2, 20);
+        calculation.plot(data, 2, labelsList, "/ext/Temp/coords.txt");
+
+
+    }
 
 }
