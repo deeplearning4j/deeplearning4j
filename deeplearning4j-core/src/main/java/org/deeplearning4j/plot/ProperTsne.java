10b7ffc19e12cfe50828d2d0fb268adbc12fa622
raver119 <raver119@gmail.com>
2016-02-25 18:11:51 +0300

    tsne p comp fixed
    Former-commit-id: ed026417182ebaa9123489ba9fd1599805a048b7 [formerly fadae33e4460a0a246c9db39f61c58f1f9360ecf]
    Former-commit-id: 91c0e899fc246bfcd2b084d6fa8d3757e301ebc1

--- deeplearning4j-core/src/main/java/org/deeplearning4j/plot/ProperTsne.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/plot/ProperTsne.java
@@ -1,14 +1,42 @@
 package org.deeplearning4j.plot;
 
+import com.google.common.primitives.Ints;
+import org.apache.commons.math3.util.FastMath;
+import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.rng.distribution.Distribution;
+import org.nd4j.linalg.api.rng.distribution.impl.NormalDistribution;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.SpecifiedIndex;
+import org.nd4j.linalg.indexing.conditions.Conditions;
+import org.nd4j.linalg.indexing.functions.Value;
+import org.nd4j.linalg.indexing.functions.Zero;
 import org.nd4j.linalg.io.ClassPathResource;
 import org.nd4j.linalg.learning.AdaGrad;
+import org.nd4j.linalg.util.ArrayUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.nd4j.linalg.factory.Nd4j.doAlongDiagonal;
+import static org.nd4j.linalg.factory.Nd4j.ones;
+import static org.nd4j.linalg.factory.Nd4j.zeros;
+import static org.nd4j.linalg.ops.transforms.Transforms.*;
+
 /**
+ * dl4j port of original t-sne algorithm described/implemented by van der Maaten and Hinton
+ *
+ * DECOMPOSED VERSION, DO NOT USE IT EVER
+ *
  * @author raver119@gmail.com
  */
 public class ProperTsne {
@@ -27,10 +55,11 @@ public class ProperTsne {
     protected AdaGrad adaGrad;
     protected boolean useAdaGrad = true;
     protected double perplexity = 30;
-    protected INDArray gains,yIncs;
-    protected INDArray y;
+    //protected INDArray gains,yIncs;
+    protected INDArray Y;
     protected transient IterationListener iterationListener;
-    protected static final Logger log = LoggerFactory.getLogger(Tsne.class);
+
+    protected static final Logger logger = LoggerFactory.getLogger(ProperTsne.class);
 
     protected ProperTsne() {
         ;
@@ -41,7 +70,244 @@ public class ProperTsne {
     }
 
     public void calculate(INDArray X, int targetDimensions, double perplexity) {
+        // pca hook
+        if (usePca) {
 
+        }
+
+        // normalization hook
+        if (normalize) {
+
+        }
+
+        int n = X.rows();
+        // FIXME: this is wrong, another distribution required here
+        Y = Nd4j.rand(new int[]{n, targetDimensions}, new NormalDistribution());
+        INDArray dY = Nd4j.zeros(n, targetDimensions);
+        INDArray iY = Nd4j.zeros(n, targetDimensions);
+        INDArray gains = Nd4j.ones(n, targetDimensions);
+
+        boolean stopLying = false;
+        System.out.println("Y:Shape is = " + Arrays.toString(Y.shape()));
+
+        // compute P-values
+        INDArray P = x2p(X, tolerance, perplexity);
+
+        // do training
+        for (int i = 0; i < maxIter; i++) {
+            INDArray sumY =  pow(Y, 2).sum(1);
+
+            //Student-t distribution
+            //also un normalized q
+            // also known as num in original implementation
+            INDArray qu = Y.mmul(
+                    Y.transpose())
+                    .muli(-2)
+                    .addiRowVector(sumY).transpose()
+                    .addiRowVector(sumY)
+                    .addi(1).rdivi(1);
+
+  //          doAlongDiagonal(qu,new Zero());
+
+            INDArray  Q =  qu.div(qu.sumNumber().doubleValue());
+            BooleanIndexing.applyWhere(Q, Conditions.lessThan(1e-12), new Value(1e-12));
+
+            INDArray PQ = P.sub(Q);
+            for (int y = 0; y < n; y++) {
+                INDArray sum1 = Nd4j.tile(PQ.getRow(i).mul(qu.getRow(i)), new int[]{Y.columns(), 1})
+                        .transpose().mul(Y.getRow(i).broadcast(Y.shape()).sub(y)).sum(0);
+                dY.putRow(i, sum1);
+            }
+
+            gains = gains.add(.2)
+                    .muli(dY.cond(Conditions.greaterThan(0)).neqi(iY.cond(Conditions.greaterThan(0))))
+                    .addi(gains.mul(0.8).muli(dY.cond(Conditions.greaterThan(0)).eqi(iY.cond(Conditions.greaterThan(0)))));
+
+
+            BooleanIndexing.applyWhere(gains, Conditions.lessThan(1e-12), new Value(1e-12));
+
+
+            INDArray gradChange = gains.mul(dY);
+
+            gradChange.muli(learningRate);
+
+            iY.muli(momentum).subi(gradChange);
+
+            double cost = P.mul(log(P.div(Q),false)).sumNumber().doubleValue();
+            logger.info("Iteration ["+ i +"] error is: [" + cost +"]");
+
+            Y.addi(iY);
+          //  Y.addi(iY).subiRowVector(Y.mean(0));
+            INDArray tiled = Nd4j.tile(Y.mean(0), new int[]{Y.rows(), 1});
+            Y.subi(tiled);
+
+            if (!stopLying && (i > maxIter / 2 || i >= stopLyingIteration)) {
+                P.divi(4);
+                stopLying = true;
+            }
+        }
+    }
+
+    public void plot(INDArray matrix, int nDims, List<String> labels, String path) throws IOException {
+
+        calculate(matrix,nDims,perplexity);
+
+        BufferedWriter write = new BufferedWriter(new FileWriter(new File(path),true));
+
+        for(int i = 0; i < Y.rows(); i++) {
+            if(i >= labels.size())
+                break;
+            String word = labels.get(i);
+            if(word == null)
+                continue;
+            StringBuffer sb = new StringBuffer();
+            INDArray wordVector = Y.getRow(i);
+            for(int j = 0; j < wordVector.length(); j++) {
+                sb.append(wordVector.getDouble(j));
+                if(j < wordVector.length() - 1)
+                    sb.append(",");
+            }
+
+            sb.append(",");
+            sb.append(word);
+            sb.append(" ");
+
+            sb.append("\n");
+            write.write(sb.toString());
+
+        }
+
+        write.flush();
+        write.close();
+    }
+
+    private INDArray square(INDArray X) {
+        INDArray F = X.dup();
+        for (int y = 0; y< F.rows(); y++) {
+            INDArray slice = F.slice(y);
+            for (int x = 0; x < F.columns(); x++) {
+                slice.putScalar(x, Math.pow(slice.getDouble(x), 2));
+            }
+        }
+        return F;
+    }
+
+    /**
+     * Computes a gaussian kernel
+     * given a vector of squared distance distances
+     *
+     * @param d the data
+     * @param beta
+     * @return
+     */
+    public Pair<Double,INDArray> hBeta(INDArray d,double beta) {
+        INDArray P =  exp(d.neg().muli(beta));
+        double sum = P.sumNumber().doubleValue();
+        double logSum = FastMath.log(sum);
+        Double H = logSum + beta * (d.mul(P).sumNumber().doubleValue()) / sum;
+        P.divi(sum + 1e-6);
+        return new Pair<>(H,P);
+    }
+
+    /**
+     * This method build probabilities for given source data
+     *
+     * @param X
+     * @param tolerance
+     * @param perplexity
+     * @return
+     */
+    private INDArray x2p(final INDArray X, double tolerance, double perplexity) {
+        int n = X.rows();
+        final INDArray p = zeros(n, n);
+        final INDArray beta =  ones(n, 1);
+        final double logU =  Math.log(perplexity);
+
+        INDArray sumX =  pow(X, 2).sum(1);
+
+        INDArray D = X.mmul(X.transpose()).muli(-2)
+                .addRowVector(sumX)
+                .transpose()
+                .addRowVector(sumX);
+
+        logger.info("Calculating probabilities of data similarities..");
+        for(int i = 0; i < n; i++) {
+            if(i % 500 == 0 && i > 0)
+                logger.info("Handled " + i + " records");
+
+            double betaMin = Double.NEGATIVE_INFINITY;
+            double betaMax = Double.POSITIVE_INFINITY;
+            int[] vals = Ints.concat(ArrayUtil.range(0,i),ArrayUtil.range(i + 1, n ));
+            INDArrayIndex[] range = new INDArrayIndex[]{new SpecifiedIndex(vals)};
+
+            INDArray row = D.slice(i).get(range);
+            Pair<Double,INDArray> pair =  hBeta(row,beta.getDouble(i));
+            //INDArray hDiff = pair.getFirst().sub(logU);
+            double hDiff = pair.getFirst() - logU;
+            int tries = 0;
+
+            //while hdiff > tolerance
+            while(Math.abs(hDiff) > tolerance && tries < 50) {
+                //if hdiff > 0
+                if(hDiff > 0) {
+                    betaMin = beta.getDouble(i);
+                    if(Double.isInfinite(betaMax))
+                        beta.putScalar(i,beta.getDouble(i) * 2.0);
+                    else
+                        beta.putScalar(i,(beta.getDouble(i) + betaMax) / 2.0);
+                } else {
+                    betaMax = beta.getDouble(i);
+                    if(Double.isInfinite(betaMin))
+                        beta.putScalar(i,beta.getDouble(i) / 2.0);
+                    else
+                        beta.putScalar(i,(beta.getDouble(i) + betaMin) / 2.0);
+                }
+
+                pair = hBeta(row,beta.getDouble(i));
+                hDiff = pair.getFirst() - logU;
+                tries++;
+            }
+        //    logger.info("numTries: " + tries);
+//            System.out.println("Row: " + Arrays.toString(pair.getSecond().data().asFloat()));
+            p.slice(i).put(range,pair.getSecond());
+
+//            System.out.println("Slice: " + Arrays.toString(p.slice(i).dup().data().asFloat()));
+        }
+
+        //scanND(p, 1e-40f, 1);
+
+        //dont need data in memory after
+        logger.info("Mean value of sigma " + sqrt(beta.rdiv(1)).mean(Integer.MAX_VALUE));
+        BooleanIndexing.applyWhere(p,Conditions.isNan(),new Value(1e-12));
+
+        //scanND(p, 1e-40f, 1);
+
+        //set 0 along the diagonal
+        INDArray permute = p.transpose();
+
+        INDArray pOut = p.add(permute);
+
+        pOut.divi(pOut.sumNumber().doubleValue());
+
+        BooleanIndexing.applyWhere(pOut,Conditions.lessThan(1e-12),new Value(1e-12));
+        //ensure no nans
+
+   //     scanND(pOut, 1e-40f, 1);
+
+        pOut.muli(4);
+
+        return pOut;
+    }
+
+    protected void scanND(INDArray array, double threshold, int op) {
+        for (int y = 0; y < array.rows(); y++) {
+            INDArray slice = array.slice(y);
+            //logger.info("Slice " + y + ": " + Arrays.toString(slice.data().asFloat()));
+            for (int x = 0; x < array.columns(); x++) {
+                if (slice.getDouble(x) > 0)
+                    logger.info("position ["+y + "," + x +"] is ["+slice.getDouble(x)+"]");
+            }
+        }
     }
 
     public static class Builder {
