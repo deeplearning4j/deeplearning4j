ea25e97ea6253b80a02b9bc249913946eac70756
raver119 <raver119@gmail.com>
2016-02-28 00:06:56 +0300

    UiConnectionInfo intro
    Former-commit-id: d5d7d5adaf7bcb09ac2f976fe86c06d105f74c17 [formerly c34b436f1e2a91c1089c24e3cb2ae599aeff0158]
    Former-commit-id: 853cce6cbf8d0f41171a26f5731a9a39131805ac

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/ui/UiConnectionInfo.java
@@ -0,0 +1,85 @@
+package org.deeplearning4j.ui;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.Random;
+
+/**
+ * POJO describing the location and credentials for DL4j UiServer instance
+ *
+ * @author raver119@gmail.com
+ */
+@Data
+public class UiConnectionInfo {
+    private long sessionId;
+    private String login;
+    private String password;
+    private String address;
+    private int port;
+    private String path;
+    private boolean useHttps;
+
+    public UiConnectionInfo() {
+        this.sessionId = new Random().nextLong();
+    }
+
+    public static class Builder {
+        private UiConnectionInfo info = new UiConnectionInfo();
+
+        /**
+         * This method allows you to specify sessionId for this UiConnectionInfo instance
+         *
+         * PLEASE NOTE: This is not recommended. Advised behaviour - keep it random, as is.
+         *
+         * @param sessionId
+         * @return
+         */
+        public Builder setSessionId(long sessionId) {
+            info.setSessionId(sessionId);
+            return this;
+        }
+
+        public Builder setLogin(String login) {
+            info.setLogin(login);
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            info.setPassword(password);
+            return this;
+        }
+
+        public Builder setAddress(@NonNull String address) {
+            info.setAddress(address);
+            return this;
+        }
+
+        public Builder setPort(int port) {
+            if (port <= 0) throw new IllegalStateException("UiServer port can't be <= 0");
+            info.setPort(port);
+            return this;
+        }
+
+        public Builder enableHttps(boolean reallyEnable) {
+            info.setUseHttps(reallyEnable);
+            return this;
+        }
+
+        /**
+         * If you're using UiServer as servlet, located not at root folder of webserver (i.e. http://yourdomain.com/somepath/webui/), you can set path here.
+         * For provided example path will be "/somepath/webui/"
+         *
+         * @param path
+         * @return
+         */
+        public Builder setPath(String path) {
+            info.setPath(path);
+            return this;
+        }
+
+        public UiConnectionInfo build() {
+            return info;
+        }
+    }
+}
