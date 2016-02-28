f4fecf7e1839e2e3702553de354d07e516bc0880
Adam Gibson <adam@skymind.io>
2016-02-28 15:08:38 -0800

    add clang compiler

--- buildnativeoperations.sh
+++ buildnativeoperations.sh
@@ -2,6 +2,17 @@
 
 export OMP_NUM_THREADS=1
 
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "RUNNING OSX CLANG" 
+    # Do something under Mac OS X platform
+    export CC=clang-omp++
+    export CXX=clang-omp++
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under Windows NT platform
+    echo "Running windows"
+fi
+
 if [ "$#" -lt 1 ]; then
     echo "Please specify an argument"
 else
