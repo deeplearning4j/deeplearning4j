02328e07c1c6f7890e76f45f6fa4b8e428522156
Adam Gibson <adam@skymind.io>
2016-03-04 22:47:09 -0800

    set var

--- cmake/FindOpenBLAS.cmake
+++ cmake/FindOpenBLAS.cmake
@@ -1,97 +1,30 @@
-if(WIN32)
-    if(DEFINED ENV{MINGW_HOME})
-        set(MINGW_HOME ENV{MINGW_HOME})
-        else()
-          message("NO MINGW HOME DEFINED USING C:\\MinGW")
-        set(MINGW_HOME C:/MinGW)
-    endif()
+SET(Open_BLAS_INCLUDE_SEARCH_PATHS
+        /usr/include
+        /usr/include/openblas
+        /usr/include/openblas-base
+        /usr/local/include
+        /usr/local/include/openblas
+        /usr/local/include/openblas-base
+        /opt/OpenBLAS/include
+        $ENV{OpenBLAS_HOME}
+        $ENV{OpenBLAS_HOME}/include
+        )
 
-    if(DEFINED ENV{MSYS_HOME})
-        set(MSYS_HOME ENV{MSYS_HOME})
-    else()
-        message("NO MSYS HOME DEFINED USING ${MINGW_HOME}/msys")
-        set(MSYS_HOME ${MINGW_HOME}/msys)
-    endif()
-
-    if(DEFINED ENV{MSYS_HOME})
-        set(MSYS_HOME ENV{MSYS_HOME})
-    else()
-        message("NO MSYS HOME DEFINED USING ${MINGW_HOME}/msys")
-        set(MSYS_HOME ${MINGW_HOME}/msys)
-    endif()
-
-
-    if(DEFINED ENV{MSYS_VERSION})
-        set(MSYS_VERSION ENV{MSYS_VERSION})
-    else()
-        message("NO MSYS VERSION DEFINED USING 1.0")
-        set(MSYS_VERSION 1.0)
-    endif()
-
-
-    if(DEFINED ENV{MSYS_ROOT})
-        set(MSYS_ROOT ENV{MSYS_ROOT})
-    else()
-        message("NO MSYS ROOT DEFINED USING ${MSYS_HOME}/${MSYS_VERSION}")
-        set(MSYS_ROOT ${MSYS_HOME}/${MSYS_VERSION})
-    endif()
-    SET(Open_BLAS_INCLUDE_SEARCH_PATHS
-            ${MSYS_ROOT}/usr/include
-            ${MSYS_ROOT}/usr/include/openblas
-            ${MSYS_ROOT}/usr/include/openblas-base
-            ${MSYS_ROOT}/usr/local/include
-            ${MSYS_ROOT}/usr/local/include/openblas
-            ${MSYS_ROOT}/usr/local/include/openblas-base
-            ${MSYS_ROOT}/opt/OpenBLAS/include
-            ${MSYS_ROOT}$ENV{OpenBLAS_HOME}
-            ${MSYS_ROOT}$ENV{OpenBLAS_HOME}/include
-            )
-
-    SET(Open_BLAS_LIB_SEARCH_PATHS
-            ${MSYS_ROOT}/lib/
-            ${MSYS_ROOT}/lib/openblas-base
-            ${MSYS_ROOT}/lib64/
-            ${MSYS_ROOT}/usr/lib
-            ${MSYS_ROOT}/usr/lib/openblas-base
-            ${MSYS_ROOT}/usr/lib64
-            ${MSYS_ROOT}/usr/local/lib
-            ${MSYS_ROOT}/usr/local/lib64
-            ${MSYS_ROOT}/opt/OpenBLAS/lib
-            ${MSYS_ROOT}/$ENV{OpenBLAS}cd
-            ${MSYS_ROOT}/$ENV{OpenBLAS}/lib
-#           ${MSYS_ROOT}/$ENV{OpenBLAS_HOME}
-            ${MSYS_ROOT}/$ENV{OpenBLAS_HOME}/lib
-            )
-    else()
-    SET(Open_BLAS_INCLUDE_SEARCH_PATHS
-            /usr/include
-            /usr/include/openblas
-            /usr/include/openblas-base
-            /usr/local/include
-            /usr/local/include/openblas
-            /usr/local/include/openblas-base
-            /opt/OpenBLAS/include
-            $ENV{OpenBLAS_HOME}
-            $ENV{OpenBLAS_HOME}/include
-            )
-
-    SET(Open_BLAS_LIB_SEARCH_PATHS
-            /lib/
-            /lib/openblas-base
-            /lib64/
-            /usr/lib
-            /usr/lib/openblas-base
-            /usr/lib64
-            /usr/local/lib
-            /usr/local/lib64
-            /opt/OpenBLAS/lib
-            $ENV{OpenBLAS}cd
-            $ENV{OpenBLAS}/lib
-            $ENV{OpenBLAS_HOME}
-            $ENV{OpenBLAS_HOME}/lib
-            )
-endif()
-message("SEARCHING INCLUDE  ${Open_BLAS_INCLUDE_SEARCH_PATHS} and LIB ${Open_BLAS_LIB_SEARCH_PATHS}a")
+SET(Open_BLAS_LIB_SEARCH_PATHS
+        /lib/
+        /lib/openblas-base
+        /lib64/
+        /usr/lib
+        /usr/lib/openblas-base
+        /usr/lib64
+        /usr/local/lib
+        /usr/local/lib64
+        /opt/OpenBLAS/lib
+        $ENV{OpenBLAS}cd
+        $ENV{OpenBLAS}/lib
+        $ENV{OpenBLAS_HOME}
+        $ENV{OpenBLAS_HOME}/lib
+        )
 
 FIND_PATH(OpenBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${Open_BLAS_INCLUDE_SEARCH_PATHS})
 FIND_LIBRARY(OpenBLAS_LIB NAMES openblas PATHS ${Open_BLAS_LIB_SEARCH_PATHS})
