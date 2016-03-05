0e467c2cd12a915cac1ea4590e902c4996c7a350
raver119 <raver119@gmail.com>
2016-03-05 14:29:36 +0300

    SeqVec graph support initial commit
    Former-commit-id: 25bfb87bb8bea28872ef411e8d5c79642739b333

---
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/graph/enums/NoEdgeHandling.java
@@ -0,0 +1,11 @@
+package org.deeplearning4j.models.sequencevectors.graph.enums;
+
+/**
+ * @author raver119@gmail.com
+ */
+public enum  NoEdgeHandling {
+    SELF_LOOP_ON_DISCONNECTED,
+    EXCEPTION_ON_DISCONNECTED,
+    PADDING_ON_DISCONNECTED,
+    CUTOFF_ON_DISCONNECTED
+}
--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/transformers/impl/GraphTransformer.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/transformers/impl/GraphTransformer.java
@@ -1,6 +1,12 @@
 package org.deeplearning4j.models.sequencevectors.transformers.impl;
 
+import lombok.NonNull;
+import org.deeplearning4j.models.sequencevectors.graph.IGraph;
+import org.deeplearning4j.models.sequencevectors.graph.enums.NoEdgeHandling;
+import org.deeplearning4j.models.sequencevectors.graph.enums.WalkMode;
+import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
+import scala.collection.Seq;
 
 import java.util.Iterator;
 
@@ -11,11 +17,47 @@ import java.util.Iterator;
  * WORK IS IN PROGRESS, DO NOT USE
  * @author raver119@gmail.com
  */
-public class GraphTransformer<T extends SequenceElement,V> implements Iterable<T> {
+public class GraphTransformer<T extends SequenceElement> implements Iterable<Sequence<T>> {
     // TODO: to be implemented
 
+    protected GraphTransformer() {
+        ;
+    }
+
     @Override
-    public Iterator<T> iterator() {
+    public Iterator<Sequence<T>> iterator() {
         return null;
+    }
+
+    public static class Builder<T extends SequenceElement> {
+        protected IGraph sourceGraph;
+        protected int walkLength = 5;
+        protected WalkMode walkMode = WalkMode.RANDOM;
+        protected NoEdgeHandling noEdgeHandling = NoEdgeHandling.EXCEPTION_ON_DISCONNECTED;
+
+        public Builder(IGraph<T, ?> sourceGraph) {
+            this.sourceGraph = sourceGraph;
+        }
+
+        public Builder<T> setNoEdgeHandling(@NonNull NoEdgeHandling handling) {
+            this.noEdgeHandling = handling;
+            return this;
+        }
+
+        public Builder<T> setWalkMode(@NonNull WalkMode walkMode) {
+            this.walkMode = walkMode;
+            return this;
+        }
+
+        public Builder<T> setWalkLength(int walkLength) {
+            this.walkLength = walkLength;
+            return this;
+        }
+
+        public GraphTransformer<T> build() {
+            GraphTransformer<T> transformer = new GraphTransformer<T>();
+
+            return transformer;
+        }
     }
 }
