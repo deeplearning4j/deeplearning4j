dddc24743ce437e0e33450ca7503d04182a244e2
Adam Gibson <adam@skymind.io>
2016-03-05 21:59:50 -0800

    update cmake

--- include/dll.h
+++ include/dll.h
@@ -4,13 +4,4 @@
 
 #ifndef NATIVEOPERATIONS_DLL_H
 #define NATIVEOPERATIONS_DLL_H
-#ifdef _WIN32
-#    ifdef LIBRARY_EXPORTS
-#        define LIBRARY_API __declspec(dllexport)
-#    else
-#        define LIBRARY_API __declspec(dllimport)
-#    endif
-#elseif
-#    define LIBRARY_API
-#endif
 #endif //NATIVEOPERATIONS_DLL_H
