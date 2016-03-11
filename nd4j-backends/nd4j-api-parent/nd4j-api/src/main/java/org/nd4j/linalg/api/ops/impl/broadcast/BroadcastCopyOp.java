5e6cb47e92bc6b7b92142dd87a823c5b1b3cd5ce
Adam Gibson <adam@skymind.io>
2016-03-11 14:17:59 -0800

    new im2ol/col2im

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/broadcast/BroadcastCopyOp.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/broadcast/BroadcastCopyOp.java
@@ -5,6 +5,9 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.BaseBroadcastOp;
 import org.nd4j.linalg.factory.Nd4j;
 
+/**
+ *
+ */
 public class BroadcastCopyOp extends BaseBroadcastOp {
 
     public BroadcastCopyOp() {
