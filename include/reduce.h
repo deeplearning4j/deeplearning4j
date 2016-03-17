e9804ffb802a8f0c6d492d97acdd76954739825e
Adam Gibson <adam@skymind.io>
2016-03-17 15:04:45 -0700

    merge

--- include/reduce.h
+++ include/reduce.h
@@ -849,8 +849,6 @@ void exec(T *x,
         if(shape::order(xShapeInfo) == 'f') {
             int tadElementWiseStride = shape::reductionIndexElementWiseStride(xShapeInfo, dimension, dimensionLength);
             int tadLength = shape::tadLength(xShapeInfo, dimension, dimensionLength);
-          
-
 #pragma omp parallel for
             for(int i = 0;  i < resultLength; i++) {
                 int baseOffset = shape::tadOffset(i,xShapeInfo,dimension,dimensionLength);
