a526aa018bd6df93096e0886b6f70beb97fb89d2
Alex Black <blacka101@gmail.com>
2016-03-18 15:13:05 +1100

    Use preprocessor when required for MultiLayerNetwork.score(DataSet)
    Former-commit-id: 0eb94ee8857fa9f144ff6e43c46d4608807de3fd [formerly 7e5a145a8b613d79247ad1eb01fe7cf7614aa2da]
    Former-commit-id: a5d674916292d824b1deeaf71865201d017a699d

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/MultiLayerTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/MultiLayerTest.java
@@ -656,6 +656,39 @@ public class MultiLayerTest {
     }
 
     @Test
+    public void testDataSetScoreCNN(){
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .regularization(false)
+                .learningRate(1.0)
+                .seed(12345L)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(2,2).nOut(3).build())
+                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation("softmax").nOut(2).build())
+                .cnnInputSize(3,3,2)
+                .pretrain(false).backprop(true).build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+
+        int miniBatch = 3;
+        int depth = 2;
+        int width = 3;
+        int height = 3;
+        int nOut = 2;
+
+        Nd4j.getRandom().setSeed(12345);
+        Random r = new Random(12345);
+        INDArray input = Nd4j.rand(miniBatch,depth*width*height);
+        INDArray labels = Nd4j.create(miniBatch,nOut);
+        for( int i=0; i<miniBatch; i++ ){
+            labels.putScalar(new int[]{i,r.nextInt(nOut)},1.0);
+        }
+
+        double score = net.score(new DataSet(input,labels));
+    }
+
+    @Test
     @Ignore
     public void testCid() throws Exception {
         System.out.println(EnvironmentUtils.buildCId());
