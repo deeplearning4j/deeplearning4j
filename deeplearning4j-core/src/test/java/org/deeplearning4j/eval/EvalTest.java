b02384eec1c4c2cf2cd730f8cbe058d6656f2966
Alex Black <blacka101@gmail.com>
2016-03-26 22:48:02 +1100

    Rework evaluation to support binary (single output variable) case
    Former-commit-id: a6cf160fafd7b0836f13a7ef780d954883656a36

--- deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/eval/EvalTest.java
@@ -399,4 +399,29 @@ public class EvalTest {
         assertEquals(evalExpected.falseAlarmRate(),evalActual.falseAlarmRate(), 1e-3);
         assertEquals(evalExpected.getConfusionMatrix(), evalActual.getConfusionMatrix());
     }
+
+
+    @Test
+    public void testSingleClassBinaryClassification(){
+
+        Evaluation eval = new Evaluation(1);
+
+        INDArray zero = Nd4j.create(1);
+        INDArray one = Nd4j.ones(1);
+
+        //One incorrect, three correct
+        eval.eval(one,zero);
+        eval.eval(one,one);
+        eval.eval(one,one);
+        eval.eval(zero,zero);
+
+        System.out.println(eval.stats());
+
+        assertEquals(0.75, eval.accuracy(), 1e-6);
+        assertEquals(4, eval.getNumRowCounter());
+
+        assertEquals(1, (int)eval.truePositives().get(0));
+        assertEquals(2, (int)eval.truePositives().get(1));
+        assertEquals(1, (int)eval.falseNegatives().get(1));
+    }
 }
