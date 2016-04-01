9c07369ef7c33f4f8e34ebbccb8af1440d6ceb50
raver119@gmail.com <raver119@gmail.com>
2016-04-01 12:07:59 +0300

    few more gemv tests

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/blas/Level2Test.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/blas/Level2Test.java
@@ -65,4 +65,52 @@ public class Level2Test {
         assertEquals(3348150f, array3.getFloat(2), 0.001f);
         assertEquals(3353200f, array3.getFloat(3), 0.001f);
     }
+
+    @Test
+    public void testGemv5() throws Exception {
+        INDArray array1 = Nd4j.linspace(1, 1000, 1000).reshape(10, 100);
+        INDArray array2 = Nd4j.linspace(1,100, 100).reshape(100,1);
+
+        INDArray array3 = Nd4j.create(10);
+
+        array1.mmul(array2, array3);
+
+        assertEquals(10, array3.length());
+        assertEquals(338350f, array3.getFloat(0), 0.001f);
+        assertEquals(843350f, array3.getFloat(1), 0.001f);
+        assertEquals(1348350f, array3.getFloat(2), 0.001f);
+        assertEquals(1853350f, array3.getFloat(3), 0.001f);
+    }
+
+    @Test
+    public void testGemv6() throws Exception {
+        INDArray array1 = Nd4j.linspace(1, 1000, 1000).reshape('f', 10, 100);
+        INDArray array2 = Nd4j.linspace(1,100, 100).reshape(100,1);
+
+        INDArray array3 = Nd4j.create(10);
+
+        array1.mmul(array2, array3);
+
+        assertEquals(10, array3.length());
+        assertEquals(3338050f, array3.getFloat(0), 0.001f);
+        assertEquals(3343100f, array3.getFloat(1), 0.001f);
+        assertEquals(3348150f, array3.getFloat(2), 0.001f);
+        assertEquals(3353200f, array3.getFloat(3), 0.001f);
+    }
+
+    @Test
+    public void testGemv7() throws Exception {
+        INDArray array1 = Nd4j.linspace(1, 1000, 1000).reshape('f', 10, 100);
+        INDArray array2 = Nd4j.linspace(1,100, 100).reshape(100,1);
+
+        INDArray array3 = Nd4j.create(10);
+
+        array1.mmul(array2, array3);
+
+        assertEquals(10, array3.length());
+        assertEquals(3338050f, array3.getFloat(0), 0.001f);
+        assertEquals(3343100f, array3.getFloat(1), 0.001f);
+        assertEquals(3348150f, array3.getFloat(2), 0.001f);
+        assertEquals(3353200f, array3.getFloat(3), 0.001f);
+    }
 }
