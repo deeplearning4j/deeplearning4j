655aba48d5ce51b1b1fb8467575c7fcf4bca6b8e
nyghtowl <warrick.melanie@gmail.com>
2016-04-08 08:22:09 -0700

    Added tests for cnn computation graph
    Former-commit-id: a84ba14aa82d8c401d4273b1ca8158e09d27fc07 [formerly 7f1a954b53a1a90f395d247e7f654c0f32c824a6]
    Former-commit-id: 0206e1cfd57e9e4d89c7b67f1cce663620fec9ad

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/graph/TestCompGraphMulti.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/graph/TestCompGraphMulti.java
@@ -9,11 +9,14 @@ import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.conf.layers.setup.ConvolutionLayerSetup;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToCnnPreProcessor;
 import org.deeplearning4j.nn.gradient.Gradient;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -40,15 +43,15 @@ public class TestCompGraphMulti {
     protected DataSetIterator cifar;
     protected DataSet ds;
 
-    protected static ComputationGraphConfiguration getMultiInputGraphConfig(){
+    protected static ComputationGraphConfiguration getMultiInputGraphConfig() {
         return new NeuralNetConfiguration.Builder()
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .graphBuilder()
                 .addInputs("input")
-                .addLayer("cnn1", new ConvolutionLayer.Builder(4,4).stride(2,2).nIn(1).nOut(3).build(), "input")
-                .addLayer("cnn2", new ConvolutionLayer.Builder(4,4).stride(2,2).nIn(1).nOut(3).build(), "input")
+                .addLayer("cnn1", new ConvolutionLayer.Builder(4, 4).stride(2, 2).nIn(1).nOut(3).build(), "input")
+                .addLayer("cnn2", new ConvolutionLayer.Builder(4, 4).stride(2, 2).nIn(1).nOut(3).build(), "input")
                 .addLayer("max1", new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX).kernelSize(2, 2).build(), "cnn1", "cnn2")
-                .addLayer("dnn1", new DenseLayer.Builder().nIn(15*15*3).nOut(7).build(), "max1")
+                .addLayer("dnn1", new DenseLayer.Builder().nIn(15 * 15 * 3).nOut(7).build(), "max1")
                 .addLayer("max2", new SubsamplingLayer.Builder().build(), "max1")
                 .addLayer("output", new OutputLayer.Builder().nIn(7).nOut(10).build(), "dnn1", "max2")
                 .setOutputs("output")
@@ -60,16 +63,16 @@ public class TestCompGraphMulti {
     }
 
     protected static DataSetIterator getDS() {
-        return new CifarDataSetIterator(5,5);
+        return new CifarDataSetIterator(5, 5);
     }
 
-    protected static int getNumParams(){
-        return (3*1*4*4+3) + (3*1*4*4+3) + (7*15*15*3+7) + (7*10+10);
+    protected static int getNumParams() {
+        return (3 * 1 * 4 * 4 + 3) + (3 * 1 * 4 * 4 + 3) + (7 * 15 * 15 * 3 + 7) + (7 * 10 + 10);
     }
 
     @Before
     @Ignore
-    public void beforeDo(){
+    public void beforeDo() {
         conf = getMultiInputGraphConfig();
         graph = new ComputationGraph(conf);
         graph.init();
@@ -81,38 +84,38 @@ public class TestCompGraphMulti {
 
     @Test
     @Ignore
-    public void testConfigBasic(){
+    public void testConfigBasic() {
 
         int[] order = graph.topologicalSortOrder();
-        int[] expOrder = new int[]{0,1,3,7,6,4,5,8,2};
-        assertArrayEquals(expOrder,order);  //Only one valid order: 0 (input) -> 1 (firstlayer) -> 2 (outputlayer)
+        int[] expOrder = new int[]{0, 1, 3, 7, 6, 4, 5, 8, 2};
+        assertArrayEquals(expOrder, order);  //Only one valid order: 0 (input) -> 1 (firstlayer) -> 2 (outputlayer)
 
         INDArray params = graph.params();
         assertNotNull(params);
 
         int nParams = getNumParams();
-        assertEquals(nParams,params.length());
+        assertEquals(nParams, params.length());
 
         INDArray arr = Nd4j.linspace(0, nParams, nParams);
-        assertEquals(nParams,arr.length());
+        assertEquals(nParams, arr.length());
 
         graph.setParams(arr);
         params = graph.params();
-        assertEquals(arr,params);
+        assertEquals(arr, params);
 
         //Number of inputs and outputs:
-        assertEquals(1,graph.getNumInputArrays());
-        assertEquals(1,graph.getNumOutputArrays());
+        assertEquals(1, graph.getNumInputArrays());
+        assertEquals(1, graph.getNumOutputArrays());
 
     }
 
     @Test
     @Ignore
-    public void testForwardBasic(){
+    public void testForwardBasic() {
 
         graph.setInput(0, ds.getFeatureMatrix());
         // TODO issue with CNN preOutput z calc in tensorMatrixMul - need to review using weights 5*1*2*2 and col includes 16 x 16 h & w
-        Map<String,INDArray> activations = graph.feedForward(true);
+        Map<String, INDArray> activations = graph.feedForward(true);
         assertEquals(7, activations.size()); //2 layers + 1 input node
         assertTrue(activations.containsKey("input"));
         assertTrue(activations.containsKey("cnn1"));
@@ -124,7 +127,7 @@ public class TestCompGraphMulti {
 
     @Test
     @Ignore
-    public void testBackwardIrisBasic(){
+    public void testBackwardIrisBasic() {
 
         //Now: set parameters of both networks to be identical. Then feedforward, and check we get the same outputs
         Nd4j.getRandom().setSeed(12345);
@@ -136,14 +139,14 @@ public class TestCompGraphMulti {
 
         //Compute gradients
         graph.computeGradientAndScore();
-        Pair<Gradient,Double> graphGradScore = graph.gradientAndScore();
+        Pair<Gradient, Double> graphGradScore = graph.gradientAndScore();
 
         // Check gradients
     }
 
     @Test
     @Ignore
-    public void testEvaluation(){
+    public void testEvaluation() {
         Evaluation evalExpected = new Evaluation();
         // TODO setup graph output evaluation
 //        INDArray out = graph.output(ds.getFeatureMatrix(), Layer.TrainingMode.TEST);
@@ -151,6 +154,109 @@ public class TestCompGraphMulti {
 
         // Check evaluation results
 
+    }
+
+    @Test
+    public void testCNNComputationGraph() {
+        int imageWidth = 23;
+        int imageHeight = 19;
+        int nChannels = 1;
+        int classes = 2;
+        int numSamples = 200;
+
+        int kernelHeight = 3;
+        int kernelWidth = 3;
+
+
+        DataSet trainInput;
+
+        ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .iterations(10)
+                .seed(123)
+                .graphBuilder()
+                .addInputs("input")
+                .setInputTypes(InputType.convolutional(nChannels, imageWidth, imageHeight))
+                .addLayer("conv1", new ConvolutionLayer.Builder()
+                        .kernelSize(kernelHeight, kernelWidth)
+                        .stride(1, 1)
+                        .nIn(nChannels)
+                        .nOut(2)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation("relu")
+                        .build(), "input")
+                .addLayer("pool1", new SubsamplingLayer.Builder()
+                        .poolingType(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(imageHeight - kernelHeight + 1, 1)
+                        .stride(1, 1)
+                        .build(), "conv1")
+                .addLayer("output", new OutputLayer.Builder()
+                        .nOut(classes)
+                        .build(), "pool1")
+                .setOutputs("output")
+                .backprop(true)
+                .pretrain(false)
+                .build();
+
+
+        ComputationGraph model = new ComputationGraph(conf);
+        model.init();
+
+
+        INDArray emptyFeatures = Nd4j.zeros(numSamples, imageWidth * imageHeight * nChannels);
+        INDArray emptyLables = Nd4j.zeros(numSamples, classes);
+
+        trainInput = new DataSet(emptyFeatures, emptyLables);
+
+        model.fit(trainInput);
+    }
+
+    @Test
+    public void testCNNStruct(){
+        int imageWidth= 23;
+        int imageHeight= 20;
+        int nChannels = 1;
+        int classes = 2;
+        int numSamples = 200;
+
+        int kernelHeight = 3;
+        int kernelWidth = imageWidth;
+
+        DataSet trainInput;
+        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder()
+                .seed(123)
+                .iterations(1)
+                .list()
+                .layer(0, new ConvolutionLayer.Builder(kernelHeight, kernelWidth)
+                        .stride(1,1)
+                        .nOut(4)
+                        .activation("relu")
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new SubsamplingLayer.Builder()
+                        .poolingType(SubsamplingLayer.PoolingType.MAX)
+                        .kernelSize(imageHeight-kernelHeight+1,1)
+                        .stride(1,1)
+                        .build())
+                .layer(2, new OutputLayer.Builder()
+                        .nOut(classes)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation("softmax")
+                        .build())
+                .backprop(true).pretrain(false);
+        new ConvolutionLayerSetup(builder,imageHeight,imageWidth,nChannels);
+
+        MultiLayerConfiguration conf = builder.build();
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+
+        INDArray emptyFeatures = Nd4j.zeros(numSamples,imageWidth*imageHeight*nChannels);
+        INDArray emptyLables = Nd4j.zeros(numSamples,classes);
+
+        trainInput = new DataSet(emptyFeatures,emptyLables);
+
+        model.fit(trainInput);
     }
 
 }
