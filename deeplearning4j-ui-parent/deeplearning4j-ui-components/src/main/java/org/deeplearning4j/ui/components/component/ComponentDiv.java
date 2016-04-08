ac4305e52d69ebb224d0f33adb4acf9ba1817c50
Alex Black <blacka101@gmail.com>
2016-04-08 15:52:14 +1000

    Final review + add copyright
    Former-commit-id: bdae15aeecbc6804ee9debf4c3760aef3ba86300

--- deeplearning4j-ui-parent/deeplearning4j-ui-components/src/main/java/org/deeplearning4j/ui/components/component/ComponentDiv.java
+++ deeplearning4j-ui-parent/deeplearning4j-ui-components/src/main/java/org/deeplearning4j/ui/components/component/ComponentDiv.java
@@ -1,13 +1,31 @@
+/*
+ *
+ *  * Copyright 2016 Skymind,Inc.
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
 package org.deeplearning4j.ui.components.component;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.deeplearning4j.ui.api.Component;
 import org.deeplearning4j.ui.api.Style;
 
 /**
- * Created by Alex on 7/04/2016.
+ * Div component (as in, HTML div)
+ *
+ * @author Alex Black
  */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
