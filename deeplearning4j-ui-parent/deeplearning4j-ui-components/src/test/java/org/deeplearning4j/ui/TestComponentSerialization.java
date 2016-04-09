0381fe6fb1ace06a0927baf9bd7a334d531ebe57
Alex Black <blacka101@gmail.com>
2016-04-09 23:33:28 +1000

    Fixes and additional testing for UI JSON serialization/deserialization
    Former-commit-id: 64612b85f06636c4258c090eec67cdfbf334752a [formerly 256dde63ea1d07c67ec0167d8a079170ffc524c9]
    Former-commit-id: f16997d69c527f9097d954ae661b1598dfd1ec03

--- deeplearning4j-ui-parent/deeplearning4j-ui-components/src/test/java/org/deeplearning4j/ui/TestComponentSerialization.java
+++ deeplearning4j-ui-parent/deeplearning4j-ui-components/src/test/java/org/deeplearning4j/ui/TestComponentSerialization.java
@@ -1,27 +1,9 @@
-/*
- *
- *  * Copyright 2016 Skymind,Inc.
- *  *
- *  *    Licensed under the Apache License, Version 2.0 (the "License");
- *  *    you may not use this file except in compliance with the License.
- *  *    You may obtain a copy of the License at
- *  *
- *  *        http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  *    Unless required by applicable law or agreed to in writing, software
- *  *    distributed under the License is distributed on an "AS IS" BASIS,
- *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *    See the License for the specific language governing permissions and
- *  *    limitations under the License.
- *
- */
 package org.deeplearning4j.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.annotations.Ignore;
-import org.apache.commons.io.FileUtils;
-import org.deeplearning4j.ui.api.*;
 import org.deeplearning4j.ui.api.Component;
+import org.deeplearning4j.ui.api.LengthUnit;
+import org.deeplearning4j.ui.api.Style;
 import org.deeplearning4j.ui.components.chart.ChartHistogram;
 import org.deeplearning4j.ui.components.chart.ChartLine;
 import org.deeplearning4j.ui.components.chart.ChartScatter;
@@ -37,25 +19,17 @@ import org.deeplearning4j.ui.components.text.ComponentText;
 import org.deeplearning4j.ui.components.text.style.StyleText;
 import org.junit.Test;
 
-import java.awt.Color;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.awt.*;
 
+import static org.junit.Assert.assertEquals;
 
 /**
- * This test: generated a HTML file that you can open to view some example graphs.
- * The generated HTML file should appear in the deeplearning4j-ui-components directory (TestRendering.html)
- * *** NOTE: Open this in IntelliJ: Right click on file -> Open In Browser ***
+ * Created by Alex on 9/04/2016.
  */
-public class TestRendering {
+public class TestComponentSerialization {
 
-    @Ignore
     @Test
-    public void test() throws Exception {
-
-        List<Component> list = new ArrayList<>();
+    public void testSerialization() throws Exception {
 
         //Common style for all of the charts
         StyleChart s = new StyleChart.Builder()
@@ -67,6 +41,8 @@ public class TestRendering {
                 .seriesColors(Color.GREEN, Color.MAGENTA)
                 .titleStyle(new StyleText.Builder().font("courier").fontSize(16).underline(true).color(Color.GRAY).build())
                 .build();
+        assertSerializable(s);
+
 
         //Line chart with vertical grid
         Component c1 = new ChartLine.Builder("Line Chart!",s)
@@ -74,7 +50,7 @@ public class TestRendering {
                 .addSeries("series1", new double[]{0,1,2,3}, new double[]{0,1,0.5,2.5})
                 .setGridWidth(1.0,null) //Vertical grid lines, no horizontal grid
                 .build();
-        list.add(c1);
+        assertSerializable(c1);
 
         //Scatter chart
         Component c2 = new ChartScatter.Builder("Scatter!",s)
@@ -82,7 +58,7 @@ public class TestRendering {
                 .showLegend(true)
                 .setGridWidth(0,0)
                 .build();
-        list.add(c2);
+        assertSerializable(c2);
 
         //Histogram with variable sized bins
         Component c3 = new ChartHistogram.Builder("Histogram!",s)
@@ -91,7 +67,7 @@ public class TestRendering {
                 .addBin(0,1,2.5)
                 .addBin(1,2,0.5)
                 .build();
-        list.add(c3);
+        assertSerializable(c3);
 
         //Stacked area chart
         Component c4 = new ChartStackedArea.Builder("Area Chart!",s)
@@ -99,7 +75,7 @@ public class TestRendering {
                 .addSeries("series0",new double[]{0,1,0,2,0,1})
                 .addSeries("series1",new double[]{2,1,2,0.5,2,1})
                 .build();
-        list.add(c4);
+        assertSerializable(c4);
 
         //Table
         StyleTable ts = new StyleTable.Builder()
@@ -110,6 +86,7 @@ public class TestRendering {
                 .width(500, LengthUnit.Px)
                 .height(200, LengthUnit.Px)
                 .build();
+        assertSerializable(ts);
 
         Component c5 = new ComponentTable.Builder(ts)
                 .header("H1","H2","H3")
@@ -117,22 +94,24 @@ public class TestRendering {
                         {"row0col0", "row0col1", "row0col2"},
                         {"row1col0", "row1col1", "row1col2"}
                 }).build();
-        list.add(c5);
+        assertSerializable(c5);
 
         //Accordion decorator, with the same chart
         StyleAccordion ac = new StyleAccordion.Builder()
                 .height(480,LengthUnit.Px).width(640,LengthUnit.Px).build();
+        assertSerializable(ac);
 
         Component c6 = new DecoratorAccordion.Builder(ac)
                 .title("Accordion - Collapsed By Default!")
                 .setDefaultCollapsed(true)
                 .addComponents(c5)
                 .build();
-        list.add(c6);
+        assertSerializable(c6);
 
         //Text with styling
         Component c7 = new ComponentText.Builder("Here's some blue text in a green div!",
                 new StyleText.Builder().font("courier").fontSize(30).underline(true).color(Color.BLUE).build()).build();
+        assertSerializable(c7);
 
         //Div, with a chart inside
         Style divStyle = new StyleDiv.Builder()
@@ -140,53 +119,31 @@ public class TestRendering {
                 .backgroundColor(Color.GREEN)
                 .floatValue(StyleDiv.FloatValue.right)
                 .build();
+        assertSerializable(divStyle);
         Component c8 = new ComponentDiv(divStyle, c7, new ComponentText("(Also: it's float right, 30% width, 200 px high )",null));
-        list.add(c8);
+        assertSerializable(c8);
+    }
 
 
-
-        //Generate HTML
-        StringBuilder sb = new StringBuilder();
-        sb.append("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Title</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "\n" +
-                "    <div id=\"maindiv\">\n" +
-                "\n" +
-                "    </div>\n" +
-                "\n" +
-                "\n" +
-                "    <script src=\"//code.jquery.com/jquery-1.10.2.js\"></script>\n" +
-                "    <script src=\"https://code.jquery.com/ui/1.11.4/jquery-ui.min.js\"></script>\n" +
-                "    <link rel=\"stylesheet\" href=\"//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css\">\n" +
-                "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js\"></script>\n" +
-                "    <script src=\"src/main/resources/assets/dl4j-ui.js\"></script>\n" +
-                "\n" +
-                "    <script>\n");
+    private static void assertSerializable(Component component) throws Exception {
 
         ObjectMapper om = new ObjectMapper();
-        for(int i=0; i<list.size(); i++ ){
-            Component component = list.get(i);
-            sb.append("        ").append("var str").append(i).append(" = '")
-                    .append(om.writeValueAsString(component).replaceAll("'","\\\\'"))
-                    .append("';\n");
 
-            sb.append("        ").append("var obj").append(i).append(" = Component.getComponent(str").append(i).append(");\n");
-            sb.append("        ").append("obj").append(i).append(".render($('#maindiv'));\n");
-            sb.append("\n\n");
-        }
+        String json = om.writeValueAsString(component);
 
+        Component fromJson = om.readValue(json,Component.class);
 
-        sb.append("    </script>\n" +
-                "\n" +
-                "</body>\n" +
-                "</html>");
+        assertEquals(component.toString(),fromJson.toString());     //Yes, this is a bit hacky, but lombok equal method doesn't seem to work properly for List<double[]> etc
+    }
 
-        FileUtils.writeStringToFile(new File("TestRendering.html"),sb.toString());
+    private static void assertSerializable(Style style) throws Exception {
+        ObjectMapper om = new ObjectMapper();
+
+        String json = om.writeValueAsString(style);
+
+        Style fromJson = om.readValue(json,Style.class);
+
+        assertEquals(style.toString(),fromJson.toString());         //Yes, this is a bit hacky, but lombok equal method doesn't seem to work properly for List<double[]> etc
     }
 
 }
