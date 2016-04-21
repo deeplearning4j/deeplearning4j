896c2d1ace80fc498e73699be7e28e8968b484d9
raver119 <raver119@gmail.com>
2016-04-21 14:39:17 +0300

    gc

--- nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/main/java/org/nd4j/jita/allocator/impl/AtomicAllocator.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/main/java/org/nd4j/jita/allocator/impl/AtomicAllocator.java
@@ -555,12 +555,13 @@ public class AtomicAllocator implements Allocator {
 
     private class UnifiedGarbageCollectorThread extends Thread implements Runnable {
         private final ReferenceQueue<BaseDataBuffer> queue;
-
+        private int threadId;
 
         public UnifiedGarbageCollectorThread(Integer threadId, @NonNull ReferenceQueue<BaseDataBuffer> queue) {
             this.queue = queue;
             this.setDaemon(true);
             this.setName("UniGC thread " + threadId);
+            this.threadId = threadId;
         }
 
         @Override
@@ -581,7 +582,9 @@ public class AtomicAllocator implements Allocator {
 
                 } else {
                     try {
-                        Thread.sleep(50);
+                        if (threadId == 0)
+                            System.gc();
+                        Thread.sleep(500);
                     } catch (Exception e) {
 
                     }
