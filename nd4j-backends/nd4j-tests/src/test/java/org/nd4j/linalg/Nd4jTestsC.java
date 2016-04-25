20132eb2766dbf241f1558828db93dc3c1be497b
Adam Gibson <adam@skymind.io>
2016-04-25 10:22:32 +0900

    Softmaxderivativetest (#854)
    * update tad
    
    * add softmaxderivative test

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
@@ -681,6 +681,13 @@ public  class Nd4jTestsC extends BaseNd4jTest {
     }
 
     @Test
+    public void testSoftmaxDerivative() {
+        INDArray input = Nd4j.create(new double[]{ -1.07, -0.01, 0.45, 0.95, 0.45, 0.16, 0.20, 0.80, 0.89, 0.25}).transpose();
+        INDArray output = Nd4j.create(10,1);
+        Nd4j.getExecutioner().exec(new SoftMaxDerivative(input, output));
+    }
+
+    @Test
     public void testSumAlongDim1sEdgeCases() {
         int[][] shapes = new int[][]{
                 //Standard case:
