217a1696b62105f96b64c09cef35ee040c8a2297
crockpotveggies <crockpotveggies@users.noreply.github.com>
2016-04-26 17:38:08 -0700

    Remove deprecated legacy classes (re: Akka) in dl4j-nlp. (#1459)
    Former-commit-id: 54ad1457fce0a5907ae0e69cfedc525912b7fd47

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/datasets/iterator/ReutersNewsGroupsDataSetIterator.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/datasets/iterator/ReutersNewsGroupsDataSetIterator.java
@@ -18,13 +18,13 @@
 
 package org.deeplearning4j.datasets.iterator;
 
-import org.deeplearning4j.datasets.loader.ReutersNewsGroupsLoader;
+//import org.deeplearning4j.datasets.loader.ReutersNewsGroupsLoader;
 
 /**
  * @author Adam Gibson
  */
-public class ReutersNewsGroupsDataSetIterator extends BaseDatasetIterator {
-    public ReutersNewsGroupsDataSetIterator(int batch, int numExamples,boolean tfidf) throws Exception {
-        super(batch, numExamples, new ReutersNewsGroupsLoader(tfidf));
-    }
-}
+// public class ReutersNewsGroupsDataSetIterator extends BaseDatasetIterator {
+//     public ReutersNewsGroupsDataSetIterator(int batch, int numExamples,boolean tfidf) throws Exception {
+//         super(batch, numExamples, new ReutersNewsGroupsLoader(tfidf));
+//     }
+// }
