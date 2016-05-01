b6f8210cae776bb5c761d38f4dd91132928352a4
Alex Black <blacka101@gmail.com>
2016-05-01 22:23:28 +1000

    Fix tests in MultiLayerNeuralNetConfigurationTest.java
    Former-commit-id: b5bf3d4bc007c12250981f13db18d552c2938bff

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/MultiLayerNeuralNetConfigurationTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/MultiLayerNeuralNetConfigurationTest.java
@@ -47,13 +47,14 @@ public class MultiLayerNeuralNetConfigurationTest {
     @Test
     public void testJson() throws Exception {
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .layer(new RBM.Builder().dist(new NormalDistribution(1, 1e-1)).build())
-                .list().inputPreProcessor(1, new ReshapePreProcessor())
+                .list()
+                .layer(0,new RBM.Builder().dist(new NormalDistribution(1, 1e-1)).build())
+                .inputPreProcessor(0, new ReshapePreProcessor())
                 .build();
 
         String json = conf.toJson();
         MultiLayerConfiguration from = MultiLayerConfiguration.fromJson(json);
-        assertEquals(conf.getConf(1),from.getConf(1));
+        assertEquals(conf.getConf(0),from.getConf(0));
 
         Properties props = new Properties();
         props.put("json",json);
@@ -132,12 +133,13 @@ public class MultiLayerNeuralNetConfigurationTest {
     @Test
     public void testYaml() throws Exception {
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .layer(new RBM.Builder().dist(new NormalDistribution(1, 1e-1)).build())
-                .list().inputPreProcessor(1, new ReshapePreProcessor())
+                .list()
+                .layer(0, new RBM.Builder().dist(new NormalDistribution(1, 1e-1)).build())
+                .inputPreProcessor(0, new ReshapePreProcessor())
                 .build();
         String json = conf.toYaml();
         MultiLayerConfiguration from = MultiLayerConfiguration.fromYaml(json);
-        assertEquals(conf.getConf(1),from.getConf(1));
+        assertEquals(conf.getConf(0),from.getConf(0));
 
         Properties props = new Properties();
         props.put("json",json);
