75e04ff2ba3848e3686f4e077ed678346ae94060
Alex Black <blacka101@gmail.com>
2016-05-12 17:07:46 +1000

    Add an optional 'flattening order' arg to Gradient/DefaultGradient
    Former-commit-id: fee8a02dfa471a9cd2aeb8a2ba6cdaa9aa7e9682 [formerly 41252d7c34b3ac843b939915d0e55cadec8bf9c2]
    Former-commit-id: b3abeffb27ce214e9b69ff984e3eee13efb87d8a

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/gradient/DefaultGradient.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/gradient/DefaultGradient.java
@@ -31,8 +31,9 @@ import java.util.*;
  */
 
 public class DefaultGradient implements Gradient {
+    public static final char DEFAULT_FLATTENING_ORDER = 'f';
     private Map<String,INDArray> gradients = new LinkedHashMap<>();
-
+    private Map<String,Character> flatteningOrders;
 
 
     @Override
@@ -42,18 +43,47 @@ public class DefaultGradient implements Gradient {
 
     @Override
     public INDArray gradient(List<String> order) {
-        List<INDArray> ret = new ArrayList<>();
-        for(String s : order) {
-            if(!gradientForVariable().containsKey(s))
-               continue;
-            ret.add(gradientForVariable().get(s));
+        List<INDArray> toFlatten = new ArrayList<>();
+        if(flatteningOrders == null) {
+            for (String s : order) {
+                if (!gradients.containsKey(s)) continue;
+                toFlatten.add(gradients.get(s));
+            }
+        } else {
+            for(String s : order){
+                if (!gradients.containsKey(s)) continue;
+                if(flatteningOrders != null && flatteningOrders.containsKey(s) && flatteningOrders.get(s) != DEFAULT_FLATTENING_ORDER){
+                    //Arrays with non-default order get flattened to row vector first, then everything is flattened to f order
+                    //TODO revisit this, and make more efficient
+                    toFlatten.add(Nd4j.toFlattened(flatteningOrders.get(s),gradients.get(s)));
+                } else {
+                    toFlatten.add(gradients.get(s));
+                }
+            }
         }
-        return Nd4j.toFlattened('f',ret);
+        return Nd4j.toFlattened(DEFAULT_FLATTENING_ORDER, toFlatten);
     }
 
     @Override
     public INDArray gradient() {
-        return Nd4j.toFlattened('f',gradients.values());
+        if(flatteningOrders != null){
+            //Arrays with non-default order get flattened to row vector first, then everything is flattened to f order
+            //TODO revisit this, and make more efficient
+            List<INDArray> toFlatten = new ArrayList<>();
+            for(Map.Entry<String,INDArray> entry : gradients.entrySet()){
+                if(flatteningOrders.containsKey(entry.getKey()) && flatteningOrders.get(entry.getKey()) != DEFAULT_FLATTENING_ORDER){
+                    //Specific flattening order for this array, that isn't the default
+                    toFlatten.add(Nd4j.toFlattened(flatteningOrders.get(entry.getKey()),entry.getValue()));
+                } else {
+                    //default flattening order for this array
+                    toFlatten.add(entry.getValue());
+                }
+            }
+            return Nd4j.toFlattened(DEFAULT_FLATTENING_ORDER, toFlatten);
+        } else {
+            //Standard case: flatten all to f order
+            return Nd4j.toFlattened(DEFAULT_FLATTENING_ORDER, gradients.values());
+        }
     }
 
     @Override
@@ -71,11 +101,23 @@ public class DefaultGradient implements Gradient {
         return gradients.put(variable, newGradient);
     }
 
+    @Override
+    public INDArray setGradientFor(String variable, INDArray gradient, Character flatteningOrder) {
+        INDArray last = setGradientFor(variable,gradient);
+
+        if(flatteningOrder != null){
+            if(flatteningOrders == null) flatteningOrders = new LinkedHashMap<>();
+            flatteningOrders.put(variable,flatteningOrder);
+        }
+        return last;
+    }
+
 
     @Override
     public String toString() {
         return "DefaultGradient{" +
                 "gradients=" + gradients +
+                (flatteningOrders != null ? flatteningOrders : "") +
                 '}';
     }
 }
