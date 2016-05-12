3b9dec1251316b11b45e46ad2a824e9f21e4c26e
Alex Black <blacka101@gmail.com>
2016-05-12 17:30:05 +1000

    Switch conv layer weight init and gradient/param flattening to c order
    Former-commit-id: 0fe8fe5d182d5539b0c53cf978786bf33acaffbf

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/convolution/ConvolutionLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/convolution/ConvolutionLayer.java
@@ -30,6 +30,7 @@ import org.deeplearning4j.util.Dropout;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.BroadcastOp;
 import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastAddOp;
+import org.nd4j.linalg.api.shape.Shape;
 import org.nd4j.linalg.convolution.Convolution;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.ops.transforms.Transforms;
@@ -100,7 +101,7 @@ public class ConvolutionLayer extends BaseLayer<org.deeplearning4j.nn.conf.layer
 
         // gW = np.tensordot(gy[0], col, ([0, 2, 3], [0, 4, 5]))
         INDArray weightGradient = Nd4j.tensorMmul(delta, col, new int[][] {{0, 2, 3},{0, 4, 5}});
-        retGradient.setGradientFor(ConvolutionParamInitializer.WEIGHT_KEY, weightGradient);
+        retGradient.setGradientFor(ConvolutionParamInitializer.WEIGHT_KEY, weightGradient, 'c');
 
         //gcol = tensorMmul(W, gy[0], (0, 1))
         INDArray nextEpsilon = Nd4j.tensorMmul(weights, delta, new int[][] {{0}, {1}});
@@ -111,19 +112,77 @@ public class ConvolutionLayer extends BaseLayer<org.deeplearning4j.nn.conf.layer
     }
 
     public INDArray preOutput(boolean training) {
-        INDArray Weights = getParam(ConvolutionParamInitializer.WEIGHT_KEY);
+        INDArray weights = getParam(ConvolutionParamInitializer.WEIGHT_KEY);
         INDArray bias = getParam(ConvolutionParamInitializer.BIAS_KEY);
         if(conf.isUseDropConnect() && training) {
             if (conf.getLayer().getDropOut() > 0) {
-                Weights = Dropout.applyDropConnect(this, ConvolutionParamInitializer.WEIGHT_KEY);
+                weights = Dropout.applyDropConnect(this, ConvolutionParamInitializer.WEIGHT_KEY);
             }
         }
 
-        INDArray z = Nd4j.tensorMmul(col, Weights, new int[][]{{1, 2, 3}, {1, 2, 3}});
+        //TODO: proper strides and order and all that good stuff.
+        int miniBatch = input.size(0);
+        int inH = input.size(2);
+        int inW = input.size(3);
+        int outH = col.size(4);
+        int outW = col.size(5);
+        int outDepth = weights.size(0);
+        int inDepth = input.size(1);
+        int kH = weights.size(2);
+        int kW = weights.size(3);
+        int[] strides = layerConf().getStride();
+        int[] pad = layerConf().getPadding();
+//
+//        //Current order of im2col: [miniBatch,depth,kH,kW,outH,outW]. Want: [miniBatch,outH,outW,depthIn,kH,kW] in C order
+//        //Means: permute(0,4,5,1,2,3)
+//        INDArray permutedCol = col.permute(0,4,5,1,2,3).dup('c');
+//        int length1 = col.length();
+//        int length2 = miniBatch*outH*outW*inDepth*kH*kW;
+//        INDArray reshapedCol = permutedCol.reshape('c',miniBatch*outH*outW, inDepth*kH*kW);
+
+        //im2col in the required order: want [miniBatch,outH,outW,depthIn,kH,kW], but need to input [miniBatch,depth,kH,kW,outH,outW]
+        //To get this: create an array of this order, permute it to the im2col order, and then do im2col
+        //to get original from required order: permute(0,3,4,5,1,2)
+//        INDArray col = Nd4j.create(new int[]{miniBatch,outH,outW,inDepth,kH,kW},'c');
+//        INDArray col2 = col.permute(0,3,4,5,1,2);
+//        Convolution.im2col(input, kH, kW, strides[0], strides[1], pad[0], pad[1], false, col2);
+
+        //im2col in the required order: want [outW,outH,miniBatch,depthIn,kH,kW], but need to input [miniBatch,depth,kH,kW,outH,outW]
+        //To get this: create an array of this order, permute it to the im2col order, and then do im2col
+        //to get old order from required order: permute(2,3,4,5,1,2)
+        INDArray col = Nd4j.create(new int[]{outW,outH,miniBatch,inDepth,kH,kW},'c');
+        INDArray col2 = col.permute(2,3,4,5,0,1);
+        Convolution.im2col(input, kH, kW, strides[0], strides[1], pad[0], pad[1], false, col2);
+
+        INDArray reshapedCol = Shape.newShapeNoCopy(col,new int[]{miniBatch*outH*outW, inDepth*kH*kW},false);
+        if(reshapedCol == null) throw new RuntimeException("Could not reshape without copy");
+
+        //Current order of weights: [depthOut,depthIn,kH,kW]
+        //Want order: [depthIn,kH,kW,depthOut], c order
+        //need: permute(1,2,3,0)
+        INDArray permutedW = weights.permute(1,2,3,0).dup('c');
+        INDArray reshapedW = permutedW.reshape('c',inDepth*kH*kW,outDepth);
+
+        //Do the MMUL, c order out. Use AB=(B^T A^T)^T
+        INDArray z = Nd4j.gemm(reshapedW,reshapedCol,true,true).transpose();
+
+        //Expected output shape: [miniBatch*outH*outW,depthOut]
+        if(z.ordering() != 'c') throw new RuntimeException();
+        if(z.rows() != miniBatch*outH*outW) throw new RuntimeException();
+        if(z.columns() != outDepth) throw new RuntimeException();
+
+        //Now, reshape to [miniBatch,outH,outW,depthOut]
+        z = z.reshape('c',miniBatch,outH,outW,outDepth);
+
+//        INDArray z = Nd4j.tensorMmul(col, weights, new int[][]{{1, 2, 3}, {1, 2, 3}});
         BroadcastOp op = new BroadcastAddOp(z,bias,z,3);
         Nd4j.getExecutioner().exec(op);
 
-        return Nd4j.rollAxis(z, 3, 1);
+//        return Nd4j.rollAxis(z, 3, 1);
+
+        //Output activations with shape [miniBath,outDepth,outH,outW];
+        INDArray out = z.permute(0,3,1,2);
+        return out;
     }
 
     @Override
@@ -158,5 +217,10 @@ public class ConvolutionLayer extends BaseLayer<org.deeplearning4j.nn.conf.layer
         throw new UnsupportedOperationException();
     }
 
+    @Override
+    public INDArray params(){
+        //C order flattening, to match the gradient flattening order
+        return Nd4j.toFlattened('c',params.values());
+    }
 
 }
