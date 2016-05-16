67bed5af37ed033c2fe5eed11ac15fa03dfe46b8
Alex Black <blacka101@gmail.com>
2016-05-16 16:00:07 +1000

    Minor unit test fixes
    Former-commit-id: 60dae0e4a219aff0149e750125ad3dd63344e1d5

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/updater/TestDecayPolicies.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/updater/TestDecayPolicies.java
@@ -332,8 +332,8 @@ public class TestDecayPolicies {
                 else if(updaterFunc.equals(org.deeplearning4j.nn.conf.Updater.RMSPROP))
                     lr = testRMSPropComputation(gradientActual, gradientExpected, lr, learningRateAfter, i);
 
-                // TODO check if this is cycling through the learning rate schedule...
-                assertEquals(lr, net.getLayer(1).conf().getLearningRateByParam("W"), 1e-4);
+                if(i == 0) assertEquals(lr, net.getLayer(1).conf().getLearningRateByParam("W"), lr);
+                else assertEquals(lr, net.getLayer(1).conf().getLearningRateByParam("W"), learningRateAfter.get(1));
             }
         }
     }
