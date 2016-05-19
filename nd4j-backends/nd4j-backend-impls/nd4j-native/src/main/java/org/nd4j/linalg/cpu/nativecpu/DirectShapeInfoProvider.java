0499d4931072e1acf0721ac36f8a02c4eb7210a2
raver119@gmail.com <raver119@gmail.com>
2016-05-19 19:49:39 +0300

    cpu shapeInfo cache

--- nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/DirectShapeInfoProvider.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/DirectShapeInfoProvider.java
@@ -1,9 +1,33 @@
 package org.nd4j.linalg.cpu.nativecpu;
 
+import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.ndarray.BaseShapeInfoProvider;
+import org.nd4j.linalg.api.shape.ShapeDescriptor;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author raver119@gmail.com
  */
 public class DirectShapeInfoProvider extends BaseShapeInfoProvider {
+    private Map<ShapeDescriptor, DataBuffer> shapeCache = new ConcurrentHashMap<>();
+
+    @Override
+    public DataBuffer createShapeInformation(int[] shape, int[] stride, int offset, int elementWiseStride, char order) {
+
+        ShapeDescriptor descriptor = new ShapeDescriptor(shape, stride, offset, elementWiseStride, order);
+        if (!shapeCache.containsKey(descriptor)) {
+            synchronized (this) {
+                if (!shapeCache.containsKey(descriptor)) {
+                    DataBuffer buffer = super.createShapeInformation(shape, stride, offset, elementWiseStride, order);
+                    shapeCache.put(descriptor, buffer);
+
+                    return buffer;
+                } else return shapeCache.get(descriptor);
+            }
+        }
+
+        return shapeCache.get(descriptor);
+    }
 }
