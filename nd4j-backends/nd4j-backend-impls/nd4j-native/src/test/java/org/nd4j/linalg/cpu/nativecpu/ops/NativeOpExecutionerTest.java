cee24e2d09f86366829e5e832563c40d2e3735f8
raver119@gmail.com <raver119@gmail.com>
2016-05-27 14:17:09 +0300

    some tests for timing simd

--- nd4j-backends/nd4j-backend-impls/nd4j-native/src/test/java/org/nd4j/linalg/cpu/nativecpu/ops/NativeOpExecutionerTest.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-native/src/test/java/org/nd4j/linalg/cpu/nativecpu/ops/NativeOpExecutionerTest.java
@@ -22,26 +22,36 @@ public class NativeOpExecutionerTest {
 
     @Test
     public void execBroadcastOp() throws Exception {
-        INDArray array = Nd4j.ones(256, 1024);
+        INDArray array = Nd4j.ones(12, 1024);
         INDArray arrayRow = Nd4j.linspace(1, 1024, 1024);
 
+        float sum = (float) array.sumNumber().doubleValue();
+
         long time1 = System.nanoTime();
-        array.addiRowVector(arrayRow);
+        for (int x = 0; x < 1000; x++) {
+            array.addiRowVector(arrayRow);
+        }
         long time2 = System.nanoTime();
 
-        System.out.println("Execution time: " + (time2 - time1));
+        System.out.println("Execution time: " + ((time2 - time1) / 1000));
+
+        assertEquals(2.0, array.getFloat(0), 0.1f);
+        assertEquals(3.0, array.getFloat(1), 0.1f);
     }
 
     @Test
     public void execReduceOp1() throws Exception {
-        INDArray array = Nd4j.ones(256, 1024);
-        INDArray arrayRow = Nd4j.linspace(1, 1024, 1024);
+        INDArray array = Nd4j.ones(1024, 1024);
+        INDArray arrayRow1 = Nd4j.linspace(1, 1024, 1024);
+        INDArray arrayRow2 = Nd4j.linspace(0, 1023, 1024);
 
+        float sum = (float) array.sumNumber().doubleValue();
         long time1 = System.nanoTime();
-        float sum = array.sumNumber().floatValue();
+        sum = array.sumNumber().floatValue();
         long time2 = System.nanoTime();
 
         System.out.println("Execution time: " + (time2 - time1));
+        System.out.println("Result: " + sum);
     }
 
     @Test
@@ -49,8 +59,9 @@ public class NativeOpExecutionerTest {
         INDArray array = Nd4j.ones(3, 1024);
         INDArray arrayRow = Nd4j.linspace(1, 1024, 1024);
 
-        long time1 = System.nanoTime();
         float sum = array.sumNumber().floatValue();
+        long time1 = System.nanoTime();
+        sum = array.sumNumber().floatValue();
         long time2 = System.nanoTime();
 
         System.out.println("Execution time: " + (time2 - time1));
