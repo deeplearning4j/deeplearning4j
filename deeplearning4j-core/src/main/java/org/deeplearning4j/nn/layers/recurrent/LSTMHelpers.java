f04a27a1fc487796675765520d6c58e666875c5b
Alex Black <blacka101@gmail.com>
2016-05-27 22:10:28 +1000

    Switch dense and LSTM layer gradients to use gradient view arrays
    Former-commit-id: 8442d76126999a95b866c2fd25ddb50ad284fad0 [formerly c0355cd1941f9f73a2029404ec0bd20e9ba36b08]
    Former-commit-id: b08c07a637a23e7c2ba78cf9379bfedec3547d92

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/recurrent/LSTMHelpers.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/layers/recurrent/LSTMHelpers.java
@@ -15,6 +15,8 @@ import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.INDArrayIndex;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 
+import java.util.Map;
+
 import static org.nd4j.linalg.indexing.NDArrayIndex.interval;
 import static org.nd4j.linalg.indexing.NDArrayIndex.point;
 
@@ -216,7 +218,8 @@ public class LSTMHelpers {
                                                                   final boolean forwards,
                                                                   final String inputWeightKey,
                                                                   final String recurrentWeightKey,
-                                                                  final String biasWeightKey) {
+                                                                  final String biasWeightKey,
+                                                                  final Map<String,INDArray> gradientViews) {
 
 
         //Expect errors to have shape: [miniBatchSize,n^(L+1),timeSeriesLength]
@@ -249,9 +252,16 @@ public class LSTMHelpers {
             endIdx = Math.max(0, timeSeriesLength - tbpttBackwardLength);
         }
 
-        INDArray iwGradientsOut = Nd4j.create(new int[]{prevLayerSize, 4 * hiddenLayerSize}, 'f');
-        INDArray rwGradientsOut = Nd4j.create(new int[]{hiddenLayerSize, 4 * hiddenLayerSize + 3},'f');    //Order: {I,F,O,G,FF,OO,GG}
-        INDArray bGradientsOut = Nd4j.create(1,4*hiddenLayerSize);
+//        INDArray iwGradientsOut = Nd4j.create(new int[]{prevLayerSize, 4 * hiddenLayerSize}, 'f');
+//        INDArray rwGradientsOut = Nd4j.create(new int[]{hiddenLayerSize, 4 * hiddenLayerSize + 3},'f');    //Order: {I,F,O,G,FF,OO,GG}
+//        INDArray bGradientsOut = Nd4j.create(1,4*hiddenLayerSize);
+
+        INDArray iwGradientsOut = gradientViews.get(inputWeightKey);
+        INDArray rwGradientsOut = gradientViews.get(recurrentWeightKey);
+        INDArray bGradientsOut = gradientViews.get(biasWeightKey);
+        iwGradientsOut.assign(0);
+        rwGradientsOut.assign(0);
+        bGradientsOut.assign(0);
 
         INDArray rwGradientsIFOG = rwGradientsOut.get(NDArrayIndex.all(), NDArrayIndex.interval(0,4*hiddenLayerSize));
         INDArray rwGradientsFF = rwGradientsOut.get(NDArrayIndex.all(), NDArrayIndex.point(4 * hiddenLayerSize));
