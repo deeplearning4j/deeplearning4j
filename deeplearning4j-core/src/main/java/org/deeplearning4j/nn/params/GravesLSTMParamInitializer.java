6c527ee06bd755474930e96aed3c65513032da73
Adam Gibson <adam@skymind.io>
2016-05-29 09:53:51 +0900

    change lstm parameter init to use point on bias
    Former-commit-id: 37d24c52664613aa1e5005dc519cbbc0c7dcc74f

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/params/GravesLSTMParamInitializer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/params/GravesLSTMParamInitializer.java
@@ -59,7 +59,7 @@ public class GravesLSTMParamInitializer implements ParamInitializer {
         params.put(INPUT_WEIGHT_KEY,WeightInitUtil.initWeights(nLast, 4 * nL, layerConf.getWeightInit(), dist));
         params.put(RECURRENT_WEIGHT_KEY,WeightInitUtil.initWeights(nL, 4 * nL + 3, layerConf.getWeightInit(), dist));
         INDArray biases = Nd4j.zeros(1,4*nL);	//Order: input, forget, output, input modulation, i.e., IFOG
-        biases.put(new INDArrayIndex[]{new NDArrayIndex(0),NDArrayIndex.interval(nL, 2*nL)}, Nd4j.ones(1,nL).muli(forgetGateInit));
+        biases.put(new INDArrayIndex[]{NDArrayIndex.point(0),NDArrayIndex.interval(nL, 2*nL)}, Nd4j.ones(1,nL).muli(forgetGateInit));
         /*The above line initializes the forget gate biases to specified value.
          * See Sutskever PhD thesis, pg19:
          * "it is important for [the forget gate activations] to be approximately 1 at the early stages of learning,
