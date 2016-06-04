9b61a0e45189341f1b9fdd61bf5c43ac9629c22e
Alex Black <blacka101@gmail.com>
2016-06-04 19:01:02 +1000

    Add newer dl4j configuration options to arbiter-deeplearning4j

--- arbiter-deeplearning4j/src/main/java/org/deeplearning4j/arbiter/BaseNetworkSpace.java
+++ arbiter-deeplearning4j/src/main/java/org/deeplearning4j/arbiter/BaseNetworkSpace.java
@@ -20,17 +20,18 @@ import lombok.AllArgsConstructor;
 import org.deeplearning4j.arbiter.layers.LayerSpace;
 import org.deeplearning4j.arbiter.optimize.parameter.FixedValue;
 import org.deeplearning4j.arbiter.optimize.api.ParameterSpace;
+import org.deeplearning4j.arbiter.optimize.parameter.discrete.DiscreteParameterSpace;
 import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.*;
 import org.deeplearning4j.nn.conf.distribution.Distribution;
+import org.deeplearning4j.nn.conf.stepfunctions.StepFunction;
 import org.deeplearning4j.nn.weights.WeightInit;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-//public class MultiLayerSpace implements ModelParameterSpace<MultiLayerConfiguration> {
 public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
 
     protected ParameterSpace<Boolean> useDropConnect;
@@ -40,11 +41,22 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
     protected ParameterSpace<Boolean> regularization;
     protected ParameterSpace<Boolean> schedules;
     protected ParameterSpace<String> activationFunction;
+    protected ParameterSpace<Double> biasInit;
     protected ParameterSpace<WeightInit> weightInit;
     protected ParameterSpace<Distribution> dist;
     protected ParameterSpace<Double> learningRate;
+    protected ParameterSpace<Double> biasLearningRate;
     protected ParameterSpace<Map<Integer,Double>> learningRateAfter;
     protected ParameterSpace<Double> lrScoreBasedDecay;
+    protected ParameterSpace<LearningRatePolicy> learningRateDecayPolicy;
+    protected ParameterSpace<Map<Integer,Double>> learningRateSchedule;
+    protected ParameterSpace<Double> lrPolicyDecayRate;
+    protected ParameterSpace<Double> lrPolicyPower;
+    protected ParameterSpace<Double> lrPolicySteps;
+    protected ParameterSpace<Integer> maxNumLineSearchIterations;
+    protected ParameterSpace<Boolean> miniBatch;
+    protected ParameterSpace<Boolean> minimize;
+    protected ParameterSpace<StepFunction> stepFunction;
     protected ParameterSpace<Double> l1;
     protected ParameterSpace<Double> l2;
     protected ParameterSpace<Double> dropOut;
@@ -56,6 +68,8 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
     protected ParameterSpace<GradientNormalization> gradientNormalization;
     protected ParameterSpace<Double> gradientNormalizationThreshold;
     protected ParameterSpace<int[]> cnnInputSize;
+    private ParameterSpace<Double> adamMeanDecay;
+    private ParameterSpace<Double> adamVarDecay;
 
     protected List<LayerConf> layerSpaces = new ArrayList<>();
 
@@ -77,11 +91,22 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         this.regularization = builder.regularization;
         this.schedules = builder.schedules;
         this.activationFunction = builder.activationFunction;
+        this.biasInit = builder.biasInit;
         this.weightInit = builder.weightInit;
         this.dist = builder.dist;
         this.learningRate = builder.learningRate;
+        this.biasLearningRate = builder.biasLearningRate;
         this.learningRateAfter = builder.learningRateAfter;
         this.lrScoreBasedDecay = builder.lrScoreBasedDecay;
+        this.learningRateDecayPolicy = builder.learningRateDecayPolicy;
+        this.learningRateSchedule = builder.learningRateSchedule;
+        this.lrPolicyDecayRate = builder.lrPolicyDecayRate;
+        this.lrPolicyPower = builder.lrPolicyPower;
+        this.lrPolicySteps = builder.lrPolicySteps;
+        this.maxNumLineSearchIterations = builder.maxNumLineSearchIterations;
+        this.miniBatch = builder.miniBatch;
+        this.minimize = builder.minimize;
+        this.stepFunction = builder.stepFunction;
         this.l1 = builder.l1;
         this.l2 = builder.l2;
         this.dropOut = builder.dropOut;
@@ -92,6 +117,9 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         this.rmsDecay = builder.rmsDecay;
         this.gradientNormalization = builder.gradientNormalization;
         this.gradientNormalizationThreshold = builder.gradientNormalizationThreshold;
+        this.adamMeanDecay = builder.adamMeanDecay;
+        this.adamVarDecay = builder.adamVarDecay;
+
 
         this.backprop = builder.backprop;
         this.pretrain = builder.pretrain;
@@ -114,11 +142,22 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(regularization != null) builder.regularization(regularization.getValue(values));
        // if(schedules != null) builder.learningRateSchedule(schedules.getValue(values));
         if(activationFunction != null) builder.activation(activationFunction.getValue(values));
+        if(biasInit != null) builder.biasInit(biasInit.getValue(values));
         if(weightInit != null) builder.weightInit(weightInit.getValue(values));
         if(dist != null) builder.dist(dist.getValue(values));
         if(learningRate != null) builder.learningRate(learningRate.getValue(values));
+        if(biasLearningRate != null) builder.biasLearningRate(biasLearningRate.getValue(values));
         if(learningRateAfter != null) builder.learningRateSchedule(learningRateAfter.getValue(values));
         if(lrScoreBasedDecay != null) builder.learningRateScoreBasedDecayRate(lrScoreBasedDecay.getValue(values));
+        if(learningRateDecayPolicy != null) builder.learningRateDecayPolicy(learningRateDecayPolicy.getValue(values));
+        if(learningRateSchedule != null) builder.learningRateSchedule(learningRateSchedule.getValue(values));
+        if(lrPolicyDecayRate != null) builder.lrPolicyDecayRate(lrPolicyDecayRate.getValue(values));
+        if(lrPolicyPower != null) builder.lrPolicyPower(lrPolicyPower.getValue(values));
+        if(lrPolicySteps != null) builder.lrPolicySteps(lrPolicySteps.getValue(values));
+        if(maxNumLineSearchIterations != null) builder.maxNumLineSearchIterations(maxNumLineSearchIterations.getValue(values));
+        if(miniBatch != null) builder.miniBatch(miniBatch.getValue(values));
+        if(minimize != null) builder.minimize(minimize.getValue(values));
+        if(stepFunction != null) builder.stepFunction(stepFunction.getValue(values));
         if(l1 != null) builder.l1(l1.getValue(values));
         if(l2 != null) builder.l2(l2.getValue(values));
         if(dropOut != null) builder.dropOut(dropOut.getValue(values));
@@ -129,6 +168,8 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(rmsDecay != null) builder.rmsDecay(rmsDecay.getValue(values));
         if(gradientNormalization != null) builder.gradientNormalization(gradientNormalization.getValue(values));
         if(gradientNormalizationThreshold != null) builder.gradientNormalizationThreshold(gradientNormalizationThreshold.getValue(values));
+        if(adamMeanDecay != null) builder.adamMeanDecay(adamMeanDecay.getValue(values));
+        if(adamVarDecay != null) builder.adamVarDecay(adamVarDecay.getValue(values));
 
         return builder;
     }
@@ -142,11 +183,22 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(regularization != null) list.addAll(regularization.collectLeaves());
         if(schedules != null) list.addAll(schedules.collectLeaves());
         if(activationFunction != null) list.addAll(activationFunction.collectLeaves());
+        if(biasInit != null) list.addAll(biasInit.collectLeaves());
         if(weightInit != null) list.addAll(weightInit.collectLeaves());
         if(dist != null) list.addAll(dist.collectLeaves());
         if(learningRate != null) list.addAll(learningRate.collectLeaves());
+        if(biasLearningRate != null) list.addAll(biasLearningRate.collectLeaves());
         if(learningRateAfter != null) list.addAll(learningRateAfter.collectLeaves());
         if(lrScoreBasedDecay != null) list.addAll(lrScoreBasedDecay.collectLeaves());
+        if(learningRateDecayPolicy != null) list.addAll(learningRateDecayPolicy.collectLeaves());
+        if(learningRateSchedule != null) list.addAll(learningRateSchedule.collectLeaves());
+        if(lrPolicyDecayRate != null) list.addAll(lrPolicyDecayRate.collectLeaves());
+        if(lrPolicyPower != null) list.addAll(lrPolicyPower.collectLeaves());
+        if(lrPolicySteps != null) list.addAll(lrPolicySteps.collectLeaves());
+        if(maxNumLineSearchIterations != null) list.addAll(maxNumLineSearchIterations.collectLeaves());
+        if(miniBatch != null) list.addAll(miniBatch.collectLeaves());
+        if(minimize != null) list.addAll(minimize.collectLeaves());
+        if(stepFunction != null) list.addAll(minimize.collectLeaves());
         if(l1 != null) list.addAll(l1.collectLeaves());
         if(l2 != null) list.addAll(l2.collectLeaves());
         if(dropOut != null) list.addAll(dropOut.collectLeaves());
@@ -158,6 +210,8 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(gradientNormalization != null) list.addAll(gradientNormalization.collectLeaves());
         if(gradientNormalizationThreshold != null) list.addAll(gradientNormalizationThreshold.collectLeaves());
         if(cnnInputSize != null) list.addAll(cnnInputSize.collectLeaves());
+        if(adamMeanDecay != null) list.addAll(adamMeanDecay.collectLeaves());
+        if(adamVarDecay != null) list.addAll(adamVarDecay.collectLeaves());
         return list;
     }
 
@@ -185,8 +239,18 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(weightInit != null) sb.append("weightInit: ").append(weightInit).append("\n");
         if(dist != null) sb.append("dist: ").append(dist).append("\n");
         if(learningRate != null) sb.append("learningRate: ").append(learningRate).append("\n");
+        if(biasLearningRate != null) sb.append("biasLearningRate: ").append(biasLearningRate).append("\n");
         if(learningRateAfter != null) sb.append("learningRateAfter: ").append(learningRateAfter).append("\n");
         if(lrScoreBasedDecay != null) sb.append("lrScoreBasedDecay: ").append(lrScoreBasedDecay).append("\n");
+        if(learningRateDecayPolicy != null) sb.append("learningRateDecayPolicy: ").append(learningRateDecayPolicy).append("\n");
+        if(learningRateSchedule != null) sb.append("learningRateSchedule: ").append(learningRateSchedule).append("\n");
+        if(lrPolicyDecayRate != null) sb.append("lrPolicyDecayRate: ").append(lrPolicyDecayRate).append("\n");
+        if(lrPolicyPower != null) sb.append("lrPolicyPower: ").append(lrPolicyPower).append("\n");
+        if(lrPolicySteps != null) sb.append("lrPolicySteps: ").append(lrPolicySteps).append("\n");
+        if(maxNumLineSearchIterations != null) sb.append("maxNumLineSearchIterations: ").append(maxNumLineSearchIterations).append("\n");
+        if(miniBatch != null) sb.append("miniBatch: ").append(miniBatch).append("\n");
+        if(minimize != null) sb.append("minimize: ").append(minimize).append("\n");
+        if(stepFunction != null) sb.append("stepFunction: ").append(stepFunction).append("\n");
         if(l1 != null) sb.append("l1: ").append(l1).append("\n");
         if(l2 != null) sb.append("l2: ").append(l2).append("\n");
         if(dropOut != null) sb.append("dropOut: ").append(dropOut).append("\n");
@@ -203,6 +267,8 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         if(tbpttFwdLength != null) sb.append("tbpttFwdLength: ").append(tbpttFwdLength).append("\n");
         if(tbpttBwdLength != null) sb.append("tbpttBwdLength: ").append(tbpttBwdLength).append("\n");
         if(cnnInputSize != null) sb.append("cnnInputSize: ").append(cnnInputSize).append("\n");
+        if(adamMeanDecay != null) sb.append("adamMeanDecay: ").append(adamMeanDecay).append("\n");
+        if(adamVarDecay != null) sb.append("adamVarDecay: ").append(adamVarDecay).append("\n");
 
         int i=0;
         for(LayerConf conf : layerSpaces){
@@ -227,18 +293,29 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
     @SuppressWarnings("unchecked")
     protected abstract static class Builder<T extends Builder<T>> {
 
+        private ParameterSpace<String> activationFunction;
+        private ParameterSpace<WeightInit> weightInit;
+        private ParameterSpace<Double> biasInit;
         private ParameterSpace<Boolean> useDropConnect;
         private ParameterSpace<Integer> iterations;
         private Long seed;
         private ParameterSpace<OptimizationAlgorithm> optimizationAlgo;
         private ParameterSpace<Boolean> regularization;
         private ParameterSpace<Boolean> schedules;
-        private ParameterSpace<String> activationFunction;
-        private ParameterSpace<WeightInit> weightInit;
         private ParameterSpace<Distribution> dist;
         private ParameterSpace<Double> learningRate;
+        private ParameterSpace<Double> biasLearningRate;
         private ParameterSpace<Map<Integer,Double>> learningRateAfter;
         private ParameterSpace<Double> lrScoreBasedDecay;
+        private ParameterSpace<LearningRatePolicy> learningRateDecayPolicy;
+        private ParameterSpace<Map<Integer,Double>> learningRateSchedule;
+        private ParameterSpace<Double> lrPolicyDecayRate;
+        private ParameterSpace<Double> lrPolicyPower;
+        private ParameterSpace<Double> lrPolicySteps;
+        private ParameterSpace<Integer> maxNumLineSearchIterations;
+        private ParameterSpace<Boolean> miniBatch;
+        private ParameterSpace<Boolean> minimize;
+        private ParameterSpace<StepFunction> stepFunction;
         private ParameterSpace<Double> l1;
         private ParameterSpace<Double> l2;
         private ParameterSpace<Double> dropOut;
@@ -250,6 +327,8 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
         private ParameterSpace<GradientNormalization> gradientNormalization;
         private ParameterSpace<Double> gradientNormalizationThreshold;
         private ParameterSpace<int[]> cnnInputSize;
+        private ParameterSpace<Double> adamMeanDecay;
+        private ParameterSpace<Double> adamVarDecay;
 
         //NeuralNetConfiguration.ListBuilder/MultiLayerConfiguration.Builder<T> options:
         private ParameterSpace<Boolean> backprop;
@@ -349,6 +428,15 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
             return (T)this;
         }
 
+        public T biasLearningRate(double learningRate){
+            return biasLearningRate(new FixedValue<>(learningRate));
+        }
+
+        public T biasLearningRate(ParameterSpace<Double> biasLearningRate){
+            this.biasLearningRate = biasLearningRate;
+            return (T)this;
+        }
+
         public T learningRateAfter(Map<Integer,Double> learningRateAfter){
             return learningRateAfter(new FixedValue<>(learningRateAfter));
         }
@@ -364,6 +452,87 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
 
         public T learningRateScoreBasedDecayRate(ParameterSpace<Double> lrScoreBasedDecay){
             this.lrScoreBasedDecay = lrScoreBasedDecay;
+            return (T)this;
+        }
+
+        public T learningRateDecayPolicy(LearningRatePolicy learningRatePolicy){
+            return learningRateDecayPolicy(new FixedValue<>(learningRatePolicy));
+        }
+
+        public T learningRateDecayPolicy(ParameterSpace<LearningRatePolicy> learningRateDecayPolicy){
+            this.learningRateDecayPolicy = learningRateDecayPolicy;
+            return (T)this;
+        }
+
+        public T learningRateSchedule(Map<Integer,Double> learningRateSchedule){
+            return learningRateSchedule(new FixedValue<>(learningRateSchedule));
+        }
+
+        public T learningRateSchedule(ParameterSpace<Map<Integer,Double>> learningRateSchedule){
+            this.learningRateSchedule = learningRateSchedule;
+            return (T)this;
+        }
+
+        public T lrPolicyDecayRate(double lrPolicyDecayRate){
+            return lrPolicyDecayRate(new FixedValue<>(lrPolicyDecayRate));
+        }
+
+        public T lrPolicyDecayRate(ParameterSpace<Double> lrPolicyDecayRate){
+            this.lrPolicyDecayRate = lrPolicyDecayRate;
+            return (T)this;
+        }
+
+        public T lrPolicyPower(double lrPolicyPower){
+            return lrPolicyPower(new FixedValue<>(lrPolicyPower));
+        }
+
+        public T lrPolicyPower(ParameterSpace<Double> lrPolicyPower){
+            this.lrPolicyPower = lrPolicyPower;
+            return (T)this;
+        }
+
+        public T lrPolicySteps(double lrPolicySteps){
+            return lrPolicySteps(new FixedValue<>(lrPolicySteps));
+        }
+
+        public T lrPolicySteps(ParameterSpace<Double> lrPolicySteps){
+            this.lrPolicySteps = lrPolicySteps;
+            return (T)this;
+        }
+
+        public T maxNumLineSearchIterations(int maxNumLineSearchIterations){
+            return maxNumLineSearchIterations(new FixedValue<>(maxNumLineSearchIterations));
+        }
+
+        public T maxNumLineSearchIterations(ParameterSpace<Integer> maxNumLineSearchIterations){
+            this.maxNumLineSearchIterations = maxNumLineSearchIterations;
+            return (T)this;
+        }
+
+        public T miniBatch(boolean minibatch){
+            return miniBatch(new FixedValue<>(minibatch));
+        }
+
+        public T miniBatch(ParameterSpace<Boolean> miniBatch){
+            this.miniBatch = miniBatch;
+            return (T)this;
+        }
+
+        public T minimize(boolean minimize){
+            return minimize(new FixedValue<>(minimize));
+        }
+
+        public T minimize(ParameterSpace<Boolean> minimize){
+            this.minimize = minimize;
+            return (T) this;
+        }
+
+        public T stepFunction(StepFunction stepFunction){
+            return stepFunction(new FixedValue<>(stepFunction));
+        }
+
+        public T stepFunction(ParameterSpace<StepFunction> stepFunction){
+            this.stepFunction = stepFunction;
             return (T)this;
         }
 
@@ -509,6 +678,34 @@ public abstract class BaseNetworkSpace<T> implements ParameterSpace<T> {
             this.numEpochs = numEpochs;
             return (T)this;
         }
+
+        public T biasInit(double biasInit){
+            return biasInit(new FixedValue<>(biasInit));
+        }
+
+        public T biasInit(ParameterSpace<Double> biasInit){
+            this.biasInit = biasInit;
+            return (T)this;
+        }
+
+        public T adamMeanDecay(double adamMeanDecay){
+            return adamMeanDecay(new FixedValue<>(adamMeanDecay));
+        }
+
+        public T adamMeanDecay(ParameterSpace<Double> adamMeanDecay){
+            this.adamMeanDecay = adamMeanDecay;
+            return (T)this;
+        }
+
+        public T adamVarDecay(double adamVarDecay){
+            return adamVarDecay(new FixedValue<>(adamVarDecay));
+        }
+
+        public T adamVarDecay(ParameterSpace<Double> adamVarDecay){
+            this.adamVarDecay = adamVarDecay;
+            return (T)this;
+        }
+
 
         public abstract <E extends BaseNetworkSpace> E build();
     }
