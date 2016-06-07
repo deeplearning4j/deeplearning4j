3701135862dc969ce3100e20e936a8edadc88d6f
raver119@gmail.com <raver119@gmail.com>
2016-06-07 16:49:51 +0300

    working prototype
    Former-commit-id: 269b2261bf04cceefe74fa6cbaa81f4eeaeb4705 [formerly 1c003e6c94053b42a6cb9146227b36cdedd745cf]
    Former-commit-id: 064c67d8626c41105ed5c082d86a8dd10145b5ad

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/embeddings/learning/impl/elements/CBOW.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/embeddings/learning/impl/elements/CBOW.java
@@ -2,13 +2,19 @@ package org.deeplearning4j.models.embeddings.learning.impl.elements;
 
 import lombok.NonNull;
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
+import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
 import org.deeplearning4j.models.embeddings.learning.ElementsLearningAlgorithm;
 import org.deeplearning4j.models.embeddings.loader.VectorsConfiguration;
 import org.deeplearning4j.models.sequencevectors.interfaces.SequenceIterator;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -21,10 +27,18 @@ public class CBOW<T extends SequenceElement> implements ElementsLearningAlgorith
     private WeightLookupTable<T> lookupTable;
     private VectorsConfiguration configuration;
 
+    private static final Logger logger = LoggerFactory.getLogger(CBOW.class);
+
+    protected static double MAX_EXP = 6;
+
     protected int window;
     protected boolean useAdaGrad;
     protected double negative;
     protected double sampling;
+
+    protected double[] expTable;
+
+    protected INDArray syn0, syn1, syn1Neg, table;
 
     @Override
     public String getCodeName() {
@@ -41,6 +55,11 @@ public class CBOW<T extends SequenceElement> implements ElementsLearningAlgorith
         this.useAdaGrad = configuration.isUseAdaGrad();
         this.negative = configuration.getNegative();
         this.sampling = configuration.getSampling();
+
+        this.syn0 = ((InMemoryLookupTable<T>) lookupTable).getSyn0();
+        this.syn1 = ((InMemoryLookupTable<T>) lookupTable).getSyn1();
+        this.syn1Neg = ((InMemoryLookupTable<T>) lookupTable).getSyn1Neg();
+        this.expTable = ((InMemoryLookupTable<T>) lookupTable).getExpTable();
     }
 
     /**
@@ -59,7 +78,8 @@ public class CBOW<T extends SequenceElement> implements ElementsLearningAlgorith
         if (sampling > 0) tempSequence = applySubsampling(sequence, nextRandom);
 
         for (int i = 0; i < tempSequence.getElements().size(); i++) {
-
+            nextRandom.set(Math.abs(nextRandom.get() * 25214903917L + 11));
+            cbow(i, tempSequence.getElements(),  (int) nextRandom.get() % window ,nextRandom, learningRate);
         }
     }
 
@@ -68,8 +88,72 @@ public class CBOW<T extends SequenceElement> implements ElementsLearningAlgorith
         return false;
     }
 
-    public void iterateSample() {
+    public void iterateSample(int c, T word, AtomicLong nextRandom, double alpha ) {
+        INDArray neu1e = Nd4j.zeros(lookupTable.layerSize());
+    }
 
+    public void cbow(int i, List<T> sentence, int b, AtomicLong nextRandom, double alpha) {
+        int end =  window * 2 + 1 - b;
+        int cw = 0;
+        INDArray neu1 = Nd4j.zeros(lookupTable.layerSize());
+        INDArray neu1e = Nd4j.zeros(lookupTable.layerSize());
+
+        T currentWord = sentence.get(i);
+
+        for(int a = b; a < end; a++) {
+            if(a != window) {
+                int c = i - window + a;
+                if(c >= 0 && c < sentence.size()) {
+                    T lastWord = sentence.get(c);
+
+                    neu1.addiRowVector(syn0.getRow(lastWord.getIndex()));
+                    cw++;
+                }
+            }
+
+        }
+
+        neu1.divi(cw);
+
+        for (int p = 0; p < currentWord.getCodeLength(); p++) {
+            double f = 0;
+            int code = currentWord.getCodes().get(p);
+            int point = currentWord.getPoints().get(p);
+
+            INDArray syn1row = syn1.getRow(point);
+
+            double dot = Nd4j.getBlasWrapper().dot(neu1, syn1.getRow(point));
+
+            if(dot < -MAX_EXP || dot >= MAX_EXP)
+                continue;
+
+            int idx = (int) ((dot + MAX_EXP) * ((double) expTable.length / MAX_EXP / 2.0));
+            if(idx >= expTable.length)
+                continue;
+
+            //score
+            f =  expTable[idx];
+
+            double g = (1 - code - f) * alpha;
+
+            Nd4j.getBlasWrapper().level1().axpy(syn1row.length(),g, syn1row, neu1e);
+            Nd4j.getBlasWrapper().level1().axpy(syn1row.length(),g, neu1, syn1row);
+        }
+
+        for(int a = b; a < end; a++) {
+            if(a != window) {
+                int c = i - window + a;
+                if(c >= 0 && c < sentence.size()) {
+                    T lastWord = sentence.get(c);
+
+                    syn0.getRow(lastWord.getIndex()).addiRowVector(neu1e);
+                    cw++;
+                }
+            }
+        }
+
+      //  logger.info("neu1: " + neu1);
+      //  throw new RuntimeException();
     }
 
     protected Sequence<T> applySubsampling(@NonNull Sequence<T> sequence, @NonNull AtomicLong nextRandom) {
