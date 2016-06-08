f3a7eb26ab3a2f0ef11df87c326e215b9c021e2d
raver119 <raver119@gmail.com>
2016-06-08 13:56:35 +0300

    deprecated legacy plot
    Former-commit-id: 0dc6481e2161b338a9e730520d4147f22a5d490d

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/embeddings/learning/impl/sequence/DM.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/embeddings/learning/impl/sequence/DM.java
@@ -2,6 +2,7 @@ package org.deeplearning4j.models.embeddings.learning.impl.sequence;
 
 import lombok.NonNull;
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
+import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
 import org.deeplearning4j.models.embeddings.learning.ElementsLearningAlgorithm;
 import org.deeplearning4j.models.embeddings.learning.SequenceLearningAlgorithm;
 import org.deeplearning4j.models.embeddings.learning.impl.elements.CBOW;
@@ -10,6 +11,8 @@ import org.deeplearning4j.models.sequencevectors.interfaces.SequenceIterator;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +28,17 @@ public class DM<T extends SequenceElement> implements SequenceLearningAlgorithm<
     private WeightLookupTable<T> lookupTable;
     private VectorsConfiguration configuration;
 
+    protected static double MAX_EXP = 6;
+
+    protected int window;
+    protected boolean useAdaGrad;
+    protected double negative;
+    protected double sampling;
+
+    protected double[] expTable;
+
+    protected INDArray syn0, syn1, syn1Neg, table;
+
     private CBOW<T> cbow = new CBOW<>();
 
     @Override
@@ -39,6 +53,17 @@ public class DM<T extends SequenceElement> implements SequenceLearningAlgorithm<
         this.configuration = configuration;
 
         cbow.configure(vocabCache, lookupTable, configuration);
+
+        this.window = configuration.getWindow();
+        this.useAdaGrad = configuration.isUseAdaGrad();
+        this.negative = configuration.getNegative();
+        this.sampling = configuration.getSampling();
+
+        this.syn0 = ((InMemoryLookupTable<T>) lookupTable).getSyn0();
+        this.syn1 = ((InMemoryLookupTable<T>) lookupTable).getSyn1();
+        this.syn1Neg = ((InMemoryLookupTable<T>) lookupTable).getSyn1Neg();
+        this.expTable = ((InMemoryLookupTable<T>) lookupTable).getExpTable();
+        this.table = ((InMemoryLookupTable<T>) lookupTable).getTable();
     }
 
     @Override
@@ -48,7 +73,7 @@ public class DM<T extends SequenceElement> implements SequenceLearningAlgorithm<
 
     @Override
     public void learnSequence(Sequence<T> sequence, AtomicLong nextRandom, double learningRate) {
-        List<T> seq = cbow.applySubsampling(sequence, nextRandom).getElements();
+        Sequence<T> seq = cbow.applySubsampling(sequence, nextRandom);
 
         List<T> labels = new ArrayList<>();
         labels.addAll(sequence.getSequenceLabels());
@@ -58,7 +83,45 @@ public class DM<T extends SequenceElement> implements SequenceLearningAlgorithm<
         if(seq.isEmpty() || labels.isEmpty())
             return;
 
-        for (T lastWord: labels) {
+        for (int i = 0; i < seq.size(); i++) {
+            nextRandom.set(Math.abs(nextRandom.get() * 25214903917L + 11));
+            dm(i, seq,  (int) nextRandom.get() % window ,nextRandom, learningRate);
+        }
+    }
+
+    public void dm(int i, Sequence<T> sequence, int b, AtomicLong nextRandom, double alpha) {
+        int end =  window * 2 + 1 - b;
+        int cw = 0;
+        INDArray neu1 = Nd4j.zeros(lookupTable.layerSize());
+
+
+        T currentWord = sequence.getElementByIndex(i);
+
+        for(int a = b; a < end; a++) {
+            if(a != window) {
+                int c = i - window + a;
+                if(c >= 0 && c < sequence.size()) {
+                    T lastWord = sequence.getElementByIndex(c);
+
+                    neu1.addiRowVector(syn0.getRow(lastWord.getIndex()));
+                    cw++;
+                }
+            }
+        }
+
+        for (T label: sequence.getSequenceLabels()) {
+            neu1.addiRowVector(syn0.getRow(label.getIndex()));
+            cw++;
+        }
+
+        if (cw == 0)
+            return;
+
+        neu1.divi(cw);
+
+        INDArray neu1e = cbow.iterateSample(currentWord, neu1, nextRandom, alpha);
+
+        for (T label: sequence.getSequenceLabels()) {
 
         }
     }
