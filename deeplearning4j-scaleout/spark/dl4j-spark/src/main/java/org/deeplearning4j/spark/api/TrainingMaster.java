7a4c86846ce40fef0d8e49ecbe48762d3cca3038
Alex Black <blacka101@gmail.com>
2016-06-17 00:50:45 +1000

    API and common implementation for spark training stats functionality
    Former-commit-id: 934033560f7aa4cc2d59e86c39173aa783f9865f

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingMaster.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingMaster.java
@@ -1,6 +1,7 @@
 package org.deeplearning4j.spark.api;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.deeplearning4j.spark.api.stats.SparkTrainingStats;
 import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
 import org.nd4j.linalg.dataset.DataSet;
 
@@ -14,5 +15,28 @@ public interface TrainingMaster<R extends TrainingResult, W extends TrainingWork
     void executeTraining(SparkDl4jMultiLayer network, JavaRDD<DataSet> trainingData);
 
 //    void processResults(SparkDl4jMultiLayer network, JavaRDD<R> results);
+
+    /**
+     * Set whether the training statistics should be collected. Training statistics may include things like per-epoch run times,
+     * time spent waiting for data, etc.
+     * <p>
+     * These statistics are primarily used for debugging and optimization, in order to gain some insight into what aspects
+     * of network training are taking the most time.
+     *
+     * @param collectTrainingStats If true: collecting training statistics will be
+     */
+    void setCollectTrainingStats(boolean collectTrainingStats);
+
+    /**
+     * Get the current setting for collectTrainingStats
+     */
+    boolean getIsCollectTrainingStats();
+
+    /**
+     * Return the training statistics. Note that this may return null, unless setCollectTrainingStats has been set first
+     *
+     * @return Training statistics
+     */
+    SparkTrainingStats getTrainingStats();
 
 }
--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingResult.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingResult.java
@@ -1,7 +1,11 @@
 package org.deeplearning4j.spark.api;
 
+import org.deeplearning4j.spark.api.stats.SparkTrainingStats;
+
 /**
  * Created by Alex on 14/06/2016.
  */
 public interface TrainingResult {
+
+    void setStats(SparkTrainingStats sparkTrainingStats);
 }
