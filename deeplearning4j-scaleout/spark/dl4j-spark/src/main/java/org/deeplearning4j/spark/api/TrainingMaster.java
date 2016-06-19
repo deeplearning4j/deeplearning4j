7e7838e74baa60bcf329799037c2e10df8ac5d4f
Alex Black <blacka101@gmail.com>
2016-06-19 23:00:45 +1000

    Partial implementation of functionality for new SparkComputationGraph implementation

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingMaster.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingMaster.java
@@ -2,8 +2,10 @@ package org.deeplearning4j.spark.api;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.deeplearning4j.spark.api.stats.SparkTrainingStats;
+import org.deeplearning4j.spark.impl.computationgraph.SparkComputationGraph;
 import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
 import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
 
 /**
  * Created by Alex on 14/06/2016.
@@ -12,9 +14,13 @@ public interface TrainingMaster<R extends TrainingResult, W extends TrainingWork
 
     W getWorkerInstance(SparkDl4jMultiLayer network);
 
+    W getWorkerInstance(SparkComputationGraph graph);
+
     void executeTraining(SparkDl4jMultiLayer network, JavaRDD<DataSet> trainingData);
 
-//    void processResults(SparkDl4jMultiLayer network, JavaRDD<R> results);
+    void executeTraining(SparkComputationGraph graph, JavaRDD<DataSet> trainingData);
+
+    void executeTrainingMDS(SparkComputationGraph graph, JavaRDD<MultiDataSet> trainingData);
 
     /**
      * Set whether the training statistics should be collected. Training statistics may include things like per-epoch run times,
