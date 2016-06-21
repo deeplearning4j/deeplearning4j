53e940222c0c503ec4ae639af3f012d9a5fa08aa
eraly <susan.eraly@gmail.com>
2016-06-20 18:01:51 -0700

    Sep tests

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxtC.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxtC.java
@@ -65,53 +65,6 @@ public class TestNdArrReadWriteTxtC extends BaseNd4jTest{
         System.out.println(readBack);
         Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
     }
-
-    //Repeating tests with precision and separator
-    @Test
-    public void TestReadWriteSepPrec() {
-        INDArray origArr = Nd4j.rand('c',3,3).muli(1000); //since we write only four decimal points..
-        Nd4j.writeTxt(origArr, "someArrNew.txt",":",3);
-        INDArray readBack = Nd4j.readTxt("someArrNew.txt",":");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.001);
-    }
-    @Test
-    public void TestReadWriteSimpleSepPrec() {
-        INDArray origArr = Nd4j.rand(1,1).muli(1000); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt","_",3);
-        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.001);
-    }
-    @Test
-    public void TestReadWriteNdSepPrec() {
-        INDArray origArr = Nd4j.rand(13,2,11,3,7,19).muli(10000); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt","_",4);
-        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.0001);
-    }
-    @Test
-    public void TestWierdShapeSepPrec() {
-        INDArray origArr = Nd4j.rand(1,1,2,1,1).muli(10000); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt","_",4);
-        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
-        System.out.println("=========================================================================");
-        System.out.println(origArr);
-        System.out.println("=========================================================================");
-        System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.0001);
-    }
-
     @Override
     public char ordering() {
         return 'c';
--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxtOpt.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/ndarray/TestNdArrReadWriteTxtOpt.java
@@ -14,59 +14,61 @@ import org.nd4j.linalg.ops.transforms.Transforms;
  * Created by susaneraly on 6/18/16.
  */
 @RunWith(Parameterized.class)
-public class TestNdArrReadWriteTxt extends BaseNd4jTest{
+public class TestNdArrReadWriteTxtOpt extends BaseNd4jTest{
 
-    public TestNdArrReadWriteTxt(Nd4jBackend backend) {
-
+    public TestNdArrReadWriteTxtOpt(Nd4jBackend backend) {
         super(backend);
     }
 
+    //Repeating tests with precision and separator
     @Test
-    public void TestReadWrite() {
-        INDArray origArr = Nd4j.rand('f',10,10).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
+    public void TestReadWriteSepPrec() {
+        INDArray origArr = Nd4j.rand('f',3,3).muli(1000); //since we write only four decimal points..
+        Nd4j.writeTxt(origArr, "someArrNew.txt",":",3);
+        INDArray readBack = Nd4j.readTxt("someArrNew.txt",":");
         System.out.println("=========================================================================");
         System.out.println(origArr);
         System.out.println("=========================================================================");
         System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
+        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.001);
     }
     @Test
-    public void TestReadWriteSimple() {
-        INDArray origArr = Nd4j.rand(1,1).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
+    public void TestReadWriteSimpleSepPrec() {
+        INDArray origArr = Nd4j.rand(1,1).muli(1000); //since we write only two decimal points..
+        Nd4j.writeTxt(origArr, "someArr.txt","_",3);
+        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
         System.out.println("=========================================================================");
         System.out.println(origArr);
         System.out.println("=========================================================================");
         System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
+        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.001);
     }
     @Test
-    public void TestReadWriteNd() {
-        INDArray origArr = Nd4j.rand(13,2,11,3,7,19).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
+    public void TestReadWriteNdSepPrec() {
+        INDArray origArr = Nd4j.rand(13,2,11,3,7,19).muli(10000); //since we write only two decimal points..
+        Nd4j.writeTxt(origArr, "someArr.txt","_",4);
+        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
         System.out.println("=========================================================================");
         System.out.println(origArr);
         System.out.println("=========================================================================");
         System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
+        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.0001);
     }
     @Test
-    public void TestWierdShape() {
-        INDArray origArr = Nd4j.rand(1,1,2,1,1).muli(100); //since we write only two decimal points..
-        Nd4j.writeTxt(origArr, "someArr.txt");
-        INDArray readBack = Nd4j.readTxt("someArr.txt");
+    public void TestWierdShapeSepPrec() {
+        INDArray origArr = Nd4j.rand(1,1,2,1,1).muli(10000); //since we write only two decimal points..
+        Nd4j.writeTxt(origArr, "someArr.txt","_",4);
+        INDArray readBack = Nd4j.readTxt("someArr.txt","_");
         System.out.println("=========================================================================");
         System.out.println(origArr);
         System.out.println("=========================================================================");
         System.out.println(readBack);
-        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.01);
+        Assert.isTrue(Transforms.abs(origArr.subi(readBack)).maxNumber().doubleValue() < 0.0001);
     }
+
     @Override
     public char ordering() {
         return 'f';
     }
 }
+
