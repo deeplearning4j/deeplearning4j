b236670929c0556b7db655b90e3ba3f02133f115
raver119 <raver119@gmail.com>
2016-06-23 13:15:13 +0300

    affinity logger

--- nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/main/java/org/nd4j/jita/concurrency/CudaAffinityManager.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/main/java/org/nd4j/jita/concurrency/CudaAffinityManager.java
@@ -3,6 +3,8 @@ package org.nd4j.jita.concurrency;
 import org.nd4j.jita.conf.Configuration;
 import org.nd4j.jita.conf.CudaEnvironment;
 import org.nd4j.linalg.api.concurrency.BasicAffinityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CudaAffinityManager extends BasicAffinityManager {
 
     private static final Configuration configuration = CudaEnvironment.getInstance().getConfiguration();
+
+    private static Logger logger = LoggerFactory.getLogger(CudaAffinityManager.class);
 
     private Map<Long, Integer> affinityMap = new ConcurrentHashMap<>();
     private AtomicInteger devPtr = new AtomicInteger(0);
@@ -59,8 +63,13 @@ public class CudaAffinityManager extends BasicAffinityManager {
                 device = devices.get(devPtr.getAndIncrement());
                 if (devPtr.get() >= devices.size())
                     devPtr.set(0);
+
+                logger.debug("Mapping to device [{}], out of [{}] devices...", device, devices.size());
             }
-        } else device = configuration.getAvailableDevices().get(0);
+        } else {
+            device = configuration.getAvailableDevices().get(0);
+            logger.debug("Single device is forced, mapping to device [{}]", device);
+        }
 
         return device;
     }
