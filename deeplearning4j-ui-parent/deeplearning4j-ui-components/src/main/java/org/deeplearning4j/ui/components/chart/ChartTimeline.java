761f1d6c37dd20c86d8b6e40c35e3d25f7048bc6
Alex Black <blacka101@gmail.com>
2016-06-27 17:37:10 +1000

    Add UI timeline chart
    Former-commit-id: 965468c05873c6de95070d56914fd252d15640ce [formerly 6a6c36e1b27f6e09edc80a6c546dc2dd62f4d228]
    Former-commit-id: 534e72862a84937fdb5988aa46a53dd2443be64b

---
+++ deeplearning4j-ui-parent/deeplearning4j-ui-components/src/main/java/org/deeplearning4j/ui/components/chart/ChartTimeline.java
@@ -0,0 +1,71 @@
+package org.deeplearning4j.ui.components.chart;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.deeplearning4j.ui.components.chart.style.StyleChart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A timeline/swimlane chart with zoom/scroll functionality.
+ *
+ * Time is represented here with long values - i.e., millisecond precision, epoch format
+ *
+ *
+ * @author Alex Black
+ */
+@Data
+@EqualsAndHashCode(callSuper=true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ChartTimeline extends Chart {
+    public static final String COMPONENT_TYPE = "ChartTimeline";
+
+    private List<String> laneNames = new ArrayList<>();
+    private List<List<TimelineEntry>> laneData = new ArrayList<>();
+
+    public ChartTimeline(){
+        super(COMPONENT_TYPE);
+        //no-arg constructor for Jackson
+    }
+
+    private ChartTimeline(Builder builder){
+        super(COMPONENT_TYPE, builder);
+        this.laneNames = builder.laneNames;
+        this.laneData = builder.laneData;
+    }
+
+
+
+    public static class Builder extends Chart.Builder<Builder> {
+        private List<String> laneNames = new ArrayList<>();
+        private List<List<TimelineEntry>> laneData = new ArrayList<>();
+
+
+        public Builder(String title, StyleChart style){
+            super(title, style);
+        }
+
+
+        public Builder addLane(String name, List<TimelineEntry> data){
+            laneNames.add(name);
+            laneData.add(data);
+            return this;
+        }
+
+        public ChartTimeline build(){
+            return new ChartTimeline(this);
+        }
+    }
+
+
+    @AllArgsConstructor @NoArgsConstructor @Data
+    public static class TimelineEntry {
+        private String entryLabel;
+        private long startTimeMs;
+        private long endTimeMs;
+    }
+}
