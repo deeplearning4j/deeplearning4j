edbd65df0fa2a9931566834f83c2b3cc12bca8fb
Alex Black <blacka101@gmail.com>
2016-07-01 00:20:01 +1000

    Javadoc for reportition enum; also remove debugging log messages
    Former-commit-id: 5db94555a61244d10ccb397fbf86d4bfe22434da

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/Repartition.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/Repartition.java
@@ -1,10 +1,15 @@
 package org.deeplearning4j.spark.api;
 
 /**
- * Created by Alex on 30/06/2016.
+ * Enumeration that is used for specifying the behaviour of repartitioning in {@link org.deeplearning4j.spark.impl.paramavg.ParameterAveragingTrainingMaster}
+ * (and possibly elsewhere.
+ *
+ * "Never" and "Always" repartition options are as expected; the "NumPartitionsExecutorsDiffers" will repartition data if and only
+ * if the number of partitions is not equal to the number of executors
+ *
+ * @author Alex Black
  */
 public enum Repartition {
-
     Never,
     Always,
     NumPartitionsExecutorsDiffers
