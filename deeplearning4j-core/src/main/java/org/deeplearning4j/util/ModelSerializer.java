77fa5d73155132e44dcd61d0f412e5bfec9ccf0f
raver119 <raver119@gmail.com>
2016-07-03 12:51:42 +0300

    pre-merge cleanup
    Former-commit-id: 28099273d783f993f73306cc3feca3273b110c07

--- deeplearning4j-core/src/main/java/org/deeplearning4j/util/ModelSerializer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/util/ModelSerializer.java
@@ -11,6 +11,7 @@ import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.updater.graph.ComputationGraphUpdater;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.heartbeat.reports.Task;
 
