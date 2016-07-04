6dbad968dd09bbc701efafeb2d601a33c4ee8748
Alex Black <blacka101@gmail.com>
2016-07-04 23:51:52 +1000

    Additional test for preprocessed data and PortableDataStreamDataSetIterator

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/canova/iterator/PortableDataStreamDataSetIterator.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/canova/iterator/PortableDataStreamDataSetIterator.java
@@ -5,12 +5,16 @@ import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
+import java.io.OutputStream;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 /**
- * Created by Alex on 04/07/2016.
+ * A DataSetIterator that loads serialized DataSet objects (saved with {@link DataSet#save(OutputStream)}) from
+ * a {@link PortableDataStream}, usually obtained from SparkContext.binaryFiles()
+ *
+ * @author Alex Black
  */
 public class PortableDataStreamDataSetIterator implements DataSetIterator {
 
