276a2378108c9d6fa86c7ed290b432372c5650d3
nyghtowl <warrick.melanie@gmail.com>
2016-07-08 14:06:10 -0700

    Fix test for lfw and flow

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
@@ -83,7 +83,7 @@ public class LFWDataSetIterator extends RecordReaderDataSetIterator {
      * @param rng random number to lock in batch shuffling
 	 * */
 	public LFWDataSetIterator(int batchSize, int numExamples, int[] imgDim, int numLabels, boolean useSubset, PathLabelGenerator labelGenerator, boolean train, double splitTrainTest, ImageTransform imageTransform, int normalizeValue, Random rng) {
-		super(new LFWLoader(imgDim, imageTransform, normalizeValue, useSubset).getRecordReader(numExamples, batchSize, imgDim, numLabels, labelGenerator, train, splitTrainTest, rng), batchSize, 1, numLabels);
+		super(new LFWLoader(imgDim, imageTransform, normalizeValue, useSubset).getRecordReader(batchSize, numExamples, imgDim, numLabels, labelGenerator, train, splitTrainTest, rng), batchSize, 1, numLabels);
 	}
 
 }
