d8f52cf4020a7eefc4a343434b40dcf0e774c386
nyghtowl <warrick.melanie@gmail.com>
2016-07-11 10:04:45 -0700

    Switch to datavec
    Former-commit-id: 0bc115d7c7311943f6bfd63131db0258778931cf [formerly 2cc910722b8e22d5978ae7a32c150513b374d07f]
    Former-commit-id: 2b749746a2f1080c7f2d3aea4f348fdca9c4cc39

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
@@ -18,10 +18,11 @@
 
 package org.deeplearning4j.datasets.iterator.impl;
 
-import org.canova.api.io.labels.PathLabelGenerator;
-import org.canova.image.loader.LFWLoader;
-import org.canova.image.transform.ImageTransform;
-import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
+
+import org.datavec.api.io.labels.PathLabelGenerator;
+import org.datavec.image.loader.LFWLoader;
+import org.datavec.image.transform.ImageTransform;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 
 import java.util.Random;
 
@@ -64,7 +65,7 @@ public class LFWDataSetIterator extends RecordReaderDataSetIterator {
     }
 
     /** Loads images with given  batchSize, numExamples, imgDim, numLabels, useSubset, train, splitTrainTest & Random returned by the generator. */
-    public LFWDataSetIterator(int batchSize, int numExamples, int[] imgDim, int numLabels, boolean useSubset, PathLabelGenerator labelGenerator, boolean train, double splitTrainTest,  Random rng) {
+    public LFWDataSetIterator(int batchSize, int numExamples, int[] imgDim, int numLabels, boolean useSubset, PathLabelGenerator labelGenerator, boolean train, double splitTrainTest, Random rng) {
         this(batchSize, numExamples, imgDim, numLabels, useSubset, labelGenerator, train, splitTrainTest, null, 0, rng);
     }
 
