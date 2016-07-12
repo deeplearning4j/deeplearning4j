dab37be239ceaf0dc9d089e658708c6b3ace17e3
Ben McCann <benjamin.j.mccann@gmail.com>
2016-07-11 17:19:33 -0700

    Fix comment formatting (#1809)
    Former-commit-id: 5740dbb71e307899f86b1a579cbe1e93e8a896f2

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/api/Updater.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/api/Updater.java
@@ -20,11 +20,12 @@ public interface Updater extends Serializable, Cloneable {
      */
     void update(Layer layer, Gradient gradient, int iteration, int miniBatchSize);
 
-    /** Given this updater, get an UpdaterAggregator that can be used to combine the current state (if any)
+    /**
+     * Given this updater, get an UpdaterAggregator that can be used to combine the current state (if any)
      * of this updater with the state of other updaters.
      * Typically used only in distributed learning scenarios (where each copy of the network has an updater
      * that needs to be combined)
-      * @param addThis whether to add the Updater to the UpdaterAggregator, or just return an empy aggregator
+     * @param addThis whether to add the Updater to the UpdaterAggregator, or just return an empy aggregator
      */
     UpdaterAggregator getAggregator(boolean addThis);
 
