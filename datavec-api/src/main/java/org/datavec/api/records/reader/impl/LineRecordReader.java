629821957f9c9f75ad77769a494a852dc51d5300
Adam Gibson <adam@skymind.io>
2016-07-16 18:48:44 +0900

    replicate PR from canova

--- datavec-api/src/main/java/org/datavec/api/records/reader/impl/LineRecordReader.java
+++ datavec-api/src/main/java/org/datavec/api/records/reader/impl/LineRecordReader.java
@@ -1,17 +1,21 @@
 /*
- *  * Copyright 2016 Skymind, Inc.
+ *
  *  *
- *  *    Licensed under the Apache License, Version 2.0 (the "License");
- *  *    you may not use this file except in compliance with the License.
- *  *    You may obtain a copy of the License at
+ *  *  * Copyright 2015 Skymind,Inc.
+ *  *  *
+ *  *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *    you may not use this file except in compliance with the License.
+ *  *  *    You may obtain a copy of the License at
+ *  *  *
+ *  *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *  *
+ *  *  *    Unless required by applicable law or agreed to in writing, software
+ *  *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *    See the License for the specific language governing permissions and
+ *  *  *    limitations under the License.
  *  *
- *  *        http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  *    Unless required by applicable law or agreed to in writing, software
- *  *    distributed under the License is distributed on an "AS IS" BASIS,
- *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *    See the License for the specific language governing permissions and
- *  *    limitations under the License.
+ *
  */
 
 package org.datavec.api.records.reader.impl;
@@ -19,11 +23,12 @@ package org.datavec.api.records.reader.impl;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
 import org.datavec.api.conf.Configuration;
-import org.datavec.api.writable.Text;
 import org.datavec.api.records.reader.BaseRecordReader;
+import org.datavec.api.split.FileSplit;
 import org.datavec.api.split.InputSplit;
 import org.datavec.api.split.InputStreamInputSplit;
 import org.datavec.api.split.StringSplit;
+import org.datavec.api.writable.Text;
 import org.datavec.api.writable.Writable;
 
 import java.io.*;
@@ -132,7 +137,7 @@ public class LineRecordReader extends BaseRecordReader {
 
     @Override
     public void setConf(Configuration conf) {
-       this.conf = conf;
+        this.conf = conf;
     }
 
     @Override
@@ -149,6 +154,7 @@ public class LineRecordReader extends BaseRecordReader {
         if(inputSplit == null) throw new UnsupportedOperationException("Cannot reset without first initializing");
         try{
             initialize(inputSplit);
+            currIndex = 0;
         }catch(Exception e){
             throw new RuntimeException("Error during LineRecordReader reset",e);
         }
