9fda9d9a24479dc852a534efece850dbbd1ff592
Alex Black <blacka101@gmail.com>
2016-07-21 14:35:24 +1000

    Remove unnecessary (and problematic) dropwizard configuration options for UI

--- arbiter-core/src/main/java/org/deeplearning4j/arbiter/optimize/ui/ArbiterUIConfig.java
+++ arbiter-core/src/main/java/org/deeplearning4j/arbiter/optimize/ui/ArbiterUIConfig.java
@@ -26,30 +26,5 @@ import org.hibernate.validator.constraints.NotEmpty;
  */
 public class ArbiterUIConfig extends Configuration {
 
-    @NotEmpty
-    private String template;
-
-    @NotEmpty
-    private String defaultName = "ArbiterConf";
-
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
-
-    @JsonProperty
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
-    }
 
 }
