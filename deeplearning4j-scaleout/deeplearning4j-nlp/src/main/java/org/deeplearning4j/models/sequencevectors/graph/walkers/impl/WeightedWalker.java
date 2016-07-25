d94ae94e55bb7d0a2382beaf36c8e5c5054763f0
raver119 <raver119@gmail.com>
2016-07-25 16:05:22 +0300

    RandomWalker draft, p.0
    Former-commit-id: d11c2a0a5ade3d8d543323e79a6a3dd3e46d1067 [formerly 1d18cfe1a8dfa8a5d5476990319f23eff39f15cb]
    Former-commit-id: 2de249623cf57f9bcd9c823c797b07acc6895208

--- deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/graph/walkers/impl/WeightedWalker.java
+++ deeplearning4j-scaleout/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/graph/walkers/impl/WeightedWalker.java
@@ -1,17 +1,26 @@
 package org.deeplearning4j.models.sequencevectors.graph.walkers.impl;
 
+import lombok.NonNull;
+import org.deeplearning4j.models.sequencevectors.graph.enums.NoEdgeHandling;
+import org.deeplearning4j.models.sequencevectors.graph.enums.WalkDirection;
 import org.deeplearning4j.models.sequencevectors.graph.primitives.IGraph;
 import org.deeplearning4j.models.sequencevectors.graph.walkers.GraphWalker;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
 
 /**
+ * This is vertex eight-based walker for SequenceVectors-based DeepWalk implementation.
+ * Instead of random walks, this walker produces walks based on weight of the vertices.
  *
- * WORK IS IN PROGRESS, DO NOT USE THIS
- *
+ * @author AlexDBlack
  * @author raver119@gmail.com
+ * Based on Alex Black WeightedWalkIterator implementation
  */
 public class WeightedWalker<T extends SequenceElement> extends RandomWalker<T>  implements GraphWalker<T> {
+
+    protected WeightedWalker(IGraph<T, ?> sourceGraph) {
+
+    }
 
     /**
      * This method checks, if walker has any more sequences left in queue
@@ -43,11 +52,77 @@ public class WeightedWalker<T extends SequenceElement> extends RandomWalker<T>  
         super.reset(shuffle);
     }
 
-    public static class Builder<T extends SequenceElement> extends RandomWalker.Builder<T> {
+    public static class Builder<T extends SequenceElement> extends RandomWalker.Builder<T>  {
 
         public Builder(IGraph<T, ?> sourceGraph) {
             super(sourceGraph);
         }
 
+        /**
+         * This method specifies output sequence (walk) length
+         *
+         * @param walkLength
+         * @return
+         */
+        @Override
+        public Builder<T> setWalkLength(int walkLength) {
+            super.setWalkLength(walkLength);
+            return this;
+        }
+
+        /**
+         * This method defines walker behavior when it gets to node which has no next nodes available
+         * Default value: RESTART_ON_DISCONNECTED
+         *
+         * @param handling
+         * @return
+         */
+        @Override
+        public Builder<T> setNoEdgeHandling(@NonNull NoEdgeHandling handling) {
+            super.setNoEdgeHandling(handling);
+            return this;
+        }
+
+        /**
+         * This method specifies random seed.
+         *
+         * @param seed
+         * @return
+         */
+        @Override
+        public Builder<T> setSeed(long seed) {
+            super.setSeed(seed);
+            return this;
+        }
+
+        /**
+         * This method defines next hop selection within walk
+         *
+         * @param direction
+         * @return
+         */
+        @Override
+        public Builder<T> setWalkDirection(@NonNull WalkDirection direction) {
+            super.setWalkDirection(direction);
+            return this;
+        }
+
+        /**
+         * This method defines a chance for walk restart
+         * Good value would be somewhere between 0.03-0.07
+         *
+         * @param alpha
+         * @return
+         */
+        @Override
+        public RandomWalker.Builder<T> setRestartProbability(double alpha) {
+            return super.setRestartProbability(alpha);
+        }
+
+        public WeightedWalker<T> build() {
+            WeightedWalker<T> walker = new WeightedWalker<>(sourceGraph);
+
+            return walker;
+        }
     }
 }
