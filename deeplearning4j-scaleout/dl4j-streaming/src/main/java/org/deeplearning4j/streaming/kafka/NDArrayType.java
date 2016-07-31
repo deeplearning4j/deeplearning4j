5fd33a86d81b100946106a77bde8e8891ff61572
Adam Gibson <adam@skymind.io>
2016-07-31 02:13:38 -0400

    add pub/sub for kafka
    Former-commit-id: 3ac191f8306094a6804fb1dd34afeaaf77713eda

---
+++ deeplearning4j-scaleout/dl4j-streaming/src/main/java/org/deeplearning4j/streaming/kafka/NDArrayType.java
@@ -0,0 +1,11 @@
+package org.deeplearning4j.streaming.kafka;
+
+/**
+ * Used for headers to distinguish
+ * the type of ndarray message
+ * being sent.
+ * @author Adam Gibson
+ */
+public enum NDArrayType {
+    SINGLE, MULTI
+}
