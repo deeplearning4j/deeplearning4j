541048ed301dda211eaf36b1401736632274e55f
Alex Black <blacka101@gmail.com>
2016-08-10 16:51:56 +1000

    InputType refactoring: api and CNN/RNN layers
    Former-commit-id: 4be7dfe8341c9a0d43f483a3106d2f022ba5dc1a [formerly 9343a34dc706f186d1a7f888bf27a23d1080765a]
    Former-commit-id: fb0a7ae90bc74905ee7e68671dce4c8e6054fb07

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/BaseRecurrentLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/BaseRecurrentLayer.java
@@ -1,18 +1,30 @@
 package org.deeplearning4j.nn.conf.layers;
 
 import lombok.*;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 
-@Data @NoArgsConstructor
+@Data
+@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class BaseRecurrentLayer extends FeedForwardLayer {
-	
-	protected BaseRecurrentLayer(Builder builder){
-		super(builder);
-	}
-	
-	@AllArgsConstructor
+public abstract class BaseRecurrentLayer extends FeedForwardLayer {
+
+    protected BaseRecurrentLayer(Builder builder) {
+        super(builder);
+    }
+
+    @Override
+    public InputType getOutputType(InputType inputType) {
+        if (inputType == null || inputType.getType() != InputType.Type.RNN || ((InputType.InputTypeRecurrent) inputType).getSize() <= 0) {
+            throw new IllegalStateException("Invalid input for RNN layer: expect RNN input type with size > 0. Got: " + inputType);
+        }
+
+        return InputType.recurrent(nOut);
+    }
+
+    @AllArgsConstructor
     public static abstract class Builder<T extends Builder<T>> extends FeedForwardLayer.Builder<Builder<T>> {
 
     }
+
 }
