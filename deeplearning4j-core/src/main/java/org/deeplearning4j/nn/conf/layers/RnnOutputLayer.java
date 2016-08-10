52d196fac4c05ddb371fa0464e7f231637b10132
Alex Black <blacka101@gmail.com>
2016-08-10 17:09:18 +1000

    InputType methods for remaining layers
    Former-commit-id: d7393cc9b6adb6ea3a779ed33e21a162062f8f92 [formerly ac527a48c05824169b35f51912baf9a8438bc230]
    Former-commit-id: f671613a665c6ca0ac364254efc0b13965ccb4f1

--- deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/RnnOutputLayer.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/nn/conf/layers/RnnOutputLayer.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
 
 @Data @NoArgsConstructor
@@ -14,6 +15,14 @@ public class RnnOutputLayer extends BaseOutputLayer {
 	private RnnOutputLayer(Builder builder){
 		super(builder);
 	}
+
+    @Override
+    public InputType getOutputType(InputType inputType) {
+        if(inputType == null || inputType.getType() != InputType.Type.RNN){
+            throw new IllegalStateException("Invalid input type: Expected RNN input, got " + inputType);
+        }
+        return InputType.recurrent(nOut);
+    }
 
     public static class Builder extends BaseOutputLayer.Builder<Builder> {
 
