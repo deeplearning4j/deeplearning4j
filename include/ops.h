9669dfc6aeeeece6deaa744fa9c90d9275741b40
raver119 <raver119@gmail.com>
2016-08-20 18:38:20 +0300

    predicate & postulate draft

--- include/ops.h
+++ include/ops.h
@@ -1764,6 +1764,58 @@ template<typename T>
             return d1;
         }
 	};
+
+/**
+ * Special case here: MetaOp which consist of 2 operations.
+ *
+ * Predicate can be either scalar or transform, to process data before actual op call
+ * Postulate will be the scalar/transform, but will be applied to result of broadcast/reduce/reduce3
+ */
+template<typename T, typename OpTypeA, typename OpTypeB>
+	class MetaOp {
+	public:
+		no_op_exec_special
+		no_op_exec_special_cuda
+
+		/*
+		 * PREDICATE
+		 */
+
+		// scalar, transform, reduce, indexreduce entry
+		op_def static T op(T d1, T *params) {
+			/*
+			 * We assume, that params for MetaOp is a set of pointers to actual op A & B extraArgs
+			 */
+			Nd4jPointer *wrap = reinterpret_cast<Nd4jPointer *> (params);
+			T *paramsA = reinterpret_cast<T *> (&wrap[0]);
+			T *paramsB = reinterpret_cast<T *> (&wrap[1]);
+
+			return OpTypeB::op(OpTypeA::op(d1, paramsA), paramsB);
+		}
+
+		// PWT, broadcast entry. Predicate can be only scalar, transform
+		op_def static T op(T d1, T d2, T *params) {
+			Nd4jPointer *wrap = reinterpret_cast<Nd4jPointer *> (params);
+			T *paramsA = reinterpret_cast<T *> (&wrap[0]);
+			T *paramsB = reinterpret_cast<T *> (&wrap[1]);
+
+			return OpTypeB::op(OpTypeA::op(d1, paramsA), d2, paramsB);
+		}
+
+		/*
+		 * POSTULATE
+		 */
+
+		// will be called for reduce, reduce3
+		op_def static T postProcess(T reduction, Nd4jIndex n, T *params) {
+			Nd4jPointer *wrap = reinterpret_cast<Nd4jPointer *> (params);
+			T *paramsA = reinterpret_cast<T *> (&wrap[0]);
+			T *paramsB = reinterpret_cast<T *> (&wrap[1]);
+
+			return OpTypeB::op(OpTypeA::postProcess(reduction, n, paramsA), paramsB);
+		}
+
+	};
 }
 
 #endif
