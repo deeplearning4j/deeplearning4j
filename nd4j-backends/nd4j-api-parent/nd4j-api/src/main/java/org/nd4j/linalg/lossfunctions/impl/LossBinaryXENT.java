623b2ede18810b2320c49ebc5f207ff7d315e49a
eraly <susan.eraly@gmail.com>
2016-08-22 12:41:01 -0700

    log loss

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossBinaryXENT.java
@@ -0,0 +1,85 @@
+package org.nd4j.linalg.lossfunctions.impl;
+
+import org.apache.commons.math3.util.Pair;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.transforms.LogSoftMax;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.ILossFunction;
+import org.nd4j.linalg.ops.transforms.Transforms;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by susaneraly on 8/22/16.
+ */
+public class LossBinaryXENT implements ILossFunction {
+
+    private static Logger logger = LoggerFactory.getLogger(LossBinaryXENT.class);
+
+    private INDArray scoreArray(INDArray labels, INDArray preOutput, String activationFn, INDArray mask){
+        INDArray scoreArr;
+        if("softmax".equals(activationFn)){
+            //Use LogSoftMax op to avoid numerical issues when calculating score
+            INDArray logsoftmax = Nd4j.getExecutioner().execAndReturn(new LogSoftMax(preOutput.dup()));
+            scoreArr = labels.mul(logsoftmax);
+
+        } else {
+            logger.info("API_USE_INFO: In the case of classification where the labels are a one hot vector, please use a softmax function for activation.");
+            INDArray output = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(activationFn, preOutput.dup()));
+            scoreArr = labels.mul(Transforms.log(output, false));
+        }
+        if(mask != null) scoreArr.muliColumnVector(mask);
+        return scoreArr;
+    }
+
+    @Override
+    public double computeScore(INDArray labels, INDArray preOutput, String activationFn, INDArray mask, boolean average) {
+        INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
+
+        double score = -scoreArr.sumNumber().doubleValue();
+
+        if(average){
+            score /= scoreArr.size(0);
+        }
+
+        return score;
+    }
+
+    @Override
+    public INDArray computeScoreArray(INDArray labels, INDArray preOutput, String activationFn, INDArray mask) {
+        INDArray scoreArr = scoreArray(labels, preOutput, activationFn, mask);
+        return scoreArr.sum(1).mul(-1);
+    }
+
+    @Override
+    public INDArray computeGradient(INDArray labels, INDArray preOutput, String activationFn, INDArray mask) {
+        INDArray grad;
+        INDArray output = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(activationFn, preOutput.dup()));
+
+        if("softmax".equals(activationFn)) {
+            grad = output.sub(labels);
+        }
+        else {
+            INDArray outputder = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(activationFn, preOutput.dup()).derivative());
+            grad = labels.mul(outputder);
+            grad.divi(output).muli(-1);
+        }
+
+        return grad;
+    }
+
+    @Override
+    public Pair<Double, INDArray> computeGradientAndScore(INDArray labels, INDArray preOutput, String activationFn, INDArray mask, boolean average) {
+        //TODO: probably a more efficient way to do this...
+
+        return new Pair<>(
+                computeScore(labels, preOutput, activationFn, mask, average),
+                computeGradient(labels, preOutput, activationFn, mask));
+    }
+
+
+    @Override
+    public String toString(){
+        return "LossBinaryXENT()";
+    }
+}
