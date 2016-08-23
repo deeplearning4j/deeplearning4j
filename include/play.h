73f89122ea19dffd5eee988b55765d825768ccd8
raver119 <raver119@gmail.com>
2016-08-23 12:40:32 +0300

    macro working

--- include/play.h
+++ include/play.h
@@ -8,19 +8,48 @@
 #include <op_boilerplate.h>
 
 #define PAIRWISE_TRANSFORM_OPS \
-        (0, PWT::Add),\
-        (1, PWT::Copy), \
-        (2, PWT::Divide)
+        (0, simdOps::Add),\
+        (1, simdOps::Copy),\
+        (2, simdOps::Divide),\
+        (3, simdOps::EqualTo),\
+        (4, simdOps::GreaterThan),\
+        (5, simdOps::LessThan),\
+        (6, simdOps::Multiply),\
+        (7, simdOps::ReverseDivide),\
+        (8, simdOps::ReverseSubtract),\
+        (9, simdOps::Subtract),\
+        (10,simdOps::Epsilon),\
+        (11,simdOps::GreaterThanOrEqual),\
+        (12,simdOps::LessThanOrEqual),\
+        (13,simdOps::Max),\
+        (14,simdOps::Min),\
+        (15,simdOps::NotEqualTo),\
+        (16,simdOps::Copy),\
+        (17,simdOps::Axpy),\
+        (45,simdOps::CompareAndSet),\
+        (46,simdOps::CompareAndReplace)
 
 
 #define SCALAR_OPS \
-        (12,SCALAR::Min), \
-        (13,SCALAR::Copy),\
-        (14,SCALAR::Mod)
-
+        (0, simdOps::Add),\
+        (1, simdOps::Subtract),\
+        (2, simdOps::Multiply),\
+        (3, simdOps::Divide),\
+        (4, simdOps::ReverseDivide),\
+        (5, simdOps::ReverseSubtract),\
+        (6, simdOps::Max),\
+        (7, simdOps::LessThan),\
+        (8, simdOps::GreaterThan),\
+        (9, simdOps::EqualTo),\
+        (10,simdOps::LessThanOrEqual),\
+        (11,simdOps::NotEqualTo),\
+        (12,simdOps::Min),\
+        (13,simdOps::Copy),\
+        (14,simdOps::Mod),\
+        (15,simdOps::ReverseMod),\
+        (16,simdOps::GreaterThanOrEqual)
 
     DISPATCH_METAOP(transformCuda, PARAMS(N, dx, dy, xStride, yStride, paramsPtr, dz, zStride, nullptr, nullptr, nullptr), OPS_A(SCALAR_OPS), OPS_B(PAIRWISE_TRANSFORM_OPS))
-
 
 
 #endif //LIBND4J_PLAY_H
