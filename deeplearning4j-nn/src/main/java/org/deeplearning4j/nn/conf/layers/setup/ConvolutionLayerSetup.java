9e0258b4ca7a376ca3786eaf07410761362491b2
Alex Black <blacka101@gmail.com>
2016-09-02 00:07:11 +1000

    Better InputType validation for CNNs; more test fixes
    Former-commit-id: fd7313654e9b8147c477834a9e2087223d59e31d

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/setup/ConvolutionLayerSetup.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/setup/ConvolutionLayerSetup.java
@@ -17,7 +17,10 @@ public class ConvolutionLayerSetup {
      * @param height initial height of the data
      * @param width initial width of the data
      * @param channels initial number of channels in the data
+     * @deprecated Use {@link org.deeplearning4j.nn.conf.MultiLayerConfiguration.Builder#setInputType(InputType)} to set nIns
+     * and add preprocessors as required. This can be done using {@code builder.setInputType(InputType.convolutional(height, width, channels))}
      */
+    @Deprecated
     public ConvolutionLayerSetup(MultiLayerConfiguration.Builder builder,int height,int width,int channels) {
         builder.setInputType(InputType.convolutional(height, width, channels));
     }
