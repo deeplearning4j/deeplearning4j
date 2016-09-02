ef4a58c1eacf60398a2e3313db56096bdbeb9230
raver119 <raver119@gmail.com>
2016-09-02 19:58:26 +0300

    test for double reduce

--- nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/test/java/jcuda/jcublas/ops/SporadicTests.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-cuda-7.5/src/test/java/jcuda/jcublas/ops/SporadicTests.java
@@ -1,9 +1,15 @@
 package jcuda.jcublas.ops;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.nd4j.jita.conf.CudaEnvironment;
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.api.buffer.util.DataTypeUtil;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.transforms.IsMax;
 import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,6 +17,11 @@ import static org.junit.Assert.assertEquals;
  * @author raver119@gmail.com
  */
 public class SporadicTests {
+
+    @Before
+    public void setUp() throws Exception {
+        CudaEnvironment.getInstance().getConfiguration().enableDebug(false).setVerbose(true);
+    }
 
     @Test
     public void testIsMax1() throws Exception {
@@ -26,5 +37,24 @@ public class SporadicTests {
         assertEquals(0.0, arrC.getDouble(1), 0.1);
         assertEquals(1.0, arrC.getDouble(2), 0.1);
         assertEquals(1.0, arrC.getDouble(3), 0.1);
+    }
+
+    @Test
+    public void randomStrangeTest() {
+        DataBuffer.Type type = Nd4j.dataType();
+        DataTypeUtil.setDTypeForContext(DataBuffer.Type.DOUBLE);
+
+        int a=9;
+        int b=2;
+        int[] shapes = new int[a];
+        for (int i = 0; i < a; i++) {
+            shapes[i] = b;
+        }
+        INDArray c = Nd4j.linspace(1, (int) (100 * 1 + 1 + 2), (int) Math.pow(b, a)).reshape(shapes);
+        c=c.sum(0);
+        double[] d = c.data().asDouble();
+        System.out.println("d: " + Arrays.toString(d));
+
+        DataTypeUtil.setDTypeForContext(type);
     }
 }
