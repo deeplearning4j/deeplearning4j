604dd3b732cbd06cb8c6306d064bb1ccd59c84f9
eraly <susan.eraly@gmail.com>
2016-09-05 16:32:00 -0700

    iloss
    Former-commit-id: 1320630a1c6453bc140cce9a114215bb4a6ab812 [formerly 417ebbe60e0c010e41c61133f9c36a9eae5e303e]
    Former-commit-id: 3e478b271776fefc27e6388b12217bd2c68ddd71

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/BaseOutputLayer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/BaseOutputLayer.java
@@ -4,23 +4,28 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.nd4j.linalg.lossfunctions.ILossFunction;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+import org.nd4j.linalg.lossfunctions.impl.LossMCXENT;
 
 @Data @NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public abstract class BaseOutputLayer extends FeedForwardLayer {
 	protected LossFunction lossFunction;
+    protected ILossFunction iLossFunction;
     protected String customLossFunction;
 
     protected BaseOutputLayer(Builder builder) {
     	super(builder);
         this.lossFunction = builder.lossFunction;
         this.customLossFunction = builder.customLossFunction;
+        this.iLossFunction = builder.iLossFunction;
     }
     
     public static abstract class Builder<T extends Builder<T>> extends FeedForwardLayer.Builder<T> {
         protected LossFunction lossFunction = LossFunction.RMSE_XENT;
+        protected ILossFunction iLossFunction = new LossMCXENT();
         protected String customLossFunction;
 
         public Builder() {}
@@ -28,9 +33,16 @@ public abstract class BaseOutputLayer extends FeedForwardLayer {
         public Builder(LossFunction lossFunction) {
             this.lossFunction = lossFunction;
         }
+        public Builder(ILossFunction lossFunction) {
+            this.iLossFunction = lossFunction;
+        }
 
         public T lossFunction(LossFunction lossFunction) {
             this.lossFunction = lossFunction;
+            return (T)this;
+        }
+        public T lossFunction(ILossFunction lossFunction) {
+            this.iLossFunction = lossFunction;
             return (T)this;
         }
 
