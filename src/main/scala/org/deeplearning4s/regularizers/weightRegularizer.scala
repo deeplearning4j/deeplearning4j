ba1e129552bae047ee5c077dc9bd15561d7a79c5
turambar <turinturumarth@gmail.com>
2016-09-05 09:41:16 -0700

    Added WeightRegularizer for L1 and L2 regularization of neural net weights.

---
+++ src/main/scala/org/deeplearning4s/regularizers/weightRegularizer.scala
@@ -0,0 +1,12 @@
+package org.deeplearning4s.regularizers
+
+/**
+  * Created by davekale on 8/29/16.
+  */
+
+sealed class WeightRegularizer(val l1: Double = Double.NaN, val l2: Double = Double.NaN)
+
+case class NoRegularizer() extends WeightRegularizer()
+case class l1(l: Double = 0.01) extends WeightRegularizer(l1 = l)
+case class l2(l: Double = 0.01) extends WeightRegularizer(l2 = l)
+case class l1l2(override val l1: Double = 0.01, override val l2: Double = 0.01) extends WeightRegularizer
