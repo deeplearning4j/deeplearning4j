29c2ba2dd24cf7be192f36993d502296135aaf58
turambar <turinturumarth@gmail.com>
2016-09-06 08:51:23 -0700

    Added Nesterov momentum to SGD optimizer.

--- src/main/scala/org/deeplearning4s/optimizers/optimizer.scala
+++ src/main/scala/org/deeplearning4s/optimizers/optimizer.scala
@@ -6,7 +6,6 @@ import org.deeplearning4j.nn.api.OptimizationAlgorithm
   * Optimizers.
   */
 
-sealed class Optimizer(val optimizationAlgorithm: OptimizationAlgorithm, lr: Double = 0.01)
-
-case class SGD(lr: Double = 0.01)
-  extends Optimizer(optimizationAlgorithm = OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT, lr = lr)
+sealed class Optimizer(val optimizationAlgorithm: OptimizationAlgorithm, val lr: Double = 1e-1)
+case class SGD(override val lr: Double = 1e-1, val momentum: Double = Double.NaN, val nesterov: Boolean = false)
+  extends Optimizer(optimizationAlgorithm = OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
