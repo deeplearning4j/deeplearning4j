e4a2ebdee8630ae7eb25f61104a86824f5dce83c
Alex Black <blacka101@gmail.com>
2016-09-08 22:35:51 +1000

    Add TorchStep (missed in merge/refactoring)
    Former-commit-id: ffe098d6892f83dfd6c25ee568cc43657fef1899 [formerly 8af8d80de232dbccd918d0f762bbbe6c8ac3b0a7]
    Former-commit-id: ef5c6fb14ac955d24aa2d59c7927b04a71139f80

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/updater/LayerUpdater.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/updater/LayerUpdater.java
@@ -133,6 +133,10 @@ public class LayerUpdater implements Updater {
             case Step:
                 conf.setLearningRateByParam(variable, lr * Math.pow(decayRate, Math.floor(iteration/conf.getLrPolicySteps())));
                 break;
+            case TorchStep:
+                if (iteration > 1 && conf.getLrPolicySteps() % iteration == 0)
+                    conf.setLearningRateByParam(variable, lr * decayRate);
+                break;
             case Poly:
                 conf.setLearningRateByParam(variable, lr * Math.pow((1 - ((double)iteration)/conf.getNumIterations()), conf.getLrPolicyPower()));
                 break;
