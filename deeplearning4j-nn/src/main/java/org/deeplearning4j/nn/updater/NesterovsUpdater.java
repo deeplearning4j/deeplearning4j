eb3c4a9572681459db134d2b7691e7f44ca32fb7
Alex Black <blacka101@gmail.com>
2016-09-08 16:51:26 +1000

    Updater tests after refactoring updaters

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/updater/NesterovsUpdater.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/updater/NesterovsUpdater.java
@@ -6,6 +6,7 @@ import org.nd4j.linalg.learning.Nesterovs;
 
 /**
  * @author Adam Gibson
+ * @deprecated As of 0.6.0. Use {@link LayerUpdater instead}
  */
 @Deprecated
 public class NesterovsUpdater extends BaseUpdater {
