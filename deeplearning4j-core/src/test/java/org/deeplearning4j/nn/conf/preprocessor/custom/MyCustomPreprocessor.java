33a923ca5b4f2978ac9fa7a888b889b2dfa9fe8b
Alex Black <blacka101@gmail.com>
2016-09-09 18:53:26 +1000

    Register custom InputPreProcessors for JSON serialization; optimize classpath scan (one scan for all types)
    Former-commit-id: 68acd63e135345d474ff131192d29583ce80fb1b

---
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/preprocessor/custom/MyCustomPreprocessor.java
@@ -0,0 +1,33 @@
+package org.deeplearning4j.nn.conf.preprocessor.custom;
+
+import lombok.EqualsAndHashCode;
+import org.deeplearning4j.nn.conf.InputPreProcessor;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * Created by Alex on 09/09/2016.
+ */
+@EqualsAndHashCode
+public class MyCustomPreprocessor implements InputPreProcessor {
+
+    @Override
+    public INDArray preProcess(INDArray input, int miniBatchSize) {
+        return input.add(1.0);
+    }
+
+    @Override
+    public INDArray backprop(INDArray output, int miniBatchSize) {
+        return output;
+    }
+
+    @Override
+    public InputPreProcessor clone() {
+        return new MyCustomPreprocessor();
+    }
+
+    @Override
+    public InputType getOutputType(InputType inputType) {
+        return inputType;
+    }
+}
