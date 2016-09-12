707e5e2d389803c0f9c0d2d2c353e075378d1362
Alex Black <blacka101@gmail.com>
2016-09-12 17:28:02 +1000

    Add LossNegativeLogLikelihood (as an alias of LossMCXENT)

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossNegativeLogLikelihood.java
@@ -0,0 +1,15 @@
+package org.nd4j.linalg.lossfunctions.impl;
+
+/**
+ * Negative log likelihood loss function
+ *
+ * In practice, this is implemented as an alias for {@link LossMCXENT} due to the mathematical equivalence
+ */
+public class LossNegativeLogLikelihood extends LossMCXENT {
+
+
+    @Override
+    public String toString(){
+        return "LossNegativeLogLikelihood()";
+    }
+}
