4edcaff47fba8460593156307a9ca4bb0cd9ce48
Samuel Audet <samuel.audet@gmail.com>
2016-09-14 15:44:17 +0900

    Add change-scala-versions.sh and rearrange artifacts to support CUDA 8.0

--- perform-release.sh
+++ perform-release.sh
@@ -23,7 +23,8 @@ export TRICK_NVCC=YES
 export LIBND4J_HOME="$(pwd)"
 if [[ -z $(git tag -l "libnd4j-$RELEASE_VERSION") ]]; then
     bash buildnativeoperations.sh -c cpu
-    bash buildnativeoperations.sh -c cuda
+    bash buildnativeoperations.sh -c cuda -v 7.5
+    bash buildnativeoperations.sh -c cuda -v 8.0
     git tag -a -m "libnd4j-$RELEASE_VERSION" "libnd4j-$RELEASE_VERSION"
 fi
 cd ../nd4j
@@ -33,6 +34,7 @@ mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$
 if [[ -z ${STAGING_REPOSITORY:-} ]]; then
     # create new staging repository with everything in it
     source change-scala-versions.sh 2.10
+    source change-cuda-versions.sh 7.5
     mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
 
     if [[ ! $(grep stagingRepository.id target/nexus-staging/staging/*.properties) =~ ^stagingRepository.id=(.*) ]]; then
@@ -41,13 +43,21 @@ if [[ -z ${STAGING_REPOSITORY:-} ]]; then
     STAGING_REPOSITORY=${BASH_REMATCH[1]}
 
     source change-scala-versions.sh 2.11
+    source change-cuda-versions.sh 8.0
     mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
 else
     # build only partially (on other platforms) and deploy to given repository
+    source change-scala-versions.sh 2.10
+    source change-cuda-versions.sh 7.5
+    mvn clean deploy -Dgpg.useagent=false -DperformRelease -Psonatype-oss-release -DskipTests -Dmaven.javadoc.skip -DstagingRepositoryId=$STAGING_REPOSITORY
+
+    source change-scala-versions.sh 2.11
+    source change-cuda-versions.sh 8.0
     mvn clean deploy -Dgpg.useagent=false -DperformRelease -Psonatype-oss-release -DskipTests -Dmaven.javadoc.skip -DstagingRepositoryId=$STAGING_REPOSITORY
 fi
 
 source change-scala-versions.sh 2.10
+source change-cuda-versions.sh 7.5
 git commit -a -m "Update to version $RELEASE_VERSION"
 git tag -a -m "nd4j-$RELEASE_VERSION" "nd4j-$RELEASE_VERSION"
 
