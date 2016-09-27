35c39286989cdfc07f0127c47f28830e3a410164
Adam Gibson <adam@skymind.io>
2016-09-27 21:12:28 +0900

    add pre/post update hooks
    Former-commit-id: 60afa7f2a7edc202275ad2cb4a5dfd2e1cb15d71 [formerly 8841ac99781e34e4716d97d5a16f08be38055ede]
    Former-commit-id: 33babba27fd14d8af2c8bb13b90cd9d09c5193a1

---
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/TrainingHook.java
@@ -0,0 +1,52 @@
+package org.deeplearning4j.spark.api;
+
+import org.deeplearning4j.nn.api.Model;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
+
+import java.io.Serializable;
+
+/**
+ * A hook for the workers when training.
+ * A pre update and post update method are specified
+ * for when certain information needs to be collected
+ * or there needs to be specific parameters
+ * or models sent to remote locations for visualization
+ * or other things.
+ *
+ * @author Adam Gibson
+ */
+public interface TrainingHook extends Serializable {
+    /**
+     * A hook method for pre update.
+     * @param minibatch the inibatch
+     *                  that was used for the update
+     * @param model themodel that was update
+     */
+    void preUpdate(DataSet minibatch, Model model);
+
+    /**
+     * A hook method for post update
+     * @param minibatch the minibatch
+     *                  that was usd for the update
+     * @param model the model that was updated
+     */
+    void postUpdate(DataSet minibatch,Model model);
+
+    /**
+     * A hook method for pre update.
+     * @param minibatch the inibatch
+     *                  that was used for the update
+     * @param model themodel that was update
+     */
+    void preUpdate(MultiDataSet minibatch,Model model);
+
+    /**
+     * A hook method for post update
+     * @param minibatch the minibatch
+     *                  that was usd for the update
+     * @param model the model that was updated
+     */
+    void postUpdate(MultiDataSet minibatch, Model model);
+
+}
