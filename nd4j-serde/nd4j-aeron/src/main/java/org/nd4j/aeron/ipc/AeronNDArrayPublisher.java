66ddbe9a42569d3c70e14d506931785c848e694d
Adam Gibson <adam@skymind.io>
2016-09-27 13:46:40 +0900

    readd module

---
+++ nd4j-serde/nd4j-aeron/src/main/java/org/nd4j/aeron/ipc/AeronNDArrayPublisher.java
@@ -0,0 +1,162 @@
+package org.nd4j.aeron.ipc;
+
+import io.aeron.Aeron;
+import io.aeron.Publication;
+import lombok.Builder;
+import lombok.Data;
+
+import org.agrona.concurrent.UnsafeBuffer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+
+/**
+ * NDArray publisher for aeron
+ *
+ * @author Adam Gibson
+ */
+@Data
+@Builder
+public class AeronNDArrayPublisher implements  AutoCloseable {
+    // A unique identifier for a stream within a channel. Stream ID 0 is reserved
+    // for internal use and should not be used by applications.
+    private int streamId;
+    // The channel (an endpoint identifier) to send the message to
+    private String channel;
+    private boolean init = false;
+    private Aeron.Context ctx;
+    private Aeron aeron;
+    private Publication publication;
+    private static Logger log = LoggerFactory.getLogger(AeronNDArrayPublisher.class);
+
+
+
+    private void init() {
+        channel = channel == null ? "aeron:udp?endpoint=localhost:40123" : channel;
+        streamId = streamId == 0 ? 10 : streamId;
+        ctx = ctx == null ? ctx = new Aeron.Context() : ctx;
+        init = true;
+        log.debug("Channel publisher" + channel + " and stream " + streamId);
+    }
+
+    /**
+     * Publish an ndarray to an aeron channel
+     * @param arr
+     * @throws Exception
+     */
+    public void publish(INDArray arr) throws Exception {
+        // Allocate enough buffer size to hold maximum message length
+        // The UnsafeBuffer class is part of the Agrona library and is used for efficient buffer management
+        log.debug("Publishing to " + channel + " on stream Id " + streamId);
+        //ensure default values are set
+        while(!arr.isCompressed())
+            Nd4j.getCompressor().compressi(arr,"GZIP");
+
+
+        UnsafeBuffer buffer = AeronNDArraySerde.toBuffer(arr);
+
+        if(!init)
+            init();
+        // Create a context, needed for client connection to media driver
+        // A separate media driver process needs to be running prior to starting this application
+
+        // Create an Aeron instance with client-provided context configuration and connect to the
+        // media driver, and create a Publication.  The Aeron and Publication classes implement
+        // AutoCloseable, and will automatically clean up resources when this try block is finished.
+        if(aeron == null)
+            aeron = Aeron.connect(ctx);
+        if(publication == null)
+            publication = aeron.addPublication(channel, streamId);
+
+
+
+        // Try to publish the buffer. 'offer' is a non-blocking call.
+        // If it returns less than 0, the message was not sent, and the offer should be retried.
+        long result;
+        log.debug("Begin publish " + channel + " and stream " + streamId);
+        while ((result = publication.offer(buffer, 0, buffer.capacity())) < 0L)
+        {
+            if (result == Publication.BACK_PRESSURED)
+            {
+                log.debug(" Offer failed due to back pressure");
+            }
+            else if (result == Publication.NOT_CONNECTED)
+            {
+                log.debug(" Offer failed because publisher is not connected to subscriber");
+            }
+            else if (result == Publication.ADMIN_ACTION)
+            {
+                log.debug("Offer failed because of an administration action in the system");
+            }
+            else if (result == Publication.CLOSED)
+            {
+                log.debug("Offer failed publication is closed");
+            }
+            else
+            {
+                log.debug(" Offer failed due to unknown reason");
+            }
+        }
+
+
+        log.debug("Done sending.");
+
+    }
+
+
+    /**
+     * Closes this resource, relinquishing any underlying resources.
+     * This method is invoked automatically on objects managed by the
+     * {@code try}-with-resources statement.
+     * <p>
+     * <p>While this interface method is declared to throw {@code
+     * Exception}, implementers are <em>strongly</em> encouraged to
+     * declare concrete implementations of the {@code close} method to
+     * throw more specific exceptions, or to throw no exception at all
+     * if the close operation cannot fail.
+     * <p>
+     * <p> Cases where the close operation may fail require careful
+     * attention by implementers. It is strongly advised to relinquish
+     * the underlying resources and to internally <em>mark</em> the
+     * resource as closed, prior to throwing the exception. The {@code
+     * close} method is unlikely to be invoked more than once and so
+     * this ensures that the resources are released in a timely manner.
+     * Furthermore it reduces problems that could arise when the resource
+     * wraps, or is wrapped, by another resource.
+     * <p>
+     * <p><em>Implementers of this interface are also strongly advised
+     * to not have the {@code close} method throw {@link
+     * InterruptedException}.</em>
+     * <p>
+     * This exception interacts with a thread's interrupted status,
+     * and runtime misbehavior is likely to occur if an {@code
+     * InterruptedException} is {@linkplain Throwable#addSuppressed
+     * suppressed}.
+     * <p>
+     * More generally, if it would cause problems for an
+     * exception to be suppressed, the {@code AutoCloseable.close}
+     * method should not throw it.
+     * <p>
+     * <p>Note that unlike the {@link Closeable#close close}
+     * method of {@link Closeable}, this {@code close} method
+     * is <em>not</em> required to be idempotent.  In other words,
+     * calling this {@code close} method more than once may have some
+     * visible side effect, unlike {@code Closeable.close} which is
+     * required to have no effect if called more than once.
+     * <p>
+     * However, implementers of this interface are strongly encouraged
+     * to make their {@code close} methods idempotent.
+     *
+     * @throws Exception if this resource cannot be closed
+     */
+    @Override
+    public void close() throws Exception {
+        if(aeron != null)
+            aeron.close();
+        if(publication != null)
+            publication.close();
+    }
+}
---
+++ nd4j-serde/nd4j-aeron/src/main/java/org/nd4j/aeron/ipc/AeronNDArraySerde.java
@@ -0,0 +1,138 @@
+package org.nd4j.aeron.ipc;
+
+import org.agrona.DirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.Pointer;
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.shape.Shape;
+import org.nd4j.linalg.compression.CompressedDataBuffer;
+import org.nd4j.linalg.compression.CompressionDescriptor;
+import org.nd4j.linalg.factory.Nd4j;
+import uk.co.real_logic.sbe.ir.generated.MessageHeaderDecoder;
+import uk.co.real_logic.sbe.ir.generated.MessageHeaderEncoder;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+/**
+ * Serialization and de serialization class for
+ * aeron.
+ * This is a low level class specifically meant for speed.
+ *
+ * @author Adam Gibson
+ */
+public class AeronNDArraySerde {
+    /**
+     * Convert an ndarray to an unsafe buffer
+     * for use by aeron
+     * @param arr the array to convert
+     * @return the unsafebuffer representation of this array
+     */
+    public static UnsafeBuffer toBuffer(INDArray arr) {
+        //subset and get rid of 1 off non 1 element wise stride cases
+        if(arr.isView()) arr = arr.dup();
+        if(!arr.isCompressed()) {
+            ByteBuffer buffer = arr.data().pointer().asByteBuffer().order(ByteOrder.nativeOrder());
+            ByteBuffer shapeBuffer = arr.shapeInfoDataBuffer().pointer().asByteBuffer().order(ByteOrder.nativeOrder());
+            //2 four byte ints at the beginning
+            int twoInts = 8;
+            ByteBuffer b3 = ByteBuffer.allocateDirect(twoInts + buffer.limit() + shapeBuffer.limit()).order(ByteOrder.nativeOrder());
+            b3.putInt(arr.rank());
+            //put data type next so its self describing
+            b3.putInt(arr.data().dataType().ordinal());
+            b3.put(shapeBuffer);
+            b3.put(buffer);
+            b3.rewind();
+            return new UnsafeBuffer(b3);
+        }
+        //compressed array
+        else {
+            CompressedDataBuffer compressedDataBuffer = (CompressedDataBuffer) arr.data();
+            CompressionDescriptor descriptor = compressedDataBuffer.getCompressionDescriptor();
+            ByteBuffer codecByteBuffer = descriptor.toByteBuffer();
+            ByteBuffer buffer = arr.data().pointer().asByteBuffer().order(ByteOrder.nativeOrder());
+            ByteBuffer shapeBuffer = arr.shapeInfoDataBuffer().pointer().asByteBuffer().order(ByteOrder.nativeOrder());
+            //1 four byte int at the beginning for the rank
+            int twoInts = 2 * 4;
+            ByteBuffer b3 = ByteBuffer.allocateDirect(twoInts + buffer.limit() + shapeBuffer.limit() + codecByteBuffer.limit()).order(ByteOrder.nativeOrder());
+            b3.putInt(arr.rank());
+            //put data type next so its self describing
+            b3.putInt(arr.data().dataType().ordinal());
+            //put shape next
+            b3.put(shapeBuffer);
+            //put codec information next
+            b3.put(codecByteBuffer);
+            //finally put the data
+            b3.put(buffer);
+            b3.rewind();
+            return new UnsafeBuffer(b3);
+        }
+
+    }
+
+
+
+    /**
+     * Create an ndarray
+     * from the unsafe buffer
+     * @param buffer the buffer to create the array from
+     * @return the ndarray derived from this buffer
+     */
+    public static INDArray toArray(DirectBuffer buffer,int offset) {
+        ByteBuffer byteBuffer = buffer.byteBuffer().order(ByteOrder.nativeOrder());
+        byteBuffer.position(offset);
+        int rank = byteBuffer.getInt();
+        //get the shape buffer length to create the shape information buffer
+        int shapeBufferLength = Shape.shapeInfoLength(rank);
+        //create the ndarray shape information
+        DataBuffer shapeBuff = Nd4j.createBuffer(new int[shapeBufferLength]);
+
+        //compute the databuffer type from the index
+        DataBuffer.Type type = DataBuffer.Type.values()[byteBuffer.getInt()];
+        for(int i = 0; i < shapeBufferLength; i++) {
+            shapeBuff.put(i,byteBuffer.getInt());
+        }
+
+        System.out.println("SHAPE BUFF " + shapeBuff);
+
+
+
+        //after the rank,data type, shape buffer (of length shape buffer length) * sizeof(int)
+        if(type != DataBuffer.Type.COMPRESSED) {
+            byteBuffer = byteBuffer.slice();
+            //wrap the data buffer for the last bit
+            DataBuffer buff = Nd4j.createBuffer(byteBuffer,type,Shape.length(shapeBuff));
+            //create the final array
+            INDArray arr = Nd4j.createArrayFromShapeBuffer(buff,shapeBuff);
+            return arr;
+        }
+        else {
+            CompressionDescriptor compressionDescriptor = CompressionDescriptor.fromByteBuffer(byteBuffer);
+            byteBuffer = byteBuffer.slice();
+            //ensure that we only deal with the slice of the buffer that is actually the data
+            BytePointer byteBufferPointer = new BytePointer(byteBuffer);
+            //create a compressed array based on the rest of the data left in the buffer
+            CompressedDataBuffer compressedDataBuffer = new CompressedDataBuffer(byteBufferPointer,compressionDescriptor);
+            INDArray arr = Nd4j.createArrayFromShapeBuffer(compressedDataBuffer,shapeBuff);
+            arr.markAsCompressed(true);
+            return arr;
+        }
+
+    }
+
+    /**
+     * Create an ndarray
+     * from the unsafe buffer
+     * @param buffer the buffer to create the array from
+     * @return the ndarray derived from this buffer
+     */
+    public static INDArray toArray(DirectBuffer buffer) {
+        return toArray(buffer,0);
+    }
+
+
+
+}
