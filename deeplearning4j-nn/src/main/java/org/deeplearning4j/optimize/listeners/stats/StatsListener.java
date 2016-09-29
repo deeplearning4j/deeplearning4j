77dab650bda3d7d3dbe89b20c522a5895846259e
Alex Black <blacka101@gmail.com>
2016-09-29 11:18:46 +1000

    Additional implementation, refactoring/cleanup etc for Stats listener

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/optimize/listeners/stats/StatsListener.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/optimize/listeners/stats/StatsListener.java
@@ -20,7 +20,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class StatsListener implements IterationListener {
 
-    private enum StatType {Mean, Stdev, MeanMagnitude};
+    private enum StatType {Mean, Stdev, MeanMagnitude}
 
     private final StatsListenerReceiver receiver;
     private int iterCount = 0;
@@ -31,7 +31,10 @@ public class StatsListener implements IterationListener {
     private int examplesSinceLastReport = 0;
     private int minibatchesSinceLastReport = 0;
 
-    public StatsListener(StatsListenerReceiver receiver){
+    private long totalExamples = 0;
+    private long totalMinibatches = 0;
+
+    public StatsListener(StatsListenerReceiver receiver) {
         this.receiver = receiver;
     }
 
@@ -50,15 +53,15 @@ public class StatsListener implements IterationListener {
         StatsListenerConfiguration config = receiver.getCurrentConfiguration();
 
         long currentTime = getTime();
-        if(iterCount == 0){
+        if (iterCount == 0) {
             initTime = currentTime;
         }
 
-        if(config.collectPerformanceStats()){
+        if (config.collectPerformanceStats()) {
             updateExamplesMinibatchesCounts(model);
         }
 
-        if(config.reportingFrequency() > 1 && (iterCount == 0 || iterCount % config.reportingFrequency() != 0) ){
+        if (config.reportingFrequency() > 1 && (iterCount == 0 || iterCount % config.reportingFrequency() != 0)) {
             iterCount++;
             return;
         }
@@ -68,99 +71,120 @@ public class StatsListener implements IterationListener {
 
         //--- Performance and System Stats ---
 
-        if(config.collectPerformanceStats()){
-            //Stats to collect: iterations/second, examples/second, total time,
-            if(iterCount > 0){ //Not possible to work out perf/second: first iteration...
+        if (config.collectPerformanceStats()) {
+            //Stats to collect: total runtime, total examples, total minibatches, iterations/second, examples/second
+            double examplesPerSecond;
+            double minibatchesPerSecond;
+            if (iterCount == 0) {
+                //Not possible to work out perf/second: first iteration...
+                examplesPerSecond = 0.0;
+                minibatchesPerSecond = 0.0;
+            } else {
                 long deltaTimeMS = currentTime - lastReportTime;
-                double examplesPerSecond = 1000.0 * examplesSinceLastReport / deltaTimeMS;
-                double minibatchesPerSecond = 1000.0 * minibatchesSinceLastReport / deltaTimeMS;
-                report.reportPerformance(examplesPerSecond, minibatchesPerSecond);
+                examplesPerSecond = 1000.0 * examplesSinceLastReport / deltaTimeMS;
+                minibatchesPerSecond = 1000.0 * minibatchesSinceLastReport / deltaTimeMS;
             }
+            long totalRuntimeMS = currentTime - initTime;
+            report.reportPerformance(totalRuntimeMS, totalExamples, totalMinibatches, examplesPerSecond, minibatchesPerSecond);
+
+            examplesSinceLastReport = 0;
+            minibatchesSinceLastReport = 0;
         }
 
-        if(config.collectMemoryStats()){
+        if (config.collectMemoryStats()) {
             Runtime runtime = Runtime.getRuntime();
             long jvmTotal = runtime.totalMemory();
             long jvmMax = runtime.maxMemory();
 
-            //TODO is this safe in general, for all backends?
+            //Off-heap memory
             long offheapTotal = Pointer.totalBytes();
             long offheapMax = Pointer.maxBytes();
 
             //TODO: GPU...
 
-            report.reportMemoryUse(jvmTotal,jvmMax,offheapTotal,offheapMax,null,null);
+            report.reportMemoryUse(jvmTotal, jvmMax, offheapTotal, offheapMax, null, null);
         }
 
         //--- General ---
 
-        if(config.collectScore()){
+        if (config.collectScore()) {
             report.reportScore(model.score());
         }
 
-        if(config.collectLearningRates()){
-            //TODO
+        if (config.collectLearningRates()) {
+            //TODO - how to get this?
         }
 
 
         //--- Histograms ---
 
-        if(config.collectHistogramParameters()){
-            Map<String,Pair<INDArray,int[]>> paramHistograms = getHistograms(model.paramTable(), config.numHistogramBins());
-            report.reportHistogramParameter(paramHistograms);
+        if (config.collectHistograms(StatsType.Parameters)) {
+            Map<String, Pair<INDArray, int[]>> paramHistograms = getHistograms(model.paramTable(), config.numHistogramBins(StatsType.Parameters));
+            report.reportHistograms(StatsType.Parameters, paramHistograms);
         }
 
-        if(config.collectHistogramUpdates()){
-            Map<String,Pair<INDArray,int[]>> updateHistograms = getHistograms(model.gradient().gradientForVariable(), config.numHistogramBins());
-            report.reportHistogramParameter(updateHistograms);
+        if (config.collectHistograms(StatsType.Updates)) {
+            Map<String, Pair<INDArray, int[]>> updateHistograms = getHistograms(model.gradient().gradientForVariable(), config.numHistogramBins(StatsType.Updates));
+            report.reportHistograms(StatsType.Updates, updateHistograms);
         }
 
-        if(config.collectHistogramActivations()){
-
+        if (config.collectHistograms(StatsType.Activations)) {
+            Map<String, INDArray> activations = getActivationArraysMap(model);
+            Map<String, Pair<INDArray, int[]>> activationHistograms = getHistograms(activations, config.numHistogramBins(StatsType.Activations));
+            report.reportHistograms(StatsType.Activations, activationHistograms);
         }
 
 
         //--- Summary Stats: Mean, Variance, Mean Magnitudes ---
 
-        if(config.collectMean(StatsType.Parameters)){
-            Map<String,Double> meanParams = calculateSummaryStats(model.paramTable(), StatType.MeanMagnitude);
+        if (config.collectMean(StatsType.Parameters)) {
+            Map<String, Double> meanParams = calculateSummaryStats(model.paramTable(), StatType.Mean);
             report.reportMean(StatsType.Parameters, meanParams);
         }
 
-        if(config.collectMean(StatsType.Updates)){
-            Map<String,Double> meanUpdates = calculateSummaryStats(model.gradient().gradientForVariable(), StatType.MeanMagnitude);
-            report.reportMean(StatsType.Updates,meanUpdates);
+        if (config.collectMean(StatsType.Updates)) {
+            Map<String, Double> meanUpdates = calculateSummaryStats(model.gradient().gradientForVariable(), StatType.Mean);
+            report.reportMean(StatsType.Updates, meanUpdates);
         }
 
-        if(config.collectMean(StatsType.Activations)){
-
-        }
-
-        if(config.collectStdev(StatsType.Parameters)){
-
-        }
-
-        if(config.collectStdev(StatsType.Updates)){
-
-        }
-
-        if(config.collectStdev(StatsType.Activations)){
-
+        if (config.collectMean(StatsType.Activations)) {
+            Map<String, INDArray> activations = getActivationArraysMap(model);
+            Map<String, Double> meanActivations = calculateSummaryStats(activations, StatType.Mean);
+            report.reportMean(StatsType.Activations, meanActivations);
         }
 
 
-        if(config.collectMeanMagnitudes(StatsType.Parameters)){
-            Map<String,Double> meanMagParams = calculateSummaryStats(model.paramTable(), StatType.MeanMagnitude);
+        if (config.collectStdev(StatsType.Parameters)) {
+            Map<String, Double> stdevParams = calculateSummaryStats(model.paramTable(), StatType.Stdev);
+            report.reportStdev(StatsType.Parameters, stdevParams);
+        }
+
+        if (config.collectStdev(StatsType.Updates)) {
+            Map<String, Double> stdevUpdates = calculateSummaryStats(model.gradient().gradientForVariable(), StatType.Stdev);
+            report.reportStdev(StatsType.Updates, stdevUpdates);
+        }
+
+        if (config.collectStdev(StatsType.Activations)) {
+            Map<String, INDArray> activations = getActivationArraysMap(model);
+            Map<String, Double> stdevActivations = calculateSummaryStats(activations, StatType.Stdev);
+            report.reportStdev(StatsType.Activations, stdevActivations);
+        }
+
+
+        if (config.collectMeanMagnitudes(StatsType.Parameters)) {
+            Map<String, Double> meanMagParams = calculateSummaryStats(model.paramTable(), StatType.MeanMagnitude);
             report.reportMeanMagnitudes(StatsType.Parameters, meanMagParams);
         }
 
-        if(config.collectMeanMagnitudes(StatsType.Updates)){
-            Map<String,Double> meanMagUpdates = calculateSummaryStats(model.gradient().gradientForVariable(), StatType.MeanMagnitude);
+        if (config.collectMeanMagnitudes(StatsType.Updates)) {
+            Map<String, Double> meanMagUpdates = calculateSummaryStats(model.gradient().gradientForVariable(), StatType.MeanMagnitude);
             report.reportMeanMagnitudes(StatsType.Updates, meanMagUpdates);
         }
 
-        if(config.collectMeanMagnitudes(StatsType.Activations)){
-            //TODO
+        if (config.collectMeanMagnitudes(StatsType.Activations)) {
+            Map<String, INDArray> activations = getActivationArraysMap(model);
+            Map<String, Double> meanMagActivations = calculateSummaryStats(activations, StatType.MeanMagnitude);
+            report.reportMeanMagnitudes(StatsType.Activations, meanMagActivations);
         }
 
 
@@ -170,28 +194,32 @@ public class StatsListener implements IterationListener {
         receiver.postResult(report);
     }
 
-    private long getTime(){
+    private long getTime() {
         //Abstraction to allow NTP to be plugged in later...
         return System.currentTimeMillis();
     }
 
-    private void updateExamplesMinibatchesCounts(Model model){
-        if(model instanceof MultiLayerNetwork){
-            examplesSinceLastReport += ((MultiLayerNetwork) model).getInput().size(0);
-        } else if(model instanceof ComputationGraph){
-            examplesSinceLastReport += ((ComputationGraph) model).getInput(0).size(0);
-        } else if(model instanceof Layer){
-            examplesSinceLastReport += ((Layer) model).getInputMiniBatchSize();
+    private void updateExamplesMinibatchesCounts(Model model) {
+        int examplesThisMinibatch = 0;
+        if (model instanceof MultiLayerNetwork) {
+            examplesThisMinibatch = ((MultiLayerNetwork) model).getInput().size(0);
+        } else if (model instanceof ComputationGraph) {
+            examplesThisMinibatch = ((ComputationGraph) model).getInput(0).size(0);
+        } else if (model instanceof Layer) {
+            examplesThisMinibatch = ((Layer) model).getInputMiniBatchSize();
         }
+        examplesSinceLastReport += examplesThisMinibatch;
+        totalExamples += examplesThisMinibatch;
         minibatchesSinceLastReport++;
+        totalMinibatches++;
     }
 
-    private static Map<String,Double> calculateSummaryStats(Map<String,INDArray> source, StatType statType){
-        Map<String,Double> out = new LinkedHashMap<>();
-        for(Map.Entry<String,INDArray> entry : source.entrySet()) {
+    private static Map<String, Double> calculateSummaryStats(Map<String, INDArray> source, StatType statType) {
+        Map<String, Double> out = new LinkedHashMap<>();
+        for (Map.Entry<String, INDArray> entry : source.entrySet()) {
             String name = entry.getKey();
             double value;
-            switch (statType){
+            switch (statType) {
                 case Mean:
                     value = entry.getValue().meanNumber().doubleValue();
                     break;
@@ -209,11 +237,11 @@ public class StatsListener implements IterationListener {
         return out;
     }
 
-    private static Map<String,Pair<INDArray,int[]>> getHistograms(Map<String,INDArray> map, int nBins){
+    private static Map<String, Pair<INDArray, int[]>> getHistograms(Map<String, INDArray> map, int nBins) {
         //TODO This is temporary approach...
-        Map<String,Pair<INDArray,int[]>> out = new LinkedHashMap<>();
+        Map<String, Pair<INDArray, int[]>> out = new LinkedHashMap<>();
 
-        for(Map.Entry<String,INDArray> entry : map.entrySet()){
+        for (Map.Entry<String, INDArray> entry : map.entrySet()) {
             HistogramBin histogram = new HistogramBin.Builder(entry.getValue().dup())
                     .setBinCount(nBins)
                     .setRounding(6)
@@ -221,12 +249,34 @@ public class StatsListener implements IterationListener {
             INDArray bins = histogram.getBins();
             int[] count = new int[nBins];
             int i = 0;
-            for( Map.Entry<BigDecimal,AtomicInteger> e : histogram.getData().entrySet()){
+            for (Map.Entry<BigDecimal, AtomicInteger> e : histogram.getData().entrySet()) {
                 count[i] = e.getValue().get();
             }
 
             out.put(entry.getKey(), new Pair<>(bins, count));
         }
         return out;
+    }
+
+    private static Map<String, INDArray> getActivationArraysMap(Model model) {
+        Map<String, INDArray> map = new LinkedHashMap<>();
+        if (model instanceof MultiLayerNetwork) {
+            MultiLayerNetwork net = (MultiLayerNetwork) model;
+
+            Layer[] layers = net.getLayers();
+            //Activations for layer i are stored as input to layer i+1
+            //TODO handle output activations...
+            //Also: complication here - things like batch norm...
+            for (int i = 1; i < layers.length; i++) {
+                String name = String.valueOf(i - 1);
+                map.put(name, layers[i].input());
+            }
+
+        } else {
+            //Compgraph is more complex: output from one layer might go to multiple other layers/vertices, etc.
+            throw new UnsupportedOperationException("Not yet implemented");
+        }
+
+        return map;
     }
 }
