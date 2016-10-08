744bab425e853bf008c4a74211a690c7ad0498a2
Alex Black <blacka101@gmail.com>
2016-10-08 20:24:57 +1100

    Simplify and improve StatsStorageListener interface; change MapDB listener reporting to avoid possibility of race condition with listeners
    Former-commit-id: e0bf7df53c8b8e28766bedea9ae01c1adb83a1c1

---
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/modules/histogram/HistogramModule.java
@@ -0,0 +1,30 @@
+package org.deeplearning4j.ui.modules.histogram;
+
+import org.deeplearning4j.ui.api.FunctionType;
+import org.deeplearning4j.ui.api.HttpMethod;
+import org.deeplearning4j.ui.api.Route;
+import org.deeplearning4j.ui.api.UIModule;
+import org.deeplearning4j.ui.stats.StatsListener;
+
+import java.util.Collections;
+import java.util.List;
+
+import static play.mvc.Results.ok;
+
+/**
+ * Created by Alex on 08/10/2016.
+ */
+public class HistogramModule implements UIModule {
+    @Override
+    public List<String> getCallbackTypeIDs() {
+        return Collections.singletonList(StatsListener.TYPE_ID);
+    }
+
+    @Override
+    public List<Route> getRoutes() {
+        Route r = new Route("/histogram", HttpMethod.GET, FunctionType.Supplier, () -> ok("This is the histogram page"));
+        return Collections.singletonList(r);
+    }
+
+
+}
