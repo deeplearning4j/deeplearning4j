6351fb5a87b0597c0c29cea29af248c2d9d12f89
Alex Black <blacka101@gmail.com>
2016-10-08 18:07:20 +1100

    First pass on new UI API
    Former-commit-id: 3f8694f905293161a7e40dec6fabc8eaeaa9db62

---
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/api/FunctionType.java
@@ -0,0 +1,11 @@
+package org.deeplearning4j.ui.api;
+
+/**
+ * Created by Alex on 08/10/2016.
+ */
+public enum FunctionType {
+    Supplier,
+    Function,
+    BiFunction,
+    Function3
+}
---
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/api/HttpMethod.java
@@ -0,0 +1,14 @@
+package org.deeplearning4j.ui.api;
+
+/**
+ * Created by Alex on 08/10/2016.
+ */
+public enum HttpMethod {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    HEAD,
+    OPTIONS,
+    PATCH
+}
