45d65ea1904951a090497489eb41cd1a380a2512
Alex Black <blacka101@gmail.com>
2016-10-08 23:29:29 +1100

    Play UI server first pass; minor UI API changes; javadoc
    Former-commit-id: d856a4ba35a6692d280c1c5a5cc9f430fdddba5e [formerly 869d052863065a8544d6905ac610a9f6910af265]
    Former-commit-id: bd5b3702008ff2122c50897b662ac69f2ea03b9c

---
+++ deeplearning4j-ui-parent/deeplearning4j-ui-model/src/main/java/org/deeplearning4j/ui/storage/impl/QueuePairStatsStorageListener.java
@@ -0,0 +1,28 @@
+package org.deeplearning4j.ui.storage.impl;
+
+import lombok.AllArgsConstructor;
+import org.deeplearning4j.berkeley.Pair;
+import org.deeplearning4j.ui.storage.StatsStorage;
+import org.deeplearning4j.ui.storage.StatsStorageEvent;
+import org.deeplearning4j.ui.storage.StatsStorageListener;
+
+import java.util.Queue;
+
+/**
+ * A very simple {@link StatsStorageListener}, that adds the {@link StatsStorageEvent} instances and the specified
+ * {@link StatsStorage} instance (i.e., the source) to the specified queue
+ * for later processing.
+ *
+ * @author Alex Black
+ */
+@AllArgsConstructor
+public class QueuePairStatsStorageListener implements StatsStorageListener {
+
+    private final StatsStorage statsStorage;
+    private final Queue<Pair<StatsStorage,StatsStorageEvent>> queue;
+
+    @Override
+    public void notify(StatsStorageEvent event) {
+        queue.add(new Pair<>(statsStorage, event));
+    }
+}
