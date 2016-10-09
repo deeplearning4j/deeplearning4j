0eaed31de84abeef407ad72942178aadeea8e9de
Alex Black <blacka101@gmail.com>
2016-10-09 21:57:12 +1100

    Implement Play (twirl) template compilation
    Former-commit-id: facbbc1bdc5b6a3e8ecdab98b2a7bfe045e82011 [formerly 3e113fc6ef42d50e841ab5d9f935ffc4ed574d95]
    Former-commit-id: 2ede10351b161383b52344cf85687279c13be32b

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/modules/histogram/HistogramModule.java
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/modules/histogram/HistogramModule.java
@@ -8,9 +8,9 @@ import org.deeplearning4j.ui.stats.StatsListener;
 import org.deeplearning4j.ui.storage.StatsStorage;
 import org.deeplearning4j.ui.storage.StatsStorageEvent;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
+
+import java.util.*;
 
 import static play.mvc.Results.ok;
 
@@ -18,6 +18,20 @@ import static play.mvc.Results.ok;
  * Created by Alex on 08/10/2016.
  */
 public class HistogramModule implements UIModule {
+
+    private ObjectMapper om = new ObjectMapper();
+    private boolean initialized = false;
+
+    private Set<String> knownSessionIDs = new LinkedHashSet<>();
+
+    private ArrayList<Double> scoreHistory = new ArrayList<>();
+    private List<Map<String,List<Double>>> meanMagHistoryParams = new ArrayList<>();    //1 map per layer; keyed by new param name
+    private List<Map<String,List<Double>>> meanMagHistoryUpdates = new ArrayList<>();
+    private Map<String,Integer> layerNameIndexes = new HashMap<>();
+    private List<String> layerNames = new ArrayList<>();
+    private int layerNameIndexesCount = 0;
+
+
     @Override
     public List<String> getCallbackTypeIDs() {
         return Collections.singletonList(StatsListener.TYPE_ID);
@@ -25,25 +39,66 @@ public class HistogramModule implements UIModule {
 
     @Override
     public List<Route> getRoutes() {
-        Route r = new Route("/histogram", HttpMethod.GET, FunctionType.Supplier, () -> ok("This is the histogram page"));
-        return Collections.singletonList(r);
+        Route r = new Route("/weights", HttpMethod.GET, FunctionType.Supplier, () -> ok("This is the histogram page"));
+        Route r2 = new Route("/weights/listSessions", HttpMethod.GET, FunctionType.Supplier, () -> ok(toJson(knownSessionIDs)));
+//        Route r3 = new Route("/weights/:something", HttpMethod.GET, FunctionType.Function,  (String s) -> ok(views.html.TestTemplate.render(s)));
+//        Route r3 = new Route("/weights/:something", HttpMethod.GET, FunctionType.Function,
+//                (String s) -> ok(MyTestTemplate.render(s)));
+//        Route r3 = new Route("/weights/:something", HttpMethod.GET, FunctionType.Function,
+//                (String s) -> ok(MyTest4.apply(s)));
+
+        return Arrays.asList(r, r2);
     }
 
     @Override
     public void reportStorageEvents(StatsStorage statsStorage, Collection<StatsStorageEvent> events) {
         //TODO actually process the events...
+        if(!initialized) doInit(statsStorage);
 
+        //TODO only do updates on demand... no point updating things over and over if nobody is actually looking at the page!
 
 
     }
 
     @Override
     public void onAttach(StatsStorage statsStorage) {
-        //TODO
+        for(String sessionID : statsStorage.listSessionIDs()){
+            for(String typeID : statsStorage.listTypeIDsForSession(sessionID)){
+                if(!StatsListener.TYPE_ID.equals(typeID)) continue;
+                knownSessionIDs.add(sessionID);
+            }
+        }
+
+        //Query the stats storage
+        if(!initialized) doInit(statsStorage);
     }
 
     @Override
     public void onDetach(StatsStorage statsStorage) {
         //TODO
+        for(String sessionID : statsStorage.listSessionIDs()){
+            knownSessionIDs.remove(sessionID);
+        }
+    }
+
+    private void doInit(StatsStorage statsStorage){
+        //Query the stats storage... determine if any relevant stats are available
+        //If so, record the session/worker IDs for later use
+        //Old histogram listener (that this is based on)
+
+        List<String> sessionIDs = statsStorage.listSessionIDs();
+        for(String sid : sessionIDs){
+//            statsStorage.get
+        }
+
+
+    }
+
+    private String toJson(Object object){
+        try{
+            return om.writeValueAsString(object);
+        }catch (Exception e){
+            return "Json Processing Exception"; //TODO
+        }
     }
 }
--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/play/staticroutes/Assets.java
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/play/staticroutes/Assets.java
@@ -1,19 +1,35 @@
 package org.deeplearning4j.ui.play.staticroutes;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.nd4j.linalg.io.ClassPathResource;
 import play.mvc.Result;
+import play.mvc.Results;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.function.Function;
 
 import static play.mvc.Results.ok;
 
 /**
- * Created by Alex on 09/10/2016.
+ * Simple function for serving assets. Assets are assumed to be in the specified root directory
+ *
+ * @author Alex Black
  */
+@AllArgsConstructor @Slf4j
 public class Assets implements Function<String,Result> {
+    private final String assetsRootDirectory;
 
     @Override
     public Result apply(String s) {
-        return ok("Received asset request: " + s);
+        File f;
+        try {
+            f = new ClassPathResource(assetsRootDirectory + s).getFile();
+        } catch (IOException e){
+            log.debug("Error for asset request: {}",s,e);
+            return Results.notFound("Not found: /assets/" + s);
+        }
+        return ok(f);
     }
 }
