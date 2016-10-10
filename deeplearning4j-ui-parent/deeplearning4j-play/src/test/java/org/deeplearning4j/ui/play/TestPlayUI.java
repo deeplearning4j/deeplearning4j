3fa8a60ad090d817061234f7d8854b15aed19d3f
Alex Black <blacka101@gmail.com>
2016-10-10 17:10:00 +1100

    First pass on Histogram listener reimplementation using new UI and StatsStorage
    Former-commit-id: 79651eee96d963d3914493b979bd6b2a8b904a7a [formerly 93526089c96fb7e53fe24bd384c11f3377d587bd]
    Former-commit-id: cf4b9dba7e26685d0ee230f42ec856f252fd5f5d

--- deeplearning4j-ui-parent/deeplearning4j-play/src/test/java/org/deeplearning4j/ui/play/TestPlayUI.java
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/test/java/org/deeplearning4j/ui/play/TestPlayUI.java
@@ -1,7 +1,19 @@
 package org.deeplearning4j.ui.play;
 
+import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.StatsStorage;
+import org.deeplearning4j.ui.storage.mapdb.MapDBStatsStorage;
 import org.junit.Test;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 /**
  * Created by Alex on 08/10/2016.
@@ -11,7 +23,29 @@ public class TestPlayUI {
     @Test
     public void testUI() throws Exception {
 
+        StatsStorage ss = new MapDBStatsStorage();  //In-memory
+
         UIServer uiServer = UIServer.getInstance();
+        uiServer.attach(ss);
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+                .list()
+                .layer(0, new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MCXENT).activation("softmax").nIn(4).nOut(3).build())
+                .pretrain(false).backprop(true).build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        net.setListeners(new StatsListener(ss), new ScoreIterationListener(1));
+
+        DataSetIterator iter = new IrisDataSetIterator(150,150);
+
+        for( int i=0; i<10; i++ ){
+            net.fit(iter);
+            Thread.sleep(1000);
+        }
+
+
 
 
         Thread.sleep(100000);
