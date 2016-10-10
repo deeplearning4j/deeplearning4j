7438d4ef7cfc7a50a32b47a79beb744c06416e61
Alex Black <blacka101@gmail.com>
2016-10-10 21:49:26 +1100

    First full pass on histogram module/listener (all functionality now available; needs optimization)
    Former-commit-id: 34cc35e19050b047cb98dfe308d3b1d1840c7004 [formerly 6398102385d1a8963082edd48afd1d5bb7327a15]
    Former-commit-id: aff81ee0c320c2a447ab631c476f543295aebc9c

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/modules/histogram/HistogramModule.java
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/java/org/deeplearning4j/ui/modules/histogram/HistogramModule.java
@@ -13,11 +13,7 @@ import org.deeplearning4j.ui.stats.api.SummaryType;
 import org.deeplearning4j.ui.storage.Persistable;
 import org.deeplearning4j.ui.storage.StatsStorage;
 import org.deeplearning4j.ui.storage.StatsStorageEvent;
-
-import org.deeplearning4j.ui.views.html.histogram.Histogram;
 import org.deeplearning4j.ui.weights.beans.CompactModelAndGradient;
-import org.nd4j.shade.jackson.databind.ObjectMapper;
-import play.api.http.ContentTypes;
 import play.libs.Json;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -32,17 +28,7 @@ import static play.mvc.Results.ok;
 @Slf4j
 public class HistogramModule implements UIModule {
 
-    private ObjectMapper om = new ObjectMapper();
-    private boolean initialized = false;
-
-    private Map<String,StatsStorage> knownSessionIDs = new LinkedHashMap<>();
-
-//    private ArrayList<Double> scoreHistory = new ArrayList<>();
-//    private List<Map<String,List<Double>>> meanMagHistoryParams = new ArrayList<>();    //1 map per layer; keyed by new param name
-//    private List<Map<String,List<Double>>> meanMagHistoryUpdates = new ArrayList<>();
-//    private Map<String,Integer> layerNameIndexes = new HashMap<>();
-//    private List<String> layerNames = new ArrayList<>();
-//    private int layerNameIndexesCount = 0;
+    private Map<String, StatsStorage> knownSessionIDs = new LinkedHashMap<>();
 
 
     @Override
@@ -62,23 +48,21 @@ public class HistogramModule implements UIModule {
 
     @Override
     public void reportStorageEvents(StatsStorage statsStorage, Collection<StatsStorageEvent> events) {
-        log.info("Received events: {}",events);
+        log.info("Received events: {}", events);
 
         //We should only be getting relevant session IDs...
-        for(StatsStorageEvent sse : events){
-            if(!knownSessionIDs.containsKey(sse.getSessionID())){
+        for (StatsStorageEvent sse : events) {
+            if (!knownSessionIDs.containsKey(sse.getSessionID())) {
                 knownSessionIDs.put(sse.getSessionID(), statsStorage);
             }
         }
-
-        //TODO only do updates on demand... no point updating things over and over if nobody is actually looking at the page!
     }
 
     @Override
     public void onAttach(StatsStorage statsStorage) {
-        for(String sessionID : statsStorage.listSessionIDs()){
-            for(String typeID : statsStorage.listTypeIDsForSession(sessionID)){
-                if(!StatsListener.TYPE_ID.equals(typeID)) continue;
+        for (String sessionID : statsStorage.listSessionIDs()) {
+            for (String typeID : statsStorage.listTypeIDsForSession(sessionID)) {
+                if (!StatsListener.TYPE_ID.equals(typeID)) continue;
                 knownSessionIDs.put(sessionID, statsStorage);
             }
         }
@@ -86,52 +70,76 @@ public class HistogramModule implements UIModule {
 
     @Override
     public void onDetach(StatsStorage statsStorage) {
-        for(String sessionID : statsStorage.listSessionIDs()){
+        for (String sessionID : statsStorage.listSessionIDs()) {
             knownSessionIDs.remove(sessionID);
         }
     }
 
-    private Result getLastUpdateTime(String sessionID){
+    private Result getLastUpdateTime(String sessionID) {
         return Results.ok(Json.toJson(System.currentTimeMillis()));
     }
 
-    private Result processRequest(String sessionId){
+    private Result processRequest(String sessionId) {
+        //TODO cache the relevant info and update, rather than querying StatsStorage and building from scratch each time
+
         StatsStorage ss = knownSessionIDs.get(sessionId);
-        if(ss == null){
+        if (ss == null) {
             return Results.notFound("Unknown session ID: " + sessionId);
         }
 
         List<String> workerIDs = ss.listWorkerIDsForSession(sessionId);
 
         //TODO checks
-        StatsInitializationReport initReport = (StatsInitializationReport)ss.getStaticInfo(sessionId,StatsListener.TYPE_ID, workerIDs.get(0));
-        if(initReport == null) return Results.ok(Json.toJson(Collections.EMPTY_MAP));
+        StatsInitializationReport initReport = (StatsInitializationReport) ss.getStaticInfo(sessionId, StatsListener.TYPE_ID, workerIDs.get(0));
+        if (initReport == null) return Results.ok(Json.toJson(Collections.EMPTY_MAP));
 
-//        String[] paramNames = initReport.getModelParamNames();
+        String[] paramNames = initReport.getModelParamNames();
+        //Infer layer names from param names...
+        Set<String> layerNameSet = new LinkedHashSet<>();
+        for (String s : paramNames) {
+            String[] split = s.split("_");
+            if (!layerNameSet.contains(split[0])) {
+                layerNameSet.add(split[0]);
+            }
+        }
+        List<String> layerNameList = new ArrayList<>(layerNameSet);
 
-        List<Persistable> list = ss.getAllUpdatesAfter(sessionId, StatsListener.TYPE_ID, workerIDs.get(0),0);
-        Collections.sort(list, (a,b) -> Long.compare(a.getTimeStamp(), b.getTimeStamp()));
+        List<Persistable> list = ss.getAllUpdatesAfter(sessionId, StatsListener.TYPE_ID, workerIDs.get(0), 0);
+        Collections.sort(list, (a, b) -> Long.compare(a.getTimeStamp(), b.getTimeStamp()));
 
         List<Double> scoreList = new ArrayList<>(list.size());
-        List<Map<String,List<Double>>> meanMagHistoryParams = new ArrayList<>();    //List.get(i) -> layer i
+        List<Map<String, List<Double>>> meanMagHistoryParams = new ArrayList<>();    //List.get(i) -> layer i. Maps: parameter for the given layer
+        List<Map<String, List<Double>>> meanMagHistoryUpdates = new ArrayList<>();    //List.get(i) -> layer i. Maps: updates for the given layer
+        for (int i = 0; i < layerNameList.size(); i++) {
+            meanMagHistoryParams.add(new HashMap<>());
+            meanMagHistoryUpdates.add(new HashMap<>());
+        }
+
         StatsReport last = null;
-        for(Persistable p : list){
-            if(!(p instanceof StatsReport)){
+        for (Persistable p : list) {
+            if (!(p instanceof StatsReport)) {
                 log.warn("Unexpected type: {}", p);
                 continue;
             }
-            StatsReport sp = (StatsReport)p;
+            StatsReport sp = (StatsReport) p;
             scoreList.add(sp.getScore());
 
-            //TODO mean magnitudes
+            //Mean magnitudes
+            if (sp.hasSummaryStats(StatsType.Parameters, SummaryType.MeanMagnitudes)) {
+                updateMeanMagnitudeMaps(sp.getMeanMagnitudes(StatsType.Parameters), layerNameList, meanMagHistoryParams);
+            }
+
+            if (sp.hasSummaryStats(StatsType.Updates, SummaryType.MeanMagnitudes)) {
+                updateMeanMagnitudeMaps(sp.getMeanMagnitudes(StatsType.Updates), layerNameList, meanMagHistoryUpdates);
+            }
 
             last = sp;
         }
 
-        Map<String,Map> newParams = getHistogram(last.getHistograms(StatsType.Parameters));
-        Map<String,Map> newGrad = getHistogram(last.getHistograms(StatsType.Updates));
+        Map<String, Map> newParams = getHistogram(last.getHistograms(StatsType.Parameters));
+        Map<String, Map> newGrad = getHistogram(last.getHistograms(StatsType.Updates));
 
-        double lastScore = (scoreList.size() == 0 ? 0.0 : scoreList.get(scoreList.size()-1));
+        double lastScore = (scoreList.size() == 0 ? 0.0 : scoreList.get(scoreList.size() - 1));
 
         CompactModelAndGradient g = new CompactModelAndGradient();
         g.setGradients(newGrad);
@@ -139,45 +147,49 @@ public class HistogramModule implements UIModule {
         g.setScore(lastScore);
         g.setScores(scoreList);
 //        g.setPath(subPath);
-//        g.setUpdateMagnitudes(meanMagHistoryUpdates);
-//        g.setParamMagnitudes(meanMagHistoryParams);
+        g.setUpdateMagnitudes(meanMagHistoryUpdates);
+        g.setParamMagnitudes(meanMagHistoryParams);
 //        g.setLayerNames(layerNames);
         g.setLastUpdateTime(last.getTimeStamp());
 
-//        Map<String,Object> ret = new HashMap<>();
-//        ret.put("lastUpdateTime", last.getTimeStamp());
-//        ret.put("score",lastScore);
-//        ret.put("scores",scoreList);
-//        ret.put("gradients", newGrad);
-//        ret.put("parameters", newParams);
-//        ret.put("paramMagnitudes", Collections.EMPTY_MAP);
-//        ret.put("updateMagnitudes", Collections.EMPTY_MAP);
-
-
-//        return Results.ok(Json.toJson(ret));
         return Results.ok(Json.toJson(g));
     }
 
-    private Map<String,Map> getHistogram(Map<String, org.deeplearning4j.ui.stats.api.Histogram> histograms){
-        Map<String,Map> ret = new LinkedHashMap<>();
-        for(String s : histograms.keySet()){
+    private void updateMeanMagnitudeMaps(Map<String, Double> current, List<String> layerNames, List<Map<String, List<Double>>> history) {
+        for (Map.Entry<String, Double> entry : current.entrySet()) {
+            String key = entry.getKey();
+            String[] split = key.split("_");
+            int idx = layerNames.indexOf(split[0]);
+            Map<String, List<Double>> map = history.get(idx);
+            List<Double> l = map.get(key);
+            if (l == null) {
+                l = new ArrayList<>();
+                map.put(key, l);
+            }
+            l.add(entry.getValue());
+        }
+    }
+
+    private Map<String, Map> getHistogram(Map<String, org.deeplearning4j.ui.stats.api.Histogram> histograms) {
+        Map<String, Map> ret = new LinkedHashMap<>();
+        for (String s : histograms.keySet()) {
             org.deeplearning4j.ui.stats.api.Histogram h = histograms.get(s);
             String newName;
-            if(Character.isDigit(s.charAt(0))) newName = "param_" + s;
+            if (Character.isDigit(s.charAt(0))) newName = "param_" + s;
             else newName = s;
 
-            Map<Number,Number> temp = new LinkedHashMap<>();
+            Map<Number, Number> temp = new LinkedHashMap<>();
             double min = h.getMin();
             double max = h.getMax();
             int n = h.getNBins();
-            double step = (max-min)/n;
+            double step = (max - min) / n;
             int[] counts = h.getBinCounts();
-            for( int i=0; i<n; i++ ){
-                double binLoc = min + i*step + step/2.0;
+            for (int i = 0; i < n; i++) {
+                double binLoc = min + i * step + step / 2.0;
                 temp.put(binLoc, counts[i]);
             }
 
-            ret.put(newName,temp);
+            ret.put(newName, temp);
         }
         return ret;
     }
