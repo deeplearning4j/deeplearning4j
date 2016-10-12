1db5c5511ce36f8e1765ce9b9a0dd06003444b89
raver119 <raver119@gmail.com>
2016-10-12 15:29:01 +0300

    hs draft p.1

---
+++ include/aggregate_ops.h
@@ -0,0 +1,162 @@
+//
+// @author raver119@gmail.com
+//
+#ifndef LIBND4J_AGGREGATE_OPS_H
+#define LIBND4J_AGGREGATE_OPS_H
+
+#include <ops.h>
+#include <templatemath.h>
+
+#define HS_MAX_EXP 6
+
+#ifdef __CUDACC__
+#define aggregate_def __host__ __device__ inline static
+#else
+#define aggregate_def inline static
+#endif
+/*
+ *
+ *
+ * Aggregate Ops are special things suited for CUDA mostly. They are meant to be executed within single block ONLY.
+ * So, when batched, they should provide proper parallelism levels on poorly parallel tasks otherwise
+ *
+ *
+ */
+namespace aggregateOps {
+
+    template<typename T>
+    class HierarchicSoftmax {
+        public:
+
+        aggregate_def void executeAggregate(T **arguments, int numArguments, int *indexArguments, int numIndexArguments, T *realArguments, int numRealArguments) {
+            int idxSyn0 = indexArguments[0];
+            int idxSyn1 = indexArguments[1];
+            int vectorLength = indexArguments[2];
+            int expLength = indexArguments[3];
+            int code = indexArguments[4];
+
+            T *syn0 = arguments[0] + (idxSyn0 * vectorLength);
+            T *syn1 = arguments[1] + (idxSyn1 * vectorLength);
+            T *expTable = arguments[2];
+            T *neu1e = arguments[3];
+
+            T dot = (T) 0.0f;
+            T g = (T) 0.0f;
+            T f = (T) 0.0f;
+
+            // dot
+// TODO: simd reduction required here
+//#pragma omp simd
+            for (int x = 0; x < vectorLength; x++) {
+                T prod = syn0[x] * syn1[x];
+                dot += prod;
+            }
+
+            // gradient
+            if (dot < - HS_MAX_EXP || dot >= HS_MAX_EXP)
+                return;
+
+            int idx = (int) ((dot + HS_MAX_EXP) * ((T) expLength / HS_MAX_EXP / 2.0));
+
+            if (idx >= expLength)
+                return;
+
+            f = expTable[idx];
+            g = (1 - code - f) * alpha;
+
+
+            // axpy1
+#pragma omp simd
+            for (int x = 0; x < vectorLength; x++) {
+                neu1e[x] = g * syn1[x] + neu1e[x];
+            }
+
+            // axpy2
+#pragma omp simd
+            for (int x = 0; x < vectorLength; x++x) {
+                syn1[x] = g * syn0[x] + syn1[x];
+            }
+        }
+
+#ifdef __CUDACC__
+        aggregate_def void executeAggregateCuda(T **arguments, int numArguments, int *indexArguments, int numIndexArguments, T *realArguments, int numRealArguments) {
+            /*
+                We know that syn0 & syn1 are 2D matrices, so we can just use offsets here
+            */
+
+            __shared__ int idxSyn0;
+            __shared__ int idxSyn1;
+            __shared__ int vectorLength;
+            __shared__ int expLength;
+            __shared__ int code;
+
+            __shared__ T *syn0;
+            __shared__ T *syn1;
+            __shared__ T *expTable;
+
+            __shared__ T *neu1e;
+            __shared__ T dot;
+            __shared__ T g;
+            __shared__ T f;
+            __shared__ T alpha;
+
+            if (threadIdx.x == 0) {
+                idxSyn0 = indexArguments[0];
+                idxSyn1 = indexArguments[1];
+                vectorLength = indexArguments[2];
+                expLength = indexArguments[3];
+                code = indexArguments[4];
+
+                syn0 = arguments[0] + (idxSyn0 * vectorLength);
+                syn1 = arguments[1] + (idxSyn1 * vectorLength);
+                expTable; = arguments[2];
+                neu1e = arguments[3];
+
+                alpha = realArguments[0];
+            }
+            __syncthreads();
+
+            // dot
+            for (int x = threadIdx.x; x < vectorLength; x+=blockDim.x) {
+                T prod = syn0[x] * syn1[x];
+                nd4j::math::nd4j_atomicAdd(&dot, prod);
+            }
+
+
+            // gradient
+            __syncthreads();
+            if (dot < - HS_MAX_EXP || dot >= HS_MAX_EXP)
+                return;
+
+            int idx = (int) ((dot + HS_MAX_EXP) * ((T) expLength / HS_MAX_EXP / 2.0));
+
+            if (idx >= expLength)
+                return;
+
+
+            if (threadIdx.x == 0) {
+                // gradient calculation
+                f = expTable[idx];
+                g = (1 - code - f) * alpha;
+            }
+            __syncthreads();
+
+
+
+            // axpy1
+            for (int x = threadIdx.x; x < vectorLength; x+=blockDim.x) {
+                neu1e[x] = g * syn1[x] + neu1e[x];
+            }
+            __syncthreads();
+
+            // axpy2
+            for (int x = threadIdx.x; x < vectorLength; x+=blockDim.x) {
+                syn1[x] = g * syn0[x] + syn1[x];
+            }
+            __syncthreads();
+        }
+#endif
+    };
+}
+
+#endif //LIBND4J_AGGREGATE_OPS_H
---
+++ include/aggregates.h
@@ -0,0 +1,10 @@
+//
+// @author raver119@gmail.com
+//
+
+#ifndef LIBND4J_AGGREGATES_H
+#define LIBND4J_AGGREGATES_H
+
+
+
+#endif //LIBND4J_AGGREGATES_H
