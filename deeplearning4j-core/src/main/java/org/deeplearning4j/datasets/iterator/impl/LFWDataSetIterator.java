0b3600dfe3598532ec5f19d948c6bf02fe21efa7
Adam Gibson <adam@skymind.io>
2016-10-14 13:16:21 +0900

    ensure constructor is setup for datavec
    Former-commit-id: 6358a4c2d81900e043562faf8632b400e3d4eb7c [formerly df569bf3bdfdeeb769e019b5f2776ff6e5a7f2ce]
    Former-commit-id: 26e9711c9f560d6b29ca5b9b28462e7b58d318d4

--- deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/LFWDataSetIterator.java
@@ -85,7 +85,7 @@ public class LFWDataSetIterator extends RecordReaderDataSetIterator {
      * @param rng random number to lock in batch shuffling
 	 * */
 	public LFWDataSetIterator(int batchSize, int numExamples, int[] imgDim, int numLabels, boolean useSubset, PathLabelGenerator labelGenerator, boolean train, double splitTrainTest, ImageTransform imageTransform, Random rng) {
-		super(new LFWLoader(imgDim, imageTransform,numExamples, useSubset).getRecordReader(batchSize, numExamples, imgDim, numLabels, labelGenerator, train, splitTrainTest, rng), batchSize, 1, numLabels);
+		super(new LFWLoader(imgDim, imageTransform, useSubset).getRecordReader(batchSize, numExamples, imgDim, numLabels, labelGenerator, train, splitTrainTest, rng), batchSize, 1, numLabels);
 	}
 
 }
