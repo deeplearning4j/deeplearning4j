36425092699dfd498e95cc729c9652f126f87635
Alex Black <blacka101@gmail.com>
2016-10-15 18:47:16 +1100

    Add i18n language fallback if msg not found; runtime language select; basic japanese nav messages

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/typescript/org/deeplearning4j/ui/module/train/TrainUi.ts
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/typescript/org/deeplearning4j/ui/module/train/TrainUi.ts
@@ -10,10 +10,39 @@ function onNavClick(source: String, errorMsg: String) {
     $.ajax({
         url: reqURL,
         success: function (data) {
+            $("#homeNavDiv").removeClass("navElementSelected");
+            $("#modelNavDiv").removeClass("navElementSelected");
+            $("#systemNavDiv").removeClass("navElementSelected");
+            $("#helpNavDiv").removeClass("navElementSelected");
+            $("#" + source + "NavDiv").addClass("navElementSelected");
             $("#mainContentDiv").html(data);
         },
         error: function (query, status, error) {
             $.notify(errorMsg,"error");
+        }
+    });
+}
+
+function changeLanguage(errorMsg: string){
+    var currSelected = $("#navLangSelect").val();
+
+    $.ajax({
+        url: "setlang/" + currSelected,
+        success: function () {
+            location.reload(true);
+            console.log("Selected language: " + currSelected);
+        },
+        error: function (query, status, error) {
+            $.notify(errorMsg,"error");
+        }
+    });
+}
+
+function setLanguageSelectorValue(){
+    $.ajax({
+        url: "/lang/getCurrent",
+        success: function(data){
+            $("#navLangSelect").val(data);
         }
     });
 }
