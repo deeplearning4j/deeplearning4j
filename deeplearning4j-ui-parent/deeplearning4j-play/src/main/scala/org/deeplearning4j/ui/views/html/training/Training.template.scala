43cecb37091fca3294744e03fa40799446364ab1
Alex Black <blacka101@gmail.com>
2016-10-16 00:43:59 +1100

    First steps for overview page implementation
    Former-commit-id: 91c62e90865a252ffc15ed237029bb2b7bc31c3e

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/scala/org/deeplearning4j/ui/views/html/training/Training.template.scala
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/scala/org/deeplearning4j/ui/views/html/training/Training.template.scala
@@ -37,8 +37,9 @@ Seq[Any](format.raw/*1.40*/("""
         function onPageLoad()"""),format.raw/*13.30*/("""{"""),format.raw/*13.31*/("""
             """),format.raw/*14.13*/("""onNavClick('home', '"""),_display_(/*14.34*/i18n/*14.38*/.getMessage("train.nav.errormsg")),format.raw/*14.71*/("""');
             setLanguageSelectorValue();
-        """),format.raw/*16.9*/("""}"""),format.raw/*16.10*/("""
-    """),format.raw/*17.5*/("""</script>
+            setInterval(getAndProcessUpdate,2000);
+        """),format.raw/*17.9*/("""}"""),format.raw/*17.10*/("""
+    """),format.raw/*18.5*/("""</script>
     <body onload="onPageLoad()">
 
         <div class="outerDiv">
@@ -48,53 +49,57 @@ Seq[Any](format.raw/*1.40*/("""
             </div>
 
             <div class="navDiv">
-                <div class="navTopSpacer">(TODO: Session Selection Here)</div>
-                <div class="navElement" id="homeNavDiv" onclick="onNavClick('home', '"""),_display_(/*28.87*/i18n/*28.91*/.getMessage("train.nav.errormsg")),format.raw/*28.124*/("""')">
+                <div class="navTopSpacer"></div>
+                <div class="navSessionID">
+                    <strong>"""),_display_(/*30.30*/i18n/*30.34*/.getMessage("train.nav.sessionid")),format.raw/*30.68*/(""":</strong><br>
+                    TODO<br>
+                </div>
+                <div class="navTopSpacer"></div>
+                <div class="navElement" id="homeNavDiv" onclick="onNavClick('home', '"""),_display_(/*34.87*/i18n/*34.91*/.getMessage("train.nav.errormsg")),format.raw/*34.124*/("""')">
                     <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_home_28.png">
                     <div class="navHorizontalSpacer"></div>
-                    """),_display_(/*32.22*/i18n/*32.26*/.getMessage("train.nav.overview")),format.raw/*32.59*/("""
-                """),format.raw/*33.17*/("""</div>
+                    """),_display_(/*38.22*/i18n/*38.26*/.getMessage("train.nav.overview")),format.raw/*38.59*/("""
+                """),format.raw/*39.17*/("""</div>
                 <div class="navElementSpacer"></div>
-                <div class="navElement" id="modelNavDiv" onclick="onNavClick('model', '"""),_display_(/*35.89*/i18n/*35.93*/.getMessage("train.nav.errormsg")),format.raw/*35.126*/("""')">
+                <div class="navElement" id="modelNavDiv" onclick="onNavClick('model', '"""),_display_(/*41.89*/i18n/*41.93*/.getMessage("train.nav.errormsg")),format.raw/*41.126*/("""')">
                     <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_model_28.png">
                     <div class="navHorizontalSpacer"></div>
-                    """),_display_(/*39.22*/i18n/*39.26*/.getMessage("train.nav.model")),format.raw/*39.56*/("""
-                """),format.raw/*40.17*/("""</div>
+                    """),_display_(/*45.22*/i18n/*45.26*/.getMessage("train.nav.model")),format.raw/*45.56*/("""
+                """),format.raw/*46.17*/("""</div>
                 <div class="navElementSpacer"></div>
-                <div class="navElement" id="systemNavDiv" onclick="onNavClick('system', '"""),_display_(/*42.91*/i18n/*42.95*/.getMessage("train.nav.errormsg")),format.raw/*42.128*/("""')">
+                <div class="navElement" id="systemNavDiv" onclick="onNavClick('system', '"""),_display_(/*48.91*/i18n/*48.95*/.getMessage("train.nav.errormsg")),format.raw/*48.128*/("""')">
                     <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_system_28.png">
                     <div class="navHorizontalSpacer"></div>
-                    """),_display_(/*46.22*/i18n/*46.26*/.getMessage("train.nav.hwsw")),format.raw/*46.55*/("""
-                """),format.raw/*47.17*/("""</div>
+                    """),_display_(/*52.22*/i18n/*52.26*/.getMessage("train.nav.hwsw")),format.raw/*52.55*/("""
+                """),format.raw/*53.17*/("""</div>
                 <div class="navElementSpacer"></div>
-                <div class="navElement" id="helpNavDiv" onclick="onNavClick('help', '"""),_display_(/*49.87*/i18n/*49.91*/.getMessage("train.nav.errormsg")),format.raw/*49.124*/("""')">
+                <div class="navElement" id="helpNavDiv" onclick="onNavClick('help', '"""),_display_(/*55.87*/i18n/*55.91*/.getMessage("train.nav.errormsg")),format.raw/*55.124*/("""')">
                     <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_help_28.png">
                     <div class="navHorizontalSpacer"></div>
-                    """),_display_(/*53.22*/i18n/*53.26*/.getMessage("train.nav.help")),format.raw/*53.55*/("""
-                """),format.raw/*54.17*/("""</div>
+                    """),_display_(/*59.22*/i18n/*59.26*/.getMessage("train.nav.help")),format.raw/*59.55*/("""
+                """),format.raw/*60.17*/("""</div>
                 <div class="navBottom">
-                    """),_display_(/*56.22*/i18n/*56.26*/.getMessage("train.nav.language")),format.raw/*56.59*/(""":
-                    <select class="languageSelect" id="navLangSelect" onchange="changeLanguage('"""),_display_(/*57.98*/i18n/*57.102*/.getMessage("train.nav.langChangeErrorMsg")),format.raw/*57.145*/("""')">
+                    """),_display_(/*62.22*/i18n/*62.26*/.getMessage("train.nav.language")),format.raw/*62.59*/(""":
+                    <select class="languageSelect" id="navLangSelect" onchange="changeLanguage('"""),_display_(/*63.98*/i18n/*63.102*/.getMessage("train.nav.langChangeErrorMsg")),format.raw/*63.145*/("""')">
                         <option value="en">English</option>
                         <option value="ja">日本語</option>
                         <option value="zh">中文</option>
                         <option value="kr">한글</option>
                     </select>
                     <div class="navElementSpacer"></div>
-                    <a href=""""),_display_(/*64.31*/i18n/*64.35*/.getMessage("train.nav.websitelink")),format.raw/*64.71*/("""" class="textlink">"""),_display_(/*64.91*/i18n/*64.95*/.getMessage("train.nav.websitelinktext")),format.raw/*64.135*/("""</a>
+                    <a href=""""),_display_(/*70.31*/i18n/*70.35*/.getMessage("train.nav.websitelink")),format.raw/*70.71*/("""" class="textlink">"""),_display_(/*70.91*/i18n/*70.95*/.getMessage("train.nav.websitelinktext")),format.raw/*70.135*/("""</a>
                     <div class="navElementSpacer"></div>
                 </div>
             </div>
 
 
             <div class="contentDiv" id="mainContentDiv">
-                Content Div<br>
-                Language: """),_display_(/*72.28*/i18n/*72.32*/.getDefaultLanguage),format.raw/*72.51*/("""
-            """),format.raw/*73.13*/("""</div>
+
+            </div>
         </div>
     </body>
 </html>"""))
@@ -117,11 +122,11 @@ Seq[Any](format.raw/*1.40*/("""
 object Training extends Training_Scope0.Training
               /*
                   -- GENERATED --
-                  DATE: Sat Oct 15 18:44:54 AEDT 2016
+                  DATE: Sun Oct 16 00:33:24 AEDT 2016
                   SOURCE: C:/DL4J/Git/deeplearning4j/deeplearning4j-ui-parent/deeplearning4j-play/src/main/views/org/deeplearning4j/ui/views/training/Training.scala.html
-                  HASH: 3ab1f263e3b575db871a1c956d81872ef50277c8
-                  MATRIX: 588->1|721->39|749->41|955->221|967->225|1017->255|1385->595|1414->596|1456->610|1504->631|1517->635|1571->668|1652->722|1681->723|1714->729|2270->1258|2283->1262|2338->1295|2585->1515|2598->1519|2652->1552|2698->1570|2875->1720|2888->1724|2943->1757|3191->1978|3204->1982|3255->2012|3301->2030|3480->2182|3493->2186|3548->2219|3797->2441|3810->2445|3860->2474|3906->2492|4081->2640|4094->2644|4149->2677|4396->2897|4409->2901|4459->2930|4505->2948|4602->3018|4615->3022|4669->3055|4796->3155|4810->3159|4875->3202|5257->3557|5270->3561|5327->3597|5374->3617|5387->3621|5449->3661|5706->3891|5719->3895|5759->3914|5801->3928
-                  LINES: 20->1|25->1|26->2|29->5|29->5|29->5|37->13|37->13|38->14|38->14|38->14|38->14|40->16|40->16|41->17|52->28|52->28|52->28|56->32|56->32|56->32|57->33|59->35|59->35|59->35|63->39|63->39|63->39|64->40|66->42|66->42|66->42|70->46|70->46|70->46|71->47|73->49|73->49|73->49|77->53|77->53|77->53|78->54|80->56|80->56|80->56|81->57|81->57|81->57|88->64|88->64|88->64|88->64|88->64|88->64|96->72|96->72|96->72|97->73
+                  HASH: a3a105645c32d0c8c1d2a24976b65713fb19d0a1
+                  MATRIX: 588->1|721->39|749->41|955->221|967->225|1017->255|1385->595|1414->596|1456->610|1504->631|1517->635|1571->668|1704->774|1733->775|1766->781|2279->1267|2292->1271|2347->1305|2580->1511|2593->1515|2648->1548|2895->1768|2908->1772|2962->1805|3008->1823|3185->1973|3198->1977|3253->2010|3501->2231|3514->2235|3565->2265|3611->2283|3790->2435|3803->2439|3858->2472|4107->2694|4120->2698|4170->2727|4216->2745|4391->2893|4404->2897|4459->2930|4706->3150|4719->3154|4769->3183|4815->3201|4912->3271|4925->3275|4979->3308|5106->3408|5120->3412|5185->3455|5567->3810|5580->3814|5637->3850|5684->3870|5697->3874|5759->3914
+                  LINES: 20->1|25->1|26->2|29->5|29->5|29->5|37->13|37->13|38->14|38->14|38->14|38->14|41->17|41->17|42->18|54->30|54->30|54->30|58->34|58->34|58->34|62->38|62->38|62->38|63->39|65->41|65->41|65->41|69->45|69->45|69->45|70->46|72->48|72->48|72->48|76->52|76->52|76->52|77->53|79->55|79->55|79->55|83->59|83->59|83->59|84->60|86->62|86->62|86->62|87->63|87->63|87->63|94->70|94->70|94->70|94->70|94->70|94->70
                   -- GENERATED --
               */
           
