81b8476bb2c608cec33bdfc84f332f7f5b0a5305
Alex Black <blacka101@gmail.com>
2016-10-15 11:37:15 +1100

    Update nav icons; more page content

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/scala/org/deeplearning4j/ui/views/html/training/Training.template.scala
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/scala/org/deeplearning4j/ui/views/html/training/Training.template.scala
@@ -33,28 +33,45 @@ Seq[Any](format.raw/*1.40*/("""
         <div class="outerDiv">
 
             <div class="navDiv">
-                <div class="navTopSpacer"></div>
+                <div class="navTopSpacer">(TODO: Session Selection Here)</div>
                 <div class="navElement">
+                    <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_home_28.png">
-                        """),_display_(/*15.26*/i18n/*15.30*/.getMessage("train.nav.overview")),format.raw/*15.63*/("""
-                """),format.raw/*16.17*/("""</div>
+                    <div class="navHorizontalSpacer"></div>
+                    """),_display_(/*17.22*/i18n/*17.26*/.getMessage("train.nav.overview")),format.raw/*17.59*/("""
+                """),format.raw/*18.17*/("""</div>
+                <div class="navElementSpacer"></div>
                 <div class="navElement">
+                    <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_model_28.png">
-                    """),_display_(/*19.22*/i18n/*19.26*/.getMessage("train.nav.model")),format.raw/*19.56*/("""
-                """),format.raw/*20.17*/("""</div>
+                    <div class="navHorizontalSpacer"></div>
+                    """),_display_(/*24.22*/i18n/*24.26*/.getMessage("train.nav.model")),format.raw/*24.56*/("""
+                """),format.raw/*25.17*/("""</div>
+                <div class="navElementSpacer"></div>
                 <div class="navElement">
+                    <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_system_28.png">
-                """),_display_(/*23.18*/i18n/*23.22*/.getMessage("train.nav.hwsw")),format.raw/*23.51*/("""
-                """),format.raw/*24.17*/("""</div>
+                    <div class="navHorizontalSpacer"></div>
+                    """),_display_(/*31.22*/i18n/*31.26*/.getMessage("train.nav.hwsw")),format.raw/*31.55*/("""
+                """),format.raw/*32.17*/("""</div>
+                <div class="navElementSpacer"></div>
                 <div class="navElement">
+                    <div class="navHorizontalSpacer"></div>
                     <img src="assets/img/train/nav_icon_help_28.png">
-                    """),_display_(/*27.22*/i18n/*27.26*/.getMessage("train.nav.help")),format.raw/*27.55*/("""
-                """),format.raw/*28.17*/("""</div>
+                    <div class="navHorizontalSpacer"></div>
+                    """),_display_(/*38.22*/i18n/*38.26*/.getMessage("train.nav.help")),format.raw/*38.55*/("""
+                """),format.raw/*39.17*/("""</div>
+                <div class="navBottom">
+                    (TODO Language Selection Here)
+                    <div class="navElementSpacer"></div>
+                    <a href="http://www.deeplearning4j.org/" class="textlink">deeplearning4j.org</a>
+                    <div class="navElementSpacer"></div>
+                </div>
             </div>
             <div class="contentDiv">
                 Content Div<br>
-                Language: """),_display_(/*32.28*/i18n/*32.32*/.getDefaultLanguage),format.raw/*32.51*/("""
-            """),format.raw/*33.13*/("""</div>
+                Language: """),_display_(/*49.28*/i18n/*49.32*/.getDefaultLanguage),format.raw/*49.51*/("""
+            """),format.raw/*50.13*/("""</div>
         </div>
     </body>
 </html>"""))
@@ -77,11 +94,11 @@ Seq[Any](format.raw/*1.40*/("""
 object Training extends Training_Scope0.Training
               /*
                   -- GENERATED --
-                  DATE: Sat Oct 15 10:37:02 AEDT 2016
+                  DATE: Sat Oct 15 11:35:46 AEDT 2016
                   SOURCE: C:/DL4J/Git/deeplearning4j/deeplearning4j-ui-parent/deeplearning4j-play/src/main/views/org/deeplearning4j/ui/views/training/Training.scala.html
-                  HASH: 04e88a5f4d0f4f05043202ec08a253f1f6ed93eb
-                  MATRIX: 588->1|721->39|749->41|1256->521|1269->525|1323->558|1369->576|1539->719|1552->723|1603->753|1649->771|1816->911|1829->915|1879->944|1925->962|2094->1104|2107->1108|2157->1137|2203->1155|2356->1281|2369->1285|2409->1304|2451->1318
-                  LINES: 20->1|25->1|26->2|39->15|39->15|39->15|40->16|43->19|43->19|43->19|44->20|47->23|47->23|47->23|48->24|51->27|51->27|51->27|52->28|56->32|56->32|56->32|57->33
+                  HASH: 70dd1f57d6d6b31e6b7ba18fa6c8c9def42f906c
+                  MATRIX: 588->1|721->39|749->41|1404->669|1417->673|1471->706|1517->724|1863->1043|1876->1047|1927->1077|1973->1095|2320->1415|2333->1419|2383->1448|2429->1466|2774->1784|2787->1788|2837->1817|2883->1835|3371->2296|3384->2300|3424->2319|3466->2333
+                  LINES: 20->1|25->1|26->2|41->17|41->17|41->17|42->18|48->24|48->24|48->24|49->25|55->31|55->31|55->31|56->32|62->38|62->38|62->38|63->39|73->49|73->49|73->49|74->50
                   -- GENERATED --
               */
           
