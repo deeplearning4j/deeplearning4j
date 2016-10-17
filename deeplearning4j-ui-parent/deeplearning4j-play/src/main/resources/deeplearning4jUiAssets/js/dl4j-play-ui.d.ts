0b1db32718ae3699bdd758eeafd8b83c463e37a2
Alex Black <blacka101@gmail.com>
2016-10-17 11:09:31 +1100

    Handle session selection; session info, list sessions, display in UI, etc

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/dl4j-play-ui.d.ts
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/dl4j-play-ui.d.ts
@@ -3,6 +3,7 @@
 /// <reference path="../../../typescript/org/deeplearning4j/ui/typedefs/dl4j-ui.d.ts" />
 declare function onNavClick(source: String, errorMsg: String): void;
 declare function changeLanguage(errorMsg: string): void;
+declare function setSessionIDDivContents(): void;
 declare function setLanguageSelectorValue(): void;
 declare function getAndProcessUpdate(): void;
 declare function getAndProcessUpdateOverview(): void;
