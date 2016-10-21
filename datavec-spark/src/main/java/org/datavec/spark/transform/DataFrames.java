400fecd280a8064f31f2c230c26d2b65572188da
Adam Gibson <adam@skymind.io>
2016-10-21 23:57:03 +0900

    finish new dataframe support

--- datavec-spark/src/main/java/org/datavec/spark/transform/DataFrames.java
+++ datavec-spark/src/main/java/org/datavec/spark/transform/DataFrames.java
@@ -2,6 +2,7 @@ package org.datavec.spark.transform;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
@@ -14,7 +15,6 @@ import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.writable.Writable;
 import org.datavec.spark.transform.sparkfunction.ToRecord;
 import org.datavec.spark.transform.sparkfunction.ToRow;
-
 import java.util.List;
 
 
@@ -27,6 +27,51 @@ import java.util.List;
 public class DataFrames {
 
     /**
+     * Standard deviation for a column
+     * @param dataFrame the dataframe to
+     *                  get the column from
+     * @param columnName the name of the column to get the standard
+     *                   deviationfor
+     * @return the column that represents the standard deviation
+     */
+    public static Column std(DataFrame dataFrame, String columnName) {
+        return dataFrame.col(columnName).desc().getItem("stddev");
+    }
+
+    /**
+     * MIn for a column
+     * @param dataFrame the dataframe to
+     *                  get the column from
+     * @param columnName the name of the column to get the min for
+     * @return the column that represents the mean
+     */
+    public static Column min(DataFrame dataFrame, String columnName) {
+        return dataFrame.col(columnName).desc().getItem("min");
+    }
+
+    /**
+     * Max for a column
+     * @param dataFrame the dataframe to
+     *                  get the column fron
+     * @param columnName the name of the column to get the max for
+     * @return the column that represents the mean
+     */
+    public static Column max(DataFrame dataFrame, String columnName) {
+        return dataFrame.col(columnName).desc().getItem("max");
+    }
+
+    /**
+     * Mean for a column
+     * @param dataFrame the dataframe to
+     *                  get the column fron
+     * @param columnName the name of the column to get the mean for
+     * @return the column that represents the mean
+     */
+    public static Column mean(DataFrame dataFrame, String columnName) {
+        return dataFrame.col(columnName).desc().getItem("mean");
+    }
+
+    /**
      * Convert a datavec schema to a
      * struct type in spark
      * @param schema the schema to convert
@@ -36,10 +81,10 @@ public class DataFrames {
         StructField[] structFields = new StructField[schema.numColumns()];
         for(int i = 0; i < structFields.length; i++) {
             switch (schema.getColumnTypes().get(i)) {
-                case Double: structFields[i] = new StructField(schema.getName(i), DataTypes.DoubleType,false,new Metadata()); break;
-                case Integer: new StructField(schema.getName(i), DataTypes.IntegerType,false,new Metadata()); break;
-                case Long: new StructField(schema.getName(i), DataTypes.LongType,false,new Metadata()); break;
-                case Float: new StructField(schema.getName(i), DataTypes.FloatType,false,new Metadata()); break;
+                case Double: structFields[i] = new StructField(schema.getName(i), DataTypes.DoubleType,false,Metadata.empty()); break;
+                case Integer: new StructField(schema.getName(i), DataTypes.IntegerType,false,Metadata.empty()); break;
+                case Long: new StructField(schema.getName(i), DataTypes.LongType,false,Metadata.empty()); break;
+                case Float: new StructField(schema.getName(i), DataTypes.FloatType,false,Metadata.empty()); break;
                 default: throw new IllegalStateException("This api should not be used with strings , binary data or ndarrays. This is only for columnar data");
             }
         }
@@ -75,7 +120,7 @@ public class DataFrames {
      */
     public static Pair<Schema,JavaRDD<List<Writable>>> toRecords(DataFrame dataFrame) {
         Schema schema = fromStructType(dataFrame.schema());
-        return new Pair<>(schema,dataFrame.javaRDD().map(new ToRecord()));
+        return new Pair<>(schema,dataFrame.javaRDD().map(new ToRecord(schema)));
     }
 
     /**
--- datavec-spark/src/test/java/org/datavec/spark/transform/DataFrameTests.java
+++ datavec-spark/src/test/java/org/datavec/spark/transform/DataFrameTests.java
@@ -1,6 +1,10 @@
 package org.datavec.spark.transform;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
 import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.writable.DoubleWritable;
 import org.datavec.api.writable.Writable;
@@ -16,23 +20,42 @@ import static org.junit.Assert.assertEquals;
  */
 public class DataFrameTests extends BaseSparkTest {
 
+
+
     @Test
     public void testDataFrameConversions() {
         List<List<Writable>> data = new ArrayList<>();
         Schema.Builder builder = new Schema.Builder();
+        int numColumns = 6;
+        for(int i = 0; i < numColumns; i++)
+            builder.addColumnDouble(String.valueOf(i));
+
         for(int i = 0; i < 5; i++) {
-            data.add(new ArrayList<Writable>());
-            for(int j = 0; j < 6; j++) {
-                data.get(i).add(new DoubleWritable(1.0));
+            List<Writable> record = new ArrayList<>(numColumns);
+            data.add(record);
+            for(int j = 0; j < numColumns; j++) {
+                record.add(new DoubleWritable(1.0));
             }
 
-            builder.addColumnDouble(String.valueOf(i));
         }
 
         Schema schema = builder.build();
         JavaRDD<List<Writable>> rdd = sc.parallelize(data);
         assertEquals(schema,DataFrames.fromStructType(DataFrames.fromSchema(schema)));
         assertEquals(rdd.collect(),DataFrames.toRecords(DataFrames.toDataFrame(schema,rdd)).getSecond().collect());
+
+        DataFrame dataFrame = DataFrames.toDataFrame(schema,rdd);
+        DataFrame desc = dataFrame.describe(dataFrame.columns());
+        desc.show();
+        System.out.println(desc.col("0").minus(DataFrames.mean(desc,"0")).desc())   ;
+        for(String column : dataFrame.columns()) {
+            System.out.println(DataFrames.mean(desc,column));
+            System.out.println(DataFrames.min(desc,column));
+            System.out.println(DataFrames.max(desc,column));
+            System.out.println(DataFrames.std(desc,column));
+
+        }
+
 
     }
 
