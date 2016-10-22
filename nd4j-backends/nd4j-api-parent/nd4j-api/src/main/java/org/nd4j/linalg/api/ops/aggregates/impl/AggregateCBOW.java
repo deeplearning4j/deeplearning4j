8a65eca1ede7cae5bb428509b16aed9ca56b1daa
raver119 <raver119@gmail.com>
2016-10-22 15:16:51 +0300

    paravec hook for cbow

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/aggregates/impl/AggregateCBOW.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/aggregates/impl/AggregateCBOW.java
@@ -10,6 +10,52 @@ import org.nd4j.linalg.factory.Nd4j;
 public class AggregateCBOW extends BaseAggregate {
     private int vectorLength;
 
+    /**
+     * Optional constructor for ParagraphVectors PV-DM implementation
+     *
+     * @param syn0
+     * @param syn1
+     * @param syn1Neg
+     * @param expTable
+     * @param negTable
+     * @param wordIdx
+     * @param idxSyn0
+     * @param idxSyn1
+     * @param codes
+     * @param negativeRounds
+     * @param ngStarter
+     * @param vectorLength
+     * @param alpha
+     * @param nextRandom
+     * @param vocabSize
+     * @param numLabels
+     * @param trainWords
+     */
+    public AggregateCBOW(INDArray syn0, INDArray syn1, INDArray syn1Neg, INDArray expTable, INDArray negTable, int wordIdx, int[] idxSyn0, int[] idxSyn1, int[] codes, int negativeRounds, int ngStarter, int vectorLength, double alpha, long nextRandom, int vocabSize, int numLabels, boolean trainWords) {
+        this(syn0, syn1, syn1Neg, expTable, negTable, wordIdx, idxSyn0, idxSyn1, codes, negativeRounds, ngStarter, vectorLength, alpha, nextRandom, vocabSize);
+
+        indexingArguments.set(9, numLabels);
+        indexingArguments.set(10, trainWords ? 1 : 0);
+    }
+
+    /**
+     * Default constructor for CBOW implementation wrapper
+     * @param syn0
+     * @param syn1
+     * @param syn1Neg
+     * @param expTable
+     * @param negTable
+     * @param wordIdx
+     * @param idxSyn0
+     * @param idxSyn1
+     * @param codes
+     * @param negativeRounds
+     * @param ngStarter
+     * @param vectorLength
+     * @param alpha
+     * @param nextRandom
+     * @param vocabSize
+     */
     public AggregateCBOW(INDArray syn0, INDArray syn1, INDArray syn1Neg, INDArray expTable, INDArray negTable, int wordIdx, int[] idxSyn0, int[] idxSyn1, int[] codes, int negativeRounds, int ngStarter, int vectorLength, double alpha, long nextRandom, int vocabSize) {
         indexingArguments.add(vectorLength);
         indexingArguments.add(idxSyn1.length);
@@ -20,6 +66,9 @@ public class AggregateCBOW extends BaseAggregate {
         indexingArguments.add(negTable == null ? 0 : negTable.length());
         indexingArguments.add(idxSyn0.length);
         indexingArguments.add(wordIdx);
+        indexingArguments.add(0);
+        indexingArguments.add(1);
+
 
         arguments.add(syn0);
         arguments.add(syn1);
@@ -69,7 +118,7 @@ public class AggregateCBOW extends BaseAggregate {
 
     @Override
     public int maxIndexArguments() {
-        return 9;
+        return 11;
     }
 
     @Override
