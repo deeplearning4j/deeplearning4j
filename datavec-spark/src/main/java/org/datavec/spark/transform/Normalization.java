aa80d61b401158210b47bd4f35a11435b818983a
Alex Black <blacka101@gmail.com>
2016-10-24 13:43:13 +1100

    Additional sequence normalization overloads + javadoc

--- datavec-spark/src/main/java/org/datavec/spark/transform/Normalization.java
+++ datavec-spark/src/main/java/org/datavec/spark/transform/Normalization.java
@@ -4,11 +4,14 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
+
 import static org.apache.spark.sql.functions.*;
+
 import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.writable.Writable;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -25,11 +28,9 @@ import java.util.List;
 public class Normalization {
 
 
-
-
-
     /**
      * Normalize by zero mean unit variance
+     *
      * @param frame the data to normalize
      * @return a zero mean unit variance centered
      * rdd
@@ -40,87 +41,85 @@ public class Normalization {
 
     /**
      * Normalize by zero mean unit variance
+     *
      * @param schema the schema to use
      *               to create the data frame
-     * @param data the data to normalize
+     * @param data   the data to normalize
      * @return a zero mean unit variance centered
      * rdd
      */
     public static JavaRDD<List<Writable>> zeromeanUnitVariance(Schema schema, JavaRDD<List<Writable>> data) {
-        return zeromeanUnitVariance(schema,data,Collections.<String>emptyList());
+        return zeromeanUnitVariance(schema, data, Collections.<String>emptyList());
     }
 
     /**
      * Scale based on min,max
+     *
      * @param dataFrame the dataframe to scale
-     * @param min the minimum value
-     * @param max the maximum value
+     * @param min       the minimum value
+     * @param max       the maximum value
      * @return the normalized dataframe per column
      */
-    public static DataFrame normalize(DataFrame dataFrame,double min,double max) {
-        return normalize(dataFrame,min,max,Collections.<String>emptyList());
+    public static DataFrame normalize(DataFrame dataFrame, double min, double max) {
+        return normalize(dataFrame, min, max, Collections.<String>emptyList());
     }
 
     /**
      * Scale based on min,max
+     *
      * @param schema the schema of the data to scale
-     * @param data the data to sclae
-     * @param min the minimum value
-     * @param max the maximum value
+     * @param data   the data to sclae
+     * @param min    the minimum value
+     * @param max    the maximum value
      * @return the normalized ata
      */
-    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data,double min,double max) {
-        DataFrame frame = DataFrames.toDataFrame(schema,data);
-        return DataFrames.toRecords(normalize(frame,min,max,Collections.<String>emptyList())).getSecond();
+    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data, double min, double max) {
+        DataFrame frame = DataFrames.toDataFrame(schema, data);
+        return DataFrames.toRecords(normalize(frame, min, max, Collections.<String>emptyList())).getSecond();
     }
-
 
 
     /**
      * Scale based on min,max
+     *
      * @param dataFrame the dataframe to scale
      * @return the normalized dataframe per column
      */
     public static DataFrame normalize(DataFrame dataFrame) {
-        return normalize(dataFrame,0,1,Collections.<String>emptyList());
+        return normalize(dataFrame, 0, 1, Collections.<String>emptyList());
     }
 
     /**
      * Scale all data  0 to 1
+     *
      * @param schema the schema of the data to scale
-     * @param data the data to scale
+     * @param data   the data to scale
      * @return the normalized ata
      */
     public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data) {
-        return normalize(schema,data,0,1,Collections.<String>emptyList());
+        return normalize(schema, data, 0, 1, Collections.<String>emptyList());
     }
-
-
-
-
-
-
-
 
 
     /**
      * Normalize by zero mean unit variance
+     *
      * @param frame the data to normalize
      * @return a zero mean unit variance centered
      * rdd
      */
-    public static DataFrame zeromeanUnitVariance(DataFrame frame,List<String> skipColumns) {
+    public static DataFrame zeromeanUnitVariance(DataFrame frame, List<String> skipColumns) {
         String[] columnNames = frame.columns();
-        for(String columnName : columnNames) {
-            if(skipColumns.contains(columnName)) continue;
+        for (String columnName : columnNames) {
+            if (skipColumns.contains(columnName)) continue;
 
             DataFrame meanStd = frame.select(columnName).agg(mean(columnName), stddev(columnName));
             Row r = meanStd.collect()[0];
-            double mean = ((Number)r.get(0)).doubleValue();
-            double std = ((Number)r.get(1)).doubleValue();
-            if(std == 0.0) std = 1; //All same value -> (x-x)/1 = 0
+            double mean = ((Number) r.get(0)).doubleValue();
+            double std = ((Number) r.get(1)).doubleValue();
+            if (std == 0.0) std = 1; //All same value -> (x-x)/1 = 0
 
-            frame = frame.withColumn(columnName,frame.col(columnName).minus(mean).divide(std));
+            frame = frame.withColumn(columnName, frame.col(columnName).minus(mean).divide(std));
         }
 
         return frame;
@@ -128,51 +127,75 @@ public class Normalization {
 
     /**
      * Normalize by zero mean unit variance
+     *
      * @param schema the schema to use
      *               to create the data frame
-     * @param data the data to normalize
+     * @param data   the data to normalize
      * @return a zero mean unit variance centered
      * rdd
      */
-    public static JavaRDD<List<Writable>> zeromeanUnitVariance(Schema schema, JavaRDD<List<Writable>> data,List<String> skipColumns) {
-        DataFrame frame = DataFrames.toDataFrame(schema,data);
-        return DataFrames.toRecords(zeromeanUnitVariance(frame,skipColumns)).getSecond();
+    public static JavaRDD<List<Writable>> zeromeanUnitVariance(Schema schema, JavaRDD<List<Writable>> data, List<String> skipColumns) {
+        DataFrame frame = DataFrames.toDataFrame(schema, data);
+        return DataFrames.toRecords(zeromeanUnitVariance(frame, skipColumns)).getSecond();
     }
 
-    public static JavaRDD<List<List<Writable>>> zeroMeanUnitVarianceSequence(Schema schema, JavaRDD<List<List<Writable>>> sequence){
+    /**
+     * Normalize the sequence by zero mean unit variance
+     *
+     * @param schema   Schema of the data to normalize
+     * @param sequence Sequence data
+     * @return Normalized sequence
+     */
+    public static JavaRDD<List<List<Writable>>> zeroMeanUnitVarianceSequence(Schema schema, JavaRDD<List<List<Writable>>> sequence) {
+        return zeroMeanUnitVarianceSequence(schema, sequence, null);
+    }
+
+    /**
+     * Normalize the sequence by zero mean unit variance
+     *
+     * @param schema         Schema of the data to normalize
+     * @param sequence       Sequence data
+     * @param excludeColumns List of  columns to exclude from the normalization
+     * @return Normalized sequence
+     */
+    public static JavaRDD<List<List<Writable>>> zeroMeanUnitVarianceSequence(Schema schema, JavaRDD<List<List<Writable>>> sequence,
+                                                                             List<String> excludeColumns) {
         DataFrame frame = DataFrames.toDataFrameSequence(schema, sequence);
-        frame = zeromeanUnitVariance(frame, Arrays.asList(DataFrames.SEQUENCE_UUID_COLUMN, DataFrames.SEQUENCE_INDEX_COLUMN));
+        if (excludeColumns == null)
+            excludeColumns = Arrays.asList(DataFrames.SEQUENCE_UUID_COLUMN, DataFrames.SEQUENCE_INDEX_COLUMN);
+        else {
+            excludeColumns = new ArrayList<>(excludeColumns);
+            excludeColumns.add(DataFrames.SEQUENCE_UUID_COLUMN);
+            excludeColumns.add(DataFrames.SEQUENCE_INDEX_COLUMN);
+        }
+        frame = zeromeanUnitVariance(frame, excludeColumns);
         return DataFrames.toRecordsSequence(frame).getSecond();
     }
 
     /**
      * Scale based on min,max
+     *
      * @param dataFrame the dataframe to scale
-     * @param min the minimum value
-     * @param max the maximum value
+     * @param min       the minimum value
+     * @param max       the maximum value
      * @return the normalized dataframe per column
      */
-    public static DataFrame normalize(DataFrame dataFrame,double min,double max,List<String> skipColumns) {
+    public static DataFrame normalize(DataFrame dataFrame, double min, double max, List<String> skipColumns) {
         String[] columnNames = dataFrame.columns();
 
-        for(String columnName : columnNames) {
-            if(skipColumns.contains(columnName))
+        for (String columnName : columnNames) {
+            if (skipColumns.contains(columnName))
                 continue;
             DataFrame minMax = dataFrame.select(columnName).agg(min(columnName), max(columnName));
             Row r = minMax.collect()[0];
-            double dMin = ((Number)r.get(0)).doubleValue();
-            double dMax = ((Number)r.get(1)).doubleValue();
+            double dMin = ((Number) r.get(0)).doubleValue();
+            double dMax = ((Number) r.get(1)).doubleValue();
 
             double maxSubMin = dMax - dMin;
-            if(maxSubMin == 0) maxSubMin = 1;
+            if (maxSubMin == 0) maxSubMin = 1;
 
             Column newCol = dataFrame.col(columnName).minus(dMin).divide(maxSubMin).multiply(max - min).plus(min);
-            dataFrame = dataFrame.withColumn(columnName,newCol);
-
-//            Column min2 = DataFrames.min(dataFrame,columnName);
-//            Column max2 = DataFrames.max(dataFrame,columnName);
-//            Column maxMinusMin = max2.minus(min2);
-//            dataFrame = dataFrame.withColumn(columnName,dataFrame.col(columnName).minus(min2).divide(maxMinusMin.plus(1e-6)).multiply(max - min).plus(min));
+            dataFrame = dataFrame.withColumn(columnName, newCol);
         }
 
         return dataFrame;
@@ -180,39 +203,77 @@ public class Normalization {
 
     /**
      * Scale based on min,max
+     *
      * @param schema the schema of the data to scale
-     * @param data the data to sclae
-     * @param min the minimum value
-     * @param max the maximum value
+     * @param data   the data to sclae
+     * @param min    the minimum value
+     * @param max    the maximum value
      * @return the normalized ata
      */
-    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data,double min,double max,List<String> skipColumns) {
-        DataFrame frame = DataFrames.toDataFrame(schema,data);
-        return DataFrames.toRecords(normalize(frame,min,max,skipColumns)).getSecond();
+    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data, double min, double max, List<String> skipColumns) {
+        DataFrame frame = DataFrames.toDataFrame(schema, data);
+        return DataFrames.toRecords(normalize(frame, min, max, skipColumns)).getSecond();
     }
 
-    public static JavaRDD<List<List<Writable>>> normalizeSequence(Schema schema, JavaRDD<List<List<Writable>>> data, double min, double max){
-        DataFrame frame = DataFrames.toDataFrameSequence(schema,data);
-        return DataFrames.toRecordsSequence(normalize(frame,min,max, Arrays.asList(DataFrames.SEQUENCE_UUID_COLUMN, DataFrames.SEQUENCE_INDEX_COLUMN))).getSecond();
+    public static JavaRDD<List<List<Writable>>> normalizeSequence(Schema schema, JavaRDD<List<List<Writable>>> data) {
+        return normalizeSequence(schema, data, 0, 1);
+    }
+
+    /**
+     * Normalize each column of a sequence, based on min/max
+     *
+     * @param schema Schema of the data
+     * @param data   Data to normalize
+     * @param min    New minimum value
+     * @param max    New maximum value
+     * @return Normalized data
+     */
+    public static JavaRDD<List<List<Writable>>> normalizeSequence(Schema schema, JavaRDD<List<List<Writable>>> data, double min, double max) {
+        return normalizeSequence(schema, data, min, max, null);
+    }
+
+    /**
+     * Normalize each column of a sequence, based on min/max
+     *
+     * @param schema         Schema of the data
+     * @param data           Data to normalize
+     * @param min            New minimum value
+     * @param max            New maximum value
+     * @param excludeColumns List of columns to exclude
+     * @return Normalized data
+     */
+    public static JavaRDD<List<List<Writable>>> normalizeSequence(Schema schema, JavaRDD<List<List<Writable>>> data,
+                                                                  double min, double max, List<String> excludeColumns) {
+        if (excludeColumns == null)
+            excludeColumns = Arrays.asList(DataFrames.SEQUENCE_UUID_COLUMN, DataFrames.SEQUENCE_INDEX_COLUMN);
+        else {
+            excludeColumns = new ArrayList<>(excludeColumns);
+            excludeColumns.add(DataFrames.SEQUENCE_UUID_COLUMN);
+            excludeColumns.add(DataFrames.SEQUENCE_INDEX_COLUMN);
+        }
+        DataFrame frame = DataFrames.toDataFrameSequence(schema, data);
+        return DataFrames.toRecordsSequence(normalize(frame, min, max, excludeColumns)).getSecond();
     }
 
 
     /**
      * Scale based on min,max
+     *
      * @param dataFrame the dataframe to scale
      * @return the normalized dataframe per column
      */
-    public static DataFrame normalize(DataFrame dataFrame,List<String> skipColumns) {
-        return normalize(dataFrame,0,1,skipColumns);
+    public static DataFrame normalize(DataFrame dataFrame, List<String> skipColumns) {
+        return normalize(dataFrame, 0, 1, skipColumns);
     }
 
     /**
      * Scale all data  0 to 1
+     *
      * @param schema the schema of the data to scale
-     * @param data the data to scale
+     * @param data   the data to scale
      * @return the normalized ata
      */
-    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data,List<String> skipColumns) {
-        return normalize(schema,data,0,1,skipColumns);
+    public static JavaRDD<List<Writable>> normalize(Schema schema, JavaRDD<List<Writable>> data, List<String> skipColumns) {
+        return normalize(schema, data, 0, 1, skipColumns);
     }
 }
