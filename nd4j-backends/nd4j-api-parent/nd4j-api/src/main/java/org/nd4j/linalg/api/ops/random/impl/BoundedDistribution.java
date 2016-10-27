f6587d2b4afaface5e81d5bb5459c2bbdef992f6
raver119 <raver119@gmail.com>
2016-10-27 15:19:45 +0300

    initial commit

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/random/impl/BoundedDistribution.java
@@ -0,0 +1,47 @@
+package org.nd4j.linalg.api.ops.random.impl;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.random.BaseRandomOp;
+
+/**
+ * @author raver119@gmail.com
+ */
+public class BoundedDistribution extends BaseRandomOp {
+    private double from;
+    private double to;
+
+    public BoundedDistribution(){
+        super();
+    }
+
+    /**
+     * This op fills Z with random values within from...to boundaries
+     * @param z
+     * @param from
+     * @param to
+     */
+    public BoundedDistribution(INDArray z, double from, double to) {
+        init(z, null, z, z.length());
+        this.from = from;
+        this.to = to;
+        this.extraArgs = new Object[]{from, to};
+    }
+
+    /**
+     * This op fills Z with random values within 0...1
+     * @param z
+     */
+    public BoundedDistribution(INDArray z) {
+        this(z, 0.0, 1.0);
+    }
+
+    @Override
+    public int opNum() {
+        return 0;
+    }
+
+    @Override
+    public String name() {
+        return "bounded_distribution";
+    }
+}
