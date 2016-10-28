782c7d9e77a500b0385eac29fee4c098295a75fb
ejunprung <ejunprung@gmail.com>
2016-10-28 00:36:59 -0700

    Add graph library, layer formatting, new UI styles

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/custom.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/custom.js
@@ -1208,7 +1208,7 @@ function charts() {
 				padding: '2px',
 				'background-color': '#dfeffc',
 				opacity: 0.80
-			}).appendTo("body").fadeIn(200);
+			}).appendTo(".box").fadeIn(200);
 		}
 
 		var previousPoint = null;
@@ -1444,40 +1444,10 @@ function charts() {
 				   colors: ["#FA5833", "#2FABE9"]
 				 });
 
-		function showTooltip(x, y, contents) {
-			$('<div id="tooltip">' + contents + '</div>').css( {
-				position: 'absolute',
-				display: 'none',
-				top: y + 5,
-				left: x + 5,
-				border: '1px solid #fdd',
-				padding: '2px',
-				'background-color': '#dfeffc',
-				opacity: 0.80
-			}).appendTo("body").fadeIn(200);
-		}
-
 		var previousPoint = null;
 		$("#sincos").bind("plothover", function (event, pos, item) {
-			$("#x").text(pos.x.toFixed(2));
+			$("#x").text(pos.x.toFixed(0));
 			$("#y").text(pos.y.toFixed(2));
-
-				if (item) {
-					if (previousPoint != item.dataIndex) {
-						previousPoint = item.dataIndex;
-
-						$("#tooltip").remove();
-						var x = item.datapoint[0].toFixed(2),
-							y = item.datapoint[1].toFixed(2);
-
-						showTooltip(item.pageX, item.pageY,
-									item.series.label + " of " + x + " = " + y);
-					}
-				}
-				else {
-					$("#tooltip").remove();
-					previousPoint = null;
-				}
 		});
 
 
