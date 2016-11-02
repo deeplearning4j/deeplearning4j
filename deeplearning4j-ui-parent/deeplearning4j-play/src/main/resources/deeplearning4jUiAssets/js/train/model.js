47426a2cd4fdf8740146f4251b639a75d01a4283
ejunprung <ejunprung@gmail.com>
2016-11-01 17:35:48 -0700

    Feeding data to model page charts
    Former-commit-id: e944951f539300efce97c42a1bc863d7fc32986a [formerly 0ec285cb99f25b45056da19580908ae4bba43a18]
    Former-commit-id: 5fd78bf43b0b87ff4f9386f14932c8a748e4fa20

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/model.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/model.js
@@ -1,5 +1,4 @@
-/* ---------- Mean Magnitudes Chart ---------- */
-function renderMeanMagChart() {
+function renderModelPage() {
 
     $.ajax({
         url: "/train/model/data/0",
@@ -10,49 +9,139 @@ function renderMeanMagChart() {
         success: function (data) {
             console.log("Keys: " + Object.keys(data));
 
-            var b = data["meanMagRatio"]["b"];
-            var w = data["meanMagRatio"]["w"];
-            var iter = data["meanMagRatio"]["iterCounts"];
-
-
-            if ($("#scoreiterchart").length) {
-                var b = [];
-                var w = [];
-
-                for (var i = 0; i < iter.length; i++) {
-                    b.push([iter[i], b[i]]);
-                    w.push([iter[i], w[i]]);
-                }
-
-                var plot = $.plot($("#scoreiterchart"),
-                    [{data: b, label: "b"},{data: w, label: "w"}], {
-                        series: {
-                            lines: {
-                                show: true,
-                                lineWidth: 2,
-                            },
-                            points: {show: true},
-                            shadowSize: 2
-                        },
-                        grid: {
-                            hoverable: true,
-                            clickable: true,
-                            tickColor: "#dddddd",
-                            borderWidth: 0
-                        },
-                        yaxis: {min: 0, max: maxScore},
-                        colors: ["#FA5833", "#2FABE9"]
-                    });
-
-                var previousPoint = null;
-                $("#scoreiterchart").bind("plothover", function (event, pos, item) {
-                    $("#x").text(pos.x.toFixed(0));
-                    $("#y").text(pos.y.toFixed(2));
-                });
-            }
+            renderMeanMagChart(data);
+            renderActivationsChart(data);
+            renderLearningRateChart(data);
+            renderParametersHistogram(data);
         }
     });
 
+}
+
+/* ---------- Mean Magnitudes Chart ---------- */
+function renderMeanMagChart(data) {
+
+    var b = data["meanMagRatio"]["b"];
+    var W = data["meanMagRatio"]["W"];
+    var iter = data["meanMagRatio"]["iterCounts"];
+
+    if ($("#meanmag").length) {
+        var bData = [];
+        var WData = [];
+
+        for (var i = 0; i < iter.length; i++) {
+            bData.push([iter[i], b[i]]);
+            WData.push([iter[i], W[i]]);
+        }
+
+        var plot = $.plot($("#meanmag"),
+            [{data: bData, label: "Bias"},{data: WData, label: "Weights"}], {
+                series: {
+                    lines: {
+                        show: true,
+                        lineWidth: 2,
+                    }
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: true,
+                    tickColor: "#dddddd",
+                    borderWidth: 0
+                },
+                yaxis: {min: 0, max: 1},
+                colors: ["#FA5833", "#2FABE9"]
+            });
+
+        var previousPoint = null;
+        $("#meanmag").bind("plothover", function (event, pos, item) {
+            $("#x").text(pos.x.toFixed(0));
+            $("#y").text(pos.y.toFixed(2));
+        });
+    }
+}
+
+/* ---------- Activations Chart ---------- */
+function renderActivationsChart(data) {
+
+    var mean = data["activations"]["mean"];
+    var stdev = data["activations"]["stdev"];
+    var iter = data["activations"]["iterCount"];
+
+    if ($("#activations").length) {
+        var meanData = [];
+        var stdevData = [];
+
+        for (var i = 0; i < iter.length; i++) {
+            meanData.push([iter[i], mean[i]]);
+            stdevData.push([iter[i], stdev[i]]);
+        }
+
+        var plot = $.plot($("#activations"),
+            [{data: meanData, label: "Mean"},{data: stdevData, label: "Standard Deviation"}], {
+                series: {
+                    lines: {
+                        show: true,
+                        lineWidth: 2,
+                    }
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: true,
+                    tickColor: "#dddddd",
+                    borderWidth: 0
+                },
+                yaxis: {min: -1, max: 1},
+                colors: ["#FA5833", "#2FABE9"]
+            });
+
+        var previousPoint = null;
+        $("#activations").bind("plothover", function (event, pos, item) {
+            $("#x").text(pos.x.toFixed(0));
+            $("#y").text(pos.y.toFixed(2));
+        });
+    }
+}
+
+/* ---------- Learning Rate Chart ---------- */
+function renderLearningRateChart(data) {
+
+    var lrs_b = data["learningRates"]["lrs"]["b"];
+    var lrs_W = data["learningRates"]["lrs"]["W"];
+    var iter = data["learningRates"]["iterCounts"];
+
+    if ($("#learningrate").length) {
+        var lrs_bData = [];
+        var lrs_WData = [];
+
+        for (var i = 0; i < iter.length; i++) {
+            lrs_bData.push([iter[i], lrs_b[i]]);
+            lrs_WData.push([iter[i], lrs_W[i]]);
+        }
+
+        var plot = $.plot($("#learningrate"),
+            [{data: lrs_bData, label: "lrs_b"},{data: lrs_WData, label: "lrs_w"}], {
+                series: {
+                    lines: {
+                        show: true,
+                        lineWidth: 2,
+                    }
+                },
+                grid: {
+                    hoverable: true,
+                    clickable: true,
+                    tickColor: "#dddddd",
+                    borderWidth: 0
+                },
+                yaxis: {min: 0, max: 1},
+                colors: ["#FA5833", "#2FABE9"]
+            });
+
+        var previousPoint = null;
+        $("#learningrate").bind("plothover", function (event, pos, item) {
+            $("#x").text(pos.x.toFixed(0));
+            $("#y").text(pos.y.toFixed(2));
+        });
+    }
 }
 
 /* ---------- Layer Table Data ---------- */
@@ -70,11 +159,11 @@ function renderLayerTable() {
             /* Layer */
             var layerName = data["layerNames"][1];
             var layerType = data["layerTypes"][1];
-            var inputSize = data["layerInfo"]["Input size"];
-            var outputSize = data["layerInfo"]["Output size"];
-            var nParams = data["layerInfo"]["Num Parameters"];
-            var activationFunction = data["layerInfo"]["Activation Function"];
-            var lossFunction = data["layerInfo"]["Loss Function"];
+            var inputSize = data["layerInfo"][1]["Input size"];
+            var outputSize = data["layerInfo"][1]["Output size"];
+            var nParams = data["layerInfo"][1]["Num Parameters"];
+            var activationFunction = data["layerInfo"][1]["Activation Function"];
+            var lossFunction = data["layerInfo"][1]["Loss Function"];
 
             $("#layerName").html(layerName);
             $("#layerType").html(layerType);
@@ -86,4 +175,54 @@ function renderLayerTable() {
         }
     });
 
+}
+
+
+
+	/* ---------- Parameters Histogram ---------- */
+
+function renderParametersHistogram(data) {
+
+	if($("#parametershistogram").length)
+	{
+		var d1 = [];
+		for (var i = 0; i <= 10; i += 1)
+		d1.push([i, parseInt(Math.random() * 30)]);
+
+		var d2 = [];
+		for (var i = 0; i <= 10; i += 1)
+			d2.push([i, parseInt(Math.random() * 30)]);
+
+		var d3 = [];
+		for (var i = 0; i <= 10; i += 1)
+			d3.push([i, parseInt(Math.random() * 30)]);
+
+		var stack = 0, bars = true, lines = false, steps = false;
+
+		function plotWithOptions() {
+			$.plot($("#parametershistogram"), [ d1, d2, d3 ], {
+				series: {
+					stack: stack,
+					lines: { show: lines, fill: true, steps: steps },
+					bars: { show: bars, barWidth: 0.6 },
+				},
+				colors: ["#FA5833", "#2FABE9", "#FABB3D"]
+			});
+		}
+
+		plotWithOptions();
+
+		$(".stackControls input").click(function (e) {
+			e.preventDefault();
+			stack = $(this).val() == "With stacking" ? true : null;
+			plotWithOptions();
+		});
+		$(".graphControls input").click(function (e) {
+			e.preventDefault();
+			bars = $(this).val().indexOf("Bars") != -1;
+			lines = $(this).val().indexOf("Lines") != -1;
+			steps = $(this).val().indexOf("steps") != -1;
+			plotWithOptions();
+		});
+	}
 }
