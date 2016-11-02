861c61557950d64c2c08a5771052345c22093f4d
DH <kepricon@gmail.com>
2016-11-02 11:25:30 +0900

    embed kuromoji 0.9.0(core & ipadic) for japanese tokenizer and  add korean tokenizer (#2249)
    fix korean nlp pom and japanese nlp pom
    
    Former-commit-id: 3dca94eb733fbc3782e82c704636f99429a4b7fd

---
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp-japanese/src/main/java/com/atilika/kuromoji/io/ByteBufferIO.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright © 2010-2015 Atilika Inc. and contributors (see CONTRIBUTORS.md)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.  A copy of the
+ * License is distributed with this work in the LICENSE.md file.  You may
+ * also obtain a copy of the License from
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atilika.kuromoji.io;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+public class ByteBufferIO {
+
+    public static ByteBuffer read(InputStream input) throws IOException {
+        DataInputStream dataInput = new DataInputStream(
+            new BufferedInputStream(input)
+        );
+
+        int size = dataInput.readInt();
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+
+        ReadableByteChannel channel = Channels.newChannel(dataInput);
+        channel.read(buffer);
+
+        buffer.rewind();
+        return buffer;
+    }
+
+    public static void write(OutputStream output, ByteBuffer buffer) throws IOException {
+        DataOutputStream dataOutput = new DataOutputStream(
+            new BufferedOutputStream(output)
+        );
+
+        dataOutput.writeInt(buffer.position());
+        buffer.flip();
+
+        WritableByteChannel channel = Channels.newChannel(dataOutput);
+        channel.write(buffer);
+        dataOutput.flush(); // TODO: Do we need this?
+    }
+}
---
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp-korean/src/test/java/org/deeplearning4j/text/tokenization/tokenizer/KoreanTokenizerTest.java
@@ -0,0 +1,27 @@
+package org.deeplearning4j.text.tokenization.tokenizer;
+
+import org.deeplearning4j.text.tokenization.tokenizerfactory.KoreanTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kepricon on 16. 10. 24.
+ */
+public class KoreanTokenizerTest {
+    @Test
+    public void testKoreanTokenizer() throws Exception {
+        String toTokenize = "세계 최초의 상용 수준 오픈소스 딥러닝 라이브러리입니다";
+        TokenizerFactory t = new KoreanTokenizerFactory();
+        Tokenizer tokenizer = t.create(toTokenize);
+        String[] expect = {"세계", "최초", "의", "상용", "수준", "오픈소스", "딥", "러닝", "라이브러리", "입니", "다"};
+
+        assertEquals(expect.length, tokenizer.countTokens());
+
+        for (int i = 0; i < tokenizer.countTokens(); ++i) {
+            assertEquals(tokenizer.nextToken(), expect[i]);
+        }
+    }
+
+}
