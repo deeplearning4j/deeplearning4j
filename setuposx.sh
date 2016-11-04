2cb7d994413b13e35262eda8e176f9f5bb1d5d9d
Samuel Audet <samuel.audet@gmail.com>
2016-11-04 21:58:15 +0900

    Update setuposx.sh to install GCC

--- setuposx.sh
+++ setuposx.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-#setup openmp for clang
-#http://stackoverflow.com/questions/20321988/error-enabling-openmp-ld-library-not-found-for-lgomp-and-clang-errors
-brew install llvm
+#setup gcc for openmp
+brew install gcc5
