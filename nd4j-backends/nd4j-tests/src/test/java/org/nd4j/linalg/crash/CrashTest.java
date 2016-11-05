a9b9d3f1ef1b3c4ed3e13bc7f72600a024c3d3d2
raver119 <raver119@gmail.com>
2016-11-05 19:14:31 +0300

    crashtest 1

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/crash/CrashTest.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/crash/CrashTest.java
@@ -4,6 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.nd4j.linalg.BaseNd4jTest;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.accum.distances.ManhattanDistance;
+import org.nd4j.linalg.api.ops.impl.indexaccum.IMax;
+import org.nd4j.linalg.api.ops.impl.transforms.LogSoftMax;
+import org.nd4j.linalg.api.ops.impl.transforms.SoftMax;
+import org.nd4j.linalg.api.ops.impl.transforms.SoftMaxDerivative;
+import org.nd4j.linalg.api.ops.impl.transforms.Sqrt;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.factory.Nd4jBackend;
 
 /**
@@ -17,9 +25,71 @@ public class CrashTest extends BaseNd4jTest {
         super(backend);
     }
 
-    @Test
-    public void testPlaceholder() {
+    private static final int ITERATIONS = 100;
 
+    @Test
+    public void testArrays1() {
+        INDArray x = Nd4j.create(1024, 64);
+
+        for(int i = 0; i < ITERATIONS; i++) {
+
+            // broadcast along row & column
+            INDArray row = Nd4j.ones(64);
+            INDArray column = Nd4j.ones(1024, 1);
+
+            x.addiRowVector(row);
+            x.addiColumnVector(column);
+
+            // casual scalar
+            x.addi(i * 2);
+
+            // reduction along all dimensions
+            float sum = x.maxNumber().floatValue();
+
+            // index reduction
+            IMax imax = new IMax(x);
+            Nd4j.getExecutioner().exec(imax, Integer.MAX_VALUE);
+            int max = imax.getFinalResult();
+
+            // casual transform
+            Nd4j.getExecutioner().exec(new Sqrt(x, x));
+
+            //  dup
+            INDArray x1 = x.dup();
+            INDArray x2 = x.dup();
+            INDArray x3 = x.dup();
+            INDArray x4 = x.dup();
+
+            // vstack && hstack
+            INDArray vstack = Nd4j.vstack(x1, x2, x3, x4);
+            INDArray hstack = Nd4j.hstack(x1, x2, x3, x4);
+
+            // reduce3 call
+            Nd4j.getExecutioner().exec(new ManhattanDistance(x, x2));
+
+            // flatten call
+            INDArray flat = Nd4j.toFlattened(x, x1, x2, x3, x4);
+
+
+            // reduction along dimension: row & column
+            INDArray max_0 = x.max(0);
+            INDArray max_1 = x.max(1);
+
+            // index reduction along dimension: row & column
+            INDArray imax_0 = Nd4j.argMax(x, 0);
+            INDArray imax_1 = Nd4j.argMax(x, 1);
+
+            // logisoftmax, softmax & softmax derivative
+            Nd4j.getExecutioner().exec(new SoftMax(x));
+            Nd4j.getExecutioner().exec(new SoftMaxDerivative(x));
+            Nd4j.getExecutioner().exec(new LogSoftMax(x));
+
+
+            // blas call
+            float dot = (float) Nd4j.getBlasWrapper().dot(x, x1);
+
+            System.out.println("Iteration passed: " + i);
+        }
     }
 
     @Override
