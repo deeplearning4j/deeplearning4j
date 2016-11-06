1df940ac5fb2e59ab5365119d6b8b05079827bdf
ejunprung <ejunprung@gmail.com>
2016-11-05 22:54:50 -0700

    Add language dropdown javascript
    Former-commit-id: 12104a9020cfa1e37ac67ec64a30438e1908d5e8 [formerly 980beef447289d11388fa8fbe816c05adb7bbcda]
    Former-commit-id: d6662b6031e326061c60f2a275ccc3a749a1d638

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/train.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/train.js
@@ -44,3 +44,10 @@ function formatBytes(bytes, precision){
 
     return newValue.toFixed(precision) + " " + unit;
 }
+
+/* ---------- Language Dropdown ---------- */
+
+	$('.dropmenu').click(function(e){
+		e.preventDefault();
+		$(this).parent().find('ul').slideToggle();
+	});
