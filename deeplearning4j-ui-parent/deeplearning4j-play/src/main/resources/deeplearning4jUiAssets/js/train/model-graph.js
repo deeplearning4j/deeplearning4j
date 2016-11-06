ed29b07bf042cc75f52803710ced0c0315dd1264
ejunprung <ejunprung@gmail.com>
2016-11-06 13:09:18 -0800

    Model layer styles (Model Page)
    Former-commit-id: 85435570c590039a5422a6150784eb1df2ceff00

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/model-graph.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/model-graph.js
@@ -13,7 +13,6 @@ function renderModelGraph(){
     });
 }
 
-
 function createGraph(data){
 
     //Generate the elements data
@@ -23,14 +22,56 @@ function createGraph(data){
     var vertexInfos = data["vertexInfo"];     //List<Map<String,String>>
     var vertexCount = vertexNames.length;
 
+    //Layer Styles
+    var layerStyles = {
+      "Activation": ["#800000", "rectangle"],
+      "AutoEncoder": ["#2874A6","rectangle"],
+      "BaseOutput": ["#2ECC71","rectangle"],
+      "BasePretrainNetwork": ["#9A7D0A","rectangle"],
+      "BaseRecurrent": ["#212F3D","rectangle"],
+      "BatchNormalization": ["#008080","rectangle"],
+      "Convolution": ["#424949","rectangle"],
+      "Dense": ["#000080","rectangle"],
+      "Embedding": ["#CD5C5C","rectangle"],
+      "FeedForward": ["#9B59B6","rectangle"],
+      "GravesBidirectionalLSTM": ["#0B5345","rectangle"],
+      "GravesLSTM": ["#2b0b06","rectangle"],
+      "Input": ["#008000","vee"],
+      "InputTypeUtil": ["#DFB391","rectangle"],
+      "LocalResponseNormalization": ["#F1948A","rectangle"],
+      "Output": ["#FF0000","ellipse"],
+      "RBM": ["#F5A45D","rectangle"],
+      "RnnOutput": ["#7D3C98","rectangle"],
+      "Subsampling": ["#7FB3D5","rectangle"],
+      "L2Vertex": ["#800000","triangle"],
+      "LayerVertex": ["#2874A6","triangle"],
+      "MergeVertex": ["#2ECC71","triangle"],
+      "PreprocessorVertex": ["#9A7D0A","triangle"],
+      "StackVertex": ["#212F3D","triangle"],
+      "SubsetVertex": ["#008080","triangle"],
+      "UnstackVertex": ["#424949","triangle"],
+      "DuplicateToTimeSeriesVertex": ["#000080","triangle"],
+      "LastTimeStepVertex": ["#CD5C5C","triangle"]
+    };
+
     var nodes = [];
     var edges = [];
     for(var i=0; i<vertexNames.length; i++ ){
+
+        //Find correct layer color and shape
+        if (Object.keys(layerStyles).indexOf(vertexTypes[i]) > 0 ) {
+          layerColor = layerStyles[vertexTypes[i]][0];
+          layerShape = layerStyles[vertexTypes[i]][1];
+        } else {
+          layerColor = "#000000";
+          layerShape = "octagon";
+        }
+
         var obj = {
             id: i,
             name: vertexTypes[i] + '\n(' + vertexNames[i] +')',
-            faveColor: '#6FB1FC',   //TODO
-            faveShape: 'triangle',   //TODO
+            faveColor: layerColor,
+            faveShape: layerShape,
             onclick: "renderLayerTable()"
         };
         nodes.push({ data: obj} );
@@ -41,7 +82,7 @@ function createGraph(data){
             var e = {
                 source: inputsToCurrent[j],
                 target: i,
-                faveColor: '#A9A9A9',   //TODO
+                faveColor: '#A9A9A9',
                 strength: 100
             };
             edges.push({ data: e} );
