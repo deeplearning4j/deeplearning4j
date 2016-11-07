e9b18cc193bfb26740f81d5174205dc2d9027dc3
rcorbish <rcorbish@outlook.com>
2016-11-06 21:57:55 -0500

    Attempt at CPU

--- nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/blas/CpuLapack.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/blas/CpuLapack.java
@@ -3,6 +3,8 @@ package org.nd4j.linalg.cpu.nativecpu.blas;
 import org.nd4j.linalg.api.blas.impl.BaseLapack;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
+import static org.bytedeco.javacpp.openblas.*;
+
 /**
  * CPU lapack implementation
  */
@@ -10,6 +12,11 @@ public class CpuLapack extends BaseLapack {
 
     @Override
     public void sgetrf(int M, int N, INDArray A, int lda, INDArray IPIV, INDArray INFO) {
+	
+	//public static native int LAPACKE_sgetrf( int matrix_layout, int m, int n,
+        //                   FloatPointer a, int lda, IntPointer ipiv );
+
+	int status = LAPACKE_sgetrf(LAPACK_ROW_MAJOR, M, N, A.data().asNioFloat(), lda, IPIV.data().asNioInt() ) ;
 
     }
 
