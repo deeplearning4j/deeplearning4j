4cc373d1418c8962c4a6c5bef288f91bdb95fb9b
Alex Black <blacka101@gmail.com>
2016-11-08 10:58:57 +1100

    UI: Redirect for / path; fix histogram listener param mean magnitudes
    Former-commit-id: adfba92e0ecee881f90e9821b3a30794a9ccfc04

--- deeplearning4j-ui-parent/deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/weights/TestHistogramListener.java
+++ deeplearning4j-ui-parent/deeplearning4j-ui/src/test/java/org/deeplearning4j/ui/weights/TestHistogramListener.java
@@ -8,6 +8,7 @@ import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
@@ -15,6 +16,7 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 /**
  * Created by Alex on 08/10/2016.
  */
+@Ignore
 public class TestHistogramListener {
 
     @Test
