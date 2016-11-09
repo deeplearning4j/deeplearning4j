893a636bc4ed3d9c19cc0463d0edd02b7a37cc03
raver119 <raver119@gmail.com>
2016-11-09 11:44:41 +0300

    initial commit

---
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/transformers/impl/iterables/BasicTransformerIterator.java
@@ -0,0 +1,54 @@
+package org.deeplearning4j.models.sequencevectors.transformers.impl.iterables;
+
+import lombok.NonNull;
+import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
+import org.deeplearning4j.models.sequencevectors.transformers.impl.SentenceTransformer;
+import org.deeplearning4j.models.word2vec.VocabWord;
+import org.deeplearning4j.text.documentiterator.LabelAwareIterator;
+import org.deeplearning4j.text.documentiterator.LabelledDocument;
+
+import java.util.Iterator;
+
+/**
+ * @author raver119@gmail.com
+ */
+public class BasicTransformerIterator implements Iterator<Sequence<VocabWord>> {
+    protected final LabelAwareIterator iterator;
+    protected boolean allowMultithreading;
+    protected final SentenceTransformer sentenceTransformer;
+
+    public BasicTransformerIterator(@NonNull LabelAwareIterator iterator, @NonNull SentenceTransformer transformer) {
+        this.iterator = iterator;
+        this.allowMultithreading = false;
+        this.sentenceTransformer = transformer;
+
+        this.iterator.reset();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.iterator.hasNextDocument();
+    }
+
+    @Override
+    public Sequence<VocabWord> next() {
+        LabelledDocument document = iterator.nextDocument();
+        if  (document.getContent() == null) return new Sequence<>();
+        Sequence<VocabWord> sequence = sentenceTransformer.transformToSequence(document.getContent());
+
+        for (String label: document.getLabels()) {
+            sequence.addSequenceLabel(new VocabWord(1.0, label));
+        }
+                /*
+                if (document.getLabel() != null && !document.getLabel().isEmpty()) {
+                    sequence.setSequenceLabel(new VocabWord(1.0, document.getLabel()));
+                }*/
+
+        return sequence;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
