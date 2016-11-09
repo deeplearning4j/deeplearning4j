ce21471e178f7e2a62d2778b5296da95b2b42a07
raver119 <raver119@gmail.com>
2016-11-09 16:04:39 +0300

    parallel tokenization p.2 proto & tests
    Former-commit-id: 552d5dc88d0964a11cde3d62d92198d6256683aa [formerly 066dd16cf79ed51847202b3aae1063b130b7d8f2]
    Former-commit-id: de8a18b12e51b23fbb9c16cece22de8230941793

--- deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/transformers/impl/iterables/ParallelTransformerIterator.java
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/models/sequencevectors/transformers/impl/iterables/ParallelTransformerIterator.java
@@ -2,13 +2,17 @@ package org.deeplearning4j.models.sequencevectors.transformers.impl.iterables;
 
 import lombok.NonNull;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
+import org.deeplearning4j.models.sequencevectors.transformers.SequenceTransformer;
 import org.deeplearning4j.models.sequencevectors.transformers.impl.SentenceTransformer;
 import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.text.documentiterator.LabelAwareIterator;
+import org.deeplearning4j.text.documentiterator.LabelledDocument;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * TransformerIterator implementation that's does transformation/tokenization/normalization/whatever in parallel threads.
@@ -20,8 +24,9 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class ParallelTransformerIterator extends BasicTransformerIterator {
 
-    protected Queue<Sequence<VocabWord>> buffer = new LinkedBlockingQueue<>(1024);
-    protected Queue<String> stringBuffer = new ConcurrentLinkedQueue<>();
+    protected LinkedBlockingQueue<Sequence<VocabWord>> buffer = new LinkedBlockingQueue<>(1024);
+    protected Queue<LabelledDocument> stringBuffer = new ConcurrentLinkedQueue<>();
+    protected TokenizerThread[] threads = new TokenizerThread[5];
 
     public ParallelTransformerIterator(@NonNull LabelAwareIterator iterator, @NonNull SentenceTransformer transformer) {
         this(iterator, transformer, true);
@@ -30,27 +35,46 @@ public class ParallelTransformerIterator extends BasicTransformerIterator {
     public ParallelTransformerIterator(@NonNull LabelAwareIterator iterator, @NonNull SentenceTransformer transformer, boolean allowMultithreading) {
         super(iterator, transformer);
         this.allowMultithreading = allowMultithreading;
+
+        for (int x = 0; x < threads.length; x++) {
+            threads[x] = new TokenizerThread(x, transformer,stringBuffer, buffer);
+            threads[x].start();
+        }
     }
 
     @Override
     public boolean hasNext() {
-        return super.hasNext();
+        return buffer.size() > 0 || stringBuffer.size() > 0 || iterator.hasNextDocument();
     }
 
     @Override
     public Sequence<VocabWord> next() {
-        return super.next();
+        try {
+            int cnt = 0;
+            while (cnt < 100 && stringBuffer.size() < 1000 && iterator.hasNextDocument()) {
+                stringBuffer.add(iterator.nextDocument());
+                cnt++;
+            }
+
+            Sequence<VocabWord> sequence = buffer.take();
+            return sequence;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
 
     private static class TokenizerThread extends Thread implements Runnable {
-        protected Queue<Sequence<VocabWord>> sequencesBuffer;
-        protected Queue<String> stringsBuffer;
+        protected LinkedBlockingQueue<Sequence<VocabWord>> sequencesBuffer;
+        protected Queue<LabelledDocument> stringsBuffer;
+        protected SentenceTransformer sentenceTransformer;
+        protected AtomicBoolean shouldWork = new AtomicBoolean(true);
 
-        public TokenizerThread(int threadIdx, Queue<String> stringsBuffer, Queue<Sequence<VocabWord>> sequencesBuffer) {
+        public TokenizerThread(int threadIdx, SentenceTransformer transformer, Queue<LabelledDocument> stringsBuffer, LinkedBlockingQueue<Sequence<VocabWord>> sequencesBuffer) {
             this.stringsBuffer = stringsBuffer;
             this.sequencesBuffer = sequencesBuffer;
+            this.sentenceTransformer = transformer;
 
             this.setDaemon(true);
             this.setName("Tokenization thread " + threadIdx);
@@ -58,7 +82,24 @@ public class ParallelTransformerIterator extends BasicTransformerIterator {
 
         @Override
         public void run() {
+            while (shouldWork.get()) {
+                LabelledDocument document = stringsBuffer.poll();
 
+                if (document != null) {
+                    Sequence<VocabWord> sequence = sentenceTransformer.transformToSequence(document.getContent());
+
+                    if (sequence != null)
+                        try {
+                            sequencesBuffer.put(sequence);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                }
+            }
+        }
+
+        public void shutdown() {
+            shouldWork.set(false);
         }
     }
 }
