b1a3111d19bcaecb52ba5b285a38acb726aaf95b
raver119 <raver119@gmail.com>
2016-11-09 18:18:18 +0300

    parallel tokenization p.3 more tests
    Former-commit-id: ed6e5a6fe1a7c35e6158f38b439bb141d5568d05

--- deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/transformers/impl/iterables/ParallelTransformerIteratorTest.java
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/transformers/impl/iterables/ParallelTransformerIteratorTest.java
@@ -1,10 +1,13 @@
 package org.deeplearning4j.models.sequencevectors.transformers.impl.iterables;
 
+import lombok.extern.slf4j.Slf4j;
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.transformers.impl.SentenceTransformer;
 import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
+import org.deeplearning4j.text.sentenceiterator.MutipleEpochsSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.PrefetchingSentenceIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
@@ -18,6 +21,7 @@ import static org.junit.Assert.*;
 /**
  * @author raver119@gmail.com
  */
+@Slf4j
 public class ParallelTransformerIteratorTest {
     private TokenizerFactory factory = new DefaultTokenizerFactory();
 
@@ -28,7 +32,9 @@ public class ParallelTransformerIteratorTest {
 
     @Test
     public void hasNext() throws Exception {
-        SentenceIterator iterator = new BasicLineIterator(new ClassPathResource("/big/raw_sentences.txt").getFile());
+        SentenceIterator iterator = new PrefetchingSentenceIterator.Builder(new BasicLineIterator(new ClassPathResource("/big/raw_sentences.txt").getFile()))
+                .setFetchSize(1000)
+                .build();
 
         SentenceTransformer transformer = new SentenceTransformer.Builder()
                 .iterator(iterator)
@@ -46,6 +52,50 @@ public class ParallelTransformerIteratorTest {
         }
 
         assertEquals(97162, cnt);
+    }
+
+    @Test
+    public void testSpeedComparison1() throws Exception {
+        SentenceIterator iterator = new MutipleEpochsSentenceIterator(new BasicLineIterator(new ClassPathResource("/big/raw_sentences.txt").getFile()), 25);
+
+        SentenceTransformer transformer = new SentenceTransformer.Builder()
+                .iterator(iterator)
+                .allowMultithreading(false)
+                .tokenizerFactory(factory)
+                .build();
+
+        Iterator<Sequence<VocabWord>> iter = transformer.iterator();
+        int cnt = 0;
+        long time1 = System.currentTimeMillis();
+        while (iter.hasNext()) {
+            Sequence<VocabWord> sequence = iter.next();
+            assertNotEquals("Failed on [" + cnt + "] iteration", null, sequence);
+            assertNotEquals("Failed on [" + cnt + "] iteration", 0, sequence.size());
+            cnt++;
+        }
+        long time2 = System.currentTimeMillis();
+
+        log.info("Single-threaded time: {} ms", time2 - time1);
+        iterator.reset();
+
+        transformer = new SentenceTransformer.Builder()
+                .iterator(iterator)
+                .allowMultithreading(true)
+                .tokenizerFactory(factory)
+                .build();
+
+        iter = transformer.iterator();
+
+        time1 = System.currentTimeMillis();
+        while (iter.hasNext()) {
+            Sequence<VocabWord> sequence = iter.next();
+            assertNotEquals("Failed on [" + cnt + "] iteration", null, sequence);
+            assertNotEquals("Failed on [" + cnt + "] iteration", 0, sequence.size());
+            cnt++;
+        }
+        time2 = System.currentTimeMillis();
+
+        log.info("Multi-threaded time: {} ms", time2 - time1);
     }
 
 }
