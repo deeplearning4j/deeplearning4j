d7801b229231e0aeaa2137ebc9a75f041c68962b
raver119 <raver119@gmail.com>
2016-11-12 13:18:56 +0300

    disable power8 gemm workaround

--- blas/cpu/NativeBlas.cpp
+++ blas/cpu/NativeBlas.cpp
@@ -907,7 +907,7 @@ void Nd4jBlas::hgemm(Nd4jPointer *extraParams,int Order, int TransA, int TransB,
                      float16 *B, int ldb,
                      float beta,
                      float16 *C, int ldc) {
-    // SIMD should be disabled for T = float16
+    // FIXME: SIMD should be fixed for T = float16
     nd4j::blas::GEMM<float16>::op(convertOrder(Order),convertTranspose(TransA),convertTranspose(TransB),M,N,K,(float16) alpha,A,lda,B,ldb,(float16) beta,C,ldc);
 }
 
@@ -930,7 +930,6 @@ void Nd4jBlas::dgemm(Nd4jPointer *extraParams,int Order, int TransA, int TransB,
                      double *B, int ldb,
                      double beta,
                      double *C, int ldc){
-    // TODO: remove this line after OpenBLAS gets fixed
     cblas_dgemm(convertOrder(Order),convertTranspose(TransA),convertTranspose(TransB),M,N,K,alpha,A,lda,B,ldb,beta,C,ldc);
 }
 
