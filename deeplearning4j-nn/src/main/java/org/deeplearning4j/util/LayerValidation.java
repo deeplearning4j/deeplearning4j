c344764cf68b55e58995473bf674664b93875b35
Alex Black <blacka101@gmail.com>
2016-11-12 21:47:10 +1100

    Additional layer config validation and improved exceptions for config/invalid input + tests
    Former-commit-id: 1100bf5c0421fa0401948a9016e92fb1439732f5

---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/util/LayerValidation.java
@@ -0,0 +1,26 @@
+package org.deeplearning4j.util;
+
+import org.deeplearning4j.exception.DL4JInvalidConfigException;
+
+/**
+ * Created by Alex on 12/11/2016.
+ */
+public class LayerValidation {
+
+    /**
+     * Asserts that the layer nIn and nOut values are set for the layer
+     *
+     * @param layerType     Type of layer ("DenseLayer", etc)
+     * @param layerName     Name of the layer (may be null if not set)
+     * @param layerIndex    Index of the layer
+     * @param nIn           nIn value
+     * @param nOut          nOut value
+     */
+    public static void assertNInNOutSet(String layerType, String layerName, int layerIndex, int nIn, int nOut){
+        if (nIn <= 0 || nOut <= 0) {
+            if(layerName == null) layerName = "(name not set)";
+            throw new DL4JInvalidConfigException(layerType + " (index=" + layerIndex + ", name=" + layerName +
+                    ") nIn=" + nIn + ", nOut=" + nOut + "; nIn and nOut must be > 0");
+        }
+    }
+}
