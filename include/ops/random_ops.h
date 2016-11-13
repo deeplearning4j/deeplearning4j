fd7f5310c30c61668696bcbb4edf7cfac7ee5d85
raver119 <raver119@gmail.com>
2016-11-13 13:24:29 +0300

    debug off

--- include/ops/random_ops.h
+++ include/ops/random_ops.h
@@ -120,7 +120,6 @@ namespace randomOps {
         random_def T op(T valueX, int idx, int length, nd4j::random::RandomHelper<T> *helper, T *extraParams) {
             T prob = extraParams[0];
             T randVal = helper->relativeT(idx);
-            printf("Index: [%i]; prob: [%f]; randVal: [%f]\n", idx, prob, randVal);
             return randVal >= prob ? (T) 0.0f : valueX / prob;
         }
     };
