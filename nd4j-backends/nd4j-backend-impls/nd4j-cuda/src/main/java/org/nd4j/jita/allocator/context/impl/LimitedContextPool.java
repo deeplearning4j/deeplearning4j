7136526dffba4564ef3c2c1745cf1c809c279e12
raver119 <raver119@gmail.com>
2016-11-18 11:04:12 +0300

    context pool fixes

--- nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/main/java/org/nd4j/jita/allocator/context/impl/LimitedContextPool.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/main/java/org/nd4j/jita/allocator/context/impl/LimitedContextPool.java
@@ -2,16 +2,12 @@ package org.nd4j.jita.allocator.context.impl;
 
 import lombok.NonNull;
 import org.apache.commons.lang3.RandomUtils;
-import org.bytedeco.javacpp.Pointer;
 import org.nd4j.jita.allocator.context.ContextPack;
-import org.nd4j.jita.allocator.garbage.GarbageBufferReference;
 import org.nd4j.jita.allocator.garbage.GarbageResourceReference;
 import org.nd4j.jita.allocator.impl.AtomicAllocator;
 import org.nd4j.jita.allocator.pointers.CudaPointer;
 import org.nd4j.jita.allocator.pointers.cuda.cublasHandle_t;
 import org.nd4j.jita.conf.CudaEnvironment;
-import org.nd4j.linalg.api.buffer.BaseDataBuffer;
-import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.jcublas.context.CudaContext;
 import org.nd4j.nativeblas.NativeOps;
 import org.nd4j.nativeblas.NativeOpsHolder;
@@ -50,11 +46,27 @@ public class LimitedContextPool extends BasicContextPool {
             queueMap.put(i, queue);
         }
 
-        addResourcesToPool(perDevicePool, false);
+        fillPoolWithResources(perDevicePool, false);
         currentPoolSize.set(perDevicePool);
     }
 
-    protected synchronized void addResourcesToPool(int numResources, boolean restoreDevice) {
+    protected void addResourcesToPool(int numResources) {
+        int device = AtomicAllocator.getInstance().getDeviceId();
+
+        cublasHandle_t handle = createNewCublasHandle();
+        for (int cnt = 0; cnt < numResources; cnt++ ) {
+            CudaContext context = createNewStream(device);
+            context.initOldStream();
+            getDeviceBuffers(context, device);
+            context.setHandle(handle);
+
+            context.syncOldStream();
+
+            pool.get(device).add(context);
+        }
+    }
+
+    protected synchronized void fillPoolWithResources(int numResources, boolean restoreDevice) {
         List<Integer> devices = CudaEnvironment.getInstance().getConfiguration().getAvailableDevices();
 
         int cDevice = 0;
@@ -97,7 +109,6 @@ public class LimitedContextPool extends BasicContextPool {
         nativeOps.setDevice(new CudaPointer(deviceId));
 
         context = pool.get(deviceId).poll();
-        context.setDeviceId(deviceId);
         if (context != null) {
             int col = RandomUtils.nextInt(0, collectors.size());
             collectors.get(col);
@@ -108,29 +119,46 @@ public class LimitedContextPool extends BasicContextPool {
             //point.attachReference(reference);
 
             acquired.put(threadIdx, context);
+            context.setDeviceId(deviceId);
             return context;
         } else {
+
             do {
                 try {
-                    context = pool.get(deviceId).poll(5, TimeUnit.SECONDS);
+                    System.gc();
+                    context = pool.get(deviceId).poll(1, TimeUnit.SECONDS);
                     if (context != null) {
+                        int col = RandomUtils.nextInt(0, collectors.size());
+                        collectors.get(col);
+
+                        GarbageResourceReference reference = new GarbageResourceReference(Thread.currentThread(), queueMap.get(col), context, deviceId.intValue());
+                        context.attachReference(reference);
+
                         acquired.put(threadIdx, context);
-                        return context;
+                        context.setDeviceId(deviceId);
                     } else {
                         if (currentPoolSize.get() < CudaEnvironment.getInstance().getConfiguration().getPoolSize() * 3) {
-                            addResourcesToPool(16, true);
+                            addResourcesToPool(16);
 
                             // there's possible race condition, but we don't really care
                             currentPoolSize.addAndGet(16);
+                        } else {
+                            logger.warn("Can't allocate new context, sleeping...");
+                            System.gc();
+                            try {
+                                Thread.sleep(500);
+                            } catch (Exception e) {
+                                //
+                            }
                         }
                     }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             } while (context == null);
-        }
 
-        throw new RuntimeException("EPTS");
+            return context;
+        }
     }
 
     @Override
