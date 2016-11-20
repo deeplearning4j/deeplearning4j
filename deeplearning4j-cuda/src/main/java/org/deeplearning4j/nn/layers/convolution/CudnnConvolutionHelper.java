f166c6d68a35d3a7bd4ed687cdf69c48aaf24691
raver119 <raver119@gmail.com>
2016-11-20 15:28:21 +0300

    sync fix for cudnn
    Former-commit-id: 6384a2bb742a191f98437609bf3d9c4bafc4ab32

--- deeplearning4j-cuda/src/main/java/org/deeplearning4j/nn/layers/convolution/CudnnConvolutionHelper.java
+++ deeplearning4j-cuda/src/main/java/org/deeplearning4j/nn/layers/convolution/CudnnConvolutionHelper.java
@@ -209,7 +209,7 @@ public class CudnnConvolutionHelper implements ConvolutionHelper {
         int[] dstStride = epsNext.stride();
 
         Allocator allocator = AtomicAllocator.getInstance();
-        CudaContext context = allocator.getFlowController().prepareAction(input, weights, weightGradView, biasGradView, delta, epsNext);
+        CudaContext context = allocator.getFlowController().prepareActionAllWrite(input, weights, weightGradView, biasGradView, delta, epsNext);
         Pointer srcData = allocator.getPointer(input, context);
         Pointer filterData = allocator.getPointer(weights, context);
         Pointer filterGradData = allocator.getPointer(weightGradView, context);
@@ -238,7 +238,7 @@ public class CudnnConvolutionHelper implements ConvolutionHelper {
         checkCudnn(cudnnConvolutionBackwardData(cudnnContext, alpha, cudnnContext.filterDesc, filterData, cudnnContext.deltaTensorDesc, deltaData, cudnnContext.convDesc,
                 algo2[0], workSpace, workSpace.capacity(), beta, cudnnContext.dstTensorDesc, dstData));
 
-        allocator.registerAction(context, input, weights, weightGradView, biasGradView, delta, epsNext);
+        allocator.getFlowController().registerActionAllWrite(context, input, weights, weightGradView, biasGradView, delta, epsNext);
 
         Gradient retGradient = new DefaultGradient();
         retGradient.setGradientFor(ConvolutionParamInitializer.BIAS_KEY, biasGradView);
@@ -280,7 +280,7 @@ public class CudnnConvolutionHelper implements ConvolutionHelper {
                         CUDNN_CONVOLUTION_FWD_NO_WORKSPACE : CUDNN_CONVOLUTION_FWD_PREFER_FASTEST, 0, algo));
 
         Allocator allocator = AtomicAllocator.getInstance();
-        CudaContext context = allocator.getFlowController().prepareAction(input, weights, bias, z);
+        CudaContext context = allocator.getFlowController().prepareAction(z, input, weights, bias);
         Pointer srcData = allocator.getPointer(input, context);
         Pointer filterData = allocator.getPointer(weights, context);
         Pointer biasData = allocator.getPointer(bias, context);
@@ -300,7 +300,7 @@ public class CudnnConvolutionHelper implements ConvolutionHelper {
         checkCudnn(cudnnSetTensor4dDescriptor(cudnnContext.biasTensorDesc, tensorFormat, dataType, 1, c[0], 1, 1));
         checkCudnn(cudnnAddTensor(cudnnContext, alpha, cudnnContext.biasTensorDesc, biasData, alpha, cudnnContext.dstTensorDesc, dstData));
 
-        allocator.registerAction(context, input, weights, bias, z);
+        allocator.registerAction(context, z, input, weights, bias);
 
         return z;
     }
