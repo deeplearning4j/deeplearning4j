de2de70d09353c1eb3afe797ba3edd335b7428ca
raver119 <raver119@gmail.com>
2016-11-20 15:28:21 +0300

    sync fix for cudnn
    Former-commit-id: f166c6d68a35d3a7bd4ed687cdf69c48aaf24691 [formerly 6384a2bb742a191f98437609bf3d9c4bafc4ab32]
    Former-commit-id: 2026e029c642446f63a8b15651fe14c73658715f

--- deeplearning4j-cuda/src/main/java/org/deeplearning4j/nn/layers/normalization/CudnnLocalResponseNormalizationHelper.java
+++ deeplearning4j-cuda/src/main/java/org/deeplearning4j/nn/layers/normalization/CudnnLocalResponseNormalizationHelper.java
@@ -144,7 +144,7 @@ public class CudnnLocalResponseNormalizationHelper implements LocalResponseNorma
                 dstStride[0], dstStride[1], dstStride[2], dstStride[3]));
 
         Allocator allocator = AtomicAllocator.getInstance();
-        CudaContext context = allocator.getFlowController().prepareAction(input, epsilon, activations, nextEpsilon);
+        CudaContext context = allocator.getFlowController().prepareActionAllWrite(input, epsilon, activations, nextEpsilon);
         Pointer srcData = allocator.getPointer(input, context);
         Pointer epsData = allocator.getPointer(epsilon, context);
         Pointer zData = allocator.getPointer(activations, context);
@@ -155,7 +155,7 @@ public class CudnnLocalResponseNormalizationHelper implements LocalResponseNorma
                 this.alpha, cudnnContext.deltaTensorDesc, zData, cudnnContext.deltaTensorDesc, epsData, cudnnContext.srcTensorDesc, srcData,
                 this.beta, cudnnContext.dstTensorDesc, dstData));
 
-        allocator.registerAction(context, input, epsilon, activations, nextEpsilon);
+        allocator.getFlowController().registerActionAllWrite(context, input, epsilon, activations, nextEpsilon);
 
         return new Pair<>(retGradient,nextEpsilon);
     }
@@ -179,7 +179,7 @@ public class CudnnLocalResponseNormalizationHelper implements LocalResponseNorma
         checkCudnn(cudnnSetLRNDescriptor(cudnnContext.lrnDesc, (int)n, alpha, beta, k));
 
         Allocator allocator = AtomicAllocator.getInstance();
-        CudaContext context = allocator.getFlowController().prepareAction(input, activations);
+        CudaContext context = allocator.getFlowController().prepareActionAllWrite(input, activations);
         Pointer srcData = allocator.getPointer(input, context);
         Pointer dstData = allocator.getPointer(activations, context);
 
@@ -190,7 +190,7 @@ public class CudnnLocalResponseNormalizationHelper implements LocalResponseNorma
         checkCudnn(cudnnLRNCrossChannelForward(cudnnContext, cudnnContext.lrnDesc, CUDNN_LRN_CROSS_CHANNEL_DIM1,
                 this.alpha, cudnnContext.srcTensorDesc, srcData, this.beta, cudnnContext.dstTensorDesc, dstData));
 
-        allocator.registerAction(context, input, activations);
+        allocator.getFlowController().registerActionAllWrite(context, input, activations);
 
         return activations;
     }
