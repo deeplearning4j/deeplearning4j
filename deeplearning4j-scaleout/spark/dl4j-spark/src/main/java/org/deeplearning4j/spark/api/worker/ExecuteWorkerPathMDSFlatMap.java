c28f4606149d3b708ba178c996fe2507033b57ab
Alex Black <blacka101@gmail.com>
2016-11-20 15:53:27 +1100

    Fix Spark ClosedByInterruptException issue
    Former-commit-id: 86fa62d6a6c4e9da53861465f53a03cb18835e14 [formerly 6bf758cf1861449d0c572c8ddb1d2d4b0f627a95]
    Former-commit-id: 635940406320d9f2f2d398bff6c65a3b47530d84

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/worker/ExecuteWorkerPathMDSFlatMap.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/worker/ExecuteWorkerPathMDSFlatMap.java
@@ -3,10 +3,13 @@ package org.deeplearning4j.spark.api.worker;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.deeplearning4j.spark.api.TrainingResult;
 import org.deeplearning4j.spark.api.TrainingWorker;
+import org.deeplearning4j.spark.api.WorkerConfiguration;
 import org.deeplearning4j.spark.iterator.PathSparkMultiDataSetIterator;
 import org.nd4j.linalg.dataset.api.MultiDataSet;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * A FlatMapFunction for executing training on serialized DataSet objects, that can be loaded from a path (local or HDFS)
@@ -17,13 +20,22 @@ import java.util.Iterator;
  */
 public class ExecuteWorkerPathMDSFlatMap<R extends TrainingResult> implements FlatMapFunction<Iterator<String>, R> {
     private final FlatMapFunction<Iterator<MultiDataSet>, R> workerFlatMap;
+    private final int maxMinibatches;
 
     public ExecuteWorkerPathMDSFlatMap(TrainingWorker<R> worker){
         this.workerFlatMap = new ExecuteWorkerMultiDataSetFlatMap<>(worker);
+        WorkerConfiguration conf = worker.getDataConfiguration();
+        this.maxMinibatches = (conf.getMaxBatchesPerWorker() > 0 ? conf.getMaxBatchesPerWorker() : Integer.MAX_VALUE);
     }
 
     @Override
     public Iterable<R> call(Iterator<String> iter) throws Exception {
-        return workerFlatMap.call(new PathSparkMultiDataSetIterator(iter));
+        List<String> list = new ArrayList<>(maxMinibatches);
+        int count = 0;
+        while(iter.hasNext() && count++ < maxMinibatches){
+            list.add(iter.next());
+        }
+
+        return workerFlatMap.call(new PathSparkMultiDataSetIterator(list.iterator()));
     }
 }
