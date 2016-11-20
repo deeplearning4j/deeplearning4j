2fc8e08cbd735c4d1bced15775550f25719e7cb6
Alex Black <blacka101@gmail.com>
2016-11-20 16:54:59 +1100

    Make spark paths fitting more robust
    Former-commit-id: a835082837ecac479b1b67725ae44d2607ef13e6

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/worker/ExecuteWorkerPathFlatMap.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/api/worker/ExecuteWorkerPathFlatMap.java
@@ -20,19 +20,33 @@ import java.util.List;
  */
 public class ExecuteWorkerPathFlatMap<R extends TrainingResult> implements FlatMapFunction<Iterator<String>, R> {
     private final FlatMapFunction<Iterator<DataSet>, R> workerFlatMap;
-    private final int maxMinibatches;
+    private final int maxDataSetObjects;
 
     public ExecuteWorkerPathFlatMap(TrainingWorker<R> worker){
         this.workerFlatMap = new ExecuteWorkerFlatMap<>(worker);
+
+        //How many dataset objects of size 'dataSetObjectNumExamples' should we load?
+        //Only pass on the required number, not all of them (to avoid async preloading data that won't be used)
+        //Most of the time we'll get exactly the number we want, but this isn't guaranteed all the time for all
+        // splitting strategies
         WorkerConfiguration conf = worker.getDataConfiguration();
-        this.maxMinibatches = (conf.getMaxBatchesPerWorker() > 0 ? conf.getMaxBatchesPerWorker() : Integer.MAX_VALUE);
+        int dataSetObjectNumExamples = conf.getDataSetObjectSizeExamples();
+        int workerMinibatchSize = conf.getBatchSizePerWorker();
+        int maxMinibatches = (conf.getMaxBatchesPerWorker() > 0 ? conf.getMaxBatchesPerWorker() : Integer.MAX_VALUE);
+
+        if(maxMinibatches == Integer.MAX_VALUE){
+            maxDataSetObjects = Integer.MAX_VALUE;
+        } else {
+            //Required: total number of examples / examples per dataset object
+            maxDataSetObjects = (int)Math.ceil(maxMinibatches * workerMinibatchSize / ((double)dataSetObjectNumExamples));
+        }
     }
 
     @Override
     public Iterable<R> call(Iterator<String> iter) throws Exception {
-        List<String> list = new ArrayList<>(maxMinibatches);
+        List<String> list = new ArrayList<>();
         int count = 0;
-        while(iter.hasNext() && count++ < maxMinibatches){
+        while(iter.hasNext() && count++ < maxDataSetObjects){
             list.add(iter.next());
         }
 
