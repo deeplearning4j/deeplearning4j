ffea3dfd0d860c05ffebe3e219cd1d8422ca083b
Justin Long <justinlong@outlook.com>
2016-11-22 18:56:29 -0800

    Add a summary (best fit) line for model scores.
    Former-commit-id: 1cb8b3a43998d8991b6dab8be366a02eb631066d

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/overview.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/js/train/overview.js
@@ -81,8 +81,20 @@ function renderScoreVsIterChart(data) {
             scoreData.push([scoresIter[i], scoresArr[i]]);
         }
 
+        var plotData = [{data: scoreData, label: "score"}];
+
+        // calculate a best fit line to summarize training progress
+        if(scoresIter.length > 4) {
+            var bestFitLine = findLineByLeastSquares(scoresIter, scoresArr);
+            var bestFitData = [];
+            for (var i = 0; i < bestFitLine[0].length; i++) {
+                bestFitData.push([bestFitLine[0][i], bestFitLine[1][i]]);
+            }
+            plotData.push({data: bestFitData, label: "summary"});
+        }
+
         var plot = $.plot($("#scoreiterchart"),
-            [{data: scoreData, label: "score"}], {
+            plotData, {
                 series: {
                     lines: {
                         show: true,
@@ -372,4 +384,67 @@ function renderStdevChart(data) {
             }
         });
     }
+}
+
+
+/* --------------- linear least squares (best fit line) ---------- */
+function findLineByLeastSquares(values_x, values_y) {
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_xx = 0;
+    var count = 0;
+
+    /*
+     * We'll use those variables for faster read/write access.
+     */
+    var x = 0;
+    var y = 0;
+    var values_length = values_x.length;
+
+    if (values_length != values_y.length) {
+        throw new Error('The parameters values_x and values_y need to have same size!');
+    }
+
+    /*
+     * Nothing to do.
+     */
+    if (values_length === 0) {
+        return [ [], [] ];
+    }
+
+    /*
+     * Calculate the sum for each of the parts necessary.
+     */
+    for (var v = 0; v < values_length; v++) {
+        x = values_x[v];
+        y = values_y[v];
+        sum_x += x;
+        sum_y += y;
+        sum_xx += x*x;
+        sum_xy += x*y;
+        count++;
+    }
+
+    /*
+     * Calculate m and b for the formular:
+     * y = x * m + b
+     */
+    var m = (count*sum_xy - sum_x*sum_y) / (count*sum_xx - sum_x*sum_x);
+    var b = (sum_y/count) - (m*sum_x)/count;
+
+    /*
+     * We will make the x and y result line now
+     */
+    var result_values_x = [];
+    var result_values_y = [];
+
+    for (var v = 0; v < values_length; v++) {
+        x = values_x[v];
+        y = x * m + b;
+        result_values_x.push(x);
+        result_values_y.push(y);
+    }
+
+    return [result_values_x, result_values_y];
 }
