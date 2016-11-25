bab9d0baf29a734a6b5a1c94111fa0259601f929
Alex Black <blacka101@gmail.com>
2016-11-25 22:47:43 +1100

    Implement Bernoulli reconstruction log prob + test

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/variational/TestReconstructionDistributions.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/variational/TestReconstructionDistributions.java
@@ -1,13 +1,18 @@
 package org.deeplearning4j.nn.layers.variational;
 
 
+import org.apache.commons.math3.distribution.BinomialDistribution;
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.layers.variational.BernoulliReconstructionDistribution;
 import org.deeplearning4j.nn.conf.layers.variational.GaussianReconstructionDistribution;
 import org.deeplearning4j.nn.conf.layers.variational.ReconstructionDistribution;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.ops.transforms.Transforms;
+
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,7 +26,7 @@ public class TestReconstructionDistributions {
         Nd4j.getRandom().setSeed(12345);
 
         int inputSize = 4;
-        int[] mbs = new int[]{1,2};
+        int[] mbs = new int[]{1,2,5};
 
         for(boolean average : new boolean[]{true,false}) {
             for (int minibatch : mbs) {
@@ -68,4 +73,59 @@ public class TestReconstructionDistributions {
         }
     }
 
+    @Test
+    public void testBernoulliLogProb(){
+        Nd4j.getRandom().setSeed(12345);
+
+        int inputSize = 4;
+        int[] mbs = new int[]{1,2,5};
+
+        Random r = new Random(12345);
+
+        for(boolean average : new boolean[]{true,false}) {
+            for (int minibatch : mbs) {
+
+                INDArray x = Nd4j.zeros(minibatch, inputSize);
+                for( int i=0; i<minibatch; i++ ){
+                    for( int j=0; j<inputSize; j++ ){
+                        x.putScalar(i,j,r.nextInt(2));
+                    }
+                }
+
+                INDArray distributionParams = Nd4j.rand(minibatch, inputSize).muli(2).subi(1);  //i.e., pre-sigmoid prob
+                INDArray prob = Transforms.sigmoid(distributionParams, true);
+
+                ReconstructionDistribution dist = new BernoulliReconstructionDistribution("sigmoid");
+
+                double logProb = dist.logProbability(x, distributionParams, average);
+
+                //Calculate the same thing, but using Apache Commons math
+
+                double logProbSum = 0.0;
+                for (int i = 0; i < minibatch; i++) {
+                    for (int j = 0; j < inputSize; j++) {
+                        double p = prob.getDouble(i,j);
+
+                        BinomialDistribution binomial = new BinomialDistribution(1,p);      //Bernoulli is a special case of binomial
+
+                        double xVal = x.getDouble(i, j);
+                        double thisLogProb = binomial.logProbability((int)xVal);
+                        logProbSum += thisLogProb;
+                    }
+                }
+
+                double expLogProb;
+                if(average){
+                    expLogProb = logProbSum / minibatch;
+                } else {
+                    expLogProb = logProbSum;
+                }
+
+//                System.out.println(x);
+
+                System.out.println(expLogProb + "\t" + logProb + "\t" + (logProb / expLogProb));
+                assertEquals(expLogProb, logProb, 1e-6);
+            }
+        }
+    }
 }
---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/variational/BernoulliReconstructionDistribution.java
@@ -0,0 +1,52 @@
+package org.deeplearning4j.nn.conf.layers.variational;
+
+import lombok.extern.slf4j.Slf4j;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.ops.transforms.Transforms;
+
+/**
+ * Created by Alex on 25/11/2016.
+ */
+@Slf4j
+public class BernoulliReconstructionDistribution implements ReconstructionDistribution {
+
+    private String activationFn;
+
+    public BernoulliReconstructionDistribution(String activationFn){
+        this.activationFn = activationFn;
+        if(!"sigmoid".equals(activationFn)){
+            log.warn("Using BernoulliRecontructionDistribution with activation function \"" + activationFn + "\"."
+                    + " Using sigmoid is recommended to bound probabilities in range 0 to 1");
+        }
+    }
+
+    @Override
+    public int distributionParamCount(int dataSize) {
+        return dataSize;
+    }
+
+    @Override
+    public double logProbability(INDArray x, INDArray distributionParams, boolean average) {
+        INDArray output = distributionParams.dup();
+        if(!"identity".equals(activationFn)){
+            output = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(activationFn, output));
+        }
+
+        INDArray logOutput = Transforms.log(output, true);
+        INDArray log1SubOut = Transforms.log(output.rsubi(1.0), false);
+
+        INDArray logProb = logOutput.muli(x).addi(x.rsub(1.0).muli(log1SubOut));
+
+        if(average){
+            return logProb.sumNumber().doubleValue() / x.size(0);
+        } else {
+            return logProb.sumNumber().doubleValue();
+        }
+    }
+
+    @Override
+    public INDArray gradient(INDArray x, INDArray preOut) {
+        return null;
+    }
+}
