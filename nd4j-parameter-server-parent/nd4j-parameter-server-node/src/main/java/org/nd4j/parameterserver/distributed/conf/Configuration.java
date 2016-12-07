4ec36b3461735c4cbf531058166d3b9d11754542
raver119 <raver119@gmail.com>
2016-12-08 00:54:06 +0300

    void p.0.1 minor config pojo tweak

--- nd4j-parameter-server-parent/nd4j-parameter-server-node/src/main/java/org/nd4j/parameterserver/distributed/conf/Configuration.java
+++ nd4j-parameter-server-parent/nd4j-parameter-server-node/src/main/java/org/nd4j/parameterserver/distributed/conf/Configuration.java
@@ -1,20 +1,27 @@
 package org.nd4j.parameterserver.distributed.conf;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nd4j.parameterserver.distributed.enums.FaultToleranceStrategy;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Basic configuration pojo for VoidParameterServer
  * @author raver119@gmail.com
  */
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Slf4j
 public class Configuration implements Serializable {
-    private int port = 40123;
+    private int port;
     private int numberOfShards;
     private FaultToleranceStrategy faultToleranceStrategy;
+    private List<String> shardAddresses;
+    private List<String> backupAddresses;
 }
