408de55f38cc5032e24b54013e3cbe56c13fbfed
raver119 <raver119@gmail.com>
2016-12-08 19:57:07 +0300

    implementations p.4.1 -shallow vocab merge

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/embeddings/sequencevectors/functions/TrainingFunction.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp/src/main/java/org/deeplearning4j/spark/models/embeddings/sequencevectors/functions/TrainingFunction.java
@@ -3,11 +3,15 @@ package org.deeplearning4j.spark.models.embeddings.sequencevectors.functions;
 import lombok.NonNull;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.broadcast.Broadcast;
+import org.deeplearning4j.models.embeddings.learning.ElementsLearningAlgorithm;
+import org.deeplearning4j.models.embeddings.learning.SequenceLearningAlgorithm;
 import org.deeplearning4j.models.embeddings.loader.VectorsConfiguration;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
 import org.deeplearning4j.models.sequencevectors.sequence.ShallowSequenceElement;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+import org.nd4j.parameterserver.distributed.VoidParameterServer;
+import org.nd4j.parameterserver.distributed.conf.Configuration;
 
 /**
  * This is wrapper for SequenceVectors training over given Sequence<T>
@@ -17,16 +21,86 @@ import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
 public class TrainingFunction<T extends SequenceElement> implements VoidFunction<Sequence<T>> {
     protected Broadcast<VocabCache<ShallowSequenceElement>> vocabCacheBroadcast;
     protected Broadcast<VectorsConfiguration> configurationBroadcast;
+    protected Broadcast<Configuration> paramServerConfigurationBroadcast;
 
-    public TrainingFunction(@NonNull Broadcast<VocabCache<ShallowSequenceElement>> vocabCacheBroadcast, @NonNull Broadcast<VectorsConfiguration> vectorsConfigurationBroadcast) {
+    protected transient VoidParameterServer paramServer;
+    protected transient VectorsConfiguration vectorsConfiguration;
+
+    protected transient ElementsLearningAlgorithm<ShallowSequenceElement> elementsLearningAlgorithm;
+    protected transient SequenceLearningAlgorithm<ShallowSequenceElement> sequenceLearningAlgorithm;
+    protected transient VocabCache<ShallowSequenceElement> shallowVocabCache;
+
+    public TrainingFunction(@NonNull Broadcast<VocabCache<ShallowSequenceElement>> vocabCacheBroadcast, @NonNull Broadcast<VectorsConfiguration> vectorsConfigurationBroadcast, @NonNull Broadcast<Configuration> paramServerConfigurationBroadcast) {
         this.vocabCacheBroadcast = vocabCacheBroadcast;
         this.configurationBroadcast = vectorsConfigurationBroadcast;
+        this.paramServerConfigurationBroadcast = paramServerConfigurationBroadcast;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void call(Sequence<T> sequence) throws Exception {
         /**
          * Depending on actual training mode, we'll either go for SkipGram/CBOW/PV-DM/PV-DBOW or whatever
+         */
+
+        if (paramServer == null) {
+            paramServer = VoidParameterServer.getInstance();
+            paramServer.init(paramServerConfigurationBroadcast.getValue());
+        }
+
+        if (vectorsConfiguration == null)
+            vectorsConfiguration = configurationBroadcast.getValue();
+
+        if (shallowVocabCache == null)
+            shallowVocabCache = vocabCacheBroadcast.getValue();
+
+
+        if (elementsLearningAlgorithm == null && vectorsConfiguration.getElementsLearningAlgorithm() != null) {
+            // TODO: do ELA initialization
+            try {
+                elementsLearningAlgorithm = (ElementsLearningAlgorithm<ShallowSequenceElement>) Class.forName(vectorsConfiguration.getElementsLearningAlgorithm()).newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (sequenceLearningAlgorithm == null && vectorsConfiguration.getSequenceLearningAlgorithm() != null) {
+            // TODO: do SLA initialization
+            try {
+                sequenceLearningAlgorithm = (SequenceLearningAlgorithm<ShallowSequenceElement>) Class.forName(vectorsConfiguration.getSequenceLearningAlgorithm()).newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        /*
+         at this moment we should have everything ready for actual initialization
+         the only limitation we have - our sequence is detached from actual vocabulary, so we need to merge it back virtually
+        */
+        Sequence<ShallowSequenceElement> mergedSequence = new Sequence<>();
+        for (T element: sequence.getElements()) {
+            ShallowSequenceElement reduced = shallowVocabCache.tokenFor(element.getStorageId());
+
+            if (reduced != null)
+                mergedSequence.addElement(reduced);
+        }
+
+        // do the same with labels, transfer them, if any
+        if (sequenceLearningAlgorithm != null) {
+            for (T label: sequence.getSequenceLabels()) {
+                ShallowSequenceElement reduced = shallowVocabCache.tokenFor(label.getStorageId());
+
+                if (reduced != null)
+                    mergedSequence.addSequenceLabel(reduced);
+            }
+        }
+
+
+        // now we have shallow sequence, which we'll use for training
+        /**
+         * All we want here, is uniform way to do training, that's matching both standalone and spark codebase.
+         * So we need some neat method, that takes sequence as input, and returns **something** that's either used for aggregation, or for ParamServer message
          */
     }
 }
