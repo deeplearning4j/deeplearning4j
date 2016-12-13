996329ff3737b1a2496fd7d169baefd2d4a2ad45
raver119 <raver119@gmail.com>
2016-12-13 16:39:05 +0300

    implementations p.6 - learningAlgorithms propagation to spark - time to use new toys
    Former-commit-id: c01b0eb6ad1b0455de7ef53b4da86d170ca5b9d7

---
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/learning/sequence/SparkDBOW.java
@@ -0,0 +1,22 @@
+package org.deeplearning4j.spark.models.sequencevectors.learning.sequence;
+
+import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
+import org.deeplearning4j.models.sequencevectors.sequence.ShallowSequenceElement;
+import org.deeplearning4j.spark.models.sequencevectors.learning.elements.BaseSparkLearningAlgorithm;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author raver119@gmail.com
+ */
+public class SparkDBOW extends BaseSparkLearningAlgorithm {
+    @Override
+    public String getCodeName() {
+        return "Spark-DBOW";
+    }
+
+    @Override
+    public double learnSequence(Sequence<ShallowSequenceElement> sequence, AtomicLong nextRandom, double learningRate) {
+        return 0;
+    }
+}
