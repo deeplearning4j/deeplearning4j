7175eb815061e889d916aa8223438bfee78c40bf
Adam Gibson <adam@skymind.io>
2016-12-15 00:35:58 -0800

    add new dependency on new parallelwrapper spin out

--- deeplearning4j-scaleout/deeplearning4j-scaleout-parallelwrapper/src/test/java/org/deeplearning4j/parallelism/ParallelWrapperTest.java
+++ deeplearning4j-scaleout/deeplearning4j-scaleout-parallelwrapper/src/test/java/org/deeplearning4j/parallelism/ParallelWrapperTest.java
@@ -106,7 +106,7 @@ public class ParallelWrapperTest {
                 .reportScoreAfterAveraging(true)
 
                 // optinal parameter, set to false ONLY if your system has support P2P memory access across PCIe (hint: AWS do not support P2P)
-                .useLegacyAveraging(true).useParameterServer(true)
+                .useLegacyAveraging(true)
 
                 .build();
 
