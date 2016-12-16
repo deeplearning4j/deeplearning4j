5fec1e74edff151340ba07a89bd5e2fa0c67f4d1
Alex Black <blacka101@gmail.com>
2016-12-17 00:20:41 +1100

    Make normalizer functionality serializable

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/dataset/api/preprocessor/NormalizerStrategy.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/dataset/api/preprocessor/NormalizerStrategy.java
@@ -3,12 +3,14 @@ package org.nd4j.linalg.dataset.api.preprocessor;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.stats.NormalizerStats;
 
+import java.io.Serializable;
+
 /**
  * Interface for strategies that can normalize and denormalize data arrays based on statistics of the population
  *
  * @author Ede Meijer
  */
-interface NormalizerStrategy<S extends NormalizerStats> {
+interface NormalizerStrategy<S extends NormalizerStats> extends Serializable {
     /**
      * Normalize a data array
      *
