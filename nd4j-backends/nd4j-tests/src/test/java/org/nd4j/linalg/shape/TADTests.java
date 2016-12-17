ccb890f7c4b9f1f5cd770d63421d2a1a68914117
raver119 <raver119@gmail.com>
2016-12-17 19:27:37 +0300

    4D -> 3D comparison

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/shape/TADTests.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/shape/TADTests.java
@@ -13,6 +13,7 @@ import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.factory.Nd4jBackend;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author raver119@gmail.com
@@ -35,8 +36,10 @@ public class TADTests extends BaseNd4jTest {
     public void testEquality1() throws Exception {
 
         char[] order = new char[]{'c','f'};
-        int[] dim_e = new int[]{0, 1};
-        int[] dim_x = new int[]{2, 3};
+        int[] dim_e = new int[]{0, 2};
+        int[] dim_x = new int[]{1, 3};
+
+        List<int[]> dim_3 = Arrays.asList(new int[]{0, 2, 3}, new int[]{0, 1, 2}, new int[]{1, 2, 3}, new int[]{0, 1, 3});
 
 
         for (char o: order) {
@@ -44,13 +47,31 @@ public class TADTests extends BaseNd4jTest {
             for (int e : dim_e) {
                 for (int x : dim_x) {
 
-                    DataBuffer tadShape_N = Nd4j.getExecutioner().getTADManager().getTADOnlyShapeInfo(array, new int[]{e, x}).getFirst();
-                    DataBuffer tadShape_J = array.tensorAlongDimension(0, e, x).shapeInfoDataBuffer();
+                    int[] shape = new int[]{e, x};
+                    Arrays.sort(shape);
 
-                    log.info("Original order: {}; Dimensions: {}; Original shape: {};", o, Arrays.toString(new int[]{e, x}), Arrays.toString(array.shapeInfoDataBuffer().asInt()));
+                    DataBuffer tadShape_N = Nd4j.getExecutioner().getTADManager().getTADOnlyShapeInfo(array, shape).getFirst();
+                    DataBuffer tadShape_J = array.tensorAlongDimension(0, shape).shapeInfoDataBuffer();
+
+                    log.info("Original order: {}; Dimensions: {}; Original shape: {};", o, Arrays.toString(shape), Arrays.toString(array.shapeInfoDataBuffer().asInt()));
                     log.info("Java shape: {}; Native shape: {}", Arrays.toString(tadShape_J.asInt()), Arrays.toString(tadShape_N.asInt()));
                     System.out.println();
                 }
+            }
+        }
+
+        log.info("3D TADs:");
+        for(char o: order) {
+            INDArray array = Nd4j.create(new int[]{9, 7, 5, 3}, o);
+            for (int[] shape: dim_3) {
+                Arrays.sort(shape);
+
+                DataBuffer tadShape_N = Nd4j.getExecutioner().getTADManager().getTADOnlyShapeInfo(array, shape).getFirst();
+                DataBuffer tadShape_J = array.tensorAlongDimension(0, shape).shapeInfoDataBuffer();
+
+                log.info("Original order: {}; Dimensions: {}; Original shape: {};", o, Arrays.toString(shape), Arrays.toString(array.shapeInfoDataBuffer().asInt()));
+                log.info("Java shape: {}; Native shape: {}", Arrays.toString(tadShape_J.asInt()), Arrays.toString(tadShape_N.asInt()));
+                System.out.println();
             }
         }
     }
