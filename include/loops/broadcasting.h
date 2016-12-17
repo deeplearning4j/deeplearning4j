2dd2d18ccf789d6ba8328a21b2937c8d283974e4
raver119 <raver119@gmail.com>
2016-12-17 14:39:25 +0300

    bad fix for alex

--- include/loops/broadcasting.h
+++ include/loops/broadcasting.h
@@ -158,10 +158,10 @@ template<typename OpType>
 				int *xShapeInfo,
 				T *y,
 				int *yShapeInfo,
-				T *result,
+				T *result, int *resultShapeInfo,
 				int *dimension,
 				int dimensionLength, int *tadShapeInfo, int *tadOffset, int *tadShapeInfoZ, int *tadOffsetZ) {
-                                DISPATCH_BY_OPNUM(exec, PARAMS(x, xShapeInfo, y, yShapeInfo, result, dimension, dimensionLength, tadShapeInfo, tadOffset, tadShapeInfoZ, tadOffsetZ), BROADCAST_OPS);
+                                DISPATCH_BY_OPNUM(exec, PARAMS(x, xShapeInfo, y, yShapeInfo, result, resultShapeInfo, dimension, dimensionLength, tadShapeInfo, tadOffset, tadShapeInfoZ, tadOffsetZ), BROADCAST_OPS);
 			}
 
 			/**
@@ -181,6 +181,7 @@ template<typename OpType>
 							 T *y,
 							 int *yShapeInfo,
 							 T *result,
+                             int *resultShapeInfo,
 							 int *dimension,
 							 int dimensionLength, int *tadShapeInfo, int *tadOffset, int *tadShapeInfoZ, int *tadOffsetZ) {
 
@@ -247,29 +248,43 @@ template<typename OpType>
 							int *zStride = shape::stride(tadShapeInfoZ);
 							int *xShape = shape::shapeOf(tadShapeShapeInfo);
 							int *xStride = shape::stride(tadShapeShapeInfo);
+                            int *yShape = shape::shapeOf(yShapeInfo);
+                            int *yStride = shape::stride(yShapeInfo);
 							int zRank = shape::rank(tadShapeInfoZ);
-							int tadRank = shape::rank(tadShapeShapeInfo);
+							int xRank = shape::rank(tadShapeShapeInfo);
+                            int yRank = shape::rank(yShapeInfo);
 
                             int xCoord[MAX_RANK];
+                            int yCoord[MAX_RANK];
                             int zCoord[MAX_RANK];
+
 
 // TODO: cover this codebranch with tests
 // all this stuff already happens within thread
 							for (int f = 0; f < tadLength; f++) {
 
-                                if (shape::order(tadShapeShapeInfo) == 'c')
-                                    shape::ind2sub(tadRank,xShape, f, xCoord);
-                                else
-                                    shape::ind2subC(tadRank,xShape, f, xCoord);
+                                if (shape::order(xShapeInfo) == 'c') {
+                                    shape::ind2sub(xRank, xShape, f, xCoord);
+                                    shape::ind2subC(yRank, yShape, f, yCoord);
+                                } else {
+                                    shape::ind2subC(xRank, xShape, f, xCoord);
+                                    shape::ind2sub(yRank, yShape, f, yCoord);
+                                }
 
-                                if (shape::order(tadShapeInfoZ) == 'c')
+                                if (shape::order(resultShapeInfo) == 'c')
                                     shape::ind2sub(zRank,zShape, f, zCoord);
                                 else
                                     shape::ind2subC(zRank,zShape, f, zCoord);
 
-                                Nd4jIndex xOffset = shape::getOffset(offset, xShape, xStride, xCoord, tadRank);
+                               // if (shape::order(yShapeInfo) == 'c')
+                               //     shape::ind2subC(yRank,yShape, f, yCoord);
+                               // else
+                                    //shape::ind2sub(yRank,yShape, f, yCoord);
+
+                                Nd4jIndex xOffset = shape::getOffset(offset, xShape, xStride, xCoord, xRank);
                                 Nd4jIndex zOffset = shape::getOffset(offsetZ, zShape, zStride, zCoord, zRank);
-                                result[zOffset] = OpType::op(x[xOffset], y[f * yStride]);
+                                Nd4jIndex yOffset = shape::getOffset(0, yShape, yStride, yCoord, yRank);
+                                result[zOffset] = OpType::op(x[xOffset], y[yOffset]);
 							}
 						}
 					}
