5640e242f98117bac64c57c9f761261bb1814e5b
Alex Black <blacka101@gmail.com>
2016-12-18 00:50:21 +1100

    Add ComputationGraph.feedForwardWithKey
    Former-commit-id: 5f8fec57e3c04c805edb54d3a261b9c56ef9dfeb [formerly 77577347018b2f390cb883ffda7bbb7df7818fc9]
    Former-commit-id: 8fc8bb39c9bcae2809be71a244959a6be38a1e86

---
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/graph/scoring/ArrayPairToPair.java
@@ -0,0 +1,17 @@
+package org.deeplearning4j.spark.impl.graph.scoring;
+
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import scala.Tuple2;
+
+/**
+ * Created by Alex on 17/12/2016.
+ */
+public class ArrayPairToPair <K> implements PairFunction<Tuple2<K,INDArray[]>,K,INDArray> {
+    @Override
+    public Tuple2<K, INDArray> call(Tuple2<K, INDArray[]> v1) throws Exception {
+        INDArray arr = (v1._2() == null ? null : v1._2()[0]);
+        return new Tuple2<>(v1._1(), arr);
+    }
+}
---
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/graph/scoring/PairToArrayPair.java
@@ -0,0 +1,16 @@
+package org.deeplearning4j.spark.impl.graph.scoring;
+
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import scala.Tuple2;
+
+/**
+ * Created by Alex on 17/12/2016.
+ */
+public class PairToArrayPair <K> implements PairFunction<Tuple2<K,INDArray>,K,INDArray[]> {
+    @Override
+    public Tuple2<K, INDArray[]> call(Tuple2<K, INDArray> v1) throws Exception {
+        return new Tuple2<>(v1._1(), new INDArray[]{v1._2()});
+    }
+}
