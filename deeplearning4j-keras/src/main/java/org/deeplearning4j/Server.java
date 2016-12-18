12c1baf71e820c4aaf494deb0006600b19f6a94e
Pawel <pkoperek@gmail.com>
2016-12-18 23:36:35 +0100

    slf logging

--- deeplearning4j-keras/src/main/java/org/deeplearning4j/Server.java
+++ deeplearning4j-keras/src/main/java/org/deeplearning4j/Server.java
@@ -1,11 +1,16 @@
 package org.deeplearning4j;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import py4j.GatewayServer;
 
 public class Server {
+
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
     public static void main(String[] args) {
         GatewayServer gatewayServer = new GatewayServer(new DeepLearning4jEntryPoint());
         gatewayServer.start();
-        System.out.println("DeepLearning4j Gateway server running");
+        logger.info("DeepLearning4j Gateway server running");
     }
 }
