52cfd683d0d8e920470541404f0c450886eb0f3f
Pawel <pkoperek@gmail.com>
2016-12-20 17:24:59 +0100

    copying data from float[] to NDArray according to specified shape

---
+++ deeplearning4j-keras/src/main/java/org/deeplearning4j/RecursiveCopier.java
@@ -0,0 +1,33 @@
+package org.deeplearning4j;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+public class RecursiveCopier {
+
+    private final INDArray output;
+    private final float[] input;
+    private final long[] shape;
+    private int inputIdx = 0;
+
+    public RecursiveCopier(INDArray output, float[] input, long[] shape) {
+        this.output = output;
+        this.input = input;
+        this.shape = shape;
+    }
+
+    public void copy() {
+        copyRecursive(0, new int[shape.length]);
+    }
+
+    private void copyRecursive(int shapeIdx, int[] indexes) {
+        if (shape.length == shapeIdx) {
+            output.putScalar(indexes, input[inputIdx]);
+        } else {
+            for (int i = 0; i < shape[shapeIdx]; i++) {
+                indexes[shapeIdx] = i;
+                copyRecursive(shapeIdx + 1, indexes);
+            }
+        }
+    }
+
+}
