27dfe48f1ea720e6b55937c5c86b3ea40e713d8f
jcastaldoFoodEssentials <jcastaldo@foodessentials.com>
2016-12-26 19:57:22 -0700

    Added test for tensors over rank 4

--- nd4j-serde/nd4j-gson/src/test/java/org/nd4j/serde/gson/GsonDeserializationUtilsTest.java
+++ nd4j-serde/nd4j-gson/src/test/java/org/nd4j/serde/gson/GsonDeserializationUtilsTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertEquals;
 
 public class GsonDeserializationUtilsTest {
     @Test
-    public void deserializeRawJson_PassInIndArray_ExpectCorrectDeserialization() {
+    public void deserializeRawJson_PassInInRank3Array_ExpectCorrectDeserialization() {
         String serializedRawArray =
                 "[[[1.00, 11.00, 3.00],\n" +
                 "[13.00, 5.00, 15.00],\n" +
                 "[7.00, 17.00, 9.00]]]";
-        INDArray expectedArray = buildExpectedArray();
+        INDArray expectedArray = buildExpectedArray(1, 1 , 3, 3);
 
         INDArray indArray = GsonDeserializationUtils.deserializeRawJson(serializedRawArray);
 
@@ -30,12 +30,61 @@ public class GsonDeserializationUtilsTest {
         assertEquals(expectedArray, indArray);
     }
 
-    private INDArray buildExpectedArray() {
-        INDArray expectedArray = Nd4j.create(3, 3);
-        expectedArray.putRow(0, Nd4j.create(new double[] { 1, 11, 3 }));
-        expectedArray.putRow(1, Nd4j.create(new double[] { 13, 5, 15 }));
-        expectedArray.putRow(2, Nd4j.create(new double[] { 7, 17, 9 }));
+    @Test
+    public void deserializeRawJson_ArrayIsRankFive_ExpectCorrectDeserialization() {
+        String serializedRawArray =
+                "[[[[[1.00, 11.00],\n" +
+                        "    [3.00, 13.00]],\n" +
+                        "   [[5.00, 15.00],\n" +
+                        "    [7.00, 17.00]]],\n" +
+                        "  [[[9.00, 1.00],\n" +
+                        "    [11.00, 3.00]],\n" +
+                        "   [[13.00, 5.00],\n" +
+                        "    [15.00, 7.00]]],\n" +
+                        "  [[[17.00, 9.00],\n" +
+                        "    [1.00, 11.00]],\n" +
+                        "   [[3.00, 13.00],\n" +
+                        "    [5.00, 15.00]]]],\n" +
+                        " [[[[7.00, 17.00],\n" +
+                        "    [9.00, 1.00]],\n" +
+                        "   [[11.00, 3.00],\n" +
+                        "    [13.00, 5.00]]],\n" +
+                        "  [[[15.00, 7.00],\n" +
+                        "    [17.00, 9.00]],\n" +
+                        "   [[1.00, 11.00],\n" +
+                        "    [3.00, 13.00]]],\n" +
+                        "  [[[5.00, 15.00],\n" +
+                        "    [7.00, 17.00]],\n" +
+                        "   [[9.00, 1.00],\n" +
+                        "    [11.00, 3.00]]]],\n" +
+                        " [[[[13.00, 5.00],\n" +
+                        "    [15.00, 7.00]],\n" +
+                        "   [[17.00, 9.00],\n" +
+                        "    [1.00, 11.00]]],\n" +
+                        "  [[[3.00, 13.00],\n" +
+                        "    [5.00, 15.00]],\n" +
+                        "   [[7.00, 17.00],\n" +
+                        "    [9.00, 1.00]]],\n" +
+                        "  [[[11.00, 3.00],\n" +
+                        "    [13.00, 5.00]],\n" +
+                        "   [[15.00, 7.00],\n" +
+                        "    [17.00, 9.00]]]]]";
+        INDArray expectedArray = buildExpectedArray(8, 3, 3, 2, 2, 2);
 
-        return expectedArray.reshape(1, 3, 3);
+        INDArray array = GsonDeserializationUtils.deserializeRawJson(serializedRawArray);
+
+        assertEquals(expectedArray, array);
+    }
+
+    private INDArray buildExpectedArray(int numberOfTripletRows, int... shape) {
+        INDArray expectedArray = Nd4j.create(3 * numberOfTripletRows, 3);
+        for(int i = 0; i < numberOfTripletRows; i++) {
+            int index = 3 * i;
+            expectedArray.putRow(index, Nd4j.create(new double[]{1, 11, 3}));
+            expectedArray.putRow(index + 1, Nd4j.create(new double[]{13, 5, 15}));
+            expectedArray.putRow(index + 2, Nd4j.create(new double[]{7, 17, 9}));
+        }
+
+        return expectedArray.reshape(shape);
     }
 }
