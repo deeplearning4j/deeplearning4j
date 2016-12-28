6adb1ede741786a4ff1e9b235eba5acc7759d9e6
Pawel <pkoperek@gmail.com>
2016-12-28 23:05:58 +0100

    javadoc

--- deeplearning4j-keras/src/main/java/org/deeplearning4j/keras/Server.java
+++ deeplearning4j-keras/src/main/java/org/deeplearning4j/keras/Server.java
@@ -5,6 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import py4j.GatewayServer;
 
+/**
+ * Main class for the DL4J-as-Keras-backend. Simply launches py4j GatewayServer with
+ * an entry point exposing API available on Python side.
+ */
 @Slf4j
 public class Server {
 
