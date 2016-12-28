f8aca9409795d4e321b6ffe0524705e9d2b27bfd
Pawel <pkoperek@gmail.com>
2016-12-28 22:45:28 +0100

    use lombok in Server

--- deeplearning4j-keras/src/main/java/org/deeplearning4j/keras/Server.java
+++ deeplearning4j-keras/src/main/java/org/deeplearning4j/keras/Server.java
@@ -1,16 +1,16 @@
 package org.deeplearning4j.keras;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import py4j.GatewayServer;
 
+@Slf4j
 public class Server {
-
-    private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         GatewayServer gatewayServer = new GatewayServer(new DeepLearning4jEntryPoint());
         gatewayServer.start();
-        logger.info("DeepLearning4j Gateway server running");
+        log.info("DeepLearning4j Gateway server running");
     }
 }
