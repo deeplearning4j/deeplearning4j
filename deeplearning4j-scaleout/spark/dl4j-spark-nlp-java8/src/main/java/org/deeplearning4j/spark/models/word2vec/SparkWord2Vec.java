aa81477a70d9271fd61c0b13e49d703d06e8df33
raver119 <raver119@gmail.com>
2017-01-05 20:17:57 +0300

    w2v builder
    Former-commit-id: dba74ae0632bdd8f3a95fa207442d804f2d27813 [formerly 55d082133eb5a5755216d34e091d4cecd05528ed]
    Former-commit-id: 4bfc50ac8e922d306655d863c52aff36a5d272bb

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/word2vec/SparkWord2Vec.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/word2vec/SparkWord2Vec.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j.spark.models.word2vec;
 
+import lombok.NonNull;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.deeplearning4j.models.embeddings.loader.VectorsConfiguration;
@@ -10,6 +11,7 @@ import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
 import org.deeplearning4j.spark.models.sequencevectors.SparkSequenceVectors;
 import org.deeplearning4j.spark.models.sequencevectors.functions.TokenizerFunction;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 
 /**
  * @author raver119@gmail.com
@@ -22,9 +24,23 @@ public class SparkWord2Vec extends SparkSequenceVectors<VocabWord> {
         configuration.setTokenizerFactory(DefaultTokenizerFactory.class.getCanonicalName());
     }
 
+    protected SparkWord2Vec(@NonNull VectorsConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
     @Override
     protected VocabCache<ShallowSequenceElement> getShallowVocabCache() {
         return super.getShallowVocabCache();
+    }
+
+
+    /**
+     * PLEASE NOTE: This method isn't supported for Spark implementation. Consider using fitLists() or fitSequences() instead.
+     */
+    @Override
+    @Deprecated
+    public void fit() {
+        throw new UnsupportedOperationException("To use fit() method, please consider using standalone implementation");
     }
 
     public void fitSentences(JavaRDD<String> sentences) {
@@ -39,5 +55,32 @@ public class SparkWord2Vec extends SparkSequenceVectors<VocabWord> {
 
         // now since we have new rdd - just pass it to SeqVec
         super.fitSequences(seqRdd);
+    }
+
+
+    public static class Builder extends SparkSequenceVectors.Builder<VocabWord> {
+
+        public Builder() {
+            super();
+        }
+
+        public Builder(@NonNull VectorsConfiguration configuration) {
+            super(configuration);
+        }
+
+        public Builder setTokenizerFactory(@NonNull TokenizerFactory tokenizerFactory) {
+            configuration.setTokenizerFactory(tokenizerFactory.getClass().getCanonicalName());
+            if (tokenizerFactory.getTokenPreProcessor() != null)
+                configuration.setTokenPreProcessor(tokenizerFactory.getTokenPreProcessor().getClass().getCanonicalName());
+
+            return this;
+        }
+
+
+        public SparkWord2Vec build() {
+            SparkWord2Vec sw2v = new SparkWord2Vec(configuration);
+
+            return sw2v;
+        }
     }
 }
