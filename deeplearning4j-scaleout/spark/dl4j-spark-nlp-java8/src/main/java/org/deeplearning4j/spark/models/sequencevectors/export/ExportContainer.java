4ca1bd8eedc0485992f2599b2efc22eb1a9a3e52
raver119 <raver119@gmail.com>
2017-01-07 20:33:04 +0300

    model export dl4j
    Former-commit-id: ca855af040b087333cb836cc1ba8ac9058927f35 [formerly e0ba1238c57eb15c239ff0ab0ea4aa7ea285398d]
    Former-commit-id: f54d4dacf1ef7498c7cdad48aa5509015e88481a

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/export/ExportContainer.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/export/ExportContainer.java
@@ -9,6 +9,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * @author raver119@gmail.com
@@ -21,10 +22,18 @@ public class ExportContainer<T extends SequenceElement> implements Serializable{
     private T element;
     private INDArray array;
 
+    protected static final Pattern pattern0 = Pattern.compile("(\\[|\\])");
+    protected static final Pattern pattern1 = Pattern.compile("(\\,|\\.|\\;)+\\s");
+
     // TODO: implement B64 optional compression here?
     @Override
     public String toString() {
         // TODO: we need proper string cleansing here
-        return element.getLabel() + " " + (Arrays.toString(array.data().asFloat()).replaceAll("(\\[\\],)"," "));
+
+        String ars = Arrays.toString(array.data().asFloat());
+        ars = pattern0.matcher(ars).replaceAll("").trim();
+        ars = pattern1.matcher(ars).replaceAll(" ").trim();
+
+        return element.getLabel().trim() + " " + ars;
     }
 }
