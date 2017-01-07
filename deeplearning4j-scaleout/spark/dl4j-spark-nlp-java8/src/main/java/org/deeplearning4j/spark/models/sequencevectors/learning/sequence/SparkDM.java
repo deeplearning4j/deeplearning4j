86387f05df5b0504205c4622ab1a706758f93ed9
raver119 <raver119@gmail.com>
2017-01-07 03:23:17 +0300

    daily commit
    Former-commit-id: cfb47d1ca87d3dd070792b00bb9f42d9eaebcce5

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/learning/sequence/SparkDM.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/learning/sequence/SparkDM.java
@@ -3,6 +3,8 @@ package org.deeplearning4j.spark.models.sequencevectors.learning.sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.ShallowSequenceElement;
 import org.deeplearning4j.spark.models.sequencevectors.learning.elements.BaseSparkLearningAlgorithm;
+import org.nd4j.parameterserver.distributed.messages.TrainingMessage;
+import org.nd4j.parameterserver.distributed.training.TrainingDriver;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -18,5 +20,10 @@ public class SparkDM extends BaseSparkLearningAlgorithm {
     @Override
     public double learnSequence(Sequence<ShallowSequenceElement> sequence, AtomicLong nextRandom, double learningRate) {
         return 0;
+    }
+
+    @Override
+    public TrainingDriver<? extends TrainingMessage> getTrainingDriver() {
+        return null;
     }
 }
