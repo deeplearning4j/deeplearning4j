ce580db1aabc405f4cae9bfec211391febf1d73f
Alex Black <blacka101@gmail.com>
2017-01-09 18:37:41 +1100

    Partial fix of TSNE
    Former-commit-id: 2e6527fed570d9c711c63c61414556d8cac215d4 [formerly 8940bf6d0840a03ae5049771076bf759b18caacc]
    Former-commit-id: 80d97ec8e505d35873060c32daf94f3c55e3bc9a

--- deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/legacy/renderTsne.js
+++ deeplearning4j-ui-parent/deeplearning4j-play/src/main/resources/deeplearning4jUiAssets/legacy/renderTsne.js
@@ -1,4 +1,4 @@
-  /*
+/*
  *
  *  * Copyright 2015 Skymind,Inc.
  *  *
@@ -16,119 +16,148 @@
  *
  */
 
-var height=700;
-var width=1024;
+var height = 700;
+var width = 1024;
 
 
 var x = [];
-  var y = [];
-  var name3 = [];
+var y = [];
+var name3 = [];
 
 
-
-
-var tx=0, ty=0;
-var ss=1;
+// var tx=0, ty=0;
+// var ss=1;
 
 var svg;
+var renderSpace;
 var xMax = 0, xMin = 0, yMax = 0, yMin = 0;
-
-var fx;
-var fy;
 
 var xAxis;
 var yAxis;
 
-function zoomHandler() {
+var xScale;
+var yScale;
 
-  svg.select(".x.axis").call(xAxis);
-  svg.select(".y.axis").call(yAxis);
+function zoomFunction() {
 
-  tx = d3.event.translate[0];
-  ty = d3.event.translate[1];
-  ss = d3.event.scale;
-  console.log('zoom called. Scale: ' + ss + " tX: " + tx + " tY: " + ty);
-  svg.selectAll('circle')
-      .data(name3)
-      .attr("transform", function(d, i) { return "translate(" +
-                                            ((x[i]*20*ss + tx) + 400) + "," +
-                                            ((y[i]*20*ss + ty) + 400) + ")";
-                                            });
-  svg.selectAll('text')
-        .data(name3)
-        .attr("transform", function(d, i) { return "translate(" +
-                                              ((x[i]*20*ss + tx) + 400) + "," +
-                                              ((y[i]*20*ss + ty) + 400) + ")";
-                                              });
+    var translateX = d3.event.translate[0];
+    var translateY = d3.event.translate[1];
+    var currentScale = d3.event.scale;
+    console.log('zoom called. Scale: ' + currentScale + " translateX: " + translateX + " translateY: " + translateY);
 
+    //Redraw the x and y axis:
+    renderSpace.select(".x.axis").call(xAxis);
+    renderSpace.select(".y.axis").call(yAxis);
+
+    renderSpace.selectAll('circle')
+        .attr("cx", function (d, i) {
+            // return xScale(d.cx);
+            return xScale(d['xPos']);
+        })
+        .attr("cy", function (d, i) {
+            // return yScale(d.cy);
+            return yScale(d['yPos']);
+        });
 }
 
+var circleRadius = 2.0;
+var textSize = "11pt";
+var textColor = "red";
 
 var marginLeft = 100;
-var widthExMargin = width - marginLeft;
 var marginTop = 100;
-var heightExMargin = height - marginTop;
+var marginBottom = 100;
+var marginRight = 100;
+var widthExMargin = width - marginLeft - marginRight;
+var heightExMargin = height - marginTop - marginBottom;
+
+var zoom;
+
+
+function centerFn(){
+    return d3.zoomIdentity.scale(0.00001);
+}
 
 function drawEmbedding() {
     $("#embed").empty();
     var div = d3.select("#embed");
 
-    xScale = d3.scale.linear().range([0,widthExMargin]);
+    xScale = d3.scale.linear().range([0, widthExMargin]);
     yScale = d3.scale.linear().range([heightExMargin, 0]);
 
     xAxis = d3.svg.axis().scale(xScale)
-        .innerTickSize(-height) //Used as grid line
+        .innerTickSize(-heightExMargin) //Used as grid line
         .orient("bottom").ticks(5);
 
     yAxis = d3.svg.axis().scale(yScale)
-        .innerTickSize(-width)  //Used as grid line
+        .innerTickSize(-widthExMargin)  //Used as grid line
         .orient("left").ticks(5);
 
     svg = div.append("svg") // svg is global
-        .attr("width", widthExMargin)
-        .attr("height", heightExMargin)
-        .attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
-
-    // Add the X Axis
-    xAxisNode = svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + heightExMargin + ")")
-        .style("stroke", "#000")
-        .style("stroke-width", "1.0")
-        .style("fill", "#000")
-        .call(xAxis);
-    xAxisNode.selectAll('text').style("stroke-width",0).style("fill","#000000");
-
-
-    // Add the Y Axis
-    var yAxisNode = svg.append("g")
-        .attr("class", "y axis")
-        .style("stroke", "#000")
-        .style("stroke-width", "1.0")
-        .style("fill", "#000")
-        .call(yAxis);
-    yAxisNode.selectAll('text').style("stroke-width",0).style("fill","#000000");
-
+        .attr("width", width)
+        .attr("height", height);
 
     xMin = d3.min(x);
     xMax = d3.max(x);
     yMin = d3.min(y);
     yMax = d3.max(y);
 
+    // zoom = d3.behavior.zoom()
+    zoom = d3.behavior.zoom()
+        .x(xScale)
+        .y(yScale)
+        // .scaleExtent([0.00000001, 100000])
+        // .translate([xMin, yMin])
+        // .center([0, 0])
+        // .translate([0, 0])
+        .center([(xMax-xMin)/2, (yMax-yMin)/2])
+        .scale(0.00001)
+        .on("zoom", zoomFunction);
+
+
+
     xScale.domain([xMin, xMax]);
     yScale.domain([yMin, yMax]);
 
+    renderSpace = svg.append("g");
+    renderSpace.attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
+    // renderSpace.call(zoom);
+    svg.call(zoom);
+
+    // Add the X Axis
+    xAxisNode = renderSpace.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + heightExMargin + ")")
+        .style("stroke", "#000")
+        .style("stroke-width", "1.0")
+        .style("fill", "none")
+        .call(xAxis);
+    xAxisNode.selectAll('text').style("stroke-width", 0).style("fill", "#000000");
+
+
+    // Add the Y Axis
+    yAxisNode = renderSpace.append("g")
+        .attr("class", "y axis")
+        .style("stroke", "#000")
+        .style("stroke-width", "1.0")
+        .style("fill", "none")
+        .call(yAxis);
+    yAxisNode.selectAll('text').style("stroke-width", 0).style("fill", "#000000");
+
+
+    console.log("x/y min/max: " + xMin + ", " + xMax + ", " + yMin + ", " + yMax);
+
     //Add the data
     var data = x.map(function (d, i) {
-        return {'xPos': x[i], 'yPos': y[i]};
+        return {'xPos': x[i], 'yPos': y[i], 'name': name3[i]};
     });
 
-    svg.selectAll("circle")
+    renderSpace.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
         .style("fill", "#000000")
-        .attr("r", "3")
+        .attr("r", circleRadius)
         .attr("cx", function (d) {
             return xScale(d['xPos']);
         })
@@ -136,90 +165,25 @@ function drawEmbedding() {
             return yScale(d['yPos']);
         });
 
-    //
-    // fx = d3.scale.linear()
-    //     .domain([xMin, xMax])
-    //     .range([0, width])
-    //
-    // fy = d3.scale.linear()
-    //     .domain([yMin, yMax])
-    //     .range([height, 0]);
-    //
-    // console.log("xMin/xMax: " + xMin + "," + xMax + "; yMin/yMax: " + yMin + "," + yMax);
-    // console.log("width = " + width + ", height = " + height);
-    //
-    // //Define X axis
-    // xAxis = d3.svg.axis()
-		// .scale(fx)
-		// .orient("bottom")
-		// .tickSize(-height)
-    //     .ticks(10)
-		// .tickFormat(d3.format("s"));
-    //
-    // //Define Y axis
-    // yAxis = d3.svg.axis()
-		// .scale(fy)
-		// .orient("left")
-		// .ticks(5)
-		// .tickSize(-width)
-		// .tickFormat(d3.format("s"));
-    //
-    //
-    // svg = div.append("svg") // svg is global
-    // .attr("width", width)
-    // .attr("height", height);
-    //
-    //
-    // svg.append("g")
-		// .attr("class", "x axis")
-		// .attr("transform", "translate(0," + height + ")")
-		// .call(xAxis);
-    //
-    // svg.append("g")
-		// .attr("class", "y axis")
-		// .call(yAxis);
-    //
-    //
-    //
-    // svg.selectAll("circle")
-    //     .data(name3)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", function(d, i) {
-    //     			   		return fx(x[i]);
-    //     			   })
-    //     			   .attr("cy", function(d, i) {
-    //     			   		return fy(y[i]);
-    //     			   })
-    //     			   .attr("r", 2);
-    //
-    // svg.selectAll("text")
-    //    .data(name3)
-    //    .enter()
-    //    .append("text")
-    //    .text(function(d,i) {
-    // 			   		return name3[i];
-    // 			   })
-    //    .attr("x", function(d,i) {
-    // 			   		return fx(x[i]);
-    // 			   })
-    //    .attr("y", function(d, i) {
-    // 			   		return fy(y[i]);
-    // 			   })
-    //    .attr("font-family", "sans-serif")
-    //    .attr("font-size", "11px")
-    //    .attr("fill", "red");
-    //
-    //
-    // var zoomListener = d3.behavior.zoom()
-    //     .x(fx)
-    //     .y(fy)
-    //     .scaleExtent([0.00000001, 100000])
-    //     .center([0,0])
-    //     .on("zoom", zoomHandler);
-    // zoomListener(svg);
-
+    renderSpace.selectAll("text")
+        .attr("class", "tsneTextLabels")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function (d) {
+            return d['name'];
+        })
+        .attr("x", function (d) {
+            return xScale(d['xPos']);
+        })
+        .attr("y", function (d) {
+            return yScale(d['yPos']);
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", textSize)
+        .attr("fill", textColor);
 }
+
 
 
 function drawTsne() {
@@ -227,61 +191,60 @@ function drawTsne() {
     if (sid == undefined) sid = "UploadedFile";
 
 
- $.ajax({
-     url: "/tsne/coords/" + sid,
-    cache: false
-  })
-    .done(function( data ) {
-        x = [];
-        y = [];
-        name3 = [];
-        if (data.length > 0 ) {
-          for(var i = 0; i < data.length; i++) {
-             var split = data[i].split(',');
-             var xCoord = split[0];
-             var yCoord = split[1];
-             var name2 = split[2];
-             x.push(xCoord);
-             y.push(yCoord);
-             name3.push(name2);
-        }
+    $.ajax({
+        url: "/tsne/coords/" + sid,
+        cache: false
+    })
+        .done(function (data) {
+            x = [];
+            y = [];
+            name3 = [];
+            if (data.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    var split = data[i].split(',');
+                    var xCoord = parseFloat(split[0]);
+                    var yCoord = parseFloat(split[1]);
+                    var name2 = split[2];
+                    x.push(xCoord);
+                    y.push(yCoord);
+                    name3.push(name2);
+                }
 
-        /*
-            This values could be pushed for debug purposes
+                /*
+                 This values could be pushed for debug purposes
 
-            x.push(10);
-            y.push(15);
-            name3.push("alpha");
-        */
+                 x.push(10);
+                 y.push(15);
+                 name3.push("alpha");
+                 */
 
-        xMax = d3.max(x);
-        xMin = d3.min(x);
+                xMax = d3.max(x);
+                xMin = d3.min(x);
 
-        yMax = d3.max(y);
-        yMin = d3.min(y);
+                yMax = d3.max(y);
+                yMin = d3.min(y);
 
-        // console.log("xMin: " + xMin);
-        // console.log("xMax: " + xMax);
-        //
-        // console.log("yMin: " + yMin);
-        // console.log("yMax: " + yMax);
+                // console.log("xMin: " + xMin);
+                // console.log("xMax: " + xMax);
+                //
+                // console.log("yMin: " + yMin);
+                // console.log("yMax: " + yMax);
 
-        drawEmbedding();
-      } else {
-        $.notify({
-        	title: '<strong>No t-SNE data available!</strong>',
-        	message: 'Please upload something, or check again later.'
-        },{
-        	type: 'danger',
-        	placement: {
-            		from: "top",
-            		align: "center"
-            	},
+                drawEmbedding();
+            } else {
+                $.notify({
+                    title: '<strong>No t-SNE data available!</strong>',
+                    message: 'Please upload something, or check again later.'
+                }, {
+                    type: 'danger',
+                    placement: {
+                        from: "top",
+                        align: "center"
+                    },
+                });
+            }
         });
-      }
-    });
 }
-
 
 
 $(window).load(drawTsne());
