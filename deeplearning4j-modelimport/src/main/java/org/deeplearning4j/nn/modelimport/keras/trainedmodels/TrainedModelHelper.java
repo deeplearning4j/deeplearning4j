7ad930ed5bd5c8133b4e37de19772fe1f3fafb60
eraly <susan.eraly@gmail.com>
2017-01-16 08:57:36 -0800

    trained model helpers

--- deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/trainedmodels/TrainedModelHelper.java
+++ deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/trainedmodels/TrainedModelHelper.java
@@ -5,7 +5,6 @@ import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
-import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,41 +30,54 @@ public class TrainedModelHelper {
 
     private File h5File;
     private File jsonFile;
-    private boolean userProvidedPath = false;
+    private boolean userProvidedH5 = false;
+    private boolean userProvidedJSON = false;
 
-    private final DataSetPreProcessor PRE_PROCESSOR;
+    private String[] decodeMap;
 
     public TrainedModelHelper(TrainedModels model) {
-        this.h5URL = model.getH5URL();
-        this.jsonURL = model.getJSONURL();
-        this.h5FileName = model.getH5FileName();
-        this.jsonFileName = model.getJSONFileName();
         this.MODEL_DIR = new File(HOME_DIR, BASE_DIR + model.getModelDir());
+
+        this.h5URL = model.getH5URL();
+        this.h5FileName = model.getH5FileName();
         this.h5File = new File(MODEL_DIR,h5FileName);
+
+        this.jsonURL = model.getJSONURL();
+        this.jsonFileName = model.getJSONFileName();
         this.jsonFile = new File(MODEL_DIR,jsonFileName);
-        this.PRE_PROCESSOR = model.getPreProcessor();
     }
 
     public void setPathToH5(String pathtoH5) {
         this.h5File = new File(pathtoH5);
-        this.userProvidedPath = true;
+        this.userProvidedH5 = true;
         logger.info("Helper will use path given to H5 file");
     }
 
+    public void setPathToJSON(String pathToJSON) {
+        this.jsonFile = new File(pathToJSON);
+        this.userProvidedJSON = true;
+        logger.info("Helper will use path given to JSON file");
+    }
+
+    /*
+        FIXME
+        Once I upload the file this should be a static class - leaving as is for now
+        And the set paths methods above must go
+     */
     public ComputationGraph loadModel() throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         download();
         return KerasModelImport.importKerasModelAndWeights(jsonFile.getAbsolutePath(), h5File.getAbsolutePath());
     }
 
     private void download() throws IOException {
-        if (!h5File.exists() && !userProvidedPath) {
+        if (!h5File.exists() && !userProvidedH5) {
             if (!(MODEL_DIR.isDirectory() || MODEL_DIR.mkdirs())) {
                 throw new IOException("Could not mkdir " + MODEL_DIR);
             }
             logger.info("H5 weights not found in default location. Copying from URL "+h5URL+"\n\tto location "+h5File.getAbsolutePath());
             FileUtils.copyURLToFile(new URL(h5URL), h5File);
         }
-        if (!jsonFile.exists()) {
+        if (!jsonFile.exists() && !userProvidedJSON) {
             if (!(MODEL_DIR.isDirectory() || MODEL_DIR.mkdirs())) {
                 throw new IOException("Could not mkdir " + MODEL_DIR);
             }
@@ -74,7 +86,4 @@ public class TrainedModelHelper {
         }
     }
 
-    public DataSetPreProcessor getPreProcessor() {
-        return this.PRE_PROCESSOR;
-    }
 }
