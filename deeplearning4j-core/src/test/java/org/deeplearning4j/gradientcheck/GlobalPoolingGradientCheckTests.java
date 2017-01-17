8fe8d29afb2ad03ca9293183c68763ec6350d1e9
Alex Black <blacka101@gmail.com>
2017-01-17 21:39:19 +1100

    Global pooling + CNN (no masks) - implemented and passing some gradient checks

--- deeplearning4j-core/src/test/java/org/deeplearning4j/gradientcheck/GlobalPoolingGradientCheckTests.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/gradientcheck/GlobalPoolingGradientCheckTests.java
@@ -4,6 +4,7 @@ import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
@@ -30,7 +31,7 @@ public class GlobalPoolingGradientCheckTests {
 
     @Test
     public void testLSTMGlobalPoolingBasicMultiLayer() {
-        //Basic test of GravesLSTM layer
+        //Basic test of global pooling w/ LSTM
         Nd4j.getRandom().setSeed(12345L);
 
         int timeSeriesLength = 10;
@@ -40,7 +41,6 @@ public class GlobalPoolingGradientCheckTests {
 
         int[] minibatchSizes = new int[]{1,3};
         PoolingType[] poolingTypes = new PoolingType[]{PoolingType.AVG, PoolingType.SUM, PoolingType.MAX};
-//        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.AVG, PoolingType.SUM};
 
         for( int miniBatchSize : minibatchSizes) {
             for (PoolingType pt : poolingTypes) {
@@ -48,13 +48,12 @@ public class GlobalPoolingGradientCheckTests {
                 MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                         .regularization(false)
                         .updater(Updater.NONE)
+                        .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 1.0))
                         .seed(12345L)
                         .list()
-                        .layer(0, new GravesLSTM.Builder().nIn(nIn).nOut(layerSize).activation(Activation.TANH)
-                                .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 1.0)).build())
+                        .layer(0, new GravesLSTM.Builder().nIn(nIn).nOut(layerSize).activation(Activation.TANH).build())
                         .layer(1, new GlobalPoolingLayer.Builder().poolingType(pt).build())
-                        .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(layerSize).nOut(nOut)
-                                .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 1.0)).build())
+                        .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(layerSize).nOut(nOut).build())
                         .pretrain(false).backprop(true)
                         .build();
 
@@ -89,6 +88,134 @@ public class GlobalPoolingGradientCheckTests {
                 assertTrue(gradOK);
 
             }
+        }
+    }
+
+    @Test
+    public void testCnnGlobalPoolingBasicMultiLayer() {
+        //Basic test of global pooling w/ CNN
+        Nd4j.getRandom().setSeed(12345L);
+
+        int timeSeriesLength = 10;
+        int inputDepth = 3;
+        int inputH = 5;
+        int inputW = 4;
+        int layerDepth = 4;
+        int nOut = 2;
+
+        int[] minibatchSizes = new int[]{1,3};
+        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.AVG, PoolingType.SUM, PoolingType.MAX};
+
+        for( int miniBatchSize : minibatchSizes) {
+            for (PoolingType pt : poolingTypes) {
+
+                MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                        .regularization(false)
+                        .updater(Updater.NONE)
+                        .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 1.0))
+                        .seed(12345L)
+                        .list()
+                        .layer(0, new ConvolutionLayer.Builder().kernelSize(2,2).stride(1,1).nOut(layerDepth).build())
+                        .layer(1, new GlobalPoolingLayer.Builder().poolingType(pt).build())
+                        .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nOut(nOut).build())
+                        .pretrain(false).backprop(true)
+                        .setInputType(InputType.convolutional(inputH, inputW, inputDepth))
+                        .build();
+
+                MultiLayerNetwork mln = new MultiLayerNetwork(conf);
+                mln.init();
+
+                Random r = new Random(12345L);
+                INDArray input = Nd4j.rand(new int[]{miniBatchSize, inputDepth, inputH, inputW}).subi(0.5);
+
+                INDArray labels = Nd4j.zeros(miniBatchSize, nOut);
+                for (int i = 0; i < miniBatchSize; i++) {
+                    int idx = r.nextInt(nOut);
+                    labels.putScalar(i, idx, 1.0);
+                }
+
+                if (PRINT_RESULTS) {
+                    System.out.println("testCnnGlobalPoolingBasicMultiLayer() - " + pt + ", minibatch = " + miniBatchSize);
+                    for (int j = 0; j < mln.getnLayers(); j++)
+                        System.out.println("Layer " + j + " # params: " + mln.getLayer(j).numParams());
+                }
+
+                boolean gradOK = GradientCheckUtil.checkGradients(mln, DEFAULT_EPS, DEFAULT_MAX_REL_ERROR, DEFAULT_MIN_ABS_ERROR,
+                        PRINT_RESULTS, RETURN_ON_FIRST_FAILURE, input, labels);
+
+                assertTrue(gradOK);
+
+            }
+        }
+    }
+
+    @Test(expected = Exception.class)   //At least until masking is properly implemented and fixed!
+    public void testLSTMGlobalPoolingBasicMultiLayerVariableLength() {
+        //Basic test of GravesLSTM layer
+        Nd4j.getRandom().setSeed(12345L);
+
+        int timeSeriesLength = 10;
+        int nIn = 5;
+        int layerSize = 4;
+        int nOut = 2;
+
+        int miniBatchSize = 3;
+        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.AVG, PoolingType.SUM, PoolingType.MAX};
+
+        for (PoolingType pt : poolingTypes) {
+
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .regularization(false)
+                    .updater(Updater.NONE)
+                    .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 1.0))
+                    .seed(12345L)
+                    .list()
+                    .layer(0, new GravesLSTM.Builder().nIn(nIn).nOut(layerSize).activation(Activation.TANH).build())
+                    .layer(1, new GlobalPoolingLayer.Builder().poolingType(pt).build())
+                    .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(layerSize).nOut(nOut).build())
+                    .pretrain(false).backprop(true)
+                    .build();
+
+            MultiLayerNetwork mln = new MultiLayerNetwork(conf);
+            mln.init();
+
+            Random r = new Random(12345L);
+            INDArray input = Nd4j.zeros(miniBatchSize, nIn, timeSeriesLength);
+            for (int i = 0; i < miniBatchSize; i++) {
+                for (int j = 0; j < nIn; j++) {
+                    for (int k = 0; k < timeSeriesLength; k++) {
+                        input.putScalar(new int[]{i, j, k}, r.nextDouble() - 0.5);
+                    }
+                }
+            }
+
+            INDArray featuresMask = Nd4j.create(miniBatchSize, timeSeriesLength);
+            for( int i=0; i<miniBatchSize; i++ ){
+                int to = timeSeriesLength-i;
+                for( int j=0; j<to; j++ ){
+                    featuresMask.putScalar(i,j,1.0);
+                }
+            }
+
+            INDArray labels = Nd4j.zeros(miniBatchSize, nOut);
+            for (int i = 0; i < miniBatchSize; i++) {
+                int idx = r.nextInt(nOut);
+                labels.putScalar(i, idx, 1.0);
+            }
+
+            mln.setLayerMaskArrays(featuresMask, null);
+
+            if (PRINT_RESULTS) {
+                System.out.println("testLSTMGlobalPoolingBasicMultiLayer() - " + pt + ", minibatch = " + miniBatchSize);
+                for (int j = 0; j < mln.getnLayers(); j++)
+                    System.out.println("Layer " + j + " # params: " + mln.getLayer(j).numParams());
+            }
+
+            boolean gradOK = GradientCheckUtil.checkGradients(mln, DEFAULT_EPS, DEFAULT_MAX_REL_ERROR, DEFAULT_MIN_ABS_ERROR,
+                    PRINT_RESULTS, RETURN_ON_FIRST_FAILURE, input, labels);
+
+            assertTrue(gradOK);
+
         }
     }
 
