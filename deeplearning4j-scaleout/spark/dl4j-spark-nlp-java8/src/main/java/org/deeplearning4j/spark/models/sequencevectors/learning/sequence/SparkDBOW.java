32b310a64a91ea0706e087b330e9cd4c8ffefde4
raver119 <raver119@gmail.com>
2017-01-19 14:43:04 +0300

    missing signatures
    Former-commit-id: ca07c7a9bec6eeb773042ba9aea25baade8100a7 [formerly efdacf7241d5a14ee12e2140debf70a9a4c97e19]
    Former-commit-id: 2532295639183d2aef35491dc232104e03d11dee

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/learning/sequence/SparkDBOW.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/learning/sequence/SparkDBOW.java
@@ -3,6 +3,7 @@ package org.deeplearning4j.spark.models.sequencevectors.learning.sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.Sequence;
 import org.deeplearning4j.models.sequencevectors.sequence.ShallowSequenceElement;
 import org.deeplearning4j.spark.models.sequencevectors.learning.elements.BaseSparkLearningAlgorithm;
+import org.nd4j.parameterserver.distributed.messages.Frame;
 import org.nd4j.parameterserver.distributed.messages.TrainingMessage;
 import org.nd4j.parameterserver.distributed.training.TrainingDriver;
 
@@ -17,9 +18,10 @@ public class SparkDBOW extends BaseSparkLearningAlgorithm {
         return "Spark-DBOW";
     }
 
+
     @Override
-    public double learnSequence(Sequence<ShallowSequenceElement> sequence, AtomicLong nextRandom, double learningRate) {
-        return 0;
+    public Frame<? extends TrainingMessage> frameSequence(Sequence<ShallowSequenceElement> sequence, AtomicLong nextRandom, double learningRate) {
+        return null;
     }
 
     @Override
