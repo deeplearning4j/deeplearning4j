3cebce6bf0fa25bbd45767250e4458c5ef8ed237
raver119 <raver119@gmail.com>
2017-01-19 17:52:11 +0300

    VocabCache model exporter for local tests
    Former-commit-id: 9985537a989cb5f2fda242a74dc4bc0ac39fab54 [formerly 3b3116f2d4530fcfd73ab8fa5fcc91983aee8ab6]
    Former-commit-id: 87f1a79898535dfa049abd32bb703bff60e77e75

--- deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/export/impl/VocabCacheExporter.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-nlp-java8/src/main/java/org/deeplearning4j/spark/models/sequencevectors/export/impl/VocabCacheExporter.java
@@ -3,53 +3,71 @@ package org.deeplearning4j.spark.models.sequencevectors.export.impl;
 import lombok.Getter;
 import lombok.NonNull;
 import org.apache.spark.api.java.JavaRDD;
+import org.deeplearning4j.berkeley.Pair;
 import org.deeplearning4j.models.embeddings.WeightLookupTable;
+import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.sequencevectors.sequence.SequenceElement;
+import org.deeplearning4j.models.sequencevectors.sequence.ShallowSequenceElement;
 import org.deeplearning4j.models.word2vec.VocabWord;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+import org.deeplearning4j.models.word2vec.wordstore.inmemory.AbstractCache;
 import org.deeplearning4j.spark.models.sequencevectors.export.ExportContainer;
+import org.deeplearning4j.spark.models.sequencevectors.export.SparkModelExporter;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * This model exporter is suitable for debug/testing only.
  *
- * PLEASE NOTE: Never use this exporter in real distributed environment!
+ * PLEASE NOTE: Never use this exporter in real environment if your model won't fit into memory of driver.
  *
  * @author raver119@gmail.com
  */
-public class VocabCacheExporter extends HdfsModelExporter<VocabWord> {
+public class VocabCacheExporter implements SparkModelExporter<VocabWord> {
 
     @Getter protected VocabCache<VocabWord> vocabCache;
-    @Getter protected WeightLookupTable<VocabWord> lookupTable;
+    @Getter protected InMemoryLookupTable<VocabWord> lookupTable;
+    @Getter protected Word2Vec word2Vec;
 
     public VocabCacheExporter() {
-        try {
-            File tempFile = File.createTempFile("temp", "file");
-            tempFile.deleteOnExit();
 
-            this.path = tempFile.getAbsolutePath();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
-
-    public VocabCacheExporter(@NonNull String path) {
-        super(path);
-    }
-
 
     @Override
     public void export(JavaRDD<ExportContainer<VocabWord>> rdd) {
-        super.export(rdd);
 
-        // now we just load it back
-        Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel(path);
+        // beware, generally that's VERY bad idea, but will work fine for testing purposes
+        List<ExportContainer<VocabWord>> list = rdd.collect();
 
-        // this is bad & dirty, but we don't really need anything else for testing
-        vocabCache = word2Vec.getVocab();
-        lookupTable = word2Vec.getLookupTable();
+        if (vocabCache == null)
+            vocabCache = new AbstractCache<>();
+
+
+        // just roll through list
+        for (ExportContainer<VocabWord> element: list) {
+            VocabWord word = element.getElement();
+            INDArray weights = element.getArray();
+
+            if (lookupTable == null)
+                lookupTable = new InMemoryLookupTable.Builder<VocabWord>()
+                        .cache(vocabCache)
+                        .vectorLength(weights.length())
+                        .build();
+
+            if (lookupTable.getSyn0() == null)
+                lookupTable.setSyn0(Nd4j.create(list.size(), weights.length()));
+
+            lookupTable.getSyn0().getRow(word.getIndex()).assign(weights);
+        }
+
+        // this is bad & dirty, but we don't really need anything else for testing :)
+        word2Vec = WordVectorSerializer.fromPair(Pair.<InMemoryLookupTable, VocabCache>makePair(lookupTable, vocabCache));
+
+        list.clear();
     }
 }
