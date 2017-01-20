1ded3e53c1c8aa525ea321b996483b1c528ac2d3
turambar <turinturumarth@gmail.com>
2017-01-20 07:42:45 -0800

    Fix dropout bug in LSTM layers.
    Former-commit-id: 934d29aea5705c3af5c4b039863dc6e2718e822c [formerly 3e20eb7bb23a0b5792f3f931c9f4c2b6d85979f1]
    Former-commit-id: 595939fbe5228c82c1940f1fa6766b34d730e287

--- deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/KerasLayer.java
+++ deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/KerasLayer.java
@@ -814,9 +814,9 @@ public class KerasLayer {
         if (innerConfig.containsKey(LAYER_FIELD_DROPOUT)) {
             /* For most feedforward layers. */
             dropout = 1.0 - (double) innerConfig.get(LAYER_FIELD_DROPOUT);
-        } else if (layerConfig.containsKey(LAYER_FIELD_DROPOUT_W)) {
+        } else if (innerConfig.containsKey(LAYER_FIELD_DROPOUT_W)) {
             /* For LSTMs. */
-            dropout = 1.0 - (double) layerConfig.get(LAYER_FIELD_DROPOUT_W);
+            dropout = 1.0 - (double) innerConfig.get(LAYER_FIELD_DROPOUT_W);
         }
         return dropout;
     }
