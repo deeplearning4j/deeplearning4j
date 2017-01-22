62ff5ef3777f652980731ccea2679e2b7f48327c
Alex Black <blacka101@gmail.com>
2017-01-22 23:00:06 +1100

    Global pooling + masking + CNNs - forward & backward implementation done (first pass)
    Former-commit-id: bde2cb4f375b41c6f655e26e9c3084dfd30ab5ab [formerly 70e15dc9cf9ad04b2d7efb0db1aab88a940817c1]
    Former-commit-id: b735f0863398b44bd1a4e871df6cf1aa0bcc9409

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/pooling/GlobalPoolingMaskingTests.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/pooling/GlobalPoolingMaskingTests.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j.nn.layers.pooling;
 
+import org.deeplearning4j.nn.conf.ConvolutionMode;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
@@ -10,12 +11,15 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.junit.Test;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastAddOp;
+import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastMulOp;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.util.Random;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -82,5 +86,193 @@ public class GlobalPoolingMaskingTests {
         assertEquals(outSubset, outMask);
     }
 
+    @Test
+    public void testMaskingCnnDim3_SingleExample(){
+        //Test masking, where mask is along dimension 3
 
+        int minibatch = 1;
+        int depthIn = 2;
+        int depthOut = 2;
+        int nOut = 2;
+        int height = 3;
+        int width = 6;
+
+        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.SUM, PoolingType.AVG, PoolingType.MAX, PoolingType.PNORM};
+
+        for(PoolingType pt : poolingTypes) {
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .weightInit(WeightInit.XAVIER)
+                    .convolutionMode(ConvolutionMode.Same)
+                    .seed(12345L)
+                    .list()
+                    .layer(0, new ConvolutionLayer.Builder().nIn(depthIn).nOut(depthOut)
+                            .kernelSize(height, 2).stride(height,1)
+                            .activation(Activation.TANH).build())
+                    .layer(1, new org.deeplearning4j.nn.conf.layers.GlobalPoolingLayer.Builder().poolingType(pt).build())
+                    .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(depthOut).nOut(nOut).build())
+                    .pretrain(false).backprop(true)
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            INDArray inToBeMasked = Nd4j.rand(new int[]{minibatch, depthIn, height, width});
+
+            //Shape for mask: [minibatch, width]
+            INDArray maskArray = Nd4j.create(new double[]{1, 1, 1, 1, 1, 0});
+
+            //Multiply the input by the mask array, to ensure the 0s in the mask correspond to 0s in the input vector
+            // as would be the case in practice...
+            Nd4j.getExecutioner().exec(new BroadcastMulOp(inToBeMasked, maskArray, inToBeMasked, 0, 3));
+
+
+            net.setLayerMaskArrays(maskArray, null);
+
+            INDArray outMasked = net.output(inToBeMasked);
+            net.clearLayerMaskArrays();
+
+            int numSteps = width - 1;
+            INDArray subset = inToBeMasked.get(NDArrayIndex.interval(0,0,true), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.interval(0,numSteps));
+            assertArrayEquals(new int[]{1,depthIn, height, 5}, subset.shape());
+
+            INDArray outSubset = net.output(subset);
+            INDArray outMaskedSubset = outMasked.getRow(0);
+
+            assertEquals(outSubset, outMaskedSubset);
+
+            //Finally: check gradient calc for exceptions
+            net.setLayerMaskArrays(maskArray, null);
+            net.setInput(inToBeMasked);
+            INDArray labels = Nd4j.create(new double[]{0,1});
+            net.setLabels(labels);
+
+            net.computeGradientAndScore();
+        }
+    }
+
+    @Test
+    public void testMaskingCnnDim2_SingleExample(){
+        //Test masking, where mask is along dimension 2
+
+        int minibatch = 1;
+        int depthIn = 2;
+        int depthOut = 2;
+        int nOut = 2;
+        int height = 6;
+        int width = 3;
+
+        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.SUM, PoolingType.AVG, PoolingType.MAX, PoolingType.PNORM};
+
+        for(PoolingType pt : poolingTypes) {
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .weightInit(WeightInit.XAVIER)
+                    .convolutionMode(ConvolutionMode.Same)
+                    .seed(12345L)
+                    .list()
+                    .layer(0, new ConvolutionLayer.Builder().nIn(depthIn).nOut(depthOut).kernelSize(2, width).stride(1,width)
+                            .activation(Activation.TANH).build())
+                    .layer(1, new org.deeplearning4j.nn.conf.layers.GlobalPoolingLayer.Builder().poolingType(pt).build())
+                    .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(depthOut).nOut(nOut).build())
+                    .pretrain(false).backprop(true)
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            INDArray inToBeMasked = Nd4j.rand(new int[]{minibatch, depthIn, height, width});
+
+            //Shape for mask: [minibatch, width]
+            INDArray maskArray = Nd4j.create(new double[]{1, 1, 1, 1, 1, 0});
+
+            //Multiply the input by the mask array, to ensure the 0s in the mask correspond to 0s in the input vector
+            // as would be the case in practice...
+            Nd4j.getExecutioner().exec(new BroadcastMulOp(inToBeMasked, maskArray, inToBeMasked, 0, 2));
+
+
+            net.setLayerMaskArrays(maskArray, null);
+
+            INDArray outMasked = net.output(inToBeMasked);
+            net.clearLayerMaskArrays();
+
+            int numSteps = height - 1;
+            INDArray subset = inToBeMasked.get(NDArrayIndex.interval(0,0,true), NDArrayIndex.all(), NDArrayIndex.interval(0,numSteps), NDArrayIndex.all());
+            assertArrayEquals(new int[]{1,depthIn, 5, width}, subset.shape());
+
+            INDArray outSubset = net.output(subset);
+            INDArray outMaskedSubset = outMasked.getRow(0);
+
+            assertEquals(outSubset, outMaskedSubset);
+
+            //Finally: check gradient calc for exceptions
+            net.setLayerMaskArrays(maskArray, null);
+            net.setInput(inToBeMasked);
+            INDArray labels = Nd4j.create(new double[]{0,1});
+            net.setLabels(labels);
+
+            net.computeGradientAndScore();
+        }
+    }
+
+
+    @Test
+    public void testMaskingCnnDim3(){
+        //Test masking, where mask is along dimension 3
+
+        int minibatch = 3;
+        int depthIn = 3;
+        int depthOut = 4;
+        int nOut = 5;
+        int height = 3;
+        int width = 6;
+
+        PoolingType[] poolingTypes = new PoolingType[]{PoolingType.SUM, PoolingType.AVG, PoolingType.MAX, PoolingType.PNORM};
+
+        for(PoolingType pt : poolingTypes) {
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .weightInit(WeightInit.XAVIER)
+                    .convolutionMode(ConvolutionMode.Same)
+                    .seed(12345L)
+                    .list()
+                    .layer(0, new ConvolutionLayer.Builder().nIn(depthIn).nOut(depthOut)
+                            .kernelSize(height, 2).stride(height,1)
+                            .activation(Activation.TANH).build())
+                    .layer(1, new org.deeplearning4j.nn.conf.layers.GlobalPoolingLayer.Builder().poolingType(pt).build())
+                    .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT).activation(Activation.SOFTMAX).nIn(depthOut).nOut(nOut).build())
+                    .pretrain(false).backprop(true)
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            INDArray inToBeMasked = Nd4j.rand(new int[]{minibatch, depthIn, height, width});
+
+            //Shape for mask: [minibatch, width]
+            INDArray maskArray = Nd4j.create(new double[][]{
+                    {1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 0},
+                    {1, 1, 1, 1, 0, 0}});
+
+            //Multiply the input by the mask array, to ensure the 0s in the mask correspond to 0s in the input vector
+            // as would be the case in practice...
+            Nd4j.getExecutioner().exec(new BroadcastMulOp(inToBeMasked, maskArray, inToBeMasked, 0, 3));
+
+
+            net.setLayerMaskArrays(maskArray, null);
+
+            INDArray outMasked = net.output(inToBeMasked);
+            net.clearLayerMaskArrays();
+
+            for( int i=0; i<minibatch; i++ ){
+                System.out.println(i);
+                int numSteps = width - i;
+                INDArray subset = inToBeMasked.get(NDArrayIndex.interval(i,i,true), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.interval(0,numSteps));
+                assertArrayEquals(new int[]{1,depthIn, height, width-i}, subset.shape());
+
+                INDArray outSubset = net.output(subset);
+                INDArray outMaskedSubset = outMasked.getRow(i);
+
+                assertEquals(outSubset, outMaskedSubset);
+            }
+        }
+    }
 }
