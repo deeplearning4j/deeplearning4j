4b4c5a9001aafc9bfe43f9eaefdab4a7a6c15864
eraly <susan.eraly@gmail.com>
2017-01-24 18:48:27 -0800

    assert same shape

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/LoneTest.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/LoneTest.java
@@ -148,4 +148,13 @@ public class LoneTest extends BaseNd4jTest {
         assertTrue(a.tensorAlongDimension(0,2,3).rank() == 2); //is rank 3 with an extra 1
     }
 
+    @Test
+    public void opsNotAllowed() {
+        INDArray A = Nd4j.ones(2,3,1);
+        INDArray B = Nd4j.ones(2,3);
+
+        System.out.println(A.add(B));
+        System.out.println(B.add(A));
+
+    }
 }
