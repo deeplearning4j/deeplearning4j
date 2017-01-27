f97ed38e87a8c222aef46c02e35445d5e13a7f7e
Alex Black <blacka101@gmail.com>
2017-01-27 23:46:35 +1100

    First steps for CNN sentence classification iterator

---
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/iterator/LabelledSentenceProvider.java
@@ -0,0 +1,25 @@
+package org.deeplearning4j.iterator;
+
+import org.deeplearning4j.berkeley.Pair;
+
+import java.util.List;
+
+/**
+ * Created by Alex on 27/01/2017.
+ */
+public interface LabelledSentenceProvider {
+
+
+    boolean hasNext();
+
+    /**
+     *
+     * @return Pair: sentence text and label
+     */
+    Pair<String,String> nextSentence();
+
+    List<String> allLabels();
+
+    int numLabels();
+
+}
