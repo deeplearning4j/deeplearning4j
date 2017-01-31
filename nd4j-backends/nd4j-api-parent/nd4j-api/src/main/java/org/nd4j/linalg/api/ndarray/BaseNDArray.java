ccb2ca936ebd316afcd6ee3910ea245eb7aad04d
Adam Gibson <adam@skymind.io>
2017-01-31 21:02:01 +0900

    tad offset fix

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ndarray/BaseNDArray.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ndarray/BaseNDArray.java
@@ -838,7 +838,7 @@ public abstract class BaseNDArray implements INDArray, Iterable {
             ews = -1;
 
         // we create new shapeInfo with possibly new ews & order
-        baseNDArray.setShapeInformation(Nd4j.getShapeInfoProvider().createShapeInformation(shape, stride, 0, ews, newOrder));
+        baseNDArray.setShapeInformation(Nd4j.getShapeInfoProvider().createShapeInformation(shape, stride, offset, ews, newOrder));
 
         return toTad;
     }
