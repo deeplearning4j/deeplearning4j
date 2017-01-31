5732d7399cb570135b6c375dc26cf8a002276312
Lukasz Jastrzebski <lukasz.jastrzebski@nokia.com>
2017-01-31 10:38:59 -0800

    Making sure maven properties are registered with proper versions
    Former-commit-id: 97f90759e2d903f8ddbf409633bf1135fa32060e [formerly ca5a96f0b7bb36c9ada298feefe966fb8c9d4c43]
    Former-commit-id: 0abdd71ec1c6533625355e1f32162d2b4fa10823

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -2,8 +2,10 @@
 set -eu
 ./change-scala-versions.sh 2.10
 mvn "$@"
+./change-spark-versions.sh 2
 mvn -Dspark.major.version=2 "$@"
 ./change-scala-versions.sh 2.11
-mvn "$@"
 mvn -Dspark.major.version=2 "$@"
+./change-spark-versions.sh 1
+mvn "$@"
 ./change-scala-versions.sh 2.10
