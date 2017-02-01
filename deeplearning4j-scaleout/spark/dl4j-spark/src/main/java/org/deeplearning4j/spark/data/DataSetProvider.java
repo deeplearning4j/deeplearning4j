ae25886167c2d388f6ced5338ff2d6b06af6c045
Adam Gibson <adam@skymind.io>
2017-02-01 23:26:33 +0900

    dataset provider interface
    Former-commit-id: f3d2b1f3e5ed2780f8c1a6ee2c2c9399a3c2cb80 [formerly 58537005cd8d80335cc9e7858e75664549fb878a]
    Former-commit-id: 651cb4f6b38525b8402131869563ae9590189948

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/DataSetProvider.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/DataSetProvider.java
@@ -1,7 +1,19 @@
 package org.deeplearning4j.spark.data;
+import org.apache.spark.SparkContext;
+import org.apache.spark.rdd.RDD;
+import org.nd4j.linalg.dataset.DataSet;
 
 /**
- * Created by agibsonccc on 2/1/17.
+ * A provider for an {@link DataSet}
+ * rdd.
+ * @author Adam Gibson
  */
-public class DataSetProvider {
+public interface DataSetProvider {
+
+
+    /**
+     * Return an rdd of type dataset
+     * @return
+     */
+    RDD<DataSet> data(SparkContext sparkContext);
 }
