804da569791d83ee0dace652ffdb74e218a741ae
Adam Gibson <adam@skymind.io>
2017-02-04 10:52:01 +0900

    change array to bas64

--- nd4j-serde/nd4j-jackson/src/main/java/org/nd4j/shade/serde/jackson/shaded/NDArrayDeSerializer.java
+++ nd4j-serde/nd4j-jackson/src/main/java/org/nd4j/shade/serde/jackson/shaded/NDArrayDeSerializer.java
@@ -2,6 +2,7 @@ package org.nd4j.shade.serde.jackson.shaded;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.serde.base64.Nd4jBase64;
 import org.nd4j.shade.jackson.core.JsonParser;
 import org.nd4j.shade.jackson.databind.DeserializationContext;
 import org.nd4j.shade.jackson.databind.JsonDeserializer;
@@ -18,9 +19,9 @@ public class NDArrayDeSerializer extends JsonDeserializer<INDArray> {
     @Override
     public INDArray deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jp.getCodec().readTree(jp);
-        String raw = node.asText();
-        ByteArrayInputStream bis = new ByteArrayInputStream(raw.getBytes());
-        INDArray ret = Nd4j.readTxtString(bis);
+        String field = node.get("array").asText();
+        INDArray ret = Nd4jBase64.fromBase64(field);
         return ret;
+
     }
 }
