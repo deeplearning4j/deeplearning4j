d12cc1fa10aa121d43fdc1015db6fc8ebef885e6
eraly <susan.eraly@gmail.com>
2017-02-04 20:36:43 -0800

    wip - transfer learning builder cleaning up

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/multilayer/MLNTransferLearning.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/multilayer/MLNTransferLearning.java
@@ -1,16 +1,19 @@
 package org.deeplearning4j.nn.multilayer;
 
-import org.deeplearning4j.nn.conf.BackpropType;
-import org.deeplearning4j.nn.conf.InputPreProcessor;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Triple;
+import org.deeplearning4j.nn.conf.*;
 import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.Layer;
+import org.deeplearning4j.nn.layers.FrozenLayer;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.shade.jackson.databind.JsonNode;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,29 +21,12 @@ import java.util.Map;
 
 
 /**
- * The API usage(WIP):
- *
- *  MLNTransferLearning.Builder(MultiLayerNetwork origModel)
-     .setFeatureExtractor(int layerNum)
-            //which layerNum and below to freeze, probably better to take a negative number so you can say "freeze everything but the last n layers"
-     .finetuneConfiguration(NeuralNetConfiguration.Builder newDefaultConfBuilder)
-            //same as the global config in neuralnetconfiguration - will override whatever is in the origModel's config
-     .nOutReplace(int layerNum, int nOut, WeightInit scheme)
-            //helper to tweak nOut of a given layer
-     .nInReplace(int layerNum, WeightInit scheme)
-            //helper to set weight init scheme for the next layer
-     .popFromLayer(int layerNum)
-            //will delete all layers from this to output
-     .addLayer(Layer layer)
-            // each layer must be explicitly added and will use the "global config" from finetuneConfigu..
-     .build()
-            //will return a MLN, init with a view of the olderParams+whatever newer params
- Other things to consider:
- - There really should be a way to featurize and save to disk and then train from the featurized data. This will help users iterate quicker and
- get a "nano net" that converges fast and then they can "fineTune" to their heart's content without wondering about having disruptive gradients
- flowing backward to the unfrozen layers.
- - And then adapting this for computation graphs (yikes)
- - Also a summary of the model before and after to show how many new params were added/deleted and how many are learnable and how many are frozen etc..
+ * Other things to consider:
+ * - There really should be a way to featurize and save to disk and then train from the featurized data. This will help users iterate quicker and
+ * get a "nano net" that converges fast and then they can "fineTune" to their heart's content without wondering about having disruptive gradients
+ * flowing backward to the unfrozen layers.
+ * - And then adapting this for computation graphs (yikes)
+ * - Also a summary of the model before and after to show how many new params were added/deleted and how many are learnable and how many are frozen etc..
  */
 public class MLNTransferLearning {
 
@@ -50,18 +36,21 @@ public class MLNTransferLearning {
     public static class Builder {
 
         private INDArray origParams;
-        private int tillIndex;
-        private List<INDArray> appendParams; //these could be new arrays, and views from origParams
+        private int tillIndex = -1;
+        private List<INDArray> appendParams = new ArrayList<>(); //these could be new arrays, and views from origParams
         private MultiLayerConfiguration origConf;
         private MultiLayerNetwork origModel;
         private MultiLayerNetwork editedModel;
         private NeuralNetConfiguration.Builder globalConfig;
 
-        private int frozenTill = 0;
-
-        protected List<NeuralNetConfiguration> confs = new ArrayList<>();
+        private int frozenTill = -1;
+        private int popFrom = -1;
+        private boolean prepDone = false;
+        private List<Integer> editedLayers = new ArrayList<>();
+        private Map<Integer, Triple<Integer,WeightInit,WeightInit>> editedLayersMap = new HashMap<>();
         private List<NeuralNetConfiguration> editedConfs = new ArrayList<>();
-        protected Map<Integer,InputPreProcessor> inputPreProcessors = new HashMap<>();
+
+        protected Map<Integer, InputPreProcessor> inputPreProcessors = new HashMap<>();
         protected boolean pretrain = false;
         protected boolean backprop = true;
         protected BackpropType backpropType = BackpropType.Standard;
@@ -69,22 +58,29 @@ public class MLNTransferLearning {
         protected int tbpttBackLength = 20;
         protected InputType inputType;
 
-        private Builder(MultiLayerConfiguration origConf) {
-            this.origConf = origConf;
-            this.confs = origConf.getConfs();
+        public Builder(MultiLayerNetwork origModel) {
+
+            this.origModel = origModel;
+            this.origConf = origModel.getLayerWiseConfigurations();
+            this.origParams = origModel.params();
+
             this.inputPreProcessors = origConf.getInputPreProcessors();
-            this.pretrain = origConf.isPretrain();
-            this.backprop = origConf.isBackprop();
             this.backpropType = origConf.getBackpropType();
             this.tbpttFwdLength = origConf.getTbpttFwdLength();
             this.tbpttBackLength = origConf.getTbpttBackLength();
-            this.inputType = origConf.getInputType(); //FIXME:no getter
+            //this.inputType = new InputType.Type()?? //FIXME
+
+            this.editedConfs.addAll(origConf.getConfs());
         }
 
-        public Builder(MultiLayerNetwork origModel) {
-            this(origModel.getLayerWiseConfigurations());
-            this.origParams  = origModel.params();
-            this.origModel = origModel;
+        public Builder setTbpttFwdLength(int l) {
+            this.tbpttFwdLength = l;
+            return this;
+        }
+
+        public Builder setTbpttBackLength(int l) {
+            this.tbpttBackLength = l;
+            return this;
         }
 
         public Builder setFeatureExtractor(int layerNum) {
@@ -94,80 +90,166 @@ public class MLNTransferLearning {
 
         public Builder finetuneConfiguration(NeuralNetConfiguration.Builder newDefaultConfBuilder) {
             this.globalConfig = newDefaultConfBuilder;
-            org.deeplearning4j.nn.api.Layer [] layers = origModel.getLayers();
-            for (int i=0;i<layers.length;i++) {
-                NeuralNetConfiguration layerConf = origModel.getLayerWiseConfigurations().getConf(i);
-                Layer layerImpl = layerConf.getLayer();
-                // for_every_param
-                //layerConf.setLearningRateByParam();
-                //FIXME - grab each of these and override in the layerImpl like this or even better clear these from the layerImpl and then??
-                layerImpl.setUpdater(??);
-                layerImpl.setMomentum(??);
-                //FIXME - instantiate a dummy layer and then grab config and set in the editedConf list??
-                INDArray dummyParams = Nd4j.create(1,layerImpl.initializer().numParams(layerConf);
-                org.deeplearning4j.nn.api.Layer dummyLayer = layerImpl.instantiate(layerConf,null,0,dummyParams,true);
-                editedConf.set(i,dummyLayer.conf());
+            return this;
+        }
+
+        public Builder noutReplace(int layerNum, int nOut, WeightInit scheme) {
+            nOutReplace(layerNum, nOut, scheme, scheme);
+            return this;
+        }
+
+        public Builder nOutReplace(int layerNum, int nOut, WeightInit scheme, WeightInit schemeNext) {
+            editedLayersMap.put(layerNum,new ImmutableTriple<>(nOut,scheme,schemeNext));
+            return this;
+        }
+
+        public Builder popOutputLayer() {
+            popFrom = origConf.getConfs().size()-1;
+            return this;
+        }
+
+        public Builder popFromOutput(int layerNum) {
+            if(popFrom == -1) {
+                popFrom = layerNum;
             }
-            return this;
-        }
-
-        public Builder nOutReplace(int layerNum, int nOut, WeightInit scheme) {
-            INDArray appendParams;
-            org.deeplearning4j.nn.api.Layer [] layers = origModel.getLayers();
-            int paramCount = 0;
-            for (int i=0;i<layers.length;i++) {
-                if (i == layerNum) {
-                    NeuralNetConfiguration layerConf = origModel.getLayerWiseConfigurations().getConf(i);
-                    Layer layerImpl = layerConf.getLayer();
-                    //FIXME - layerConf gets edited for nIn and nOut setters, keep it as package private
-                    layerImpl.setnIn(..);
-                    layerImpl.setnOut(nOut);
-                    layerImpl.setWeightInit(scheme);
-                    int numParams = layerImpl.initializer().numParams(layerConf);
-                    INDArray params = Nd4j.create(1, numParams);
-                    org.deeplearning4j.nn.api.Layer someLayer = layerImpl.instantiate(layerConf,null,0,params,true);
-                    appendParams.add(params); //FIXLATER:does this need to be a list or something
-
-                }
-                else if (i < layerNum){
-                    paramCount += origModel.getLayer(i).params().length();
-                }
-            }
-            return this;
-        }
-
-        public Builder nInReplace(int layerNum, WeightInit scheme) {
-            //same idea as above - the editConfs get updated and the appendParams get updated
-            return this;
-        }
-
-
-        public Builder popFromLayer(int layerNum) {
-            for (int i=layerNum;i<origConf.getConfs().size();i++) {
-                editedConfs.remove(i);
+            else {
+                throw new IllegalArgumentException("Pop from can only be called once");
             }
             return this;
         }
 
         public Builder addLayer(Layer layer) {
-            // Use the fineTune NeuralNetConfigurationBuilder and the layerConf to get a N
+
+            if(!prepDone) {
+                doPrep();
+            }
+            // Use the fineTune NeuralNetConfigurationBuilder and the layerConf to get the NeuralNetConfig
+            //instantiate dummy layer to get the params
             editedConfs.add(globalConfig.clone().layer(layer).build());
-            //instantiate dummy layer
-            org.deeplearning4j.nn.api.Layer dummyLayer = editedConfs.get(editedConfs.size()-1).getLayer().instantiate(globalConfig.build(),....)
+            org.deeplearning4j.nn.api.Layer dummyLayer = editedConfs.get(editedConfs.size() - 1).getLayer().instantiate(globalConfig.build(),....)
             appendParams.add(dummyLayer.params());
             return this;
         }
 
+        private void doPrep() {
+            //first set finetune configs on all layers in model
+            finetuneConfigurationBuild();
+            //apply changes to nout/nin if any in sorted order
+            if(!editedLayers.isEmpty()) {
+
+
+            }
+        }
+
+
+        private void finetuneConfigurationBuild() {
+
+            for (int i = 0; i < origConf.getConfs().size(); i++) {
+
+                NeuralNetConfiguration layerConf = origConf.getConf(i);
+                Layer layerConfImpl = layerConf.getLayer();
+
+                //clear the learning related params for all layers in the origConf and set to defaults
+                layerConfImpl.setUpdater(null);
+                layerConfImpl.setMomentum(Double.NaN);
+                layerConfImpl.setWeightInit(null);
+                layerConfImpl.setBiasInit(Double.NaN);
+                layerConfImpl.setDist(null);
+                layerConfImpl.setLearningRate(Double.NaN);
+                layerConfImpl.setBiasLearningRate(Double.NaN);
+                layerConfImpl.setLearningRateSchedule(null);
+                layerConfImpl.setMomentumSchedule(null);
+                layerConfImpl.setL1(Double.NaN);
+                layerConfImpl.setL2(Double.NaN);
+                layerConfImpl.setDropOut(Double.NaN);
+                layerConfImpl.setRho(Double.NaN);
+                layerConfImpl.setEpsilon(Double.NaN);
+                layerConfImpl.setRmsDecay(Double.NaN);
+                layerConfImpl.setAdamMeanDecay(Double.NaN);
+                layerConfImpl.setAdamVarDecay(Double.NaN);
+                layerConfImpl.setGradientNormalization(GradientNormalization.None);
+                layerConfImpl.setGradientNormalizationThreshold(1.0);
+
+                editedConfs.set(i, globalConfig.clone().layer(layerConfImpl).build());
+            }
+        }
+
+        private void nOutReplaceBuild(int layerNum, int nOut, WeightInit scheme, WeightInit schemeNext) {
+
+            for (int i = 0; i < editedConfs.size(); i++) {
+                NeuralNetConfiguration layerConf = editedConfs.get(i);
+                Layer layerImpl = layerConf.getLayer();
+
+                if (i < layerNum && tillIndex < 0) {
+                    //first edit to architecture
+                    tillIndex += layerImpl.initializer().numParams(layerConf);
+                }
+                else if (i == layerNum) {
+                    layerImpl.setWeightInit(scheme);
+                    String layerConfJson = layerConf.toJson();
+                    NeuralNetConfiguration newLayerConf = layerConf.clone();
+                    //FIXME: This is a hack via json to change nOut, Could expose nIn and nOut via setters instead?
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        JsonNode root = mapper.readTree(layerConfJson);
+                        JsonNode layerRoot = root.path("layer").path(root.path("layer").fieldNames().next());
+                        ((org.nd4j.shade.jackson.databind.node.ObjectNode) layerRoot).put("nout", nOut);
+                        newLayerConf = mapper.readValue(root.toString(), NeuralNetConfiguration.class);
+                        editedConfs.set(i, newLayerConf);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    int numParams = layerImpl.initializer().numParams(newLayerConf);
+                    INDArray params = Nd4j.create(1, numParams);
+                    org.deeplearning4j.nn.api.Layer someLayer = layerImpl.instantiate(layerConf, null, 0, params, true);
+                    appendParams.add(someLayer.params());
+
+                }
+                else if (i == layerNum + 1) {
+                    layerImpl.setWeightInit(schemeNext);
+                    String layerConfJson = layerConf.toJson();
+                    NeuralNetConfiguration newLayerConf = layerConf.clone();
+                    //FIXME: This is a hack via json to change nIn, Could expose nIn and nOut via setters instead?
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        JsonNode root = mapper.readTree(layerConfJson);
+                        JsonNode layerRoot = root.path("layer").path(root.path("layer").fieldNames().next());
+                        ((org.nd4j.shade.jackson.databind.node.ObjectNode) layerRoot).put("nin", nOut);
+                        newLayerConf = mapper.readValue(root.toString(), NeuralNetConfiguration.class);
+                        editedConfs.set(i, newLayerConf);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    int numParams = layerImpl.initializer().numParams(newLayerConf);
+                    INDArray params = Nd4j.create(1, numParams);
+                    org.deeplearning4j.nn.api.Layer someLayer = layerImpl.instantiate(layerConf, null, 0, params, true);
+                    appendParams.add(someLayer.params());
+                }
+                else {
+                    org.deeplearning4j.nn.api.Layer origLayer = origModel.getLayer(i);
+                    appendParams.add(origLayer.params());
+                }
+            }
+        }
+
+        public void popFromOutputToBuild(int layerNum) {
+            for (int i = layerNum; i < origConf.getConfs().size(); i++) {
+                editedConfs.remove(i);
+            }
+        }
+
+        //unchecked?
         public MultiLayerNetwork build() {
             if (editConf() == null) {
                 editedModel = origModel;
+            } else {
+                editedModel = new MultiLayerNetwork(editConf(), constructParams());
             }
-            else {
-                editedModel = new MultiLayerNetwork(editConf(),constructParams());
-            }
-            //editedModel.setParams(constructParams());
-            org.deeplearning4j.nn.api.Layer [] layers = editedModel.getLayers();
-            for (int i=frozenTill;i>=0;i--) {
+
+            org.deeplearning4j.nn.api.Layer[] layers = editedModel.getLayers();
+            for (int i = frozenTill; i >= 0; i--) {
                 layers[i] = new FrozenLayer(layers[i]);
             }
             editedModel.setLayers(layers);
@@ -175,9 +257,9 @@ public class MLNTransferLearning {
         }
 
         private INDArray constructParams() {
-            INDArray keepView = origParams.get(NDArrayIndex.point(0),NDArrayIndex.interval(0,tillIndex,true));
+            INDArray keepView = origParams.get(NDArrayIndex.point(0), NDArrayIndex.interval(0, tillIndex, true));
             INDArray appendView = Nd4j.hstack(appendParams);
-            return Nd4j.hstack(keepView,appendView);
+            return Nd4j.hstack(keepView, appendView);
         }
 
         private MultiLayerConfiguration editConf() {
