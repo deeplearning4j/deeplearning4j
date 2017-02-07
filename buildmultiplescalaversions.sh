546e1094fc8d70d91b239e63ce97b19f398556f7
Lukasz Jastrzebski <lukasz.jastrzebski@nokia.com>
2017-02-07 10:16:13 -0800

    Removing Spark 2.x from Scala 2.10 build

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -2,10 +2,9 @@
 set -eu
 ./change-scala-versions.sh 2.10
 mvn "$@"
+./change-scala-versions.sh 2.11
+mvn "$@"
 ./change-spark-versions.sh 2
 mvn -Dspark.major.version=2 "$@"
-./change-scala-versions.sh 2.11
-mvn -Dspark.major.version=2 "$@"
 ./change-spark-versions.sh 1
-mvn "$@"
 ./change-scala-versions.sh 2.10
