715c7140a77caf58eeae7171f53f54ab5f7f691e
Lukasz Jastrzebski <lukasz.jastrzebski@nokia.com>
2017-02-09 11:59:15 -0800

    Updating to default Scala version 2.11
    Former-commit-id: ed1d1e038e10f1905f08d258a9156187711d6449 [formerly d52849b2afb5d6667a18bf7db190bd4433bfd324]
    Former-commit-id: 9c46c54978d77f476d169753201bf0bbbba9fa5b

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 set -eu
-./change-scala-versions.sh 2.10
-mvn "$@"
-./change-scala-versions.sh 2.11
 mvn "$@"
 ./change-spark-versions.sh 2
 mvn -Dspark.major.version=2 "$@"
-./change-spark-versions.sh 1
 ./change-scala-versions.sh 2.10
+./change-spark-versions.sh 1
+mvn "$@"
+./change-scala-versions.sh 2.11
