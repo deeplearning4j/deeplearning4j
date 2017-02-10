ee747975da163b806a6c2f3a3e21af3c9245289a
Lukasz Jastrzebski <lukasz.jastrzebski@nokia.com>
2017-02-10 11:50:03 -0800

    Moving streaming code in the common place
    Former-commit-id: dd58f049c09a268c1018e976b45d722eb5b7a72f [formerly eac349688a6cdaf74885643a40b14ab8e59c9903]
    Former-commit-id: 694d4fea3e682ca91dc9576534a3f094b65bc5df

--- deeplearning4j-scaleout/dl4j-streaming/src/main/spark-2/org/deeplearning4j/streaming/pipeline/spark/StreamingContextUtils.java
+++ deeplearning4j-scaleout/dl4j-streaming/src/main/spark-2/org/deeplearning4j/streaming/pipeline/spark/StreamingContextUtils.java
@@ -3,23 +3,11 @@ package org.deeplearning4j.streaming.pipeline.spark;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 /**
  * In order to handle changes between Spark 1.x and 2.x
  */
 public class StreamingContextUtils {
-
-    public static void awaitTermination(JavaStreamingContext jssc, long timeout) {
-        try {
-            if(timeout < 0)
-                jssc.awaitTermination();
-            else
-                jssc.awaitTerminationOrTimeout(timeout);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static <K> void foreach(JavaDStream<K> stream, VoidFunction<JavaRDD<K>> func) {
         stream.foreachRDD(func);
