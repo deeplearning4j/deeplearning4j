5a9758401a00469c972260b0ddb753cf33d34c39
Alex Black <blacka101@gmail.com>
2017-02-17 21:42:26 +1100

    Add bias l1/l2 test; fix conditions for l1/l2 calc to allow bias-only l1/l2
    Former-commit-id: c08a5d054342f8aed1fae77364fa30751f08c643

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
@@ -428,7 +428,7 @@ public abstract class BaseLayer<LayerConfT extends org.deeplearning4j.nn.conf.la
 
     @Override
     public double calcL2(boolean backpropParamsOnly) {
-    	if(!conf.isUseRegularization() || conf.getLayer().getL2() <= 0.0 ) return 0.0;
+    	if(!conf.isUseRegularization() ) return 0.0;
 
         //L2 norm: sqrt( sum_i x_i^2 ) -> want sum squared weights, so l2 norm squared
         double l2Sum = 0.0;
@@ -445,7 +445,7 @@ public abstract class BaseLayer<LayerConfT extends org.deeplearning4j.nn.conf.la
 
     @Override
     public double calcL1(boolean backpropParamsOnly) {
-    	if(!conf.isUseRegularization() || conf.getLayer().getL1()  <= 0.0 ) return 0.0;
+    	if(!conf.isUseRegularization() ) return 0.0;
         double l1Sum = 0.0;
         if(conf.getL1ByParam(DefaultParamInitializer.WEIGHT_KEY) > 0.0){
             l1Sum += conf.getL1ByParam(DefaultParamInitializer.WEIGHT_KEY) * getParam(DefaultParamInitializer.WEIGHT_KEY).norm1Number().doubleValue();
