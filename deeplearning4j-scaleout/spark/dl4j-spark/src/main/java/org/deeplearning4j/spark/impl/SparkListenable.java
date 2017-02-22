d02ce3b1afa6a87b91db5feea68a4ff515687ee5
François Garillot <francois@skymind.io>
2017-02-21 21:34:05 -0500

    Reformat SparkListenable, @Slf4j it.
    Former-commit-id: d3535b82a1bbb2defd2482356989d0673e0bc77d [formerly bbc3bed33be40bc89d2b552d0c1d80fe10092132]
    Former-commit-id: bda2f4b39b8a18f005ce5ceb221c8ab35f47bdac

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/SparkListenable.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/impl/SparkListenable.java
@@ -1,6 +1,7 @@
 package org.deeplearning4j.spark.impl;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.deeplearning4j.api.storage.StatsStorageRouter;
 import org.deeplearning4j.api.storage.StatsStorageRouterProvider;
 import org.deeplearning4j.api.storage.listener.RoutingIterationListener;
@@ -17,79 +18,82 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-/**
- * Created by huitseeker on 2/15/17.
- */
+/** Created by huitseeker on 2/15/17. */
+@Slf4j
 public class SparkListenable implements Serializable {
-    protected static final Logger log = LoggerFactory.getLogger(SparkComputationGraph.class);
-    protected TrainingMaster trainingMaster;
-    private List<IterationListener> listeners = new ArrayList<>();
 
-    /**
-     * This method allows you to specify IterationListeners for this model.
-     *
-     * @param listeners Iteration listeners
-     */
-    public void setListeners(@NonNull Collection<IterationListener> listeners) {
-        this.listeners.clear();
-        this.listeners.addAll(listeners);
-        if (trainingMaster != null) trainingMaster.setListeners(this.listeners);
-    }
+  protected TrainingMaster trainingMaster;
+  private List<IterationListener> listeners = new ArrayList<>();
 
-    /**
-     * This method allows you to specify IterationListeners for this model.
-     * Note that for listeners like StatsListener (that have state that will be sent somewhere), consider instead
-     * using {@link #setListeners(StatsStorageRouter, Collection)}
-     *
-     * @param listeners    Listeners to set
-     */
-    public void setListeners(@NonNull IterationListener... listeners) {
-        setListeners(Arrays.asList(listeners));
-    }
+  /**
+   * This method allows you to specify IterationListeners for this model.
+   *
+   * @param listeners Iteration listeners
+   */
+  public void setListeners(@NonNull Collection<IterationListener> listeners) {
+    this.listeners.clear();
+    this.listeners.addAll(listeners);
+    if (trainingMaster != null) trainingMaster.setListeners(this.listeners);
+  }
 
-    /**
-     * Set the listeners, along with a StatsStorageRouter that the results will be shuffled to (in the case of any listeners
-     * that implement the {@link RoutingIterationListener} interface)
-     *
-     * @param statsStorage Stats storage router to place the results into
-     * @param listeners    Listeners to set
-     */
-    public void setListeners(StatsStorageRouter statsStorage, IterationListener... listeners) {
-        setListeners(statsStorage, Arrays.asList(listeners));
-    }
+  /**
+   * This method allows you to specify IterationListeners for this model. Note that for listeners
+   * like StatsListener (that have state that will be sent somewhere), consider instead using {@link
+   * #setListeners(StatsStorageRouter, Collection)}
+   *
+   * @param listeners Listeners to set
+   */
+  public void setListeners(@NonNull IterationListener... listeners) {
+    setListeners(Arrays.asList(listeners));
+  }
 
-    /**
-     * Set the listeners, along with a StatsStorageRouter that the results will be shuffled to (in the case of any listeners
-     * that implement the {@link RoutingIterationListener} interface)
-     *
-     * @param statsStorage Stats storage router to place the results into
-     * @param listeners    Listeners to set
-     */
-    public void setListeners(StatsStorageRouter statsStorage, Collection<? extends IterationListener> listeners) {
-        //Check if we have any RoutingIterationListener instances that need a StatsStorage implementation...
-        StatsStorageRouterProvider routerProvider = null;
-        if(listeners != null ){
-            for(IterationListener l : listeners){
-                if(l instanceof RoutingIterationListener){
-                    RoutingIterationListener rl = (RoutingIterationListener)l;
-                    if(rl.getStorageRouter() == null){
-                        log.warn("RoutingIterationListener provided without providing any StatsStorage instance. Iterator may not function without one. Listener: {}", l);
-                    } else if(!(rl.getStorageRouter() instanceof Serializable)){
-                        //Spark would throw a (probably cryptic) serialization exception later anyway...
-                        throw new IllegalStateException("RoutingIterationListener provided with non-serializable storage router");
-                    }
+  /**
+   * Set the listeners, along with a StatsStorageRouter that the results will be shuffled to (in the
+   * case of any listeners that implement the {@link RoutingIterationListener} interface)
+   *
+   * @param statsStorage Stats storage router to place the results into
+   * @param listeners Listeners to set
+   */
+  public void setListeners(StatsStorageRouter statsStorage, IterationListener... listeners) {
+    setListeners(statsStorage, Arrays.asList(listeners));
+  }
 
-                    //Need to give workers a router provider...
-                    if(routerProvider == null){
-                        routerProvider = new VanillaStatsStorageRouterProvider();
-                    }
-                }
-            }
+  /**
+   * Set the listeners, along with a StatsStorageRouter that the results will be shuffled to (in the
+   * case of any listeners that implement the {@link RoutingIterationListener} interface)
+   *
+   * @param statsStorage Stats storage router to place the results into
+   * @param listeners Listeners to set
+   */
+  public void setListeners(
+      StatsStorageRouter statsStorage, Collection<? extends IterationListener> listeners) {
+    //Check if we have any RoutingIterationListener instances that need a StatsStorage implementation...
+    StatsStorageRouterProvider routerProvider = null;
+    if (listeners != null) {
+      for (IterationListener l : listeners) {
+        if (l instanceof RoutingIterationListener) {
+          RoutingIterationListener rl = (RoutingIterationListener) l;
+          if (rl.getStorageRouter() == null) {
+            log.warn(
+                "RoutingIterationListener provided without providing any StatsStorage instance. Iterator may not function without one. Listener: {}",
+                l);
+          } else if (!(rl.getStorageRouter() instanceof Serializable)) {
+            //Spark would throw a (probably cryptic) serialization exception later anyway...
+            throw new IllegalStateException(
+                "RoutingIterationListener provided with non-serializable storage router");
+          }
+
+          //Need to give workers a router provider...
+          if (routerProvider == null) {
+            routerProvider = new VanillaStatsStorageRouterProvider();
+          }
         }
-        this.listeners.clear();
-        if(listeners != null) {
-            this.listeners.addAll(listeners);
-            if (trainingMaster != null) trainingMaster.setListeners(statsStorage, this.listeners);
-        }
+      }
     }
+    this.listeners.clear();
+    if (listeners != null) {
+      this.listeners.addAll(listeners);
+      if (trainingMaster != null) trainingMaster.setListeners(statsStorage, this.listeners);
+    }
+  }
 }
