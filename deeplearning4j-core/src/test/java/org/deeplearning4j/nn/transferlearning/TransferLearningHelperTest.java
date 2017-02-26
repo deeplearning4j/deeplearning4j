b87d1b94977728030dc3e2806545cf6da5c3d68e
eraly <susan.eraly@gmail.com>
2017-02-26 11:02:13 -0800

    helpers for saving to disk and fitting from featurized
    Former-commit-id: 2087d5c5d09bfb3dcccc8a0cbab143fbc2b8fb05

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/transferlearning/TransferLearningHelperTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/transferlearning/TransferLearningHelperTest.java
@@ -11,6 +11,9 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.junit.Test;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.MultiDataSet;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import static org.junit.Assert.assertEquals;
@@ -91,5 +94,68 @@ public class TransferLearningHelperTest {
         ComputationGraph expectedModel = new ComputationGraph(expectedConf);
         expectedModel.init();
         assertEquals(expectedConf.toJson(),modelSubset.getConfiguration().toJson());
+    }
+
+    @Test
+    public void testFitUnFrozen() {
+
+        NeuralNetConfiguration.Builder overallConf = new NeuralNetConfiguration.Builder()
+                .learningRate(0.1)
+                .seed(124)
+                .activation(Activation.IDENTITY)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .updater(Updater.SGD);
+
+        ComputationGraphConfiguration conf
+                = overallConf.graphBuilder()
+                .addInputs("inCentre", "inRight")
+                .addLayer("denseCentre0", new DenseLayer.Builder().nIn(10).nOut(9).build(), "inCentre")
+                .addLayer("denseCentre1", new DenseLayer.Builder().nIn(9).nOut(8).build(), "denseCentre0")
+                .addLayer("denseCentre2", new DenseLayer.Builder().nIn(8).nOut(7).build(), "denseCentre1")
+                .addLayer("denseCentre3", new DenseLayer.Builder().nIn(7).nOut(7).build(), "denseCentre2")
+                .addLayer("outCentre", new OutputLayer.Builder(LossFunctions.LossFunction.MSE).nIn(7).nOut(4).build(), "denseCentre3")
+                .addVertex("subsetLeft", new SubsetVertex(0, 3), "denseCentre1")
+                .addLayer("denseLeft0", new DenseLayer.Builder().nIn(4).nOut(5).build(), "subsetLeft")
+                .addLayer("outLeft", new OutputLayer.Builder(LossFunctions.LossFunction.MSE).nIn(5).nOut(6).build(), "denseLeft0")
+                .addLayer("denseRight", new DenseLayer.Builder().nIn(7).nOut(7).build(), "denseCentre2")
+                .addLayer("denseRight0", new DenseLayer.Builder().nIn(2).nOut(3).build(), "inRight")
+                .addVertex("mergeRight", new MergeVertex(), "denseRight", "denseRight0")
+                .addLayer("denseRight1", new DenseLayer.Builder().nIn(10).nOut(5).build(), "mergeRight")
+                .addLayer("outRight", new OutputLayer.Builder(LossFunctions.LossFunction.MSE).nIn(5).nOut(5).build(), "denseRight1")
+                .setOutputs("outLeft", "outCentre", "outRight")
+                .build();
+
+        ComputationGraph modelToTune = new ComputationGraph(conf);
+        modelToTune.init();
+        modelToTune.getVertex("denseCentre0").setLayerAsFrozen();
+        modelToTune.getVertex("denseCentre1").setLayerAsFrozen();
+        modelToTune.getVertex("denseCentre2").setLayerAsFrozen();
+
+        INDArray inRight = Nd4j.rand(10,2);
+        INDArray inCentre = Nd4j.rand(10,10);
+        INDArray outLeft = Nd4j.rand(10,6);
+        INDArray outRight = Nd4j.rand(10,5);
+        INDArray outCentre = Nd4j.rand(10,4);
+        MultiDataSet origData = new MultiDataSet(new INDArray[] {inCentre,inRight},new INDArray[] {outLeft,outCentre,outRight});
+        ComputationGraph modelIdentical = modelToTune.clone();
+
+        TransferLearningHelper helper = new TransferLearningHelper(modelToTune);
+        MultiDataSet featurizedDataSet = helper.featurize(origData);
+
+        modelIdentical.fit(origData);
+        helper.fitFeaturized(featurizedDataSet);
+
+        assertEquals(modelIdentical.getLayer("denseCentre0").params(),modelToTune.getLayer("denseCentre0").params());
+        assertEquals(modelIdentical.getLayer("denseCentre1").params(),modelToTune.getLayer("denseCentre1").params());
+        assertEquals(modelIdentical.getLayer("denseCentre2").params(),modelToTune.getLayer("denseCentre2").params());
+        assertEquals(modelIdentical.getLayer("denseCentre3").params(),modelToTune.getLayer("denseCentre3").params());
+        assertEquals(modelIdentical.getLayer("outCentre").params(),modelToTune.getLayer("outCentre").params());
+        //assertEquals(modelIdentical.getLayer("denseRight0").params(),modelToTune.getLayer("denseRight0").params());
+        //assertEquals(modelIdentical.getLayer("denseRight1").params(),modelToTune.getLayer("denseRight1").params());
+        //assertEquals(modelIdentical.getLayer("outRight").params(),modelToTune.getLayer("outRight").params());
+        assertEquals(modelIdentical.getLayer("denseLeft0").params(),modelToTune.getLayer("denseLeft0").params());
+        assertEquals(modelIdentical.getLayer("outLeft").params(),modelToTune.getLayer("outLeft").params());
+
+
     }
 }
