485fd7c3725c4172d3ed4a736e08d34df538fe4b
François Garillot <francois@skymind.io>
2017-03-07 08:44:19 -0500

    make sure the buildmultiple*versions script is safe

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 set -eu
+./change-scala-versions.sh 2.11 # should be idempotent, this is the default
+./change-spark-versions.sh 1
 mvn "$@"
 ./change-spark-versions.sh 2
 mvn -Dspark.major.version=2 "$@"
 ./change-scala-versions.sh 2.10
 ./change-spark-versions.sh 1
 mvn "$@"
-./change-scala-versions.sh 2.11
+./change-scala-versions.sh 2.11 # back to the default
+./change-spark-versions.sh 1
