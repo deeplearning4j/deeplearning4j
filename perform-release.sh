f5a562c34f1a25368652b44cac96ae424340a4b2
Samuel Audet <samuel.audet@gmail.com>
2017-03-11 12:15:10 +0900

    Update perform-release.sh to be able to update version of Spark modules (pull #3017)

--- perform-release.sh
+++ perform-release.sh
@@ -20,6 +20,10 @@ fi
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\/nd4j.version>/" pom.xml
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$RELEASE_VERSION<\/datavec.version>/" pom.xml
+#Spark versions, like <version>xxx_spark_2-SNAPSHOT</version>
+for f in $(find . -name 'pom.xml' -not -path '*target*'); do
+    sed -i "s/version>.*_spark_.*</version>${RELEASE_VERSION}_spark_1</g" $f
+done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION
 
 source change-scala-versions.sh 2.10
@@ -39,6 +43,11 @@ git tag -a -m "deeplearning4j-$RELEASE_VERSION" "deeplearning4j-$RELEASE_VERSION
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$SNAPSHOT_VERSION<\/datavec.version>/" pom.xml
+#Spark versions, like <version>xxx_spark_2-SNAPSHOT</version>
+SPLIT_VERSION=(${SNAPSHOT_VERSION//-/ })
+for f in $(find . -name 'pom.xml' -not -path '*target*'); do
+    sed -i "s/version>.*_spark_.*</version>${SPLIT_VERSION[0]}_spark_1-${SPLIT_VERSION[1]}</g" $f
+done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
 git commit -a -m "Update to version $SNAPSHOT_VERSION"
 
