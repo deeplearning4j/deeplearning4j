3723c039bd798470db4f72914fc8a61287052905
Samuel Audet <samuel.audet@gmail.com>
2017-03-11 12:14:49 +0900

    Update perform-release.sh to be able to update version of Spark modules (pull #204)

--- perform-release.sh
+++ perform-release.sh
@@ -19,6 +19,10 @@ if [[ ! -z $(git tag -l "datavec-$RELEASE_VERSION") ]]; then
 fi
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\/nd4j.version>/" pom.xml
+#Spark versions, like <version>xxx_spark_2-SNAPSHOT</version>
+for f in $(find . -name 'pom.xml' -not -path '*target*'); do
+    sed -i "s/version>.*_spark_.*</version>${RELEASE_VERSION}_spark_1</g" $f
+done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION
 
 mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
@@ -30,11 +34,15 @@ mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -
 
 source change-spark-versions.sh 1
 source change-scala-versions.sh 2.11
-source change-cuda-versions.sh 8.0
 git commit -a -m "Update to version $RELEASE_VERSION"
 git tag -a -m "datavec-$RELEASE_VERSION" "datavec-$RELEASE_VERSION"
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
+#Spark versions, like <version>xxx_spark_2-SNAPSHOT</version>
+SPLIT_VERSION=(${SNAPSHOT_VERSION//-/ })
+for f in $(find . -name 'pom.xml' -not -path '*target*'); do
+    sed -i "s/version>.*_spark_.*</version>${SPLIT_VERSION[0]}_spark_1-${SPLIT_VERSION[1]}</g" $f
+done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
 git commit -a -m "Update to version $SNAPSHOT_VERSION"
 
