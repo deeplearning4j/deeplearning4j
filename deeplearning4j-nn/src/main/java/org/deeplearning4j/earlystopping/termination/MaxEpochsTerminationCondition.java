e1be2a7f20bdca91e3d9e5bf65bd2a817a1cc7e9
Adam Gibson <adam@skymind.io>
2017-03-15 13:09:03 -0700

    Dl4jjson (#3020)
    * update camel version
    
    * get rid of vector serializer in jackson
    
    * local
    
    * updated spark versions, activations, and added new test
    
    * update dl4j streaming
    
    * update kafka tests
    
    * local
    
    * add scala version to parallelwrapper
    
    *  updated json config
    
    
    Former-commit-id: 64bd96cc2bd7391e7b0ff94db6275fe74a08af70 [formerly 1e3daba8fd790f9e6d6284d5de44bf0c85466d50]
    Former-commit-id: 0c7aea3b793b1a684ed75a2ead6d9b1f9b014f72

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/earlystopping/termination/MaxEpochsTerminationCondition.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/earlystopping/termination/MaxEpochsTerminationCondition.java
@@ -18,10 +18,16 @@
 
 package org.deeplearning4j.earlystopping.termination;
 
-/** Terminate training if the number of epochs exceeds the maximum number of epochs */
-public class MaxEpochsTerminationCondition implements EpochTerminationCondition {
-    private int maxEpochs;
+import lombok.NoArgsConstructor;
+import org.nd4j.shade.jackson.annotation.JsonCreator;
+import org.nd4j.shade.jackson.annotation.JsonProperty;
 
+/** Terminate training if the number of epochs exceeds the maximum number of epochs */
+@NoArgsConstructor
+public class MaxEpochsTerminationCondition implements EpochTerminationCondition {
+    @JsonProperty
+    private int maxEpochs;
+    @JsonCreator
     public MaxEpochsTerminationCondition(int maxEpochs) {
         if (maxEpochs <= 0)
             throw new IllegalArgumentException("Max number of epochs must be >= 1");
