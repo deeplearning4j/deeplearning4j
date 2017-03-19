3ef0f5f2e26838da43ff45c6b0aed12a615cec10
raver119 <raver119@gmail.com>
2017-03-19 22:28:38 +0300

    meh...

--- nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/main/java/org/nd4j/jita/workspace/CudaWorkspace.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/main/java/org/nd4j/jita/workspace/CudaWorkspace.java
@@ -2,15 +2,22 @@ package org.nd4j.jita.workspace;
 
 import lombok.NonNull;
 import org.bytedeco.javacpp.Pointer;
+import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.memory.conf.WorkspaceConfiguration;
 import org.nd4j.linalg.api.memory.enums.MemoryKind;
 import org.nd4j.linalg.api.memory.pointers.PagedPointer;
 import org.nd4j.linalg.memory.abstracts.Nd4jWorkspace;
+import org.nd4j.nativeblas.NativeOps;
+import org.nd4j.nativeblas.NativeOpsHolder;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author raver119@gmail.com
  */
 public class CudaWorkspace extends Nd4jWorkspace {
+
+
     public CudaWorkspace(@NonNull WorkspaceConfiguration configuration) {
         super(configuration);
     }
@@ -24,9 +31,20 @@ public class CudaWorkspace extends Nd4jWorkspace {
         super.init();
 
         if (currentSize.get() > 0) {
-            workspace.setHostPointer(new PagedPointer(memoryManager.allocate(currentSize.get() + 1024, MemoryKind.HOST, true)));
+            NativeOps nativeOps = NativeOpsHolder.getInstance().getDeviceNativeOps();
 
-            Pointer.memset(workspace.getHostPointer(), 0, currentSize.get() + 1024);
+            workspace.setHostPointer(new PagedPointer(nativeOps.mallocHost(currentSize.get(), 0)));
+            workspace.setDevicePointer(new PagedPointer(nativeOps.mallocDevice(currentSize.get(), null, 0)));
         }
+    }
+
+    @Override
+    public PagedPointer alloc(long requiredMemory, DataBuffer.Type type) {
+        return this.alloc(requiredMemory, MemoryKind.DEVICE, type);
+    }
+
+    @Override
+    public PagedPointer alloc(long requiredMemory, MemoryKind kind, DataBuffer.Type type) {
+        return super.alloc(requiredMemory, kind, type);
     }
 }
