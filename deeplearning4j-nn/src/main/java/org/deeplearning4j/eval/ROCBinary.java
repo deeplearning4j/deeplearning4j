f71f83ae6849304c9dd481804532daa49a36ccad
Alex Black <blacka101@gmail.com>
2017-03-21 12:49:07 +1100

    ROCBinary tests, additional methods, javadoc etc
    Former-commit-id: f9e2ef07157dc37d4980b1aebf9bab84be46f828 [formerly ad3991c16b868fbf0b5fec0b70c5d5fef8b0beea]
    Former-commit-id: a5b213045e3250cba63758226f718ca44362536f

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/eval/ROCBinary.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/eval/ROCBinary.java
@@ -12,8 +12,7 @@ import org.nd4j.linalg.indexing.conditions.Condition;
 import org.nd4j.linalg.indexing.conditions.Conditions;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 /**
  * ROC (Receiver Operating Characteristic) for multi-task binary classifiers, using the specified number of threshold steps.
@@ -31,11 +30,13 @@ import java.util.Map;
  * (mask value 0) and hence won't be included in the calculated ROC.
  */
 public class ROCBinary extends BaseEvaluation<ROCBinary> {
+    public static final int DEFAULT_PRECISION = 4;
 
     private final int thresholdSteps;
     private long[] countActualPositive;
     private long[] countActualNegative;
-    private Map<Double,CountsForThreshold> countsForThresholdMap;
+    private Map<Double, CountsForThreshold> countsForThresholdMap;
+    private List<String> labels;
 
     public ROCBinary(int thresholdSteps) {
         this.thresholdSteps = thresholdSteps;
@@ -54,16 +55,17 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
                     + "size " + countActualPositive.length + ", got labels array with size " + labels.size(1));
         }
 
-        if(labels.rank() == 3){
+        if (labels.rank() == 3) {
             evalTimeSeries(labels, networkPredictions, maskArray);
             return;
         }
 
-        if(countActualPositive == null){
+        if (countActualPositive == null) {
             //Initialize
             countActualPositive = new long[labels.size(1)];
             countActualNegative = new long[labels.size(1)];
 
+            countsForThresholdMap = new LinkedHashMap<>();
             double step = 1.0 / thresholdSteps;
             for (int i = 0; i <= thresholdSteps; i++) {
                 double currThreshold = i * step;
@@ -73,8 +75,8 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
 
         //First: need to increment actual positive/negative (label counts) for each output
         INDArray actual1 = labels;
-        INDArray actual0 = Nd4j.getExecutioner().execAndReturn( new Not(labels.dup()));
-        if(maskArray != null){
+        INDArray actual0 = Nd4j.getExecutioner().execAndReturn(new Not(labels.dup()));
+        if (maskArray != null) {
             actual1 = actual1.mul(maskArray);
             actual0.muli(maskArray);
         }
@@ -105,7 +107,7 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
 
             //Apply mask array:
             if (maskArray != null) {
-                if(Arrays.equals(labels.shape(), maskArray.shape())){
+                if (Arrays.equals(labels.shape(), maskArray.shape())) {
                     //Per output masking
                     isTruePositive.muli(maskArray);
                     isFalsePositive.muli(maskArray);
@@ -127,30 +129,30 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
 
     }
 
-    private static void addInPlace(long[] addTo, int[] toAdd){
-        for( int i=0; i<addTo.length; i++ ){
+    private static void addInPlace(long[] addTo, int[] toAdd) {
+        for (int i = 0; i < addTo.length; i++) {
             addTo[i] += toAdd[i];
         }
     }
 
-    private static void addInPlace(long[] addTo, long[] toAdd){
-        for( int i=0; i<addTo.length; i++ ){
+    private static void addInPlace(long[] addTo, long[] toAdd) {
+        for (int i = 0; i < addTo.length; i++) {
             addTo[i] += toAdd[i];
         }
     }
 
     @Override
     public void merge(ROCBinary other) {
-        if(this.countActualPositive == null){
+        if (this.countActualPositive == null) {
             this.countActualPositive = other.countActualPositive;
             this.countActualNegative = other.countActualNegative;
             this.countsForThresholdMap = other.countsForThresholdMap;
             return;
-        } else if(other.countActualPositive == null){
+        } else if (other.countActualPositive == null) {
             return;
         }
 
-        if(this.countActualPositive.length != other.countActualPositive.length){
+        if (this.countActualPositive.length != other.countActualPositive.length) {
             throw new IllegalStateException("Cannot merge ROCBinary instances with different number of coulmns. " +
                     "numColumns = " + this.countActualPositive.length + "; other numColumns = " + other.countActualPositive.length);
         }
@@ -158,12 +160,234 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
         //Both have data
         addInPlace(this.countActualPositive, other.countActualPositive);
         addInPlace(this.countActualNegative, other.countActualNegative);
-        for(Map.Entry<Double,CountsForThreshold> e : countsForThresholdMap.entrySet()){
+        for (Map.Entry<Double, CountsForThreshold> e : countsForThresholdMap.entrySet()) {
             CountsForThreshold o = other.countsForThresholdMap.get(e.getKey());
 
             e.getValue().incrementTruePositive(o.getCountTruePositive());
             e.getValue().incrementFalsePositive(o.getCountFalsePositive());
         }
+    }
+
+    private void assertIndex(int outputNum){
+        if(countActualPositive == null){
+            throw new UnsupportedOperationException("ROCBinary does not have any stats: eval must be called first");
+        }
+        if(outputNum < 0 || outputNum >= countActualPositive.length){
+            throw new IllegalArgumentException("Invalid input: output number must be between 0 and " + (outputNum-1));
+        }
+    }
+
+    /**
+     * Returns the number of labels - (i.e., size of the prediction/labels arrays) - if known. Returns -1 otherwise
+     */
+    public int numLabels() {
+        if (countActualPositive == null) {
+            return -1;
+        }
+
+        return countActualPositive.length;
+    }
+
+    /**
+     * Get the actual positive count (accounting for any masking) for  the specified output/column
+     *
+     * @param outputNum Index of the output (0 to {@link #numLabels()}-1)
+     */
+    public long getCountActualPositive(int outputNum){
+        assertIndex(outputNum);
+        return countActualPositive[outputNum];
+    }
+
+    /**
+     * Get the actual negative count (accounting for any masking) for  the specified output/column
+     *
+     * @param outputNum Index of the output (0 to {@link #numLabels()}-1)
+     */
+    public long getCountActualNegative(int outputNum){
+        assertIndex(outputNum);
+        return countActualNegative[outputNum];
+    }
+
+    /**
+     * Get the ROC curve, as a set of points
+     *
+     * @param outputNum Index of the output (0 to {@link #numLabels()}-1)
+     * @return ROC curve, as a list of points
+     */
+    public List<ROCBinary.ROCValue> getResults(int outputNum) {
+        assertIndex(outputNum);
+        List<ROCBinary.ROCValue> out = new ArrayList<>(countsForThresholdMap.size());
+
+        for (Map.Entry<Double, ROCBinary.CountsForThreshold> entry : countsForThresholdMap.entrySet()) {
+            double t = entry.getKey();
+            ROCBinary.CountsForThreshold c = entry.getValue();
+            double tpr = c.getCountTruePositive()[outputNum] / ((double) countActualPositive[outputNum]);
+            double fpr = c.getCountFalsePositive()[outputNum] / ((double) countActualNegative[outputNum]);
+
+            out.add(new ROCBinary.ROCValue(t, tpr, fpr));
+        }
+
+        return out;
+    }
+
+    /**
+     * Get the precision/recall curve, for the specified output
+     *
+     * @param outputNum Index of the output (0 to {@link #numLabels()}-1)
+     * @return the precision/recall curve
+     */
+    public List<ROCBinary.PrecisionRecallPoint> getPrecisionRecallCurve(int outputNum) {
+        assertIndex(outputNum);
+        //Precision: (true positive count) / (true positive count + false positive count) == true positive rate
+        //Recall: (true positive count) / (true positive count + false negative count) = (TP count) / (total dataset positives)
+
+        List<ROCBinary.PrecisionRecallPoint> out = new ArrayList<>(countsForThresholdMap.size());
+
+        for (Map.Entry<Double, ROCBinary.CountsForThreshold> entry : countsForThresholdMap.entrySet()) {
+            double t = entry.getKey();
+            ROCBinary.CountsForThreshold c = entry.getValue();
+            long tpCount = c.getCountTruePositive()[outputNum];
+            long fpCount = c.getCountFalsePositive()[outputNum];
+            //For edge cases: http://stats.stackexchange.com/questions/1773/what-are-correct-values-for-precision-and-recall-in-edge-cases
+            //precision == 1 when FP = 0 -> no incorrect positive predictions
+            //recall == 1 when no dataset positives are present (got all 0 of 0 positives)
+            double precision;
+            if (tpCount == 0 && fpCount == 0) {
+                //At this threshold: no predicted positive cases
+                precision = 1.0;
+            } else {
+                precision = tpCount / (double) (tpCount + fpCount);
+            }
+
+            double recall;
+            if (countActualPositive[outputNum] == 0) {
+                recall = 1.0;
+            } else {
+                recall = tpCount / ((double) countActualPositive[outputNum]);
+            }
+
+
+            out.add(new ROCBinary.PrecisionRecallPoint(c.getThreshold(), precision, recall));
+        }
+
+        return out;
+    }
+
+    /**
+     * Get the ROC curve, as a set of (falsePositive, truePositive) points
+     * <p>
+     * Returns a 2d array of {falsePositive, truePositive values}.<br>
+     * Size is [2][thresholdSteps], with out[0][.] being false positives, and out[1][.] being true positives
+     *
+     * @return ROC curve as double[][]
+     */
+    public double[][] getResultsAsArray(int outputNum) {
+        assertIndex(outputNum);
+
+        double[][] out = new double[2][thresholdSteps + 1];
+        int i = 0;
+        for (Map.Entry<Double, ROCBinary.CountsForThreshold> entry : countsForThresholdMap.entrySet()) {
+            ROCBinary.CountsForThreshold c = entry.getValue();
+            double tpr = c.getCountTruePositive()[outputNum] / ((double) countActualPositive[outputNum]);
+            double fpr = c.getCountFalsePositive()[outputNum] / ((double) countActualNegative[outputNum]);
+
+            out[0][i] = fpr;
+            out[1][i] = tpr;
+            i++;
+        }
+        return out;
+    }
+
+    /**
+     * Calculate the AUC - Area Under Curve<br>
+     * Utilizes trapezoidal integration internally
+     *
+     * @param outputNum
+     * @return AUC
+     */
+    public double calculateAUC(int outputNum) {
+        assertIndex(outputNum);
+
+        //Calculate AUC using trapezoidal rule
+        List<ROCBinary.ROCValue> list = getResults(outputNum);
+
+        //Given the points
+        double auc = 0.0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            ROCBinary.ROCValue left = list.get(i);
+            ROCBinary.ROCValue right = list.get(i + 1);
+
+            //y axis: TPR
+            //x axis: FPR
+            double deltaX = Math.abs(right.getFalsePositiveRate() - left.getFalsePositiveRate()); //Iterating in threshold order, so FPR decreases as threshold increases
+            double avg = (left.getTruePositiveRate() + right.getTruePositiveRate()) / 2.0;
+
+            auc += deltaX * avg;
+        }
+        return auc;
+    }
+
+    /**
+     * Set the label names, for printing via {@link #stats()}
+     */
+    public void setLabelNames(List<String> labels) {
+        if (labels == null) {
+            this.labels = null;
+            return;
+        }
+        this.labels = new ArrayList<>(labels);
+    }
+
+    public String stats() {
+        return stats(DEFAULT_PRECISION);
+    }
+
+    public String stats(int printPrecision) {
+        //Calculate AUC and also print counts, for each output
+
+        StringBuilder sb = new StringBuilder();
+
+        int maxLabelsLength = 15;
+        if (labels != null) {
+            for (String s : labels) {
+                maxLabelsLength = Math.max(s.length(), maxLabelsLength);
+            }
+        }
+
+        String patternHeader = "%-" + (maxLabelsLength+5) + "s%-12s%-10s%-10s";
+        String header = String.format(patternHeader, "Label", "AUC", "# Pos", "# Neg");
+
+        String pattern = "%-" + (maxLabelsLength + 5) + "s"     //Label
+                + "%-12." + printPrecision + "f"                //AUC
+                + "%-10d%-10d";                                   //Count pos, count neg
+
+        sb.append(header);
+
+        for( int i=0; i<countActualPositive.length; i++ ){
+            double auc = calculateAUC(i);
+
+            String label = (labels == null ? String.valueOf(i) : labels.get(i));
+
+            sb.append("\n").append(String.format(pattern, label, auc, countActualPositive[i], countActualNegative[i]));
+        }
+
+        return sb.toString();
+    }
+
+    @AllArgsConstructor
+    @Data
+    public static class ROCValue {
+        private final double threshold;
+        private final double truePositiveRate;
+        private final double falsePositiveRate;
+    }
+
+    @AllArgsConstructor
+    @Data
+    public static class PrecisionRecallPoint {
+        private final double classiferThreshold;
+        private final double precision;
+        private final double recall;
     }
 
 
@@ -178,19 +402,19 @@ public class ROCBinary extends BaseEvaluation<ROCBinary> {
             this(threshold, new long[size], new long[size]);
         }
 
-        public void incrementTruePositive(int[] counts){
+        public void incrementTruePositive(int[] counts) {
             addInPlace(countTruePositive, counts);
         }
 
-        public void incrementFalsePositive(int[] counts){
+        public void incrementFalsePositive(int[] counts) {
             addInPlace(countFalsePositive, counts);
         }
 
-        public void incrementTruePositive(long[] counts){
+        public void incrementTruePositive(long[] counts) {
             addInPlace(countTruePositive, counts);
         }
 
-        public void incrementFalsePositive(long[] counts){
+        public void incrementFalsePositive(long[] counts) {
             addInPlace(countFalsePositive, counts);
         }
 
