e7d9e5f009e9a9c1c92a5bbd077e7808fda88633
Justin Long <crockpotveggies@users.noreply.github.com>
2017-03-23 14:20:11 -0700

    Use Nd4j.getRandom().

--- datavec-data/datavec-data-image/src/main/java/org/datavec/image/transform/CombinedImageTransform.java
+++ datavec-data/datavec-data-image/src/main/java/org/datavec/image/transform/CombinedImageTransform.java
@@ -34,31 +34,54 @@ import java.util.Random;
  *
  * @author Justin Long (@crockpotveggies)
  */
+import javafx.util.Pair;
+import org.bytedeco.javacpp.opencv_core;
+import org.datavec.image.data.ImageWritable;
+import org.datavec.image.transform.BaseImageTransform;
+import org.datavec.image.transform.ImageTransform;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Allows creation of image transform pipelines, either sequentially or randomly.
+ *
+ * Pipelines are defined as a list of tuples, where each transform is assigned
+ * a probability between 0.0-1.0 of being invoked. Pipeline can also be randomly
+ * shuffled with each transform, further increasing the available dataset.
+ *
+ * @author Justin Long (@crockpotveggies)
+ */
 public class CombinedImageTransform extends BaseImageTransform<opencv_core.Mat> {
 
     protected List<Pair<ImageTransform, Double>> transforms;
     protected boolean shuffle;
+    protected org.nd4j.linalg.api.rng.Random rng;
 
     public CombinedImageTransform(List<Pair<ImageTransform, Double>> transforms) {
-        this(new Random(1234), transforms, false);
+        this(1234, transforms, false);
     }
 
     public CombinedImageTransform(List<Pair<ImageTransform, Double>> transforms, boolean shuffle) {
-        this(new Random(1234), transforms, shuffle);
+        this(1234, transforms, shuffle);
     }
 
     public CombinedImageTransform(long seed, List<Pair<ImageTransform, Double>> transforms) {
-        this(new Random(seed), transforms, false);
+        this(seed, transforms, false);
     }
 
     public CombinedImageTransform(long seed, List<Pair<ImageTransform, Double>> transforms, boolean shuffle) {
-        this(new Random(seed), transforms, shuffle);
+        this(null, seed, transforms, shuffle);
     }
 
-    public CombinedImageTransform(Random random, List<Pair<ImageTransform, Double>> transforms, boolean shuffle) {
-        super(random);
+    public CombinedImageTransform(Random random, long seed, List<Pair<ImageTransform, Double>> transforms, boolean shuffle) {
+        super(null); // for perf reasons we ignore java Random, create our own
         this.transforms = transforms;
         this.shuffle = shuffle;
+        this.rng = Nd4j.getRandom();
+        rng.setSeed(seed);
     }
 
     /**
@@ -75,7 +98,7 @@ public class CombinedImageTransform extends BaseImageTransform<opencv_core.Mat> 
         // execute each item in the pipeline
         ImageWritable output = image;
         for(Pair<ImageTransform, Double> tuple : transforms) {
-            if(random.nextDouble() < tuple.getValue()) { // probability of execution
+            if(rng.nextDouble() < tuple.getValue()) { // probability of execution
                 output = tuple.getKey().transform(image);
             }
         }
