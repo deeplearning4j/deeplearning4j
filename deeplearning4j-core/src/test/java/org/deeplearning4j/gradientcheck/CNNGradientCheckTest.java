4f76d30454c1f973835a5117ec620ec0c8581d2a
Alex Black <blacka101@gmail.com>
2017-03-24 18:37:40 +1100

    Gradient check util: throw exception if not set to double precision

--- deeplearning4j-core/src/test/java/org/deeplearning4j/gradientcheck/CNNGradientCheckTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/gradientcheck/CNNGradientCheckTest.java
@@ -42,8 +42,6 @@ public class CNNGradientCheckTest {
     private static final double DEFAULT_MIN_ABS_ERROR = 1e-8;
 
     static {
-        //Force Nd4j initialization, then set data type to double:
-        Nd4j.zeros(1);
         DataTypeUtil.setDTypeForContext(DataBuffer.Type.DOUBLE);
     }
 
