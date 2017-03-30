1f444f7c00917121a2d6adec912965c5663712f4
François Garillot <francois@garillot.net>
2017-03-30 14:51:54 -0400

    Fix various issues related to recent Spark-2 additions (#3160)
    * Fixed test issues
    
    * Hide import change in Spark v 1/2
    
    https://spark.apache.org/docs/2.0.1/api/java/org/apache/spark/sql/expressions/UserDefinedFunction.html
    https://spark.apache.org/docs/1.6.3/api/java/org/apache/spark/sql/UserDefinedFunction.html
    
    Fixes #3155
    
    * Reorganize sources & build-helper version
    
    * Force cleaning of sources on spark-x modules
    
    Fixes #3147

--- deeplearning4j-scaleout/spark/dl4j-spark-ml/src/test/java/org/deeplearning4j/spark/ml/impl/SparkDl4jNetworkTest.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-ml/src/test/java/org/deeplearning4j/spark/ml/impl/SparkDl4jNetworkTest.java
@@ -1,21 +1,12 @@
 package org.deeplearning4j.spark.ml.impl;
 
 
-import com.google.common.collect.Lists;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.Pipeline;
 import org.apache.spark.ml.PipelineStage;
 import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.mllib.linalg.Vectors;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -27,13 +18,13 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.spark.impl.paramavg.ParameterAveragingTrainingMaster;
+import org.deeplearning4j.spark.ml.utils.DatasetFacade;
 import org.deeplearning4j.spark.ml.utils.ParamSerializer;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class SparkDl4jNetworkTest  {
@@ -44,13 +35,9 @@ public class SparkDl4jNetworkTest  {
 
     @Test
     public void testNetwork() {
-        List<Row> dubs = Lists.newArrayList(
-                RowFactory.create(1000.0, 1000.0, 1.0),
-                RowFactory.create(90.0, 90.0, 0.0)
-        );
-        DataFrame df = sqlContext.createDataFrame(dubs, createStruct());
+        DatasetFacade df = DatasetFacade.dataRows(sqlContext.read().json("src/test/resources/dl4jnetwork"));
         Pipeline p = new Pipeline().setStages(new PipelineStage[]{getAssembler(new String[]{"x", "y"}, "features")});
-        DataFrame part2 = p.fit(df).transform(df).select("features", "label");
+        DatasetFacade part2 = DatasetFacade.dataRows(p.fit(df.get()).transform(df.get()).select("features", "label"));
 
         ParamSerializer ps = new ParamHelper();
         MultiLayerConfiguration mc = getNNConfiguration();
@@ -61,18 +48,9 @@ public class SparkDl4jNetworkTest  {
                 .setFeaturesCol("features")
                 .setLabelCol("label");
 
-        SparkDl4jModel sm = sparkDl4jNetwork.fit(part2);
+        SparkDl4jModel sm = sparkDl4jNetwork.fit(part2.get());
         MultiLayerNetwork mln = sm.getMultiLayerNetwork();
         Assert.assertNotNull(mln);
-        System.out.println(sm.output(Vectors.dense(0.0, 0.0)));
-    }
-
-    private StructType createStruct() {
-        return  new StructType(new StructField[]{
-                new StructField("x", DataTypes.DoubleType, true, Metadata.empty()),
-                new StructField("y", DataTypes.DoubleType, true, Metadata.empty()),
-                new StructField("label", DataTypes.DoubleType, true, Metadata.empty())
-        });
     }
 
     private MultiLayerConfiguration getNNConfiguration(){
@@ -91,7 +69,7 @@ public class SparkDl4jNetworkTest  {
                 .build();
     }
 
-    public static VectorAssembler getAssembler(String[] input, String output){
+    private static VectorAssembler getAssembler(String[] input, String output){
         return new VectorAssembler()
                 .setInputCols(input)
                 .setOutputCol(output);
