547ca7cb8087d19592404dc8a8cc766cded78da0
Adam Gibson <adam@skymind.io>
2017-04-02 19:15:29 +0900

    test resources

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/TestNDArrayCreation.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/TestNDArrayCreation.java
@@ -1,13 +1,11 @@
 package org.nd4j.linalg.api;
 
-import org.bytedeco.javacpp.Pointer;
 import org.junit.Test;
 import org.nd4j.linalg.BaseNd4jTest;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.factory.Nd4jBackend;
 import org.nd4j.linalg.io.ClassPathResource;
-import org.nd4j.nativeblas.NativeOpsHolder;
 
 import java.io.File;
 
@@ -26,7 +24,7 @@ public class TestNDArrayCreation extends BaseNd4jTest {
 
     @Test
     public void testCreateNpy() throws Exception {
-        INDArray arrCreate = Nd4j.createFromNpyFile(new File("/home/agibsonccc/code/nd4j/nd4j-backends/nd4j-tests/src/test/resources/test.npy"));
+        INDArray arrCreate = Nd4j.createFromNpyFile(new ClassPathResource("test.npy").getFile());
         assertEquals(2,arrCreate.size(0));
         assertEquals(2,arrCreate.size(1));
         assertEquals(1.0,arrCreate.getDouble(0,0),1e-1);
