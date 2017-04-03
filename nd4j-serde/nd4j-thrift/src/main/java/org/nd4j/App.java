f9987e7d81a55e6ed048b83806fc3e859f8a3063
Adam Gibson <adam@skymind.io>
2017-04-03 19:44:21 +0900

    fix indexers and allocation

---
+++ nd4j-serde/nd4j-thrift/src/main/java/org/nd4j/App.java
@@ -0,0 +1,13 @@
+package org.nd4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
