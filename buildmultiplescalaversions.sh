ebeb63c267acdecf153c0ffb58a561a649c53687
François Garillot <francois@garillot.net>
2017-04-20 23:31:47 +0200

    Fix : buildmultiplescalaversions thought the default Scala version was 2.10

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -eu
-./change-scala-versions.sh 2.10
-mvn "$@"
-./change-scala-versions.sh 2.11
+./change-scala-versions.sh 2.11 # This should be idempotent : it's the default
 mvn "$@"
 ./change-scala-versions.sh 2.10
+mvn "$@"
+./change-scala-versions.sh 2.11 # Back to the default
