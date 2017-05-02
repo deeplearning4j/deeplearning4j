750ddeed3cfe51335f1ffd27e7c91553eabcff57
Alex Black <blacka101@gmail.com>
2017-05-02 11:21:23 +1000

    Update gradient norm tests to new assumptions

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/updater/TestGradientNormalization.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/updater/TestGradientNormalization.java
@@ -31,16 +31,18 @@ public class TestGradientNormalization {
         int numParams = conf.getLayer().initializer().numParams(conf);
         INDArray params = Nd4j.create(1, numParams);
         Layer layer = conf.getLayer().instantiate(conf, null, 0, params, true);
-        layer.setBackpropGradientsViewArray(Nd4j.create(params.shape()));
-        Updater updater = UpdaterCreator.getUpdater(layer);
-        INDArray weightGrad = Nd4j.rand(10, 20);
-        INDArray biasGrad = Nd4j.rand(1, 10);
+        INDArray gradArray = Nd4j.rand(1,220).muli(10).subi(5);
+        layer.setBackpropGradientsViewArray(gradArray);
+        INDArray weightGrad = Shape.newShapeNoCopy(gradArray.get(NDArrayIndex.point(0), NDArrayIndex.interval(0,200)),
+                new int[]{10,20},true);
+        INDArray biasGrad = gradArray.get(NDArrayIndex.point(0), NDArrayIndex.interval(200,220));
         INDArray weightGradCopy = weightGrad.dup();
         INDArray biasGradCopy = biasGrad.dup();
-        Gradient gradient = new DefaultGradient();
+        Gradient gradient = new DefaultGradient(gradArray);
         gradient.setGradientFor(DefaultParamInitializer.WEIGHT_KEY, weightGrad);
         gradient.setGradientFor(DefaultParamInitializer.BIAS_KEY, biasGrad);
 
+        Updater updater = UpdaterCreator.getUpdater(layer);
         updater.update(layer, gradient, 0, 1);
 
         assertNotEquals(weightGradCopy, weightGrad);
@@ -164,13 +166,14 @@ public class TestGradientNormalization {
             int numParams = conf.getLayer().initializer().numParams(conf);
             INDArray params = Nd4j.create(1, numParams);
             Layer layer = conf.getLayer().instantiate(conf, null, 0, params, true);
-            layer.setBackpropGradientsViewArray(Nd4j.create(params.shape()));
-            Updater updater = UpdaterCreator.getUpdater(layer);
-            INDArray weightGrad = Nd4j.rand(10, 20).muli((t == 0 ? 0.05 : 10));
-            INDArray biasGrad = Nd4j.rand(1, 10).muli((t == 0 ? 0.05 : 10));
+            INDArray gradArray = Nd4j.rand(1,220).muli(t == 0 ? 0.05 : 10).subi(t == 0 ? 0 : 5);
+            layer.setBackpropGradientsViewArray(gradArray);
+            INDArray weightGrad = Shape.newShapeNoCopy(gradArray.get(NDArrayIndex.point(0), NDArrayIndex.interval(0,200)),
+                    new int[]{10,20},true);
+            INDArray biasGrad = gradArray.get(NDArrayIndex.point(0), NDArrayIndex.interval(200,220));
             INDArray weightGradCopy = weightGrad.dup();
             INDArray biasGradCopy = biasGrad.dup();
-            Gradient gradient = new DefaultGradient();
+            Gradient gradient = new DefaultGradient(gradArray);
             gradient.setGradientFor(DefaultParamInitializer.WEIGHT_KEY, weightGrad);
             gradient.setGradientFor(DefaultParamInitializer.BIAS_KEY, biasGrad);
 
@@ -180,6 +183,7 @@ public class TestGradientNormalization {
             else
                 assertTrue(layerGradL2 > threshold);
 
+            Updater updater = UpdaterCreator.getUpdater(layer);
             updater.update(layer, gradient, 0, 1);
 
             if (t == 0) {
