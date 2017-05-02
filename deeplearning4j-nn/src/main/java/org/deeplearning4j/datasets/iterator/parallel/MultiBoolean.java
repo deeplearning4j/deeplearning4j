8c349910407c5a12cb4e7d5151a7509ebcf2d4ae
raver119 <raver119@gmail.com>
2017-05-02 11:37:36 +0300

    MultiBoolean javadoc

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/datasets/iterator/parallel/MultiBoolean.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/datasets/iterator/parallel/MultiBoolean.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.nd4j.linalg.exception.ND4JIllegalStateException;
 
 /**
+ * This is utility class, that allows easy handling of multiple joint boolean states
+ *
  * @author raver119@gmail.com
  */
 @Slf4j
@@ -36,6 +38,12 @@ public class MultiBoolean {
             this.timeTracker = new MultiBoolean(numEntries, false, false);
     }
 
+    /**
+     * Sets specified entry to specified state
+     *
+     * @param value
+     * @param entry
+     */
     public void set(boolean value, int entry){
         if (entry > numEntries)
             throw new ND4JIllegalStateException("Entry index given (" + entry + ")in is higher then configured one (" + numEntries + ")");
@@ -52,6 +60,12 @@ public class MultiBoolean {
             this.timeTracker.set(true, entry);
     }
 
+    /**
+     * Gets current state for specified entry
+     *
+     * @param entry
+     * @return
+     */
     public boolean get(int entry) {
         if (entry > numEntries)
             throw new ND4JIllegalStateException("Entry index given (" + entry + ")in is higher then configured one (" + numEntries + ")");
@@ -59,11 +73,20 @@ public class MultiBoolean {
         return (this.holder & 1 << (entry + 1)) != 0;
     }
 
+    /**
+     * This method returns true if ALL states are true. False otherwise.
+     *
+     * @return
+     */
     public boolean allTrue() {
         //log.info("Holder: {}; Max: {}", holder, max);
         return holder == max;
     }
 
+    /**
+     * This method returns true if ALL states are false. False otherwise
+     * @return
+     */
     public boolean allFalse() {
         return holder == 0;
     }
