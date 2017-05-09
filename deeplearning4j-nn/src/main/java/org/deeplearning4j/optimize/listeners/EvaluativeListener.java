117dfd869edde43e05b872ad35d477c38b7f5963
raver119 <raver119@gmail.com>
2017-05-09 18:09:19 +0300

    - evaluative listener - micro tweaks for performance listener

---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/optimize/listeners/EvaluativeListener.java
@@ -0,0 +1,101 @@
+package org.deeplearning4j.optimize.listeners;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.eval.IEvaluation;
+import org.deeplearning4j.exception.DL4JInvalidInputException;
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.MultiDataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ *
+ *
+ * @author raver119@gmail.com
+ */
+@Slf4j
+public class EvaluativeListener implements IterationListener {
+    private ThreadLocal<AtomicLong> iterationCount = new ThreadLocal<>();
+    private int frequency;
+
+    private DataSetIterator dsIterator;
+    private MultiDataSetIterator mdsIterator;
+    private DataSet ds;
+    private MultiDataSet mds;
+
+    private IEvaluation[] evaluations;
+
+    public EvaluativeListener(@NonNull DataSetIterator iterator, int frequency) {
+        this(iterator, frequency, new Evaluation());
+    }
+
+    public EvaluativeListener(@NonNull DataSetIterator iterator, int frequency, IEvaluation... evaluations) {
+        this.dsIterator = iterator;
+        this.frequency = frequency;
+        this.evaluations = evaluations;
+    }
+
+    /**
+     * Get if listener invoked
+     */
+    @Override
+    public boolean invoked() {
+        return false;
+    }
+
+    /**
+     * Change invoke to true
+     */
+    @Override
+    public void invoke() {
+
+    }
+
+    /**
+     * Event listener for each iteration
+     *
+     * @param model     the model iterating
+     * @param iteration the iteration
+     */
+    @Override
+    public void iterationDone(Model model, int iteration) {
+        if (iterationCount.get() == null)
+            iterationCount.set(new AtomicLong(0));
+
+        if (iterationCount.get().getAndIncrement() % frequency != 0)
+            return;
+
+        log.info("Starting evaluation on iteration {}", iteration);
+        if (model instanceof MultiLayerNetwork) {
+            if (dsIterator != null) {
+                ((MultiLayerNetwork) model).doEvaluation(dsIterator, evaluations);
+            } else if (ds != null) {
+                for (IEvaluation evaluation : evaluations)
+                    evaluation.eval(ds.getLabels(), ((MultiLayerNetwork) model).output(ds.getFeatureMatrix()));
+            }
+        } else if (model instanceof ComputationGraph) {
+            if (dsIterator != null) {
+                ((ComputationGraph) model).doEvaluation(dsIterator, evaluations);
+            } else if (mdsIterator != null) {
+                ((ComputationGraph) model).doEvaluation(mdsIterator, evaluations);
+            } else if (ds != null) {
+                for (IEvaluation evaluation : evaluations)
+                    evaluation.eval(ds.getLabels(), ((ComputationGraph) model).output(ds.getFeatureMatrix())[0]);
+            } else if (mds != null) {
+                for (IEvaluation evaluation : evaluations)
+                    evaluation.eval(mds.getLabels()[0], ((ComputationGraph) model).output(mds.getFeatures())[0]);
+            }
+        } else throw new DL4JInvalidInputException("Model is unknown: " + model.getClass().getCanonicalName());
+
+        // TODO: need to print out something now. Callback probably
+    }
+}
