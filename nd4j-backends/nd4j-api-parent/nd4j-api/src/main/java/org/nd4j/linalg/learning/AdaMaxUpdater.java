fe50fb059c4c3b006416c1073d114d668aedc4d6
Alex Black <blacka101@gmail.com>
2017-05-09 19:03:05 +1000

    AdaMaxUpdater fix

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/learning/AdaMaxUpdater.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/learning/AdaMaxUpdater.java
@@ -62,6 +62,10 @@ public class AdaMaxUpdater implements GradientUpdater<AdaMax> {
         if (m == null || u == null)
             throw new IllegalStateException("Updater has not been initialized with view state");
 
+        //m = B_1 * m + (1-B_1)*grad
+        m.muli(config.getBeta1()).addi(gradient.mul(1-config.getBeta1()));
+
+        //u = max(B_2 * u, |grad|)
         u.muli(config.getBeta2());
         Transforms.abs(gradient,false);   //In-place should be OK here, original gradient values aren't used again later
         Nd4j.getExecutioner().exec(new Max(u,gradient,u,u.length()));
