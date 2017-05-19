76aeffcadc86c6c33e90c3e067a09236631b4271
François Garillot <francois@garillot.net>
2017-05-19 13:00:38 -0400

    formatting commit

--- deeplearning4j-zoo/src/main/java/org/deeplearning4j/zoo/ZooType.java
+++ deeplearning4j-zoo/src/main/java/org/deeplearning4j/zoo/ZooType.java
@@ -1,7 +1,7 @@
 package org.deeplearning4j.zoo;
 
 /**
- * Enumerator for choosing which CNN models to benchmark.
+ * Enumerator for choosing different models, and different types of models.
  */
 public enum ZooType {
     ALL, CNN, ALEXNET, LENET, VGG16, RESNET50, INCEPTIONRESNETV1, FACENETNN4SMALL2, RNN,
