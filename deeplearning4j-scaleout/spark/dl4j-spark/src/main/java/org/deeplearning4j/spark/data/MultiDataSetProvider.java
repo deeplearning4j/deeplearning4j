d1e2b15a6f84b7ebe3f87e13fbd135d4ebe52052
Adam Gibson <adam@skymind.io>
2017-05-28 19:06:54 +0900

    Transformprocessdatasetprovider (#3466)
    * add eval for spark computtationgraph
    
    * added new transformprocess call

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/MultiDataSetProvider.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/MultiDataSetProvider.java
@@ -2,6 +2,7 @@ package org.deeplearning4j.spark.data;
 
 import org.apache.spark.SparkContext;
 import org.apache.spark.rdd.RDD;
+import org.datavec.api.transform.TransformProcess;
 import org.nd4j.linalg.dataset.api.MultiDataSet;
 
 /**
@@ -17,4 +18,13 @@ public interface MultiDataSetProvider {
      * @return
      */
     RDD<MultiDataSet> data(SparkContext sparkContext);
+
+
+    /**
+     * (Optional) The transform process
+     * for the dataset provider.
+     * @return
+     */
+    TransformProcess transformProcess();
+
 }
