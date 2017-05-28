8adf29d919ea7bfb15f194050ff01b66589c3e61
Adam Gibson <adam@skymind.io>
2017-05-28 18:56:49 +0900

    added new transformprocess call

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/DataSetProvider.java
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/java/org/deeplearning4j/spark/data/DataSetProvider.java
@@ -2,6 +2,7 @@ package org.deeplearning4j.spark.data;
 
 import org.apache.spark.SparkContext;
 import org.apache.spark.rdd.RDD;
+import org.datavec.api.transform.TransformProcess;
 import org.nd4j.linalg.dataset.DataSet;
 
 /**
@@ -16,4 +17,12 @@ public interface DataSetProvider {
      * @return
      */
     RDD<DataSet> data(SparkContext sparkContext);
+
+    /**
+     * (Optional) The transform process
+     * for the dataset provider.
+     * @return
+     */
+    TransformProcess transformProcess();
+
 }
