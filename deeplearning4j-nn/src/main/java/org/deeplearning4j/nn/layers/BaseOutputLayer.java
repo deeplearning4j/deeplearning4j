9e38515d5bc16df6253069f8ab484d065095bdf4
Jonathan Arney <jarneyg@gmail.com>
2017-05-31 07:18:52 -0700

    Issue #3468 : Move loss function size assertions into loss functions to accomodate custom loss functions with label size != output size.

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseOutputLayer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseOutputLayer.java
@@ -173,11 +173,6 @@ public abstract class BaseOutputLayer<LayerConfT extends org.deeplearning4j.nn.c
     private Pair<Gradient, INDArray> getGradientsAndDelta(INDArray preOut) {
         ILossFunction lossFunction = layerConf().getLossFn();
         INDArray labels2d = getLabels2d();
-        if (labels2d.size(1) != preOut.size(1)) {
-            throw new DL4JInvalidInputException("Labels array numColumns (size(1) = " + labels2d.size(1)
-                            + ") does not match output layer" + " number of outputs (nOut = " + preOut.size(1)
-                            + ") " + layerId() );
-        }
         //INDArray delta = lossFunction.computeGradient(labels2d, preOut, layerConf().getActivationFunction(), maskArray);
         INDArray delta = lossFunction.computeGradient(labels2d, preOut, layerConf().getActivationFn(), maskArray);
 
