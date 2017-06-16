57d3ae6fcef8f3e3b0a19103aee5f9bd3fa5a9c8
raver119 <raver119@gmail.com>
2017-06-16 21:29:17 +0300

    impl p.9.4 debug info

--- deeplearning4j-scaleout/spark/dl4j-spark-parameterserver/src/main/java/org/deeplearning4j/spark/parameterserver/networking/SilentTrainingDriver.java
+++ deeplearning4j-scaleout/spark/dl4j-spark-parameterserver/src/main/java/org/deeplearning4j/spark/parameterserver/networking/SilentTrainingDriver.java
@@ -73,8 +73,12 @@ public class SilentTrainingDriver implements TrainingDriver<SilentUpdatesMessage
                 return;
             };
 
+            log.info("Applying message at Worker");
+
             accumulator.receiveUpdate(message.getUpdates());
         } else if (params != null && stepFunction != null) {
+
+            log.info("Applying message at Master");
             // master invokes everything
             synchronized (this) {
                 // TODO: change this to memset?
@@ -84,14 +88,14 @@ public class SilentTrainingDriver implements TrainingDriver<SilentUpdatesMessage
 
                 stepFunction.step(params, updates);
             }
+
+            // we should echo this message to everyone but this shard, but only if there's > 1 shard/client available
+            if (transport.numberOfKnownClients() > 1) {
+                //log.info("Resending message, skipping {}", message.getOriginatorId());
+                transport.sendMessageToAllClients(message, message.getOriginatorId());
+            }
         } else
             throw new DL4JInvalidConfigException("Neither GradientsAccumulator or StepFunction is defined!");
-
-        // we should echo this message to everyone but this shard, but only if there's > 1 shard/client available
-        if (transport.numberOfKnownClients() > 1) {
-            log.info("Resending message, skipping {}", message.getOriginatorId());
-            transport.sendMessageToAllClients(message, message.getOriginatorId());
-        }
     }
 
     @Override
