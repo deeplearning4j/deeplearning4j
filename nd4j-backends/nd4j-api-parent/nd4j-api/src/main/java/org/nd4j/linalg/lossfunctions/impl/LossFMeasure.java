380b4fa6b3bfc60d9e9eb820d689c24833e1098d
Alex Black <blacka101@gmail.com>
2017-06-26 16:57:28 +1000

    F-measure loss function: backprop fix, javadoc, cleanup

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossFMeasure.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossFMeasure.java
@@ -6,20 +6,35 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.ILossFunction;
 import org.nd4j.linalg.ops.transforms.Transforms;
+import org.nd4j.shade.jackson.annotation.JsonProperty;
 
 /**
- * Created by Alex on 15/06/2017.
+ * F–measure loss function is a loss function design for training on imbalanced datasets.
+ * Essentially, this loss function is a continuous approximation of the F_Beta evaluation measure, of which F_1 is
+ * a special case.<br>
+ *
+ * This implementation supports 2 types of operation:<br>
+ * - Binary: single output/label (Typically sigmoid activation function)<br>
+ * - Binary: 2-output/label (softmax activation function + 1-hot labels)<br>
+ * Note that the beta value can be configured
+ * <br>
+ * The following situations are not currently supported, may be added in the future:
+ * - Multi-label (multiple independent binary outputs)<br>
+ * - Multiclass (via micro or macro averaging)<br>
+ *
+ * <br>
+ * Reference: Pastor-Pellicer et al. (2013), F-Measure as the Error Function to Train Neural Networks,
+ * <a href="https://link.springer.com/chapter/10.1007/978-3-642-38679-4_37">
+ *     https://link.springer.com/chapter/10.1007/978-3-642-38679-4_37</a>
+ *
+ * @author Alex Black
  */
 public class LossFMeasure implements ILossFunction {
 
     /**
-     * Cases:
-     * - Binary: single output
-     * - Binary: 2 outputs (softmax style)
-     * - Multiple independent binary
-     * - Multiclass (via micro or macro averaging) -> not supported; do this in a separate loss function
-     *
-     *
+     * Multiple modes are supported in this implementation of the F-Measure loss function.<br>
+     * - BINARY: single output (sigmoid style) OR 2 outputs (softmax style)<br>
+     * - BINARY_MULTILABEL: Multiple independent binary classes<br>
      */
     public enum OutputType {BINARY, BINARY_MULTILABEL};
 
@@ -28,11 +43,22 @@ public class LossFMeasure implements ILossFunction {
     private final OutputType outputType;
     private final double beta;
 
+    public LossFMeasure() {
+        this(DEFAULT_BETA);
+    }
+
+    public LossFMeasure(double beta){
+        this(OutputType.BINARY, beta);
+    }
+
     public LossFMeasure(OutputType outputType){
         this(outputType, DEFAULT_BETA);
     }
 
-    public LossFMeasure(OutputType outputType, double beta){
+    public LossFMeasure(@JsonProperty("outputType") OutputType outputType, @JsonProperty("beta") double beta){
+        if(beta <= 0){
+            throw new UnsupportedOperationException("Invalid value: beta must be >= 0. Got: " + beta);
+        }
         this.outputType = outputType;
         this.beta = beta;
     }
@@ -53,12 +79,11 @@ public class LossFMeasure implements ILossFunction {
     }
 
     private double[] computeScoreNumDenom(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask, boolean average){
-
         INDArray output = activationFn.getActivation(preOutput.dup(), true);
 
         int n = labels.size(1);
         if(outputType == OutputType.BINARY && n != 1 && n != 2){
-            throw new UnsupportedOperationException("For binary classification: expect output size of 1 or 2");
+            throw new UnsupportedOperationException("For binary classification: expect output size of 1 or 2. Got: " + n);
         } else if(outputType == OutputType.BINARY_MULTILABEL){
             throw new UnsupportedOperationException("binary multi-label not yet supported");
         }
@@ -114,20 +139,27 @@ public class LossFMeasure implements ILossFunction {
 
         double secondTerm = numerator / (denominator * denominator);
 
-        INDArray grad = labels.mul(1+beta*beta).divi(denominator).subi(secondTerm);
+        INDArray dLdOut = labels.mul(1+beta*beta).divi(denominator).subi(secondTerm);
+
+        INDArray dLdPreOut = activationFn.backprop(preOutput, dLdOut).getFirst();
 
         if(mask != null){
-            grad.muliColumnVector(mask);
+            dLdPreOut.muliColumnVector(mask);
         }
 
-        return grad;
+        return dLdPreOut;
     }
 
     @Override
     public Pair<Double, INDArray> computeGradientAndScore(INDArray labels, INDArray preOutput, IActivation activationFn, INDArray mask, boolean average) {
-        //TODO optimize to reduce 
+        //TODO optimize
         return new Pair<>(
                 computeScore(labels, preOutput, activationFn, mask, average),
                 computeGradient(labels, preOutput, activationFn, mask));
+    }
+
+    @Override
+    public String toString(){
+        return "LossFMeasure(outputType=" + outputType + ")";
     }
 }
