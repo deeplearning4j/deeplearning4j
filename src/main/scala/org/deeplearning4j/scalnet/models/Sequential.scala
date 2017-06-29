72fddc71d0b8564c247e19457c2618e02550d9e8
Max Pumperla <max.pumperla@googlemail.com>
2017-06-29 22:04:32 +0200

    shape inference and checking, better logging

--- src/main/scala/org/deeplearning4j/scalnet/models/Sequential.scala
+++ src/main/scala/org/deeplearning4j/scalnet/models/Sequential.scala
@@ -21,12 +21,12 @@ package org.deeplearning4j.scalnet.models
 import org.deeplearning4j.nn.conf.{NeuralNetConfiguration, Updater}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.optimize.api.IterationListener
-import org.deeplearning4j.scalnet.layers.{Layer, Node, Output, Preprocessor}
-import org.deeplearning4j.scalnet.layers.convolutional.Convolution
+import org.deeplearning4j.scalnet.layers._
 import org.deeplearning4j.scalnet.optimizers.{Optimizer, SGD}
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
+import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.JavaConverters._
 
@@ -43,82 +43,80 @@ import scala.collection.JavaConverters._
   * @author David Kale
   */
 class Sequential(val rngSeed: Long = 0) extends Model {
-  private var layers: List[Node] = List()
-  private var model: MultiLayerNetwork = _
+
+  private val log: Logger = LoggerFactory.getLogger(this.getClass)
+
   private var preprocessors: Map[Int, Node] = Map()
   private var _inputShape: List[Int] = List()
+  private val seed: Long = rngSeed
 
   def inputShape: List[Int] = _inputShape
 
-  def add(layer: Node): Unit = {
-    if (preprocessors.contains(layers.length))
-      layer.inputShape = preprocessors(layers.length).outputShape
-    else if (layers.nonEmpty)
-      layer.inputShape = layers.last.outputShape
-    else if (layer.inputShape.length == 1 && layer.inputShape.head == 0)
-      throw new IllegalArgumentException("Input layer must have non-empty inputShape")
-    else if (inputShape.isEmpty && layers.isEmpty && preprocessors.isEmpty)
-      _inputShape = layer.inputShape
-
-    println("in=" + layer.inputShape + " out=" + layer.outputShape)
-
-    layer match {
-      case l: Preprocessor =>
-        preprocessors = preprocessors + (layers.length -> layer)
-      case _ =>
-        layers = layers :+ layer
+  def inferInputShape(layer: Node): List[Int] = {
+    if (preprocessors.contains(layers.length)) {
+      preprocessors(layers.length).outputShape
+    } else if (layers.nonEmpty) {
+      layers.last.outputShape
+    } else {
+      layer.inputShape
     }
   }
 
-  override def compile(lossFunction: LossFunction = null, optimizer: Optimizer = SGD(lr = 0.01)): Unit = {
-    var builder: NeuralNetConfiguration.Builder = new NeuralNetConfiguration.Builder()
-    if (rngSeed != 0)
-      builder = builder.seed(rngSeed)
-    optimizer match {
-      case o: SGD =>
-        builder = builder.optimizationAlgo(o.optimizationAlgorithm)
-          .learningRate(o.lr)
-        if (o.nesterov)
-          builder = builder.updater(Updater.NESTEROVS).momentum(o.momentum)
-      case _ =>
-        builder = builder.optimizationAlgo(optimizer.optimizationAlgorithm)
-          .learningRate(optimizer.asInstanceOf[SGD].lr)
+  def checkShape(layer: Node): Unit = {
+    if (!(preprocessors.contains(layers.length) || layers.nonEmpty)
+      && layer.inputShape.length == 1 && layer.inputShape.head == 0) {
+      throw new IllegalArgumentException("Input layer must have non-empty inputShape")
+    } else if (inputShape.isEmpty && layers.isEmpty && preprocessors.isEmpty) {
+      _inputShape = layer.inputShape
     }
+  }
+
+
+  def add(layer: Node): Unit = {
+    val inferredInput: List[Int] = inferInputShape(layer)
+    checkShape(layer)
+    val inferredLayer = layer.reshapeInput(inferredInput)
+    inferredLayer match {
+      case _: Preprocessor =>
+        preprocessors = preprocessors + (layers.length -> inferredLayer)
+      case _ =>
+        layers = layers :+ inferredLayer
+    }
+  }
+
+  override def compile(lossFunction: LossFunction, optimizer: Optimizer = defaultOptimizer): Unit = {
+    val builder = buildModelConfig(optimizer, seed)
+    buildOutput(lossFunction)
 
     var listBuilder: NeuralNetConfiguration.ListBuilder = builder.iterations(1).list()
-    if (!layers.last.isInstanceOf[Output])
-      throw new IllegalArgumentException("Last layer must have Output trait")
-    else if (!layers.last.asInstanceOf[Output].isOutput)
-      layers.last.asInstanceOf[Output].makeOutput(lossFunction)
     for ((layer, layerIndex) <- layers.zipWithIndex) {
-      println("Layer " + layerIndex + ": " + layer.getClass.getSimpleName)
+      log.info("Layer " + layerIndex + ": " + layer.getClass.getSimpleName)
+      log.info(" size: " + layer.descibe())
       listBuilder.layer(layerIndex, layer.asInstanceOf[Layer].compile)
     }
     for ((layerIndex, preprocessor) <- preprocessors) {
-      println("Preprocessor " + layerIndex + ": " + preprocessor.getClass.getSimpleName)
+      log.info("Preprocessor " + layerIndex + ": " + preprocessor.getClass.getSimpleName)
+      log.info(" size: " + preprocessor.descibe())
       listBuilder.inputPreProcessor(layerIndex, preprocessor.asInstanceOf[Preprocessor].compile)
     }
     listBuilder = listBuilder.pretrain(false).backprop(true)
-
-    println("Length: " + layers.length)
-    for ((layer, layerIndex) <- layers.zipWithIndex) {
-      println(layerIndex + " size: " + layer.inputShape)
-    }
 
     model = new MultiLayerNetwork(listBuilder.build())
     model.init()
   }
 
-  override def fit(iter: DataSetIterator, nbEpoch: Int = 10, listeners: List[IterationListener]): Unit = {
+  override def fit(iter: DataSetIterator, nbEpoch: Int = defaultEpochs, listeners: List[IterationListener]): Unit = {
     model.setListeners(listeners.asJavaCollection)
-    for (epoch <- 0 until nbEpoch)
+    for (_ <- 0 until nbEpoch)
       model.fit(iter)
   }
 
   override def predict(x: INDArray): INDArray = model.output(x, false)
 
   override def toString: String = model.getLayerWiseConfigurations.toString
+
   override def toJson: String = model.getLayerWiseConfigurations.toJson
+
   override def toYaml: String = model.getLayerWiseConfigurations.toYaml
 
   def getNetwork: MultiLayerNetwork = model
