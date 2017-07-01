ec2ed9e71f82ccce1c4f16dba9f339c0b61a27f9
Adam Gibson <adam@skymind.io>
2017-07-01 16:50:38 -0500

    Nearest neighbors break up (#3600)
    * add eval for spark computtationgraph
    
    * added new transformprocess call
    
    * make parallelwrappermain emeddable within another main
    
    * local
    
    * remove old parallelwrapper invocations /cc @raver119
    
    * update nn server to have scala version
    
    * add knn for data point
    
    * get rid of org.json dep
    
    * change vptree storage to float
    
    * updated float functions using counter in nlp modules
    
    * get rid of distance caching
    
    * parallelize tree search
    
    * update pom configurations for new layout
    
    * fix bytebuffer loading

---
+++ deeplearning4j-nearestneighbors-parent/deeplearning4j-nearestneighbor-server/src/test/java/org/deeplearning4j/nearestneighbor/server/NearestNeighborsServerTest.java
@@ -0,0 +1,67 @@
+package org.deeplearning4j.nearestneighbor.server;
+
+import org.apache.commons.io.IOUtils;
+import org.deeplearning4j.nearestneighbor.client.NearestNeighborsClient;
+import org.deeplearning4j.nearestneighbor.model.NearstNeighborsResults;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.serde.binary.BinarySerde;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by agibsonccc on 1/22/17.
+ */
+public class NearestNeighborsServerTest {
+
+    private static NearestNeighborsServer server;
+    private static File fileSave = new File(UUID.randomUUID().toString() + ".json");
+    private static INDArray arr;
+    @BeforeClass
+    public static void before() throws Exception {
+        server = new NearestNeighborsServer();
+        arr = Nd4j.create(new double[][]{
+                {1,2,3,4},
+                {1,2,3,5},
+                {3,4,5,6}
+        });
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileSave);
+        ByteBuffer byteBuffer = BinarySerde.toByteBuffer(arr);
+        byte[] allBuffer = new byte[byteBuffer.capacity()];
+        byteBuffer.get(allBuffer);
+        IOUtils.write(allBuffer,fileOutputStream);
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        server.runMain(new String[] {"--ndarrayPath", fileSave.getAbsolutePath(), "--nearestNeighborsPort", "9050"});
+    }
+
+    @AfterClass
+    public static void after() throws Exception {
+        fileSave.delete();
+        server.stop();
+
+    }
+
+    @Test
+    public void testServer() throws Exception {
+        NearestNeighborsClient nearestNeighborsClient = new NearestNeighborsClient("http://localhost:9050");
+        NearstNeighborsResults csvRecord = nearestNeighborsClient.knn(0,1);
+
+        assertEquals(1,csvRecord.getResults().size());
+
+        NearstNeighborsResults csvRecord2 = nearestNeighborsClient.knnNew(1,arr.slice(0));
+        assertEquals(1,csvRecord2.getResults().size());
+
+
+    }
+
+}
