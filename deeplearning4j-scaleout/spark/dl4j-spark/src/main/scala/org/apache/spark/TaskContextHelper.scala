9eceb1b22258e55bdd9433c2ce3ba04c15eee2a6
raver119 <raver119@gmail.com>
2017-07-03 10:36:03 +0300

    javadoc

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/scala/org/apache/spark/TaskContextHelper.scala
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/scala/org/apache/spark/TaskContextHelper.scala
@@ -2,6 +2,8 @@ package org.apache.spark
 
 
 /**
+  * This simple helper is used to get access to package-protected Scala TaskContext.setTaskContext method
+  *
   * @author raver119@gmail.com
   */
 object TaskContextHelper {
