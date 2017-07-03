b32b84df2e7583e2fb57e91ba2d94da8f6b72005
François Garillot <francois@garillot.net>
2017-07-03 17:54:29 -0400

    Improve buildmultiplescalaversions.sh (#3615)
    * make failures of buildmultiplescalaversions.sh more informative
    
    * change the Spark defualt verison back to 1

--- buildmultiplescalaversions.sh
+++ buildmultiplescalaversions.sh
@@ -1,23 +1,46 @@
 #! /bin/bash
 BASEDIR=$(dirname $(readlink -f "$0"))
 
+function echoError() {
+    (>&2 echo $1)
+}
+
+function sparkError() {
+    echoError "Changing Spark major version to 2 in the build did not change the state of your working copy, is Spark 1.x still the default ?"
+    exit 2
+}
+
+function scalaError() {
+    echoError "Changing Scala major version to 2.10 in the build did not change the state of your working copy, is Scala 2.11 still the default ?"
+    exit 2
+}
+
 function whatchanged() {
     cd $BASEDIR
     for i in $(git status -s --porcelain -- $(find ./ -mindepth 2 -name pom.xml)|awk '{print $2}'); do
-	echo $(dirname $i)
-	cd $BASEDIR
+        echo $(dirname $i)
+        cd $BASEDIR
     done
 }
 function beep(){ echo -e '\a';}
 
 set -eu
-./change-scala-versions.sh 2.11 # should be idempotent, this is the default
+./change-scala-versions.sh 2.11
+./change-spark-versions.sh 1 # should be idempotent, this is the default
 mvn "$@"
 ./change-spark-versions.sh 2
-mvn -Dmaven.clean.skip=true -pl $(whatchanged| tr '\n' ',') -amd "$@"
+if [ -z $(whatchanged)]; then
+    sparkError;
+else
+    mvn -Dmaven.clean.skip=true -pl $(whatchanged| tr '\n' ',') -amd "$@"
+fi
 ./change-scala-versions.sh 2.10
 ./change-spark-versions.sh 1
-mvn -Dmaven.clean.skip=true -pl $(whatchanged| tr '\n' ',') -amd "$@"
+if [ -z $(whatchanged)]; then
+    scalaError;
+else
+    mvn -Dmaven.clean.skip=true -pl $(whatchanged| tr '\n' ',') -amd "$@"
+fi
 ./change-scala-versions.sh 2.11 # back to the default
 
 # notify user the build is done
