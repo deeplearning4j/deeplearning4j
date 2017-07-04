24403b9d77bdf41daccebd79ee437c9af2e63d36
raver119 <raver119@gmail.com>
2017-07-04 06:46:04 +0300

    link to spark issue tracker

--- deeplearning4j-scaleout/spark/dl4j-spark/src/main/scala/org/apache/spark/TaskContextHelper.scala
+++ deeplearning4j-scaleout/spark/dl4j-spark/src/main/scala/org/apache/spark/TaskContextHelper.scala
@@ -2,7 +2,8 @@ package org.apache.spark
 
 
 /**
-  * This simple helper is used to get access to package-protected Scala TaskContext.setTaskContext method
+  * This simple helper is used to get access to package-protected Scala TaskContext.setTaskContext method.
+  * For more details, please read: https://issues.apache.org/jira/browse/SPARK-18406
   *
   * @author raver119@gmail.com
   */
