b640779af1790aa0a5d4ac8f218b85547e31a16c
Adam Gibson <adam@skymind.io>
2017-07-11 02:09:06 +0800

    fix vector

--- jumpy/__init__.py
+++ jumpy/__init__.py
@@ -1,11 +1,64 @@
 import jnius_config
 import os
 
+
 try:
     jnius_classpath = os.environ['JUMPY_CLASS_PATH']
 except KeyError:
-    raise 'Please specify a jar or directory for JUMP_CLASS_PATH'
+    raise 'Please specify a jar or directory for JUMPY_CLASS_PATH in the environment'
+
+
+def get_classpath(base_path):
+
+    """
+    Get the classpath of based on the given folder.
+    :param base_path: the directory to get the classpath for
+    :return:
+    """
+
+    ret = ''
+    for jar_file in os.listdir(base_path):
+        ret += base_path + '/' + jar_file + ':'
+    return ret
+
+
+def _expand_directory(directory):
+        if directory.__contains__('*'):
+            # Get only the directory name (no wild card)
+            jars = get_classpath(directory[:-2])
+        else:
+            jars = get_classpath(directory)
+
+        return jars
+
+new_class_path = ''
+class_path_list = jnius_classpath.split(':')
+
+
+if len(class_path_list) > 0 and len(class_path_list[0]) > 0:
+    for class_path_item in class_path_list:
+        if class_path_item.endswith('jar'):
+            new_class_path += class_path_item
+        # wild card expansions like /somedir/* (we do this because of jnius being unable to handle class path expansion
+        else:
+            new_class_path += _expand_directory(class_path_item)
+    # update class path
+
+else:
+    class_path_item = jnius_classpath
+    if class_path_item.endswith('jar'):
+        new_class_path += class_path_item
+        # wild card expansions like /somedir/* (we do this because of jnius being unable to handle class path expansion
+    else:
+        new_class_path += _expand_directory(class_path_item)
+
+
+jnius_classpath = new_class_path
+
+
 jnius_config.set_classpath(jnius_classpath)
+
+# after jnius is initialized with proper class path *then* we setup nd4j
 
 from jnius import autoclass
 
@@ -43,6 +96,8 @@ def get_buffer_from_arr(np_arr):
         return nd4j.createBuffer(as_int, size)
 
 
+
+
 def get_array_address(np_arr):
     """
     :param np_arr: The numpy array to get the pointer address for
@@ -55,10 +110,21 @@ def get_array_address(np_arr):
 def from_np(np_arr):
     """
 
-    :param np_arr:
+    Create an nd4j ndarray from a numpy array (passing the
+    numpy pointer buffer by reference)
+
+    :param np_arr: a numpy array
     :return:
     """
+
+    import numpy as np
+    # nd4j doesn't have 1d arrays. Convert to a row vector
+    if np_arr.ndim == 1:
+        np_arr = np.reshape(np_arr, (1, np_arr.size))
+
     data_buffer = get_buffer_from_arr(np_arr)
     #   note here we divide the strides by 8 for numpy
     # the reason we do this is because numpy's strides are based on bytes rather than words
     return nd4j.create(data_buffer, np_arr.shape, map(lambda x: x / 8, np_arr.strides), 0)
+
+
--- tests/jumpy/array_creation.py
+++ tests/jumpy/array_creation.py
@@ -1,12 +1,11 @@
 import unittest
 
 import numpy as np
-from jnius import JavaException
 
 from jumpy import *
 
-
 class TestArrayCreation(unittest.TestCase):
+
     def test_arr_creation(self):
         a = np.linspace(1, 4, 4).reshape(2, 2)
         nd4j_arr = from_np(a)
@@ -14,10 +13,11 @@ class TestArrayCreation(unittest.TestCase):
         self.assertEqual(4, length)
         self.assertEquals(list(a.shape),nd4j_arr.shape())
         self.assertEquals(map(lambda x: x / 8, list(a.strides)), nd4j_arr.stride())
-        try:
-            self.assertEquals(1.0, nd4j_arr.data().getDouble(0))
-            self.assertEquals(1.0, nd4j_arr.getDouble(0, 0))
-            system.out.println(nd4j_arr)
-        except JavaException as e:
-            print e.stacktrace
+        self.assertEquals(1.0, nd4j_arr.data().getDouble(0))
+        self.assertEquals(1.0, nd4j_arr.getDouble(0, 0))
+
+    def test_arr_creation_vector(self):
+        a = np.linspace(1,4,4)
+        nd4j_arr = from_np(a)
+        self.assertEqual(2,nd4j_arr.rank())
 
