04ad1f23dcb118e5f10e7363a0b089a5f320e14e
Alex Black <blacka101@gmail.com>
2017-07-11 16:39:18 +1000

    Fix test (expected case) for concat

--- deeplearning4j-core/src/test/java/org/deeplearning4j/datasets/datavec/RecordReaderDataSetiteratorTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/datasets/datavec/RecordReaderDataSetiteratorTest.java
@@ -1132,7 +1132,7 @@ public class RecordReaderDataSetiteratorTest {
         DataSetIterator iter = new RecordReaderDataSetIterator(rr, 1);
 
         DataSet ds = iter.next();
-        INDArray expF = Nd4j.create(new double[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+        INDArray expF = Nd4j.create(new double[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
 
         assertEquals(expF, ds.getFeatures());
     }
