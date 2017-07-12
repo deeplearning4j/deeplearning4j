9826b801362123adb704f0bed3b52eea20d805e1
Adam Gibson <adam@skymind.io>
2017-07-12 22:38:28 +0800

    more benchmarks

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -4,19 +4,35 @@ import timeit
 
 
 class Benchmark(object):
-    def __init__(self):
-        self.np_arr = np.linspace(1, 10000, 10000)
+    def __init__(self, n=100):
+        self.np_arr = np.linspace(1, n * n, n * n).reshape((n,n))
         self.nd4j_arr = jumpy.from_np(self.np_arr)
 
-    def run_nd4j(self):
+    def run_nd4j_add(self):
         self.nd4j_arr += self.nd4j_arr
 
-    def run_numpy(self):
+    def run_numpy_add(self):
         self.np_arr += self.np_arr
 
+    def run_numpy_sub(self):
+        self.np_arr -= self.np_arr
+
+    def run_nd4j_sub(self):
+        self.nd4j_arr -= self.nd4j_arr
+
+    def run_nd4j_mmul(self):
+        jumpy.dot(self.nd4j_arr,self.nd4j_arr)
+
+    def run_numpy_mmul(self):
+        np.dot(self.np_arr,self.np_arr)
+
     def run_benchmark(self,n_trials=1000):
-        print 'nd4j ', timeit.timeit(self.run_nd4j, number=n_trials)
-        print 'numpy ', timeit.timeit(self.run_numpy, number=n_trials)
+        print 'nd4j add ', timeit.timeit(self.run_nd4j_add, number=n_trials)
+        print 'numpy add ', timeit.timeit(self.run_numpy_add, number=n_trials)
+        print 'nd4j sub ', timeit.timeit(self.run_nd4j_sub, number=n_trials)
+        print 'numpy sub ', timeit.timeit(self.run_numpy_sub, number=n_trials)
+        print 'nd4j mmul ', timeit.timeit(self.run_nd4j_mmul, number=n_trials)
+        print 'numpy mmul ', timeit.timeit(self.run_numpy_mmul, number=n_trials)
 
 
 
