c6600c597d7c2b660cff80ab6d9e0e7cde24f6c1
Adam Gibson <adam@skymind.io>
2017-07-13 13:45:11 +0800

    formatting

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -3,15 +3,16 @@ from jumpy import Nd4jArray
 import numpy as np
 import timeit
 import gc
-gc.disable()
 
+gc.disable()
 jumpy.disable_gc()
+
 
 class Benchmark(object):
     def __init__(self, n=80):
         self.np_arr = np.linspace(1, n * n, n * n).reshape((n, n))
-        self.nd4j_arr = Nd4jArray(jumpy.nd4j.linspace(1,n * n,n * n).reshape(n,n))
-        #self.nd4j_arr = jumpy.from_np(np.copy(self.np_arr))
+        self.nd4j_arr = Nd4jArray(jumpy.nd4j.linspace(1, n * n, n * n).reshape(n, n))
+        # self.nd4j_arr = jumpy.from_np(np.copy(self.np_arr))
 
     def run_nd4j_add(self):
         self.nd4j_arr += self.nd4j_arr
@@ -26,12 +27,12 @@ class Benchmark(object):
         self.nd4j_arr -= self.nd4j_arr
 
     def run_nd4j_mmul(self):
-        jumpy.dot(self.nd4j_arr,self.nd4j_arr)
+        jumpy.dot(self.nd4j_arr, self.nd4j_arr)
 
     def run_numpy_mmul(self):
-        np.dot(self.np_arr,self.np_arr)
+        np.dot(self.np_arr, self.np_arr)
 
-    def run_benchmark(self,n_trials=1000):
+    def run_benchmark(self, n_trials=1000):
         print 'nd4j add ', timeit.timeit(self.run_nd4j_add, number=n_trials)
         print 'numpy add ', timeit.timeit(self.run_numpy_add, number=n_trials)
         print 'nd4j sub ', timeit.timeit(self.run_nd4j_sub, number=n_trials)
@@ -40,9 +41,5 @@ class Benchmark(object):
         print 'numpy mmul ', timeit.timeit(self.run_numpy_mmul, number=n_trials)
 
 
-
 benchmark = Benchmark()
 benchmark.run_benchmark()
-
-
-
