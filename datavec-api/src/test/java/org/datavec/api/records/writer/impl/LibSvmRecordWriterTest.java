3114e36e83e035d8cd38e377e713d2bb33fa9f57
turambar <turinturumarth@gmail.com>
2017-07-14 11:18:22 -0700

    Change LibSvmRecordWriter "testNDArrayWritables" unit test to use try-with-resources.

--- datavec-api/src/test/java/org/datavec/api/records/writer/impl/LibSvmRecordWriterTest.java
+++ datavec-api/src/test/java/org/datavec/api/records/writer/impl/LibSvmRecordWriterTest.java
@@ -179,13 +179,13 @@ public class LibSvmRecordWriterTest {
 
         String lineOriginal = "13.0,14.0,15.0,4 1:1.0 2:11.0 3:12.0 4:2.0 5:3.0";
 
-        LibSvmRecordWriter writer = new LibSvmRecordWriter(tempFile, true);
-        Configuration configWriter = new Configuration();
-        configWriter.setInt(LibSvmRecordWriter.FEATURE_FIRST_COLUMN, 0);
-        configWriter.setInt(LibSvmRecordWriter.FEATURE_LAST_COLUMN, 3);
-        writer.setConf(configWriter);
-        writer.write(record);
-        writer.close();
+        try     (LibSvmRecordWriter writer = new LibSvmRecordWriter(tempFile, true)) {
+            Configuration configWriter = new Configuration();
+            configWriter.setInt(LibSvmRecordWriter.FEATURE_FIRST_COLUMN, 0);
+            configWriter.setInt(LibSvmRecordWriter.FEATURE_LAST_COLUMN, 3);
+            writer.setConf(configWriter);
+            writer.write(record);
+        }
 
         String lineNew = FileUtils.readFileToString(tempFile).trim();
         assertEquals(lineOriginal, lineNew);
