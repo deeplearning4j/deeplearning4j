94d55d01e4c85e20046aa00cbc407ecff04d0496
Adam Gibson <adam@skymind.io>
2017-07-15 13:53:57 +0800

    licensing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(name='jumpy',
-      version='0.2.1',
+      version='0.2.2',
       description='Numpy and nd4j interop',
       author='Adam Gibson',
       author_email='adam@skymind.io',
