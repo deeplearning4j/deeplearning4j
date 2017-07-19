20f6a9902711b04938ba06437609d1ea1729aaf2
François Garillot <francois@garillot.net>
2017-07-18 21:04:48 -0400

    Test classpath expansion
    also remove obsolete test dependency

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='jumpy',
           'visualize': ['pydot-ng'],
           'tests': ['pytest',
                     'pytest-pep8',
-                    'pytest-xdist'],
+                    'mock'],
       },
       packages=find_packages())
