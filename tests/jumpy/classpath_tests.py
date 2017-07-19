cb1937d13c016c164553f2d5f25ad9fa250d8f76
François Garillot <francois@garillot.net>
2017-07-19 13:22:17 -0400

    Extend classpath tests to jar / wildcard mix

--- tests/jumpy/classpath_tests.py
+++ tests/jumpy/classpath_tests.py
@@ -11,7 +11,16 @@ class TestClassPath(unittest.TestCase):
         os.listdir = MagicMock(return_value=['nd4j-buffer-0.8.1-20170627.172301-977.jar', 'leptonica-1.73-1.3-linux-x86_64.jar'])
 
         res = '/opt/skil/lib/nd4j-buffer-0.8.1-20170627.172301-977.jar:/opt/skil/lib/leptonica-1.73-1.3-linux-x86_64.jar'
-        # this test won't work on Windows (though the function it's testing would)
+        # this test won't work on Windows (though the function it's testing will)
         if (os.path.sep == '/'):
             # the directory argument is moot
             self.assertEqual(get_classpath('/opt/skil/lib/*'), res)
+
+    def test_mix_jar_wildcard(self):
+        os.listdir = MagicMock(return_value=['nd4j-buffer-0.8.1-20170627.172301-977.jar', 'leptonica-1.73-1.3-linux-x86_64.jar'])
+
+        res = '/opt/skil/lib/nd4j-buffer-0.8.1-20170627.172301-977.jar:/opt/skil/lib/nd4j-buffer-0.8.1-20170627.172301-977.jar:/opt/skil/lib/leptonica-1.73-1.3-linux-x86_64.jar'
+        # this test won't work on Windows (though the function it's testing will)
+        if (os.path.sep == '/'):
+            # the directory argument is moot
+            self.assertEqual(get_classpath('/opt/skil/lib/nd4j-buffer-0.8.1-20170627.172301-977.jar:/opt/skil/lib/*'), res)
