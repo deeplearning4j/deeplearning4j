71e95c28321584c1c22e3e752b098be3954c7a10
raver119 <raver119@gmail.com>
2017-07-27 11:56:18 +0300

    Revert "r119_averaging"
    This reverts commit 4f21cf7

--- nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/CpuNDArrayFactory.java
+++ nd4j-backends/nd4j-backend-impls/nd4j-native/src/main/java/org/nd4j/linalg/cpu/nativecpu/CpuNDArrayFactory.java
@@ -838,7 +838,7 @@ public class CpuNDArrayFactory extends BaseNDArrayFactory {
         if (arrays.length == 1)
             return target.assign(arrays[0]);
 
-        long len = target != null ? target.lengthLong() : arrays[0].length();
+        long len = target.lengthLong();
 
         PointerPointer dataPointers = new PointerPointer(arrays.length);
 
@@ -855,13 +855,14 @@ public class CpuNDArrayFactory extends BaseNDArrayFactory {
         }
 
         if (target.data().dataType() == DataBuffer.Type.DOUBLE) {
-            nativeOps.averageDouble(null, dataPointers, target == null ? null : (DoublePointer) target.data().addressPointer(), arrays.length,
+            nativeOps.averageDouble(null, dataPointers, (DoublePointer) target.data().addressPointer(), arrays.length,
                     len, true);
         } else if (target.data().dataType() == DataBuffer.Type.FLOAT) {
-            nativeOps.averageFloat(null, dataPointers, target == null ? null : (FloatPointer) target.data().addressPointer(), arrays.length,
+            nativeOps.averageFloat(null, dataPointers, (FloatPointer) target.data().addressPointer(), arrays.length,
                     len, true);
         } else {
-            nativeOps.averageHalf(null, dataPointers, target == null ? null :  (ShortPointer) target.data().addressPointer(), arrays.length, len, true);
+            nativeOps.averageHalf(null, dataPointers, (ShortPointer) target.data().addressPointer(), arrays.length, len,
+                    true);
         }
 
         return target;
