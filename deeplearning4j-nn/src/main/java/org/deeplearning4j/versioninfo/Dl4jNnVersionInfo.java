3d36cc116a39c8b9760abb3348cb7f8c50014606
Alex Black <blacka101@gmail.com>
2017-08-03 21:58:10 +1000

    Add version check functionality to deeplearning4j-nn

---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/versioninfo/Dl4jNnVersionInfo.java
@@ -0,0 +1,13 @@
+package org.deeplearning4j.versioninfo;
+
+import org.nd4j.linalg.versioncheck.VersionInfo;
+
+/**
+ * Created by Alex on 03/08/2017.
+ */
+public class Dl4jNnVersionInfo extends VersionInfo {
+    @Override
+    public String getGroupId() {
+        return "org.deeplearning4j";
+    }
+}
