b3faffb2386c97a8dc4046a6c01c3751e182c9bc
Adam Gibson <adam@skymind.io>
2017-08-08 00:48:12 +0900

    fix interval

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/indexing/IndexingTestsC.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/api/indexing/IndexingTestsC.java
@@ -52,6 +52,18 @@ public class IndexingTestsC extends BaseNd4jTest {
 
 
     @Test
+    public void testIntervalsIn3D() {
+        INDArray arr = Nd4j.arange(8).reshape(2, 2, 2);
+        INDArray assertion = Nd4j.create(new double[][]{
+                {4, 5},
+                {6,7}
+        }).reshape(1,2,2);
+        INDArray rest = arr.get(interval(1, 2), interval(0, 2), interval(0, 2));
+        assertEquals(assertion,rest);
+
+    }
+
+    @Test
     public void testSmallInterval() {
         INDArray arr = Nd4j.arange(8).reshape(2, 2, 2);
         INDArray assertion = Nd4j.create(new double[][]{
