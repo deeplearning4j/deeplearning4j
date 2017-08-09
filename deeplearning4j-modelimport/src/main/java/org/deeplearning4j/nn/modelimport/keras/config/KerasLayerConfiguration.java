1ce140ef9c5813c0ae7ab06fdd092a20ebc83c58
Max Pumperla <max.pumperla@googlemail.com>
2017-08-09 10:55:24 +0200

    [WIP] documentation and minor refactoring of shared config

--- deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/config/KerasLayerConfiguration.java
+++ deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/config/KerasLayerConfiguration.java
@@ -1,15 +1,46 @@
+/*-
+ *
+ *  * Copyright 2017 Skymind,Inc.
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
 package org.deeplearning4j.nn.modelimport.keras.config;
-
 
 import lombok.Data;
 
+
+/**
+ * All relevant property fields of keras layers.
+ *
+ * Empty String fields mean Keras 1 and 2 implementations differ,
+ * supplied fields stand for shared properties.
+ *
+ * @author Max Pumperla
+ */
 @Data
 public class KerasLayerConfiguration {
 
     private final String LAYER_FIELD_KERAS_VERSION = "keras_version";
     private final String LAYER_FIELD_CLASS_NAME = "class_name";
 
-    /* Basic layer names */
+    /**
+     *  Basic layer names
+     *  */
+    // Missing Layers: Reshape, Permute, RepeatVector, Lambda, ActivityRegularization, Masking
+    // Conv1D, Conv3D, SeparableConv2D, Conv2DTranspose, Cropping1D-3D, UpSampling1D-3D, ZeroPadding3D
+    // LocallyConnected1D-2D
+    // Missing layers from keras 1: AtrousConvolution1D-2D, Highway, MaxoutDense
     private final String LAYER_CLASS_NAME_ACTIVATION = "Activation";
     private final String LAYER_CLASS_NAME_INPUT = "InputLayer";
     private final String LAYER_CLASS_NAME_DROPOUT = "Dropout";
@@ -34,7 +65,7 @@ public class KerasLayerConfiguration {
     private final String LAYER_CLASS_NAME_CONVOLUTION_1D = "";
     private final String LAYER_CLASS_NAME_CONVOLUTION_2D = "";
 
-    /* Keras layer configurations. */
+    /* Partially shared layer configurations. */
     private final String LAYER_FIELD_CONFIG = "config";
     private final String LAYER_FIELD_NAME = "name";
     private final String LAYER_FIELD_BATCH_INPUT_SHAPE = "batch_input_shape";
@@ -45,8 +76,21 @@ public class KerasLayerConfiguration {
     private final String LAYER_FIELD_DROPOUT_U = "dropout_U";
     private final String LAYER_FIELD_OUTPUT_DIM = "output_dim";
     private final String LAYER_FIELD_NB_FILTER = "nb_filter";
+
+    /* Advanced activations */
+    // Missing: LeakyReLU, PReLU, ThresholdedReLU
+
+    /* Noise layers */
+    // Missing: GaussianNoise, GaussianDropout, AlphaDropout
+
+    /* Layer wrappers */
+    // Missing: TimeDistributed, Bidirectional
+
+
+    /* Convolutional layer properties */
     private final String LAYER_FIELD_NB_ROW = "nb_row"; // TODO: gone
     private final String LAYER_FIELD_NB_COL = "nb_col"; // TODO: kernel_size
+    private final String LAYER_FIELD_KERNEL_SIZE = "kernel_size";
     private final String LAYER_FIELD_POOL_SIZE = "pool_size";
     private final String LAYER_FIELD_SUBSAMPLE = "subsample";
     private final String LAYER_FIELD_STRIDES = "strides";
@@ -58,8 +102,8 @@ public class KerasLayerConfiguration {
     private final String LAYER_BORDER_MODE_FULL = "full";
 
     /* Keras weight regularizers. */
-    private final String LAYER_FIELD_W_REGULARIZER = "W_regularizer";
-    private final String LAYER_FIELD_B_REGULARIZER = "b_regularizer";
+    private final String LAYER_FIELD_W_REGULARIZER = "";
+    private final String LAYER_FIELD_B_REGULARIZER = "";
     private final String REGULARIZATION_TYPE_L1 = "l1";
     private final String REGULARIZATION_TYPE_L2 = "l2";
 
