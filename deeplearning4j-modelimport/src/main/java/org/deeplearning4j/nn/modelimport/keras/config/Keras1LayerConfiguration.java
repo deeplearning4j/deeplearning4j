9ee96693e10dc12870d6b7bf625e007343a8bea2
Max Pumperla <max.pumperla@googlemail.com>
2017-08-09 10:54:34 +0200

    license and signature for configs

--- deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/config/Keras1LayerConfiguration.java
+++ deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/config/Keras1LayerConfiguration.java
@@ -1,8 +1,30 @@
+/*-
+ *
+ *  * Copyright 2017 Skymind,Inc.
+ *  *
+ *  *    Licensed under the Apache License, Version 2.0 (the "License");
+ *  *    you may not use this file except in compliance with the License.
+ *  *    You may obtain a copy of the License at
+ *  *
+ *  *        http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *    Unless required by applicable law or agreed to in writing, software
+ *  *    distributed under the License is distributed on an "AS IS" BASIS,
+ *  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *    See the License for the specific language governing permissions and
+ *  *    limitations under the License.
+ *
+ */
 package org.deeplearning4j.nn.modelimport.keras.config;
 
 
 import lombok.Data;
 
+/**
+ * All relevant property fields of keras 1.x layers.
+ *
+ * @author Max Pumperla
+ */
 @Data
 public class Keras1LayerConfiguration extends KerasLayerConfiguration {
 
@@ -14,7 +36,7 @@ public class Keras1LayerConfiguration extends KerasLayerConfiguration {
     private final String LAYER_CLASS_NAME_TIME_DISTRIBUTED_DENSE = "TimeDistributedDense";
     private final String LAYER_CLASS_NAME_LSTM = "LSTM";
     private final String LAYER_CLASS_NAME_CONVOLUTION_1D = "Convolution1D";
-//    private final String LAYER_CLASS_NAME_CONVOLUTION_2D = "Convolution2D";
+    private final String LAYER_CLASS_NAME_CONVOLUTION_2D = "Convolution2D";
     private final String LAYER_CLASS_NAME_MAX_POOLING_1D = "MaxPooling1D";
     private final String LAYER_CLASS_NAME_MAX_POOLING_2D = "MaxPooling2D";
     private final String LAYER_CLASS_NAME_AVERAGE_POOLING_1D = "AveragePooling1D";
