c5a6b724c152cf81cb2a4b76448d40d32d34cda5
Samuel Audet <samuel.audet@gmail.com>
2017-08-10 12:49:44 -0400

    Introduce SKIP_BUILD mode to perform-release.sh script (#17)

--- perform-release.sh
+++ perform-release.sh
@@ -9,6 +9,7 @@ fi
 RELEASE_VERSION=$1
 SNAPSHOT_VERSION=$2
 STAGING_REPOSITORY=${3:-}
+SKIP_BUILD=${SKIP_BUILD:-0}
 
 echo "Releasing version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
 echo "========================================================================================"
@@ -22,14 +23,17 @@ sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$RELEASE_VERSION<\/nd4j
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$RELEASE_VERSION<\/datavec.version>/" pom.xml
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION
 
+if [[ "${SKIP_BUILD}" == "0" ]]; then
 mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
+fi
 
-git commit -a -m "Update to version $RELEASE_VERSION"
-git tag -a -m "gym-java-client-$RELEASE_VERSION" "gym-java-client-$RELEASE_VERSION"
+git commit -s -a -m "Update to version $RELEASE_VERSION"
+git tag -s -a -m "gym-java-client-$RELEASE_VERSION" "gym-java-client-$RELEASE_VERSION"
+git tag -s -a -f -m "gym-java-client-$RELEASE_VERSION" "latest_release"
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$SNAPSHOT_VERSION<\/datavec.version>/" pom.xml
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
-git commit -a -m "Update to version $SNAPSHOT_VERSION"
+git commit -s -a -m "Update to version $SNAPSHOT_VERSION"
 
 echo "Successfully performed release of version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
