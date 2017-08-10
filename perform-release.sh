4291ec8d249f8609a197e47f445a444410d536cf
Samuel Audet <samuel.audet@gmail.com>
2017-08-10 12:44:56 -0400

    Introduce SKIP_BUILD mode to perform-release.sh script (#372)

--- perform-release.sh
+++ perform-release.sh
@@ -9,6 +9,7 @@ fi
 RELEASE_VERSION=$1
 SNAPSHOT_VERSION=$2
 STAGING_REPOSITORY=$3
+SKIP_BUILD=${SKIP_BUILD:-0}
 
 echo "Releasing version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
 echo "========================================================================================"
@@ -25,17 +26,22 @@ for f in $(find . -name 'pom.xml' -not -path '*target*'); do
 done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION
 
-mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
-source change-spark-versions.sh 2
-mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
-source change-scala-versions.sh 2.10
-source change-spark-versions.sh 1
-mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
+if [[ "${SKIP_BUILD}" == "0" ]]; then
+    source change-spark-versions.sh 1
+    source change-scala-versions.sh 2.11
+    mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
+    source change-spark-versions.sh 2
+    mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
+    source change-scala-versions.sh 2.10
+    source change-spark-versions.sh 1
+    mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
 
-source change-spark-versions.sh 1
-source change-scala-versions.sh 2.11
-git commit -a -m "Update to version $RELEASE_VERSION"
-git tag -a -m "datavec-$RELEASE_VERSION" "datavec-$RELEASE_VERSION"
+    source change-spark-versions.sh 1
+    source change-scala-versions.sh 2.11
+fi
+git commit -s -a -m "Update to version $RELEASE_VERSION"
+git tag -s -a -m "datavec-$RELEASE_VERSION" "datavec-$RELEASE_VERSION"
+git tag -s -a -f -m "datavec-$RELEASE_VERSION" "latest_release"
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
 #Spark versions, like <version>xxx_spark_2-SNAPSHOT</version>
@@ -44,6 +50,6 @@ for f in $(find . -name 'pom.xml' -not -path '*target*'); do
     sed -i "s/version>.*_spark_.*</version>${SPLIT_VERSION[0]}_spark_1-${SPLIT_VERSION[1]}</g" $f
 done
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
-git commit -a -m "Update to version $SNAPSHOT_VERSION"
+git commit -s -a -m "Update to version $SNAPSHOT_VERSION"
 
 echo "Successfully performed release of version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
