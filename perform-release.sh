23e44f5bd30be8944380df52f7c0c69099e9f9c9
Samuel Audet <samuel.audet@gmail.com>
2017-08-10 12:48:01 -0400

    Introduce SKIP_BUILD mode to perform-release.sh script (#35)

--- perform-release.sh
+++ perform-release.sh
@@ -9,6 +9,7 @@ fi
 RELEASE_VERSION=$1
 SNAPSHOT_VERSION=$2
 STAGING_REPOSITORY=${3:-}
+SKIP_BUILD=${SKIP_BUILD:-0}
 
 if [[ "$2" != *-SNAPSHOT ]]
 then
@@ -32,14 +33,17 @@ sed -i "s/<dl4j.version>.*<\/dl4j.version>/<dl4j.version>$RELEASE_VERSION<\/dl4j
 sed -i "s/<version>.*-SNAPSHOT<\/version>/<version>$RELEASE_VERSION<\/version>/" pom.xml
 mvn versions:set -DallowSnapshots=false -DgenerateBackupPoms=false -DnewVersion=$RELEASE_VERSION
 
-source change-scala-versions.sh 2.10
-mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip
-source change-scala-versions.sh 2.11
-mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip
+if [[ "${SKIP_BUILD}" == "0" ]]; then
+    source change-scala-versions.sh 2.10
+    mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip
+    source change-scala-versions.sh 2.11
+    mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dscalastyle.skip
 
-source change-scala-versions.sh 2.11
-git commit -a -m "Update to version $RELEASE_VERSION"
-git tag -a -m "scalnet-$RELEASE_VERSION" "scalnet-$RELEASE_VERSION"
+    source change-scala-versions.sh 2.11
+fi
+git commit -s -a -m "Update to version $RELEASE_VERSION"
+git tag -s -a -m "scalnet-$RELEASE_VERSION" "scalnet-$RELEASE_VERSION"
+git tag -s -a -f -m "scalnet-$RELEASE_VERSION" "latest_release"
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$SNAPSHOT_VERSION<\/datavec.version>/" pom.xml
@@ -47,6 +51,6 @@ sed -i "s/<dl4j.version>.*<\/dl4j.version>/<dl4j.version>$SNAPSHOT_VERSION<\/dl4
 # back to a version stanza
 sed -i "s/<version>$RELEASE_VERSION<\/version>/<version>$SNAPSHOT_VERSION<\/version>/" pom.xml
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
-git commit -a -m "Update to version $SNAPSHOT_VERSION"
+git commit -s -a -m "Update to version $SNAPSHOT_VERSION"
 
 echo "Successfully performed release of version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
