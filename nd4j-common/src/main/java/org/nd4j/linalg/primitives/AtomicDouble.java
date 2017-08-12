cbc0ab7be34bfb2b90e3583fdc38cea50254044d
Alex Black <blacka101@gmail.com>
2017-08-12 10:42:16 +1000

    Custom JSON ser/de for AtomicDouble

--- nd4j-common/src/main/java/org/nd4j/linalg/primitives/AtomicDouble.java
+++ nd4j-common/src/main/java/org/nd4j/linalg/primitives/AtomicDouble.java
@@ -1,7 +1,13 @@
 package org.nd4j.linalg.primitives;
 
+import org.nd4j.linalg.primitives.serde.JsonDeserializerAtomicDouble;
+import org.nd4j.linalg.primitives.serde.JsonSerializerAtomicDouble;
 import org.nd4j.shade.jackson.annotation.JsonProperty;
+import org.nd4j.shade.jackson.databind.annotation.JsonDeserialize;
+import org.nd4j.shade.jackson.databind.annotation.JsonSerialize;
 
+@JsonSerialize(using = JsonSerializerAtomicDouble.class)
+@JsonDeserialize(using = JsonDeserializerAtomicDouble.class)
 public class AtomicDouble extends com.google.common.util.concurrent.AtomicDouble {
 
     public AtomicDouble(){
@@ -10,6 +16,10 @@ public class AtomicDouble extends com.google.common.util.concurrent.AtomicDouble
 
     public AtomicDouble(@JsonProperty("value") double value){
         super(value);
+    }
+
+    public AtomicDouble(float value){
+        this((double)value);
     }
 
 }
