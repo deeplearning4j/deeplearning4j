5de6083f3f3135dcac1160ed18ccbe7a1faca012
Samuel Audet <samuel.audet@gmail.com>
2017-08-16 21:51:41 +0900

    Fix HistoryProcess.transform() not properly resizing images anymore

--- rl4j-core/src/main/java/org/deeplearning4j/rl4j/learning/HistoryProcessor.java
+++ rl4j-core/src/main/java/org/deeplearning4j/rl4j/learning/HistoryProcessor.java
@@ -101,7 +101,7 @@ public class HistoryProcessor implements IHistoryProcessor {
         int[] shape = raw.shape();
         Mat ocvmat = new Mat(shape[0], shape[1], CV_32FC(3), raw.data().pointer());
         Mat cvmat = new Mat(shape[0], shape[1], CV_8UC(3));
-        ocvmat.convertTo(cvmat, CV_8UC(3));
+        ocvmat.convertTo(cvmat, CV_8UC(3), 255.0, 0.0);
         cvtColor(cvmat, cvmat, COLOR_RGB2GRAY);
         Mat resized = new Mat(conf.getRescaledHeight(), conf.getRescaledWidth(), CV_8UC(1));
         resize(cvmat, resized, new Size(conf.getRescaledWidth(), conf.getRescaledHeight()));
@@ -119,7 +119,7 @@ public class HistoryProcessor implements IHistoryProcessor {
             e.printStackTrace();
         }
         //System.out.println(out.shapeInfoToString());
-        out = out.reshape(1, conf.getCroppingHeight(), conf.getCroppingWidth());
+        out = out.reshape(1, conf.getCroppingHeight(), conf.getCroppingWidth()).muli(1.0 / 255.0);
         INDArray compressed = compressor.compress(out);
         return compressed;
     }
