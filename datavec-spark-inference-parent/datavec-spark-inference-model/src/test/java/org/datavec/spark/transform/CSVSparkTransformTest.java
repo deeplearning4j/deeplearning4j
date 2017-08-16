bc4dc20913fb3a0b1521205f77c8f9a5ebcd7cab
crockpotveggies <justinlong@outlook.com>
2017-08-15 20:06:13 -0700

    Modify single batch to use convertToSequence

--- datavec-spark-inference-parent/datavec-spark-inference-model/src/test/java/org/datavec/spark/transform/CSVSparkTransformTest.java
+++ datavec-spark-inference-parent/datavec-spark-inference-model/src/test/java/org/datavec/spark/transform/CSVSparkTransformTest.java
@@ -2,6 +2,7 @@ package org.datavec.spark.transform;
 
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.nlp.TextToCharacterIndexTransform;
 import org.datavec.api.writable.DoubleWritable;
 import org.datavec.api.writable.Text;
 import org.datavec.api.writable.Writable;
@@ -13,9 +14,7 @@ import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.serde.base64.Nd4jBase64;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
@@ -77,19 +76,19 @@ public class CSVSparkTransformTest {
 
     @Test
     public void testSingleBatchSequence() throws Exception {
-        List<Writable> input = new ArrayList<>();
-        input.add(new DoubleWritable(1.0));
-        input.add(new DoubleWritable(2.0));
+        Schema schema = new Schema.Builder()
+                .addColumnsString("action")
+                .build();
 
-        Schema schema = new Schema.Builder().addColumnDouble("1.0").addColumnDouble("2.0").build();
-        List<Writable> output = new ArrayList<>();
-        output.add(new Text("1.0"));
-        output.add(new Text("2.0"));
+        TransformProcess transformProcess = new TransformProcess.Builder(schema)
+                .removeAllColumnsExceptFor("action")
+                .convertToSequence()
+                .transform(new TextToCharacterIndexTransform("action", "action_sequence", defaultCharIndex(), true))
+                .integerToOneHot("action_sequence",0,29)
+                .build();
 
-        TransformProcess transformProcess =
-                new TransformProcess.Builder(schema).convertToString("1.0").convertToString("2.0").build();
         CSVSparkTransform csvSparkTransform = new CSVSparkTransform(transformProcess);
-        String[] values = new String[] {"1.0", "2.0"};
+        String[] values = new String[] {"install new part"};
         SingleCSVRecord record = csvSparkTransform.transform(new SingleCSVRecord(values));
         BatchCSVRecord batchCSVRecord = new BatchCSVRecord();
         for (int i = 0; i < 3; i++)
@@ -98,7 +97,6 @@ public class CSVSparkTransformTest {
         SequenceBatchCSVRecord sequenceBatchCSVRecord = new SequenceBatchCSVRecord();
         sequenceBatchCSVRecord.add(Arrays.asList(batchCSVRecord));
         Base64NDArrayBody sequenceArray = csvSparkTransform.transformSequenceArray(sequenceBatchCSVRecord);
-
 
 
         Base64NDArrayBody body = csvSparkTransform.toArray(batchCSVRecord1);
@@ -115,6 +113,43 @@ public class CSVSparkTransformTest {
         System.out.println(transformed);
 
 
+    }
+
+    public static Map<Character,Integer> defaultCharIndex() {
+        Map<Character,Integer> ret = new TreeMap<>();
+
+        ret.put('a',0);
+        ret.put('b',1);
+        ret.put('c',2);
+        ret.put('d',3);
+        ret.put('e',4);
+        ret.put('f',5);
+        ret.put('g',6);
+        ret.put('h',7);
+        ret.put('i',8);
+        ret.put('j',9);
+        ret.put('k',10);
+        ret.put('l',11);
+        ret.put('m',12);
+        ret.put('n',13);
+        ret.put('o',14);
+        ret.put('p',15);
+        ret.put('q',16);
+        ret.put('r',17);
+        ret.put('s',18);
+        ret.put('t',19);
+        ret.put('u',20);
+        ret.put('v',21);
+        ret.put('w',22);
+        ret.put('x',23);
+        ret.put('y',24);
+        ret.put('z',25);
+        ret.put('/',26);
+        ret.put(' ',27);
+        ret.put('(',28);
+        ret.put(')',29);
+
+        return ret;
     }
 
 }
