a7a59265a202e8e188daf118d0d99cdab8746e6d
raver119 <raver119@gmail.com>
2017-08-22 13:43:23 +0300

    tmp

---
+++ include/graph/ExecutorConfiguration.h
@@ -0,0 +1,22 @@
+//
+// @author raver119@gmail.com
+//
+
+#ifndef LIBND4J_EXECUTORCONFIGURATION_H
+#define LIBND4J_EXECUTORCONFIGURATION_H
+
+namespace nd4j {
+    namespace graph {
+        enum ExecutionMode {
+            SEQUENTIAL,
+
+        };
+
+        class ExecutorConfiguration {
+        protected:
+
+        };
+    }
+}
+
+#endif //LIBND4J_EXECUTORCONFIGURATION_H
