efef5209460d91aba679bbe926ab6e195bb38a7f
Max Pumperla <max.pumperla@googlemail.com>
2017-08-22 12:38:11 +0200

    allow no bias in dense layer import

--- deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/layers/KerasDense.java
+++ deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/layers/KerasDense.java
@@ -25,10 +25,12 @@ import java.util.Set;
 public class KerasDense extends KerasLayer {
 
     /* Keras layer parameter names. */
-    private final int NUM_TRAINABLE_PARAMS = 2;
+    private int numTrainableParams = 2;
+    private boolean hasBias;
 
     /**
      * Pass-through constructor from KerasLayer
+     *
      * @param kerasVersion major keras version
      * @throws UnsupportedKerasConfigurationException
      */
@@ -39,36 +41,40 @@ public class KerasDense extends KerasLayer {
     /**
      * Constructor from parsed Keras layer configuration dictionary.
      *
-     * @param layerConfig       dictionary containing Keras layer configuration
+     * @param layerConfig dictionary containing Keras layer configuration
      * @throws InvalidKerasConfigurationException
      * @throws UnsupportedKerasConfigurationException
      */
     public KerasDense(Map<String, Object> layerConfig)
-                    throws InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
+            throws InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         this(layerConfig, true);
     }
 
     /**
      * Constructor from parsed Keras layer configuration dictionary.
      *
-     * @param layerConfig               dictionary containing Keras layer configuration
-     * @param enforceTrainingConfig     whether to enforce training-related configuration options
+     * @param layerConfig           dictionary containing Keras layer configuration
+     * @param enforceTrainingConfig whether to enforce training-related configuration options
      * @throws InvalidKerasConfigurationException
      * @throws UnsupportedKerasConfigurationException
      */
     public KerasDense(Map<String, Object> layerConfig, boolean enforceTrainingConfig)
-                    throws InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
+            throws InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         super(layerConfig, enforceTrainingConfig);
+        hasBias = getHasBiasFromConfig(layerConfig);
+        numTrainableParams = hasBias ? 2 : 1;
+
         this.layer = new DenseLayer.Builder().name(this.layerName).nOut(getNOutFromConfig(layerConfig))
-                        .dropOut(this.dropout).activation(getActivationFromConfig(layerConfig))
-                        .weightInit(getWeightInitFromConfig(layerConfig, enforceTrainingConfig)).biasInit(0.0)
-                        .l1(this.weightL1Regularization).l2(this.weightL2Regularization).build();
+                .dropOut(this.dropout).activation(getActivationFromConfig(layerConfig))
+                .weightInit(getWeightInitFromConfig(layerConfig, enforceTrainingConfig)).biasInit(0.0)
+                .l1(this.weightL1Regularization).l2(this.weightL2Regularization)
+                .hasBias(hasBias).build();
     }
 
     /**
      * Get DL4J DenseLayer.
      *
-     * @return  DenseLayer
+     * @return DenseLayer
      */
     public DenseLayer getDenseLayer() {
         return (DenseLayer) this.layer;
@@ -77,8 +83,8 @@ public class KerasDense extends KerasLayer {
     /**
      * Get layer output type.
      *
-     * @param  inputType    Array of InputTypes
-     * @return              output type as InputType
+     * @param inputType Array of InputTypes
+     * @return output type as InputType
      * @throws InvalidKerasConfigurationException
      */
     @Override
@@ -94,11 +100,11 @@ public class KerasDense extends KerasLayer {
     /**
      * Returns number of trainable parameters in layer.
      *
-     * @return          number of trainable parameters (2)
+     * @return number of trainable parameters (2)
      */
     @Override
     public int getNumParams() {
-        return NUM_TRAINABLE_PARAMS;
+        return numTrainableParams;
     }
 
     /**
@@ -113,19 +119,21 @@ public class KerasDense extends KerasLayer {
             this.weights.put(DefaultParamInitializer.WEIGHT_KEY, weights.get(conf.getKERAS_PARAM_NAME_W()));
         else
             throw new InvalidKerasConfigurationException(
-                            "Parameter " + conf.getKERAS_PARAM_NAME_W() + " does not exist in weights");
-        if (weights.containsKey(conf.getKERAS_PARAM_NAME_B()))
-            this.weights.put(DefaultParamInitializer.BIAS_KEY, weights.get(conf.getKERAS_PARAM_NAME_B()));
-        else
-            throw new InvalidKerasConfigurationException(
-                            "Parameter " + conf.getKERAS_PARAM_NAME_B() + " does not exist in weights");
+                    "Parameter " + conf.getKERAS_PARAM_NAME_W() + " does not exist in weights");
+        if (hasBias) {
+            if (weights.containsKey(conf.getKERAS_PARAM_NAME_B()))
+                this.weights.put(DefaultParamInitializer.BIAS_KEY, weights.get(conf.getKERAS_PARAM_NAME_B()));
+            else
+                throw new InvalidKerasConfigurationException(
+                        "Parameter " + conf.getKERAS_PARAM_NAME_B() + " does not exist in weights");
+        }
         if (weights.size() > 2) {
             Set<String> paramNames = weights.keySet();
             paramNames.remove(conf.getKERAS_PARAM_NAME_W());
             paramNames.remove(conf.getKERAS_PARAM_NAME_B());
             String unknownParamNames = paramNames.toString();
             log.warn("Attemping to set weights for unknown parameters: "
-                            + unknownParamNames.substring(1, unknownParamNames.length() - 1));
+                    + unknownParamNames.substring(1, unknownParamNames.length() - 1));
         }
     }
 }
