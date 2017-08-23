6e7ef0f9117f694efafd7480a1bf20f5bef73206
raver119 <raver119@gmail.com>
2017-08-23 10:18:22 +0300

    new broadcast ops + tests for them

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/broadcast/BroadcastMax.java
@@ -0,0 +1,72 @@
+package org.nd4j.linalg.api.ops.impl.broadcast;
+
+import org.nd4j.linalg.api.complex.IComplexNumber;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.BaseBroadcastOp;
+
+/**
+ * Broadcast Max comparison op
+ *
+ * @author raver119@gmail.com
+ */
+public class BroadcastMax extends BaseBroadcastOp {
+
+    public BroadcastMax() {}
+
+    public BroadcastMax(INDArray x, INDArray y, INDArray z, int... dimension) {
+        super(x, y, z, dimension);
+    }
+
+
+    @Override
+    public int opNum() {
+        return 14;
+    }
+
+    @Override
+    public String name() {
+        return "broadcast_max";
+    }
+
+    @Override
+    public IComplexNumber op(IComplexNumber origin, double other) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IComplexNumber op(IComplexNumber origin, float other) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IComplexNumber op(IComplexNumber origin, IComplexNumber other) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public float op(float origin, float other) {
+        return origin > other ? 1.0f : 0.0f;
+    }
+
+    @Override
+    public double op(double origin, double other) {
+        return origin > other ? 1.0f : 0.0f;
+    }
+
+    @Override
+    public double op(double origin) {
+        return 1;
+    }
+
+    @Override
+    public float op(float origin) {
+        return 1;
+    }
+
+    @Override
+    public IComplexNumber op(IComplexNumber origin) {
+        return origin;
+    }
+
+
+}
