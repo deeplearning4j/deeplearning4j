6ef993350d17fe8e5840cb0a5757792fc1d2546a
Alex Black <blacka101@gmail.com>
2017-08-24 16:44:06 +1000

    Fix CUDA col2im with dilation

--- include/ops/special_ops.h
+++ include/ops/special_ops.h
@@ -937,16 +937,6 @@ namespace simdOps {
 							int data_col_index = num_im * strideex + depth_im * stridech + h_k * stridekrow + w_k * stridekcol + h_col * striderow + w_col * stridecol;
 							val += dx[data_col_index];
 						}
-						
-						/*
-						if (h_k % dH == 0 && w_k % dW == 0) {
-						  h_k /= dH;
-						  w_k /= dW;
-						  int data_col_index = (((c_im * kernel_h + h_k) * kernel_w + w_k) *
-												height_col + h_col) * width_col + w_col;
-						  val += data_col[data_col_index];
-						}
-						*/
 					}
 				}
 				int i_f = 0;
@@ -957,33 +947,6 @@ namespace simdOps {
 					i_c = i_c / outShape[dim];
 				}
 				result[i_f] += val;
-				
-				/*
-				int w_idx = i % imgW + padWidth;
-				int h_idx = (i / imgW) % imgH + padHeight;
-				int c_idx = i / (imgW * imgH);
-				int kernel_extent_w = (kernel_w - 1) * dilation_w + 1;
-				int kernel_extent_h = (kernel_h - 1) * dilation_h + 1;
-				// compute the start and end of the output
-				int w_col_start = (w_im < kernel_extent_w) ? 0 : (w_im - kernel_extent_w) / stride_w + 1;
-				int w_col_end = min(w_im / stride_w + 1, width_col);
-				int h_col_start = (h_im < kernel_extent_h) ? 0 : (h_im - kernel_extent_h) / stride_h + 1;
-				const int h_col_end = min(h_im / stride_h + 1, height_col);
-				for (int h_col = h_col_start; h_col < h_col_end; h_col += 1) {
-				  for (int w_col = w_col_start; w_col < w_col_end; w_col += 1) {
-					int h_k = (h_im - h_col * stride_h);
-					int w_k = (w_im - w_col * stride_w);
-					if (h_k % dilation_h == 0 && w_k % dilation_w == 0) {
-					  h_k /= dilation_h;
-					  w_k /= dilation_w;
-					  int data_col_index = (((c_im * kernel_h + h_k) * kernel_w + w_k) *
-											height_col + h_col) * width_col + w_col;
-					  val += data_col[data_col_index];
-					}
-				  }
-				}
-				data_im[index] = val;
-				*/
 			}
 		}
 #endif
