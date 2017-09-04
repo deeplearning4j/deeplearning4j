e5edc5ac78378c106e37a8e6d5dd69ab4a0618aa
Max Pumperla <max.pumperla@googlemail.com>
2017-09-04 17:35:02 +0200

    tests

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/constraints/TestConstraints.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/constraints/TestConstraints.java
@@ -30,17 +30,208 @@ import static org.junit.Assert.assertTrue;
 public class TestConstraints {
 
     @Test
-    public void testConstraints() throws Exception {
-
+    public void testLayerBiasConstraints() throws Exception {
 
         LayerConstraint[] constraints = new LayerConstraint[]{
                 new MaxNormConstraint(0.5, 1),
-                new MinMaxNormConstraint(0.3, 0.4, 1),
+                new MinMaxNormConstraint(0.3, 0.4, 1.0, 1),
+                new NonNegativeConstraint(),
+                new UnitNormConstraint(1)
+        };
+
+        for (LayerConstraint lc : constraints) {
+
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .learningRate(0.0)
+                    .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 5))
+                    .biasInit(10.0)
+                    .list()
+                    .layer(new DenseLayer.Builder().nIn(12).nOut(10)
+                            .biasConstraints(lc).build())
+                    .layer(new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).nIn(10).nOut(8).build())
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            List<LayerConstraint> exp = Collections.singletonList(lc.clone());
+            assertEquals(exp, net.getLayer(0).conf().getLayer().getConstraints());
+
+            INDArray input = Nd4j.rand(3, 12);
+            INDArray labels = Nd4j.rand(3, 8);
+
+            net.fit(input, labels);
+
+            INDArray b0 = net.getParam("0_b");
+
+
+            if (lc instanceof MaxNormConstraint) {
+                assertTrue(b0.norm2(1).maxNumber().doubleValue() <= 0.5);
+
+            } else if (lc instanceof MinMaxNormConstraint) {
+                assertTrue(b0.norm2(1).minNumber().doubleValue() >= 0.3);
+                assertTrue(b0.norm2(1).maxNumber().doubleValue() <= 0.4);
+            } else if (lc instanceof NonNegativeConstraint) {
+                assertTrue(b0.minNumber().doubleValue() >= 0.0);
+            } else if (lc instanceof UnitNormConstraint) {
+                assertEquals(b0.norm2(1).minNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(b0.norm2(1).maxNumber().doubleValue(), 1.0, 1e-6);
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ModelSerializer.writeModel(net, baos, true);
+            byte[] bytes = baos.toByteArray();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            MultiLayerNetwork restored = ModelSerializer.restoreMultiLayerNetwork(bais, true);
+
+            assertEquals(net.getLayerWiseConfigurations(), restored.getLayerWiseConfigurations());
+            assertEquals(net.params(), restored.params());
+        }
+    }
+
+    @Test
+    public void testLayerWeightsConstraints() throws Exception {
+
+        LayerConstraint[] constraints = new LayerConstraint[]{
+                new MaxNormConstraint(0.5, 1),
+                new MinMaxNormConstraint(0.3, 0.4, 1.0, 1),
+                new NonNegativeConstraint(),
+                new UnitNormConstraint(1)
+        };
+
+        for (LayerConstraint lc : constraints) {
+
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .learningRate(0.0)
+                    .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 5))
+                    .list()
+                    .layer(new DenseLayer.Builder().nIn(12).nOut(10)
+                            .weightConstraints(lc).build())
+                    .layer(new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).nIn(10).nOut(8).build())
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            List<LayerConstraint> exp = Collections.singletonList(lc.clone());
+            assertEquals(exp, net.getLayer(0).conf().getLayer().getConstraints());
+
+            INDArray input = Nd4j.rand(3, 12);
+            INDArray labels = Nd4j.rand(3, 8);
+
+            net.fit(input, labels);
+
+            INDArray w0 = net.getParam("0_W");
+
+
+            if (lc instanceof MaxNormConstraint) {
+                assertTrue(w0.norm2(1).maxNumber().doubleValue() <= 0.5);
+
+            } else if (lc instanceof MinMaxNormConstraint) {
+                assertTrue(w0.norm2(1).minNumber().doubleValue() >= 0.3);
+                assertTrue(w0.norm2(1).maxNumber().doubleValue() <= 0.4);
+            } else if (lc instanceof NonNegativeConstraint) {
+                assertTrue(w0.minNumber().doubleValue() >= 0.0);
+            } else if (lc instanceof UnitNormConstraint) {
+                assertEquals(w0.norm2(1).minNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(w0.norm2(1).maxNumber().doubleValue(), 1.0, 1e-6);
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ModelSerializer.writeModel(net, baos, true);
+            byte[] bytes = baos.toByteArray();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            MultiLayerNetwork restored = ModelSerializer.restoreMultiLayerNetwork(bais, true);
+
+            assertEquals(net.getLayerWiseConfigurations(), restored.getLayerWiseConfigurations());
+            assertEquals(net.params(), restored.params());
+        }
+    }
+
+    @Test
+    public void testLayerWeightsAndBiasConstraints() throws Exception {
+
+        LayerConstraint[] constraints = new LayerConstraint[]{
+                new MaxNormConstraint(0.5, 1),
+                new MinMaxNormConstraint(0.3, 0.4, 1.0, 1),
+                new NonNegativeConstraint(),
+                new UnitNormConstraint(1)
+        };
+
+        for (LayerConstraint lc : constraints) {
+
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .learningRate(0.0)
+                    .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 5))
+                    .biasInit(0.2)
+                    .list()
+                    .layer(new DenseLayer.Builder().nIn(12).nOut(10)
+                            .constraints(lc).build())
+                    .layer(new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).nIn(10).nOut(8).build())
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            List<LayerConstraint> exp = Collections.singletonList(lc.clone());
+            assertEquals(exp, net.getLayer(0).conf().getLayer().getConstraints());
+
+            INDArray input = Nd4j.rand(3, 12);
+            INDArray labels = Nd4j.rand(3, 8);
+
+            net.fit(input, labels);
+
+            INDArray w0 = net.getParam("0_W");
+            INDArray b0 = net.getParam("0_b");
+
+
+            if (lc instanceof MaxNormConstraint) {
+                assertTrue(w0.norm2(1).maxNumber().doubleValue() <= 0.5);
+                assertTrue(b0.norm2(1).maxNumber().doubleValue() <= 0.5);
+
+            } else if (lc instanceof MinMaxNormConstraint) {
+                assertTrue(w0.norm2(1).minNumber().doubleValue() >= 0.3);
+                assertTrue(w0.norm2(1).maxNumber().doubleValue() <= 0.4);
+                assertTrue(b0.norm2(1).minNumber().doubleValue() >= 0.3);
+                assertTrue(b0.norm2(1).maxNumber().doubleValue() <= 0.4);
+            } else if (lc instanceof NonNegativeConstraint) {
+                assertTrue(w0.minNumber().doubleValue() >= 0.0);
+                assertTrue(b0.minNumber().doubleValue() >= 0.0);
+            } else if (lc instanceof UnitNormConstraint) {
+                assertEquals(w0.norm2(1).minNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(w0.norm2(1).maxNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(b0.norm2(1).minNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(b0.norm2(1).maxNumber().doubleValue(), 1.0, 1e-6);
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ModelSerializer.writeModel(net, baos, true);
+            byte[] bytes = baos.toByteArray();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            MultiLayerNetwork restored = ModelSerializer.restoreMultiLayerNetwork(bais, true);
+
+            assertEquals(net.getLayerWiseConfigurations(), restored.getLayerWiseConfigurations());
+            assertEquals(net.params(), restored.params());
+        }
+    }
+
+
+        @Test
+    public void testModelConstraints() throws Exception {
+
+        LayerConstraint[] constraints = new LayerConstraint[]{
+                new MaxNormConstraint(0.5, 1),
+                new MinMaxNormConstraint(0.3, 0.4, 1, 1),
                 new NonNegativeConstraint(),
                 new UnitNormConstraint(1)
         };
 
         for(LayerConstraint lc : constraints){
+
+            System.out.println(lc.toString());
 
             MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                     .constraints(lc)
@@ -64,9 +255,7 @@ public class TestConstraints {
             net.fit(input, labels);
 
             INDArray w0 = net.getParam("0_W");
-            INDArray b0 = net.getParam("0_b");
             INDArray w1 = net.getParam("1_W");
-            INDArray b1 = net.getParam("1_b");
 
             if(lc instanceof MaxNormConstraint){
                 assertTrue(w0.norm2(1).maxNumber().doubleValue() <= 0.5 );
