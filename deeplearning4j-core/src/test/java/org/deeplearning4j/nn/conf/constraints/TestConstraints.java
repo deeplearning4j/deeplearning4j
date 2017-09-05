5b1390d4b6c6bc088c2568d2ddabb3edf92c5815
Max Pumperla <max.pumperla@googlemail.com>
2017-09-05 09:59:54 +0200

    custom constraint test

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/constraints/TestConstraints.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/conf/constraints/TestConstraints.java
@@ -22,13 +22,80 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestConstraints {
+
+    @Test
+    public void testLayerCustomConstraints() throws Exception {
+
+        LayerConstraint[] constraints = new LayerConstraint[]{
+                new MaxNormConstraint(0.5, 1),
+                new MinMaxNormConstraint(0.3, 0.4, 1.0, 1),
+                new NonNegativeConstraint(),
+                new UnitNormConstraint(1)
+        };
+
+        Set<String> customConstraint = new HashSet<>();
+        customConstraint.add("RW");
+
+        for (LayerConstraint lc : constraints) {
+
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .learningRate(0.0)
+                    .weightInit(WeightInit.DISTRIBUTION).dist(new NormalDistribution(0, 5))
+                    .list()
+                    .layer(new LSTM.Builder().nIn(12).nOut(10)
+                            .constraints(
+                                    Arrays.asList(lc),
+                                    false,
+                                    false,
+                                    customConstraint)
+                            .build())
+                    .layer(new OutputLayer.Builder().lossFunction(LossFunctions.LossFunction.MSE).nIn(10).nOut(8).build())
+                    .build();
+
+            MultiLayerNetwork net = new MultiLayerNetwork(conf);
+            net.init();
+
+            List<LayerConstraint> exp = Collections.singletonList(lc.clone());
+            assertEquals(exp, net.getLayer(0).conf().getLayer().getConstraints());
+
+            INDArray input = Nd4j.rand(3, 12);
+            INDArray labels = Nd4j.rand(3, 8);
+
+            net.fit(input, labels);
+
+            INDArray RW0 = net.getParam("0_RW");
+
+
+            if (lc instanceof MaxNormConstraint) {
+                assertTrue(RW0.norm2(1).maxNumber().doubleValue() <= 0.5);
+
+            } else if (lc instanceof MinMaxNormConstraint) {
+                assertTrue(RW0.norm2(1).minNumber().doubleValue() >= 0.3);
+                assertTrue(RW0.norm2(1).maxNumber().doubleValue() <= 0.4);
+            } else if (lc instanceof NonNegativeConstraint) {
+                assertTrue(RW0.minNumber().doubleValue() >= 0.0);
+            } else if (lc instanceof UnitNormConstraint) {
+                assertEquals(RW0.norm2(1).minNumber().doubleValue(), 1.0, 1e-6);
+                assertEquals(RW0.norm2(1).maxNumber().doubleValue(), 1.0, 1e-6);
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ModelSerializer.writeModel(net, baos, true);
+            byte[] bytes = baos.toByteArray();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            MultiLayerNetwork restored = ModelSerializer.restoreMultiLayerNetwork(bais, true);
+
+            assertEquals(net.getLayerWiseConfigurations(), restored.getLayerWiseConfigurations());
+            assertEquals(net.params(), restored.params());
+        }
+    }
 
     @Test
     public void testLayerRecurrentConstraints() throws Exception {
