4b35c5e555316a480200486cfb99c4343317fe70
Yurii <yurii@skymind.io>
2017-09-06 18:18:51 +0300

    do not use some vector constructor

--- include/ops/declarable/cpu/parity_ops.h
+++ include/ops/declarable/cpu/parity_ops.h
@@ -711,7 +711,8 @@ namespace nd4j {
 			std::vector<int>* argumets = block.getIArguments();
 			int argsSize = argumets->size();
 			char order = (*argumets)[argsSize-1];
-			std::vector<int> shapeNew(argumets->begin(), argumets->end() - 1);						
+			std::vector<int> shapeNew = *argumets;
+			shapeNew.pop_back();
 
 			NDArray<T> *x = block.getVariables().at(0)->getNDArray();            			
 			if(block.isInplace()) {
@@ -738,7 +739,8 @@ namespace nd4j {
 			std::vector<int>* argumets = block.getIArguments();
 			int argsSize = argumets->size();
 			int dimension = (*argumets)[argsSize-1];
-			std::vector<int> repeats(argumets->begin(), argumets->end() - 1);						
+			std::vector<int> repeats = *argumets;
+			repeats.pop_back();
 
 			NDArray<T> *x = block.getVariables().at(0)->getNDArray();            			
 			NDArray<T>* ret = x->repeat(dimension, repeats);
