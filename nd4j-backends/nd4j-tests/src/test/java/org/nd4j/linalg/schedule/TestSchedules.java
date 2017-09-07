c771691c09e103dc02a7d2d73a688c03f1990389
Alex Black <blacka101@gmail.com>
2017-09-07 21:20:37 +1000

    Test schedule values

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/schedule/TestSchedules.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/schedule/TestSchedules.java
@@ -36,6 +36,57 @@ public class TestSchedules {
     }
 
     @Test
+    public void testScheduleValues(){
+
+        double lr = 0.8;
+        double decay = 0.9;
+        double power = 2;
+        double gamma = 0.5;
+        int step = 20;
+
+        for(ScheduleType st : ScheduleType.values()) {
+
+            ISchedule[] schedules = new ISchedule[]{
+                    new ExponentialSchedule(st, lr, gamma),
+                    new InverseSchedule(st, lr, gamma, power),
+                    new PolySchedule(st, lr, power, step),
+                    new SigmoidSchedule(st, lr, gamma, step),
+                    new StepSchedule(st, lr, decay, step)};
+
+            for(ISchedule s : schedules) {
+
+                for (int i = 0; i < 9; i++) {
+                    int epoch = i / 3;
+                    int x;
+                    if (st == ScheduleType.ITERATION) {
+                        x = i;
+                    } else {
+                        x = epoch;
+                    }
+
+                    double now = s.valueAt(i, epoch);
+                    double e;
+                    if(s instanceof ExponentialSchedule){
+                        e = calcExponentialDecay(lr, gamma, x);
+                    } else if(s instanceof InverseSchedule){
+                        e = calcInverseDecay(lr, gamma, x, power);
+                    } else if(s instanceof PolySchedule){
+                        e = calcPolyDecay(lr, x, power, step);
+                    } else if(s instanceof SigmoidSchedule){
+                        e = calcSigmoidDecay(lr, gamma, x, step);
+                    } else if(s instanceof StepSchedule){
+                        e = calcStepDecay(lr, decay, x, step);
+                    } else {
+                        throw new RuntimeException();
+                    }
+
+                    assertEquals(s.toString() + ", " + st, e, now, 1e-6);
+                }
+            }
+        }
+    }
+
+    @Test
     public void testMapSchedule(){
 
         ISchedule schedule = new MapSchedule.Builder(ScheduleType.ITERATION)
@@ -50,7 +101,26 @@ public class TestSchedules {
                 assertEquals(0.1, schedule.valueAt(i, 0), 1e-6);
             }
         }
+    }
 
+    private static double calcExponentialDecay(double lr, double decayRate, double iteration) {
+        return lr * Math.pow(decayRate, iteration);
+    }
+
+    private static double calcInverseDecay(double lr, double decayRate, double iteration, double power) {
+        return lr / Math.pow((1 + decayRate * iteration), power);
+    }
+
+    private static double calcStepDecay(double lr, double decayRate, double iteration, double steps) {
+        return lr * Math.pow(decayRate, Math.floor(iteration / steps));
+    }
+
+    private static double calcPolyDecay(double lr, double iteration, double power, double maxIterations) {
+        return lr * Math.pow(1 + iteration / maxIterations, power);
+    }
+
+    private static double calcSigmoidDecay(double lr, double decayRate, double iteration, double steps) {
+        return lr / (1 + Math.exp(-decayRate * (iteration - steps)));
     }
 
 }
