e4bfeca78a8a8144823be0edc21b41d375503e5b
Alex Black <blacka101@gmail.com>
2017-09-08 22:32:15 +1000

    Add probability clipping to LossBinaryXENT (to avoid underflow/NaNs)

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossBinaryXENT.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/lossfunctions/impl/LossBinaryXENT.java
@@ -2,6 +2,8 @@ package org.nd4j.linalg.lossfunctions.impl;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.conditions.Conditions;
 import org.nd4j.linalg.primitives.Pair;
 import org.nd4j.linalg.activations.IActivation;
 import org.nd4j.linalg.activations.impl.ActivationSoftmax;
@@ -15,6 +17,7 @@ import org.nd4j.linalg.lossfunctions.serde.RowVectorDeserializer;
 import org.nd4j.linalg.lossfunctions.serde.RowVectorSerializer;
 import org.nd4j.linalg.ops.transforms.Transforms;
 import org.nd4j.shade.jackson.annotation.JsonInclude;
+import org.nd4j.shade.jackson.annotation.JsonProperty;
 import org.nd4j.shade.jackson.databind.annotation.JsonDeserialize;
 import org.nd4j.shade.jackson.databind.annotation.JsonSerialize;
 
@@ -30,10 +33,13 @@ import org.nd4j.shade.jackson.databind.annotation.JsonSerialize;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public class LossBinaryXENT implements ILossFunction {
+    public static final double DEFAULT_CLIPPING_EPSILON = 1e-10;
 
     @JsonSerialize(using = RowVectorSerializer.class)
     @JsonDeserialize(using = RowVectorDeserializer.class)
     private final INDArray weights;
+
+    private double clipEps;
 
     public LossBinaryXENT() {
         this(null);
@@ -49,10 +55,28 @@ public class LossBinaryXENT implements ILossFunction {
      * @param weights Weights array (row vector). May be null.
      */
     public LossBinaryXENT(INDArray weights) {
+        this(DEFAULT_CLIPPING_EPSILON, weights);
+    }
+
+    /**
+     * Binary cross entropy where each the output is
+     * (optionally) weighted/scaled by a fixed scalar value.
+     * Note that the weights array must be a row vector, of length equal to
+     * the labels/output dimension 1 size.
+     * A weight vector of 1s should give identical results to no weight vector.
+     *
+     * @param weights Weights array (row vector). May be null.
+     */
+    public LossBinaryXENT(@JsonProperty("clipEps") double clipEps, @JsonProperty("weights") INDArray weights){
         if (weights != null && !weights.isRowVector()) {
             throw new IllegalArgumentException("Weights array must be a row vector");
         }
+        if(clipEps < 0 || clipEps > 0.5){
+            throw new IllegalArgumentException("Invalid clipping epsilon value: epsilon should be >= 0 (but near zero)."
+                    + "Got: " + clipEps);
+        }
 
+        this.clipEps = clipEps;
         this.weights = weights;
     }
 
@@ -73,6 +97,10 @@ public class LossBinaryXENT implements ILossFunction {
         } else {
             //INDArray output = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(activationFn, preOutput.dup()));
             INDArray output = activationFn.getActivation(preOutput.dup(), true);
+            if (clipEps > 0.0) {
+                BooleanIndexing.replaceWhere(output, clipEps, Conditions.lessThan(clipEps));
+                BooleanIndexing.replaceWhere(output, 1.0 - clipEps, Conditions.greaterThan(1.0 - clipEps));
+            }
             scoreArr = Transforms.log(output, true).muli(labels);
             INDArray secondTerm = output.rsubi(1);
             Transforms.log(secondTerm, false);
@@ -128,6 +156,10 @@ public class LossBinaryXENT implements ILossFunction {
         }
 
         INDArray output = activationFn.getActivation(preOutput.dup(), true);
+        if (clipEps > 0.0) {
+            BooleanIndexing.replaceWhere(output, clipEps, Conditions.lessThan(clipEps));
+            BooleanIndexing.replaceWhere(output, 1.0 - clipEps, Conditions.greaterThan(1.0 - clipEps));
+        }
 
         INDArray numerator = output.sub(labels);
         INDArray denominator = Nd4j.getExecutioner().execAndReturn(new TimesOneMinus(output)); // output * (1-output)
