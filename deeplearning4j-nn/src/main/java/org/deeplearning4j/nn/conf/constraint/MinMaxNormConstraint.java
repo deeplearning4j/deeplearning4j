19db3ccd31f9105a7fc854cadc3d7288f442dd41
Alex Black <blacka101@gmail.com>
2017-09-12 12:07:56 +1000

    Use ClipByValue op in a few more places

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/constraint/MinMaxNormConstraint.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/constraint/MinMaxNormConstraint.java
@@ -3,7 +3,10 @@ package org.deeplearning4j.nn.conf.constraint;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.CustomOp;
+import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.factory.Broadcast;
+import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.BooleanIndexing;
 import org.nd4j.linalg.indexing.conditions.Conditions;
 
@@ -86,8 +89,12 @@ public class MinMaxNormConstraint extends BaseConstraint {
     public void apply(INDArray param) {
         INDArray norm = param.norm2(dimensions);
         INDArray clipped = norm.unsafeDuplication();
-        BooleanIndexing.replaceWhere(clipped, max, Conditions.greaterThan(max));
-        BooleanIndexing.replaceWhere(clipped, min, Conditions.lessThan(min));
+        CustomOp op = DynamicCustomOp.builder("clipbyvalue")
+                .setInputs(clipped)
+                .callInplace(true)
+                .setFloatingPointArguments(min, max)
+                .build();
+        Nd4j.getExecutioner().exec(op);
 
         norm.addi(epsilon);
         clipped.divi(norm);
