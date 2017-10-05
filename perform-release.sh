98c2266cbc88a6fa79d3d676edf6f93577263b81
Samuel Audet <samuel.audet@gmail.com>
2017-10-05 10:48:26 +0900

    Update perform-release.sh for CUDA 9.0

--- perform-release.sh
+++ perform-release.sh
@@ -32,17 +32,17 @@ mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$
 if [[ "${SKIP_BUILD}" == "0" ]]; then
     source change-spark-versions.sh 1
     source change-scala-versions.sh 2.10
-    source change-cuda-versions.sh 7.5
+    source change-cuda-versions.sh 8.0
     mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
     source change-scala-versions.sh 2.11
-    source change-cuda-versions.sh 8.0
+    source change-cuda-versions.sh 9.0
     mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
     source change-spark-versions.sh 2
     mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY -Dspark.major.version=2
 
     source change-spark-versions.sh 1
     source change-scala-versions.sh 2.11
-    source change-cuda-versions.sh 8.0
+    source change-cuda-versions.sh 9.0
 fi
 git commit -s -a -m "Update to version $RELEASE_VERSION"
 git tag -s -a -m "deeplearning4j-$RELEASE_VERSION" "deeplearning4j-$RELEASE_VERSION"
