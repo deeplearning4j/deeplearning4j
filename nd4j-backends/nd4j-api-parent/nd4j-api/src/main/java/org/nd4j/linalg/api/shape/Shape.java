36f7e5537fa79b0ada81fd2239b0a4baf789a8e8
raver119@gmail.com <raver119@gmail.com>
2017-10-06 19:29:27 +0300

    toMuulCompatible method restored

--- nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/shape/Shape.java
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/shape/Shape.java
@@ -2249,5 +2249,24 @@ public class Shape {
         return Arrays.equals(in.stride(), stridesIfContiguous);
     }
 
+    /**
+     * This method is used in DL4J LSTM implementation
+     * @param input
+     * @return
+     */
+    public static INDArray toMmulCompatible(INDArray input) {
+        if (input.rank() != 2)
+            throw new IllegalArgumentException("Input must be rank 2 (matrix)");
+        //Same conditions as GemmParams.copyIfNecessary()
+        boolean doCopy = false;
+        if (input.ordering() == 'c' && (input.stride(0) != input.size(1) || input.stride(1) != 1))
+            doCopy = true;
+        else if (input.ordering() == 'f' && (input.stride(0) != 1 || input.stride(1) != input.size(0)))
+            doCopy = true;
 
+        if (doCopy)
+            return Shape.toOffsetZeroCopyAnyOrder(input);
+        else
+            return input;
+    }
 }
