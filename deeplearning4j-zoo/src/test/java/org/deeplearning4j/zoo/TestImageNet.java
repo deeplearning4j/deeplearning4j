0c7ec7f4598030e83e53aa1523514e5774f119c3
Samuel Audet <samuel.audet@gmail.com>
2017-10-07 11:23:51 +0900

    Add Darknet19 to the zoo (#4149)

--- deeplearning4j-zoo/src/test/java/org/deeplearning4j/zoo/TestImageNet.java
+++ deeplearning4j-zoo/src/test/java/org/deeplearning4j/zoo/TestImageNet.java
@@ -2,16 +2,23 @@ package org.deeplearning4j.zoo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.transform.ColorConversionTransform;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.zoo.model.Darknet19;
 import org.deeplearning4j.zoo.model.VGG19;
 import org.deeplearning4j.zoo.util.imagenet.ImageNetLabels;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 
 import java.io.IOException;
 
+import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -40,6 +47,28 @@ public class TestImageNet {
         String decodedLabels = new ImageNetLabels().decodePredictions(output[0]);
         log.info(decodedLabels);
         assertTrue(decodedLabels.contains("golden_retriever"));
+
+        // clean up for current model
+        Nd4j.getWorkspaceManager().destroyAllWorkspacesForCurrentThread();
+        System.gc();
+
+        // set up model
+        model = new Darknet19(1, 123, 1); //num labels doesn't matter since we're getting pretrained imagenet
+        initializedModel = (ComputationGraph) model.initPretrained();
+
+        // set up input and feedforward
+        loader = new NativeImageLoader(224, 224, 3, new ColorConversionTransform(COLOR_BGR2RGB));
+        image = loader.asMatrix(classloader.getResourceAsStream("goldenretriever.jpg"));
+        scaler = new ImagePreProcessingScaler(0, 1);
+        scaler.transform(image);
+        INDArray result = initializedModel.outputSingle(image);
+        result = Nd4j.vstack(Nd4j.linspace(1, 1000, 1000), result);
+        result = Nd4j.sortColumns(result, 1, false);
+        result = result.get(NDArrayIndex.all(), NDArrayIndex.interval(0, 10));
+
+        // check output labels of result
+        log.info(result.toString());
+        assertEquals(125, (int)result.getDouble(0, 0));
     }
 
 }
