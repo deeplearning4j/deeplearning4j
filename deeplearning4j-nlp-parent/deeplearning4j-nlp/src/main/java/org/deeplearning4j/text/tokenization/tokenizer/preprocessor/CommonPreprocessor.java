1ce05abf65044ad1bbc7013b74f5661eb3d80d9c
tkurosaka <tkurosaka@users.noreply.github.com>
2017-10-12 18:57:27 -0700

    Adding javadoc comment to CommonPreprocessor

--- deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/text/tokenization/tokenizer/preprocessor/CommonPreprocessor.java
+++ deeplearning4j-nlp-parent/deeplearning4j-nlp/src/main/java/org/deeplearning4j/text/tokenization/tokenizer/preprocessor/CommonPreprocessor.java
@@ -3,6 +3,10 @@ package org.deeplearning4j.text.tokenization.tokenizer.preprocessor;
 import org.deeplearning4j.text.tokenization.tokenizer.TokenPreProcess;
 
 /**
+ * A ToeknPreProcess implementation that removes puncuation marks and lower-cases.
+ * <br>
+ * Note that the implementation uses String#toLowerCase(String) and its behavior depends on the default locale.
+ * @see StringCleaning#stripPunct(String)
  * @author jeffreytang
  */
 public class CommonPreprocessor implements TokenPreProcess {
