434691b6b5317a4f89a4ea3a8998b5a81bae3289
raver119 <raver119@gmail.com>
2017-10-24 20:38:26 +0300

    wrong op removed

--- include/ops/declarable/CustomOperations.h
+++ include/ops/declarable/CustomOperations.h
@@ -73,7 +73,9 @@ namespace nd4j {
         DECLARE_CUSTOM_OP(pooling2d, 1, 1, false, 0, 11);
         DECLARE_CUSTOM_OP(avgpool2d_bp, 2, 1, false, 0, 9);
         DECLARE_CUSTOM_OP(pnormpool2d_bp, 2, 1, false, 1, 10);
-        DECLARE_CUSTOM_OP(sru_taolei87, 5, 2, false, 0, 0);
+
+
+        // recurrent ops
         DECLARE_CUSTOM_OP(sru1, 5, 2, false, 0, 0);
         DECLARE_CUSTOM_OP(sru2, 5, 2, false, 0, 0);
         DECLARE_CUSTOM_OP(sru_bp_1, 8, 4, true, 0, 0);        
