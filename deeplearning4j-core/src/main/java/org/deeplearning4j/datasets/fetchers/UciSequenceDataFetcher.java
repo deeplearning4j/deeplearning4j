de9a12b48716ed41d6810e0490c0630c010d979a
Briton Park <bgpark738@gmail.com>
2017-11-03 09:44:06 -0700

    Add UCI sequence iterator and fetcher.

---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/fetchers/UciSequenceDataFetcher.java
@@ -0,0 +1,4 @@
+package org.deeplearning4j.datasets.fetchers;
+
+public class UciDataFetcher {
+}
---
+++ deeplearning4j-core/src/main/java/org/deeplearning4j/datasets/iterator/impl/UciSequenceDataSetIterator.java
@@ -0,0 +1,4 @@
+package org.deeplearning4j.datasets.iterator.impl;
+
+public class UciSequenceDataSetIterator {
+}
