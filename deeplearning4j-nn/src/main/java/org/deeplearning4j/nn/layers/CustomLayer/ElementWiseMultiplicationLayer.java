dee7c7cecb677471e0a05251d17cf6c00b8530af
Jingshu LIU <jingshu@dictastages1>
2017-11-07 09:28:06 +0100

    components for a simple learnable element wise layer

---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/CustomLayer/ElementWiseMultiplicationLayer.java
@@ -0,0 +1,108 @@
+package Utils.Utils_dpl4j.CustomLayer;
+
+import Utils.Utils_dpl4j.CustomParamInitializer.ElementWiseParamInitializer;
+import lombok.*;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.ParamInitializer;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.FeedForwardLayer;
+import org.deeplearning4j.nn.conf.memory.LayerMemoryReport;
+import org.deeplearning4j.nn.conf.memory.MemoryReport;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import java.util.Collection;
+import java.util.Map;
+
+
+/**
+ * created by jingshu
+ */
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ElementWiseMultiplicationLayer extends org.deeplearning4j.nn.conf.layers.FeedForwardLayer {
+
+//  ATTENTION!!! we have to add an empty constructor for custom layers otherwise we will have errors when loading the model
+    public ElementWiseMultiplicationLayer(){}
+
+    protected ElementWiseMultiplicationLayer(Builder builder){
+        super(builder);
+    }
+
+    @Override
+    public ElementWiseMultiplicationLayer clone() {
+        ElementWiseMultiplicationLayer clone = (ElementWiseMultiplicationLayer) super.clone();
+        return clone;
+    }
+
+    @Override
+    public Layer instantiate(NeuralNetConfiguration conf, Collection<IterationListener> iterationListeners, int layerIndex, INDArray layerParamsView, boolean initializeParams) {
+        if(this.nIn!=this.nOut) throw  new IllegalStateException("element wise layer must have the same input and output size!!!");
+        ElementWiseMultiplicationLayer_impl ret = new ElementWiseMultiplicationLayer_impl(conf);
+        ret.setListeners(iterationListeners);
+        ret.setIndex(layerIndex);
+        ret.setParamsViewArray(layerParamsView);
+        Map<String, INDArray> paramTable = initializer().init(conf, layerParamsView, initializeParams);
+        ret.setParamTable(paramTable);
+        ret.setConf(conf);
+
+        return ret;
+    }
+
+    @Override
+    public ParamInitializer initializer() {
+        return ElementWiseParamInitializer.getInstance();
+    }
+
+    /**
+     * This is a report of the estimated memory consumption for the given layer
+     *
+     * @param inputType Input type to the layer. Memory consumption is often a function of the input type
+     * @return Memory report for the layer
+     */
+    @Override
+    public LayerMemoryReport getMemoryReport(InputType inputType) {
+        InputType outputType = getOutputType(-1, inputType);
+
+        int numParams = initializer().numParams(this);
+        int updaterStateSize = (int) getIUpdater().stateSize(numParams);
+
+        int trainSizeFixed = 0;
+        int trainSizeVariable = 0;
+        if (getDropOut() > 0) {
+            if (false) {
+                //TODO drop connect
+                //Dup the weights... note that this does NOT depend on the minibatch size...
+                trainSizeVariable += 0; //TODO
+            } else {
+                //Assume we dup the input
+                trainSizeVariable += inputType.arrayElementsPerExample();
+            }
+        }
+
+        //Also, during backprop: we do a preOut call -> gives us activations size equal to the output size
+        // which is modified in-place by activation function backprop
+        // then we have 'epsilonNext' which is equivalent to input size
+        trainSizeVariable += outputType.arrayElementsPerExample();
+
+        return new LayerMemoryReport.Builder(layerName, ElementWiseMultiplicationLayer.class, inputType, outputType)
+                .standardMemory(numParams, updaterStateSize)
+                .workingMemory(0, 0, trainSizeFixed, trainSizeVariable) //No additional memory (beyond activations) for inference
+                .cacheMemory(MemoryReport.CACHE_MODE_ALL_ZEROS, MemoryReport.CACHE_MODE_ALL_ZEROS) //No caching in DenseLayer
+                .build();
+    }
+
+
+    @AllArgsConstructor
+    public static class Builder extends FeedForwardLayer.Builder<ElementWiseMultiplicationLayer.Builder> {
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public ElementWiseMultiplicationLayer build() {
+            return new ElementWiseMultiplicationLayer(this);
+        }
+    }
+}
---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/CustomLayer/ElementWiseMultiplicationLayer_impl.java
@@ -0,0 +1,158 @@
+package Utils.Utils_dpl4j.CustomLayer;
+
+
+import Utils.Utils_dpl4j.CustomParamInitializer.ElementWiseParamInitializer;
+import org.deeplearning4j.exception.DL4JInvalidInputException;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.gradient.DefaultGradient;
+import org.deeplearning4j.nn.gradient.Gradient;
+import org.deeplearning4j.nn.layers.BaseLayer;
+import org.deeplearning4j.nn.params.DefaultParamInitializer;
+import org.deeplearning4j.util.Dropout;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
+import org.nd4j.linalg.primitives.Pair;
+
+import java.util.Arrays;
+
+/**
+ * created by jingshu
+ */
+public class ElementWiseMultiplicationLayer_impl extends BaseLayer<ElementWiseMultiplicationLayer> {
+
+    public ElementWiseMultiplicationLayer_impl(NeuralNetConfiguration conf){
+        super(conf);
+    }
+
+    public ElementWiseMultiplicationLayer_impl(NeuralNetConfiguration conf, INDArray input) {
+        super(conf, input);
+    }
+
+    @Override
+    public Gradient error(INDArray errorSignal) {
+        INDArray W = getParam(ElementWiseParamInitializer.WEIGHT_KEY);
+        Gradient nextLayerGradient = new DefaultGradient();
+        INDArray wErrorSignal = errorSignal.mul(W);
+        nextLayerGradient.gradientForVariable().put(ElementWiseParamInitializer.WEIGHT_KEY, wErrorSignal);
+        return nextLayerGradient;
+    }
+
+    @Override
+    public Gradient calcGradient(Gradient layerError, INDArray activation) {
+        Gradient ret = new DefaultGradient();
+        INDArray weightErrorSignal = layerError.getGradientFor(ElementWiseParamInitializer.WEIGHT_KEY);
+        INDArray weightError = weightErrorSignal.mul(activation);
+        ret.gradientForVariable().put(ElementWiseParamInitializer.WEIGHT_KEY, weightError);
+        INDArray biasGradient = weightError.mean(0);
+        ret.gradientForVariable().put(ElementWiseParamInitializer.BIAS_KEY, biasGradient);
+
+        return ret;
+    }
+
+    @Override
+    public Pair<Gradient, INDArray> backpropGradient(INDArray epsilon) {
+        //If this layer is layer L, then epsilon for this layer is ((w^(L+1)*(delta^(L+1))^T))^T (or equivalent)
+        INDArray z = preOutput(true); //Note: using preOutput(INDArray) can't be used as this does a setInput(input) and resets the 'appliedDropout' flag
+        //INDArray activationDerivative = Nd4j.getExecutioner().execAndReturn(Nd4j.getOpFactory().createTransform(conf().getLayer().getActivationFunction(), z).derivative());
+        //        INDArray activationDerivative = conf().getLayer().getActivationFn().getGradient(z);
+        //        INDArray delta = epsilon.muli(activationDerivative);
+        INDArray delta = layerConf().getActivationFn().backprop(z, epsilon).getFirst(); //TODO handle activation function params
+
+//        if activation is a identity function
+//        delta = Nd4j.ones(this.layerConf().getNIn());
+
+        if (maskArray != null) {
+            applyMask(delta);
+        }
+
+        Gradient ret = new DefaultGradient();
+
+        INDArray weightGrad = gradientViews.get(ElementWiseParamInitializer.WEIGHT_KEY); //f order
+
+        for(int row =0;row<input.rows();row++){
+            weightGrad.addi(input.getRow(row).mul(delta.getRow(row)));
+        }
+
+//        weightGrad.addi(input.getRow(0).mul(delta));
+
+        INDArray biasGrad = gradientViews.get(ElementWiseParamInitializer.BIAS_KEY);
+        delta.sum(biasGrad, 0); //biasGrad is initialized/zeroed first
+
+        ret.gradientForVariable().put(ElementWiseParamInitializer.WEIGHT_KEY, weightGrad);
+        ret.gradientForVariable().put(ElementWiseParamInitializer.BIAS_KEY, biasGrad);
+
+        INDArray epsilonNext = Nd4j.zeros(params.get(ElementWiseParamInitializer.WEIGHT_KEY).shape());
+
+        for(int row =0;row<delta.rows();row++){
+            epsilonNext.addi(params.get(ElementWiseParamInitializer.WEIGHT_KEY).mul(delta.getRow(row)));
+        }
+
+        return new Pair<>(ret, epsilonNext);
+    }
+
+    @Override
+    public void fit(INDArray input) {}
+
+    /**
+     * Returns true if the layer can be trained in an unsupervised/pretrain manner (VAE, RBMs etc)
+     *
+     * @return true if the layer can be pretrained (using fit(INDArray), false otherwise
+     */
+    @Override
+    public boolean isPretrainLayer() {
+        return false;
+    }
+
+    public INDArray preOutput(boolean training) {
+        applyDropOutIfNecessary(training);
+        INDArray b = getParam(DefaultParamInitializer.BIAS_KEY);
+        INDArray W = getParam(DefaultParamInitializer.WEIGHT_KEY);
+
+        //Input validation:
+        if ( input.columns() != W.columns()) {
+
+            throw new DL4JInvalidInputException(
+                    "Input size (" + input.columns() + " columns; shape = " + Arrays.toString(input.shape())
+                            + ") is invalid: does not match layer input size (layer # inputs = "
+                            + W.shapeInfoToString() + ") " + layerId());
+        }
+
+        if (conf.isUseDropConnect() && training && layerConf().getDropOut() > 0) {
+            W = Dropout.applyDropConnect(this, DefaultParamInitializer.WEIGHT_KEY);
+        }
+
+//        System.out.println(W.shapeInfoToString());
+//        System.out.println(input.shapeInfoToString());
+
+        INDArray ret = Nd4j.zeros(input.rows(),input.columns());
+
+        for(int row = 0; row<input.rows();row++){
+            ret.put(new INDArrayIndex[]{NDArrayIndex.point(row), NDArrayIndex.all()},input.getRow(row).mul(W).addRowVector(b));
+        }
+
+//        INDArray ret = input.mul(W).addRowVector(b);
+
+        if (maskArray != null) {
+            applyMask(ret);
+        }
+
+        return ret;
+    }
+
+
+    @Override
+    public INDArray activationMean() {
+        INDArray b = getParam(DefaultParamInitializer.BIAS_KEY);
+        INDArray W = getParam(DefaultParamInitializer.WEIGHT_KEY);
+        INDArray ret = Nd4j.zeros(input.rows(),input.columns());
+
+        for(int row = 0; row<input.rows();row++){
+            ret.put(new INDArrayIndex[]{NDArrayIndex.point(row), NDArrayIndex.all()},input.getRow(row).mul(W).addRowVector(b));
+        }
+        return ret;
+    }
+
+
+}
