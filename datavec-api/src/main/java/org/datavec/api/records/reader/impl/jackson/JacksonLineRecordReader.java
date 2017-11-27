90055814979ad2c63aa0a0c4908165cad86dd283
Peter Lin <woolfel@gmail.com>
2017-11-27 12:19:19 -0500

    more javadoc
    added a quick explanation of the limitations to make it clear the user
    has to provide the labels for training.

--- datavec-api/src/main/java/org/datavec/api/records/reader/impl/jackson/JacksonLineRecordReader.java
+++ datavec-api/src/main/java/org/datavec/api/records/reader/impl/jackson/JacksonLineRecordReader.java
@@ -22,10 +22,18 @@ import org.nd4j.shade.jackson.databind.ObjectMapper;
  * to make sure splits work properly in a cluster environment. For those new to Hadoop<br/>
  * file format convention, the reason is a large file can be split into chunks and<br/>
  * sent to different nodes in a cluster. If a record spanned multiple lines, split<br/>
- * wouldn't be the complete record, which will result in runtime errors and calculation<br/>
+ * might not get the complete record, which will result in runtime errors and calculation<br/>
  * errors. Where and how a job splits a file varies depending on the job configuration<br/>
- * and cluster size. It's good practice to format the data to work well with splits.<br/>
+ * and cluster size.<br/>
  * <br/>
+ * A couple of important notes. The reader doesn't automatically create labels for each<br/>
+ * record like JacksonRecordReader. JacksonRecordReader uses the folder name for the label<br/>
+ * at runtime. It assumes a top level folder has multiple subfolders. The labels are the
+ * subfolder names.<br/>
+ * <br/>
+ * In the case of JacksonLineRecordReader, you have to provide the labels in the configuration<br/>
+ * for the training. Please look at the examples in dl4j-examples repository on how to provide
+ * labels.<br/>
  * 
  * @author peter
  *
