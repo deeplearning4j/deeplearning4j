8e7069ce04086ac42ef3a46ac376e9d65a846204
Alex Black <blacka101@gmail.com>
2017-12-06 21:12:46 +1100

    Fix javadoc typo

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/RnnLossLayer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/RnnLossLayer.java
@@ -27,7 +27,7 @@ import java.util.Map;
  * Recurrent Neural Network Loss Layer.<br>
  * Handles calculation of gradients etc for various objective functions.<br>
  * NOTE: Unlike {@link RnnOutputLayer} this RnnLossLayer does not have any parameters - i.e., there is no time
- * distributed dense component here. Consequently, the output activations size is equail to the input size.<br>
+ * distributed dense component here. Consequently, the output activations size is equal to the input size.<br>
  * Input and output activations are same as other RNN layers: 3 dimensions with shape
  * [miniBatchSize,nIn,timeSeriesLength] and [miniBatchSize,nOut,timeSeriesLength] respectively.
  *
