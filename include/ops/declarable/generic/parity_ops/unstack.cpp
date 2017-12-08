e5ff51be9424cf8ed382119ff5d9a8303f047157
raver119 <raver119@gmail.com>
2017-12-08 09:24:27 +0300

    unstack allows negative dims (#615)

--- include/ops/declarable/generic/parity_ops/unstack.cpp
+++ include/ops/declarable/generic/parity_ops/unstack.cpp
@@ -1,21 +1,30 @@
 //
-// Created by raver119 on 12.10.2017.
+// @author raver119@gmail.com
 //
 
 #include <ops/declarable/CustomOperations.h>
-#include <TAD.h>
 
 namespace nd4j {
     namespace ops {
-        CUSTOM_OP_IMPL(tear, 1, -1, false, 0, -1) {
+        /**
+         * This op does the same as tear, just uses different input format:
+         * @tparam T
+         */
+        CUSTOM_OP_IMPL(unstack, 1, -1, false, 0, 1) {
             auto input = INPUT_VARIABLE(0);
 
-            REQUIRE_TRUE(!block.getIArguments()->empty(), 0, "At least 1 dimension should be specified for Tear");
+            auto dim = INT_ARG(0);
+            if (dim < 0)
+                dim += input->rankOf();
 
-            std::vector<int> dims(*block.getIArguments());
 
-            for (auto &v: dims)
-                REQUIRE_TRUE(v >= 0 && v < input->rankOf(), 0, "Tear dimensions should be non-negative values, and lower then input rank. Got %i instead", v);
+            REQUIRE_TRUE(dim < input->rankOf(), 0, "Unstack dimension should be lower then rank of input");
+            REQUIRE_TRUE(dim >= 0, 0, "Unstack dimension should be non-negative value");
+
+            std::vector<int> dims;
+            for (int e = 0; e < input->rankOf(); e++)
+                if (e != dim)
+                    dims.emplace_back(e);
 
             auto tads = NDArrayFactory<T>::allTensorsAlongDimension(input, dims);
             for (int e = 0; e < tads->size(); e++) {
@@ -29,11 +38,19 @@ namespace nd4j {
 
             return ND4J_STATUS_OK;
         }
-        DECLARE_SHAPE_FN(tear) {
+        DECLARE_SYN(unpack, unstack);
+        
+        DECLARE_SHAPE_FN(unstack) {
             auto inShape = inputShape->at(0);
 
-            std::vector<int> dims(*block.getIArguments());
-            std::sort(dims.begin(), dims.end());
+            auto dim = INT_ARG(0);
+            if (dim < 0)
+                dim += shape::rank(inShape);
+
+            std::vector<int> dims;
+            for (int e = 0; e < shape::rank(inShape); e++)
+                if (e != dim)
+                    dims.emplace_back(e);
 
             shape::TAD tad(inShape, dims.data(), (int) dims.size());
             tad.createTadOnlyShapeInfo();
