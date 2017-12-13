94555a48a0e14200f85165a6ef825b646b409c55
Susan Eraly <susan.eraly@gmail.com>
2017-12-12 20:42:28 -0800

    skip model by name in test (#2375)
    * cleaned up search
    
    * change def test and fail threshold
    
    * skip model added

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestAllLibnd4j.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestAllLibnd4j.java
@@ -1,5 +1,6 @@
 package org.nd4j.imports;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -7,8 +8,7 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 import static org.nd4j.imports.TFGraphTestAllHelper.checkOnlyOutput;
 import static org.nd4j.imports.TFGraphTestAllHelper.fetchTestParams;
@@ -17,15 +17,22 @@ import static org.nd4j.imports.TFGraphTestAllHelper.fetchTestParams;
  * Created by susaneraly on 11/29/17.
  */
 @RunWith(Parameterized.class)
+@Slf4j
 public class TFGraphTestAllLibnd4j {
     private Map<String, INDArray> inputs;
     private Map<String, INDArray> predictions;
     private String modelName;
-    private static final TFGraphTestAllHelper.ExecuteWith executeWith = TFGraphTestAllHelper.ExecuteWith.LIBND4J;
+    private static final TFGraphTestAllHelper.ExecuteWith EXECUTE_WITH = TFGraphTestAllHelper.ExecuteWith.LIBND4J;
+    private static final String[] SKIP_ARR = new String[] {
+            "deep_mnist",
+            "deep_mnist_no_dropout",
+            "inception_v3_with_softmax"
+    };
+    public static final Set<String> SKIP_SET = new HashSet<>(Arrays.asList(SKIP_ARR));
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() throws IOException {
-        return fetchTestParams(executeWith);
+        return fetchTestParams(EXECUTE_WITH);
     }
 
     public TFGraphTestAllLibnd4j(Map<String, INDArray> inputs, Map<String, INDArray> predictions, String modelName) throws IOException {
@@ -37,7 +44,11 @@ public class TFGraphTestAllLibnd4j {
     @Test
     public void test() throws Exception {
         Nd4j.create(1);
-        checkOnlyOutput(inputs, predictions, modelName, executeWith);
+        if (SKIP_SET.contains(modelName)) {
+            log.info("\n\tSKIPPED MODEL: " + modelName);
+            return;
+        }
+        checkOnlyOutput(inputs, predictions, modelName, EXECUTE_WITH);
     }
 
 }
--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestAllSameDiff.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphTestAllSameDiff.java
@@ -1,5 +1,6 @@
 package org.nd4j.imports;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -7,24 +8,30 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 import static org.nd4j.imports.TFGraphTestAllHelper.*;
 
 /**
  * Created by susaneraly on 11/29/17.
  */
+@Slf4j
 @RunWith(Parameterized.class)
 public class TFGraphTestAllSameDiff {
     private Map<String, INDArray> inputs;
     private Map<String, INDArray> predictions;
     private String modelName;
-    private static final ExecuteWith executeWith = ExecuteWith.SAMEDIFF;
+    private static final ExecuteWith EXECUTE_WITH = ExecuteWith.SAMEDIFF;
+    private static final String[] SKIP_ARR = new String[] {
+            "deep_mnist",
+            "deep_mnist_no_dropout",
+            "inception_v3_with_softmax"
+    };
+    public static final Set<String> SKIP_SET = new HashSet<>(Arrays.asList(SKIP_ARR));
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() throws IOException {
-        return fetchTestParams(executeWith);
+        return fetchTestParams(EXECUTE_WITH);
     }
 
     public TFGraphTestAllSameDiff(Map<String, INDArray> inputs, Map<String, INDArray> predictions, String modelName) throws IOException {
@@ -36,7 +43,11 @@ public class TFGraphTestAllSameDiff {
     @Test
     public void testOutputOnly() throws Exception {
         Nd4j.create(1);
-        checkOnlyOutput(inputs, predictions, modelName, executeWith);
+        if (SKIP_SET.contains(modelName)) {
+            log.info("\n\tSKIPPED MODEL: " + modelName);
+            return;
+        }
+        checkOnlyOutput(inputs, predictions, modelName, EXECUTE_WITH);
     }
 
 }
