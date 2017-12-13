c8df90bcad2b565e88bb0000bf0e60923745f437
Alex Black <blacka101@gmail.com>
2017-12-13 15:15:53 +1100

    Remove old commented out code

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/BaseLayer.java
@@ -355,35 +355,6 @@ public abstract class BaseLayer<LayerConfT extends org.deeplearning4j.nn.conf.la
         return ret;
     }
 
-//    @Override
-//    public double calcL2(boolean backpropParamsOnly) {
-//        //L2 norm: sqrt( sum_i x_i^2 ) -> want sum squared weights, so l2 norm squared
-//        double l2Sum = 0.0;
-//        if (conf.getL2ByParam(DefaultParamInitializer.WEIGHT_KEY) > 0.0) {
-//            double l2Norm = getParam(DefaultParamInitializer.WEIGHT_KEY).norm2Number().doubleValue();
-//            l2Sum += 0.5 * conf.getL2ByParam(DefaultParamInitializer.WEIGHT_KEY) * l2Norm * l2Norm;
-//        }
-//        if (hasBias() && conf.getL2ByParam(DefaultParamInitializer.BIAS_KEY) > 0.0) {
-//            double l2Norm = getParam(DefaultParamInitializer.BIAS_KEY).norm2Number().doubleValue();
-//            l2Sum += 0.5 * conf.getL2ByParam(DefaultParamInitializer.BIAS_KEY) * l2Norm * l2Norm;
-//        }
-//        return l2Sum;
-//    }
-//
-//    @Override
-//    public double calcL1(boolean backpropParamsOnly) {
-//        double l1Sum = 0.0;
-//        if (conf.getL1ByParam(DefaultParamInitializer.WEIGHT_KEY) > 0.0) {
-//            l1Sum += conf.getL1ByParam(DefaultParamInitializer.WEIGHT_KEY)
-//                            * getParam(DefaultParamInitializer.WEIGHT_KEY).norm1Number().doubleValue();
-//        }
-//        if (hasBias() && conf.getL1ByParam(DefaultParamInitializer.BIAS_KEY) > 0.0) {
-//            l1Sum += conf.getL1ByParam(DefaultParamInitializer.BIAS_KEY)
-//                            * getParam(DefaultParamInitializer.BIAS_KEY).norm1Number().doubleValue();
-//        }
-//        return l1Sum;
-//    }
-
     @Override
     public double calcL2(boolean backpropParamsOnly) {
         double l2Sum = 0.0;
