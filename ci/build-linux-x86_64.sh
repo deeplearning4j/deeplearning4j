7112b21b7f9b60be641bc5ccd165a8b7d7dea4e7
Samuel Audet <samuel.audet@gmail.com>
2017-12-22 16:11:45 +0900

    Update .travis.yml and build script for Linux (#479)

---
+++ ci/build-linux-x86_64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -evx
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    MAVEN_PHASE="deploy"
+else
+    MAVEN_PHASE="install"
+fi
+
+source change-scala-versions.sh $SCALA
+source change-spark-versions.sh $SPARK
+mvn clean $MAVEN_PHASE -B -U --settings ./ci/settings.xml -Dmaven.test.skip=true -Dlocal.software.repository=sonatype
+
