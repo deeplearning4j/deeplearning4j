a06979583fe29090beeea5565d0810230476afba
raver119 <raver119@gmail.com>
2018-01-06 22:41:15 +0300

    DataSetSplitter

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/datasets/iterator/DataSetIteratorSplitter.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/datasets/iterator/DataSetIteratorSplitter.java
@@ -3,25 +3,22 @@ package org.deeplearning4j.datasets.iterator;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.nd4j.linalg.dataset.api.MultiDataSet;
-import org.nd4j.linalg.dataset.api.MultiDataSetPreProcessor;
-import org.nd4j.linalg.dataset.api.iterator.MultiDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.exception.ND4JIllegalStateException;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * This iterator virtually splits given MultiDataSetIterator into Train and Test parts.
- *
- * PLEASE NOTE: You can't use Test iterator twice in a row.
- * PLEASE NOTE: You can't use this iterator, if underlying iterator uses randomization/shuffle between epochs.
  *
  * @author raver119@gmail.com
  */
 @Slf4j
-public class MultiDataSetIteratorSplitter {
-    protected MultiDataSetIterator backedIterator;
+public class DataSetIteratorSplitter {
+    protected DataSetIterator backedIterator;
     protected final long totalExamples;
     protected final double ratio;
     protected final long numTrain;
@@ -30,15 +27,9 @@ public class MultiDataSetIteratorSplitter {
     protected AtomicLong counter = new AtomicLong(0);
 
     protected AtomicBoolean resetPending = new AtomicBoolean(false);
-    protected org.nd4j.linalg.dataset.MultiDataSet firstTrain = null;
+    protected DataSet firstTrain = null;
 
-    /**
-     *
-     * @param baseIterator
-     * @param totalExamples - total number of examples in underlying iterator. this value will be used to determine number of test/train examples
-     * @param ratio - this value will be used as splitter. should be between in range of 0.0 > X < 1.0. I.e. if value 0.7 is provided, then 70% of total examples will be used for training, and 30% of total examples will be used for testing
-     */
-    public MultiDataSetIteratorSplitter(@NonNull MultiDataSetIterator baseIterator, long totalExamples, double ratio) {
+    public DataSetIteratorSplitter(@NonNull DataSetIterator baseIterator, long totalExamples, double ratio) {
         if (!(ratio > 0.0 && ratio < 1.0))
             throw new ND4JIllegalStateException("Ratio value should be in range of 0.0 > X < 1.0");
 
@@ -58,21 +49,37 @@ public class MultiDataSetIteratorSplitter {
         log.warn("IteratorSplitter is used: please ensure you don't use randomization/shuffle in underlying iterator!");
     }
 
-    public MultiDataSetIterator getTrainIterator() {
-        return new MultiDataSetIterator() {
+
+    public DataSetIterator getTrainIterator() {
+        return new DataSetIterator() {
             @Override
-            public MultiDataSet next(int num) {
-                throw new UnsupportedOperationException("To be implemented yet");
+            public DataSet next(int i) {
+                throw new UnsupportedOperationException();
             }
 
             @Override
-            public void setPreProcessor(MultiDataSetPreProcessor preProcessor) {
-                backedIterator.setPreProcessor(preProcessor);
+            public List<String> getLabels() {
+                throw new UnsupportedOperationException();
             }
 
             @Override
-            public MultiDataSetPreProcessor getPreProcessor() {
-                return backedIterator.getPreProcessor();
+            public int totalExamples() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int inputColumns() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int totalOutcomes() {
+                throw new UnsupportedOperationException();
             }
 
             @Override
@@ -89,6 +96,32 @@ public class MultiDataSetIteratorSplitter {
             public void reset() {
                 resetPending.set(true);
             }
+
+            @Override
+            public int batch() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int cursor() {
+                return counter.intValue();
+            }
+
+            @Override
+            public int numExamples() {
+                return (int) numTrain;
+            }
+
+            @Override
+            public void setPreProcessor(DataSetPreProcessor dataSetPreProcessor) {
+                backedIterator.setPreProcessor(dataSetPreProcessor);
+            }
+
+            @Override
+            public DataSetPreProcessor getPreProcessor() {
+                return backedIterator.getPreProcessor();
+            }
+
 
             @Override
             public boolean hasNext() {
@@ -109,47 +142,56 @@ public class MultiDataSetIteratorSplitter {
             }
 
             @Override
-            public MultiDataSet next() {
+            public DataSet next() {
                 counter.incrementAndGet();
                 val p = backedIterator.next();
 
                 if (counter.get() == 1 && firstTrain == null) {
                     // first epoch ever, we'll save first dataset and will use it to check for equality later
-                    firstTrain = (org.nd4j.linalg.dataset.MultiDataSet) p.copy();
+                    firstTrain =  p.copy();
                     firstTrain.detach();
                 } else if (counter.get() == 1) {
                     // epoch > 1, comparing first dataset to previously stored dataset. they should be equal
                     int cnt = 0;
-                    for (val c: p.getFeatures())
-                        if (!c.equalsWithEps(firstTrain.getFeatures()[cnt++], 1e-5))
-                            throw new ND4JIllegalStateException("First examples do not match. Randomization was used?");
+                    if (!p.getFeatures().equalsWithEps(firstTrain.getFeatures(), 1e-5))
+                        throw new ND4JIllegalStateException("First examples do not match. Randomization was used?");
                 }
 
                 return p;
+            }
+        };
+    }
+
+    public DataSetIterator getTestIterator() {
+        return new DataSetIterator() {
+            @Override
+            public DataSet next(int i) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public List<String> getLabels() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int totalExamples() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int inputColumns() {
+                throw new UnsupportedOperationException();
             }
 
             @Override
             public void remove() {
                 throw new UnsupportedOperationException();
             }
-        };
-    }
-
-    public MultiDataSetIterator getTestIterator() {
-        return new MultiDataSetIterator() {
-            @Override
-            public MultiDataSet next(int num) {
-                throw new UnsupportedOperationException("To be implemented yet");
-            }
 
             @Override
-            public void setPreProcessor(MultiDataSetPreProcessor preProcessor) {
-                backedIterator.setPreProcessor(preProcessor);
-            }
-
-            @Override
-            public MultiDataSetPreProcessor getPreProcessor() {
-                return backedIterator.getPreProcessor();
+            public int totalOutcomes() {
+                throw new UnsupportedOperationException();
             }
 
             @Override
@@ -162,11 +204,36 @@ public class MultiDataSetIteratorSplitter {
                 return backedIterator.asyncSupported();
             }
 
-
             @Override
             public void reset() {
                 resetPending.set(true);
             }
+
+            @Override
+            public int batch() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int cursor() {
+                return counter.intValue();
+            }
+
+            @Override
+            public int numExamples() {
+                return (int) numTest;
+            }
+
+            @Override
+            public void setPreProcessor(DataSetPreProcessor dataSetPreProcessor) {
+                backedIterator.setPreProcessor(dataSetPreProcessor);
+            }
+
+            @Override
+            public DataSetPreProcessor getPreProcessor() {
+                return backedIterator.getPreProcessor();
+            }
+
 
             @Override
             public boolean hasNext() {
@@ -178,13 +245,8 @@ public class MultiDataSetIteratorSplitter {
             }
 
             @Override
-            public MultiDataSet next() {
+            public DataSet next() {
                 return backedIterator.next();
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
             }
         };
     }
