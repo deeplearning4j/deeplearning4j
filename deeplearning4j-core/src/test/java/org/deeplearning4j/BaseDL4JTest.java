a3a5ce25bde17777b7f892ee41d792becc66ac6d
Alex Black <blacka101@gmail.com>
2018-02-05 16:09:22 +1100

    Additional workspace test, also destroy workspaces after each test

--- deeplearning4j-core/src/test/java/org/deeplearning4j/BaseDL4JTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/BaseDL4JTest.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j;
 
+import org.junit.After;
 import org.junit.Before;
 import org.nd4j.linalg.api.ops.executioner.OpExecutioner;
 import org.nd4j.linalg.factory.Nd4j;
@@ -13,6 +14,12 @@ public class BaseDL4JTest {
     @Before
     public void beforeTest(){
         Nd4j.getExecutioner().setProfilingMode(getProfilingMode());
+    }
+
+    @After
+    public void afterTest(){
+        //Attempt to keep workspaces isolated between tests
+        Nd4j.getWorkspaceManager().destroyAllWorkspacesForCurrentThread();
     }
 
 }
