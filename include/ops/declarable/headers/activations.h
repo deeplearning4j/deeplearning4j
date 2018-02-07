e011efffafe84bafcc7bd1737b6871e9ec1942ce
raver119@gmail.com <raver119@gmail.com>
2018-02-07 09:44:05 +0300

    get rid of polisq

--- include/ops/declarable/headers/activations.h
+++ include/ops/declarable/headers/activations.h
@@ -106,10 +106,5 @@ namespace nd4j {
          */
         DECLARE_CUSTOM_OP(crelu, 1, 1, false, 0, 0);        
         DECLARE_CUSTOM_OP(crelu_bp, 2, 1, false, 0, 0);
-        
-        /**
-         * x^2 + y operation
-         */
-        DECLARE_OP(polisq, 2, 1, true);
     }
 }
