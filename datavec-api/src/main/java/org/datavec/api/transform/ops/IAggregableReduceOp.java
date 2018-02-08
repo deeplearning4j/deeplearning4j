d07e4575868271ce9f78e28449bfaca540624a10
Alex Black <blacka101@gmail.com>
2018-02-08 17:24:16 +1100

    Remove backport functions, switch to nd4j functions

--- datavec-api/src/main/java/org/datavec/api/transform/ops/IAggregableReduceOp.java
+++ datavec-api/src/main/java/org/datavec/api/transform/ops/IAggregableReduceOp.java
@@ -1,14 +1,15 @@
 package org.datavec.api.transform.ops;
 
+import org.nd4j.linalg.function.Consumer;
+import org.nd4j.linalg.function.Supplier;
+
 import java.io.Serializable;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 /**
  * Created by huitseeker on 4/28/17.
  */
 public interface IAggregableReduceOp<T, V> extends Consumer<T>, Supplier<V>, Serializable {
 
-    public <W extends IAggregableReduceOp<T, V>> void combine(W accu);
+    <W extends IAggregableReduceOp<T, V>> void combine(W accu);
 
 }
