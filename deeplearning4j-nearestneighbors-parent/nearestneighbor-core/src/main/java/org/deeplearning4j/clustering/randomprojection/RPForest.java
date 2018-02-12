31f7ee173ada47cd85803526ca1907f09ea972af
Adam Gibson <adam@skymind.io>
2018-02-12 23:05:44 +0900

    split out gradients and errors for gradient checks

--- deeplearning4j-nearestneighbors-parent/nearestneighbor-core/src/main/java/org/deeplearning4j/clustering/randomprojection/RPForest.java
+++ deeplearning4j-nearestneighbors-parent/nearestneighbor-core/src/main/java/org/deeplearning4j/clustering/randomprojection/RPForest.java
@@ -34,7 +34,7 @@ public class RPForest {
     public void fit(INDArray x) {
         this.data = x;
         for(int i = 0; i < numTrees; i++) {
-            RPTree tree = new RPTree(data.columns(),maxSize,0,similarityFunction);
+            RPTree tree = new RPTree(data.columns(),maxSize,similarityFunction);
             tree.buildTree(x);
             trees.add(tree);
         }
