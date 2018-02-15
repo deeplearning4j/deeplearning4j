13b5034b5d43269037be91d3365eddf729ae3536
Martin Boyanov <martin.boyanov@komfo.com>
2018-02-15 15:18:05 +0200

    Work on mask zero for RNNs

---
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/recurrent/MaskZeroLayer.java
@@ -0,0 +1,104 @@
+package org.deeplearning4j.nn.layers.recurrent;
+
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.MaskState;
+import org.deeplearning4j.nn.gradient.Gradient;
+import org.deeplearning4j.nn.layers.wrapper.BaseWrapperLayer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.primitives.Pair;
+
+import lombok.NonNull;
+
+public class MaskZeroLayer extends BaseWrapperLayer {
+
+    public MaskZeroLayer(@NonNull Layer underlying){
+        super(underlying);
+    }
+
+    @Override
+    public void migrateInput() {
+        underlying.migrateInput();
+    }
+
+
+    @Override
+    public Type type() {
+        return Type.RECURRENT;
+    }
+
+    @Override
+    public Pair<Gradient, INDArray> backpropGradient(INDArray epsilon) {
+        return underlying.backpropGradient(epsilon);
+    }
+
+
+    @Override
+    public INDArray preOutput(INDArray x) {
+        return underlying.preOutput(x);
+    }
+
+    @Override
+    public INDArray preOutput(INDArray x, TrainingMode training) {
+        return underlying.preOutput(x, training);
+    }
+
+    @Override
+    public INDArray activate(TrainingMode training) {
+        INDArray input = input();
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate(training);
+    }
+
+    @Override
+    public INDArray activate(INDArray input, TrainingMode training) {
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate(input, training);
+    }
+
+    @Override
+    public INDArray preOutput(INDArray x, boolean training) {
+        return underlying.activate(x, training);
+    }
+
+    @Override
+    public INDArray activate(boolean training) {
+        INDArray input = input();
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate(training);
+    }
+
+    @Override
+    public INDArray activate(INDArray input, boolean training) {
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate(input, training);
+    }
+
+    @Override
+    public INDArray activate() {
+        INDArray input = input();
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate();
+    }
+
+    @Override
+    public INDArray activate(INDArray input) {
+        INDArray mask = input.eq(0).sum(1).neq(input.shape()[1]);
+        underlying.setMaskArray(mask);
+        return underlying.activate(input);
+    }
+
+    @Override
+    public Pair<INDArray, MaskState> feedForwardMaskArray(INDArray maskArray, MaskState currentMaskState, int minibatchSize) {
+        underlying.feedForwardMaskArray(maskArray, currentMaskState, minibatchSize);
+
+        //Input: 2d mask array, for masking a time series. After extracting out the last time step, we no longer need the mask array
+        return new Pair<>(null, currentMaskState);
+    }
+
+
+}
