40da6a3d1a9237d8c0a300769eeae2e02d069ffc
farizrahman4u <farizrahman4u@gmail.com>
2018-02-20 14:39:12 +0530

    test for range of sizes

--- tests/jumpy/buffer_tests.py
+++ tests/jumpy/buffer_tests.py
@@ -8,16 +8,20 @@ class TestBufferCreation(unittest.TestCase):
     init()
 
     def test_buffer_creation(self):
-        buffer = get_buffer_from_arr(np.linspace(1, 4, 4))
-        self.assertEqual(4, buffer.length())
-        self.assertEqual(8, buffer.element_size())
-        for i in range(0, 4):
-            self.assertEqual(i + 1, buffer[i])
+        sizes = [1, 4, 32, 100, 128, 1000, 1024]
+        for size in sizes:
+            buffer = get_buffer_from_arr(np.linspace(1, size, size))
+            self.assertEqual(size, buffer.length())
+            self.assertEqual(8, buffer.element_size())
+            for i in range(0, size):
+                self.assertEqual(i + 1, buffer[i])
 
     def test_buffer_creation_float(self):
-        arr = np.asarray(np.linspace(1, 4, 4), dtype=np.float32)
-        buffer = get_buffer_from_arr(arr)
-        self.assertEquals(4, buffer.length())
-        self.assertEqual(4, buffer.element_size())
-        for i in range(0, 4):
-            self.assertEqual(i + 1, buffer[i])
+        sizes = [1, 4, 32, 100, 128, 1000, 1024]
+        for size in sizes:
+            arr = np.asarray(np.linspace(1, size, size), dtype=np.float32)
+            buffer = get_buffer_from_arr(arr)
+            self.assertEquals(size, buffer.length())
+            self.assertEqual(4, buffer.element_size())
+            for i in range(0, size):
+                self.assertEqual(i + 1, buffer[i])
