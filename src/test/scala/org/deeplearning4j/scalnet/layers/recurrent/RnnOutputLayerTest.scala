c8bbbd3f955fdb730f9a5648bd0a7f8947fd7bf6
ishy7496 <gaarv1911@gmail.com>
2018-03-05 17:11:42 +0100

    couple more tests for RNNOutputLayer

--- src/test/scala/org/deeplearning4j/scalnet/layers/recurrent/RnnOutputLayerTest.scala
+++ src/test/scala/org/deeplearning4j/scalnet/layers/recurrent/RnnOutputLayerTest.scala
@@ -1,7 +1,8 @@
 package org.deeplearning4j.scalnet.layers.recurrent
 
 import org.deeplearning4j.nn.conf.layers.{OutputLayer => JOutputLayer}
-import org.deeplearning4j.scalnet.layers.RnnOutputLayer
+import org.deeplearning4j.nn.layers.BaseOutputLayer
+import org.deeplearning4j.scalnet.layers.{OutputLayer, RnnOutputLayer}
 import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
 import org.scalatest.{Matchers, WordSpec}
@@ -20,8 +21,14 @@ class RnnOutputLayerTest extends WordSpec with Matchers {
       rnnOutputLayer.outputShape shouldBe List(100, 10)
     }
 
-    "compile to a DL4J RnnOutputLayer" in {
+    "compile to a DL4J RnnOutputLayer without loss" in {
       val rnnOutputLayer = RnnOutputLayer(10, 100, Activation.SOFTMAX)
+      val compiledLayer = rnnOutputLayer.compile
+      compiledLayer.isInstanceOf[org.deeplearning4j.nn.conf.layers.RnnOutputLayer] shouldBe true
+    }
+
+    "compile to a DL4J RnnOutputLayer with loss" in {
+      val rnnOutputLayer = RnnOutputLayer(10, 100, Activation.SOFTMAX, Option(LossFunction.MCXENT))
       val compiledLayer = rnnOutputLayer.compile
       compiledLayer.isInstanceOf[org.deeplearning4j.nn.conf.layers.RnnOutputLayer] shouldBe true
     }
@@ -34,6 +41,12 @@ class RnnOutputLayerTest extends WordSpec with Matchers {
     "become an output layer when instanciated with proper loss" in {
       val rnnOutputLayer = RnnOutputLayer(10, 100, Activation.SOFTMAX, Option(LossFunction.MCXENT))
       rnnOutputLayer.output.isOutput shouldBe true
+    }
+
+    "become an output layer when when converted to ouput layer with proper loss" in {
+      val rnnOutputLayer = RnnOutputLayer(10, 100, Activation.SOFTMAX)
+      val compiledLayer = rnnOutputLayer.toOutputLayer(LossFunction.MCXENT)
+      compiledLayer.isInstanceOf[OutputLayer] shouldBe true
     }
 
   }
