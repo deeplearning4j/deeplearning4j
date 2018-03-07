e52618e95227574df14469e8f4d70e413a83fea3
Adam Gibson <adam@skymind.io>
2018-03-07 19:39:51 +0900

    writeModel should not close streams

--- deeplearning4j-nn/src/main/java/org/deeplearning4j/util/ModelSerializer.java
+++ deeplearning4j-nn/src/main/java/org/deeplearning4j/util/ModelSerializer.java
@@ -131,8 +131,8 @@ public class ModelSerializer {
                 Nd4j.write(model.params(), dos);
             } finally {
                 dos.flush();
-                if (!saveUpdater)
-                    dos.close();
+               /* if (!saveUpdater)
+                    dos.close();*/
             }
         } else {
             ZipEntry noParamsMarker = new ZipEntry("noParams.marker");
@@ -155,7 +155,7 @@ public class ModelSerializer {
                     Nd4j.write(updaterState, dos);
                 } finally {
                     dos.flush();
-                    dos.close();
+                    //dos.close();
                 }
             }
         }
