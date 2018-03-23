0f25c6812d735269e33b0b842fdbbad3ffdc2fed
Adam Gibson <adam@skymind.io>
2018-03-23 14:29:14 +0900

    add another arrow to csv test

--- datavec-arrow/src/test/java/org/datavec/arrow/RecordMapperTest.java
+++ datavec-arrow/src/test/java/org/datavec/arrow/RecordMapperTest.java
@@ -1,8 +1,10 @@
 package org.datavec.arrow;
 
+import lombok.val;
 import org.apache.commons.io.FileUtils;
 import org.datavec.api.records.mapper.RecordMapper;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
 import org.datavec.api.split.FileSplit;
 import org.datavec.api.split.partition.NumberOfRecordsPartitioner;
 import org.datavec.api.transform.schema.Schema;
@@ -11,6 +13,7 @@ import org.datavec.api.writable.Writable;
 import org.datavec.arrow.recordreader.ArrowRecordReader;
 import org.datavec.arrow.recordreader.ArrowRecordWriter;
 import org.junit.Test;
+import org.nd4j.linalg.primitives.Triple;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -23,7 +26,75 @@ import static org.junit.Assert.assertEquals;
 public class RecordMapperTest {
 
     @Test
+    public void testCopyFromArrowToCsv() throws Exception {
+        val recordsPair = records();
+
+        Path p = Files.createTempFile("arrowwritetest", ".arrow");
+        FileUtils.write(p.toFile(),recordsPair.getFirst());
+        p.toFile().deleteOnExit();
+
+        ArrowRecordWriter arrowRecordWriter = new ArrowRecordWriter(recordsPair.getMiddle());
+        FileSplit split = new FileSplit(p.toFile());
+        arrowRecordWriter.initialize(split,new NumberOfRecordsPartitioner());
+        arrowRecordWriter.writeBatch(recordsPair.getRight());
+
+
+        ArrowRecordReader arrowRecordReader = new ArrowRecordReader();
+        arrowRecordReader.initialize(split);
+
+
+        CSVRecordWriter csvRecordWriter = new CSVRecordWriter();
+        Path p2 = Files.createTempFile("arrowwritetest", ".csv");
+        FileUtils.write(p2.toFile(),recordsPair.getFirst());
+        p.toFile().deleteOnExit();
+        FileSplit outputCsv = new FileSplit(p2.toFile());
+
+        RecordMapper mapper = RecordMapper.builder().batchSize(10).inputUrl(split)
+                .outputUrl(outputCsv)
+                .partitioner(new NumberOfRecordsPartitioner())
+                .recordReader(arrowRecordReader).recordWriter(csvRecordWriter)
+                .build();
+        mapper.copy();
+
+        CSVRecordReader recordReader = new CSVRecordReader();
+        recordReader.initialize(outputCsv);
+
+
+        List<List<Writable>> loadedCSvRecords = recordReader.next(10);
+        assertEquals(10,loadedCSvRecords.size());
+    }
+
+
+    @Test
     public void testCopyFromCsvToArrow() throws Exception {
+        val recordsPair = records();
+
+        Path p = Files.createTempFile("csvwritetest", ".csv");
+        FileUtils.write(p.toFile(),recordsPair.getFirst());
+        p.toFile().deleteOnExit();
+
+
+        CSVRecordReader recordReader = new CSVRecordReader();
+        FileSplit fileSplit = new FileSplit(p.toFile());
+
+        ArrowRecordWriter arrowRecordWriter = new ArrowRecordWriter(recordsPair.getMiddle());
+        File outputFile = Files.createTempFile("outputarrow","arrow").toFile();
+        FileSplit outputFileSplit = new FileSplit(outputFile);
+        RecordMapper mapper = RecordMapper.builder().batchSize(5).inputUrl(fileSplit)
+                .outputUrl(outputFileSplit).partitioner(new NumberOfRecordsPartitioner())
+                .recordReader(recordReader).recordWriter(arrowRecordWriter)
+                .build();
+        mapper.copy();
+
+        ArrowRecordReader arrowRecordReader = new ArrowRecordReader();
+        arrowRecordReader.initialize(outputFileSplit);
+        List<List<Writable>> next = arrowRecordReader.next(10);
+        System.out.println(next);
+        assertEquals(10,next.size());
+
+    }
+
+    private Triple<String,Schema,List<List<Writable>>> records() {
         List<List<Writable>> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         int numColumns = 3;
@@ -42,33 +113,13 @@ public class RecordMapperTest {
         }
 
 
-        Path p = Files.createTempFile("csvwritetest", ".csv");
-        FileUtils.write(p.toFile(),sb.toString());
-        p.toFile().deleteOnExit();
-
         Schema.Builder schemaBuilder = new Schema.Builder();
         for(int i = 0; i < numColumns; i++) {
             schemaBuilder.addColumnInteger(String.valueOf(i));
         }
 
-        CSVRecordReader recordReader = new CSVRecordReader();
-        FileSplit fileSplit = new FileSplit(p.toFile());
-
-        ArrowRecordWriter arrowRecordWriter = new ArrowRecordWriter(schemaBuilder.build());
-        File outputFile = Files.createTempFile("outputarrow","arrow").toFile();
-        FileSplit outputFileSplit = new FileSplit(outputFile);
-        RecordMapper mapper = RecordMapper.builder().batchSize(5).inputUrl(fileSplit)
-                .outputUrl(outputFileSplit).partitioner(new NumberOfRecordsPartitioner())
-                .recordReader(recordReader).recordWriter(arrowRecordWriter)
-                .build();
-        mapper.copy();
-
-        ArrowRecordReader arrowRecordReader = new ArrowRecordReader();
-        arrowRecordReader.initialize(outputFileSplit);
-        List<List<Writable>> next = arrowRecordReader.next(10);
-        System.out.println(next);
-        assertEquals(10,next.size());
-
+        return Triple.of(sb.toString(),schemaBuilder.build(),list);
     }
+
 
 }
