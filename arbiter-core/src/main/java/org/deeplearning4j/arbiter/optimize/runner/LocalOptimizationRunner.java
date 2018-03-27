317dc842a96ecc5840ebdae5ee5fa90c90ba9566
Alex Black <blacka101@gmail.com>
2018-03-27 19:40:55 +1100

    Extra tests; make LocalOptimizationRunner not require user to specify task creator in most cases

--- arbiter-core/src/main/java/org/deeplearning4j/arbiter/optimize/runner/LocalOptimizationRunner.java
+++ arbiter-core/src/main/java/org/deeplearning4j/arbiter/optimize/runner/LocalOptimizationRunner.java
@@ -3,9 +3,7 @@ package org.deeplearning4j.arbiter.optimize.runner;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
-import org.deeplearning4j.arbiter.optimize.api.Candidate;
-import org.deeplearning4j.arbiter.optimize.api.OptimizationResult;
-import org.deeplearning4j.arbiter.optimize.api.TaskCreator;
+import org.deeplearning4j.arbiter.optimize.api.*;
 import org.deeplearning4j.arbiter.optimize.api.data.DataProvider;
 import org.deeplearning4j.arbiter.optimize.api.score.ScoreFunction;
 import org.deeplearning4j.arbiter.optimize.config.OptimizationConfiguration;
@@ -34,8 +32,16 @@ public class LocalOptimizationRunner extends BaseOptimizationRunner {
     private TaskCreator taskCreator;
     private ListeningExecutorService executor;
 
+    public LocalOptimizationRunner(OptimizationConfiguration config){
+        this(config, null);
+    }
+
     public LocalOptimizationRunner(OptimizationConfiguration config, TaskCreator taskCreator) {
         this(DEFAULT_MAX_CONCURRENT_TASKS, config, taskCreator);
+    }
+
+    public LocalOptimizationRunner(int maxConcurrentTasks, OptimizationConfiguration config){
+        this(maxConcurrentTasks, config, null);
     }
 
     public LocalOptimizationRunner(int maxConcurrentTasks, OptimizationConfiguration config, TaskCreator taskCreator) {
@@ -43,6 +49,17 @@ public class LocalOptimizationRunner extends BaseOptimizationRunner {
         if (maxConcurrentTasks <= 0)
             throw new IllegalArgumentException("maxConcurrentTasks must be > 0 (got: " + maxConcurrentTasks + ")");
         this.maxConcurrentTasks = maxConcurrentTasks;
+
+        if(taskCreator == null){
+            Class<? extends ParameterSpace> psClass = config.getCandidateGenerator().getParameterSpace().getClass();
+            taskCreator = TaskCreatorProvider.defaultTaskCreatorFor(psClass);
+            if(taskCreator == null){
+                throw new IllegalStateException("No TaskCreator was provided and a default TaskCreator cannot be " +
+                        "inferred for ParameterSpace class " + psClass.getName() + ". Please provide a TaskCreator " +
+                        "via the LocalOptimizationRunner constructor");
+            }
+        }
+
         this.taskCreator = taskCreator;
 
         ExecutorService exec = Executors.newFixedThreadPool(maxConcurrentTasks, new ThreadFactory() {
