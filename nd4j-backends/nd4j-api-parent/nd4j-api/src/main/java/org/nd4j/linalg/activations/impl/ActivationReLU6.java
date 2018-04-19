a139cc20f13c50b7f12d71b1a45426e6e075bb46
maxpumperla <max.pumperla@googlemail.com>
2018-04-19 10:51:27 +0200

    relu6 activation wrapper

---
+++ nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/activations/impl/ActivationReLU6.java
@@ -0,0 +1,38 @@
+package org.nd4j.linalg.activations.impl;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.nd4j.linalg.activations.BaseActivationFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.transforms.RectifedLinear;
+import org.nd4j.linalg.api.ops.impl.transforms.Relu6;
+import org.nd4j.linalg.api.ops.impl.transforms.Step;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.primitives.Pair;
+
+/**
+ * f(x) = min(max(input, cutoff), 6)
+ */
+@EqualsAndHashCode
+@Getter
+public class ActivationReLU6 extends BaseActivationFunction {
+
+    @Override
+    public INDArray getActivation(INDArray in, boolean training) {
+        Nd4j.getExecutioner().execAndReturn(new Relu6(in));
+        return in;
+    }
+
+    @Override
+    public Pair<INDArray, INDArray> backprop(INDArray in, INDArray epsilon) {
+        INDArray dLdz = Nd4j.getExecutioner().execAndReturn(new Step(in));
+        dLdz.muli(epsilon);
+        return new Pair<>(dLdz, null);
+    }
+
+    @Override
+    public String toString() {
+        return "relu6";
+    }
+
+}
