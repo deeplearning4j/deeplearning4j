7888329e412f715df0e60996941386e2cbe37def
raver119@gmail.com <raver119@gmail.com>
2018-05-01 14:09:47 -0700

    minor tweak for Apple

--- tests_cpu/layers_tests/GraphTests.cpp
+++ tests_cpu/layers_tests/GraphTests.cpp
@@ -1546,7 +1546,7 @@ TEST_F(GraphTests, Test_Inplace_Outputs_1) {
 }
 
 TEST_F(GraphTests, Test_Inplace_Outputs_2) {
-#ifndef __APPLE__
+#ifndef __APPLE_OS__
     // we dont want testing this on apple. due to try/catch
 
     NDArray<float> x('c', {2, 3}, {1.f, 2.f, 3.f, 4.f, 5.f, 6.f});
