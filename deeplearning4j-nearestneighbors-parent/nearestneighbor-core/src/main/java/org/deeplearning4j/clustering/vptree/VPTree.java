bc01831794e74b481f24fde4379887b773e8ac2f
Alex Black <blacka101@gmail.com>
2018-05-09 14:53:39 +1000

    Minor VPTree validation fix

--- deeplearning4j-nearestneighbors-parent/nearestneighbor-core/src/main/java/org/deeplearning4j/clustering/vptree/VPTree.java
+++ deeplearning4j-nearestneighbors-parent/nearestneighbor-core/src/main/java/org/deeplearning4j/clustering/vptree/VPTree.java
@@ -469,7 +469,7 @@ public class VPTree {
      */
     public void search(@NonNull INDArray target, int k, List<DataPoint> results, List<Double> distances) {
         if (items != null)
-            if (!target.isVector() || target.columns() != items.columns() || target.rows() > 1)
+            if (!target.isVectorOrScalar() || target.columns() != items.columns() || target.rows() > 1)
                 throw new ND4JIllegalStateException("Target for search should have shape of [" + 1 + ", "
                         + items.columns() + "] but got " + Arrays.toString(target.shape()) + " instead");
 
