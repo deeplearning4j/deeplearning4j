d88a9249ecf7ad01be28b6206981f69cf9b0a120
Alex Black <blacka101@gmail.com>
2018-05-11 16:42:24 +1000

    Another minor test fix

--- deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/GravesLSTMOutputTest.java
+++ deeplearning4j-core/src/test/java/org/deeplearning4j/nn/multilayer/GravesLSTMOutputTest.java
@@ -9,6 +9,7 @@ import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
 import org.deeplearning4j.nn.conf.layers.GravesLSTM;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.conf.preprocessor.RnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.conf.stepfunctions.NegativeDefaultStepFunction;
 import org.deeplearning4j.nn.weights.WeightInit;
@@ -91,11 +92,10 @@ public class GravesLSTMOutputTest extends BaseDL4JTest {
                                         .layer(0, new GravesLSTM.Builder().weightInit(WeightInit.DISTRIBUTION)
                                                         .dist(new NormalDistribution(0.0, 0.01)).nIn(nIn)
                                                         .nOut(layerSize).activation(Activation.TANH).build())
-                                        .layer(1, new OutputLayer.Builder(
+                                        .layer(1, new RnnOutputLayer.Builder(
                                                         LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).nIn(layerSize)
                                                                         .nOut(nIn).activation(Activation.SOFTMAX)
                                                                         .build())
-                                        .inputPreProcessor(1, new RnnToFeedForwardPreProcessor()).backprop(true)
                                         .pretrain(false);
         if (useTBPTT) {
             builder.backpropType(BackpropType.TruncatedBPTT);
