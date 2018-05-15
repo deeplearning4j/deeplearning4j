9f0bcdb0faa51d51d52282540c7214b733a6ed37
Alex Black <blacka101@gmail.com>
2018-05-15 12:33:22 +1000

    Performance module cleanup/fixes

--- dl4j-perf/src/main/java/org/deeplearning4j/perf/listener/DiskInfo.java
+++ dl4j-perf/src/main/java/org/deeplearning4j/perf/listener/DiskInfo.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j.perf.listener;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,7 +8,12 @@ import java.io.Serializable;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class DiskInfo implements Serializable {
     private long bytesRead,bytesWritten,transferTime;
     private String name,modelName;
+
+    private DiskInfo(){
+        //No-arg for JSON/YAML
+    }
 }
