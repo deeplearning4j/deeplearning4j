491e6d86a47b2531237c8189b7a3c2829e1b077d
shugeo <sgazeos@gmail.com>
2018-05-23 22:17:29 +0300

    Added CXX and CXX_PATH env vars

--- libnd4j/tests_cpu/run_tests.sh
+++ libnd4j/tests_cpu/run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+CXX=/usr/bin/g++
+CXX_PATH=`$CXX --print-search-dirs | awk '/install/{print $2;}'`
+export CXX_PATH
+export CXX
 
 make -j4 && layers_tests/runtests --gtest_output="xml:cpu_test_results.xml"
