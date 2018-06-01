afba4a1df94e5ef1925a57446b1480ddf8b490d0
raver119 <raver119@gmail.com>
2018-06-01 14:46:50 -0700

    - reverse/reverse_bp tweaks

--- libnd4j/include/ops/declarable/generic/transforms/reverse.cpp
+++ libnd4j/include/ops/declarable/generic/transforms/reverse.cpp
@@ -12,13 +12,55 @@
 namespace nd4j {
 namespace ops  {
 
+    CONFIGURABLE_OP_IMPL(reverse, 1, 1, true, 0, -2) {
+        auto input = INPUT_VARIABLE(0);
+        std::vector<int> axis;
 
-CONFIGURABLE_OP_IMPL(reverse, 1, 1, true, 0, -2) {
-       
-    helpers::reverse(INPUT_VARIABLE(0), OUTPUT_VARIABLE(0), block.getIArguments());
+        if (block.width() > 1) {
+            // dynamic input
+            axis = INPUT_VARIABLE(1)->template asVectorT<int>();
+        } else if (block.numI() > 0) {
+            axis = *block.getIArguments();
+        }
+
+        for (int e = 0; e < axis.size(); e++)
+            if (axis[e] < 0)
+                axis[e] += input->rankOf();
+
+        helpers::reverse(input, OUTPUT_VARIABLE(0), &axis);
    
-    return Status::OK();
-}
+        return Status::OK();
+    }
+
+    CUSTOM_OP_IMPL(reverse_bp, 2, 1, false, 0, -2) {
+        auto input = INPUT_VARIABLE(0);
+        auto eps = block.width() == 3 ? INPUT_VARIABLE(2) : INPUT_VARIABLE(1);
+        std::vector<int> axis;
+
+        if (block.width() == 3) {
+            // dynamic input
+            axis = INPUT_VARIABLE(1)->template asVectorT<int>();
+        } else if (block.numI() > 0) {
+            axis = *block.getIArguments();
+        }
+
+        for (int e = 0; e < axis.size(); e++)
+            if (axis[e] < 0)
+                axis[e] += input->rankOf();
+
+        // we just reverse back original array
+        helpers::reverse(eps, OUTPUT_VARIABLE(0), &axis);
+
+        return Status::OK();
+    }
+
+    DECLARE_SHAPE_FN(reverse_bp) {
+        auto in = inputShape->at(0);
+        Nd4jLong *out;
+        COPY_SHAPE(in, out);
+
+        return SHAPELIST(out);
+    }
 
 }
 }
