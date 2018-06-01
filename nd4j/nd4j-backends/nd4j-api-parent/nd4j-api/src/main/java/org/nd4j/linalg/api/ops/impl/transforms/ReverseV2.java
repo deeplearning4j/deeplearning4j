cacaa4f1e6df92cfff16159c284d7a899f705be3
raver119 <raver119@gmail.com>
2018-06-01 15:57:26 -0700

    ReverseV2 pulled off

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/ReverseV2.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/ReverseV2.java
@@ -8,20 +8,19 @@ import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import java.util.Arrays;
 import java.util.List;
 
-public class Reverse extends DynamicCustomOp {
+/**
+ * This is compatibility op for ReverseV2
+ */
+public class ReverseV2 extends DynamicCustomOp {
+    protected final boolean isLegacy = true;
 
-    public Reverse(SameDiff sameDiff, SDVariable i_v, int... dimensions) {
-        super(null, sameDiff, new SDVariable[]{i_v}, false);
-        this.dimensions = dimensions;
-        addIArgument(dimensions);
-    }
-
-    public Reverse() {
+    public ReverseV2() {
+        iArguments.add(isLegacy ? 1L : 0L);
     }
 
     @Override
     public String opName() {
-        return "reverse";
+        return "reverse_v2";
     }
 
     @Override
@@ -31,13 +30,12 @@ public class Reverse extends DynamicCustomOp {
 
     @Override
     public String tensorflowName() {
-        return "Reverse";
+        return "ReverseV2";
     }
 
     @Override
     public List<SDVariable> doDiff(List<SDVariable> f1) {
-        SDVariable ret = f().reverse(f1.get(0), dimensions);
-        return Arrays.asList(ret);
+        throw new UnsupportedOperationException("BP mode isn't supported for this op");
     }
 
 }
