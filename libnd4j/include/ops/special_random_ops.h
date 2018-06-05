6fb8495d5693b7eeaa29d162d293c07da4bed163
raver119 <raver119@gmail.com>
2018-06-05 12:30:34 -0700

    early term for tests

--- libnd4j/include/ops/special_random_ops.h
+++ libnd4j/include/ops/special_random_ops.h
@@ -743,7 +743,7 @@ namespace randomOps {
                     generation0 += zLength;
 
                     // we only allow 10 attempts on this
-                } while (blocker++ < 10 && (ds < aRealMean0 + nd4j::math::nd4j_abs<T>(result0) || aRealMean1 + nd4j::math::nd4j_abs<T>(result1) > ds));
+                } while (++blocker < 2 && (ds < aRealMean0 + nd4j::math::nd4j_abs<T>(result0) || aRealMean1 + nd4j::math::nd4j_abs<T>(result1) > ds));
 
                 z[e * zEWS] = result0;
                 if((epm) < zLength)
