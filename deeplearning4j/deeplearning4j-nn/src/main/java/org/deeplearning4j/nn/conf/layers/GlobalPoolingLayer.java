5d7b6798d68ffde3b16974cdf972e0b661fb0160
maxpumperla <max.pumperla@googlemail.com>
2018-06-07 16:42:14 +0200

    java docs

--- deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/GlobalPoolingLayer.java
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/GlobalPoolingLayer.java
@@ -20,20 +20,28 @@ import java.util.Map;
 /**
  * Global pooling layer - used to do pooling over time for RNNs, and 2d pooling for CNNs.<br>
  * Supports the following {@link PoolingType}s: SUM, AVG, MAX, PNORM<br>
+ *
  * Global pooling layer can also handle mask arrays when dealing with variable length inputs. Mask arrays are assumed
  * to be 2d, and are fed forward through the network during training or post-training forward pass:<br>
- * - Time series: mask arrays are shape [minibatchSize, maxTimeSeriesLength] and contain values 0 or 1 only<br>
- * - CNNs: mask have shape [minibatchSize, height] or [minibatchSize, width]. Important: the current implementation assumes
- *   that for CNNs + variable length (masking), the input shape is [minibatchSize, channels, height, 1] or
- *   [minibatchSize, channels, 1, width] respectively. This is the case with global pooling in architectures like CNN for
+ * - Time series: mask arrays are shape [miniBatchSize, maxTimeSeriesLength] and contain values 0 or 1 only<br>
+ * - CNNs: mask have shape [miniBatchSize, height] or [miniBatchSize, width]. Important: the current implementation assumes
+ *   that for CNNs + variable length (masking), the input shape is [miniBatchSize, channels, height, 1] or
+ *   [miniBatchSize, channels, 1, width] respectively. This is the case with global pooling in architectures like CNN for
  *   sentence classification.<br>
  * <p>
+ *
  * Behaviour with default settings:<br>
- * - 3d (time series) input with shape [minibatchSize, vectorSize, timeSeriesLength] -> 2d output [minibatchSize, vectorSize]<br>
- * - 4d (CNN) input with shape [minibatchSize, channels, height, width] -> 2d output [minibatchSize, channels]<br>
+ * - 3d (time series) input with shape [miniBatchSize, vectorSize, timeSeriesLength] -> 2d output [miniBatchSize, vectorSize]<br>
+ * - 4d (CNN) input with shape [miniBatchSize, channels, height, width] -> 2d output [miniBatchSize, channels]<br>
+ * - 5d (CNN3D) input with shape [miniBatchSize, channels, depth, height, width] -> 2d output [miniBatchSize, channels]<br>
+ *
  * <p>
  * Alternatively, by setting collapseDimensions = false in the configuration, it is possible to retain the reduced dimensions
- * as 1s: this gives [minibatchSize, vectorSize, 1] for RNN output, and [minibatchSize, channels, 1, 1] for CNN output.<br>
+ * as 1s: this gives
+ * - [miniBatchSize, vectorSize, 1] for RNN output,
+ * - [miniBatchSize, channels, 1, 1] for CNN output, and
+ * - [miniBatchSize, channels, 1, 1, 1] for CNN3D output.
+ * <br>
  *
  * @author Alex Black
  */
@@ -227,12 +235,15 @@ public class GlobalPoolingLayer extends Layer {
         /**
          * Whether to collapse dimensions when pooling or not. Usually you *do* want to do this. Default: true.
          * If true:<br>
-         * - 3d (time series) input with shape [minibatchSize, vectorSize, timeSeriesLength] -> 2d output [minibatchSize, vectorSize]<br>
-         * - 4d (CNN) input with shape [minibatchSize, channels, height, width] -> 2d output [minibatchSize, channels]<br>
+         * - 3d (time series) input with shape [miniBatchSize, vectorSize, timeSeriesLength] -> 2d output [miniBatchSize, vectorSize]<br>
+         * - 4d (CNN) input with shape [miniBatchSize, channels, height, width] -> 2d output [miniBatchSize, channels]<br>
+         * - 5d (CNN3D) input with shape [miniBatchSize, channels, depth, height, width] -> 2d output [miniBatchSize, channels]<br>
+
          *
          * If false:<br>
-         * - 3d (time series) input with shape [minibatchSize, vectorSize, timeSeriesLength] -> 3d output [minibatchSize, vectorSize, 1]<br>
-         * - 4d (CNN) input with shape [minibatchSize, channels, height, width] -> 2d output [minibatchSize, channels, 1, 1]<br>
+         * - 3d (time series) input with shape [miniBatchSize, vectorSize, timeSeriesLength] -> 3d output [miniBatchSize, vectorSize, 1]<br>
+         * - 4d (CNN) input with shape [miniBatchSize, channels, height, width] -> 2d output [miniBatchSize, channels, 1, 1]<br>
+         * - 5d (CNN3D) input with shape [miniBatchSize, channels, depth, height, width] -> 2d output [miniBatchSize, channels, 1, 1, 1]<br>
          *
          * @param collapseDimensions Whether to collapse the dimensions or not
          */
