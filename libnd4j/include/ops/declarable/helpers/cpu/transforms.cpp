f9ab27d44bc6f42a0b88511ac993235737aed769
Yurii <yurii@skymind.io>
2018-06-10 02:08:54 +0300

    Shyrma mirror pad (#5547)
    * implementation of mirrorPad op
    
    * add shapes check for edge cases in mirrorPad op
    
    * testing edge cases in mirrorPad op

--- libnd4j/include/ops/declarable/helpers/cpu/transforms.cpp
+++ libnd4j/include/ops/declarable/helpers/cpu/transforms.cpp
@@ -681,6 +681,66 @@ void clipByAveraged(NDArray<T>& input, NDArray<T>& output, const std::vector<int
     }
 }
 
+
+//////////////////////////////////////////////////////////////////////////
+template<typename T>
+void mirrorPad(const NDArray<T>& input, const NDArray<T>& paddings, NDArray<T>& output, const int mode) {
+    
+    // mode:  0 - REFLECT, else - SYMMETRIC
+    const int reflBorder = (bool)mode ? 1 : 0;
+    const int symmBorder = (bool)mode ? 0 : 1;
+
+    const int rank        = input.rankOf();
+    const Nd4jLong outLen = output.lengthOf();
+    const Nd4jLong inLen  = input.lengthOf();    
+
+    if(rank <= 1) {
+
+        const int leftSide  = static_cast<int>(paddings(static_cast<Nd4jLong>(0)));
+        const int rightSide = static_cast<int>(paddings(static_cast<Nd4jLong>(1)));
+
+#pragma omp parallel for if(outLen > Environment::getInstance()->elementwiseThreshold()) schedule(guided)
+        for(int i = 0; i < outLen; ++i) {
+            
+            for(int j = 0; j < leftSide; ++j)
+                output(j) = input(inLen - leftSide + symmBorder - j);
+            for(int j = 0; j < inLen; ++j)
+                output(j + leftSide) = input(j);
+            for(int j = 0; j < rightSide; ++j)
+                output(leftSide + inLen + j) = input(inLen - 1 - symmBorder - j);
+        }  
+    }
+    else {
+
+        std::vector<Nd4jLong> inIdx(rank), outIdx(rank);
+#pragma omp parallel for if(outLen > Environment::getInstance()->elementwiseThreshold()) schedule(guided) firstprivate(inIdx, outIdx)
+        for(int i = 0; i < outLen; ++i) {
+
+            shape::ind2subC(rank, output.shapeOf(), i, outIdx.data());
+
+            for(int j = 0; j < rank; ++j) {
+            
+                const int leftSide  = static_cast<int>(paddings(j, 0));
+
+                if(outIdx[j] < leftSide) 
+                    inIdx[j] = leftSide - outIdx[j] - reflBorder;
+
+                else if(outIdx[j] >= leftSide && outIdx[j] < leftSide + input.sizeAt(j)) 
+                    inIdx[j] = outIdx[j] - leftSide;
+
+                else
+                    inIdx[j] = 2 * input.sizeAt(j) + leftSide - outIdx[j] - 1 - symmBorder;                
+            }
+    
+            Nd4jLong outOffset = shape::getOffset(0, output.shapeOf(), output.stridesOf(), outIdx.data(), rank);
+            Nd4jLong inOffset  = shape::getOffset(0, input.shapeOf(),  input.stridesOf(),  inIdx.data(),  rank);
+            output.buffer()[outOffset] = input.getBuffer()[inOffset];
+        }
+    }
+}
+
+
+
 template void triu<float>(const NDArray<float>& input, NDArray<float>& output, const int diagonal);
 template void triu<float16>(const NDArray<float16>& input, NDArray<float16>& output, const int diagonal);
 template void triu<double>(const NDArray<double>& input, NDArray<double>& output, const int diagonal);
@@ -744,6 +804,11 @@ template void clipByNorm<double>(NDArray<double>& input, NDArray<double>& output
 template void clipByAveraged<float>(NDArray<float>& input, NDArray<float>& output, const std::vector<int>& dimensions, const float clipNorm, const bool isInplace);
 template void clipByAveraged<float16>(NDArray<float16>& input, NDArray<float16>& output, const std::vector<int>& dimensions, const float16 clipNorm, const bool isInplace);
 template void clipByAveraged<double>(NDArray<double>& input, NDArray<double>& output, const std::vector<int>& dimensions, const double clipNorm, const bool isInplace);
+
+template void mirrorPad<float>(const NDArray<float>& input, const NDArray<float>& paddings, NDArray<float>& output, const int mode);
+template void mirrorPad<float16>(const NDArray<float16>& input, const NDArray<float16>& paddings, NDArray<float16>& output, const int mode);
+template void mirrorPad<double>(const NDArray<double>& input, const NDArray<double>& paddings, NDArray<double>& output, const int mode);
+
 
 }
 }
