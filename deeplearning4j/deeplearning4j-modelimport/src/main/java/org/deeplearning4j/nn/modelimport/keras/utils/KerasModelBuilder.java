4f373cbaf4db7d5eb3fb7155a1238173d204d842
maxpumperla <max.pumperla@googlemail.com>
2018-06-14 10:30:49 +0200

    doc strings and clean-up for model builder

--- deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/utils/KerasModelBuilder.java
+++ deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/utils/KerasModelBuilder.java
@@ -23,6 +23,7 @@ public class KerasModelBuilder implements Cloneable, Closeable {
     protected String modelJson = null;
     protected String modelYaml = null;
     protected String trainingJson = null;
+    protected String trainingYaml = null;
     protected Hdf5Archive weightsArchive = null;
     protected String weightsRoot = null;
     protected Hdf5Archive trainingArchive = null;
@@ -31,20 +32,68 @@ public class KerasModelBuilder implements Cloneable, Closeable {
     protected int[] inputShape = null;
 
 
+    /**
+     * KerasModelBuilder constructed from a model configuration.
+     *
+     * @param config KerasModelConfiguration
+     */
     public KerasModelBuilder(KerasModelConfiguration config) {
         this.config = config;
     }
 
+    /**
+     * Set model architecture from model JSON string.
+     *
+     * @param modelJson model as JSON string.
+     * @return Model Builder
+     */
     public KerasModelBuilder modelJson(String modelJson) {
         this.modelJson = modelJson;
         return this;
     }
 
+    /**
+     * Set model architecture from model YAML string.
+     *
+     * @param modelYaml model as YAML string.
+     * @return Model Builder
+     */
+    public KerasModelBuilder modelYaml(String modelYaml) {
+        this.modelYaml = modelYaml;
+        return this;
+    }
+
+    /**
+     * Set model architecture from file name pointing to model JSON string.
+     *
+     * @param modelJsonFilename Name of file containing model JSON string
+     * @return Model Builder
+     * @throws IOException I/O Exception
+     */
     public KerasModelBuilder modelJsonFilename(String modelJsonFilename) throws IOException {
         this.modelJson = new String(Files.readAllBytes(Paths.get(modelJsonFilename)));
         return this;
     }
 
+    /**
+     * Set model architecture from file name pointing to model YAML string.
+     *
+     * @param modelYamlFilename Name of file containing model YAML string
+     * @return Model Builder
+     * @throws IOException I/O Exception
+     */
+    public KerasModelBuilder modelYamlFilename(String modelYamlFilename) throws IOException {
+        this.modelJson = new String(Files.readAllBytes(Paths.get(modelYamlFilename)));
+        return this;
+    }
+
+    /**
+     * Set model architecture from input stream of model JSON.
+     *
+     * @param modelJsonInputStream Input stream of model JSON
+     * @return Model builder
+     * @throws IOException I/O exception
+     */
     public KerasModelBuilder modelJsonInputStream(InputStream modelJsonInputStream) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         IOUtils.copy(modelJsonInputStream, byteArrayOutputStream);
@@ -52,21 +101,13 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return this;
     }
 
-    public KerasModelBuilder inputShape(int[] inputShape) {
-        this.inputShape = inputShape;
-        return this;
-    }
-
-    public KerasModelBuilder modelYaml(String modelYaml) {
-        this.modelYaml = modelYaml;
-        return this;
-    }
-
-    public KerasModelBuilder modelYamlFilename(String modelYamlFilename) throws IOException {
-        this.modelJson = new String(Files.readAllBytes(Paths.get(modelYamlFilename)));
-        return this;
-    }
-
+    /**
+     * Set model architecture from input stream of model YAML.
+     *
+     * @param modelYamlInputStream Input stream of model YAML
+     * @return Model builder
+     * @throws IOException I/O exception
+     */
     public KerasModelBuilder modelYamlInputStream(InputStream modelYamlInputStream) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         IOUtils.copy(modelYamlInputStream, byteArrayOutputStream);
@@ -74,11 +115,47 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return this;
     }
 
+    /**
+     * Provide input shape for Keras models that have been compiled without one. DL4J
+     * needs this shape information on import to infer shapes of later layers and do
+     * shape validation.
+     *
+     * @param inputShape Input shape as int array
+     * @return Model Builder
+     */
+    public KerasModelBuilder inputShape(int[] inputShape) {
+        this.inputShape = inputShape;
+        return this;
+    }
+
+    /**
+     * Provide training configuration as JSON string
+     *
+     * @param trainingJson Training JSON string
+     * @return Model builder
+     */
     public KerasModelBuilder trainingJson(String trainingJson) {
         this.trainingJson = trainingJson;
         return this;
     }
 
+    /**
+     * Provide training configuration as YAML string
+     *
+     * @param trainingYaml Training YAML string
+     * @return Model builder
+     */
+    public KerasModelBuilder trainingYaml(String trainingYaml) {
+        this.trainingYaml = trainingYaml;
+        return this;
+    }
+
+    /**
+     * Provide training configuration as file input stream from JSON
+     *
+     * @param trainingJsonInputStream Input stream of training JSON string
+     * @return Model builder
+     */
     public KerasModelBuilder trainingJsonInputStream(InputStream trainingJsonInputStream) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         IOUtils.copy(trainingJsonInputStream, byteArrayOutputStream);
@@ -86,6 +163,28 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return this;
     }
 
+    /**
+     * Provide training configuration as file input stream from YAML
+     *
+     * @param trainingYamlInputStream Input stream of training YAML string
+     * @return Model builder
+     */
+    public KerasModelBuilder trainingYamlInputStream(InputStream trainingYamlInputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        IOUtils.copy(trainingYamlInputStream, byteArrayOutputStream);
+        this.trainingYaml = new String(byteArrayOutputStream.toByteArray());
+        return this;
+    }
+
+    /**
+     * Set full model HDF5 (architecture, weights and training configuration) by providing the HDF5 filename.
+     *
+     * @param modelHdf5Filename File name of HDF5 file containing full model
+     * @return Model builder
+     * @throws UnsupportedKerasConfigurationException Unsupported configuration
+     * @throws InvalidKerasConfigurationException Invalid configuration
+     * @throws IOException I/O exception
+     */
     public KerasModelBuilder modelHdf5Filename(String modelHdf5Filename)
             throws UnsupportedKerasConfigurationException, InvalidKerasConfigurationException, IOException {
         try {
@@ -118,23 +217,51 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return this;
     }
 
+    /**
+     * Set weights of the model by providing the file name of the corresponding weights HDF5 file.
+     * The root of the HDF5 group containing weights won't be set by this method.
+     *
+     * @param weightsHdf5Filename File name of weights HDF5
+     * @return Model builder
+     */
     public KerasModelBuilder weightsHdf5FilenameNoRoot(String weightsHdf5Filename) {
         this.weightsArchive = new Hdf5Archive(weightsHdf5Filename);
         return this;
     }
 
+    /**
+     * Set weights of the model by providing the file name of the corresponding weights HDF5 file.
+     * The root of the HDF5 group containing weights will be read and set from the configuration of this
+     * model builder instance.
+     *
+     * @param weightsHdf5Filename File name of weights HDF5
+     * @return Model builder
+     */
     public KerasModelBuilder weightsHdf5Filename(String weightsHdf5Filename) {
         this.weightsArchive = new Hdf5Archive(weightsHdf5Filename);
         this.weightsRoot = config.getTrainingWeightsRoot();
         return this;
     }
 
+    /**
+     * Determine whether to enforce loading a training configuration or not.
+     *
+     * @param enforceTrainingConfig boolean, read training config or not
+     * @return Model builder
+     */
     public KerasModelBuilder enforceTrainingConfig(boolean enforceTrainingConfig) {
         this.enforceTrainingConfig = enforceTrainingConfig;
         return this;
     }
 
-
+    /**
+     * Build a KerasModel (corresponding to ComputationGraph) from this model builder.
+     *
+     * @return KerasModel
+     * @throws IOException I/O exception
+     * @throws InvalidKerasConfigurationException Invalid configuration
+     * @throws UnsupportedKerasConfigurationException Unsupported configuration
+     */
     public KerasModel buildModel()
             throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         KerasModel model = new KerasModel(this);
@@ -142,6 +269,14 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return model;
     }
 
+    /**
+     * Build a KerasSequentialModel (corresponding to MultiLayerNetwork) from this model builder.
+     *
+     * @return KerasSequentialModel
+     * @throws IOException I/O exception
+     * @throws InvalidKerasConfigurationException Invalid configuration
+     * @throws UnsupportedKerasConfigurationException Unsupported configuration
+     */
     public KerasSequentialModel buildSequential()
             throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         KerasSequentialModel sequentialModel = new KerasSequentialModel(this);
@@ -149,6 +284,9 @@ public class KerasModelBuilder implements Cloneable, Closeable {
         return sequentialModel;
     }
 
+    /**
+     * Close all HDF5 archives for this model builder.
+     */
     @Override
     public void close() {
         if (trainingArchive != null && trainingArchive != weightsArchive) {
