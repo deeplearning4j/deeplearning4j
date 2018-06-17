f09148cb629d57b0e0487c8399f38b0f18a4a7f4
farizrahman4u <farizrahman4u@gmail.com>
2018-06-17 15:33:22 +0530

    typo

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TensorFlowImportTest.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TensorFlowImportTest.java
@@ -962,7 +962,7 @@ public class TensorFlowImportTest extends BaseNd4jTest {
     public void testTensorArray_119_1() throws Exception {
         Nd4j.create(1);
 
-        val tg = TFGraphMapper.getInstance().importGraph(new ClassPathResource("tf_graphs/tensor_array.txt").getInputStream());
+        val tg = TFGraphMapper.getInstance().importGraph(new ClassPathResource("tf_graphs/tensor_array.pb.txt").getInputStream());
         assertNotNull(tg);
 
         val input_matrix = Nd4j.ones(3, 2);
