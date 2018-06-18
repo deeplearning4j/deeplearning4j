ca969a12be6a02e921e052661ac0650848990158
raver119 <raver119@gmail.com>
2018-06-18 15:34:31 -0700

    few passing tests

--- libnd4j/tests_cpu/layers_tests/EmptyTests.cpp
+++ libnd4j/tests_cpu/layers_tests/EmptyTests.cpp
@@ -31,19 +31,48 @@ TEST_F(EmptyTests, Test_Create_Empty) {
 
 TEST_F(EmptyTests, Test_Concat_1) {
     auto empty = NDArray<float>::createEmpty();
-    auto scalar = new NDArray<float>(1.0f);
+    auto vector = new NDArray<float>('c', {1}, {1.0f});
+
+    ASSERT_TRUE(empty->isEmpty());
 
     nd4j::ops::concat<float> op;
-    auto result = op.execute({empty, scalar}, {}, {0});
+    auto result = op.execute({empty, vector}, {}, {0});
     ASSERT_EQ(Status::OK(), result->status());
 
     auto z = result->at(0);
 
-    ASSERT_TRUE(scalar->isSameShape(z));
-    ASSERT_EQ(*scalar, *z);
+//    z->printShapeInfo("z shape");
+//    z->printIndexedBuffer("z buffr");
+
+    ASSERT_EQ(*vector, *z);
 
     delete empty;
-    delete scalar;
+    delete vector;
     delete result;
 }
 
+
+TEST_F(EmptyTests, Test_Concat_2) {
+    auto empty = NDArray<float>::createEmpty();
+    auto scalar1 = new NDArray<float>(1.0f);
+    auto scalar2 = new NDArray<float>(2.0f);
+    NDArray<float> exp('c', {2}, {1.f, 2.f});
+
+    ASSERT_TRUE(empty->isEmpty());
+
+    nd4j::ops::concat<float> op;
+    auto result = op.execute({empty, scalar1, scalar2}, {}, {0});
+    ASSERT_EQ(Status::OK(), result->status());
+
+    auto z = result->at(0);
+
+//    z->printShapeInfo("z shape");
+//    z->printIndexedBuffer("z buffr");
+
+    ASSERT_EQ(exp, *z);
+
+    delete empty;
+    delete scalar1;
+    delete scalar2;
+    delete result;
+}
