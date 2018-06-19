01edcc28faa84ce94566494d92039b313c0916eb
maxpumperla <max.pumperla@googlemail.com>
2018-06-19 10:43:31 +0200

    repeat vector impl

---
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/RepeatVector.java
@@ -0,0 +1,177 @@
+package org.deeplearning4j.nn.layers;
+
+
+import org.deeplearning4j.exception.DL4JInvalidInputException;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.conf.CacheMode;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.gradient.DefaultGradient;
+import org.deeplearning4j.nn.gradient.Gradient;
+import org.deeplearning4j.nn.layers.convolution.upsampling.Upsampling2D;
+import org.deeplearning4j.nn.workspace.ArrayType;
+import org.deeplearning4j.nn.workspace.LayerWorkspaceMgr;
+import org.nd4j.linalg.api.memory.MemoryWorkspace;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.CustomOp;
+import org.nd4j.linalg.api.ops.DynamicCustomOp;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.primitives.Pair;
+
+import java.util.Arrays;
+
+/**
+ * RepeatVector layer.
+ *
+ * RepeatVector takes a mini-batch of vectors of shape (mb, length) and a repeat factor n and outputs
+ * a 3D tensor of shape (mb, n, length) in which x is repeated n times.
+ *
+ * @author Max Pumperla
+ */
+public class RepeatVector extends AbstractLayer<org.deeplearning4j.nn.conf.layers.misc.RepeatVector> {
+
+    public RepeatVector(NeuralNetConfiguration conf) {
+        super(conf);
+    }
+
+    public RepeatVector(NeuralNetConfiguration conf, INDArray input) {
+        super(conf, input);
+    }
+
+    @Override
+    public double calcL2(boolean backpropParamsOnly) {
+        return 0;
+    }
+
+    @Override
+    public double calcL1(boolean backpropParamsOnly) {
+        return 0;
+    }
+
+    @Override
+    public Type type() {
+        return Type.UPSAMPLING;
+    }
+
+
+    @Override
+    public Pair<Gradient, INDArray> backpropGradient(INDArray epsilon, LayerWorkspaceMgr workspaceMgr) {
+        assertInputSet(true);
+
+        INDArray outEpsilon = Nd4j.sum(epsilon,2);
+
+        Gradient gradient = new DefaultGradient();
+        return new Pair<>(gradient, outEpsilon);
+    }
+
+    protected int getN(){
+        return layerConf().getN();
+    }
+
+    protected INDArray preOutput(boolean training, boolean forBackprop, LayerWorkspaceMgr workspaceMgr) {
+        assertInputSet(false);
+        applyDropOutIfNecessary(training, workspaceMgr);
+
+        if (input.rank() != 2) {
+            throw new DL4JInvalidInputException("Got rank " + input.rank()
+                    + " array as input to RepeatVector with shape " + Arrays.toString(input.shape())
+                    + ". Expected rank 2 array with shape [minibatchSize, size]. "
+                    + layerId());
+        }
+
+        if (preOutput != null && forBackprop) {
+            return preOutput;
+        }
+
+        return input.repeat(1, (long) getN());
+    }
+
+    @Override
+    public INDArray activate(boolean training, LayerWorkspaceMgr workspaceMgr) {
+        assertInputSet(false);
+
+        if (cacheMode == null)
+            cacheMode = CacheMode.NONE;
+
+        INDArray z = preOutput(training, false, workspaceMgr);
+
+        if (training && cacheMode != CacheMode.NONE && workspaceMgr.hasConfiguration(ArrayType.FF_CACHE)
+                && workspaceMgr.isWorkspaceOpen(ArrayType.FF_CACHE)) {
+            try (MemoryWorkspace wsB = workspaceMgr.notifyScopeBorrowed(ArrayType.FF_CACHE)) {
+                preOutput = z.unsafeDuplication();
+            }
+        }
+        return z;
+    }
+
+    @Override
+    public Layer transpose() {
+        throw new UnsupportedOperationException(layerId());
+    }
+
+    @Override
+    public Layer clone() {
+        return new RepeatVector(conf.clone());
+    }
+
+    @Override
+    public boolean isPretrainLayer() {
+        return false;
+    }
+
+    @Override
+    public void clearNoiseWeightParams() {
+        //No op
+    }
+
+    @Override
+    public Gradient gradient() {
+        throw new UnsupportedOperationException("Not supported - no parameters");
+    }
+
+    @Override
+    public void fit() {
+
+    }
+
+    @Override
+    public int numParams() {
+        return 0;
+    }
+
+    @Override
+    public void fit(INDArray input, LayerWorkspaceMgr workspaceMgr) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    @Override
+    public double score() {
+        return 0;
+    }
+
+    @Override
+    public void accumulateScore(double accum) {
+        throw new UnsupportedOperationException(layerId());
+    }
+
+
+    @Override
+    public void update(INDArray gradient, String paramType) {
+
+    }
+
+    @Override
+    public INDArray params() {
+        return null;
+    }
+
+    @Override
+    public INDArray getParam(String param) {
+        return params();
+    }
+
+    @Override
+    public void setParams(INDArray params) {
+
+    }
+
+}
