594e6b2634b4a67f24eac8ae89b671c05bbc9a1d
Alex Black <blacka101@gmail.com>
2018-06-19 11:00:51 +1000

    Fix test compilation issue

--- deeplearning4j/deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/samediff/testlayers/SameDiffConv.java
+++ deeplearning4j/deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/samediff/testlayers/SameDiffConv.java
@@ -121,10 +121,10 @@ public class SameDiffConv extends BaseSameDiffLayer {
         }
 
         Conv2DConfig c = Conv2DConfig.builder()
-                .kh(kernel[0]).kw(kernel[1])
-                .ph(padding[0]).pw(padding[1])
-                .sy(stride[0]).sx(stride[1])
-                .dh(dilation[0]).dw(dilation[1])
+                .kH(kernel[0]).kW(kernel[1])
+                .pH(padding[0]).pW(padding[1])
+                .sH(stride[0]).sW(stride[1])
+                .dH(dilation[0]).dW(dilation[1])
                 .isSameMode(this.cm == ConvolutionMode.Same)
                 .build();
 
