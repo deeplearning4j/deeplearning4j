2e11c7effd6cd63ec246476f2c0500afa95abe69
maxpumperla <max.pumperla@googlemail.com>
2018-06-21 09:47:58 +0200

    add scalnet to docs build

--- docs/gen_all_docs.sh
+++ docs/gen_all_docs.sh
@@ -5,3 +5,7 @@ python generate_docs.py \
     --project keras-import \
     --code ../deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/
 
+python generate_docs.py \
+    --project scalnet \
+    --language scala \
+    --code ../scalnet/src/main/scala/org/deeplearning4j/scalnet/
