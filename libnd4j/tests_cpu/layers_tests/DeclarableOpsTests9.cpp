4b4d7769d3304f3c8c16081c40995eb3ccf477f6
Yurii <yurii@skymind.io>
2018-06-22 12:50:09 +0300

    add one more file for tests DeclarableOpsTests9.cpp

---
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests9.cpp
@@ -0,0 +1,21 @@
+//
+// @author Yurii Shyrma (iuriish@yahoo.com), created on 22.06.2018
+//
+
+
+#include "testlayers.h"
+#include <ops/declarable/CustomOperations.h>
+#include <NDArray.h>
+
+using namespace nd4j;
+
+
+class DeclarableOpsTests9 : public testing::Test {
+public:
+
+    DeclarableOpsTests8() {
+        printf("\n");
+        fflush(stdout);
+    }
+};
+
