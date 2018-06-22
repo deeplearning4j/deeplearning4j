85b2f10ca670539763fa6ed37b31479f5a5796f8
Alex Black <blacka101@gmail.com>
2018-06-22 11:33:21 +1000

    Revert "Slice : validate input args" (#5679)
    * Revert "Reshape : Use negative ints for order (#5635)"
    
    This reverts commit 3d8007b5f0b064fb5776c10d3054570f36d5694c.
    
    * Revert "Slice : validate input args (#5675)"
    
    This reverts commit cedfa5889fb33b5304776ebfbdee4c9b1ae7cceb.

--- libnd4j/include/ops/declarable/generic/parity_ops/slice.cpp
+++ libnd4j/include/ops/declarable/generic/parity_ops/slice.cpp
@@ -37,19 +37,13 @@ namespace nd4j {
 
             IndicesList indices;
             for (int e = 0; e < x_rank; e++) {
-                int size = end[e];
+                int stop = end[e];
                 int start = begin[e];
 
-                REQUIRE_TRUE(start >= 0, 0, "Slice: start index should not be negative");
 
-                REQUIRE_TRUE(start < x_rank, 0, "Index %i is invalid for dimension %i with size %i.", start, e, input->shapeInfo()[e + 1]);
-                if (size < 0){
-                    size = input->shapeInfo()[e + 1] - start;
-                }
-                REQUIRE_TRUE(size > 0, 0, "Slice: interval for dimension %i is less then 1");
-                REQUIRE_TRUE(start + size <= x_rank, 0, "Slice: interval [%i, %i] is out of bounds for dimension %i with size %i", start, start + size, e, input->shapeInfo()[e + 1]);
+                REQUIRE_TRUE(stop > 0, 0, "Slice: interval for dimension %i is less then 1", e);
 
-                indices.push_back(NDIndex::interval(start, start + size, 1));
+                indices.push_back(NDIndex::interval(start, start+stop, 1));
             }
             auto sub = input->subarray(indices);
             output->assign(sub);
