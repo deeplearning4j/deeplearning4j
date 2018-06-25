cd0bc6c66b3b60c7c742d2193fa1cf6f8d3a59c0
maxpumperla <max.pumperla@googlemail.com>
2018-06-25 12:53:38 +0200

    gen indices, clean up

--- docs/gen_all_docs.sh
+++ docs/gen_all_docs.sh
@@ -3,9 +3,12 @@
 # Generate Keras docs
 python generate_docs.py \
     --project keras-import \
-    --code ../deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/
+    --code ../deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/ \
+    --docs_root deeplarning4j.org/keras
 
+# Generate ScalNet docs
 python generate_docs.py \
     --project scalnet \
     --language scala \
-    --code ../scalnet/src/main/scala/org/deeplearning4j/scalnet/
+    --code ../scalnet/src/main/scala/org/deeplearning4j/scalnet/ \
+    --docs_root deeplarning4j.org/scalnet
