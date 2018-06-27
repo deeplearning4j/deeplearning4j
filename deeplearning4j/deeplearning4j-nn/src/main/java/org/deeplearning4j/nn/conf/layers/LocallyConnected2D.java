c746c526ea73f745b09ff0f536d05e1284766ec8
maxpumperla <max.pumperla@googlemail.com>
2018-06-27 14:58:17 +0200

    finish layer conf

--- deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/LocallyConnected2D.java
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/layers/LocallyConnected2D.java
@@ -6,19 +6,25 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.api.ParamInitializer;
+import org.deeplearning4j.nn.conf.ConvolutionMode;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.memory.LayerMemoryReport;
 import org.deeplearning4j.nn.params.ConvolutionParamInitializer;
+import org.deeplearning4j.nn.params.LocallyConnected2DParamInitializer;
 import org.deeplearning4j.optimize.api.TrainingListener;
 import org.deeplearning4j.util.ConvolutionUtils;
+import org.nd4j.base.Preconditions;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.Collection;
 import java.util.Map;
 
 /**
- * A 2D locally connected layer computes a convolution with unshared weights.
+ * A 2D locally connected layer computes a convolution with unshared weights. In a regular
+ * convolution operation for each input filter there is one kernel that moves over patches
+ * of the filter. In a locally connected layer, there is a separate kernel for each patch.
  *
  * @author Max Pumperla
  */
@@ -28,12 +34,24 @@ import java.util.Map;
 @EqualsAndHashCode(callSuper = true)
 public class LocallyConnected2D extends ConvolutionLayer {
 
-    // TODO: we need to set inputSize manually here (can only be inferred at runtime),
-    // TODO: i.e. specify (input height, input width) for param init
+    int[] inputSize;
+    int[] outputSize;
 
     private LocallyConnected2D(Builder builder) {
         super(builder);
         initializeConstraints(builder);
+        this.inputSize = builder.inputSize;
+        computeOutputSize();
+    }
+
+    private void computeOutputSize() {
+        int nIn = (int) getNIn();
+
+        int[] inputShape = new int[] {1, nIn, inputSize[0], inputSize[1]};
+        INDArray dummyInputForShapeInference = Nd4j.ones(inputShape);
+
+        this.outputSize = ConvolutionUtils.getOutputSize(
+                dummyInputForShapeInference, kernelSize, stride, padding, convolutionMode, dilation);
     }
 
     @Override
@@ -55,7 +73,7 @@ public class LocallyConnected2D extends ConvolutionLayer {
 
     @Override
     public ParamInitializer initializer() {
-        return ConvolutionParamInitializer.getInstance();
+        return LocallyConnected2DParamInitializer.getInstance();
     }
 
     @Override
@@ -91,6 +109,8 @@ public class LocallyConnected2D extends ConvolutionLayer {
          * @return Builder
          */
         public Builder setInputSize(int[] inputSize){
+            Preconditions.checkState(inputSize.length == 2, "Input size argument of a locally connected" +
+                    "layer has to have length 2, got " + inputSize.length);
             this.inputSize = inputSize;
             return this;
         }
@@ -100,7 +120,6 @@ public class LocallyConnected2D extends ConvolutionLayer {
         public LocallyConnected2D build() {
             ConvolutionUtils.validateConvolutionModePadding(convolutionMode, padding);
             ConvolutionUtils.validateCnnKernelStridePadding(kernelSize, stride, padding);
-
             return new LocallyConnected2D(this);
         }
     }
