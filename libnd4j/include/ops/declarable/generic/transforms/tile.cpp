18c3bb16e9bda195bb64ec24808f4710ecb07211
Yurii <yurii@skymind.io>
2018-06-28 18:33:36 +0300

    refine tile op, add strict requirement on length of int args input array

--- libnd4j/include/ops/declarable/generic/transforms/tile.cpp
+++ libnd4j/include/ops/declarable/generic/transforms/tile.cpp
@@ -1,5 +1,6 @@
 //
-//  @author raver119@gmail.com
+// @author raver119@gmail.com
+// @author Yurii Shyrma (iuriish@yahoo.com)
 //
 
 #include <op_boilerplate.h>
@@ -8,85 +9,73 @@
 #include <ops/declarable/CustomOperations.h>
 
 namespace nd4j {
-    namespace ops {
-        CUSTOM_OP_IMPL(tile, 1, 1, false, 0, -2) {
-            auto input = INPUT_VARIABLE(0);
-            NDArray<T>* output;
-            std::vector<Nd4jLong> reps(input->rankOf());
-            bool overwrite = false;
+namespace ops {
 
-            if (block.getIArguments()->size() >= input->rankOf()) {
-                reps = ArrayUtils::toLongVector(*(block.getIArguments()));
+CUSTOM_OP_IMPL(tile, 1, 1, false, 0, -2) {
+    
+    NDArray<T>* input  = INPUT_VARIABLE(0);
+    NDArray<T>* output = OUTPUT_VARIABLE(0);    
+    
+    const int inRank = input->rankOf();
+    std::vector<Nd4jLong> reps;
 
-                output = OUTPUT_VARIABLE(0);
-            } else if (block.width() > 1) {
-                auto reps_vector = INPUT_VARIABLE(1);
-                REQUIRE_TRUE(reps_vector->lengthOf() == input->rankOf(), 0, "TILE op: repeats vector length should be equal to input rank, but got %i and %i correspondingly !", reps_vector->lengthOf(), input->rankOf());
+    if (block.getIArguments()->size() == inRank) {
 
-                for (int e = 0; e < input->rankOf(); e++)
-                    reps[e] = (int) (*reps_vector)(e);
+        reps = ArrayUtils::toLongVector(*(block.getIArguments()));        
+    } 
+    else if (block.width() > 1)  {
+        
+        NDArray<T>* reps_vector = INPUT_VARIABLE(1);
+        REQUIRE_TRUE(reps_vector->lengthOf() == inRank, 0, "TILE op: repeats vector length should be equal to input rank, but got %i and %i correspondingly !", reps_vector->lengthOf(), inRank);
 
-
-                std::vector<int> shape(input->rankOf());
-                for (int e = 0; e < input->rankOf(); e++)
-                    shape[e] = input->sizeAt(e) * reps[e];
-
-                output = OUTPUT_VARIABLE(0);
-
-            } else {
-                REQUIRE_TRUE(false, 0, "Tile: this op requires input array and repeats vector, either as IArgs or second array !");
-            }
-            
-            input->tile(reps, *output);
-
-            return Status::OK();
-        }
-
-        DECLARE_SHAPE_FN(tile) {
-            auto shapeList = SHAPELIST();
-            auto inShape = inputShape->at(0);
-
-            if (block.width() > 1) {
-                auto repsVectorShape = inputShape->at(1);
-                REQUIRE_TRUE(shape::length(repsVectorShape) == shape::rank(inShape), 0, "TILE op: repeats vector length should be equal to input rank, but got %i and %i correspondingly !", shape::length(repsVectorShape), shape::rank(inShape));
-            }
-
-            Nd4jLong *newShape;
-            ALLOCATE(newShape, block.getWorkspace(), shape::shapeInfoLength(inShape), Nd4jLong);
-            std::vector<Nd4jLong> shape(shape::rank(inShape));
-
-            for (int e = 0; e < shape::rank(inShape); e++)
-                shape[e] = shape::sizeAt(inShape, e);
-
-            if (block.getIArguments()->size() >= shape.size()) {
-                auto reps = block.getIArguments();
-
-                for (int e = 0; e < shape.size(); e++)
-                    shape[e] *= reps->at(e);
-
-                if (shape::order(inShape) == 'c')
-                    shape::shapeBuffer(shape.size(), shape.data(), newShape);
-                else
-                    shape::shapeBufferFortran(shape.size(), shape.data(), newShape);
-            } else {
-                auto r = INPUT_VARIABLE(1);
-                auto reps = r->template asVectorT<Nd4jLong>();
-
-                // runtime evaluation, sorry
-                for (int e = 0; e < shape.size(); e++)
-                    shape[e] *= reps[e];
-
-                if (shape::order(inShape) == 'c')
-                    shape::shapeBuffer(shape.size(), shape.data(), newShape);
-                else
-                    shape::shapeBufferFortran(shape.size(), shape.data(), newShape);
-            }
-
-            shapeList->push_back(newShape);
-
-            return shapeList;
-        }
+        reps = reps_vector->template asVectorT<Nd4jLong>();
     }
+    else {
+        REQUIRE_TRUE(false, 0, "TILE op: this op requires repeats vector, either as IArgs or second array with length equal to rank of input array to be tiled !");
+    }
+            
+    input->tile(reps, *output);
+
+    return Status::OK();
+}
+
+
+DECLARE_SHAPE_FN(tile) {
+    
+    Nd4jLong* inShape = inputShape->at(0);
+    const int inRank = inShape[0];
+    std::vector<Nd4jLong> reps;
+
+    if (block.getIArguments()->size() == inRank) {
+
+        reps = ArrayUtils::toLongVector(*(block.getIArguments()));        
+    } 
+    else if (block.width() > 1)  {
+        
+        NDArray<T>* reps_vector = INPUT_VARIABLE(1);
+        REQUIRE_TRUE(reps_vector->lengthOf() == inRank, 0, "TILE op: repeats vector length should be equal to input rank, but got %i and %i correspondingly !", reps_vector->lengthOf(), inRank);
+        reps = reps_vector->template asVectorT<Nd4jLong>();
+    }
+    else {
+        REQUIRE_TRUE(false, 0, "TILE op: this op requires repeats vector, either as IArgs or second array with length equal to rank of input array to be tiled !");
+    }
+
+    Nd4jLong* newShape;
+    ALLOCATE(newShape, block.getWorkspace(), shape::shapeInfoLength(inRank), Nd4jLong);
+    
+    std::vector<Nd4jLong> shape(inRank);
+    for (int e = 0; e < shape::rank(inShape); e++)
+        shape[e] = shape::sizeAt(inShape, e) * reps[e];
+
+    if (shape::order(inShape) == 'c')
+        shape::shapeBuffer(shape.size(), shape.data(), newShape);
+    else
+        shape::shapeBufferFortran(shape.size(), shape.data(), newShape);
+       
+    return SHAPELIST(newShape);
+}
+
+}
 }
 
 #endif
