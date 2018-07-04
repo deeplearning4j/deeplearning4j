0093a9b0ae44931615ad05c358214d08767c90c5
Adam Gibson <adam@skymind.io>
2018-07-04 16:31:34 +0900

    Nd4j tensorflow interop (#5722)
    * add image pre processing scaler normalizer strategy for persistence
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * add nd4j tensorflow pointer interop
    
    * local
    
    * add basic device inference relative to current thread
    
    * ensure device change
    
    * get rid of extra tensorflow older
    
    * get rid of extra tensorflow older
    
    * add graph runner
    
    * update graph runner for multiple
    
    * test run
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * add another test
    
    * basic zero copy poc working
    
    * clean up graph runner tests
    
    * add configuration options
    
    * add protobuf serde
    
    * add protobuf serde
    
    * add test for from json
    
    * more docs
    
    * add more protobuf files
    
    * add more protobuf files
    
    * update deps
    
    * update pom
    
    * dont allow growth
    
    * add support for loading ndarrays via bytebuffers in arrow
    
    * add tensor support for datavec
    
    * ensure slice is used with buffer serialization
    
    * local
    
    * add documentation forr slice() in ArrowConverter and more robust testing/docs for arrow tensor/nd4j interactions
    
    * trigger jenkins
    
    * add tests for views and null arrays in conversion
    
    * clean up constructors
    
    * switch tensorflow conversion to singleton
    
    * add lib directory

---
+++ nd4j/nd4j-backends/nd4j-tests-tensorflow/src/test/cpujava/org/nd4j/tensorflow/conversion/GraphRunnerTest.java
@@ -0,0 +1,74 @@
+package org.nd4j.tensorflow.conversion;
+
+import com.github.os72.protobuf351.util.JsonFormat;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.io.ClassPathResource;
+import org.nd4j.tensorflow.conversion.graphrunner.GraphRunner;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.bytedeco.javacpp.tensorflow.ConfigProto;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class GraphRunnerTest {
+
+    @Test
+    public void testGraphRunner() throws Exception {
+        byte[] content = IOUtils.toByteArray(new ClassPathResource("/tf_graphs/nd4j_convert/simple_graph/frozen_model.pb").getInputStream());
+
+        try(GraphRunner graphRunner = new GraphRunner(content)) {
+            runGraphRunnerTest(graphRunner);
+        }
+    }
+
+    @Test
+    public void testGraphRunnerFilePath() throws Exception {
+        File file = new ClassPathResource("/tf_graphs/nd4j_convert/simple_graph/frozen_model.pb").getFile();
+        try(GraphRunner graphRunner = new GraphRunner(file.getAbsolutePath())) {
+            runGraphRunnerTest(graphRunner);
+        }
+    }
+
+
+    private void runGraphRunnerTest(GraphRunner graphRunner) throws Exception {
+
+        org.tensorflow.framework.ConfigProto.Builder builder = org.tensorflow.framework.ConfigProto.newBuilder();
+        String json = graphRunner.sessionOptionsToJson();
+        JsonFormat.parser().merge(json,builder);
+        org.tensorflow.framework.ConfigProto build = builder.build();
+        assertEquals(build,graphRunner.getProtoBufConfigProto());
+        assertNotNull(graphRunner.getInputsForGraph());
+        assertNotNull(graphRunner.getOutputsForGraph());
+
+
+        org.tensorflow.framework.ConfigProto configProto1 = GraphRunner.fromJson(json);
+
+        assertEquals(graphRunner.getProtoBufConfigProto(),configProto1);
+        assertEquals(2,graphRunner.getInputsForGraph().size());
+        assertEquals(1,graphRunner.getOutputsForGraph().size());
+
+        INDArray input1 = Nd4j.linspace(1,4,4).reshape(4);
+        INDArray input2 = Nd4j.linspace(1,4,4).reshape(4);
+
+        Map<String,INDArray> inputs = new LinkedHashMap<>();
+        inputs.put("input_0",input1);
+        inputs.put("input_1",input2);
+
+        for(int i = 0; i < 2; i++) {
+            Map<String,INDArray> outputs = graphRunner.run(inputs);
+
+            INDArray assertion = input1.add(input2);
+            assertEquals(assertion,outputs.get("output"));
+        }
+
+    }
+
+
+
+}
