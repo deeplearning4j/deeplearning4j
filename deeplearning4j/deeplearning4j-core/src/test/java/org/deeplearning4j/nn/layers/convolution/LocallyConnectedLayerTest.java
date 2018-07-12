7067b2f128413157f65f5725efd6fbe1f29c3d41
Max Pumperla <max.pumperla@googlemail.com>
2018-07-12 11:33:34 +0200

    ff test

--- deeplearning4j/deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/convolution/LocallyConnectedLayerTest.java
+++ deeplearning4j/deeplearning4j-core/src/test/java/org/deeplearning4j/nn/layers/convolution/LocallyConnectedLayerTest.java
@@ -34,7 +34,7 @@ public class LocallyConnectedLayerTest extends BaseDL4JTest {
     }
 
     @Test
-    public void testForward(){
+    public void test2dForward(){
         MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder().seed(123)
                         .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).l2(2e-4)
                         .updater(new Nesterovs(0.9)).dropOut(0.5)
@@ -58,6 +58,34 @@ public class LocallyConnectedLayerTest extends BaseDL4JTest {
         INDArray output = network.output(input, false);
 
         assert Arrays.equals(output.shape(), new long[] {10, 10});
+    }
+
+    @Test
+    public void test1dForward(){
+        MultiLayerConfiguration.Builder builder = new NeuralNetConfiguration.Builder().seed(123)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).l2(2e-4)
+                .updater(new Nesterovs(0.9)).dropOut(0.5)
+                .list()
+                .layer(new LocallyConnected1D.Builder().kernelSize(8).nIn(3)
+                        .stride(4).nOut(16).dropOut(0.5)
+                        .convolutionMode(ConvolutionMode.Strict)
+                        .setInputSize(28)
+                        .activation(Activation.RELU).weightInit(
+                                WeightInit.XAVIER)
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.SQUARED_LOSS) //output layer
+                        .nOut(10).weightInit(WeightInit.XAVIER).activation(Activation.SOFTMAX).build())
+                .setInputType(InputType.recurrent(3,  28)).backprop(true).pretrain(false);
+
+        MultiLayerConfiguration conf = builder.build();
+        MultiLayerNetwork network = new MultiLayerNetwork(conf);
+        network.init();
+
+        INDArray input = Nd4j.ones(10, 3, 28);
+        INDArray output = network.output(input, false);
+
+        System.out.println(Arrays.toString(output.shape()));
+        //assert Arrays.equals(output.shape(), new long[] {10, 10});
     }
 
 }
