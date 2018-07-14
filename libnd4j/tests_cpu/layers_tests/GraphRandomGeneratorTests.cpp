a90f6cce9259fecaae48ea9d73ec7e71e41549cf
raver119@gmail.com <raver119@gmail.com>
2018-07-14 15:20:59 -0700

    one failing test

--- libnd4j/tests_cpu/layers_tests/GraphRandomGeneratorTests.cpp
+++ libnd4j/tests_cpu/layers_tests/GraphRandomGeneratorTests.cpp
@@ -1,6 +1,7 @@
 #include "testlayers.h"
 #include <graph/RandomGenerator.h>
 #include <array/DataTypeUtils.h>
+#include <graph/Graph.h>
 
 using namespace nd4j;
 using namespace nd4j::graph;
@@ -110,4 +111,49 @@ TEST_F(GraphRandomGeneratorTests, Sequential_Test_3) {
 
     // and here output must be equal as well
     ASSERT_EQ(r1, r0);
+}
+
+
+TEST_F(GraphRandomGeneratorTests, FloatingPoint_Test_1) {
+    nd4j::graph::RandomGenerator g0(119, 5);
+    nd4j::graph::RandomGenerator g1(119, 5);
+
+    std::array<double, 100> z0, z1, z2, z3;
+
+    for (int e = 0; e < z0.size(); e++) {
+        z0[e] = g0.relativeT<double>(e, -1.0, 1.0);
+        z1[e] = g1.relativeT<double>(e, -1.0, 1.0);
+    }
+
+    g0.rewindH(z0.size());
+    g1.rewindH(z0.size());
+
+    for (int e = 0; e < z0.size(); e++) {
+        z2[e] = g0.relativeT<double>(e, -1.0, 1.0);
+        z3[e] = g1.relativeT<double>(e, -1.0, 1.0);
+    }
+
+    // these sequences should be equal
+    ASSERT_EQ(z0, z1);
+    ASSERT_EQ(z2, z3);
+
+    // these sequences should be different due to rewind
+    ASSERT_NE(z0, z3);
+
+    // we'll count negatives as well
+    int negs = 0;
+
+    // make sure every value stays within distribution borders
+    for (int e = 0; e < z0.size(); e++) {
+        auto v = z0[e];
+        if (!(v >= -1.0 && v <= 1.0)) {
+            nd4j_printf("Failed at idx [%i]: %f\n", e, (float) v);
+            ASSERT_TRUE(v >= -1.0 && v <= 1.0);
+        }
+
+        if (v < 0.0)
+            negs++;
+    }
+
+    ASSERT_TRUE(negs > 0);
 }
