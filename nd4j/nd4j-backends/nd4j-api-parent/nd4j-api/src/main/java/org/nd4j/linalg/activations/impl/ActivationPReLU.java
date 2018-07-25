5d88bd4a0c22e50e6b6bef9e4a59b19aa949097c
Max Pumperla <max.pumperla@googlemail.com>
2018-07-25 11:38:52 +0200

    prelu activation function

---
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/activations/impl/ActivationPReLU.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.nd4j.linalg.activations.impl;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.nd4j.linalg.activations.BaseActivationFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.DynamicCustomOp;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.primitives.Pair;
+
+/**
+ /** Parametrized Rectified Linear Unit (PReLU)
+ *
+ * f(x) = alpha * x for x < 0, f(x) = x for x >= 0
+ *
+ * alpha has the same shape as x and is a learned parameter.
+ *
+ * @author Max Pumperla
+ */
+@EqualsAndHashCode
+@Getter
+public class ActivationPReLU extends BaseActivationFunction {
+
+    private INDArray alpha;
+
+    public ActivationPReLU() {}
+
+    public ActivationPReLU(INDArray alpha) {
+        this.alpha = alpha;
+    }
+
+    @Override
+    public INDArray getActivation(INDArray in, boolean training) {
+        INDArray activation = Nd4j.create(in.shape());
+        DynamicCustomOp prelu = DynamicCustomOp.builder("prelu")
+                .addOutputs(activation).addInputs(in, alpha).build();
+        Nd4j.getExecutioner().exec(prelu);
+        return activation;
+    }
+
+    @Override
+    public Pair<INDArray, INDArray> backprop(INDArray in, INDArray epsilon) {
+        assertShape(in, epsilon);
+        INDArray dLdz = Nd4j.create(in.shape());
+        INDArray dLdalpha = Nd4j.create(in.shape());
+        DynamicCustomOp threshReluBp = DynamicCustomOp.builder("prelu_bp")
+                .addInputs(in, alpha, epsilon).addOutputs(dLdz, dLdalpha).build();
+        Nd4j.getExecutioner().exec(threshReluBp);
+        return new Pair<>(dLdz, dLdalpha);
+    }
+
+    @Override
+    public String toString() {
+        return "prelu";
+    }
+}
