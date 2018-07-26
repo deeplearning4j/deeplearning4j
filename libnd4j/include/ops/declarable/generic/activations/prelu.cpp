13004879e85ea51c84ff763ac4e7c98c78d466e2
Yurii <yurii@skymind.io>
2018-07-26 08:42:42 +0300

    Shyrma prelu2 (#5969)
    * add additional parameter sharedAxes to prelu ff op, provide corresponding tests
    
    * provide xhanges in prelu_bp and add corresponding tests

--- libnd4j/include/ops/declarable/generic/activations/prelu.cpp
+++ libnd4j/include/ops/declarable/generic/activations/prelu.cpp
@@ -23,6 +23,8 @@
 #if NOT_EXCLUDED(OP_prelu)
 
 #include <ops/declarable/CustomOperations.h>
+#include <ops/declarable/helpers/activations.h>
+#include <numeric>
 
 namespace nd4j {
 namespace ops  {
@@ -35,9 +37,40 @@ CONFIGURABLE_OP_IMPL(prelu, 2, 1, true, 0, 0) {
     NDArray<T>* alpha  = INPUT_VARIABLE(1);
     NDArray<T>* output = OUTPUT_VARIABLE(0);
 
-    auto preluFunc = LAMBDA_TT(i, a) { if (i < static_cast<T>(0)) return i * a; else return i; };
+    std::vector<int> sharedAxes = *block.getIArguments();
+    
+    const int inputRank     = input->rankOf();
+    const int alphaRank     = alpha->rankOf();
+    const int numSharedAxes = sharedAxes.size();            // can be zero as well
+    const Nd4jLong inputLen = input->lengthOf();
+    const Nd4jLong alphaLen = alpha->lengthOf();
+    const std::vector<Nd4jLong> inputShape = input->getShapeAsVector();
+    const std::vector<Nd4jLong> alphaShape = alpha->getShapeAsVector();
 
-    input->applyPairwiseLambda(alpha, preluFunc, output);
+    //***** input validation *****//
+    std::vector<Nd4jLong> expectedAlphaShape(&inputShape[1], &inputShape[inputRank]);
+
+    REQUIRE_TRUE(inputRank > 1, 0, "PRELU OP: wrong rank of input array, expected rank should be > 1, but got %i instead !", inputRank);   
+    
+    for(int i = 0; i < numSharedAxes; ++i) {
+        if(sharedAxes[i] <= 0)
+            sharedAxes[i] += inputRank - 1;
+        REQUIRE_TRUE(1 <= sharedAxes[i] && sharedAxes[i] <= inputRank - 1, 0, "PRELU OP: wrong axis value %i in sharedAxes at position %i, axis value must be within range [1, input_rank-1] !", sharedAxes[i], i);            
+        expectedAlphaShape[sharedAxes[i] - 1] = 1;
+    }
+
+    const Nd4jLong expectedAlphaLen = std::accumulate(expectedAlphaShape.begin(), expectedAlphaShape.end(), 1, std::multiplies<T>());        
+
+    REQUIRE_TRUE(alphaLen == expectedAlphaLen, 0, "PRELU OP: wrong shape of alpha array, expected is %s, but got %s instead !", ShapeUtils<T>::shapeAsString(expectedAlphaShape).c_str(), ShapeUtils<T>::shapeAsString(alphaShape).c_str());   
+    // ***** end of validation ***** //
+
+    if(alphaShape != expectedAlphaShape)
+        alpha = alpha->reshape(alpha->ordering(), expectedAlphaShape);
+
+    helpers::prelu(*input, *alpha, *output);
+
+    if(alphaShape != expectedAlphaShape)
+        delete alpha;
     
     return Status::OK();
 }
@@ -52,12 +85,45 @@ CONFIGURABLE_OP_IMPL(prelu_bp, 3, 2, true, 0, 0) {
     
     NDArray<T>* dLdI = OUTPUT_VARIABLE(0);
     NDArray<T>* dLdA = OUTPUT_VARIABLE(1);
+
+    std::vector<int> sharedAxes = *block.getIArguments();
     
-    auto derivI = LAMBDA_TTT(i, a, grO) {if (i < static_cast<T>(0)) return a * grO; else return grO; };
-    auto derivA = LAMBDA_TTT(i, a, grO) {if (i < static_cast<T>(0)) return i * grO; else return static_cast<T>(0); };
-        
-    input->applyTriplewiseLambda(alpha, dLdO, derivI, dLdI);
-    input->applyTriplewiseLambda(alpha, dLdO, derivA, dLdA);    
+    const int inputRank     = input->rankOf();
+    const int alphaRank     = alpha->rankOf();
+    const int numSharedAxes = sharedAxes.size();            // can be zero as well
+    const Nd4jLong inputLen = input->lengthOf();
+    const Nd4jLong alphaLen = alpha->lengthOf();
+    const std::vector<Nd4jLong> inputShape = input->getShapeAsVector();
+    const std::vector<Nd4jLong> alphaShape = alpha->getShapeAsVector();
+
+    //***** input validation *****//
+    std::vector<Nd4jLong> expectedAlphaShape(&inputShape[1], &inputShape[inputRank]);
+
+    REQUIRE_TRUE(inputRank > 1, 0, "PRELU_BP OP: wrong rank of input array, expected rank should be > 1, but got %i instead !", inputRank);   
+    
+    for(int i = 0; i < numSharedAxes; ++i) {
+        if(sharedAxes[i] <= 0)
+            sharedAxes[i] += inputRank - 1;
+        REQUIRE_TRUE(1 <= sharedAxes[i] && sharedAxes[i] <= inputRank - 1, 0, "PRELU_BP OP: wrong axis value %i in sharedAxes at position %i, axis value must be within range [1, input_rank-1] !", sharedAxes[i], i);            
+        expectedAlphaShape[sharedAxes[i] - 1] = 1;
+    }
+
+    const Nd4jLong expectedAlphaLen = std::accumulate(expectedAlphaShape.begin(), expectedAlphaShape.end(), 1, std::multiplies<T>());        
+
+    REQUIRE_TRUE(alphaLen == expectedAlphaLen, 0, "PRELU_BP OP: wrong shape of alpha array, expected is %s, but got %s instead !", ShapeUtils<T>::shapeAsString(expectedAlphaShape).c_str(), ShapeUtils<T>::shapeAsString(alphaShape).c_str());   
+    // ***** end of validation ***** //
+    
+    if(alphaShape != expectedAlphaShape) {
+        alpha = alpha->reshape(alpha->ordering(), expectedAlphaShape);
+        dLdA  = dLdA->reshape(dLdA->ordering(), expectedAlphaShape);
+    }
+
+    helpers::preluBP(*input, *alpha, *dLdO, *dLdI, *dLdA);
+
+    if(alphaShape != expectedAlphaShape) {        
+        delete alpha;
+        delete dLdA;
+    }
     
     return Status::OK();
 }
