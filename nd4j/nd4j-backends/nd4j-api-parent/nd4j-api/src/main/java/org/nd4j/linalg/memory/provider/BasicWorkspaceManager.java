097796655e028249e9c5d162276b5ceff5d7bd79
Christine Poerschke <cpoerschke@bloomberg.net>
2018-07-27 20:00:29 +0100

    Revert "nd4j: facilitate deallocator thread identification (for test purposes)"
    This reverts commit 1a331791939668761a42b3a6f8c6352aa638af04.

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/memory/provider/BasicWorkspaceManager.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/memory/provider/BasicWorkspaceManager.java
@@ -238,16 +238,13 @@ public abstract class BasicWorkspaceManager implements MemoryWorkspaceManager {
     }
 
 
-    @Deprecated // For test use within the github.com/deeplearning4j/deeplearning4j repo only.
-    public static final String WorkspaceDeallocatorThreadName = "Workspace deallocator thread";
-
     protected class WorkspaceDeallocatorThread extends Thread implements Runnable {
         private final ReferenceQueue<MemoryWorkspace> queue;
 
         protected WorkspaceDeallocatorThread(ReferenceQueue<MemoryWorkspace> queue) {
             this.queue = queue;
             this.setDaemon(true);
-            this.setName(WorkspaceDeallocatorThreadName);
+            this.setName("Workspace deallocator thread");
         }
 
         @Override
