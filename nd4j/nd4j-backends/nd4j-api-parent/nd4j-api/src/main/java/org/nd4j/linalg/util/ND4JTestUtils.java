6c9fd562edf24fd924a278b9dfa9009fc6bd9cd4
Alex Black <blacka101@gmail.com>
2018-07-31 16:39:55 +1000

    ND4jTestUtilities polishing

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/util/ND4JTestUtils.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/util/ND4JTestUtils.java
@@ -17,6 +17,7 @@
 package org.nd4j.linalg.util;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.apache.commons.io.FileUtils;
 import org.nd4j.base.Preconditions;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -26,17 +27,20 @@ import org.nd4j.linalg.primitives.Triple;
 
 import java.io.File;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+/**
+ * Test utilitiez for ND4J
+ *
+ * @author Alex Black
+ */
 public class ND4JTestUtils {
 
     private ND4JTestUtils(){ }
 
 
     @AllArgsConstructor
+    @Data
     public static class ComparisonResult {
         List<Triple<File,File,Boolean>> allResults;
         List<Triple<File,File,Boolean>> passed;
@@ -45,6 +49,9 @@ public class ND4JTestUtils {
         List<File> skippedDir2;
     }
 
+    /**
+     * A function for use with {@link #validateSerializedArrays(File, File, boolean, BiFunction)} using {@link INDArray#equals(Object)}
+     */
     public static class EqualsFn implements BiFunction<INDArray,INDArray,Boolean> {
         @Override
         public Boolean apply(INDArray i1, INDArray i2) {
@@ -52,6 +59,9 @@ public class ND4JTestUtils {
         }
     }
 
+    /**
+     * A function for use with {@link #validateSerializedArrays(File, File, boolean, BiFunction)} using {@link INDArray#equalsWithEps(Object, double)}
+     */
     @AllArgsConstructor
     public static class EqualsWithEpsFn implements BiFunction<INDArray,INDArray,Boolean> {
         private final double eps;
@@ -62,12 +72,29 @@ public class ND4JTestUtils {
         }
     }
 
-
-    public ComparisonResult validateSerializedArrays(File dir1, File dir2, boolean recursive) throws Exception {
+    /**
+     * Scan the specified directories for matching files (i.e., same path relative to their respective root directories)
+     * and compare the contents using INDArray.equals (via {@link EqualsFn}
+     * Assumes the saved files represent INDArrays saved with {@link Nd4j#saveBinary(INDArray, File)}
+     * @param dir1      First directory
+     * @param dir2      Second directory
+     * @param recursive Whether to search recursively (i.e., include files in subdirectories
+     * @return Comparison results
+     */
+    public static ComparisonResult validateSerializedArrays(File dir1, File dir2, boolean recursive) throws Exception {
         return validateSerializedArrays(dir1, dir2, recursive, new EqualsFn());
     }
 
-    public ComparisonResult validateSerializedArrays(File dir1, File dir2, boolean recursive, BiFunction<INDArray,INDArray,Boolean> evalFn) throws Exception {
+    /**
+     * Scan the specified directories for matching files (i.e., same path relative to their respective root directories)
+     * and compare the contents using a provided function.<br>
+     * Assumes the saved files represent INDArrays saved with {@link Nd4j#saveBinary(INDArray, File)}
+     * @param dir1      First directory
+     * @param dir2      Second directory
+     * @param recursive Whether to search recursively (i.e., include files in subdirectories
+     * @return Comparison results
+     */
+    public static ComparisonResult validateSerializedArrays(File dir1, File dir2, boolean recursive, BiFunction<INDArray,INDArray,Boolean> evalFn) throws Exception {
         File[] f1 = FileUtils.listFiles(dir1, null, recursive).toArray(new File[0]);
         File[] f2 = FileUtils.listFiles(dir2, null, recursive).toArray(new File[0]);
 
@@ -79,13 +106,17 @@ public class ND4JTestUtils {
 
         URI u = dir1.toURI();
         for(File f : f1){
-            String relative = f.toURI().relativize(u).getPath();
+            if(!f.isFile())
+                continue;
+            String relative = u.relativize(f.toURI()).getPath();
             relativized1.put(relative, f);
         }
 
         u = dir2.toURI();
         for(File f : f2){
-            String relative = f.toURI().relativize(u).getPath();
+            if(!f.isFile())
+                continue;
+            String relative = u.relativize(f.toURI()).getPath();
             relativized2.put(relative, f);
         }
 
@@ -109,6 +140,10 @@ public class ND4JTestUtils {
         for(Map.Entry<String,File> e : relativized1.entrySet()){
             File file1 = e.getValue();
             File file2 = relativized2.get(e.getKey());
+
+            if(file2 == null)
+                continue;
+
             INDArray i1 = Nd4j.readBinary(file1);
             INDArray i2 = Nd4j.readBinary(file2);
             boolean b = evalFn.apply(i1, i2);
@@ -120,6 +155,20 @@ public class ND4JTestUtils {
                 failed.add(t);
             }
         }
+
+        Comparator<Triple<File,File,Boolean>> c = new Comparator<Triple<File, File, Boolean>>() {
+            @Override
+            public int compare(Triple<File, File, Boolean> o1, Triple<File, File, Boolean> o2) {
+                return o1.getFirst().compareTo(o2.getFirst());
+            }
+        };
+
+        Collections.sort(allResults, c);
+        Collections.sort(passed, c);
+        Collections.sort(failed, c);
+        Collections.sort(skipped1);
+        Collections.sort(skipped2);
+
 
         return new ComparisonResult(allResults, passed, failed, skipped1, skipped2);
     }
