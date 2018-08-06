b49572c0cd76c646c4c5c80014d0525de5b00111
Alex Black <blacka101@gmail.com>
2018-08-06 11:42:04 +1000

    Update copyright headers

--- deeplearning4j/deeplearning4j-dataimport-solrj/src/main/java/org/deeplearning4j/nn/dataimport/solr/client/solrj/io/stream/TupleStreamDataSetIterator.java
+++ deeplearning4j/deeplearning4j-dataimport-solrj/src/main/java/org/deeplearning4j/nn/dataimport/solr/client/solrj/io/stream/TupleStreamDataSetIterator.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
 package org.deeplearning4j.nn.dataimport.solr.client.solrj.io.stream;
 
 import java.io.Closeable;
@@ -23,7 +39,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
-  * A {@link DataSetIterator} which uses a <a href="https://lucene.apache.org/solr/guide/7_4/streaming-expressions.html">streaming expression</a> to fetch data from Apache Solr and/or one of the sources (e.g. <code>jdbc</code>) supported as a <a href="https://lucene.apache.org/solr/guide/7_4/stream-source-reference.html">stream source</a>.
+  * A {@link DataSetIterator} which uses a <a href="https://lucene.apache.org/solr/guide/7_4/streaming-expressions.html">streaming expression</a>
+ * to fetch data from Apache Solr and/or one of the sources (e.g. <code>jdbc</code>) supported as a <a href="https://lucene.apache.org/solr/guide/7_4/stream-source-reference.html">stream source</a>.
  * <p>
  * Example code snippet:
 <pre>{
