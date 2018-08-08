0016a29d7388e5762ebbd1880da66fe976172f53
Justin Long <justinlong@outlook.com>
2018-08-08 15:58:00 -0700

    Arbiter docs.

--- docs/gen_all_docs.sh
+++ docs/gen_all_docs.sh
@@ -66,20 +66,26 @@ python generate_docs.py \
     --out_language en
 
 python generate_docs.py \
+    --project arbiter \
+    --language java \
+    --code ../arbiter \
+    --out_language en
+
+python generate_docs.py \
     --project keras-import \
     --language java \
     --code ../deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/ \
     --docs_root deeplarning4j.org/keras \
     --out_language en
 
-python generate_docs.py \
-    --project scalnet \
-    --language scala \
-    --code ../scalnet/src/main/scala/org/deeplearning4j/scalnet/ \
-    --docs_root deeplarning4j.org/scalnet
+# python generate_docs.py \
+#     --project scalnet \
+#     --language scala \
+#     --code ../scalnet/src/main/scala/org/deeplearning4j/scalnet/ \
+#     --docs_root deeplarning4j.org/scalnet
 
-python generate_docs.py \
-    --project samediff \
-    --language java \
-    --code ../nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/autodiff/ \
-    --docs_root deeplarning4j.org/samediff
+# python generate_docs.py \
+#     --project samediff \
+#     --language java \
+#     --code ../nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/autodiff/ \
+#     --docs_root deeplarning4j.org/samediff
