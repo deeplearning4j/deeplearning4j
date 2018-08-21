328d1b6376c2b5a360fdd31a4d40f0f13cd7823f
shugeo <sgazeos@gmail.com>
2018-08-21 17:54:46 +0300

    Fixed where_np op shape and behavior for only param and tests.

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests10.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests10.cpp
@@ -170,16 +170,32 @@ TEST_F(DeclarableOpsTests10, Where_SGO_Test_1) {
 TEST_F(DeclarableOpsTests10, WhereNP_SGO_Test_1) {
     NDArray<double> cond3d('c', {2, 2, 2}, {1., 0., 0., 1., 1., 1., 1., 0.});
 //    NDArray<double> expIdx({0., 1., 0., 2., 0., 3., 4., 1., 4., 1.});
-    NDArray<double> exp({3., 4., 1., 0., 2.});
-
+    NDArray<double> exp1({0., 0., 1., 1., 1.});
+    NDArray<double> exp2({0., 1., 0., 0., 1.});
+    NDArray<double> exp3({0., 1., 0., 1., 0.});
     nd4j::ops::where_np<double> op;
     auto res = op.execute({&cond3d}, {}, {});
     ASSERT_TRUE(res->size() == 3);
     ASSERT_TRUE(res->status() == ND4J_STATUS_OK);
-    res->at(0)->printIndexedBuffer("Res1 WHERE_NP");
-    res->at(1)->printIndexedBuffer("Res2 WHERE_NP");
-    res->at(2)->printIndexedBuffer("Res3 WHERE_NP");
-    //ASSERT_TRUE(exp.equalsTo(res->at(0)));
+    ASSERT_TRUE(exp1.equalsTo(res->at(0)));
+    ASSERT_TRUE(exp2.equalsTo(res->at(1)));
+    ASSERT_TRUE(exp3.equalsTo(res->at(2)));
+    //ASSERT_TRUE(expIdx.equalsTo(res->at(1)));
+    delete res;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests10, WhereNP_SGO_Test_2) {
+    NDArray<double> cond2d('c', {3, 5}, {1., 1., 0., 0., 1., 1., 1., 1., 1., 1., 0., 1., 1., 1., 1.});
+//    NDArray<double> expIdx({0., 1., 0., 2., 0., 3., 4., 1., 4., 1.});
+    NDArray<double> exp1({0., 0., 0., 1., 1., 1., 1., 1., 2., 2., 2., 2.});
+    NDArray<double> exp2({0., 1., 4., 0., 1., 2., 3., 4., 1., 2., 3., 4.});
+    nd4j::ops::where_np<double> op;
+    auto res = op.execute({&cond2d}, {}, {});
+    ASSERT_TRUE(res->size() == 2);
+    ASSERT_TRUE(res->status() == ND4J_STATUS_OK);
+    ASSERT_TRUE(exp1.equalsTo(res->at(0)));
+    ASSERT_TRUE(exp2.equalsTo(res->at(1)));
     //ASSERT_TRUE(expIdx.equalsTo(res->at(1)));
     delete res;
 }
