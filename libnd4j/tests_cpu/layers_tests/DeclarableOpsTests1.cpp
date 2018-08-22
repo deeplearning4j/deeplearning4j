b7c6137fef2bfe6350740c5d121df6ba493db1dc
shugeo <35595510+shugeo@users.noreply.github.com>
2018-08-22 05:57:27 +0300

    Shugeo argmin fix (#6176)
    * Shugeo dynamic bp (#6106)
    
    * Added dynamic_partition_bp op and helpers. Initial revision.
    
    * Implementation of dynamic_partion_bp and test.
    
    * Fixed implementation bugs.
    
    * Minor fixes with dynamic_partition_bp and test.
    
    * Modified dynamic_partition_bp op and tests.
    
    * Modified dynamic_stitch invocation with dynamic_partition_bp op.
    
    * Another approach to implement.
    
    * Final version of dynamic_partition_bp op.
    
    * Refactored revision of dynamic_partition_bp op.
    
    * The first working revision.
    
    * Fixed form bug with argmin op.
    
    * Added test for zeta op.
    
    * Fixed adjustAxis helper.
    
    * Drop alpha_dropout and dropout_bp ops as too early.
    
    * Unify argmin op with argmax op.

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests1.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests1.cpp
@@ -3366,7 +3366,8 @@ TEST_F(DeclarableOpsTests1, ArgMax6) {
 TEST_F(DeclarableOpsTests1, ArgMin1) {
     NDArray<float> x('c', {3, 5});
     x.linspace(1);
-    NDArray<float> exp('c', {3, 1});
+//    NDArray<float> exp('c', {3, 1});
+    NDArray<float> exp('c', {3});
     exp.assign(0.0f);
 
     nd4j::ops::argmin<float> op;
