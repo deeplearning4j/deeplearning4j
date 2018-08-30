c93a0c3e008b0857bbe5408c8d528bd2b72b661a
Alex Black <blacka101@gmail.com>
2018-08-30 20:44:08 +1000

    Mapping fix for sparse softmax loss

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/accum/SparseSoftmaxCrossEntropyLossWithLogits.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/accum/SparseSoftmaxCrossEntropyLossWithLogits.java
@@ -17,9 +17,11 @@
 package org.nd4j.linalg.api.ops.impl.accum;
 
 import lombok.NoArgsConstructor;
+import lombok.val;
 import org.nd4j.autodiff.samediff.SDVariable;
 import org.nd4j.autodiff.samediff.SameDiff;
 import org.nd4j.imports.NoOpNameFoundException;
+import org.nd4j.imports.descriptors.properties.PropertyMapping;
 import org.nd4j.imports.graphmapper.tf.TFGraphMapper;
 import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.api.ops.Op;
@@ -27,6 +29,8 @@ import org.tensorflow.framework.AttrValue;
 import org.tensorflow.framework.GraphDef;
 import org.tensorflow.framework.NodeDef;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 
@@ -51,11 +55,17 @@ public class SparseSoftmaxCrossEntropyLossWithLogits extends DynamicCustomOp {
     @Override
     public void initFromTensorFlow(NodeDef nodeDef, SameDiff initWith, Map<String, AttrValue> attributesForNode, GraphDef graph) {
         TFGraphMapper.getInstance().initFunctionFromProperties(nodeDef.getOp(), this, attributesForNode, nodeDef, graph);
+
+        //Switch order: TF uses [logits, labels]; libnd4j expects [labels, logits]
+        String[] inputs = initWith.getInputsForFunction(this);
+        String temp = inputs[0];
+        inputs[0] = inputs[1];
+        inputs[1] = temp;
     }
 
     @Override
     public String opName() {
-        return "sparse_softmax_cross_entropy_with_logits";
+        return "sparse_softmax_cross_entropy_loss_with_logits";
     }
 
     @Override
