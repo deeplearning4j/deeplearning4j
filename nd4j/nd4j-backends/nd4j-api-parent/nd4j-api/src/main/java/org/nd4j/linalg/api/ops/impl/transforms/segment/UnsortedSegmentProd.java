4a8f6f2e5e29bf22ac75a0a72dea10f60a7d9b71
Alex Black <blacka101@gmail.com>
2018-09-07 21:52:19 +1000

    Unsorted segment ops

---
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/transforms/segment/UnsortedSegmentProd.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.nd4j.linalg.api.ops.impl.transforms.segment;
+
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.linalg.api.ops.DynamicCustomOp;
+
+/**
+ * Unsorted segment product operation
+ *
+ * @author Alex Black
+ */
+public class UnsortedSegmentProd extends DynamicCustomOp {
+
+    public UnsortedSegmentProd(SameDiff sameDiff, SDVariable data, SDVariable segmentIds) {
+        super(null, sameDiff,  new SDVariable[] {data, segmentIds}, false);
+    }
+
+    public UnsortedSegmentProd(){ }
+
+    @Override
+    public String opName(){
+        return "unsorted_segment_prod";
+    }
+
+    @Override
+    public String tensorflowName() {
+        return "UnsortedSegmentProd";
+    }
+
+}
