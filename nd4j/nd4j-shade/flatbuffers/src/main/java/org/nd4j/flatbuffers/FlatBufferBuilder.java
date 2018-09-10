dbb1d3e3ef01b6e1034cfbb82617ced7da94b925
Alex Black <blacka101@gmail.com>
2018-09-10 14:40:12 +1000

    Constructors on FlatBufferBuilder; use shaded flatbuffers in datavec

--- nd4j/nd4j-shade/flatbuffers/src/main/java/org/nd4j/flatbuffers/FlatBufferBuilder.java
+++ nd4j/nd4j-shade/flatbuffers/src/main/java/org/nd4j/flatbuffers/FlatBufferBuilder.java
@@ -1,7 +1,28 @@
 package org.nd4j.flatbuffers;
 
+import java.nio.ByteBuffer;
+
 public class FlatBufferBuilder extends com.google.flatbuffers.FlatBufferBuilder {
 
+    public FlatBufferBuilder(int initial_size, com.google.flatbuffers.FlatBufferBuilder.ByteBufferFactory bb_factory) {
+        super(initial_size, bb_factory);
+    }
+
+    public FlatBufferBuilder(int initial_size) {
+        super(initial_size);
+    }
+
+    public FlatBufferBuilder(){
+        super();
+    }
+
+    public FlatBufferBuilder(ByteBuffer existing_bb, com.google.flatbuffers.FlatBufferBuilder.ByteBufferFactory bb_factory) {
+        super(existing_bb, bb_factory);
+    }
+
+    public FlatBufferBuilder(ByteBuffer existing_bb) {
+        super(existing_bb);
+    }
     public int createString(String s){
         return super.createString(s);
     }
