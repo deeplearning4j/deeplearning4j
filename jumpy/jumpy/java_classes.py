88f8a8214f032916b553998a640a2f63f92ead4d
Max Pumperla <max.pumperla@googlemail.com>
2018-09-11 15:40:41 +0200

    bump jumpy version, use pydl4j (#6416)

--- jumpy/jumpy/java_classes.py
+++ jumpy/jumpy/java_classes.py
@@ -18,18 +18,12 @@
 import jnius_config
 import os
 import warnings
+import pydl4j
 
-try:
-    jnius_config.add_options('-Dorg.bytedeco.javacpp.nopointergc=true')
-    jnius_class_path = os.environ.get('JUMPY_CLASS_PATH')
-    if not jnius_class_path:
-        raise Exception('Environment variable JUMPY_CLASS_PATH not set.')
-    elif not os.path.exists(jnius_class_path):
-        raise Exception('File not found : {0}'.format(jnius_class_path))
-    jnius_config.set_classpath(jnius_class_path)
-except:
-    warnings.warn('JVM is already running. Can not set options and class path.')
+pydl4j.validate_nd4j_jars()
 
+
+# -------------JVM starts here-------------
 
 from jnius import autoclass
 
