b782ce28be563a5b7956c46ab30146f3610248ba
Max Pumperla <max.pumperla@googlemail.com>
2018-09-12 14:46:58 +0200

    proper setup.py

--- pydatavec/setup.py
+++ pydatavec/setup.py
@@ -15,8 +15,35 @@
 ################################################################################
 
 
+
+
 from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='pydatavec', packages=find_packages())
+setup(name='pydatavec',
+      version='0.1',
+      description='Python interface for DataVec',
+      long_description='Python interface for DataVec',
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+          'Topic :: Software Development :: Libraries',
+      ],
+      keywords='python java datavec etl deeplearning4j',
+      url='https://github.com/deeplearning4j/deeplearning4j.git',
+      license='Apache',
+      setup_requires=['Cython', 'pytest-runner'],
+      install_requires=['Cython', 'requests', 'pydl4j', 'numpy'],
+      extras_require={
+          'spark': ['pyspark'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'mock'],
+      },
+      packages=find_packages())
