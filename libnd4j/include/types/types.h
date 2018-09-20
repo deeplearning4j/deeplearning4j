54ecf67dee9fbf28fd22f1da1b8f6916ac5c4409
raver119 <raver119@gmail.com>
2018-09-20 18:13:50 +0300

    minus uint16/32/64

--- libnd4j/include/types/types.h
+++ libnd4j/include/types/types.h
@@ -39,11 +39,12 @@
         (nd4j::DataType::DataType_INT8, int8_t), \
         (nd4j::DataType::DataType_UINT8, uint8_t), \
         (nd4j::DataType::DataType_INT16, int16_t), \
-        (nd4j::DataType::DataType_UINT16, uint16_t), \
         (nd4j::DataType::DataType_INT32, int32_t), \
-        (nd4j::DataType::DataType_UINT32, uint32_t), \
-        (nd4j::DataType::DataType_INT64, Nd4jLong), \
-        (nd4j::DataType::DataType_UINT64, Nd4jULong)
+        (nd4j::DataType::DataType_INT64, Nd4jLong)
+
+        //(nd4j::DataType::DataType_UINT16, uint16_t), \
+        //(nd4j::DataType::DataType_UINT64, Nd4jULong)
+        //(nd4j::DataType::DataType_UINT32, uint32_t), \
 
 #define FLOAT_TYPES \
         (nd4j::DataType::DataType_HALF, float16), \
@@ -54,11 +55,8 @@
         (nd4j::DataType::DataType_INT8, int8_t), \
         (nd4j::DataType::DataType_UINT8, uint8_t), \
         (nd4j::DataType::DataType_INT16, int16_t), \
-        (nd4j::DataType::DataType_UINT16, uint16_t), \
         (nd4j::DataType::DataType_INT32, int32_t), \
-        (nd4j::DataType::DataType_UINT32, uint32_t), \
-        (nd4j::DataType::DataType_INT64, Nd4jLong), \
-        (nd4j::DataType::DataType_UINT64, Nd4jULong)
+        (nd4j::DataType::DataType_INT64, Nd4jLong)
 
 
 #define NUMERIC_TYPES \
@@ -68,11 +66,8 @@
         (nd4j::DataType::DataType_INT8, int8_t), \
         (nd4j::DataType::DataType_UINT8, uint8_t), \
         (nd4j::DataType::DataType_INT16, int16_t), \
-        (nd4j::DataType::DataType_UINT16, uint16_t), \
         (nd4j::DataType::DataType_INT32, int32_t), \
-        (nd4j::DataType::DataType_UINT32, uint32_t), \
-        (nd4j::DataType::DataType_INT64, Nd4jLong), \
-        (nd4j::DataType::DataType_UINT64, Nd4jULong)
+        (nd4j::DataType::DataType_INT64, Nd4jLong)
 
 
 
