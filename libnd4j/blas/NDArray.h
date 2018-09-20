a12e6479a6dda288150fc20f24e47fd5fd22dc75
raver119 <raver119@gmail.com>
2018-09-20 15:51:15 +0300

    couple of fixes

--- libnd4j/blas/NDArray.h
+++ libnd4j/blas/NDArray.h
@@ -1194,9 +1194,10 @@ namespace nd4j {
         template <typename T>
         void putScalar(const Nd4jLong i, const Nd4jLong j, const Nd4jLong k, const Nd4jLong l, const T value);
 
+        /*
         template <typename T>
         void putScalar(const Nd4jLong* indices, const T value);
-
+*/
         /**
         *  returns true if array is 2D
         */
