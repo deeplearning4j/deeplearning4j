080a9f4135883dfabae705548a1fb38da8c79f6e
raver119 <raver119@gmail.com>
2018-09-28 15:03:26 +0300

    next step

--- libnd4j/include/ops/declarable/impl/LegacyTransformOp.cpp
+++ libnd4j/include/ops/declarable/impl/LegacyTransformOp.cpp
@@ -43,7 +43,7 @@ namespace nd4j {
 
             int opNum = block.opNum() < 0 ? this->_opNum : block.opNum();
 
-            NativeOpExcutioner::execTransform(opNum, input->getBuffer(), input->getShapeInfo(), z->getBuffer(), z->getShapeInfo(), block.getTArguments()->data(), nullptr, nullptr);
+            NativeOpExcutioner::execTransformSame(opNum, input->getBuffer(), input->getShapeInfo(), z->getBuffer(), z->getShapeInfo(), block.getTArguments()->data(), nullptr, nullptr);
 
             STORE_RESULT(*z);
 
