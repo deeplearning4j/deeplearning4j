9c1871f5433425c348dbbd7b2dd8a5a6321723ff
Alex Black <blacka101@gmail.com>
2018-10-17 16:29:06 +1100

    FileBatch javadoc and testing

--- nd4j/nd4j-common/src/main/java/org/nd4j/api/loader/FileBatch.java
+++ nd4j/nd4j-common/src/main/java/org/nd4j/api/loader/FileBatch.java
@@ -2,6 +2,9 @@ package org.nd4j.api.loader;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
@@ -11,59 +14,122 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * FileBatch: stores the raw contents of multiple files in byte arrays (one per file) along with their original paths.
+ * FileBatch can be stored to disk (or any output stream) in zip format.
+ * Typical use cases include creating batches of data in their raw format for distributed training. This can reduce the
+ * number of disk reads required (fewer files) and network transfers when reading from remote storage, due to the zip
+ * format used for compression.
+ *
+ * @author Alex Black
+ */
 @AllArgsConstructor
 @Data
 public class FileBatch implements Serializable {
-    public static final String ORIG_PATHS_FILENAME = "originalPaths.txt";
+    /**
+     * Name of the file in the zip file that contains the original paths/filenames
+     */
+    public static final String ORIGINAL_PATHS_FILENAME = "originalPaths.txt";
 
     private final List<byte[]> fileBytes;
     private final List<String> originalPaths;
 
-    public void writeAsZip(OutputStream os) throws IOException {
-        try(ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(os))){
-
-            //Write original paths as a text file:
-            ZipEntry ze = new ZipEntry(ORIG_PATHS_FILENAME);
-            String originalPathsJoined = StringUtils.join(originalPaths, "\n"); //Java String.join is Java 8
-            zos.putNextEntry(ze);
-            zos.write(originalPathsJoined.getBytes(StandardCharsets.UTF_8));
-
-            for( int i=0; i<fileBytes.size(); i++ ){
-                String name = "file_" + i + ".bin";
-                ze = new ZipEntry(name);
-                zos.putNextEntry(ze);
-                zos.write(fileBytes.get(i));
-            }
-        }
-    }
-
+    /**
+     * Read a FileBatch from the specified input stream. This method assumes the FileBatch was previously saved to
+     * zip format using {@link #writeAsZip(File)} or {@link #writeAsZip(OutputStream)}
+     *
+     * @param is Input stream to read from
+     * @return The loaded FileBatch
+     * @throws IOException If an error occurs during reading
+     */
     public static FileBatch readFromZip(InputStream is) throws IOException {
         String originalPaths = null;
-        Map<Integer,byte[]> bytesMap = new HashMap<>();
-        try(ZipInputStream zis = new ZipInputStream(new BufferedInputStream(is))){
+        Map<Integer, byte[]> bytesMap = new HashMap<>();
+        try (ZipInputStream zis = new ZipInputStream(new BufferedInputStream(is))) {
             ZipEntry ze;
-            while((ze = zis.getNextEntry()) != null){
+            while ((ze = zis.getNextEntry()) != null) {
                 String name = ze.getName();
-                long size = ze.getSize();
-                byte[] bytes = new byte[(int)size];
-                zis.read(bytes);
-                if(name.equals(ORIG_PATHS_FILENAME)){
+                byte[] bytes = IOUtils.toByteArray(zis);
+                if (name.equals(ORIGINAL_PATHS_FILENAME)) {
                     originalPaths = new String(bytes, 0, bytes.length, StandardCharsets.UTF_8);
                 } else {
                     int idxSplit = name.indexOf("_");
                     int idxSplit2 = name.indexOf(".");
-                    int fileIdx = Integer.parseInt(name.substring(idxSplit+1, idxSplit2));
+                    int fileIdx = Integer.parseInt(name.substring(idxSplit + 1, idxSplit2));
                     bytesMap.put(fileIdx, bytes);
                 }
             }
         }
 
         List<byte[]> list = new ArrayList<>(bytesMap.size());
-        for(int i=0; i<bytesMap.size(); i++ ){
+        for (int i = 0; i < bytesMap.size(); i++) {
             list.add(bytesMap.get(i));
         }
 
         List<String> origPaths = Arrays.asList(originalPaths.split("\n"));
         return new FileBatch(list, origPaths);
+    }
+
+    /**
+     * Create a FileBatch from the specified files
+     *
+     * @param files Files to create the FileBatch from
+     * @return The created FileBatch
+     * @throws IOException If an error occurs during reading of the file content
+     */
+    public static FileBatch forFiles(File... files) throws IOException {
+        return forFiles(Arrays.asList(files));
+    }
+
+    /**
+     * Create a FileBatch from the specified files
+     *
+     * @param files Files to create the FileBatch from
+     * @return The created FileBatch
+     * @throws IOException If an error occurs during reading of the file content
+     */
+    public static FileBatch forFiles(List<File> files) throws IOException {
+        List<String> origPaths = new ArrayList<>(files.size());
+        List<byte[]> bytes = new ArrayList<>(files.size());
+        for (File f : files) {
+            bytes.add(FileUtils.readFileToByteArray(f));
+            origPaths.add(f.getPath());
+        }
+        return new FileBatch(bytes, origPaths);
+    }
+
+    /**
+     * Write the FileBatch to the specified File, in zip file format
+     *
+     * @param f File to write to
+     * @throws IOException If an error occurs during writing
+     */
+    public void writeAsZip(File f) throws IOException {
+        writeAsZip(new FileOutputStream(f));
+    }
+
+    /**
+     * @param os Write the FileBatch to the specified output stream, in zip file format
+     * @throws IOException If an error occurs during writing
+     */
+    public void writeAsZip(OutputStream os) throws IOException {
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(os))) {
+
+            //Write original paths as a text file:
+            ZipEntry ze = new ZipEntry(ORIGINAL_PATHS_FILENAME);
+            String originalPathsJoined = StringUtils.join(originalPaths, "\n"); //Java String.join is Java 8
+            zos.putNextEntry(ze);
+            zos.write(originalPathsJoined.getBytes(StandardCharsets.UTF_8));
+
+            for (int i = 0; i < fileBytes.size(); i++) {
+                String ext = FilenameUtils.getExtension(originalPaths.get(i));
+                if (ext == null || ext.isEmpty())
+                    ext = "bin";
+                String name = "file_" + i + "." + ext;
+                ze = new ZipEntry(name);
+                zos.putNextEntry(ze);
+                zos.write(fileBytes.get(i));
+            }
+        }
     }
 }
