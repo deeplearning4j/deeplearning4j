14967f87496eee3a737fc35909c7079d9d56dbd3
Yurii <yurii@skymind.io>
2018-10-19 13:42:59 +0300

    add some tests for gruCell_bp

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests9.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests9.cpp
@@ -2429,3 +2429,130 @@ TEST_F(DeclarableOpsTests9, batchnorm_bp_test3) {
     ASSERT_TRUE(isGradCorrect);
 }
 
+////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests9, gru_cell_bp_test1) {
+
+    const int bS = 2;
+    const int iS = 3;
+    const int nU = 4;
+
+    NDArray x('c', {bS, iS}, nd4j::DataType::DOUBLE);
+    NDArray h0('c', {bS, nU}, nd4j::DataType::DOUBLE);
+    NDArray Wx('c', {iS, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray Wh('c', {nU, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray b('c', {3*nU}, nd4j::DataType::DOUBLE);
+    NDArray dLdh('c', {bS, nU}, nd4j::DataType::DOUBLE);
+
+    x.linspace(0.5, 0.5);
+    h0 = 1.;
+    Wx = 0.003;
+    Wh = 0.006;
+    b  = 0.5;
+
+    const OpArgsHolder argsHolderFF({&x, &h0, &Wx, &Wh, &b}, {}, {});
+    const OpArgsHolder argsHolderBP({&x, &h0, &Wx, &Wh, &b, &dLdh}, {}, {});    
+
+    nd4j::ops::gruCell opFF;
+    nd4j::ops::gruCell_bp opBP;
+
+    const bool isGradCorrect = GradCheck::checkGrad(opFF, opBP, argsHolderFF, argsHolderBP);
+
+    ASSERT_TRUE(isGradCorrect);
+}
+
+////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests9, gru_cell_bp_test2) {
+
+    const int bS = 2;
+    const int iS = 3;
+    const int nU = 4;
+
+    NDArray x('c', {bS, iS}, nd4j::DataType::DOUBLE);
+    NDArray h0('c', {bS, nU}, nd4j::DataType::DOUBLE);
+    NDArray Wx('c', {iS, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray Wh('c', {nU, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray b('c', {3*nU}, nd4j::DataType::DOUBLE);
+    NDArray dLdh('c', {bS, nU}, nd4j::DataType::DOUBLE);
+  
+    x.linspace(0.5, 0.5);
+    h0 = 1.;
+    Wx = 0.003;
+    Wh = 0.006;
+    b  = 0.;
+
+    const OpArgsHolder argsHolderFF({&x, &h0, &Wx, &Wh, &b}, {}, {});
+    const OpArgsHolder argsHolderBP({&x, &h0, &Wx, &Wh, &b, &dLdh}, {}, {});    
+
+    nd4j::ops::gruCell opFF;
+    nd4j::ops::gruCell_bp opBP;
+
+    const bool isGradCorrect = GradCheck::checkGrad(opFF, opBP, argsHolderFF, argsHolderBP);
+
+    ASSERT_TRUE(isGradCorrect);
+}
+
+////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests9, gru_cell_bp_test3) {
+
+    const int bS = 2;
+    const int iS = 3;
+    const int nU = 4;
+
+    NDArray x('c', {bS, iS}, nd4j::DataType::DOUBLE);
+    NDArray h0('c', {bS, nU}, nd4j::DataType::DOUBLE);
+    NDArray Wx('c', {iS, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray Wh('c', {nU, 3*nU}, nd4j::DataType::DOUBLE);
+    NDArray b('c', {3*nU}, nd4j::DataType::DOUBLE);
+    NDArray dLdh('c', {bS, nU}, nd4j::DataType::DOUBLE);
+    // NDArray<double> dLdWx0('c', {iS, 3*nU});
+    // NDArray<double> dLdWh0('c', {nU, 3*nU});
+    // NDArray<double> dLdb0 ('c', {3*nU});
+
+    x = 1.;
+    h0 = 0.0;
+    Wx = 0.0;
+    Wh = 0.0;
+    b  = 0.5;
+
+    const OpArgsHolder argsHolderFF({&x, &h0, &Wx, &Wh, &b}, {}, {});
+    const OpArgsHolder argsHolderBP({&x, &h0, &Wx, &Wh, &b, &dLdh}, {}, {});    
+
+    nd4j::ops::gruCell opFF;
+    nd4j::ops::gruCell_bp opBP;
+
+    const bool isGradCorrect = GradCheck::checkGrad(opFF, opBP, argsHolderFF, argsHolderBP);
+
+    ASSERT_TRUE(isGradCorrect);
+}
+
+////////////////////////////////////////////////////////////////////
+// TEST_F(DeclarableOpsTests9, gru_bp_test1) {
+
+//     const int time = 5;
+//     const int bS   = 2;
+//     const int iS   = 3;
+//     const int nU   = 4;
+
+//     NDArray<double> x     ('c', {time, bS, iS});
+//     NDArray<double> h0    ('c', {bS, nU});
+//     NDArray<double> Wx    ('c', {iS, 3*nU});
+//     NDArray<double> Wh    ('c', {nU, 3*nU});
+//     NDArray<double> b     ('c', {3*nU});
+//     NDArray<double> dLdh  ('c', {time, bS, nU});
+
+//     x.linspace(0.5, 0.5);
+//     h0 = 1.;
+//     Wx = 0.003;
+//     Wh = 0.006;
+//     b  = 0.5;
+
+//     const OpArgsHolder<double> argsHolderFF({&x, &h0, &Wx, &Wh, &b}, {}, {});
+//     const OpArgsHolder<double> argsHolderBP({&x, &h0, &Wx, &Wh, &b, &dLdh}, {}, {});    
+
+//     nd4j::ops::gru<double> opFF;
+//     nd4j::ops::gru_bp<double> opBP;
+
+//     const bool isGradCorrect = GradCheck::checkGrad(opFF, opBP, argsHolderFF, argsHolderBP);
+
+//     ASSERT_TRUE(isGradCorrect);
+// }
