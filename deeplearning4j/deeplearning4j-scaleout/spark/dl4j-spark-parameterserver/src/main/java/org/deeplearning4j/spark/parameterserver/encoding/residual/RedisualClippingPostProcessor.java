ff8e68a6195402c280b3a25dbab21da4f363419c
Alex Black <blacka101@gmail.com>
2018-10-20 16:20:29 +1100

    First steps for threshold/residual interfaces

---
+++ deeplearning4j/deeplearning4j-scaleout/spark/dl4j-spark-parameterserver/src/main/java/org/deeplearning4j/spark/parameterserver/encoding/residual/RedisualClippingPostProcessor.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.deeplearning4j.spark.parameterserver.encoding.residual;
+
+import lombok.AllArgsConstructor;
+import org.deeplearning4j.spark.parameterserver.encoding.ResidualPostProcessor;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.indexing.BooleanIndexing;
+import org.nd4j.linalg.indexing.conditions.Conditions;
+
+@AllArgsConstructor
+public class RedisualClippingPostProcessor implements ResidualPostProcessor {
+
+    private final double thresholdMultipleClipValue;
+
+    @Override
+    public void processResidual(int iteration, int epoch, double lastThreshold, INDArray residualVector) {
+
+        double currClip = lastThreshold * thresholdMultipleClipValue;
+        //TODO replace with single op once we have this
+        BooleanIndexing.replaceWhere(residualVector, currClip, Conditions.greaterThan(currClip));
+        BooleanIndexing.replaceWhere(residualVector, -currClip, Conditions.lessThan(-currClip));
+    }
+}
