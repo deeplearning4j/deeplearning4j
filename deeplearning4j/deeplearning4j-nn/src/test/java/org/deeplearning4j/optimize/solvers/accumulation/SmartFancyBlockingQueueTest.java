405a5b804d7cb896f1d93088bc875119067a3411
raver119 <raver119@gmail.com>
2018-10-23 21:37:37 +0300

    one more test

--- deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/SmartFancyBlockingQueueTest.java
+++ deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/SmartFancyBlockingQueueTest.java
@@ -18,12 +18,15 @@ package org.deeplearning4j.optimize.solvers.accumulation;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.RandomUtils;
 import org.deeplearning4j.util.ThreadUtils;
 import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.util.ArrayList;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.Assert.*;
@@ -133,7 +136,7 @@ public class SmartFancyBlockingQueueTest {
             public void run() {
                 while (true) {
                     queue.registerConsumers(4);
-                    ThreadUtils.uncheckedSleep(100);
+                    ThreadUtils.uncheckedSleep(30);
                 }
             }
         });
@@ -149,7 +152,7 @@ public class SmartFancyBlockingQueueTest {
                     for (int e = 0; e <250; e++) {
                         try {
                             queue.put(Nd4j.createUninitialized(5, 5).assign(e));
-                            Thread.sleep(100);
+                            Thread.sleep(30);
                         } catch (Exception ex) {
                             throw new RuntimeException(ex);
                         }
@@ -165,6 +168,73 @@ public class SmartFancyBlockingQueueTest {
             t.join();
 
         for (val t: threads)
+            t.join();
+    }
+
+    @Test
+    public void testSFBQ_4() throws Exception {
+        final val queue = new SmartFancyBlockingQueue(16, Nd4j.create(5, 5));
+        final val barrier = new CyclicBarrier(4);
+/*
+        val m = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    queue.registerConsumers(4);
+                    ThreadUtils.uncheckedSleep(100);
+                }
+            }
+        });
+
+
+        m.setName("master thread");
+        m.setDaemon(true);
+        m.start();
+*/
+
+        val threads = new ArrayList<Thread>();
+        for (int e = 0; e < 4; e++) {
+            val f= e;
+            val t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        for (int e = 0; e < 100; e++) {
+
+                            log.info("[Thread {}]: fill phase {}", f, e);
+                            val numUpdates = RandomUtils.nextInt(8, 128);
+                            for (int p = 0; p < numUpdates; p++) {
+                                queue.put(Nd4j.createUninitialized(5, 5));
+                            }
+
+                            if (f == 0)
+                                queue.registerConsumers(4);
+
+                            barrier.await();
+                            log.info("[Thread {}]: read phase {}", f, e);
+                            while (!queue.isEmpty()) {
+                                val arr = queue.poll();
+
+                                assertNotNull(arr);
+                            }
+
+                            barrier.await();
+
+                        }
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    } catch (BrokenBarrierException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+            t.setName("worker thread " + f);
+            t.start();
+            threads.add(t);
+        }
+
+        for (val t:threads)
             t.join();
     }
 }
