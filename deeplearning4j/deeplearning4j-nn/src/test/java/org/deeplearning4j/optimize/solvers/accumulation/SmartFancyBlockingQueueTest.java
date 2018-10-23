5cfec7683cd445d459c8a12cbd6dc61a894e9261
raver119 <raver119@gmail.com>
2018-10-23 13:15:45 +0300

    special test

--- deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/SmartFancyBlockingQueueTest.java
+++ deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/SmartFancyBlockingQueueTest.java
@@ -22,6 +22,7 @@ import org.junit.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.Assert.*;
@@ -45,5 +46,49 @@ public class SmartFancyBlockingQueueTest {
         }
 
         assertEquals(1, queue.size());
+    }
+
+    @Test
+    public void testSFBQ_2() throws Exception {
+        final val queue = new SmartFancyBlockingQueue(8, Nd4j.create(5, 5));
+
+        val threads = new ArrayList<Thread>();
+        for (int e = 0; e< 4; e++) {
+            val f = e;
+            val t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int cnt = 0;
+                    while (true) {
+                        while (cnt < 1000) {
+                            log.info("Thread {}: [{}]", f, cnt);
+                            if (!queue.isEmpty()) {
+                                val arr = queue.poll();
+
+                                assertNotNull(arr);
+                                val local = arr.unsafeDuplication(true);
+
+                                assertEquals(cnt, local.meanNumber().intValue());
+                                cnt++;
+                            }
+                        }
+                        break;
+                    }
+
+
+                }
+            });
+            t.start();
+            threads.add(t);
+        }
+
+        for (int e = 0; e < 1000; e++) {
+            queue.put(Nd4j.create(5, 5).assign(e));
+            queue.registerConsumers(4);
+        }
+
+
+        for (val t: threads)
+            t.join();
     }
 }
