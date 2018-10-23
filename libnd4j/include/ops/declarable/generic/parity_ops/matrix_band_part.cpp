8f9837df83accbb84880f8d3f8b58ebd45809d03
shugeo <sgazeos@gmail.com>
2018-10-23 12:33:59 +0300

    Fixed matrix_band helper.

--- libnd4j/include/ops/declarable/generic/parity_ops/matrix_band_part.cpp
+++ libnd4j/include/ops/declarable/generic/parity_ops/matrix_band_part.cpp
@@ -15,7 +15,7 @@
  ******************************************************************************/
 
 //
-// @author George A. Shulinok <sgazeos@gmail.com>, created on 8/22/2018
+// @author GS <sgazeos@gmail.com>, created on 8/22/2018
 //
 
 #include <op_boilerplate.h>
@@ -45,6 +45,13 @@ namespace nd4j {
             return ND4J_STATUS_OK;
         }
         DECLARE_SYN(band_part, matrix_band_part);
+    }
+
+    DECLARE_TYPES(matrix_band_part) {
+        getOpDescriptor()
+            ->setAllowedInputTypes(nd4j::DataType::ANY)
+            ->setAllowedInputTypes({ALL_FLOATS})
+            ->setSameMode(true);
     }
 }
 
