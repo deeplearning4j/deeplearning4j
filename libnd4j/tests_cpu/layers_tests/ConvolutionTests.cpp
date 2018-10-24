0e46c2129cba811f01e26ecafb6d9b9cc17d68a0
shugeo <sgazeos@gmail.com>
2018-10-24 11:33:31 +0300

    Fixed multityped conv2d test.

--- libnd4j/tests_cpu/layers_tests/ConvolutionTests.cpp
+++ libnd4j/tests_cpu/layers_tests/ConvolutionTests.cpp
@@ -52,7 +52,8 @@ TYPED_TEST(TypedConvolutionTests, TestConv2D_1) {
 
     int bS=1, iH=5,iW=4,  iC=2,oC=3,  kH=2,kW=2,  sH=1,sW=1,  pH=0,pW=0,  dH=1,dW=1;   
     TypeParam _expB[]{664.0, 700.0, 736.0, 344.0, 808.0, 844.0, 880.0, 408.0, 952.0, 988.0, 1024.0, 472.0, 1096.0, 1132.0, 1168.0, 536.0, 466.0, 480.0, 494.0, 220.0, 1528.0, 1628.0, 1728.0, 856.0, 1928.0, 2028.0, 2128.0, 1048.0, 2328.0, 2428.0, 2528.0, 1240.0, 2728.0, 2828.0, 2928.0, 1432.0, 1346.0, 1392.0, 1438.0, 700.0, 2392.0, 2556.0, 2720.0, 1368.0, 3048.0, 3212.0, 3376.0, 1688.0, 3704.0, 3868.0, 4032.0, 2008.0, 4360.0, 4524.0, 4688.0, 2328.0, 2226.0, 2304.0, 2382.0, 1180.0};
-    Nd4jLong _expS[]{4, 1, 3, 5, 4, 60, 20, 4, 1, 16384, 1, 99};
+//    Nd4jLong _expS[]{4, 1, 3, 5, 4, 60, 20, 4, 1, 16384, 1, 99};
+    Nd4jLong _expS[]{4, 1, 3, 5, 4, 60, 20, 4, 1, 8192, 1, 99};
     auto input = NDArrayFactory::create_<TypeParam>('c', {bS, iC, iH, iW});
     auto weights = NDArrayFactory::create_<TypeParam>('c', {oC, iC, kH, kW});
 
@@ -72,7 +73,6 @@ TYPED_TEST(TypedConvolutionTests, TestConv2D_1) {
 
     auto block = new Context(1, variableSpace, false);  // not-in-place
     block->fillInputs({-1, -2});
-
     // 5,5 kernel
     block->getIArguments()->push_back(kH);
     block->getIArguments()->push_back(kW);
@@ -114,7 +114,7 @@ TYPED_TEST(TypedConvolutionTests, TestConv2D_1) {
     // just for visual validation
     //exp->printBuffer("Expected");
     //res->printBuffer("Actual  ");
-    //res->printShapeInfo("Result shape");
+    res->printShapeInfo("Result shape");
     // final check
     ASSERT_TRUE(res->equalsTo(exp));
 
