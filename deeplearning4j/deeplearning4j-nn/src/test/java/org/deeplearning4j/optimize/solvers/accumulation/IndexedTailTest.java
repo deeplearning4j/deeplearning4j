c19c1eb6b465c9df53b10d93c139f7a92651541d
raver119 <raver119@gmail.com>
2018-10-25 08:14:04 +0000

    next step

--- deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/IndexedTailTest.java
+++ deeplearning4j/deeplearning4j-nn/src/test/java/org/deeplearning4j/optimize/solvers/accumulation/IndexedTailTest.java
@@ -2,6 +2,7 @@ package org.deeplearning4j.optimize.solvers.accumulation;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -27,6 +28,33 @@ public class IndexedTailTest {
             assertTrue(status);
             assertArrayEquals(orig.shape(), temp.shape());
             assertEquals(orig, temp);
+        }
+
+        assertEquals(0, tail.updatesSize());
+    }
+
+    @Test
+    public void testSingleThreaded_2() throws Exception {
+        val tail = new IndexedTail(1);
+
+        for (int e = 0; e < 100; e++) {
+            int numUpdates = RandomUtils.nextInt(1, 10);
+            int sum = 0;
+
+            for (int f = 1; f <= numUpdates; f++) {
+                sum += f;
+                val orig = Nd4j.create(5, 5).assign(f);
+                tail.put(orig);
+            }
+            Nd4j.getExecutioner().commit();
+
+            assertTrue(tail.hasAynthing());
+
+            val temp = Nd4j.create(5, 5);
+            val status = tail.drainTo(temp);
+
+            assertTrue(status);
+            assertEquals(sum, temp.meanNumber().intValue());
         }
 
         assertEquals(0, tail.updatesSize());
