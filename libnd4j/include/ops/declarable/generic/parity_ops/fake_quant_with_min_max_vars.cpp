b276ef44f51baee43127edca06a458cab71af329
shugeo <sgazeos@gmail.com>
2018-11-14 14:56:58 +0200

    Added helper for fake_quant_with_min_max_vars op.

--- libnd4j/include/ops/declarable/generic/parity_ops/fake_quant_with_min_max_vars.cpp
+++ libnd4j/include/ops/declarable/generic/parity_ops/fake_quant_with_min_max_vars.cpp
@@ -22,7 +22,7 @@
 #if NOT_EXCLUDED(OP_fake_quant_with_min_max_vars)
 
 #include <ops/declarable/CustomOperations.h>
-
+#include <ops/declarable/helpers/fake_quantization.h>
 namespace nd4j {
     namespace ops {
         CONFIGURABLE_OP_IMPL(fake_quant_with_min_max_vars, 3, 1, true, 0, 2) {
@@ -38,7 +38,7 @@ namespace nd4j {
                 numBits = INT_ARG(0);
                 REQUIRE_TRUE(numBits > 1 && numBits < 17, 0, "fake_quant_with_min_max_vars: Number of bits for quatization should be in between 2 and 16, but %i was given.", numBits);
             }
-            //helpers::fakeQuantWithMinMaxVars(x, min, max, numBits, narrowed, output);
+            helpers::fakeQuantWithMinMaxVars(x, min->getScalar(0), max->getScalar(0), numBits, narrowed, output);
             return ND4J_STATUS_OK;
         }
         DECLARE_SYN(Zeta, zeta);
---
+++ libnd4j/include/ops/declarable/helpers/cpu/fake_quantization.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+//
+//  @author sgazeos@gmail.com
+//
+
+#include <ops/declarable/helpers/fake_quantization.h>
+
+namespace nd4j {
+namespace ops {
+namespace helpers {
+
+    template <typename T>
+    void fakeQuantWithMinMaxVars(NDArray<T>* input, T min, T max, int numBits, bool narrowed, NDArray<T>* output) {
+        int lowIntBound = narrowed?1:0;
+        int upperIntBound = 1 << numBits - 1;
+        
+    }
+
+    template void fakeQuantWithMinMaxVars(NDArray<float>* input, float min, float max, int numBits, bool narrowed, NDArray<float>* output);
+    template void fakeQuantWithMinMaxVars(NDArray<float16>* input, float16 min, float16 max, int numBits, bool narrowed, NDArray<float16>* output);
+    template void fakeQuantWithMinMaxVars(NDArray<double>* input, double min, double max, int numBits, bool narrowed, NDArray<double>* output);
+}
+}
+}
