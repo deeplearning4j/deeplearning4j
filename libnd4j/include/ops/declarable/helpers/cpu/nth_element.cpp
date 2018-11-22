7bf252a048a32d4acfc1aff3b506a5d840014b3c
shugeo <sgazeos@gmail.com>
2018-11-22 12:18:54 +0200

    Fixed nth_element to eliminate warning.

--- libnd4j/include/ops/declarable/helpers/cpu/nth_element.cpp
+++ libnd4j/include/ops/declarable/helpers/cpu/nth_element.cpp
@@ -27,16 +27,17 @@ namespace helpers {
     template <typename T>
     void nthElementFunctor_(NDArray* input, NDArray* nVal, NDArray* output) {
         Nd4jLong n = nVal->e<Nd4jLong>(0);
+        // TODO: fix using std::nth_element to use typename T instead float or avoid use double with this op.
         if (input->isVector()) {
-            std::vector<T> data(input->lengthOf());
+            std::vector<float> data(input->lengthOf());
             //memcpy(&data[0], input->getBuffer(), sizeof(T) * data.size());
             size_t l = 0;
             for (size_t l = 0; l < data.size(); ++l)
-                data[l] = input->e<T>(l);
+                data[l] = input->e<float>(l);
             auto nthPos = data.begin();
             nthPos += n;
             std::nth_element(data.begin(), nthPos, data.end());
-            output->p<T>(0, data[n]);
+            output->p<float>(0, data[n]);
         }
         else { // rank greater than 1
             std::vector<int> lastDims({input->rankOf() - 1});
@@ -44,16 +45,16 @@ namespace helpers {
 #pragma omp parallel for
             for (Nd4jLong e = 0; e < output->lengthOf(); e++) {
                 auto row = rows->at(e);
-                std::vector<T> internalData(row->lengthOf());
+                std::vector<float> internalData(row->lengthOf());
                 //T* internalP = const_cast<T*>(internalData.data());
                 for (size_t l = 0; l < internalData.size(); ++l)
-                    internalData[l] = row->e<T>(l);
+                    internalData[l] = row->e<float>(l);
 
                 //memcpy(&internalData[0], row->getBuffer(), sizeof(T) * internalData.size());
                 auto nthPos = internalData.begin();
                 nthPos += n;
                 std::nth_element(internalData.begin(), nthPos, internalData.end());
-                output->p<T>(e, internalData[n]);
+                output->p<float>(e, internalData[n]);
             }
         }
     }
