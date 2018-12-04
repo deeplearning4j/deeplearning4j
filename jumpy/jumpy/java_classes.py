605c40da7afe889a544b17f375c76b4a8a8da5e7
Fariz Rahman <farizrahman4u@gmail.com>
2018-12-05 01:56:53 +0530

    jp updates

--- jumpy/jumpy/java_classes.py
+++ jumpy/jumpy/java_classes.py
@@ -32,6 +32,7 @@ INDArray = autoclass('org.nd4j.linalg.api.ndarray.INDArray')
 Transforms = autoclass('org.nd4j.linalg.ops.transforms.Transforms')
 NDArrayIndex = autoclass('org.nd4j.linalg.indexing.NDArrayIndex')
 DataBuffer = autoclass('org.nd4j.linalg.api.buffer.DataBuffer')
+DataType = jnius.autoclass('org.nd4j.linalg.api.buffer.DataBuffer$Type')
 System = autoclass('java.lang.System')
 Integer = autoclass('java.lang.Integer')
 Float = autoclass('java.lang.Float')
@@ -46,9 +47,15 @@ DataTypeUtil = autoclass('org.nd4j.linalg.api.buffer.util.DataTypeUtil')
 MemoryManager = autoclass('org.nd4j.linalg.memory.MemoryManager')
 SameDiff = autoclass('org.nd4j.autodiff.samediff.SameDiff')
 TFGraphMapper = autoclass('org.nd4j.imports.graphmapper.tf.TFGraphMapper')
+DataSet = autoclass('org.nd4j.linalg.dataset.DataSet')
+ArrayList = autoclass('java.util.ArrayList')
+
 
 try:
     KerasModelImport = autoclass('org.deeplearning4j.nn.modelimport.keras.KerasModelImport')
+    ArrayDescriptor = autoclass('org.deeplearning4j.spark.parameterserver.python.ArrayDescriptor')
+    DataSetDescriptor = autoclass('org.deeplearning4j.spark.parameterserver.python.DataSetDescriptor')
+    spark_utils = autoclass('org.deeplearning4j.spark.parameterserver.python.Utils')
 except Exception as e:
-    warnings.warn('Dl4J Core not installed. Keras model import not available.')
+    warnings.warn('Dl4J Core not installed. Keras model import and pyspark interop not available.')
     KerasModelImport = None
---
+++ jumpy/jumpy/spark/fast_impl.py
@@ -0,0 +1,86 @@
+################################################################################
+# Copyright (c) 2015-2018 Skymind, Inc.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+################################################################################
+
+import numpy as np
+from ..java_classes import ArrayList
+from ..java_classes import ArrayDescriptor
+from ..java_classes import DataSetDescriptor
+from ..java_classes import DataType
+from ..java_classes import spark_utils
+from ..java_classes import DataSet
+from ..ndarray import array
+
+
+def java2pyRDD(java_rdd, py_sc):
+    '''
+    Arguments
+
+    `java_rdd`: JavaRDD<INDArray> instance
+    `py_sc`: Pyspark context instance
+
+    Returns
+
+    pyspark.RDD instance
+    '''
+    desc_rdd = spark_utils.getArrayDescriptorRDD(java_rdd)
+    descriptors = desc_rdd.collect()
+    num_descriptors = descriptors.size()
+    nparrays = []
+    for i in range(num_descriptors):
+        desc = descriptors.get(i)
+        indarray = desc.getArray()
+        jparray = array(indarray)
+        nparray = jparray.numpy()
+        nparrays.append(nparray)
+    return py_sc.parallelize(nparrays)
+
+
+def py2javaRDD(py_rdd, java_sc):
+    '''
+    Arguments
+
+    `py_rdd`: pyspark.RDD instance
+    `java_sc`: JavaSparkContext instance
+
+    Returns
+
+    JavaRDD<INDArray> instance
+    '''
+    def np2desc(nparray):
+        address = nparray.__array_interface__['data'][0]
+        shape = nparray.shape
+        stride = nparray.strides
+        nptype = nparray.dtype
+        if nptype == np.float32:
+            dtype = "float"
+        elif nptype == np.float64:
+            dtype = "double"
+        else:
+            raise Exception("Unsupported data type: " + str(nptype))
+        return (address, shape, stride, dtype)
+    
+    dtype_map = {
+        "float": DataType.FLOAT,
+        "double": DataType.DOUBLE
+    }
+    desc_rdd = py_rdd.map(np2desc)
+    descriptors = desc_rdd.collect()
+    arrlist = ArrayList()
+    for d in descriptors:
+        arrlist.add(ArrayDescriptor(d[0], d[1], d[2], dtype_map[d[3]]))
+    java_rdd = java_sc.parallelize(arrlist)
+    java_rdd = spark_utils.getArrayRDD(java_rdd)
+    return java_rdd
