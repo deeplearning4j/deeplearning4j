1b7cc9e6dfdad825d51a2d0307fb1699c2405221
raver119 <raver119@gmail.com>
2018-12-12 12:56:23 +0300

    meh

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests11.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests11.cpp
@@ -38,6 +38,9 @@ public:
 };
 
 TEST_F(DeclarableOpsTests11, test_mixed_biasadd_1) {
+    if (!Environment::getInstance()->isExperimentalBuild())
+        return;
+
     auto x = NDArrayFactory::create<double>('c', {2, 3});
     auto y = NDArrayFactory::create<float>('c', {3}, {1.f, 2.f, 3.f});
     auto z = NDArrayFactory::create<float>('c', {2, 3});
