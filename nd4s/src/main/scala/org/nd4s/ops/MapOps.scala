6531f22133fc4a451ad49d614cd1ec62889858b8
Max Pumperla <max.pumperla@googlemail.com>
2018-12-18 13:11:47 +0100

    fix ops

--- nd4s/src/main/scala/org/nd4s/ops/MapOps.scala
+++ nd4s/src/main/scala/org/nd4s/ops/MapOps.scala
@@ -1,34 +1,34 @@
 /*******************************************************************************
- * Copyright (c) 2015-2018 Skymind, Inc.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
+  * Copyright (c) 2015-2018 Skymind, Inc.
+  *
+  * This program and the accompanying materials are made available under the
+  * terms of the Apache License, Version 2.0 which is available at
+  * https://www.apache.org/licenses/LICENSE-2.0.
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations
+  * under the License.
+  *
+  * SPDX-License-Identifier: Apache-2.0
  ******************************************************************************/
-
 package org.nd4s.ops
 
 import org.nd4j.autodiff.samediff.SDVariable
-import org.nd4j.linalg.api.complex.IComplexNumber
 import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.api.ops.{BaseScalarOp, BaseOp, Op}
+import org.nd4j.linalg.api.ops.BaseScalarOp
 import org.nd4s.Implicits._
 
-object MapOps{
-  def apply(x:INDArray,f:Double=>Double,g:IComplexNumber =>IComplexNumber):MapOps = new MapOps(x,f,g)
+object MapOps {
+  def apply(x: INDArray, f: Double => Double): MapOps = new MapOps(x, f)
 }
-class MapOps(_x:INDArray,f:Double => Double, g:IComplexNumber => IComplexNumber) extends BaseScalarOp(_x,null,_x,_x.length(),0) with LeftAssociativeBinaryOp {
+class MapOps(_x: INDArray, f: Double => Double)
+    extends BaseScalarOp(_x, null, _x, _x.length().toInt, 0)
+    with LeftAssociativeBinaryOp {
   x = _x
-  def this(){
-    this(0.toScalar,null,null)
+  def this() {
+    this(0.toScalar, null)
   }
 
   override def opNum(): Int = -1
@@ -37,9 +37,11 @@ class MapOps(_x:INDArray,f:Double => Double, g:IComplexNumber => IComplexNumber)
 
   override def onnxName(): String = throw new UnsupportedOperationException
 
-  override def tensorflowName(): String = throw new UnsupportedOperationException
+  override def tensorflowName(): String =
+    throw new UnsupportedOperationException
 
-  override def doDiff(f1: java.util.List[SDVariable]): java.util.List[SDVariable] = throw new UnsupportedOperationException
+  override def doDiff(f1: java.util.List[SDVariable]): java.util.List[SDVariable] =
+    throw new UnsupportedOperationException
 
 //  override def opForDimension(index: Int, dimension: Int): Op = MapOps(x.tensorAlongDimension(index,dimension),f,g)
 //
@@ -49,5 +51,4 @@ class MapOps(_x:INDArray,f:Double => Double, g:IComplexNumber => IComplexNumber)
 
   override def op(origin: Float): Float = f(origin).toFloat
 
-  override def op(origin: IComplexNumber): IComplexNumber = g(origin)
 }
