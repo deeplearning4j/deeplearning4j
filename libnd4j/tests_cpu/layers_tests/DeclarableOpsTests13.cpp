5a9d983c9974779ca34531d473adbc974bbeeacf
raver119 <raver119@gmail.com>
2019-01-07 09:15:18 +0300

    Few more fixes (#6944)
    * broadcastable pow
    
    * one range test
    
    * few more range tests
    
    * listdiff now accepts all int types for indices + test
    
    * or/xor/and tests

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests13.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests13.cpp
@@ -37,3 +37,105 @@ public:
         fflush(stdout);
     }
 };
+
+TEST_F(DeclarableOpsTests13, test_pow_1) {
+    auto x = NDArrayFactory::create<float>('c', {2, 2}, {2.f, 2.f, 2.f, 2.f});
+    auto y = NDArrayFactory::create<int>('c', {2}, {3, 3});
+    auto e = NDArrayFactory::create<float>('c', {2, 2}, {8.f, 8.f, 8.f, 8.f});
+
+    nd4j::ops::Pow op;
+    auto result = op.execute({&x, &y}, {}, {});
+    ASSERT_EQ(Status::OK(), result->status());
+
+    auto z = result->at(0);
+
+    ASSERT_EQ(e, *z);
+
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests13, test_empty_range_1) {
+    auto start = NDArrayFactory::create<int>(0);
+    auto limit = NDArrayFactory::create<int>(0);
+
+    nd4j::ops::range op;
+    auto result = op.execute({&start, &limit}, {}, {});
+    ASSERT_EQ(Status::OK(), result->status());
+
+    auto z = result->at(0);
+    ASSERT_TRUE(z->isEmpty());
+
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests13, test_empty_range_2) {
+
+    nd4j::ops::range op;
+    auto result = op.execute({}, {1.0, 1.0}, {});
+    ASSERT_EQ(Status::OK(), result->status());
+
+    auto z = result->at(0);
+    ASSERT_TRUE(z->isEmpty());
+
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests13, test_empty_range_3) {
+
+    nd4j::ops::range op;
+    auto result = op.execute({}, {}, {1, 1});
+    ASSERT_EQ(Status::OK(), result->status());
+
+    auto z = result->at(0);
+    ASSERT_TRUE(z->isEmpty());
+
+    delete result;
+}
+
+TEST_F(DeclarableOpsTests13, test_listdiff_1) {
+    auto x = NDArrayFactory::create<int>('c', {4}, {0, 1, 2, 3});
+    auto y = NDArrayFactory::create<int>('c', {2}, {3, 1});
+
+    auto od = NDArrayFactory::create<int>('c', {2});
+    auto oi = NDArrayFactory::create<int>('c', {2});
+
+    nd4j::ops::listdiff op;
+    auto result = op.execute({&x, &y}, {&od, &oi}, {}, {}, {});
+    ASSERT_EQ(Status::OK(), result);
+}
+
+TEST_F(DeclarableOpsTests13, test_or_1) {
+    auto x = NDArrayFactory::create<bool>('c', {4}, {false, true, false, true});
+    auto y = NDArrayFactory::create<bool>('c', {4}, {false, false, true, true});
+    auto e = NDArrayFactory::create<bool>('c', {4}, {false, true, true, true});
+
+    auto z = NDArrayFactory::create<bool>('c', {4});
+
+    x.applyPairwiseTransform(pairwise::Or, &y, &z, nullptr);
+
+    ASSERT_EQ(e, z);
+}
+
+TEST_F(DeclarableOpsTests13, test_and_1) {
+    auto x = NDArrayFactory::create<bool>('c', {4}, {false, true, false, true});
+    auto y = NDArrayFactory::create<bool>('c', {4}, {false, false, true, true});
+    auto e = NDArrayFactory::create<bool>('c', {4}, {false, false, false, true});
+
+    auto z = NDArrayFactory::create<bool>('c', {4});
+
+    x.applyPairwiseTransform(pairwise::And, &y, &z, nullptr);
+
+    ASSERT_EQ(e, z);
+}
+
+TEST_F(DeclarableOpsTests13, test_xor_1) {
+    auto x = NDArrayFactory::create<bool>('c', {4}, {false, true, false, true});
+    auto y = NDArrayFactory::create<bool>('c', {4}, {false, false, true, true});
+    auto e = NDArrayFactory::create<bool>('c', {4}, {false, true, true, false});
+
+    auto z = NDArrayFactory::create<bool>('c', {4});
+
+    x.applyPairwiseTransform(pairwise::Xor, &y, &z, nullptr);
+
+    ASSERT_EQ(e, z);
+}
