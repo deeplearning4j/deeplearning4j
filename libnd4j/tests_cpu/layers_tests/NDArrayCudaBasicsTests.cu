78bf3031bdee88cfc43be7d38ab04ec7f0af6677
shugeo <sgazeos@gmail.com>
2019-01-11 20:10:56 +0200

    Added test for further work.

--- libnd4j/tests_cpu/layers_tests/NDArrayCudaBasicsTests.cu
+++ libnd4j/tests_cpu/layers_tests/NDArrayCudaBasicsTests.cu
@@ -1892,6 +1892,31 @@ TEST_F(NDArrayCudaBasicsTests, TestFloat16_3) {
     ASSERT_TRUE(x.equalsTo(&y));
 }
 
+//////////////////////////////////////////////////////////////////////
+TEST_F(NDArrayCudaBasicsTests, Operator_Plus_Test_5)
+{
+    auto x = NDArrayFactory::create<float>('c', {8, 8, 8});
+    auto y = NDArrayFactory::create<float>('c', {8, 1, 8});
+    auto expected = NDArrayFactory::create<float>('c', {8, 8, 8});
+    NDArray res2(expected);
+    x = 1.;
+    y = 2.;
+    expected = 3.;
+    x.printBuffer("X=");
+    y.printBuffer("Y=");
+    expected.printBuffer("EXPECTED");
+    auto result = x + y;
+    result.printBuffer("1 + 2 =");
+    res2.assign(x + y);
+
+    //x.applyTrueBroadcast(BroadcastOpsTuple::Add(), &y, &res2);
+    res2.printBuffer("Z=");
+    x += y;
+    x.printIndexedBuffer("OUtputX");
+    ASSERT_TRUE(expected.isSameShape(&result));
+    ASSERT_TRUE(expected.equalsTo(&result));
+}
+
 // printCudaGlobal<double><<<1,1,0,*stream>>>(dX, 6);
 //     printCudaGlobal<Nd4jLong><<<1,1,0,*stream>>>(dXShapeInfo, 8);
 //     printCudaGlobal<double><<<1,1,0,*stream>>>(dZ, 2);
