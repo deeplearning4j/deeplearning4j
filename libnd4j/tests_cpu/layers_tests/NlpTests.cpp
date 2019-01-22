db1ae06fb2e9b35d7e0b6e6bb4f65477a1f79868
raver119 <raver119@gmail.com>
2019-01-22 20:46:15 +0300

    couple of tests

--- libnd4j/tests_cpu/layers_tests/NlpTests.cpp
+++ libnd4j/tests_cpu/layers_tests/NlpTests.cpp
@@ -37,7 +37,7 @@ public:
     }
 };
 
-TEST_F(NlpTests, basic_sg_test_1) {
+TEST_F(NlpTests, basic_sg_hs_test_1) {
     auto target = NDArrayFactory::create<int>(0);
     auto ngStarter = NDArrayFactory::empty<int>();
     auto indices = NDArrayFactory::create<int>('c', {5}, {1, 2, 3, 4, 5});
@@ -47,6 +47,48 @@ TEST_F(NlpTests, basic_sg_test_1) {
     auto syn1Neg = NDArrayFactory::empty<float>();
     auto expTable = NDArrayFactory::create<float>('c', {1000});
     auto negTable = NDArrayFactory::empty<float>();
+
+    auto alpha = NDArrayFactory::create<double>(1.25);
+    auto randomValue = NDArrayFactory::create<Nd4jLong>(119L);
+    auto inferenceVector = NDArrayFactory::empty<float>();
+
+    nd4j::ops::skipgram op;
+    auto result = *op.execute({&target, &ngStarter, &indices, &codes, &syn0, &syn1, &syn1Neg, &expTable, &negTable, &alpha, &randomValue, &inferenceVector}, {}, {}, {false});
+    ASSERT_EQ(Status::OK(), result.status());
+}
+
+TEST_F(NlpTests, basic_sg_hs_ns_test_1) {
+    auto target = NDArrayFactory::create<int>(0);
+    auto ngStarter = NDArrayFactory::create<int>(1);
+    auto indices = NDArrayFactory::create<int>('c', {5}, {1, 2, 3, 4, 5});
+    auto codes = NDArrayFactory::create<int8_t>('c', {5}, {1, 1, 0, 1, 1});
+    auto syn0 = NDArrayFactory::create<float>('c', {100, 150});
+    auto syn1 = NDArrayFactory::create<float>('c', {100, 150});
+    auto syn1Neg = NDArrayFactory::create<float>('c', {100, 150});
+    auto expTable = NDArrayFactory::create<float>('c', {1000});
+    auto negTable = NDArrayFactory::create<float>('c', {1000});
+    negTable.linspace(1.0);
+
+    auto alpha = NDArrayFactory::create<double>(1.25);
+    auto randomValue = NDArrayFactory::create<Nd4jLong>(119L);
+    auto inferenceVector = NDArrayFactory::empty<float>();
+
+    nd4j::ops::skipgram op;
+    auto result = *op.execute({&target, &ngStarter, &indices, &codes, &syn0, &syn1, &syn1Neg, &expTable, &negTable, &alpha, &randomValue, &inferenceVector}, {}, {}, {false});
+    ASSERT_EQ(Status::OK(), result.status());
+}
+
+TEST_F(NlpTests, basic_sg_ns_test_1) {
+    auto target = NDArrayFactory::create<int>(0);
+    auto ngStarter = NDArrayFactory::create<int>(1);
+    auto indices = NDArrayFactory::empty<int>();
+    auto codes = NDArrayFactory::empty<int8_t>();
+    auto syn0 = NDArrayFactory::create<float>('c', {100, 150});
+    auto syn1 = NDArrayFactory::empty<float>();
+    auto syn1Neg = NDArrayFactory::create<float>('c', {100, 150});
+    auto expTable = NDArrayFactory::empty<float>();
+    auto negTable = NDArrayFactory::create<float>('c', {1000});
+    negTable.linspace(1.0);
 
     auto alpha = NDArrayFactory::create<double>(1.25);
     auto randomValue = NDArrayFactory::create<Nd4jLong>(119L);
