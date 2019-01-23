efc529a23ab7bab42fef7ecd47d39fe4973a7fd8
raver119 <raver119@gmail.com>
2019-01-23 16:13:42 +0300

    minor tweaks

--- deeplearning4j/deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/serialization/ExtVocabWord.java
+++ deeplearning4j/deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/models/sequencevectors/serialization/ExtVocabWord.java
@@ -1,5 +1,6 @@
 package org.deeplearning4j.models.sequencevectors.serialization;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.deeplearning4j.models.word2vec.VocabWord;
@@ -9,6 +10,7 @@ import org.nd4j.shade.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE,
         setterVisibility = JsonAutoDetect.Visibility.NONE)
+@Data
 public class ExtVocabWord extends VocabWord {
     protected String extString;
     protected long counter;
