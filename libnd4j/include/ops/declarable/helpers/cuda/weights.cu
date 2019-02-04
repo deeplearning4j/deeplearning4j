ac82631b8a88dd779ced87e2c3d73e756d40ecf0
shugeo <sgazeos@gmail.com>
2019-02-04 20:04:05 +0200

    Refactored adjustWeight kernel.

--- libnd4j/include/ops/declarable/helpers/cuda/weights.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/weights.cu
@@ -23,13 +23,49 @@
 namespace nd4j {
 namespace ops {
 namespace helpers {
+
+
+
     template <typename T>
+    static __device__ void adjustWeightsKernelD(void* inputBuffer,   Nd4jLong* inputShape,
+                                               void* weightsBuffer, Nd4jLong* weightsShape,
+                                               void* outputBuffer,  Nd4jLong* outputShape,
+                                               Nd4jLong inputLength, Nd4jLong weightsLength,
+                                               Nd4jLong outputLength, int maxLength) {
+
+        auto tid = blockIdx.x * blockDim.x + threadIdx.x;
+        int threadCount = gridDim.x * blockDim.x;
+//        __shared__ T* outputPart;
+
+        //for (int e = 0; e < inputLength; e++) {
+        for (Nd4jLong e = tid; e < inputLength; e++) {
+            printf("%d %d %d\n", blockIdx.x, blockDim.x, threadIdx.x);
+
+            Nd4jLong xOffset = shape::getIndexOffset(e, inputShape, inputLength);
+            int val = *(reinterpret_cast<int*>(inputBuffer) + xOffset);
+            if (val < maxLength) {
+                Nd4jLong zOffset = shape::getIndexOffset(val, outputShape, outputLength);
+                if (weightsBuffer != nullptr) {
+                    Nd4jLong yOffset = shape::getIndexOffset(e, weightsShape, weightsLength);
+                    reinterpret_cast<T *>(outputBuffer)[0] +=  reinterpret_cast<T *>(weightsBuffer)[yOffset];
+                }
+                else {
+                    //printf("outputBuffer[0] = %d\n", static_cast<int>(*(reinterpret_cast<T *>(outputBuffer))));
+                    reinterpret_cast<T *>(outputBuffer)[0] += T(1); //output->p(val, output->e<T>(val) + 1);
+                    //            printf("outputBuffer[%ld] = %d\n", zOffset, static_cast<int>(*(reinterpret_cast<T *>(outputBuffer) + zOffset)));
+                }
+                //printf("xOffset is %ld, zOffset is %ld\n", xOffset, zOffset);
+            }
+        }
+    }
+
+        template <typename T>
     static __global__ void adjustWeightsKernel(void* inputBuffer,   Nd4jLong* inputShape,
                                                void* weightsBuffer, Nd4jLong* weightsShape,
                                                void* outputBuffer,  Nd4jLong* outputShape,
                                                int minLength, int maxLength) {
 
-        auto tid = blockIdx.x * blockDim.x + threadIdx.x;
+        //auto tid = blockIdx.x * blockDim.x + threadIdx.x; // * blockDim.x; // + threadIdx.x;
         int threadCount = gridDim.x * blockDim.x;
         Nd4jLong inputLength = shape::length(inputShape);
         Nd4jLong weightsLength = 0;
@@ -37,24 +73,19 @@ namespace helpers {
             weightsLength = shape::length(weightsShape);
 
         Nd4jLong outputLength = shape::length(outputShape);
-        for (Nd4jLong e = tid; e < inputLength; e += threadCount) {
-            Nd4jLong xOffset = shape::getIndexOffset(e, inputShape, inputLength);
-            int val = *(reinterpret_cast<int*>(inputBuffer) + xOffset);
-            if (val < maxLength) {
-                Nd4jLong zOffset = shape::getIndexOffset(val, outputShape, outputLength);
-                if (weightsBuffer != nullptr) {
-                    Nd4jLong yOffset = shape::getIndexOffset(e, weightsShape, weightsLength);
-                    *(reinterpret_cast<T *>(outputBuffer) + zOffset) += *(reinterpret_cast<T *>(weightsBuffer) + yOffset);
-                }
-                else {
-                    printf("outputBuffer[%ld] = %d\n", zOffset, static_cast<int>(*(reinterpret_cast<T *>(outputBuffer) + zOffset)));
-                    *(reinterpret_cast<T *>(outputBuffer) + zOffset) += T(1); //output->p(val, output->e<T>(val) + 1);
-                    printf("outputBuffer[%ld] = %d\n", zOffset, static_cast<int>(*(reinterpret_cast<T *>(outputBuffer) + zOffset)));
-                }
-                //printf("xOffset is %ld, zOffset is %ld\n", xOffset, zOffset);
-            }
-        }
+        Nd4jLong borderLen = 1;//outputLength / gridDim.x + outputLength % gridDim.x;
 
+        for (Nd4jLong e = blockIdx.x; e < outputLength; e += threadCount) {
+        //if (blockIdx.x < outputLength) {
+            //if (e + threadCount < outputLength) {
+            Nd4jLong zOffset = shape::getIndexOffset(e, outputShape, outputLength);
+            //printf("%d %d %d\n", blockIdx.x, blockDim.x, threadIdx.x);
+            Nd4jLong borderLen = 1;
+            T* outputBufferZ = reinterpret_cast<T*>(outputBuffer) + zOffset;
+            adjustWeightsKernelD<T>(inputBuffer, inputShape, weightsBuffer, weightsShape, (void*)outputBufferZ, outputShape,
+                                 inputLength, weightsLength, borderLen, maxLength);
+
+        }
     }
 
     template <typename T>
