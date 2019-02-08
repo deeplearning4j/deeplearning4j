0b8de149095c3f33e69e1f69e7bf476ca646beb0
shugeo <sgazeos@gmail.com>
2019-02-08 17:11:11 +0200

    Refactored stack op helper cuda kernel.

--- libnd4j/include/ops/declarable/helpers/cuda/stack.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/stack.cu
@@ -22,6 +22,7 @@
 #include <helpers/ShapeUtils.h>
 #include <array/ResultSet.h>
 #include <cuda_exception.h>
+#include <TAD.h>
 
 namespace nd4j {
 namespace ops {
@@ -46,11 +47,11 @@ namespace helpers {
 //	}
 
 	template <typename T>
-	static __global__ void stackKernel(void** inputList, void** inputShapeList, int inputListLength, Nd4jLong arrLen, void* outputBuffer, Nd4jLong* outputShape) {
+	static __global__ void stackKernel(void** inputList, void** inputShapeList, int inputListLength, Nd4jLong arrLen, void* outputBuffer, Nd4jLong* outputShape) {  //, Nd4jLong* tadShape, Nd4jLong* tadOffsets) {
 
 		__shared__ int arrIdx, blocksPerArr;
 		__shared__ T *x, *z;
-		__shared__ Nd4jLong *zShapeInfo, *xShapeInfo, arrLenPerBlock, start, end;
+		__shared__ Nd4jLong *zShapeInfo, *xShapeInfo, arrLenPerBlock, start, end, offsetZ;
 
 		if (threadIdx.x == 0) {
 
@@ -67,20 +68,23 @@ namespace helpers {
 
 			start = (blockIdx.x % blocksPerArr) * arrLenPerBlock;
 			end   = (start + arrLenPerBlock) > arrLen ? arrLen : (start + arrLenPerBlock);
-
+            offsetZ = arrLen * arrIdx;
 			//if (blockIdx.x == 0)
 			    //printf("Block: [%i]; arrLen: %i\n", blockIdx.x, arrLen);
 			    printf("Block: [%i]; arrIdx: [%i]; start: [%lld]; end: [%lld], arrLen: [%lld], arrLenPerBlock: [%lld]; blocksPerArr: [%i]; \n", blockIdx.x, arrIdx, start, end, arrLen, arrLenPerBlock, blocksPerArr);
 		}
 
 		__syncthreads();
-        //for (Nd4jLong arr = blockIdx.x; arr < inputListLength; arr += gridDim.x) {
-		for (Nd4jLong i = start + threadIdx.x; i < end; i += blockDim.x)
-				z[shape::getIndexOrderOffset(i, zShapeInfo, arrLen,
-											 shape::order(zShapeInfo))] = x[shape::getIndexOrderOffset(i, xShapeInfo,
-																									   arrLen,
-																									   shape::order(
-																											   xShapeInfo))];
+
+//		auto z = reinterpret_cast<T*>(outputBuffer);
+//        for (Nd4jLong arrIdx = blockIdx.x; arrIdx < inputListLength; arrIdx += gridDim.x) {
+//			auto x = reinterpret_cast<T*>(inputList[arrIdx]);
+//			auto xShapeInfo = reinterpret_cast<Nd4jLong*>(inputShapeList[arrIdx]);
+//			printf("Block: [%i]; arrIdx: [%lld]; arrLen: [%lld]; TAD offset: [%lld]\n", blockIdx.x, arrIdx, arrLen, tadOffsets[arrIdx]);
+
+			for (Nd4jLong i = start + threadIdx.x; i < end; i += blockDim.x)
+				z[offsetZ + shape::getIndexOffset(i, outputShape, arrLen)] = x[shape::getIndexOffset(i, xShapeInfo, arrLen)];
+//		}
 
 	}
 	///////////////////////////////////////////////////////////////////
@@ -108,8 +112,27 @@ namespace helpers {
 				inputList[i] = inArrs[i]->getSpecialBuffer();
 				inputShapeList[i] = inArrs[i]->getSpecialShapeInfo();
 			}
+            std::vector<int> axis({dim});
+            shape::TAD tadOutput(outArr->shapeInfo(), axis.data(), axis.size());
+            tadOutput.createTadOnlyShapeInfo();
+            tadOutput.createOffsets();
 
-			cudaError_t cudaResult = cudaMalloc(reinterpret_cast<void **>(&dInBuffers), inputList.size() * sizeof(void*));
+            // prepare input arrays for prepareDataForCuda function
+//            Nd4jLong* dTadShape; //,size_t>> hostData;
+//            Nd4jLong* dTadOffsets;
+
+            // create cuda stream and LaunchContext
+            cudaError_t cudaResult;
+//            cudaResult = cudaMalloc(&dTadShape, shape::shapeInfoByteLength(tadOutput.tadOnlyShapeInfo));
+//            if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: Cannot allocate memory for tads on device", cudaResult);
+//            cudaResult = cudaMemcpy(dTadShape, tadOutput.tadOnlyShapeInfo, shape::shapeInfoByteLength(tadOutput.tadOnlyShapeInfo), cudaMemcpyHostToDevice);
+//            if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: Cannot copy memory block for tads on device", cudaResult);
+//            cudaResult = cudaMalloc(&dTadOffsets, tadOutput.numTads * sizeof(Nd4jLong));
+//            if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: Cannot allocate memory for tad offsets on device", cudaResult);
+//            cudaResult = cudaMemcpy(dTadOffsets, tadOutput.tadOffsets, tadOutput.numTads * sizeof(Nd4jLong), cudaMemcpyHostToDevice);
+//            if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: Cannot copy memory block for tads on device", cudaResult);
+
+			cudaResult = cudaMalloc(reinterpret_cast<void **>(&dInBuffers), inputList.size() * sizeof(void*));
 			if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: cannot allocate global memory on device", cudaResult);
 			cudaResult = cudaMalloc(reinterpret_cast<void **>(&dInShapeInfo), inputShapeList.size() * sizeof(Nd4jLong*));
 			if(cudaResult != 0) throw cuda_exception::build("helpers::stack_: cannot allocate global memory on device", cudaResult);
@@ -117,17 +140,24 @@ namespace helpers {
 			cudaMemcpyAsync(dInBuffers,    inputList.data(),    inputList.size()  * sizeof(void*),       cudaMemcpyHostToDevice, *stream);
 			cudaMemcpyAsync(dInShapeInfo,  inputShapeList.data(),  inputShapeList.size() * sizeof(Nd4jLong*),  cudaMemcpyHostToDevice, *stream);
 
-            dim3 launchDims(256, 512, 8192);
+            dim3 launchDims(inArrs.size(), inArrs[0]->lengthOf(), 1024);
+			//Nd4jLong lastDim = outArr->sizeAt(dim);
 
-            nd4j_printf("length: %i\n", inArrs[0]->lengthOf());
-			stackKernel<T><<<launchDims.x, launchDims.y, launchDims.z, *stream>>>((void**)dInBuffers, (void**)dInShapeInfo, inputList.size(), inArrs[0]->lengthOf(), outArr->specialBuffer(), outArr->specialShapeInfo());
-
+            //nd4j_printf("length: %i\n", inArrs[0]->lengthOf());
+			stackKernel<T><<<launchDims.x, launchDims.y, launchDims.z, *stream>>>((void**)dInBuffers, (void**)dInShapeInfo, inputList.size(), inArrs[0]->lengthOf(), outArr->specialBuffer(), outArr->specialShapeInfo()); //, dTadShape, dTadOffsets);
 			cudaResult = cudaFree(dInBuffers);
 			if(cudaResult != 0)
 				throw cuda_exception::build("helpers::stack_: cannot deallocate global memory on device for buffer list", cudaResult);
 			cudaResult = cudaFree(dInShapeInfo);
 			if(cudaResult != 0)
 				throw cuda_exception::build("helpers::stack_: cannot deallocate global memory on device for shape list", cudaResult);
+
+//            cudaResult = cudaFree(dTadShape);
+//            if(cudaResult != 0)
+//                throw cuda_exception::build("helpers::stack_: cannot deallocate global memory on device for tad shape", cudaResult);
+//            cudaResult = cudaFree(dTadOffsets);
+//            if(cudaResult != 0)
+//                throw cuda_exception::build("helpers::stack_: cannot deallocate global memory on device for tad offsets", cudaResult);
 
 		}
 	}
