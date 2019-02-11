3df96e6c32de557b74feab6f86a534135e4ba4af
raver119 <raver119@gmail.com>
2019-02-11 14:41:59 +0300

    CUDA pooling 2D

--- libnd4j/include/ops/declarable/helpers/cuda/convolutions.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/convolutions.cu
@@ -17,6 +17,7 @@
 #include <ops/declarable/helpers/convolutions.h>
 #include <ops/declarable/helpers/im2col.h>
 #include <ops/declarable/helpers/col2im.h>
+#include <exceptions/cuda_exception.h>
 #include <NDArrayFactory.h>
 #include <MmulHelper.h>
 
@@ -75,34 +76,375 @@ namespace nd4j {
 
         }
 
-        template <typename T>
-        static __global__ void global_avg_pooling2d() {
+        template <typename T, typename Z>
+        static __global__ void global_avg_pooling2d(void *vx, Nd4jLong *xShapeBuffer, void *vz, Nd4jLong *zShapeBuffer, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            auto dx = reinterpret_cast<T*>(vx);
+            auto result = reinterpret_cast<Z*>(vz);
 
+            __shared__ int batchSize;
+            __shared__ int inChannels;
+            __shared__ int outH;
+            __shared__ int outW;
+            __shared__ int inH;
+            __shared__ int inW;
+
+            __shared__ int strideB;
+            __shared__ int strideC;
+            __shared__ int strideY;
+            __shared__ int strideX;
+
+            __shared__ int strideOB;
+            __shared__ int strideOC;
+            __shared__ int strideOY;
+            __shared__ int strideOX;
+
+            __shared__ int length;
+            __shared__ int kHEff;
+            __shared__ int kWEff;
+            __shared__ bool fOrder;
+
+
+            if (threadIdx.x == 0) {
+                batchSize = shape::sizeAt(xShapeBuffer, 0);
+                inChannels = shape::sizeAt(xShapeBuffer, 1);
+                outH = shape::sizeAt(zShapeBuffer, 2);
+                outW = shape::sizeAt(zShapeBuffer, 3);
+                inH = shape::sizeAt(xShapeBuffer, 2);
+                inW = shape::sizeAt(xShapeBuffer, 3);
+
+                strideB = shape::stride(xShapeBuffer)[0];
+                strideC = shape::stride(xShapeBuffer)[1];
+                strideY = shape::stride(xShapeBuffer)[2];
+                strideX = shape::stride(xShapeBuffer)[3];
+
+                strideOB = shape::stride(zShapeBuffer)[0];
+                strideOC = shape::stride(zShapeBuffer)[1];
+                strideOY = shape::stride(zShapeBuffer)[2];
+                strideOX = shape::stride(zShapeBuffer)[3];
+
+                length = shape::length(zShapeBuffer);
+
+                //Replace kernel H/W with *effective* kernel H/W accounting for dilatyon
+                kHEff = kH + (kH-1)*(dH-1);
+                kWEff = kW + (kW-1)*(dW-1);
+
+                fOrder = shape::order(zShapeBuffer) == 'f';
+            }
+            __syncthreads();
+
+            int tid = blockIdx.x * gridDim.x + threadIdx.x;
+
+            for (int index = tid; index < length; index += blockDim.x * gridDim.x) {
+                const int pw = index % outW;
+                const int ph = (index / outW) % outH;
+                const int c = (index / outW / outH) % inChannels;
+                const int n = index / outW / outH / inChannels;
+                int hstart = sH * ph - pH;
+                int wstart = sW * pw - pW;
+                int hend = hstart + kHEff;
+                int wend = wstart + kWEff;
+
+                if(hstart < 0){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) -hstart / (Z)dH);
+                    hstart += f * dH;
+                }
+                if(wstart < 0){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) -wstart / (Z) dW);
+                    wstart += f * dW;
+                }
+                if(hend > inH){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) (hend-inH) / (Z) dH);
+                    hend -= f * dH;
+                }
+                if(wend > inW){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) (wend-inW) / (Z) dW);
+                    wend -= f * dW;
+                }
+                //Accounts for dilation
+                int pool_size = nd4j::math::nd4j_ceil<double,int>((double) (hend-hstart) / (double) dH) * nd4j::math::nd4j_ceil<double,int>((double) (wend-wstart) / (double) dW);
+
+                Z sum(0.0f);
+
+                T *input_slice = dx + (n * strideB + c * strideC);
+
+                for (int h = hstart; h < hend; h += dH) {
+                    for (int w = wstart; w < wend; w += dW) {
+                        sum += static_cast<Z>(input_slice[h * strideY + w * strideX]);
+                    }
+                }
+
+                int divide_factor = pool_size;  //Case 0: exclude padding
+                if (extraParam0 == 1)     //Case 1: include padding
+                    divide_factor = kH * kW;
+
+                auto res = sum / static_cast<Z>(divide_factor);
+
+                if (!fOrder) {
+                    result[index] = res;
+                } else {
+                    result[n * strideOB + c * strideOC + pw * strideOX + ph * strideOY] = res;
+                }
+            }
         }
 
-        template <typename T>
-        static void _avg_pooling2d(nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+        template <typename T, typename Z>
+        static __global__ void global_pnorm_pooling2d(void *vx, Nd4jLong *xShapeBuffer, void *vz, Nd4jLong *zShapeBuffer, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            auto dx = reinterpret_cast<T*>(vx);
+            auto result = reinterpret_cast<Z*>(vz);
 
+            __shared__ int batchSize;
+            __shared__ int inChannels;
+            __shared__ int outH;
+            __shared__ int outW;
+            __shared__ int inH;
+            __shared__ int inW;
+
+            __shared__ int strideB;
+            __shared__ int strideC;
+            __shared__ int strideY;
+            __shared__ int strideX;
+
+            __shared__ int strideOB;
+            __shared__ int strideOC;
+            __shared__ int strideOY;
+            __shared__ int strideOX;
+
+            __shared__ int length;
+            __shared__ int kHEff;
+            __shared__ int kWEff;
+            __shared__ bool fOrder;
+
+
+            if (threadIdx.x == 0) {
+                batchSize = shape::sizeAt(xShapeBuffer, 0);
+                inChannels = shape::sizeAt(xShapeBuffer, 1);
+                outH = shape::sizeAt(zShapeBuffer, 2);
+                outW = shape::sizeAt(zShapeBuffer, 3);
+                inH = shape::sizeAt(xShapeBuffer, 2);
+                inW = shape::sizeAt(xShapeBuffer, 3);
+
+                strideB = shape::stride(xShapeBuffer)[0];
+                strideC = shape::stride(xShapeBuffer)[1];
+                strideY = shape::stride(xShapeBuffer)[2];
+                strideX = shape::stride(xShapeBuffer)[3];
+
+                strideOB = shape::stride(zShapeBuffer)[0];
+                strideOC = shape::stride(zShapeBuffer)[1];
+                strideOY = shape::stride(zShapeBuffer)[2];
+                strideOX = shape::stride(zShapeBuffer)[3];
+
+                length = shape::length(zShapeBuffer);
+
+                //Replace kernel H/W with *effective* kernel H/W accounting for dilatyon
+                kHEff = kH + (kH-1)*(dH-1);
+                kWEff = kW + (kW-1)*(dW-1);
+
+                fOrder = shape::order(zShapeBuffer) == 'f';
+            }
+            __syncthreads();
+
+            int tid = blockIdx.x * gridDim.x + threadIdx.x;
+
+            for (int index = tid; index < length; index += blockDim.x * gridDim.x) {
+                const int pw = index % outW;
+                const int ph = (index / outW) % outH;
+                const int c = (index / outW / outH) % inChannels;
+                const int n = index / outW / outH / inChannels;
+                int hstart = sH * ph - pH;
+                int wstart = sW * pw - pW;
+                int hend = hstart + kHEff;
+                int wend = wstart + kWEff;
+
+                if (hstart < 0) {
+                    int f = nd4j::math::nd4j_ceil<Z, int>((Z) -hstart / (Z) dH);
+                    hstart += f * dH;
+                }
+                if (wstart < 0) {
+                    int f = nd4j::math::nd4j_ceil<Z, int>((Z) -wstart / (Z) dW);
+                    wstart += f * dW;
+                }
+                if (hend > inH) {
+                    int f = nd4j::math::nd4j_ceil<Z, int>((Z) (hend - inH) / (Z) dH);
+                    hend -= f * dH;
+                }
+                if (wend > inW) {
+                    int f = nd4j::math::nd4j_ceil<Z, int>((Z) (wend - inW) / (Z) dW);
+                    wend -= f * dW;
+                }
+                //Accounts for dilation
+                int pool_size = nd4j::math::nd4j_ceil<double, int>((double) (hend - hstart) / (double) dH) *
+                                nd4j::math::nd4j_ceil<double, int>((double) (wend - wstart) / (double) dW);
+
+                Z sum(0.0f);
+
+                T *input_slice = dx + (n * strideB + c * strideC);
+
+                for (int h = hstart; h < hend; h += dH) {
+                    for (int w = wstart; w < wend; w += dW) {
+                        sum += nd4j::math::nd4j_pow<Z, Z, Z>(
+                                static_cast<Z>(nd4j::math::nd4j_abs<T>(input_slice[h * strideY + w * strideX])),
+                                extraParam0);
+                    }
+                }
+
+                auto res = nd4j::math::nd4j_pow<Z, Z, Z>(sum, (Z) 1.0f / extraParam0);
+
+
+                if (!fOrder) {
+                    result[index] = res;
+                } else {
+                    result[n * strideOB + c * strideOC + pw * strideOX + ph * strideOY] = res;
+                }
+            }
         }
-        BUILD_SINGLE_TEMPLATE(template void _avg_pooling2d, (nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0), FLOAT_TYPES);
+
+        template <typename T, typename Z>
+        static __global__ void global_max_pooling2d(void *vx, Nd4jLong *xShapeBuffer, void *vz, Nd4jLong *zShapeBuffer, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            auto dx = reinterpret_cast<T*>(vx);
+            auto result = reinterpret_cast<Z*>(vz);
+
+            __shared__ int batchSize;
+            __shared__ int inChannels;
+            __shared__ int outH;
+            __shared__ int outW;
+            __shared__ int inH;
+            __shared__ int inW;
+
+            __shared__ int strideB;
+            __shared__ int strideC;
+            __shared__ int strideY;
+            __shared__ int strideX;
+
+            __shared__ int strideOB;
+            __shared__ int strideOC;
+            __shared__ int strideOY;
+            __shared__ int strideOX;
+
+            __shared__ int length;
+            __shared__ int kHEff;
+            __shared__ int kWEff;
+            __shared__ bool fOrder;
+
+
+            if (threadIdx.x == 0) {
+                batchSize = shape::sizeAt(xShapeBuffer, 0);
+                inChannels = shape::sizeAt(xShapeBuffer, 1);
+                outH = shape::sizeAt(zShapeBuffer, 2);
+                outW = shape::sizeAt(zShapeBuffer, 3);
+                inH = shape::sizeAt(xShapeBuffer, 2);
+                inW = shape::sizeAt(xShapeBuffer, 3);
+
+                strideB = shape::stride(xShapeBuffer)[0];
+                strideC = shape::stride(xShapeBuffer)[1];
+                strideY = shape::stride(xShapeBuffer)[2];
+                strideX = shape::stride(xShapeBuffer)[3];
+
+                strideOB = shape::stride(zShapeBuffer)[0];
+                strideOC = shape::stride(zShapeBuffer)[1];
+                strideOY = shape::stride(zShapeBuffer)[2];
+                strideOX = shape::stride(zShapeBuffer)[3];
+
+                length = shape::length(zShapeBuffer);
+
+                //Replace kernel H/W with *effective* kernel H/W accounting for dilatyon
+                kHEff = kH + (kH-1)*(dH-1);
+                kWEff = kW + (kW-1)*(dW-1);
+
+                fOrder = shape::order(zShapeBuffer) == 'f';
+            }
+            __syncthreads();
+
+            int tid = blockIdx.x * gridDim.x + threadIdx.x;
+
+            for (int index = tid; index < length; index += blockDim.x * gridDim.x) {
+                const int pw = index % outW;
+                const int ph = (index / outW) % outH;
+                const int c = (index / outW / outH) % inChannels;
+                const int n = index / outW / outH / inChannels;
+                int hstart = sH * ph - pH;
+                int wstart = sW * pw - pW;
+                int hend = hstart + kHEff;
+                int wend = wstart + kWEff;
+
+                if(hstart < 0){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) -hstart / (Z)dH);
+                    hstart += f * dH;
+                }
+                if(wstart < 0){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) -wstart / (Z) dW);
+                    wstart += f * dW;
+                }
+                if(hend > inH){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) (hend-inH) / (Z) dH);
+                    hend -= f * dH;
+                }
+                if(wend > inW){
+                    int f = nd4j::math::nd4j_ceil<Z,int>((Z) (wend-inW) / (Z) dW);
+                    wend -= f * dW;
+                }
+                //Accounts for dilation
+                int pool_size = nd4j::math::nd4j_ceil<double,int>((double) (hend-hstart) / (double) dH) * nd4j::math::nd4j_ceil<double,int>((double) (wend-wstart) / (double) dW);
+
+                Z max = -nd4j::DataTypeUtils::max<Z>();
+
+                T *input_slice = dx + (n * strideB + c * strideC);
+
+                for (int h = hstart; h < hend; h += dH) {
+                    for (int w = wstart; w < wend; w += dW) {
+                        Z v = static_cast<Z>(input_slice[h * strideY + w * strideX]);
+                        if (v > max)
+                            max = v;
+                    }
+                }
+
+
+                if (!fOrder) {
+                    result[index] = max;
+                } else {
+                    result[n * strideOB + c * strideOC + pw * strideOX + ph * strideOY] = max;
+                }
+            }
+        }
+
+        template <typename T, typename Z>
+        static void _max_pooling2d(nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            global_max_pooling2d<T,Z><<<512, 512, 2048, *block.getCudaStream()>>>(vx, vxShapeInfo, vz, vzShapeInfo, kH, kW, sH, sW, pH, pW, dH, dW, extraParam0);
+        }
+        BUILD_DOUBLE_TEMPLATE(template void _max_pooling2d, (nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
+
+        template <typename T, typename Z>
+        static void _pnorm_pooling2d(nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            global_pnorm_pooling2d<T,Z><<<512, 512, 2048, *block.getCudaStream()>>>(vx, vxShapeInfo, vz, vzShapeInfo, kH, kW, sH, sW, pH, pW, dH, dW, extraParam0);
+        }
+        BUILD_DOUBLE_TEMPLATE(template void _pnorm_pooling2d, (nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
+
+        template <typename T, typename Z>
+        static void _avg_pooling2d(nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0) {
+            global_avg_pooling2d<T,Z><<<512, 512, 2048, *block.getCudaStream()>>>(vx, vxShapeInfo, vz, vzShapeInfo, kH, kW, sH, sW, pH, pW, dH, dW, extraParam0);
+        }
+        BUILD_DOUBLE_TEMPLATE(template void _avg_pooling2d, (nd4j::graph::LaunchContext& block, void *vx, Nd4jLong *vxShapeInfo, void *vz, Nd4jLong *vzShapeInfo, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const int extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
 
         void ConvolutionUtils::pooling2d(nd4j::graph::LaunchContext& block, const NDArray& input, NDArray& output, const int kH, const int kW, const int sH, const int sW, const int pH, const int pW, const int dH, const int dW, const PoolingType poolingMode, const int extraParam0) {
             switch (poolingMode) {
                 case MAX_POOL: {
-
+                        BUILD_DOUBLE_SELECTOR(input.dataType(), output.dataType(), _max_pooling2d, (block, input.getSpecialBuffer(), input.getSpecialShapeInfo(), output.getSpecialBuffer(), output.getSpecialShapeInfo(), kH, kW, sH, sW, pH, pW, dH, dW, extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
                     }
                     break;
                 case AVG_POOL: {
-
+                        BUILD_DOUBLE_SELECTOR(input.dataType(), output.dataType(), _avg_pooling2d, (block, input.getSpecialBuffer(), input.getSpecialShapeInfo(), output.getSpecialBuffer(), output.getSpecialShapeInfo(), kH, kW, sH, sW, pH, pW, dH, dW, extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
                     }
                     break;
                 case PNORM_POOL: {
-
+                        BUILD_DOUBLE_SELECTOR(input.dataType(), output.dataType(), _pnorm_pooling2d, (block, input.getSpecialBuffer(), input.getSpecialShapeInfo(), output.getSpecialBuffer(), output.getSpecialShapeInfo(), kH, kW, sH, sW, pH, pW, dH, dW, extraParam0), LIBND4J_TYPES, FLOAT_TYPES);
                     }
                     break;
                 default:
                     throw std::runtime_error("Pooling2D: Unknown PoolingType used");
             }
+
+            auto result = cudaStreamSynchronize(*block.getCudaStream());
+            if (result != 0)
+                throw cuda_exception::build("Pooling2D failed", result);
         }
 
         void ConvolutionUtils::pooling3d(nd4j::graph::LaunchContext& block, const NDArray& input, NDArray& output, const int kD, const int kH, const int kW, const int sD, const int sH, const int sW, const int pD, const int pH, const int pW, const int dD, const int dH, const int dW, const int poolingMode, const int extraParam0) {
