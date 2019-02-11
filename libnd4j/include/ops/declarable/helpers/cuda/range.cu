5401c8af38f91263c4262786c65442023aa40048
raver119 <raver119@gmail.com>
2019-02-11 17:50:48 +0300

    range kernel

--- libnd4j/include/ops/declarable/helpers/cuda/range.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/range.cu
@@ -25,18 +25,28 @@ namespace nd4j {
 namespace ops {
 namespace helpers {
 
+    template <typename T>
+    static __global__ void global_range(void *output, Nd4jLong length, T start, T delta) {
+        auto buff = reinterpret_cast<T*>(output);
+        auto tid = blockIdx.x * gridDim.x + threadIdx.x;
+        auto step = gridDim.x * blockDim.x;
+
+        for(Nd4jLong i = tid; i < length; i += step)
+            buff[i] = start + i * delta;
+    }
+
     //////////////////////////////////////////////////////////////////////////
     // be careful: outVector must have c-order and ews = 1 !!!
     template <typename T>
-    static void _range(const NDArray& start, const NDArray& delta, NDArray& outVector) {
-
+    static void _range(graph::LaunchContext* context, const NDArray& start, const NDArray& delta, NDArray& outVector) {
+        global_range<T><<<512, 512, 2048, *context->getCudaStream()>>>(outVector.getSpecialBuffer(), outVector.lengthOf(), start.e<T>(0), delta.e<T>(0));
     }
 
     void range(graph::LaunchContext* context, const NDArray& start, const NDArray& delta, NDArray& outVector) {
-        BUILD_SINGLE_SELECTOR(outVector.dataType(), _range, (start, delta, outVector), LIBND4J_TYPES);
+        BUILD_SINGLE_SELECTOR(outVector.dataType(), _range, (context, start, delta, outVector), LIBND4J_TYPES);
     }
 
-    BUILD_SINGLE_TEMPLATE(template void _range, (const NDArray& start, const NDArray& delta, NDArray& outVector), LIBND4J_TYPES);
+    BUILD_SINGLE_TEMPLATE(template void _range, (graph::LaunchContext* context, const NDArray& start, const NDArray& delta, NDArray& outVector), LIBND4J_TYPES);
 }
 }
 }
