bd3d428c9f0e40fadb9380839011288cdebd2b2f
Alex Black <blacka101@gmail.com>
2019-02-15 19:00:53 +1100

    [WIP] Misc DL4J/ND4J Fixes (#7169)
    * #7152 Yolo2OutputLayer label shape validation
    
    * #7143 empty array checks
    
    * #7123 Update old URLs
    
    * #7115 Fix issue with LastTimeStep and masks
    
    * #6809 Fix updater state view array layout when dynamically changing LR with updaters like Adam
    
    * Delete old/unmaintained VideoRecordReader
    
    * #7104 Fix memory info reporting when activations shape/length can't be inferred (some RNNs etc)

--- deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/exceptions/UnsupportedKerasConfigurationException.java
+++ deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/exceptions/UnsupportedKerasConfigurationException.java
@@ -21,7 +21,7 @@ package org.deeplearning4j.nn.modelimport.keras.exceptions;
  * Indicates that user is attempting to import a Keras model configuration that
  * is not currently supported.
  *
- * See <a href="http://deeplearning4j.org/model-import-keras">http://deeplearning4j.org/model-import-keras</a>
+ * See <a href="https://deeplearning4j.org/docs/latest/keras-import-overview">https://deeplearning4j.org/docs/latest/keras-import-overview</a>
  * for more information and file an issue at <a href="http://github.com/deeplearning4j/deeplearning4j/issues">http://github.com/deeplearning4j/deeplearning4j/issues</a>.
  *
  * @author dave@skymind.io
