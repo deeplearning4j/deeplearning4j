c882d8dda5ac15de07690dacff6e41475774b092
Paul Dubs <paul.dubs@gmail.com>
2019-02-15 07:25:59 +0100

    Expose GELU Activation Function for use with MLN/CG

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/activations/Activation.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/activations/Activation.java
@@ -41,7 +41,7 @@ import org.nd4j.linalg.api.ops.impl.transforms.strict.TanhDerivative;
 public enum Activation {
     CUBE, ELU, HARDSIGMOID, HARDTANH, IDENTITY, LEAKYRELU, RATIONALTANH, RELU, RELU6,
     RRELU, SIGMOID, SOFTMAX, SOFTPLUS, SOFTSIGN, TANH, RECTIFIEDTANH, SELU, SWISH,
-    THRESHOLDEDRELU;
+    THRESHOLDEDRELU, GELU;
 
     /**
      * Creates an instance of the activation function
@@ -88,6 +88,8 @@ public enum Activation {
                 return new ActivationTanH();
             case THRESHOLDEDRELU:
                 return new ActivationThresholdedReLU();
+            case GELU:
+                return new ActivationGELU();
             default:
                 throw new UnsupportedOperationException("Unknown or not supported activation function: " + this);
         }
@@ -147,6 +149,8 @@ public enum Activation {
                 return sd.softsign(variableName, input);
             case TANH:
                 return sd.tanh(variableName, input);
+            case GELU:
+                return sd.gelu(variableName, input);
             case HARDSIGMOID:
             case RATIONALTANH:
             case RRELU:
@@ -202,6 +206,8 @@ public enum Activation {
                 return new SELU(in);
             case SWISH:
                 return new Swish(in);
+            case GELU:
+                return new GELU(in);
             case RRELU:
             default:
                 throw new UnsupportedOperationException("Not supported via this method: " + this);
@@ -253,6 +259,8 @@ public enum Activation {
                 return new ScalarSet(in, 1.0);
             case RELU:
                 return new Step(in);
+            case GELU:
+                return new GELUDerivative(in);
             case RRELU:
             default:
                 throw new UnsupportedOperationException("Not supported via this method: " + this);
---
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/activations/impl/ActivationGELU.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.nd4j.linalg.activations.impl;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.nd4j.linalg.activations.BaseActivationFunction;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.impl.transforms.strict.GELU;
+import org.nd4j.linalg.api.ops.impl.transforms.strict.GELUDerivative;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.primitives.Pair;
+
+/**
+ * GELU activation function - Gaussian Error Linear Units
+ *
+ * @see GELU
+ */
+@EqualsAndHashCode
+@Getter
+public class ActivationGELU extends BaseActivationFunction {
+
+    private boolean precise;
+
+    public ActivationGELU(boolean precise){
+        this.precise = precise;
+    }
+
+    public ActivationGELU(){
+        this(false);
+    }
+
+    @Override
+    public INDArray getActivation(INDArray in, boolean training) {
+        Nd4j.getExecutioner().execAndReturn(new GELU(in, in, precise));
+        return in;
+    }
+
+    @Override
+    public Pair<INDArray, INDArray> backprop(INDArray in, INDArray epsilon) {
+        assertShape(in, epsilon);
+        INDArray dLdz = Nd4j.getExecutioner().exec(new GELUDerivative(in, in, precise));
+        dLdz.muli(epsilon);
+        return new Pair<>(dLdz, null);
+    }
+
+    @Override
+    public String toString() {
+        return "gelu(precise="+precise+")";
+    }
+
+}
