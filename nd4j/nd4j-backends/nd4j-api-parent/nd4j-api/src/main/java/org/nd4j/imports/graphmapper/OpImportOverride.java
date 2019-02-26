9da94d568e1b77dea6c24eb2e52cfe2754b1443b
Alex Black <blacka101@gmail.com>
2019-02-26 16:34:45 +1100

    Add ability to ignore/skip ops during import; fixes for op import overrides

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/imports/graphmapper/OpImportOverride.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/imports/graphmapper/OpImportOverride.java
@@ -2,6 +2,7 @@ package org.nd4j.imports.graphmapper;
 
 import org.nd4j.autodiff.samediff.SDVariable;
 import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.linalg.api.buffer.DataType;
 
 import java.util.List;
 import java.util.Map;
@@ -9,5 +10,7 @@ import java.util.Map;
 public interface OpImportOverride<GRAPH_TYPE, NODE_TYPE, ATTR_TYPE> {
 
     List<SDVariable> initFromTensorFlow(List<SDVariable> inputs, List<SDVariable> controlDepInputs, NODE_TYPE nodeDef, SameDiff initWith, Map<String,ATTR_TYPE> attributesForNode, GRAPH_TYPE graph);
+
+    //DataType dataTypeForOutput(int outputNum, List<SDVariable> inputs, List<SDVariable> controlDepInputs, NODE_TYPE nodeDef, SameDiff initWith, Map<String,ATTR_TYPE> attributesForNode, GRAPH_TYPE graph);
 
 }
