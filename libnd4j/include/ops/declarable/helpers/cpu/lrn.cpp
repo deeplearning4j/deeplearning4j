c855c7c1771122a184d1cbc9af4ee7a61c6cfb10
raver119 <raver119@gmail.com>
2019-02-27 14:44:10 +0300

    more lrn

--- libnd4j/include/ops/declarable/helpers/cpu/lrn.cpp
+++ libnd4j/include/ops/declarable/helpers/cpu/lrn.cpp
@@ -202,7 +202,7 @@ static void getMKLDNNMemoryDescLrn(const NDArray* src, const NDArray* diff_src,
                     }
                     T aSum = alpha * quadSum;
                     T tXe = iX[e];
-                    scaleBuffer[shift + e] = one + (tXe * tXe * 2 * tbeta) / (tbias - aSum);
+                    scaleBuffer[shift + e] = one - (tXe * tXe * 2 * tbeta) / (tbias - aSum);
                     T dividor = nd4j::math::nd4j_pow<T, T, T>(tbias + aSum, tbeta);
                     outputBuffer[shift + e] = tXe / dividor;
                 }
@@ -223,7 +223,7 @@ static void getMKLDNNMemoryDescLrn(const NDArray* src, const NDArray* diff_src,
                     }
 
                     auto p = shape::getIndexOffset(shift + e, input->getShapeInfo(), totalLength);
-                    scaleBuffer[shift + e] += (inputBuffer[p] * inputBuffer[p] * 2 * beta) / (bias - alpha * quadSum);
+                    scaleBuffer[shift + e] = one - (inputBuffer[p] * inputBuffer[p] * 2 * beta) / (bias - alpha * quadSum);
                     T dividor = nd4j::math::nd4j_pow<T, T, T>(bias + alpha * quadSum, beta);
                     outputBuffer[shape::getIndexOffset(shift + e, output->shapeInfo(), totalLength)] = inputBuffer[p] / dividor;
                 }
