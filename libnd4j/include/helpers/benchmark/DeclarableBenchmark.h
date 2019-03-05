ca7e35e6a619806e8109ba998ef1e438d9924b92
Alex Black <blacka101@gmail.com>
2019-03-05 12:12:59 +1100

    Formatting fixes

--- libnd4j/include/helpers/benchmark/DeclarableBenchmark.h
+++ libnd4j/include/helpers/benchmark/DeclarableBenchmark.h
@@ -31,61 +31,61 @@
 namespace nd4j {
     class ND4J_EXPORT DeclarableBenchmark : public OpBenchmark  {
     protected:
-    nd4j::ops::DeclarableOp *_op = nullptr;
-    nd4j::graph::Context *_context = nullptr;
+        nd4j::ops::DeclarableOp *_op = nullptr;
+        nd4j::graph::Context *_context = nullptr;
     public:
-    DeclarableBenchmark(nd4j::ops::DeclarableOp &op, std::string name = 0) : OpBenchmark() {
-        _op = ops::OpRegistrator::getInstance()->getOperation(op.getOpHash());
-        _testName = name;
-    }
+        DeclarableBenchmark(nd4j::ops::DeclarableOp &op, std::string name = 0) : OpBenchmark() {
+            _op = ops::OpRegistrator::getInstance()->getOperation(op.getOpHash());
+            _testName = name;
+        }
 
-    void setContext(nd4j::graph::Context *ctx) {
-        _context = ctx;
-    }
+        void setContext(nd4j::graph::Context *ctx) {
+            _context = ctx;
+        }
 
-    std::string axis() override {
-    return "N/A";
-}
+        std::string axis() override {
+            return "N/A";
+        }
 
-std::string orders() override {
-return "N/A";
-}
+        std::string orders() override {
+            return "N/A";
+        }
 
-std::string strides() override {
-return "N/A";
-}
+        std::string strides() override {
+            return "N/A";
+        }
 
-std::string inplace() override {
-return "N/A";
-}
+        std::string inplace() override {
+            return "N/A";
+        }
 
-void executeOnce() override {
-_op->execute(_context);
-}
+        void executeOnce() override {
+            _op->execute(_context);
+        }
 
-OpBenchmark *clone() override {
-return new DeclarableBenchmark(*_op, _testName);
-}
+        OpBenchmark *clone() override {
+            return new DeclarableBenchmark(*_op, _testName);
+        }
 
-std::string shape() override {
-if (_context != nullptr && _context->isFastPath())
-return ShapeUtils::shapeAsString(_context->getNDArray(0));
-else
-return "N/A";
-}
+        std::string shape() override {
+            if (_context != nullptr && _context->isFastPath())
+                return ShapeUtils::shapeAsString(_context->getNDArray(0));
+            else
+                return "N/A";
+        }
 
-std::string dataType() override {
-if (_context != nullptr && _context->isFastPath())
-return DataTypeUtils::asString(_context->getNDArray(0)->dataType());
-else
-return "N/A";
-}
+        std::string dataType() override {
+            if (_context != nullptr && _context->isFastPath())
+                return DataTypeUtils::asString(_context->getNDArray(0)->dataType());
+            else
+                return "N/A";
+        }
 
-~DeclarableBenchmark() {
-    if (_context != nullptr)
-        delete _context;
-}
-};
+        ~DeclarableBenchmark() {
+            if (_context != nullptr)
+                delete _context;
+        }
+    };
 }
 
 #endif //DEV_TESTS_DECLARABLEBENCHMARKS_H
