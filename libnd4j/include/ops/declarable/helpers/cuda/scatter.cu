f59a2cea8b7cd2e13ed11d1df01ac606a6ecda44
raver119 <raver119@gmail.com>
2019-03-06 11:16:28 +0300

    scatter step 3

--- libnd4j/include/ops/declarable/helpers/cuda/scatter.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/scatter.cu
@@ -21,13 +21,107 @@
 #include <ops/declarable/helpers/scatter.h>
 #include <numeric>
 #include <helpers/ShapeUtils.h>
+#include <helpers/PointersManager.h>
+#include <TAD.h>
 
 
 namespace nd4j {
     namespace ops {
         namespace helpers {
-            void scatter(graph::LaunchContext *context, pairwise::Ops op, const NDArray& indices, const NDArray& updates, NDArray& output, const bool lock) {
+            template<typename T>
+            __global__ static void scatterCuda(const int opCode, const int numOfSubArrs,
+                                                     void* vx, const Nd4jLong *xShapeInfo, const Nd4jLong *xOffsets,
+                                                     void* vy, const Nd4jLong *yShapeInfo, const Nd4jLong *yOffsets,
+                                                     const int* indexes, const bool locking) {
 
+                __shared__ T *x, *y;
+                __shared__ Nd4jLong arrLenX, arrLenY;
+
+                for (int e = 0; e < numOfSubArrs; e++ ) {
+
+                    const auto xIndex = indexes[e];
+                    const bool isOwner = xIndex < gridDim.x ? blockIdx.x == xIndex : blockIdx.x == xIndex % gridDim.x;
+
+                    if (!isOwner)
+                        continue;
+
+                    if (threadIdx.x == 0) {
+                        x = reinterpret_cast<T*>(vx) + xOffsets[xIndex];
+                        y = reinterpret_cast<T*>(vy) + yOffsets[e];
+                        arrLenX = shape::length(xShapeInfo);
+                        arrLenY = shape::length(yShapeInfo);
+                    }
+                    __syncthreads();
+
+                    if (arrLenX != arrLenY)
+                        return;
+
+                    for (Nd4jLong i = threadIdx.x; i < arrLenX; i += blockDim.x) {
+
+                        const auto xOffset = shape::getIndexOffset(i, xShapeInfo, arrLenX);
+                        const auto yOffset = shape::getIndexOffset(i, yShapeInfo, arrLenY);
+
+                        switch (opCode) {
+                            case 0:
+                                x[xOffset] += y[yOffset];
+                                break;
+                            case 1:
+                                x[xOffset] -= y[yOffset];
+                                break;
+                            case 2:
+                                x[xOffset] *= y[yOffset];
+                                break;
+                            case 3:
+                                x[xOffset] /= y[yOffset];
+                                break;
+                            case 4:
+                                x[xOffset] = y[yOffset] - x[xOffset];
+                                break;
+                            case 5:
+                                x[xOffset] = y[yOffset] / x[xOffset];
+                                break;
+                            case 6:
+                                x[xOffset] = y[yOffset];
+                                break;
+                            default:
+                                continue;
+                        }
+                    }
+                    __syncthreads();
+                }
+            }
+
+
+            template <typename T>
+            void scatter_(graph::LaunchContext *context, pairwise::Ops op, const NDArray& indices, const NDArray& updates, NDArray& output, const bool lock) {
+                int axis = 0;
+                shape::TAD tadX;
+                tadX.init(output.getShapeInfo(), &axis, 1);
+                tadX.createTadOnlyShapeInfo();
+                tadX.createOffsets();
+
+                shape::TAD tadY;
+                tadY.init(updates.getShapeInfo(), &axis, 1);
+                tadY.createTadOnlyShapeInfo();
+                tadY.createOffsets();
+
+                PointersManager manager(context, "scatter");
+                auto psX = reinterpret_cast<Nd4jLong *>(manager.replicatePointer(tadX.tadOnlyShapeInfo, shape::shapeInfoByteLength(tadX.tadOnlyShapeInfo)));
+                auto psY = reinterpret_cast<Nd4jLong *>(manager.replicatePointer(tadY.tadOnlyShapeInfo, shape::shapeInfoByteLength(tadY.tadOnlyShapeInfo)));
+
+                auto poX = reinterpret_cast<Nd4jLong *>(manager.replicatePointer(tadX.tadOffsets, tadX.numTads * sizeof(Nd4jLong)));
+                auto poY = reinterpret_cast<Nd4jLong *>(manager.replicatePointer(tadY.tadOffsets, tadY.numTads * sizeof(Nd4jLong)));
+
+                NDArray::prepareSpecialUse({&output}, {&updates, &indices});
+
+                scatterCuda<T><<<512, 512, 1024, *context->getCudaStream()>>>(op, indices.lengthOf(), output.getSpecialBuffer(), psX, poX, updates.getSpecialBuffer(), psY, poY, reinterpret_cast<int *>(indices.getSpecialBuffer()), lock);
+
+                NDArray::registerSpecialUse({&output}, {&updates, &indices});
+                manager.synchronize();
+            }
+
+            void scatter(graph::LaunchContext *context, pairwise::Ops op, const NDArray& indices, const NDArray& updates, NDArray& output, const bool lock) {
+                BUILD_SINGLE_SELECTOR(output.dataType(), scatter_, (context, op, indices, updates, output, lock), LIBND4J_TYPES);
             }
 
             void scatterND(graph::LaunchContext *context, pairwise::Ops op, const NDArray& indices, const NDArray& updates, NDArray& output, const bool lock) {
