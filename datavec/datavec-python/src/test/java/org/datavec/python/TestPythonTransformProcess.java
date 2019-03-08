0fc7b6c0aacdde2e1dd3ba22b3b19eccbf587d3f
Fariz Rahman <farizrahman4u@gmail.com>
2019-03-08 08:14:11 +0400

    mixed types test

--- datavec/datavec-python/src/test/java/org/datavec/python/TestPythonTransformProcess.java
+++ datavec/datavec-python/src/test/java/org/datavec/python/TestPythonTransformProcess.java
@@ -4,12 +4,10 @@ import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.condition.Condition;
 import org.datavec.api.transform.filter.ConditionFilter;
 import org.datavec.api.transform.filter.Filter;
-import org.datavec.api.writable.IntWritable;
-import org.datavec.api.writable.NDArrayWritable;
-import org.datavec.api.writable.Writable;
+import org.datavec.api.writable.*;
 import org.datavec.api.transform.schema.Schema;
-import org.datavec.api.writable.Text;
 import org.junit.Test;
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
@@ -51,6 +49,37 @@ public class TestPythonTransformProcess {
     }
 
     @Test
+    public void testMixedTypes() throws Exception{
+        Schema.Builder schemaBuilder = new Schema.Builder();
+        schemaBuilder
+                .addColumnInteger("col1")
+                .addColumnFloat("col2")
+                .addColumnString("col3")
+                .addColumnDouble("col4");
+
+
+        Schema initialSchema = schemaBuilder.build();
+        schemaBuilder.addColumnInteger("col5");
+        Schema finalSchema = schemaBuilder.build();
+
+        String pythonCode = "col5  = (int(col3) + col1 + int(col2)) * int(col4)";
+
+        TransformProcess tp = new TransformProcess.Builder(initialSchema).transform(
+                new PythonTransform(pythonCode, finalSchema)
+        ).build();
+
+        List<Writable> inputs = Arrays.asList((Writable)
+        new IntWritable(10),
+                new FloatWritable(3.5f),
+                new Text("5"),
+                new DoubleWritable(2.0)
+        );
+
+        List<Writable> outputs = tp.execute(inputs);
+        assertEquals(((LongWritable)outputs.get(4)).get(), 36);
+    }
+
+    @Test
     public void testNDArray() throws Exception{
         long[] shape = new long[]{3, 2};
         INDArray arr1 = Nd4j.rand(shape);
@@ -75,6 +104,39 @@ public class TestPythonTransformProcess {
         List<Writable> inputs = Arrays.asList(
                 (Writable) new NDArrayWritable(arr1),
                  new NDArrayWritable(arr2)
+        );
+
+        List<Writable> outputs = tp.execute(inputs);
+        assertEquals(arr1, ((NDArrayWritable)outputs.get(0)).get());
+        assertEquals(arr2, ((NDArrayWritable)outputs.get(1)).get());
+        assertEquals(expectedOutput,((NDArrayWritable)outputs.get(2)).get());
+
+    }
+
+    @Test
+    public void testNDArrayMixed() throws Exception{
+        long[] shape = new long[]{3, 2};
+        INDArray arr1 = Nd4j.rand(DataType.DOUBLE, shape);
+        INDArray arr2 = Nd4j.rand(DataType.FLOAT, shape);
+        INDArray expectedOutput = arr1.add(arr2.castTo(DataType.DOUBLE));
+
+        Schema.Builder schemaBuilder = new Schema.Builder();
+        schemaBuilder
+                .addColumnNDArray("col1", shape)
+                .addColumnNDArray("col2", shape);
+
+        Schema initialSchema = schemaBuilder.build();
+        schemaBuilder.addColumnNDArray("col3", shape);
+        Schema finalSchema = schemaBuilder.build();
+
+        String pythonCode = "col3 = col1 + col2";
+        TransformProcess tp = new TransformProcess.Builder(initialSchema).transform(
+                new PythonTransform(pythonCode, finalSchema)
+        ).build();
+
+        List<Writable> inputs = Arrays.asList(
+                (Writable) new NDArrayWritable(arr1),
+                new NDArrayWritable(arr2)
         );
 
         List<Writable> outputs = tp.execute(inputs);
