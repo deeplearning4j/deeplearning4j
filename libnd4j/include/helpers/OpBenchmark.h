31e51af1479a54557e6bb8837a35b833a076d9b1
Alex Black <blacka101@gmail.com>
2019-03-08 16:27:01 +1100

    [WIP] Improvements to libnd4j benchmark framework (#7265)
    * Improvements to benchmark framework
    
    * More fixes
    
    * Another fix

--- libnd4j/include/helpers/OpBenchmark.h
+++ libnd4j/include/helpers/OpBenchmark.h
@@ -12,7 +12,7 @@
 namespace nd4j {
     class ND4J_EXPORT OpBenchmark {
     protected:
-        int _opNum;
+        int _opNum = 0;
         std::string _testName;
         NDArray *_x = nullptr;
         NDArray *_y = nullptr;
@@ -40,6 +40,7 @@ namespace nd4j {
         std::string testName();
         std::vector<int> getAxis();
 
+        virtual std::string extra();
         virtual std::string dataType();
         virtual std::string axis() = 0;
         virtual std::string orders() = 0;
