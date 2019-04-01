f0014aa182358b5ec6d7a62a4c1e10e229a5d02f
Fariz Rahman <farizrahman4u@gmail.com>
2019-04-01 23:23:52 +0530

    datavec-python (#7188)
    * datavec-python initial
    
    * basic test working
    
    * add ndarray tests
    
    * move versions to top level pom
    
    * move versions to top level pom
    
    * javaccp version
    
    * rem to and from json
    
    * convert to custom transform
    
    * rem outputcode
    
    * Python filters
    
    * rem exception
    
    * add jvm crash checks
    
    * executioner fix + tests
    
    * fixme
    
    * rem comments
    
    * docstr + run time errs
    
    * executioner fixes
    
    * fix nits
    
    * rem spark dependency
    
    * lombok
    
    * mixed types test
    
    * javadocs
    
    * more executioner fixes
    
    * safeexec -> exec
    
    * simplify executioner + test fixes
    
    * rem restricted python for now
    
    * serde + spark tests
    
    * add dep
    
    * javadoc + author
    
    * copyright headers
    
    * rem unused importsd
    
    * local tests
    
    * local exec
    
    * add profiles
    
    * Update version of JavaCPP, JavaCV, and all presets to 1.5-SNAPSHOT
    
    Including change-cuda-versions.sh to support CUDA 10.1 and cuDNN 7.5
    
    * executioner fixes
    
    * use javacpp 1.5-SNAPSHOT
    
    * static executioner
    
    * few fixes
    
    * fix threading issues
    
    * more fixes
    
    * spark tests passing
    
    * simplify executioner

---
+++ datavec/datavec-python/src/test/java/org/datavec/python/TestSerde.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.datavec.python;
+
+import org.datavec.api.transform.Transform;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.serde.JsonSerializer;
+import org.datavec.api.transform.serde.YamlSerializer;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestSerde {
+
+    public static YamlSerializer y = new YamlSerializer();
+    public static JsonSerializer j = new JsonSerializer();
+
+    @Test
+    public void testBasicSerde() throws Exception{
+        Schema schema = new Schema.Builder()
+                .addColumnInteger("col1")
+                .addColumnFloat("col2")
+                .addColumnString("col3")
+                .addColumnDouble("col4")
+                .build();
+
+        Transform t = new PythonTransform(
+                "col1+=3\ncol2+=2\ncol3+='a'\ncol4+=2.0",
+                schema
+        );
+
+        String yaml = y.serialize(t);
+        String json = j.serialize(t);
+
+        Transform t2 = y.deserializeTransform(json);
+        Transform t3 = j.deserializeTransform(json);
+        assertEquals(t, t2);
+        assertEquals(t, t3);
+    }
+
+}
