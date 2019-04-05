cbe7cf4d4984ed26567ced9e60970dd32ea0ed67
raver119 <raver119@gmail.com>
2019-04-05 07:44:48 +0300

    flatten: helper

--- libnd4j/include/ops/declarable/helpers/cpu/flatten.cpp
+++ libnd4j/include/ops/declarable/helpers/cpu/flatten.cpp
@@ -25,8 +25,45 @@ namespace nd4j {
         namespace helpers {
 
             template <typename T>
-            static void flatten_(std::vector<NDArray*> &inputs, NDArray *output, char order) {
+            static void flatten_(std::vector<NDArray*> &inputs, NDArray *output, const char order) {
 
+                int numArrays = inputs.size();
+                std::vector<Nd4jLong> offsets(numArrays);
+                Nd4jLong cOffset = 0;
+
+                // calculating offsets in output
+                for (int e = 0; e < numArrays; e++) {
+                    offsets[e] = cOffset;
+                    cOffset += inputs[e]->lengthOf();
+                }
+
+                Nd4jLong xCoord[MAX_RANK];
+
+                // actually transferring data
+                for (int e = 0; e < numArrays; e++) {
+                    auto z = reinterpret_cast<T *>(output->bufferWithOffset(offsets[e]));
+
+                    auto xBuffer = inputs[e]->bufferAsT<T>();
+                    auto xShapeInfo = inputs[e]->shapeInfo();
+                    auto xShape = shape::shapeOf(xShapeInfo);
+                    auto xStride = shape::stride(xShapeInfo);
+                    auto xRank = shape::rank(xShapeInfo);
+                    auto xLength = inputs[e]->lengthOf();
+
+                    if (order == 'c') {
+                        for (uint i = 0; i < xLength; i++) {
+                            shape::ind2subC(xRank, xShape, i, xLength, xCoord);
+                            auto xOffset = shape::getOffset(0, xShape, xStride, xCoord, xRank);
+                            z[i] = xBuffer[xOffset];
+                        }
+                    } else if (order == 'f') {
+                        for (uint i = 0; i < xLength; i++) {
+                            shape::ind2sub(xRank, xShape, i, xLength, xCoord);
+                            auto xOffset = shape::getOffset(0, xShape, xStride, xCoord, xRank);
+                            z[i] = xBuffer[xOffset];
+                        }
+                    }
+                }
             }
 
             void flatten(std::vector<NDArray*> &inputs, NDArray *output, char order) {
