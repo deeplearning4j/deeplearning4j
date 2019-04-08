1ceee081ec694782e546bc99f8616a573b20805d
alexanderst <alexander.stoyakin@gmail.com>
2019-04-08 14:45:48 +0000

    Test

---
+++ nd4j/nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/main/java/org/nd4j/jita/allocator/impl/MemoryTracker.java
@@ -0,0 +1,47 @@
+package org.nd4j.jita.allocator.impl;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class MemoryTracker {
+
+    private List<AtomicLong> allocatedPerDevice = new ArrayList<>();
+    private List<AtomicLong> cachedPerDevice = new ArrayList<>();
+    private List<AtomicLong> totalPerDevice = new ArrayList<>();
+    private static MemoryTracker INSTANCE = new MemoryTracker();
+
+    private MemoryTracker() {
+        for (int i = 0; i < Nd4j.getAffinityManager().getNumberOfDevices(); ++i) {
+            allocatedPerDevice.get(i).set(0);
+            cachedPerDevice.get(i).set(0);
+            totalPerDevice.get(i).set(0);
+        }
+    }
+
+    public static MemoryTracker getInstance() {
+        return INSTANCE;
+    }
+
+    public AtomicLong getAllocated(int deviceId) {
+        return allocatedPerDevice.get(deviceId);
+    }
+
+    public AtomicLong getCached(int deviceId) {
+        return cachedPerDevice.get(deviceId);
+    }
+
+    public AtomicLong getTotal(int deviceId) {
+        return cachedPerDevice.get(deviceId);
+    }
+
+    public synchronized void incrementAllocated(int deviceId) {
+        allocatedPerDevice.get(deviceId).getAndIncrement();
+        totalPerDevice.get(deviceId).getAndIncrement();
+    }
+
+    public synchronized void incrementCached(int deviceId) {
+        cachedPerDevice.get(deviceId).getAndIncrement();
+        totalPerDevice.get(deviceId).getAndIncrement();
+    }
+}
---
+++ nd4j/nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/test/java/org/nd4j/jita/allocator/AllocatorTest.java
@@ -0,0 +1,30 @@
+package test.org.nd4j.jita.allocator;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.nd4j.jita.allocator.impl.MemoryTracker;
+import static org.junit.Assert.assertTrue;
+
+
+class AllocatorTest {
+
+
+    @Test
+    public void test1() {
+        int deviceId = 0;
+        MemoryTracker tracker = MemoryTracker.getInstance();
+	assertTrue(false);
+        assertTrue(0 == tracker.getAllocated(deviceId).get());
+        assertTrue(0 == tracker.getCached(deviceId).get());
+        assertTrue(0 == tracker.getTotal(deviceId).get());
+
+        tracker.incrementAllocated(deviceId);
+        assertTrue(1 == tracker.getAllocated(deviceId).get());
+
+        tracker.incrementCached(deviceId);
+        assertTrue(1 == tracker.getCached(deviceId).get());
+
+        assertTrue(2 == tracker.getTotal(deviceId).get());
+    }
+}
