510ec68a277bd893b597c958d864841e3cc7fe60
Alex <alexander.stoyakin@gmail.com>
2019-04-17 15:56:27 +0300

    Performance test

--- deeplearning4j/deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/TsneTest.java
+++ deeplearning4j/deeplearning4j-nlp-parent/deeplearning4j-nlp/src/test/java/org/deeplearning4j/TsneTest.java
@@ -17,6 +17,7 @@
 package org.deeplearning4j;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 import org.deeplearning4j.models.embeddings.inmemory.InMemoryLookupTable;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
@@ -91,6 +92,103 @@ public class TsneTest {
                 tsne.saveAsFile(cacheList, new File(outDir, "out.txt").getAbsolutePath());
             }
         }
+    }
+
+    @Test
+    public void testPerformance() throws Exception {
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        for (boolean syntheticData : new boolean[]{false, true}) {
+            for (WorkspaceMode wsm : new WorkspaceMode[]{WorkspaceMode.NONE, WorkspaceMode.ENABLED}) {
+                log.info("Starting test: WSM={}, syntheticData={}", wsm, syntheticData);
+
+                //STEP 1: Initialization
+                int iterations = 300;
+                //create an n-dimensional array of doubles
+                Nd4j.setDataType(DataType.DOUBLE);
+                List<String> cacheList = new ArrayList<>(); //cacheList is a dynamic array of strings used to hold all words
+
+                //STEP 2: Turn text input into a list of words
+                INDArray weights;
+                if(syntheticData){
+                    weights = Nd4j.rand(500, 20);
+                } else {
+                    log.info("Load & Vectorize data....");
+                    File wordFile = new ClassPathResource("deeplearning4j-tsne/words.txt").getFile();   //Open the file
+                    //Get the data of all unique word vectors
+                    Pair<InMemoryLookupTable, VocabCache> vectors = WordVectorSerializer.loadTxt(wordFile);
+                    VocabCache cache = vectors.getSecond();
+                    weights = vectors.getFirst().getSyn0();    //seperate weights of unique words into their own list
+
+                    for (int i = 0; i < cache.numWords(); i++)   //seperate strings of words into their own list
+                        cacheList.add(cache.wordAtIndex(i));
+                }
+
+                //STEP 3: build a dual-tree tsne to use later
+                log.info("Build model....");
+                BarnesHutTsne tsne = new BarnesHutTsne.Builder()
+                        .setMaxIter(iterations)
+                        .theta(0.5)
+                        .normalize(false)
+                        .learningRate(500)
+                        .useAdaGrad(false)
+                        .workspaceMode(wsm)
+                        .build();
+
+
+                //STEP 4: establish the tsne values and save them to a file
+                log.info("Store TSNE Coordinates for Plotting....");
+                File outDir = testDir.newFolder();
+                tsne.fit(weights);
+                tsne.saveAsFile(cacheList, new File(outDir, "out.txt").getAbsolutePath());
+            }
+        }
+        watch.stop();
+        System.out.println("Elapsed time : " + watch);
+    }
+
+    @Test
+    public void testTSNEPerformance() throws Exception {
+
+            for (WorkspaceMode wsm : new WorkspaceMode[]{WorkspaceMode.NONE, WorkspaceMode.ENABLED}) {
+
+                //STEP 1: Initialization
+                int iterations = 100;
+                //create an n-dimensional array of doubles
+                Nd4j.setDataType(DataType.DOUBLE);
+                List<String> cacheList = new ArrayList<>(); //cacheList is a dynamic array of strings used to hold all words
+
+                //STEP 2: Turn text input into a list of words
+                INDArray weights = Nd4j.rand(500, 20);
+
+                StopWatch watch = new StopWatch();
+                watch.start();
+                //STEP 3: build a dual-tree tsne to use later
+                log.info("Build model....");
+                BarnesHutTsne tsne = new BarnesHutTsne.Builder()
+                        .setMaxIter(iterations)
+                        .theta(0.5)
+                        .normalize(false)
+                        .learningRate(500)
+                        .useAdaGrad(false)
+                        .workspaceMode(wsm)
+                        .build();
+
+                watch.stop();
+                System.out.println("Elapsed time for construction: " + watch);
+
+                //STEP 4: establish the tsne values and save them to a file
+                log.info("Store TSNE Coordinates for Plotting....");
+                File outDir = testDir.newFolder();
+
+                watch.reset();
+                watch.start();
+                tsne.fit(weights);
+                watch.stop();
+                System.out.println("Elapsed time for fit: " + watch);
+                tsne.saveAsFile(cacheList, new File(outDir, "out.txt").getAbsolutePath());
+            }
     }
 
 }
