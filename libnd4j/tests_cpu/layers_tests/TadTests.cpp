7f3d32315acc56abdcf03f940cf3b27bafff1d4e
raver119 <raver119@gmail.com>
2019-04-18 17:26:20 +0300

    one more test

--- libnd4j/tests_cpu/layers_tests/TadTests.cpp
+++ libnd4j/tests_cpu/layers_tests/TadTests.cpp
@@ -269,13 +269,6 @@ TEST_F(TadTests, test_column_1) {
     ASSERT_EQ(1, shape::rank(tadPack.primaryShapeInfo()));
     ASSERT_EQ(5, shape::length(tadPack.primaryShapeInfo()));
     ASSERT_TRUE(shape::isVector(tadPack.primaryShapeInfo()));
-
-    auto scalarViewPack = nd4j::ConstantTadHelper::getInstance()->tadForDimensions(tadPack.primaryShapeInfo(), 0);
-
-    shape::printShapeInfoLinear("scalar view", scalarViewPack.primaryShapeInfo());
-    ASSERT_EQ(0, shape::rank(scalarViewPack.primaryShapeInfo()));
-    ASSERT_EQ(1, shape::length(scalarViewPack.primaryShapeInfo()));
-    ASSERT_TRUE(shape::isScalar(scalarViewPack.primaryShapeInfo()));
 }
 
 
