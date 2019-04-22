d1726b4935fce83bd7b3075a51eb6b532ce831ba
Alex Black <blacka101@gmail.com>
2019-04-22 23:50:26 +1000

    DTypeTests - add attention layers; fix SameDiffGraphVertex dtype issues (#7592)

--- libnd4j/include/ops/declarable/generic/parity_ops/strided_slice.cpp
+++ libnd4j/include/ops/declarable/generic/parity_ops/strided_slice.cpp
@@ -618,7 +618,7 @@ namespace nd4j {
             REQUIRE_TRUE(_preprocess_strided_slice(&indices, &final_shape, input_shape, begin, end, strides, begin_mask, ellipsis_mask, end_mask, new_axis_mask, shrink_axis_mask, &is_identity, &is_simple_slice, &is_dim0), 0, "StridedSliceBP: shape calculation failed");
 
 			//Zero output array, so unused elements have 0 gradient
-			output->assign(0.0);
+			output->nullify();
 
             if(indices.size() == 3 && (indices[1] - indices[0]) == 1) {
                 output->p(indices[0], *epsNext);
