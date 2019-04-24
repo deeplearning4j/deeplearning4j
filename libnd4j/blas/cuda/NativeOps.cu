631ed2599c4e7e5564609faf41245f102e0b0a54
raver119 <raver119@gmail.com>
2019-04-24 09:36:33 +0300

    [WIP] more fixes (#7616)
    * sortTad typo fix
    
    * - CUDA sortAlongDimension rewritten
    - additional shuffle tests
    
    * - shuffle fix
    - cached sort along dim
    
    * additional iteration

--- libnd4j/blas/cuda/NativeOps.cu
+++ libnd4j/blas/cuda/NativeOps.cu
@@ -2361,7 +2361,7 @@ void NativeOps::shuffle(Nd4jPointer *extras,
     dim3 launchDims(256, 512, 8192);
     BUILD_SINGLE_SELECTOR(xType, shuffleKernelGeneric, (launchDims, stream, dX, dxShape, dZ, N, shuffleMap,  tadOnlyShapeInfo, tadOffset), LIBND4J_TYPES);
 
-	DEBUG_KERNEL(stream, 0);
+    nd4j::DebugHelper::checkErrorCode(stream, "shuffle(...) failed");
 }
 
 /*
@@ -3223,8 +3223,12 @@ void NativeOps::sortTad(Nd4jPointer *extraPointers,
 						Nd4jLong *tadOffsets,
 						bool descending) {
     // to be implemented
-    cudaStream_t *stream = reinterpret_cast<cudaStream_t *>(&extraPointers[1]);
-    dim3 launchDims(512, 512, 32768);
+    auto stream = reinterpret_cast<cudaStream_t *>(&extraPointers[1]);
+
+    auto tadPack = nd4j::ConstantTadHelper::getInstance()->tadForDimensions(xShapeInfo, dimension, dimensionLength);
+
+    dim3 launchDims(tadPack.numberOfTads(), 1024, 33768);
+
 	auto xType = nd4j::ArrayOptions::dataType(xShapeInfo);
     BUILD_SINGLE_SELECTOR(xType, oesTadGeneric, (launchDims, stream, dX, dXShapeInfo, dimension, dimensionLength, tadShapeInfo, tadOffsets, descending), LIBND4J_TYPES);                     
     
