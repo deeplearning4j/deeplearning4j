8baf57a4678e6f11c563c899f2b113993aba7c9b
raver119 <raver119@gmail.com>
2019-04-26 09:26:43 +0300

    one more test

--- nd4j/nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/test/java/org/nd4j/jita/allocator/DeviceLocalNDArrayTests.java
+++ nd4j/nd4j-backends/nd4j-backend-impls/nd4j-cuda/src/test/java/org/nd4j/jita/allocator/DeviceLocalNDArrayTests.java
@@ -16,10 +16,51 @@ import static org.junit.Assert.assertEquals;
 public class DeviceLocalNDArrayTests {
 
     @Test
-    public void testDeviceLocalArray() throws Exception{
+    public void testDeviceLocalArray_1() throws Exception{
         val arr = Nd4j.create(DataType.DOUBLE, 10, 10);
 
         val dl = new DeviceLocalNDArray(arr);
+
+        for (int e = 0; e < Nd4j.getAffinityManager().getNumberOfDevices(); e++) {
+            val t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dl.get().addi(1.0);
+                    Nd4j.getExecutioner().commit();
+                }
+            });
+            Nd4j.getAffinityManager().attachThreadToDevice(t, e);
+            t.start();
+            t.join();
+        }
+    }
+
+
+    @Test
+    public void testDeviceLocalArray_2() throws Exception{
+        val shape = new long[]{10, 10};
+        val arr = Nd4j.create(DataType.DOUBLE, shape);
+
+        val dl = new DeviceLocalNDArray(arr);
+
+        for (int e = 0; e < Nd4j.getAffinityManager().getNumberOfDevices(); e++) {
+            val t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        val tmp = Nd4j.create(DataType.DOUBLE, shape);
+                        tmp.addi(1.0);
+                        Nd4j.getExecutioner().commit();
+                    }
+                }
+            });
+            Nd4j.getAffinityManager().attachThreadToDevice(t, e);
+            t.start();
+            t.join();
+
+            System.gc();
+        }
+        System.gc();
 
         for (int e = 0; e < Nd4j.getAffinityManager().getNumberOfDevices(); e++) {
             val t = new Thread(new Runnable() {
