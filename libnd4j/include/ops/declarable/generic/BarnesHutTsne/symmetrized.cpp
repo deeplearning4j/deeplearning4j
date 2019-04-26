8a7f8128c01fd5c2040e150637f6579c50a59f33
shugeo <sgazeos@gmail.com>
2019-04-26 16:18:06 +0300

    Eliminated waste logging.

--- libnd4j/include/ops/declarable/generic/BarnesHutTsne/symmetrized.cpp
+++ libnd4j/include/ops/declarable/generic/BarnesHutTsne/symmetrized.cpp
@@ -64,7 +64,9 @@ namespace ops  {
                 N = INT_ARG(0);
 
             NDArray* rowCounts = NDArrayFactory::create_<Nd4jLong>('c', {1, N}); //rowP->dup();
+            //srowCounts->assign(0);
             Nd4jLong len = helpers::barnes_row_count(rowP, colP, N, *rowCounts);
+            //rowCounts->printBuffer("Row Counts");
             if (len <= 0) throw std::runtime_error("barnes_symmetrized: Cannot allocate shape due non-positive len.");
             rowCountsPtr = rowCounts;
             ALLOCATE(outShapeInfo, block.workspace(), shape::shapeInfoLength(2), Nd4jLong);
