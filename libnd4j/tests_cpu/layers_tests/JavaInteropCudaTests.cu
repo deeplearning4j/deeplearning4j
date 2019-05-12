08b53fe782098f17548d0c6054d01f3386a3a411
raver119 <raver119@gmail.com>
2019-05-12 19:42:31 +0300

    one more test for CUDA

---
+++ libnd4j/tests_cpu/layers_tests/JavaInteropCudaTests.cu
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+//
+// @author raver119@gmail.com
+//
+
+#include "testlayers.h"
+#include <helpers/PointersManager.h>
+#include <array/ExtraArguments.h>
+#include <ops/declarable/CustomOperations.h>
+#include <array>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+using namespace nd4j;
+using namespace nd4j::ops;
+
+class JavaInteropCudaTests : public testing::Test {
+public:
+
+};
+
+TEST_F(JavaInteropCudaTests, test_DeclarableOp_execution_1) {
+    auto x = NDArrayFactory::create<float>('c', {3, 5});
+    auto y = NDArrayFactory::create<float>('c', {5}, {1.f, 1.f, 1.f, 1.f, 1.f});
+    auto e = NDArrayFactory::create<float>('c', {3, 5});
+    e.assign(1.f);
+
+    nd4j::ops::add op;
+    NativeOps nativeOps;
+    Context context(1);
+
+    context.setCudaStream(LaunchContext::defaultContext()->getCudaStream());
+    context.setInputArray(0, x.buffer(), x.shapeInfo(), x.specialBuffer(), x.specialShapeInfo());
+    context.setInputArray(1, y.buffer(), y.shapeInfo(), y.specialBuffer(), y.specialShapeInfo());
+
+    context.setOutputArray(0, x.buffer(), x.shapeInfo(), x.specialBuffer(), x.specialShapeInfo());
+
+    PointersManager pm(LaunchContext::defaultContext(), "test_DeclarableOp_execution_1");
+    nativeOps.execCustomOp(nullptr, op.getOpHash(), &context);
+
+    pm.synchronize();
+
+    ASSERT_EQ(e, x);
+}
+
