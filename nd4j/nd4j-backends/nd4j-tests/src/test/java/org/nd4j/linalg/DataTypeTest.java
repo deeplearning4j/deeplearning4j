2d40966960e017f239f05464d577cdc9af2015f0
Alex <alexander.stoyakin@gmail.com>
2019-05-13 17:27:18 +0300

    New test

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/DataTypeTest.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/DataTypeTest.java
@@ -12,126 +12,26 @@ import static org.junit.Assert.assertEquals;
 public class DataTypeTest {
 
     @Test
-    public void testUINT16() throws IOException {
-            INDArray in1 = Nd4j.ones(DataType.UINT16);
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test1.bin"));
+    public void testDataTypes() throws IOException {
+        for (DataType type : DataType.values()) {
+            INDArray in1 = Nd4j.ones(type);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
             oos.writeObject(in1);
 
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1.bin"));
+            ByteArrayInputStream bios = new ByteArrayInputStream(baos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bios);
             INDArray in2 = null;
             try {
                 in2 = (INDArray) ois.readObject();
-            } catch (ClassNotFoundException e) {
+            }
+            catch (ClassNotFoundException e) {
 
             }
 
             assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testUINT32() throws IOException {
-
-            INDArray in1 = Nd4j.ones(DataType.UINT32);
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test2.bin"));
-            oos.writeObject(in1);
-
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test2.bin"));
-            INDArray in2 = null;
-            try {
-                in2 = (INDArray) ois.readObject();
-            } catch (ClassNotFoundException e) {
-
-            }
-
-            assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testUINT64() throws IOException {
-
-            INDArray in1 = Nd4j.ones(DataType.UINT64);
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test3.bin"));
-            oos.writeObject(in1);
-
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test3.bin"));
-            INDArray in2 = null;
-            try {
-                in2 = (INDArray) ois.readObject();
-            } catch(ClassNotFoundException e) {
-
-            }
-
-            assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testInt() throws IOException {
-        INDArray in1 = Nd4j.ones(DataType.INT);
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test1.bin"));
-        oos.writeObject(in1);
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1.bin"));
-        INDArray in2 = null;
-        try {
-            in2 = (INDArray) ois.readObject();
-        } catch (ClassNotFoundException e) {
 
         }
-
-        assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testBfloat16() throws IOException {
-        INDArray in1 = Nd4j.ones(DataType.BFLOAT16);
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test1.bin"));
-        oos.writeObject(in1);
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1.bin"));
-        INDArray in2 = null;
-        try {
-            in2 = (INDArray) ois.readObject();
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testFloat() throws IOException {
-        INDArray in1 = Nd4j.ones(DataType.FLOAT);
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test1.bin"));
-        oos.writeObject(in1);
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test1.bin"));
-        INDArray in2 = null;
-        try {
-            in2 = (INDArray) ois.readObject();
-        } catch (ClassNotFoundException e) {
-
-        }
-
-        assertEquals(in1, in2);
-    }
-
-    @Test
-    public void testDouble() throws IOException {
-        for (int i = 0; i < 10; ++i) {
-            INDArray in1 = Nd4j.rand(DataType.DOUBLE, new int[]{100, 100});
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("test.bin"));
-            oos.writeObject(in1);
-
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("test.bin"));
-            INDArray in2 = null;
-            try {
-                in2 = (INDArray) ois.readObject();
-            } catch(ClassNotFoundException e) {
-
-            }
-
-            assertEquals(in1, in2);
-        }
-
     }
 }
