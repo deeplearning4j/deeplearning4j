c613baf9e9bd5ee9a00ac786c4f9ceb7a4d1ed05
raver119 <raver119@gmail.com>
2019-05-13 18:22:06 +0300

    CPU deserialization fixes

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/DataTypeTest.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/DataTypeTest.java
@@ -1,40 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
 package org.nd4j.linalg;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.factory.Nd4jBackend;
 
 import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
-public class DataTypeTest {
+@RunWith(Parameterized.class)
+@Slf4j
+public class DataTypeTest extends BaseNd4jTest {
+    public DataTypeTest(Nd4jBackend backend) {
+        super(backend);
+    }
 
     @Test
-    public void testDataTypes() throws IOException {
-        for (DataType type : DataType.values()) {
+    public void testDataTypes() throws Exception {
+        for (val type : DataType.values()) {
             if (DataType.UTF8.equals(type) || DataType.UNKNOWN.equals(type) || DataType.COMPRESSED.equals(type))
                 continue;
 
-            INDArray in1 = Nd4j.ones(type);
+            val in1 = Nd4j.ones(type, 10, 10);
 
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(in1);
-
-            ByteArrayInputStream bios = new ByteArrayInputStream(baos.toByteArray());
-            ObjectInputStream ois = new ObjectInputStream(bios);
-            INDArray in2 = null;
+            val baos = new ByteArrayOutputStream();
+            val oos = new ObjectOutputStream(baos);
             try {
-                in2 = (INDArray) ois.readObject();
+                oos.writeObject(in1);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed for data type [" + type + "]", e);
             }
-            catch (ClassNotFoundException e) {
 
+            val bios = new ByteArrayInputStream(baos.toByteArray());
+            val ois = new ObjectInputStream(bios);
+            try {
+                val in2 = (INDArray) ois.readObject();
+                assertEquals("Failed for data type [" + type + "]", in1, in2);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed for data type [" + type + "]", e);
             }
-
-            assertEquals(in1, in2);
-
         }
+    }
+
+    @Override
+    public char ordering() {
+        return 'c';
     }
 }
