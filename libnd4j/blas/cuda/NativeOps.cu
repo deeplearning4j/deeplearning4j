5a7b9ae5850323c817d2b2be6d39bc8ebef59b52
raver119 <raver119@gmail.com>
2019-06-03 20:01:11 +0300

    inpectArray updated

--- libnd4j/blas/cuda/NativeOps.cu
+++ libnd4j/blas/cuda/NativeOps.cu
@@ -3041,8 +3041,9 @@ void NativeOps::scatterUpdate(Nd4jPointer *extraPointers, int opCode, int numOfS
 }
 
 void NativeOps::inspectArray(Nd4jPointer *extraPointers, Nd4jPointer buffer, Nd4jLong *shapeInfo, Nd4jPointer specialBuffer, Nd4jLong *specialShapeInfo, Nd4jPointer debugInfo) {
+    LaunchContext lc(extraPointers[1], extraPointers[4], extraPointers[5], extraPointers[3]);
     auto p = reinterpret_cast<nd4j::DebugInfo*>(debugInfo);
-    NDArray array(buffer, shapeInfo, nullptr);
+    NDArray array(buffer, specialBuffer, shapeInfo, &lc);
     nd4j::DebugHelper::retrieveDebugStatistics(p, &array);
 }
 
