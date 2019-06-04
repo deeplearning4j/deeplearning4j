1d991c8c1f825d18d0f1d3ee100d9c8068b40126
Alex Black <blacka101@gmail.com>
2019-06-04 19:59:06 +1000

    Small fixes

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
@@ -4348,20 +4348,6 @@ public class Nd4jTestsC extends BaseNd4jTest {
         assertEquals(1.0, sum, 0.001);
     }
 
-    @Test
-    public void sumResultArrayEdgeCase() {
-        INDArray delta = Nd4j.create(1, 3);
-        delta.assign(Nd4j.rand(delta.shape()));
-
-        INDArray out = delta.sum(0);
-
-        INDArray out2 = Nd4j.zeros(new long[] {1, 3}, 'c');
-        INDArray res = delta.sum(out2, 0);
-
-        assertEquals(out, out2);
-        assertTrue(res == out2);
-    }
-
 
     @Test
     public void tesAbsReductions1() {
