6cdf54c7593f9c8080a1ea2405d48b3c1cc1cabe
AlexDBlack <blacka101@gmail.com>
2019-08-16 12:57:08 +1000

    Legacy deserialization cleanup
    Signed-off-by: AlexDBlack <blacka101@gmail.com>

--- deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/serde/legacy/LegacyIntArrayDeserializer.java
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/conf/serde/legacy/LegacyIntArrayDeserializer.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ******************************************************************************/
 
-package org.deeplearning4j.nn.conf.serde.legacyformat;
+package org.deeplearning4j.nn.conf.serde.legacy;
 
 import org.nd4j.shade.jackson.core.JsonParser;
 import org.nd4j.shade.jackson.core.JsonProcessingException;
