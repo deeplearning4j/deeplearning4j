4897e09de58e24c3b01fd502974e5893e60b8998
raver119 <raver119@gmail.com>
2019-09-06 19:17:40 +0300

    initial commit
    Signed-off-by: raver119 <raver119@gmail.com>

--- libnd4j/tests_cpu/layers_tests/DeclarableOpsTests2.cpp
+++ libnd4j/tests_cpu/layers_tests/DeclarableOpsTests2.cpp
@@ -96,6 +96,7 @@ TEST_F(DeclarableOpsTests2, gather_3) {
     delete result;
 }
 
+
 TEST_F(DeclarableOpsTests2, gather_4) {
 
     NDArray input('c', {2,3,4}, {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24});
