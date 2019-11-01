7802a15245f4726c1568f7c1f434f36596cb2ee5
shugeo <sgazeos@gmail.com>
2019-11-01 21:34:53 +0200

    Added cuda implementation for helper of poisson distribution.

--- libnd4j/include/ops/declarable/helpers/cuda/random.cu
+++ libnd4j/include/ops/declarable/helpers/cuda/random.cu
@@ -23,18 +23,75 @@
 #include <vector>
 #include <memory>
 #include <graph/Context.h>
+#include <helpers/RandomLauncher.h>
+
 namespace nd4j {
 namespace ops {
 namespace helpers {
 
 
-    void fillRandomGamma(graph::LaunchContext* context, graph::RandomGenerator& rng, NDArray* alpha, NDArray* beta, NDArray* output) {
-    }
-
-    void fillRandomPoisson(graph::LaunchContext* context, graph::RandomGenerator& rng, NDArray* lambda, NDArray* output) {
+    void fillRandomGamma(LaunchContext* context, graph::RandomGenerator& rng, NDArray* alpha, NDArray* beta, NDArray* output) {
 
     }
 
+    /*
+ * algorithm Poisson generator based upon the inversion by sequential search
+init:
+     Let x ← 0, p ← e−λ, s ← p.
+     using uniformly random sequence U (u in U) distributed at [0, 1].
+while u > s do:
+     x ← x + 1.
+     p ← p * λ / x.
+     s ← s + p.
+return x.
+ * */
+    template <typename T>
+    static __global__ void fillPoissonKernel(T* uList, Nd4jLong uLength, T* lambda, Nd4jLong* lambdaShape, T* output,
+            Nd4jLong* outputShape) {
+
+        __shared__ Nd4jLong step;
+
+        if (threadIdx.x == 0) {
+            step = shape::length(lambdaShape);
+        }
+        __syncthreads();
+
+        for (auto k = blockIdx.x; k < (int)uLength; k += gridDim.x) {
+            auto pos = k * step;
+            auto u = uList[k];
+            for (auto e = threadIdx.x; e < step; e += blockDim.x) {
+                auto p = math::nd4j_exp<T,T>(-lambda[e]);
+                auto s = p;
+                auto x = T(0.f);
+                while (u > s) {
+                    x += T(1.);
+                    p *= lambda[e] / x;
+                    s += p;
+                }
+                output[pos + e] = x;
+            }
+        }
+    }
+
+    template <typename T>
+    static void fillRandomPoisson_(LaunchContext* context, graph::RandomGenerator& rng, NDArray* lambda, NDArray* output) {
+        auto shift = output->lengthOf() / lambda->lengthOf();
+        NDArray uniform('c', {shift}, output->dataType());
+        auto stream = context->getCudaStream();
+        // fill up uniform with given length
+        RandomLauncher::fillUniform(context, rng, &uniform, 0., 1.);
+        fillPoissonKernel<T><<<128, 256, 128, *stream>>>(uniform.dataBuffer()->specialAsT<T>(), uniform.lengthOf(),
+                lambda->dataBuffer()->specialAsT<T>(), lambda->specialShapeInfo(),
+                output->dataBuffer()->specialAsT<T>(), output->specialShapeInfo());
+    }
+
+    void fillRandomPoisson(LaunchContext* context, graph::RandomGenerator& rng, NDArray* lambda, NDArray* output) {
+        NDArray::prepareSpecialUse({output}, {lambda});
+        BUILD_SINGLE_SELECTOR(output->dataType(), fillRandomPoisson_, (context, rng, lambda, output), FLOAT_NATIVE);
+        NDArray::registerSpecialUse({output}, {lambda});
+    }
+
+    BUILD_SINGLE_TEMPLATE(template void fillRandomPoisson_, (LaunchContext* context, graph::RandomGenerator& rng, NDArray* lambda, NDArray* output), FLOAT_NATIVE);
 }
 }
 }
