abd2017a0ae532e1f2789c9045ef274e04de988b
Alex Black <blacka101@gmail.com>
2019-11-27 16:35:05 +1100

    Add ignore for known issue with non_max_suppression_v2/float16 test (#85)
    Signed-off-by: AlexDBlack <blacka101@gmail.com>

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphs/TFGraphTestAllSameDiff.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/imports/TFGraphs/TFGraphTestAllSameDiff.java
@@ -126,7 +126,10 @@ public class TFGraphTestAllSameDiff {   //Note: Can't extend BaseNd4jTest here a
             "fake_quant/min_max_args_per_channel.*",
 
             // 2019/11/15 - failure https://github.com/eclipse/deeplearning4j/issues/8403
-            "resize_bilinear/int32.*"
+            "resize_bilinear/int32.*",
+
+            // Suggesting TF 1.15 bug - see https://github.com/eclipse/deeplearning4j/issues/8449
+            "non_max_suppression_v2/float16.*"
     };
 
     /* As per TFGraphTestList.printArraysDebugging - this field defines a set of regexes for test cases that should have
