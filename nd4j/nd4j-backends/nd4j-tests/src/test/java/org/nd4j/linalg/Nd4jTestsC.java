ddf70ac450cb4a108fa7a0889d710ac743487d7a
Alex Black <blacka101@gmail.com>
2020-02-03 22:18:01 +1100

    Avoid double printing of start/stop test in a few cases (#210)
    Signed-off-by: AlexDBlack <blacka101@gmail.com>

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/Nd4jTestsC.java
@@ -138,7 +138,6 @@ public class Nd4jTestsC extends BaseNd4jTest {
 
     @Before
     public void before() throws Exception {
-        super.beforeTest();
         Nd4j.setDataType(DataType.DOUBLE);
         Nd4j.getRandom().setSeed(123);
         Nd4j.getExecutioner().enableDebugMode(false);
@@ -147,7 +146,6 @@ public class Nd4jTestsC extends BaseNd4jTest {
 
     @After
     public void after() throws Exception {
-        super.afterTest();
         Nd4j.setDataType(initialType);
     }
 
