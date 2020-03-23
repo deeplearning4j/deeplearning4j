8b25dd8830e013cb060197241a43b57aa9f170e6
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2020-03-23 12:40:17 +0200

    lstm mismatch with c++ op issue
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/layers/recurrent/LSTMLayer.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/impl/layers/recurrent/LSTMLayer.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (c) 2015-2019 Skymind, Inc.
+/* ******************************************************************************
+ * Copyright (c) 2020 Konduit K.K.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
@@ -13,7 +13,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ******************************************************************************/
-
 package org.nd4j.linalg.api.ops.impl.layers.recurrent;
 
 import lombok.Getter;
@@ -25,137 +24,104 @@ import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.DynamicCustomOp;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.config.LSTMConfiguration;
-import org.nd4j.linalg.api.ops.impl.layers.recurrent.config.RnnDataFormat;
-<<<<<<< HEAD
-import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.LSTMWeights;
-=======
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.LSTMLayerWeights;
->>>>>>> df614a6597... Fix LSTMLayer inputs order
-import org.tensorflow.framework.AttrValue;
-import org.tensorflow.framework.GraphDef;
-import org.tensorflow.framework.NodeDef;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-/**
- * LSTM layer implemented as a single operation.
- * Implementation of operation for LSTM layer with optional peep hole connections.<br>
- * S. Hochreiter and J. Schmidhuber. "Long Short-Term Memory". Neural Computation and <a href="https://research.google.com/pubs/archive/43905.pdf">https://research.google.com/pubs/archive/43905.pdf</a><br>
- * Hasim Sak, Andrew Senior, and Francoise Beaufays. "Long short-term memory recurrent neural network architectures for large scale acoustic modeling." INTERSPEECH, 2014.<br>
- * See also: <a href="https://arxiv.org/pdf/1503.04069.pdf">https://arxiv.org/pdf/1503.04069.pdf</a><br>
- * <p>
- * See also {@link LSTMBlockCell} - lstmBlockCell op is used internally at C++ level for computation.<br>
- * <br>
- * Input arrays:<br>
- * 0: max sequence length; long/int64 scalar<br>
- * 1: input [seqLength, bS, inSize] at time t<br>
- * 2: previous/initial cell state  [bS, numUnits]<br>
- * 3: previous/initial output [bS, numUnits]<br>
- * 4: Weights - concatenated (input-to-hidden, hidden-to-hidden weights)  weights, [(inSize+numUnits), 4*numUnits]<br>
- * 5: weights - cell peephole (t-1) connections to input modulation gate, [numUnits]<br>
- * 6: weights - cell peephole (t-1) connections to forget gate, [numUnits]<br>
- * 7: weights - cell peephole (t) connections to output gate, [numUnits]<br>
- * 8: biases, shape [4*numUnits]<br>
- * <br>
- * Input integer arguments: set via {@link LSTMConfiguration}<br>
- * 0: if not zero, provide peephole connections<br>
- * 1: Data format - 0=TNS=[seqLen,mb,size]; 1=NST=[mb,size,seqLen]; 2=NTS=[mb,seqLen,size]<br>
- * <br>
- * Input float arguments: set via {@link LSTMConfiguration}<br>
- * 0: the bias added to forget gates in order to reduce the scale of forgetting in the beginning of the training<br>
- * 1: clipping value for cell state, if it is not equal to zero, then cell state is clipped<br>
- * <p>
- * Output arrays:<br>
- * 0: i      - Input modulation gate activations, rank 3, shape as per dataFormat<br>
- * 1: c (cs) - Cell state (pre tanh), rank 3, shape as per dataFormat<br>
- * 2: f      - Output - forget gate activations, rank 3, shape as per dataFormat<br>
- * 3: o      - Output - output gate activations, rank 3, shape as per dataFormat<br>
- * 4: z (ci) - Output - block input, rank 3, shape as per dataFormat<br>
- * 5: h (co) - Cell state, post tanh, rank 3, shape as per dataFormat<br>
- * 6: y (h)  - Current cell output, rank 3, shape as per dataFormat<br>
- *
- * @author Alex Black
- */
-public class LSTMLayer extends DynamicCustomOp {
 
-    private LSTMConfiguration configuration;
+    /**
+     * LSTM layer implemented as a single operation.
+     * Implementation of operation for LSTM layer with optional peep hole connections.<br>
+     * S. Hochreiter and J. Schmidhuber. "Long Short-Term Memory". Neural Computation and <a href="https://research.google.com/pubs/archive/43905.pdf">https://research.google.com/pubs/archive/43905.pdf</a><br>
+     * Hasim Sak, Andrew Senior, and Francoise Beaufays. "Long short-term memory recurrent neural network architectures for large scale acoustic modeling." INTERSPEECH, 2014.<br>
+     * See also: <a href="https://arxiv.org/pdf/1503.04069.pdf">https://arxiv.org/pdf/1503.04069.pdf</a><br>
+     * <p>
+     * See also {@link LSTMBlockCell} - lstmBlockCell op is used internally at C++ level for computation.<br>
+     * <br>
+     * Input arrays:<br>
+     * 0: max sequence length; long/int64 scalar<br>
+     * 1: input [seqLength, bS, inSize] at time t<br>
+     * 2: previous/initial cell state  [bS, numUnits]<br>
+     * 3: previous/initial output [bS, numUnits]<br>
+     * 4: Weights - concatenated (input-to-hidden, hidden-to-hidden weights)  weights, [(inSize+numUnits), 4*numUnits]<br>
+     * 5: weights - cell peephole (t-1) connections to input modulation gate, [numUnits]<br>
+     * 6: weights - cell peephole (t-1) connections to forget gate, [numUnits]<br>
+     * 7: weights - cell peephole (t) connections to output gate, [numUnits]<br>
+     * 8: biases, shape [4*numUnits]<br>
+     * <br>
+     * Input integer arguments: set via {@link LSTMConfiguration}<br>
+     * 0: if not zero, provide peephole connections<br>
+     * 1: Data format - 0=TNS=[seqLen,mb,size]; 1=NST=[mb,size,seqLen]; 2=NTS=[mb,seqLen,size]<br>
+     * <br>
+     * Input float arguments: set via {@link LSTMConfiguration}<br>
+     * 0: the bias added to forget gates in order to reduce the scale of forgetting in the beginning of the training<br>
+     * 1: clipping value for cell state, if it is not equal to zero, then cell state is clipped<br>
+     * <p>
+     * Output arrays:<br>
+     * 0: i      - Input modulation gate activations, rank 3, shape as per dataFormat<br>
+     * 1: c (cs) - Cell state (pre tanh), rank 3, shape as per dataFormat<br>
+     * 2: f      - Output - forget gate activations, rank 3, shape as per dataFormat<br>
+     * 3: o      - Output - output gate activations, rank 3, shape as per dataFormat<br>
+     * 4: z (ci) - Output - block input, rank 3, shape as per dataFormat<br>
+     * 5: h (co) - Cell state, post tanh, rank 3, shape as per dataFormat<br>
+     * 6: y (h)  - Current cell output, rank 3, shape as per dataFormat<br>
+     *
+     * @author Alex Black
+     */
+    public class LSTMLayer extends DynamicCustomOp {
 
-    @Getter
-<<<<<<< HEAD
-    private LSTMWeights weights;
-=======
-    private LSTMLayerWeights weights;
->>>>>>> df614a6597... Fix LSTMLayer inputs order
+        private LSTMConfiguration configuration;
 
-    public LSTMLayer() {
+        @Getter
+        private LSTMLayerWeights weights;
+
+        public LSTMLayer() {
+        }
+
+        public LSTMLayer(@NonNull SameDiff sameDiff, SDVariable maxTSLength, SDVariable x, SDVariable cLast, SDVariable yLast, LSTMLayerWeights weights, LSTMConfiguration configuration) {
+            super(null, sameDiff, weights.argsWithInputs(x, maxTSLength, cLast, yLast));
+            this.configuration = configuration;
+            this.weights = weights;
+            addIArgument(configuration.iArgs(true));
+            addTArgument(configuration.tArgs());
+        }
+
+        public LSTMLayer(INDArray x, INDArray cLast, INDArray yLast, INDArray maxTSLength, LSTMLayerWeights lstmWeights, LSTMConfiguration lstmConfiguration) {
+            super(null, null, lstmWeights.argsWithInputs(maxTSLength, x, cLast, yLast));
+            this.configuration = lstmConfiguration;
+            this.weights = lstmWeights;
+            addIArgument(configuration.iArgs(true));
+            addTArgument(configuration.tArgs());
+        }
+
+        @Override
+        public List<DataType> calculateOutputDataTypes(List<DataType> inputDataTypes) {
+            Preconditions.checkState(inputDataTypes != null && inputDataTypes.size() == 8, "Expected exactly 8 inputs to LSTMLayer, got %s", inputDataTypes);
+            //7 outputs, all of same type as input. Note that input 0 is max sequence length (int64), input 1 is actual input
+            DataType dt = inputDataTypes.get(1);
+            Preconditions.checkState(dt.isFPType(), "Input type 1 must be a floating point type, got %s", dt);
+            return Arrays.asList(dt, dt, dt, dt, dt, dt, dt);
+        }
+
+        @Override
+        public List<SDVariable> doDiff(List<SDVariable> grads) {
+            throw new UnsupportedOperationException("Not yet implemented");
+        }
+
+
+
+        @Override
+        public String opName() {
+            return "lstmLayer";
+        }
+
+        @Override
+        public Map<String, Object> propertiesForFunction() {
+            return configuration.toProperties(true);
+        }
+
     }
 
-<<<<<<< HEAD
-    public LSTMLayer(@NonNull SameDiff sameDiff, SDVariable maxTSLength, SDVariable x, SDVariable cLast, SDVariable yLast, LSTMWeights weights, LSTMConfiguration configuration) {
-        super(null, sameDiff, weights.argsWithInputs(maxTSLength, x, cLast, yLast));
-=======
-    public LSTMLayer(@NonNull SameDiff sameDiff, SDVariable maxTSLength, SDVariable x, SDVariable cLast, SDVariable yLast, LSTMLayerWeights weights, LSTMConfiguration configuration) {
-        super(null, sameDiff, weights.argsWithInputs(x, maxTSLength, cLast, yLast));
->>>>>>> df614a6597... Fix LSTMLayer inputs order
-        this.configuration = configuration;
-        this.weights = weights;
-        addIArgument(configuration.iArgs(true));
-        addTArgument(configuration.tArgs());
-    }
 
-<<<<<<< HEAD
-    public LSTMLayer(INDArray x, INDArray cLast, INDArray yLast, INDArray maxTSLength, LSTMWeights lstmWeights, LSTMConfiguration lstmConfiguration) {
-=======
-    public LSTMLayer(INDArray x, INDArray cLast, INDArray yLast, INDArray maxTSLength, LSTMLayerWeights lstmWeights, LSTMConfiguration lstmConfiguration) {
->>>>>>> df614a6597... Fix LSTMLayer inputs order
-        super(null, null, lstmWeights.argsWithInputs(maxTSLength, x, cLast, yLast));
-        this.configuration = lstmConfiguration;
-        this.weights = lstmWeights;
-        addIArgument(configuration.iArgs(true));
-        addTArgument(configuration.tArgs());
-    }
-
-    @Override
-    public List<DataType> calculateOutputDataTypes(List<DataType> inputDataTypes) {
-        Preconditions.checkState(inputDataTypes != null && inputDataTypes.size() == 9, "Expected exactly 9 inputs to LSTMLayer, got %s", inputDataTypes);
-        //7 outputs, all of same type as input. Note that input 0 is max sequence length (int64), input 1 is actual input
-        DataType dt = inputDataTypes.get(1);
-        Preconditions.checkState(dt.isFPType(), "Input type 1 must be a floating point type, got %s", dt);
-        return Arrays.asList(dt, dt, dt, dt, dt, dt, dt);
-    }
-
-    @Override
-    public List<SDVariable> doDiff(List<SDVariable> grads) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public void initFromTensorFlow(NodeDef nodeDef, SameDiff initWith, Map<String, AttrValue> attributesForNode, GraphDef graph) {
-        configuration = LSTMConfiguration.builder()
-                .forgetBias(attributesForNode.get("forget_bias").getF())
-                .clippingCellValue(attributesForNode.get("cell_clip").getF())
-                .peepHole(attributesForNode.get("use_peephole").getB())
-                .dataFormat(RnnDataFormat.TNS)  //Always time major for TF BlockLSTM
-                .build();
-        addIArgument(configuration.iArgs(true));
-        addTArgument(configuration.tArgs());
-    }
-
-    @Override
-    public String opName() {
-        return "lstmBlock";
-    }
-
-    @Override
-    public Map<String, Object> propertiesForFunction() {
-        return configuration.toProperties(true);
-    }
-
-    @Override
-    public String tensorflowName() {
-        return "BlockLSTM";
-    }
-
-}
