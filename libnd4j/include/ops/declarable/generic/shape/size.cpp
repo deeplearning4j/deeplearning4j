226f0672bc0e5d0fa7d0397483d91b623b224ccf
raver119 <raver119@gmail.com>
2020-03-23 17:02:03 +0300

    size op fixed
    Signed-off-by: raver119 <raver119@gmail.com>

--- libnd4j/include/ops/declarable/generic/shape/size.cpp
+++ libnd4j/include/ops/declarable/generic/shape/size.cpp
@@ -32,6 +32,7 @@ namespace sd {
             REQUIRE_TRUE(output->isScalar(), 0, "Size output should be scalar");
 
             output->p(0, input->lengthOf());
+            output->syncToDevice();
 
             return Status::OK();
         }
