1b4f04a937c943787d7e7da3f1b81c7346e896e4
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2020-03-25 20:46:39 +0200

    NDRNN generated by codegen
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/factory/ops/NDRNN.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/factory/ops/NDRNN.java
@@ -22,17 +22,13 @@ import static org.nd4j.linalg.factory.NDValidation.isSameType;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.config.LSTMConfiguration;
+import org.nd4j.linalg.api.ops.impl.layers.recurrent.config.LSTMLayerConfig;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.GRUWeights;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.LSTMLayerWeights;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.LSTMWeights;
 import org.nd4j.linalg.api.ops.impl.layers.recurrent.weights.SRUWeights;
 import org.nd4j.linalg.factory.NDValidation;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.factory.enums.CellAct;
-import org.nd4j.linalg.factory.enums.GateAct;
-import org.nd4j.linalg.factory.enums.LSTMDataFormat;
-import org.nd4j.linalg.factory.enums.LSTMDirectionMode;
-import org.nd4j.linalg.factory.enums.OutAct;
 
 public class NDRNN {
   public NDRNN() {
@@ -97,80 +93,17 @@ public class NDRNN {
    * @param cLast Previous/initial cell state, with shape [batchSize, numUnits] (NUMERIC type)
    * @param yLast Previous/initial cell output, with shape [batchSize, numUnits] (NUMERIC type)
    * @param maxTSLength  (NUMERIC type)
-   * @param LSTMDataFormat for unidirectional:
-   *   TNS: shape [timeLength, numExamples, inOutSize] - sometimes referred to as "time major"<br>
-   *   NST: shape [numExamples, inOutSize, timeLength]<br>
-   *   NTS: shape [numExamples, timeLength, inOutSize] - TF "time_major=false" layout<br>
-   *  for bidirectional:
-   *    T2NS: 3 = [timeLength, 2, numExamples, inOutSize] (for ONNX)
-   * @param LSTMDirectionMode direction <br>
-   *  FWD: 0 = fwd
-   *  BWD: 1 = bwd
-   *  BS: 2 = bidirectional sum
-   *  BC: 3 = bidirectional concat
-   *  BE: 4 = bidirectional extra output dim (in conjunction with format dataFormat = 3)
-   * @param gateAct Activations
-   * @param cellAct Activations
-   * @param outAct Activations
-   * @param retFullSequence indicates whether to return whole time sequence h {h_0, h_1, ... , h_sL-1}
-   * @param retLastH indicates whether to return output at last time step only,
-   *  in this case shape would be [bS, nOut] (exact shape depends on dataFormat argument)
-   * @param retLastC indicates whether to return cells state at last time step only,
-   *  in this case shape would be [bS, nOut] (exact shape depends on dataFormat argument)
-   * @param cellClip Cell clipping value, if it = 0 then do not apply clipping
    * @param LSTMLayerWeights Configuration Object
+   * @param LSTMLayerConfig Configuration Object
    * @return output The layer's outputs. (NUMERIC type)
    */
   public INDArray lstmlayer(INDArray x, INDArray cLast, INDArray yLast, INDArray maxTSLength,
-      LSTMDataFormat LSTMDataFormat, LSTMDirectionMode LSTMDirectionMode, GateAct gateAct,
-      CellAct cellAct, OutAct outAct, boolean retFullSequence, boolean retLastH, boolean retLastC,
-      double cellClip, LSTMLayerWeights LSTMLayerWeights) {
+      LSTMLayerWeights LSTMLayerWeights, LSTMLayerConfig LSTMLayerConfig) {
     NDValidation.validateNumerical("lstmlayer", "x", x);
     NDValidation.validateNumerical("lstmlayer", "cLast", cLast);
     NDValidation.validateNumerical("lstmlayer", "yLast", yLast);
     NDValidation.validateNumerical("lstmlayer", "maxTSLength", maxTSLength);
-    return Nd4j.exec(new org.nd4j.linalg.api.ops.impl.layers.recurrent.LSTMLayer(x, cLast, yLast, maxTSLength, LSTMDataFormat, LSTMDirectionMode, gateAct, cellAct, outAct, retFullSequence, retLastH, retLastC, cellClip, LSTMLayerWeights))[0];
-  }
-
-  /**
-   * The LSTM layer<br>
-   *
-   * @param x  Input, with shape dependent on the data format (in config). (NUMERIC type)
-   * @param cLast Previous/initial cell state, with shape [batchSize, numUnits] (NUMERIC type)
-   * @param yLast Previous/initial cell output, with shape [batchSize, numUnits] (NUMERIC type)
-   * @param maxTSLength  (NUMERIC type)
-   * @param LSTMDataFormat for unidirectional:
-   *   TNS: shape [timeLength, numExamples, inOutSize] - sometimes referred to as "time major"<br>
-   *   NST: shape [numExamples, inOutSize, timeLength]<br>
-   *   NTS: shape [numExamples, timeLength, inOutSize] - TF "time_major=false" layout<br>
-   *  for bidirectional:
-   *    T2NS: 3 = [timeLength, 2, numExamples, inOutSize] (for ONNX)
-   * @param LSTMDirectionMode direction <br>
-   *  FWD: 0 = fwd
-   *  BWD: 1 = bwd
-   *  BS: 2 = bidirectional sum
-   *  BC: 3 = bidirectional concat
-   *  BE: 4 = bidirectional extra output dim (in conjunction with format dataFormat = 3)
-   * @param gateAct Activations
-   * @param cellAct Activations
-   * @param outAct Activations
-   * @param retLastH indicates whether to return output at last time step only,
-   *  in this case shape would be [bS, nOut] (exact shape depends on dataFormat argument)
-   * @param retLastC indicates whether to return cells state at last time step only,
-   *  in this case shape would be [bS, nOut] (exact shape depends on dataFormat argument)
-   * @param cellClip Cell clipping value, if it = 0 then do not apply clipping
-   * @param LSTMLayerWeights Configuration Object
-   * @return output The layer's outputs. (NUMERIC type)
-   */
-  public INDArray lstmlayer(INDArray x, INDArray cLast, INDArray yLast, INDArray maxTSLength,
-      LSTMDataFormat LSTMDataFormat, LSTMDirectionMode LSTMDirectionMode, GateAct gateAct,
-      CellAct cellAct, OutAct outAct, boolean retLastH, boolean retLastC, double cellClip,
-      LSTMLayerWeights LSTMLayerWeights) {
-    NDValidation.validateNumerical("lstmlayer", "x", x);
-    NDValidation.validateNumerical("lstmlayer", "cLast", cLast);
-    NDValidation.validateNumerical("lstmlayer", "yLast", yLast);
-    NDValidation.validateNumerical("lstmlayer", "maxTSLength", maxTSLength);
-    return Nd4j.exec(new org.nd4j.linalg.api.ops.impl.layers.recurrent.LSTMLayer(x, cLast, yLast, maxTSLength, LSTMDataFormat, LSTMDirectionMode, gateAct, cellAct, outAct, true, retLastH, retLastC, cellClip, LSTMLayerWeights))[0];
+    return Nd4j.exec(new org.nd4j.linalg.api.ops.impl.layers.recurrent.LSTMLayer(x, cLast, yLast, maxTSLength, LSTMLayerWeights, LSTMLayerConfig))[0];
   }
 
   /**
