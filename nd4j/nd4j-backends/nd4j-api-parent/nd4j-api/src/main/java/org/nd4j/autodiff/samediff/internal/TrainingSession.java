f5f77df8467c65da3985c1be079a2c5bd16ec2c7
Alex Black <blacka101@gmail.com>
2020-04-23 15:38:42 +1000

    Dependency version upgrades + small SameDiff fix (#405)
    * #8861 Training evaluation on variables not required for loss fix
    
    Signed-off-by: Alex Black <blacka101@gmail.com>
    
    * Dependency version updates flagged by dependabot
    
    Signed-off-by: Alex Black <blacka101@gmail.com>

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/autodiff/samediff/internal/TrainingSession.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/autodiff/samediff/internal/TrainingSession.java
@@ -75,7 +75,7 @@ public class TrainingSession extends InferenceSession {
             this.listeners = filtered.isEmpty() ? null : filtered;
         }
 
-        List<String> requiredActivations = new ArrayList<>();
+        Set<String> requiredActivations = new HashSet<>();
         gradVarToVarMap = new HashMap<>();       //Key: gradient variable. Value: variable that the key is gradient for
         for (String s : paramsToTrain) {
             Preconditions.checkState(sameDiff.hasVariable(s), "SameDiff instance does not have a variable with name \"%s\"", s);
@@ -93,6 +93,12 @@ public class TrainingSession extends InferenceSession {
             requiredActivations.add(grad.name());
 
             gradVarToVarMap.put(grad.name(), s);
+        }
+
+        //Also add evaluations - in case we want to evaluate something that isn't required to determine loss
+        // (hence wouldn't normally be calculated)
+        if(config.getTrainEvaluations() != null){
+            requiredActivations.addAll(config.getTrainEvaluations().keySet());
         }
 
         //Set up losses
