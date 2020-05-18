6e9c849e4a4e6ae87c8876b6b3929d6581c20f39
Paul Dubs <paul.dubs@gmail.com>
2020-05-18 07:46:46 +0200

    Fix typo (#469)

--- deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/KerasModel.java
+++ deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/KerasModel.java
@@ -171,7 +171,7 @@ public class KerasModel {
                 importTrainingConfiguration(trainingJson);
             else log.warn("If enforceTrainingConfig is true, a training " +
                     "configuration object has to be provided. Usually the only practical way to do this is to store" +
-                    " your keras model with `model.save('model_path.h5'. If you store model config and weights" +
+                    " your keras model with `model.save('model_path.h5')`. If you store model config and weights" +
                     " separately no training configuration is attached.");
         }
 
