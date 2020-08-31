8ce440659888dfef0f7c8a2a9fa1306aceb10b64
Dariusz Zbyrad <dariusz.zbyrad@gmail.com>
2020-08-31 21:08:56 +0200

    Removed manuall array copy
    Signed-off-by: Dariusz Zbyrad <dariusz.zbyrad@gmail.com>

--- deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/preprocessors/ReshapePreprocessor.java
+++ deeplearning4j/deeplearning4j-modelimport/src/main/java/org/deeplearning4j/nn/modelimport/keras/preprocessors/ReshapePreprocessor.java
@@ -96,9 +96,7 @@ public class ReshapePreprocessor extends BaseInputPreProcessor {
         int shapeLength = shape.length;
         val miniBatchShape = new long[shapeLength + 1];
         miniBatchShape[0] = miniBatchSize;
-        for (int i = 1; i < miniBatchShape.length; i++) {
-            miniBatchShape[i] = shape[i - 1];
-        }
+        System.arraycopy(shape, 0, miniBatchShape, 1, miniBatchShape.length - 1);
         return miniBatchShape;
     }
 
