6080844e7d0e8da9f093e105713617b5d3c065fb
agibsonccc <adam@konduit.ai>
2021-02-20 09:07:22 +0900

    Update

--- .github/actions/actions-docker/build.sh
+++ .github/actions/actions-docker/build.sh
@@ -17,27 +17,27 @@ for os in OS_TYPES do
         
         docker build -f "${docker_file_path}" -t "${docker_tag}" .
 
-docker build -f manifests/jenkins-agent/centos/6/amd64/base/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/base/Dockerfile -t konduit/jenkins-agent:amd64-centos6 .
 
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.0/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6 .
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.1/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6 .
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.2/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6-cuda9.2-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.0/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6-cuda10.0-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.1/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6-cuda10.1-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.2/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos6-cuda10.2-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.0/Dockerfile -t konduit/jenkins-agent:amd64-centos6 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.1/Dockerfile -t konduit/jenkins-agent:amd64-centos6 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/9.2/Dockerfile -t konduit/jenkins-agent:amd64-centos6-cuda9.2-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.0/Dockerfile -t konduit/jenkins-agent:amd64-centos6-cuda10.0-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.1/Dockerfile -t konduit/jenkins-agent:amd64-centos6-cuda10.1-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/6/amd64/cuda/10.2/Dockerfile -t konduit/jenkins-agent:amd64-centos6-cuda10.2-cudnn7 .
 
-docker build -f manifests/jenkins-agent/centos/7/amd64/base/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos7 .
+docker build -f manifests/jenkins-agent/centos/7/amd64/base/Dockerfile -t konduit/jenkins-agent:amd64-centos7 .
 
-docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/9.2/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos7-cuda9.2-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.0/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos7-cuda10.0-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.1/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos7-cuda10.1-cudnn7 .
-docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.2/Dockerfile.centos -t konduit/jenkins-agent:amd64-centos7-cuda10.2-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/9.2/Dockerfile -t konduit/jenkins-agent:amd64-centos7-cuda9.2-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.0/Dockerfile -t konduit/jenkins-agent:amd64-centos7-cuda10.0-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.1/Dockerfile -t konduit/jenkins-agent:amd64-centos7-cuda10.1-cudnn7 .
+docker build -f manifests/jenkins-agent/centos/7/amd64/cuda/10.2/Dockerfile -t konduit/jenkins-agent:amd64-centos7-cuda10.2-cudnn7 .
 
-docker build -f manifests/jenkins-agent/android/arm/Dockerfile.centos -t konduit/jenkins-agent:arm-android .
-docker build -f manifests/jenkins-agent/centos/7/armhf/Dockerfile.centos -t konduit/jenkins-agent:armhf-centos7 .
+docker build -f manifests/jenkins-agent/android/arm/Dockerfile -t konduit/jenkins-agent:arm-android .
+docker build -f manifests/jenkins-agent/centos/7/armhf/Dockerfile -t konduit/jenkins-agent:armhf-centos7 .
 
-docker build -f manifests/jenkins-agent/ubuntu/16.04/ppc64le/base/Dockerfile.centos -t konduit/jenkins-agent:ppc64le-ubuntu16.04 .
-docker build -f manifests/jenkins-agent/ubuntu/16.04/ppc64le/cuda/9.2/Dockerfile.centos -t konduit/jenkins-agent:ppc64le-ubuntu16.04-cuda9.2-cudnn7 .
+docker build -f manifests/jenkins-agent/ubuntu/16.04/ppc64le/base/Dockerfile -t konduit/jenkins-agent:ppc64le-ubuntu16.04 .
+docker build -f manifests/jenkins-agent/ubuntu/16.04/ppc64le/cuda/9.2/Dockerfile -t konduit/jenkins-agent:ppc64le-ubuntu16.04-cuda9.2-cudnn7 .
 
 docker push konduit/jenkins-agent:amd64-centos6
 docker push konduit/jenkins-agent:amd64-centos6-cuda9.2-cudnn7
