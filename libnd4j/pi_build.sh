cf546e3e17e7be51dffccf4d833231523ae0cff4
agibsonccc <adam@konduit.ai>
2021-04-05 07:55:01 +0900

    Update pi_build.sh

--- libnd4j/pi_build.sh
+++ libnd4j/pi_build.sh
@@ -336,24 +336,24 @@ export ARMCOMPUTE_ROOT="${ARMCOMPUTE_DIR}"
 fi #if armcompute
 
 
-if [ "${TARGET_OS}" = "android" ];then
-	#export ANDROID_NDK=${CROSS_COMPILER_DIR}
-	#XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -pl \":libnd4j,:nd4j-native\" "
-else
-	 if [ "${CURRENT_TARGET}" == "jetson_arm64" ];then
-	 	message  "jetson cuda build "
-		cuda_cross_setup ${CUDA_VER}
-		XTRA_ARGS="${XTRA_ARGS} -c cuda  -h cudnn  "
-		XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -Djavacpp.version=1.5.3 -Dcuda.version=${CUDA_VER} -Dlibnd4j.cuda=${CUDA_VER} -Dlibnd4j.chip=cuda -Dlibnd4j.compute=5.3 "
-		XTRA_MVN_ARGS="${XTRA_MVN_ARGS}  -Dlibnd4j.helper=cudnn  "
-		export SYSROOT=${CROSS_COMPILER_DIR}/${PREFIX}/libc
-	else
-		XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -pl \":libnd4j,:nd4j-native\" "
-	fi
-	export RPI_BIN=${CROSS_COMPILER_DIR}/bin/${PREFIX}
-	export JAVA_LIBRARY_PATH=${CROSS_COMPILER_DIR}/${PREFIX}/lib
-	fix_pi_linker ${BINUTILS_BIN}
-fi
+#if [ "${TARGET_OS}" = "android" ];then
+#	#export ANDROID_NDK=${CROSS_COMPILER_DIR}
+#	#XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -pl \":libnd4j,:nd4j-native\" "
+#else
+#	 if [ "${CURRENT_TARGET}" == "jetson_arm64" ];then
+#	 	message  "jetson cuda build "
+#		cuda_cross_setup ${CUDA_VER}
+#		XTRA_ARGS="${XTRA_ARGS} -c cuda  -h cudnn  "
+#		XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -Djavacpp.version=1.5.3 -Dcuda.version=${CUDA_VER} -Dlibnd4j.cuda=${CUDA_VER} -Dlibnd4j.chip=cuda -Dlibnd4j.compute=5.3 "
+#		XTRA_MVN_ARGS="${XTRA_MVN_ARGS}  -Dlibnd4j.helper=cudnn  "
+#		export SYSROOT=${CROSS_COMPILER_DIR}/${PREFIX}/libc
+#	else
+#		XTRA_MVN_ARGS="${XTRA_MVN_ARGS} -pl \":libnd4j,:nd4j-native\" "
+#	fi
+#	export RPI_BIN=${CROSS_COMPILER_DIR}/bin/${PREFIX}
+#	export JAVA_LIBRARY_PATH=${CROSS_COMPILER_DIR}/${PREFIX}/lib
+#	fix_pi_linker ${BINUTILS_BIN}
+#fi
 
 
 #because of the toolchain passive detection we have to delete build folder manually
