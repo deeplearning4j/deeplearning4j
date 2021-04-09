afca30b45159f34674cb89079255e6f641f6906d
Adam Gibson <adam@konduit.ai>
2021-04-09 15:44:33 +0900

    Preven tfailure because of javadoc build

--- release-specified-component.sh
+++ release-specified-component.sh
@@ -50,7 +50,8 @@ bash ./update-versions.sh "$SNAPSHOT_VERSION" "$RELEASE_VERSION"
 
 #    mvn clean deploy -Dgpg.executable=gpg2 -Prelease -Dlocal.software.repository=$RELEASE_PROFILE -Dmaven.test.skip -Dlibnd4j.cuda=8.0 -Denforcer.skip -DstagingRepositoryId=$STAGING_REPOSITORY
 # add flags specific to release, assumes an mvn deploy command is given
-DEPLOY_COMMAND="${DEPLOY_COMMAND}  -Prelease  -Denforcer.skip -DstagingRepositoryId=$STAGING_REPOSITORY"
+# Note: we add javadoc fail on error here to avoid javadoc errors blocking a release
+DEPLOY_COMMAND="${DEPLOY_COMMAND}  -Prelease  -Denforcer.skip -DstagingRepositoryId=$STAGING_REPOSITORY  -Dmaven.javadoc.failOnError=false"
 eval "$DEPLOY_COMMAND"
 
 if [[ -z ${STAGING_REPOSITORY:-} ]]; then
