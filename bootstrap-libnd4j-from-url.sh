0fc899219858f51e294612333c4946d3f5cbb41f
Adam Gibson <adam@konduit.ai>
2021-04-22 18:33:52 +0900

    Update bootstrap-libnd4j-from-url.sh

--- bootstrap-libnd4j-from-url.sh
+++ bootstrap-libnd4j-from-url.sh
@@ -4,7 +4,7 @@ if ! [[ -z "$LIBND4J_URL" ]]; then
               cd libnd4j_home
               # Set a suffix for the downloaded libnd4j directory
               if [ -z "${LIBND4J_HOME_SUFFIX}" ]; then export LIBND4J_HOME_SUFFIX="cpu"; fi
-              wget "${LIBND4J_URL}" -o libnd4j.zip
+              wget "${LIBND4J_URL}" -O libnd4j.zip
               unzip libnd4j.zip
               # Note: for builds, the whole source directory is uploaded, but a valid libnd4j home is actually only the compiled output
               export LIBND4J_HOME="${GITHUB_WORKSPACE}/libnd4j_home/libnd4j/blasbuild/${LIBND4J_HOME_SUFFIX}"
