e9da9015d2f8862531ba25b811f2eca60b46a8f9
Adam Gibson <adam@konduit.ai>
2021-04-22 14:54:17 +0900

    Try alternative for setting libnd4j home

--- bootstrap-libnd4j-from-url.sh
+++ bootstrap-libnd4j-from-url.sh
@@ -7,6 +7,7 @@ if ! [[ -z "$LIBND4J_URL" ]]; then
               wget "${LIBND4J_URL}" -o libnd4j.zip
               unzip libnd4j.zip
               # Note: for builds, the whole source directory is uploaded, but a valid libnd4j home is actually only the compiled output
-              export LIBND4J_HOME=${GITHUB_WORKSPACE}/libnd4j_home/libnd4j/blasbuild/"${LIBND4J_HOME_SUFFIX}"
+              export LIBND4J_HOME="${GITHUB_WORKSPACE}/libnd4j_home/libnd4j/blasbuild/${LIBND4J_HOME_SUFFIX}"
+              echo "Set libnd4j home to ${LIBND4J_HOME}"
               cd ..
 fi
