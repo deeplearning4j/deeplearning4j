375a42e963272ee2e4c7e334317096ed65dce8e0
Adam Gibson <adam@konduit.ai>
2021-06-16 14:31:04 +0900

    Update for testing

--- .github/actions/build-centos/build.sh
+++ .github/actions/build-centos/build.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
-SCL_ENABLE="devtoolset-7"
-yum -y update && yum -y install centos-release-scl-rh epel-release
-yum -y install $SCL_ENABLE rh-java-common-ant boost-devel ccache clang gcc-c++ gcc-gfortran java-1.8.0-openjdk-devel ant python python36-devel python36-pip swig file which wget unzip tar bzip2 gzip xz patch autoconf-archive automake make libtool bison flex perl nasm alsa-lib-devel freeglut-devel gtk2-devel libusb-devel libusb1-devel curl-devel expat-devel gettext-devel openssl-devel bzip2-devel zlib-devel SDL-devel libva-devel libxkbcommon-devel libxkbcommon-x11-devel xcb-util* fontconfig-devel libffi-devel ragel ocl-icd-devel GeoIP-devel pcre-devel ssdeep-devel yajl-devel
-sed -i 's/_mm512_abs_pd (__m512 __A)/_mm512_abs_pd (__m512d __A)/g' /opt/rh/devtoolset-7/root/usr/lib/gcc/x86_64-redhat-linux/7/include/avx512fintrin.h
-source scl_source enable $SCL_ENABLE || true
-curl -LO https://github.com/Kitware/CMake/releases/download/v3.16.6/cmake-3.16.6-Linux-x86_64.tar.gz
-curl -LO https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-curl -LO https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.18.3.tar.gz
-tar -xzf cmake-3.16.6-Linux-x86_64.tar.gz -C /opt/
-mv /opt/cmake-3.16.6-Linux-x86_64 /opt/cmake
-tar -xzf apache-maven-3.6.3-bin.tar.gz -C /opt/
-tar -xzf git-2.18.3.tar.gz
-pushd git-2.18.3; make -j2 prefix=/usr/local/; make -j2 prefix=/usr/local/ install; popd
-ln -sf /usr/bin/python3.6 /usr/bin/python3
-ln -sf /opt/cmake-3.16.6-Linux-x86_64/bin/* /usr/bin/
-ln -sf /opt/apache-maven-3.6.3/bin/mvn /usr/bin/mvn
-curl -fsSL https://github.com/google/protobuf/releases/download/v3.8.0/protobuf-cpp-3.8.0.tar.gz \
-                       | tar xz && \
-                       cd protobuf-3.8.0 && \
-                       ./configure --prefix=/opt/protobuf && \
-                       make -j2 && \
-                       make install && \
-                       cd .. && \
-                       rm -rf protobuf-3.8.0
-echo "/opt/protobuf/bin" >> $GITHUB_PATH
-
-export PATH=/opt/protobuf/bin:/opt/cmake/bin:$PATH
-mvn --version
-cmake --version
-protoc --version
-export PATH=/opt/protobuf/bin:/opt/cmake/bin:$PATH
-# download libnd4j from a url and set it up if LIBND4J_FILE_NAME is defined
+env
 pwd
-cd "/github/workspace/deeplearning4j"
-bash ./bootstrap-libnd4j-from-url.sh
-
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$OPENBLAS_PATH"
-if [ "$PERFORM_RELEASE" == 1 ]; then
-          bash /github/workspace/deeplearning4j/release-specified-component.sh "${RELEASE_VERSION}" "${SNAPSHOT_VERSION}" "${RELEASE_REPO_ID}" "${command}"
-         else
-             echo "Running build and deploying to snapshots"
-             eval "${COMMAND}"
-fi
+ls -R /
+#SCL_ENABLE="devtoolset-7"
+#yum -y update && yum -y install centos-release-scl-rh epel-release
+#yum -y install $SCL_ENABLE rh-java-common-ant boost-devel ccache clang gcc-c++ gcc-gfortran java-1.8.0-openjdk-devel ant python python36-devel python36-pip swig file which wget unzip tar bzip2 gzip xz patch autoconf-archive automake make libtool bison flex perl nasm alsa-lib-devel freeglut-devel gtk2-devel libusb-devel libusb1-devel curl-devel expat-devel gettext-devel openssl-devel bzip2-devel zlib-devel SDL-devel libva-devel libxkbcommon-devel libxkbcommon-x11-devel xcb-util* fontconfig-devel libffi-devel ragel ocl-icd-devel GeoIP-devel pcre-devel ssdeep-devel yajl-devel
+#sed -i 's/_mm512_abs_pd (__m512 __A)/_mm512_abs_pd (__m512d __A)/g' /opt/rh/devtoolset-7/root/usr/lib/gcc/x86_64-redhat-linux/7/include/avx512fintrin.h
+#source scl_source enable $SCL_ENABLE || true
+#curl -LO https://github.com/Kitware/CMake/releases/download/v3.16.6/cmake-3.16.6-Linux-x86_64.tar.gz
+#curl -LO https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+#curl -LO https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.18.3.tar.gz
+#tar -xzf cmake-3.16.6-Linux-x86_64.tar.gz -C /opt/
+#mv /opt/cmake-3.16.6-Linux-x86_64 /opt/cmake
+#tar -xzf apache-maven-3.6.3-bin.tar.gz -C /opt/
+#tar -xzf git-2.18.3.tar.gz
+#pushd git-2.18.3; make -j2 prefix=/usr/local/; make -j2 prefix=/usr/local/ install; popd
+#ln -sf /usr/bin/python3.6 /usr/bin/python3
+#ln -sf /opt/cmake-3.16.6-Linux-x86_64/bin/* /usr/bin/
+#ln -sf /opt/apache-maven-3.6.3/bin/mvn /usr/bin/mvn
+#curl -fsSL https://github.com/google/protobuf/releases/download/v3.8.0/protobuf-cpp-3.8.0.tar.gz \
+#                       | tar xz && \
+#                       cd protobuf-3.8.0 && \
+#                       ./configure --prefix=/opt/protobuf && \
+#                       make -j2 && \
+#                       make install && \
+#                       cd .. && \
+#                       rm -rf protobuf-3.8.0
+#echo "/opt/protobuf/bin" >> $GITHUB_PATH
+#
+#export PATH=/opt/protobuf/bin:/opt/cmake/bin:$PATH
+#mvn --version
+#cmake --version
+#protoc --version
+#export PATH=/opt/protobuf/bin:/opt/cmake/bin:$PATH
+## download libnd4j from a url and set it up if LIBND4J_FILE_NAME is defined
+#pwd
+#cd "/github/workspace/deeplearning4j"
+#bash ./bootstrap-libnd4j-from-url.sh
+#
+#export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$OPENBLAS_PATH"
+#if [ "$PERFORM_RELEASE" == 1 ]; then
+#          bash /github/workspace/deeplearning4j/release-specified-component.sh "${RELEASE_VERSION}" "${SNAPSHOT_VERSION}" "${RELEASE_REPO_ID}" "${command}"
+#         else
+#             echo "Running build and deploying to snapshots"
+#             eval "${COMMAND}"
+#fi
