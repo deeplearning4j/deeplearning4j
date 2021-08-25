67368a075c302ee8e227a1050c891216fd9979a9
Adam Gibson <adam@konduit.ai>
2021-08-25 10:41:58 +0900

    Add missing optimization framework tests

---
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/optimization/TestOptimization.java
@@ -0,0 +1,148 @@
+/*
+ *  ******************************************************************************
+ *  *
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  *  See the NOTICE file distributed with this work for additional
+ *  *  information regarding copyright ownership.
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+package org.nd4j.autodiff.optimization;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.nd4j.autodiff.optimization.util.OptTestConfig;
+import org.nd4j.autodiff.optimization.util.OptimizationTestUtil;
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.autodiff.samediff.VariableType;
+import org.nd4j.autodiff.samediff.optimize.GraphOptimizer;
+import org.nd4j.autodiff.samediff.optimize.optimizations.ConstantFunctionOptimizations;
+import org.nd4j.autodiff.samediff.optimize.optimizations.IdentityFunctionOptimizations;
+import org.nd4j.linalg.BaseNd4jTestWithBackends;
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class TestOptimization extends BaseNd4jTestWithBackends {
+
+    @Rule
+    public TemporaryFolder testDir = new TemporaryFolder();
+
+
+    @Override
+    public char ordering() {
+        return 'c';
+    }
+
+    @Override
+    public long getTimeoutMilliseconds() {
+        return 1_000_000_000L;
+    }
+
+    @Test
+    public void testConstantOpFolding() {
+        //We expect 2 things in this test:
+        //(a) the output of  add(constant, constant) is pre-calculated and itself becomes a constant
+        //(b) the
+
+
+        SameDiff sd = SameDiff.create();
+        SDVariable c = sd.constant("c", Nd4j.scalar(1.0));
+        SDVariable c2 = c.add("add", 1);
+        SDVariable v = sd.var("variable", Nd4j.scalar(1.0));
+        SDVariable out = v.sub("out", c2);
+
+        SameDiff copy = sd.dup();
+
+        SameDiff optimized = GraphOptimizer.optimize(sd, "out");
+        assertEquals(3, optimized.getVariables().size());       //"add", "variable", "out" -> "c" should be removed
+        assertEquals(VariableType.CONSTANT, optimized.getVariable("add").getVariableType());
+        assertEquals(1, optimized.getOps().size());
+        assertEquals("subtract", optimized.getOps().values().iterator().next().getName());
+
+        assertFalse(optimized.hasVariable("c"));
+
+        assertEquals(sd.outputSingle(Collections.emptyMap(), "out"), optimized.outputSingle(Collections.emptyMap(), "out"));
+
+        //Check the
+
+        //Check that the original can be saved and loaded, and still gives the same results
+
+    }
+
+    @Test
+    public void testConstantOpFolding2(){
+        //We expect 2 things in this test:
+        //(a) the output of  add(constant, constant) is pre-calculated and itself becomes a constant
+        //(b) the
+
+
+        SameDiff sd = SameDiff.create();
+        SDVariable c = sd.constant("c", Nd4j.scalar(1.0));
+        SDVariable c2 = c.add("add", 1);
+        SDVariable v = sd.var("variable", Nd4j.scalar(1.0));
+        SDVariable out = v.sub("out", c2);
+
+        OptTestConfig conf = OptTestConfig.builder()
+                .original(sd)
+                .outputs(Collections.singletonList("out"))
+                .mustApply(sd.getVariables().get("add").getOutputOfOp(), ConstantFunctionOptimizations.FoldConstantFunctions.class)
+                .build();
+
+        SameDiff optimized = OptimizationTestUtil.testOptimization(conf);
+        assertEquals(3, optimized.getVariables().size());       //"add", "variable", "out" -> "c" should be removed
+        assertEquals(VariableType.CONSTANT, optimized.getVariable("add").getVariableType());
+        assertEquals(1, optimized.getOps().size());
+        assertEquals("subtract", optimized.getOps().values().iterator().next().getName());
+
+        assertFalse(optimized.hasVariable("c"));
+
+        assertEquals(sd.outputSingle(Collections.emptyMap(), "out"), optimized.outputSingle(Collections.emptyMap(), "out"));
+
+    }
+
+    @Test
+    public void testIdentityRemoval(){
+
+        //Ensure that optimizer is actually used when calling output methods:
+        SameDiff sd = SameDiff.create();
+        SDVariable in = sd.placeHolder("in", DataType.FLOAT, -1, 4);
+        SDVariable w = sd.var("w", Nd4j.rand(DataType.FLOAT, 4, 3));
+        SDVariable b = sd.var("b", Nd4j.rand(DataType.FLOAT, 3));
+        SDVariable i1 = sd.identity(in);
+        SDVariable i2 = sd.identity(w);
+        SDVariable i3 = sd.identity(b);
+        SDVariable out = sd.nn.softmax("out", sd.identity(i1.mmul(i2).add(i3)));
+
+        OptTestConfig conf = OptTestConfig.builder()
+                .original(sd)
+                .outputs(Collections.singletonList("out"))
+                .placeholder("in", Nd4j.rand(DataType.FLOAT, 5, 4))
+                .mustApply(sd.getVariables().get(i1.name()).getOutputOfOp(), IdentityFunctionOptimizations.RemoveIdentityOps.class)
+                .mustApply(sd.getVariables().get(i2.name()).getOutputOfOp(), IdentityFunctionOptimizations.RemoveIdentityOps.class)
+                .mustApply(sd.getVariables().get(i3.name()).getOutputOfOp(), IdentityFunctionOptimizations.RemoveIdentityOps.class)
+                .build();
+
+        SameDiff optimized = OptimizationTestUtil.testOptimization(conf);
+        assertEquals(3, optimized.getOps().size());
+        assertFalse(optimized.hasVariable(i1.name()));
+        assertFalse(optimized.hasVariable(i2.name()));
+        assertFalse(optimized.hasVariable(i3.name()));
+        assertTrue(optimized.hasVariable("out"));
+    }
+}
---
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/optimization/TestSeamlessOptimization.java
@@ -0,0 +1,156 @@
+/*
+ *  ******************************************************************************
+ *  *
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  *  See the NOTICE file distributed with this work for additional
+ *  *  information regarding copyright ownership.
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+package org.nd4j.autodiff.optimization;
+
+import lombok.Data;
+import org.junit.Test;
+import org.nd4j.autodiff.functions.DifferentialFunction;
+import org.nd4j.autodiff.listeners.At;
+import org.nd4j.autodiff.listeners.BaseListener;
+import org.nd4j.autodiff.listeners.Operation;
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.autodiff.samediff.internal.SameDiffOp;
+
+import org.nd4j.common.base.Preconditions;
+import org.nd4j.linalg.BaseNd4jTestWithBackends;
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.OpContext;
+import org.nd4j.linalg.api.ops.impl.reduce.Mmul;
+import org.nd4j.linalg.api.ops.impl.transforms.custom.SoftMax;
+import org.nd4j.linalg.api.ops.impl.transforms.pairwise.arithmetic.AddOp;
+import org.nd4j.linalg.api.ops.impl.transforms.same.Identity;
+import org.nd4j.linalg.dataset.api.MultiDataSet;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TestSeamlessOptimization extends BaseNd4jTestWithBackends {
+
+
+
+
+    @Test
+    public void testOutput() {
+
+        //Ensure that optimizer is actually used when calling output methods:
+        SameDiff sd = SameDiff.create();
+        SDVariable in = sd.placeHolder("in", DataType.FLOAT, -1, 4);
+        SDVariable w = sd.var("w", Nd4j.rand(DataType.FLOAT, 4, 3));
+        SDVariable b = sd.var("b", Nd4j.rand(DataType.FLOAT, 3));
+
+        SDVariable i1 = sd.identity(in);
+        SDVariable i2 = sd.identity(w);
+        SDVariable i3 = sd.identity(b);
+
+        SDVariable out = sd.nn.softmax("out", sd.identity(i1.mmul(i2).add(i3)));
+
+        RecordOpsListener l = new RecordOpsListener();
+        sd.setListeners(new AssertNoOpsOfTypeListener(Identity.class), l);
+
+        Map<String, INDArray> ph = Collections.singletonMap("in", Nd4j.rand(DataType.FLOAT, 10, 4));
+
+        for( int i=0; i<3; i++ ) {
+            l.ops.clear();
+
+            switch (i){
+                case 0:
+                    sd.outputSingle(ph, "out");
+                    break;
+                case 1:
+                    sd.output(ph, "out");
+                    break;
+                case 2:
+                    sd.batchOutput().output("out")
+                            .input("in", ph.get("in"))
+                            .outputSingle();
+                    break;
+            }
+
+
+            List<Class<?>> expClasses = Arrays.asList(Mmul.class, AddOp.class, SoftMax.class);
+            assertEquals(3, l.ops.size());
+            for (int j = 0; j < 3; j++) {
+                assertEquals(expClasses.get(j), l.ops.get(j).getOp().getClass());
+            }
+
+        }
+    }
+
+    @Test
+    public void testDifferentOutputs(){
+        //Test when the user requests different outputs instead
+    }
+
+    @Test
+    public void testGraphModification(){
+        //User modifies the graph -> should reoptimize?
+
+        fail("Not yet implemented");
+    }
+
+    public static class AssertNoOpsOfTypeListener extends BaseListener {
+        private List<Class<?>> list;
+
+        public AssertNoOpsOfTypeListener(Class<? extends DifferentialFunction>... c) {
+            Preconditions.checkState(c != null && c.length > 0, "No classes provided");
+            this.list = Arrays.asList(c);
+        }
+
+        @Override
+        public boolean isActive(Operation operation) {
+            return true;
+        }
+
+        @Override
+        public void opExecution(SameDiff sd, At at, MultiDataSet batch, SameDiffOp op, OpContext opContext, INDArray[] outputs) {
+            if(list.contains(op.getOp().getClass())){
+                throw new IllegalStateException("Encountered unexpected class: " + op.getOp().getClass().getName());
+            }
+        }
+    }
+
+    @Data
+    public static class RecordOpsListener extends BaseListener {
+
+        private List<SameDiffOp> ops = new ArrayList<>();
+
+        @Override
+        public boolean isActive(Operation operation) {
+            return true;
+        }
+
+        @Override
+        public void opExecution(SameDiff sd, At at, MultiDataSet batch, SameDiffOp op, OpContext opContext, INDArray[] outputs) {
+            ops.add(op);
+
+        }
+    }
+
+
+    @Override
+    public char ordering() {
+        return 'c';
+    }
+}
