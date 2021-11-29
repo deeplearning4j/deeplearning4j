2dadd5452f8dd4c16345b21159dd10dee9b2af5d
Adam Gibson <adam@konduit.ai>
2021-11-29 12:34:06 +0900

    Update AuroraPresets.java

--- nd4j/nd4j-backends/nd4j-backend-impls/nd4j-aurora-presets/src/main/java/org/nd4j/aurora/AuroraPresets.java
+++ nd4j/nd4j-backends/nd4j-backend-impls/nd4j-aurora-presets/src/main/java/org/nd4j/aurora/AuroraPresets.java
@@ -38,7 +38,7 @@ import org.bytedeco.javacpp.tools.*;
                         includepath = "/opt/nec/ve/veos/include/",
                         linkpath = "/opt/nec/ve/veos/lib64/",
                         library = "aurora",
-                        resource = {"aurora", "libaurora.so"}
+                        resource = {"aurora", "libaurora.so","nd4jaurora"}
                 )
         },
         target = "org.nd4j.aurora.Aurora"
