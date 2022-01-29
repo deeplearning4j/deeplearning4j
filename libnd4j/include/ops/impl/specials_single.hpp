fb1c82a3b4bbd598902f424bc4179f8bc61c55a7
Adam Gibson <adam@konduit.ai>
2022-01-29 16:18:22 +0900

    Fix android compilation error

--- libnd4j/include/ops/impl/specials_single.hpp
+++ libnd4j/include/ops/impl/specials_single.hpp
@@ -240,7 +240,7 @@ void SpecialMethods<T>::concatCpuGeneric(const std::vector<const NDArray *> &inA
     int totalCopySize = output.lengthOf() / times;
 
     std::vector<InputArgsCase2<T>> inputArgs;
-    for (int i = 0; i < numOfInArrs; i++) {
+    for (sd::LongType i = 0; i < numOfInArrs; i++) {
       InputArgsCase2<T> input = {inArrs[i]->bufferAsT<T>(), inArrs[i]->lengthOf() / times};
       inputArgs.push_back(input);
     }
