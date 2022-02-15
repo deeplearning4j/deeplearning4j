bf6fd6ab7cc288717205f24076ad2b8a9b41a4fe
Adam Gibson <adam@konduit.ai>
2022-02-15 22:44:25 +0900

    Update part 2

--- contrib/version-updater/src/main/java/org/nd4j/fileupdater/impl/CudaFileUpdater.java
+++ contrib/version-updater/src/main/java/org/nd4j/fileupdater/impl/CudaFileUpdater.java
@@ -23,7 +23,6 @@ public class CudaFileUpdater implements FileUpdater {
         ret.put( "\\<artifactId\\>nd4j-cuda-[0-9\\.]+\\<\\/artifactId\\>",String.format("<artifactId>nd4j-cuda-%s</artifactId>",cudaVersion));
         ret.put( "\\<artifactId\\>nd4j-cuda-[0-9\\.]*-preset<\\/artifactId\\>",String.format("<artifactId>nd4j-cuda-%s-preset</artifactId>",cudaVersion));
         ret.put( "\\<artifactId\\>nd4j-cuda-[0-9\\.]*-platform\\<\\/artifactId\\>",String.format("<artifactId>nd4j-cuda-%s-platform</artifactId>",cudaVersion));
-        ret.put( "\\<artifactId\\>deeplearning4j-cuda-[0-9\\.]*\\<\\/artifactId\\>",String.format("<artifactId>deeplearning4j-cuda-%s</artifactId>",cudaVersion));
         ret.put( "\\<cuda.version\\>[0-9\\.]*<\\/cuda.version\\>",String.format("<cuda.version>%s</cuda.version>",cudaVersion));
         ret.put( "\\<cudnn.version\\>[0-9\\.]*\\<\\/cudnn.version\\>",String.format("<cudnn.version>%s</cudnn.version>",cudnnVersion));
         ret.put( "\\<javacpp-presets.cuda.version\\>[0-9\\.]*<\\/javacpp-presets.cuda.version\\>",String.format("<javacpp-presets.cuda.version>%s</javacpp-presets.cuda.version>",javacppVersion));
