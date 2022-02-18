5da130bdc345233b7af8e241842517620b7b3f71
Adam Gibson <adam@konduit.ai>
2022-02-18 19:48:12 +0900

    Copy artifacts to known location instead of trying to set variables

--- bootstrap-libnd4j-from-url.sh
+++ bootstrap-libnd4j-from-url.sh
@@ -14,8 +14,6 @@ if ! [[ -z "$LIBND4J_URL" ]]; then
               wget "${LIBND4J_URL}" -O libnd4j.zip
               unzip libnd4j.zip
               # Note: for builds, the whole source directory is uploaded, but a valid libnd4j home is actually only the compiled output
-              export LIBND4J_HOME="${GITHUB_WORKSPACE}/libnd4j_home/libnd4j/blasbuild/${LIBND4J_HOME_SUFFIX}"
-              echo "LIBND4J_HOME=$LIBND4J_HOME" >> "$GITHUB_ENV"
-              echo "Set libnd4j home to ${LIBND4J_HOME}"
+              cp -rf ${GITHUB_WORKSPACE}/libnd4j_home/libnd4j/blasbuild/ "${GITHUB_WORKSPACE}/libnd4j"
               cd ..
 fi
