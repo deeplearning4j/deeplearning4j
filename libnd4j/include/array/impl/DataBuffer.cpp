f87a5be2b5e8a2f50a0ad08120c651cc6495b3b1
Adam Gibson <adam@konduit.ai>
2022-04-30 17:04:41 +0900

    fix data buffer formatting

--- libnd4j/include/array/impl/DataBuffer.cpp
+++ libnd4j/include/array/impl/DataBuffer.cpp
@@ -32,276 +32,276 @@ namespace sd {
 
 ////////////////////////////////////////////////////////////////////////
 // default constructor
-    DataBuffer::DataBuffer() {
-        _primaryBuffer = nullptr;
-        _specialBuffer = nullptr;
-        _lenInBytes = 0;
-        _dataType = INT8;
-        _workspace = nullptr;
-        _isOwnerPrimary = false;
-        _isOwnerSpecial = false;
-        _deviceId = sd::AffinityManager::currentDeviceId();
+DataBuffer::DataBuffer() {
+  _primaryBuffer = nullptr;
+  _specialBuffer = nullptr;
+  _lenInBytes = 0;
+  _dataType = INT8;
+  _workspace = nullptr;
+  _isOwnerPrimary = false;
+  _isOwnerSpecial = false;
+  _deviceId = sd::AffinityManager::currentDeviceId();
 
-        setCountersToZero();
-    }
+  setCountersToZero();
+}
 
 ////////////////////////////////////////////////////////////////////////
 // copy constructor
-    DataBuffer::DataBuffer(const DataBuffer& other) {
-        throw std::runtime_error("DataBuffer copy constructor: we don't expect using of this constructor!");
+DataBuffer::DataBuffer(const DataBuffer& other) {
+  throw std::runtime_error("DataBuffer copy constructor: we don't expect using of this constructor!");
 
-        _lenInBytes = other._lenInBytes;
-        _dataType = other._dataType;
-        _workspace = other._workspace;
+  _lenInBytes = other._lenInBytes;
+  _dataType = other._dataType;
+  _workspace = other._workspace;
 
-        _primaryBuffer = nullptr;
-        _specialBuffer = nullptr;
+  _primaryBuffer = nullptr;
+  _specialBuffer = nullptr;
 
-        _deviceId.store(other._deviceId.load());
+  _deviceId.store(other._deviceId.load());
 
-        setCountersToZero();
+  setCountersToZero();
 
-        allocateBuffers();
-        copyBufferFrom(other);
-    }
-
-////////////////////////////////////////////////////////////////////////
-    DataBuffer::DataBuffer(void* primary, void* special, const size_t lenInBytes, const DataType dataType,
-                           const bool isOwnerPrimary, const bool isOwnerSpecial, memory::Workspace* workspace) {
-        if (primary == nullptr && special == nullptr)
-            throw std::runtime_error("DataBuffer constructor: can't be initialized with both nullptr buffers !");
-
-        _primaryBuffer = primary;
-        _specialBuffer = special;
-        _lenInBytes = lenInBytes;
-        _dataType = dataType;
-        _workspace = workspace;
-        _isOwnerPrimary = isOwnerPrimary;
-        _isOwnerSpecial = isOwnerSpecial;
-        _deviceId = sd::AffinityManager::currentDeviceId();
-
-        setCountersToZero();
-
-        if (primary != nullptr) readPrimary();
-        if (special != nullptr) readSpecial();
-    }
+  allocateBuffers();
+  copyBufferFrom(other);
+}
 
 ////////////////////////////////////////////////////////////////////////
-    DataBuffer::DataBuffer(void* primary, const size_t lenInBytes, const DataType dataType, const bool isOwnerPrimary,
-                           memory::Workspace* workspace)
-            : DataBuffer(primary, nullptr, lenInBytes, dataType, isOwnerPrimary, false, workspace) {
-        syncToSpecial(true);
-    }
+DataBuffer::DataBuffer(void* primary, void* special, const size_t lenInBytes, const DataType dataType,
+                       const bool isOwnerPrimary, const bool isOwnerSpecial, memory::Workspace* workspace) {
+  if (primary == nullptr && special == nullptr)
+    throw std::runtime_error("DataBuffer constructor: can't be initialized with both nullptr buffers !");
+
+  _primaryBuffer = primary;
+  _specialBuffer = special;
+  _lenInBytes = lenInBytes;
+  _dataType = dataType;
+  _workspace = workspace;
+  _isOwnerPrimary = isOwnerPrimary;
+  _isOwnerSpecial = isOwnerSpecial;
+  _deviceId = sd::AffinityManager::currentDeviceId();
+
+  setCountersToZero();
+
+  if (primary != nullptr) readPrimary();
+  if (special != nullptr) readSpecial();
+}
+
+////////////////////////////////////////////////////////////////////////
+DataBuffer::DataBuffer(void* primary, const size_t lenInBytes, const DataType dataType, const bool isOwnerPrimary,
+                       memory::Workspace* workspace)
+    : DataBuffer(primary, nullptr, lenInBytes, dataType, isOwnerPrimary, false, workspace) {
+  syncToSpecial(true);
+}
 
 ////////////////////////////////////////////////////////////////////////
 // copies data from hostBuffer to own memory buffer
-    DataBuffer::DataBuffer(const void* hostBuffer, const DataType dataType, const size_t lenInBytes,
-                           memory::Workspace* workspace) {
-        if (hostBuffer == nullptr)
-            throw std::runtime_error("DataBuffer constructor: can't be initialized with nullptr host buffer !");
-        if (lenInBytes == 0) throw std::runtime_error("DataBuffer constructor: can't be initialized with zero length !");
+DataBuffer::DataBuffer(const void* hostBuffer, const DataType dataType, const size_t lenInBytes,
+                       memory::Workspace* workspace) {
+  if (hostBuffer == nullptr)
+    throw std::runtime_error("DataBuffer constructor: can't be initialized with nullptr host buffer !");
+  if (lenInBytes == 0) throw std::runtime_error("DataBuffer constructor: can't be initialized with zero length !");
 
-        _primaryBuffer = nullptr;
-        _specialBuffer = nullptr;
-        _lenInBytes = lenInBytes;
-        _dataType = dataType;
-        _workspace = workspace;
+  _primaryBuffer = nullptr;
+  _specialBuffer = nullptr;
+  _lenInBytes = lenInBytes;
+  _dataType = dataType;
+  _workspace = workspace;
 
-        _deviceId = sd::AffinityManager::currentDeviceId();
+  _deviceId = sd::AffinityManager::currentDeviceId();
 
-        setCountersToZero();
+  setCountersToZero();
 
-        allocateBuffers();
+  allocateBuffers();
 
-        copyBufferFromHost(hostBuffer, lenInBytes);
-    }
+  copyBufferFromHost(hostBuffer, lenInBytes);
+}
 
 ////////////////////////////////////////////////////////////////////////
-    DataBuffer::DataBuffer(const size_t lenInBytes, const DataType dataType, memory::Workspace* workspace,
-                           const bool allocBoth) {
-        _dataType = dataType;
-        _workspace = workspace;
-        _lenInBytes = lenInBytes;
+DataBuffer::DataBuffer(const size_t lenInBytes, const DataType dataType, memory::Workspace* workspace,
+                       const bool allocBoth) {
+  _dataType = dataType;
+  _workspace = workspace;
+  _lenInBytes = lenInBytes;
 
-        _primaryBuffer = nullptr;
-        _specialBuffer = nullptr;
+  _primaryBuffer = nullptr;
+  _specialBuffer = nullptr;
 
-        _deviceId = sd::AffinityManager::currentDeviceId();
+  _deviceId = sd::AffinityManager::currentDeviceId();
 
-        setCountersToZero();
+  setCountersToZero();
 
-        if (lenInBytes != 0) {
-            allocateBuffers(allocBoth);
-            writeSpecial();
-        }
-    }
+  if (lenInBytes != 0) {
+    allocateBuffers(allocBoth);
+    writeSpecial();
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////
 // move constructor
-    DataBuffer::DataBuffer(DataBuffer&& other) {
-        _primaryBuffer = other._primaryBuffer;
-        _specialBuffer = other._specialBuffer;
-        _lenInBytes = other._lenInBytes;
-        _dataType = other._dataType;
-        _workspace = other._workspace;
-        _isOwnerPrimary = other._isOwnerPrimary;
-        _isOwnerSpecial = other._isOwnerSpecial;
-        _deviceId.store(other._deviceId);
+DataBuffer::DataBuffer(DataBuffer&& other) {
+  _primaryBuffer = other._primaryBuffer;
+  _specialBuffer = other._specialBuffer;
+  _lenInBytes = other._lenInBytes;
+  _dataType = other._dataType;
+  _workspace = other._workspace;
+  _isOwnerPrimary = other._isOwnerPrimary;
+  _isOwnerSpecial = other._isOwnerSpecial;
+  _deviceId.store(other._deviceId);
 
-        copyCounters(other);
+  copyCounters(other);
 
-        other._primaryBuffer = other._specialBuffer = nullptr;
-        other.setAllocFlags(false, false);
-        other._lenInBytes = 0;
-    }
+  other._primaryBuffer = other._specialBuffer = nullptr;
+  other.setAllocFlags(false, false);
+  other._lenInBytes = 0;
+}
 
 ////////////////////////////////////////////////////////////////////////
 // assignment operator
-    DataBuffer& DataBuffer::operator=(const DataBuffer& other) {
-        if (this == &other) return *this;
+DataBuffer& DataBuffer::operator=(const DataBuffer& other) {
+  if (this == &other) return *this;
 
-        deleteBuffers();
+  deleteBuffers();
 
-        _lenInBytes = other._lenInBytes;
-        _dataType = other._dataType;
-        _workspace = other._workspace;
+  _lenInBytes = other._lenInBytes;
+  _dataType = other._dataType;
+  _workspace = other._workspace;
 
-        allocateBuffers();
-        copyBufferFrom(other);
+  allocateBuffers();
+  copyBufferFrom(other);
 
-        return *this;
-    }
+  return *this;
+}
 
 ////////////////////////////////////////////////////////////////////////
 // move assignment operator
-    DataBuffer& DataBuffer::operator=(DataBuffer&& other) noexcept {
-        if (this == &other) return *this;
+DataBuffer& DataBuffer::operator=(DataBuffer&& other) noexcept {
+  if (this == &other) return *this;
 
-        deleteBuffers();
+  deleteBuffers();
 
-        _primaryBuffer = other._primaryBuffer;
-        _specialBuffer = other._specialBuffer;
-        _lenInBytes = other._lenInBytes;
-        _dataType = other._dataType;
-        _workspace = other._workspace;
-        _isOwnerPrimary = other._isOwnerPrimary;
-        _isOwnerSpecial = other._isOwnerSpecial;
+  _primaryBuffer = other._primaryBuffer;
+  _specialBuffer = other._specialBuffer;
+  _lenInBytes = other._lenInBytes;
+  _dataType = other._dataType;
+  _workspace = other._workspace;
+  _isOwnerPrimary = other._isOwnerPrimary;
+  _isOwnerSpecial = other._isOwnerSpecial;
 
-        copyCounters(other);
+  copyCounters(other);
 
-        other._primaryBuffer = other._specialBuffer = nullptr;
-        other.setAllocFlags(false, false);
-        other._lenInBytes = 0;
+  other._primaryBuffer = other._specialBuffer = nullptr;
+  other.setAllocFlags(false, false);
+  other._lenInBytes = 0;
 
-        return *this;
+  return *this;
+}
+
+////////////////////////////////////////////////////////////////////////
+void* DataBuffer::primary() { return _primaryBuffer; }
+
+////////////////////////////////////////////////////////////////////////
+void* DataBuffer::special() { return _specialBuffer; }
+
+////////////////////////////////////////////////////////////////////////
+DataType DataBuffer::getDataType() { return _dataType; }
+
+////////////////////////////////////////////////////////////////////////
+size_t DataBuffer::getLenInBytes() const { return _lenInBytes; }
+
+////////////////////////////////////////////////////////////////////////
+void DataBuffer::allocatePrimary() {
+  if (_primaryBuffer == nullptr && getLenInBytes() > 0) {
+    auto deviceId = sd::AffinityManager::currentDeviceId();
+    // check if this allocation won't bring us above limit
+    if (_workspace == nullptr) {
+      if (Environment::getInstance().isCPU()) {
+        // on cpu backend we validate against device 0 for now
+        if (!sd::memory::MemoryCounter::getInstance().validate(getLenInBytes()))
+          throw sd::allocation_exception::build("Requested amount exceeds HOST device limits",
+                                                sd::memory::MemoryCounter::getInstance().deviceLimit(deviceId),
+                                                getLenInBytes());
+      } else {
+        // in heterogenous mode we valdate against device group
+        if (!sd::memory::MemoryCounter::getInstance().validateGroup(sd::memory::MemoryType::HOST, getLenInBytes()))
+          throw sd::allocation_exception::build(
+              "Requested amount exceeds HOST group limits",
+              sd::memory::MemoryCounter::getInstance().groupLimit(sd::memory::MemoryType::HOST), getLenInBytes());
+      }
     }
 
-////////////////////////////////////////////////////////////////////////
-    void* DataBuffer::primary() { return _primaryBuffer; }
+    ALLOCATE(_primaryBuffer, _workspace, getLenInBytes(), int8_t);
+    _isOwnerPrimary = true;
 
-////////////////////////////////////////////////////////////////////////
-    void* DataBuffer::special() { return _specialBuffer; }
+    // count in towards current deviceId if we're not in workspace mode
+    if (_workspace == nullptr) {
+      if (Environment::getInstance().isCPU())  // we don't want this counter to be added to CUDA device
+        sd::memory::MemoryCounter::getInstance().countIn(deviceId, getLenInBytes());
 
-////////////////////////////////////////////////////////////////////////
-    DataType DataBuffer::getDataType() { return _dataType; }
-
-////////////////////////////////////////////////////////////////////////
-    size_t DataBuffer::getLenInBytes() const { return _lenInBytes; }
-
-////////////////////////////////////////////////////////////////////////
-    void DataBuffer::allocatePrimary() {
-        if (_primaryBuffer == nullptr && getLenInBytes() > 0) {
-            auto deviceId = sd::AffinityManager::currentDeviceId();
-            // check if this allocation won't bring us above limit
-            if (_workspace == nullptr) {
-                if (Environment::getInstance().isCPU()) {
-                    // on cpu backend we validate against device 0 for now
-                    if (!sd::memory::MemoryCounter::getInstance().validate(getLenInBytes()))
-                        throw sd::allocation_exception::build("Requested amount exceeds HOST device limits",
-                                                              sd::memory::MemoryCounter::getInstance().deviceLimit(deviceId),
-                                                              getLenInBytes());
-                } else {
-                    // in heterogenous mode we valdate against device group
-                    if (!sd::memory::MemoryCounter::getInstance().validateGroup(sd::memory::MemoryType::HOST, getLenInBytes()))
-                        throw sd::allocation_exception::build(
-                                "Requested amount exceeds HOST group limits",
-                                sd::memory::MemoryCounter::getInstance().groupLimit(sd::memory::MemoryType::HOST), getLenInBytes());
-                }
-            }
-
-            ALLOCATE(_primaryBuffer, _workspace, getLenInBytes(), int8_t);
-            _isOwnerPrimary = true;
-
-            // count in towards current deviceId if we're not in workspace mode
-            if (_workspace == nullptr) {
-                if (Environment::getInstance().isCPU())  // we don't want this counter to be added to CUDA device
-                    sd::memory::MemoryCounter::getInstance().countIn(deviceId, getLenInBytes());
-
-                sd::memory::MemoryCounter::getInstance().countIn(sd::memory::MemoryType::HOST, getLenInBytes());
-            }
-        }
+      sd::memory::MemoryCounter::getInstance().countIn(sd::memory::MemoryType::HOST, getLenInBytes());
     }
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////
-    void DataBuffer::setAllocFlags(const bool isOwnerPrimary, const bool isOwnerSpecial) {
-        _isOwnerPrimary = isOwnerPrimary;
-        _isOwnerSpecial = isOwnerSpecial;
-    }
+void DataBuffer::setAllocFlags(const bool isOwnerPrimary, const bool isOwnerSpecial) {
+  _isOwnerPrimary = isOwnerPrimary;
+  _isOwnerSpecial = isOwnerSpecial;
+}
 
 ////////////////////////////////////////////////////////////////////////
-    void DataBuffer::deletePrimary() {
-        if (_isOwnerPrimary && _primaryBuffer != nullptr && getLenInBytes() != 0) {
-            auto p = reinterpret_cast<int8_t*>(_primaryBuffer);
-            RELEASE(p, _workspace);
-            _primaryBuffer = nullptr;
-            _isOwnerPrimary = false;
+void DataBuffer::deletePrimary() {
+  if (_isOwnerPrimary && _primaryBuffer != nullptr && getLenInBytes() != 0) {
+    auto p = reinterpret_cast<int8_t*>(_primaryBuffer);
+    RELEASE(p, _workspace);
+    _primaryBuffer = nullptr;
+    _isOwnerPrimary = false;
 
-            // count out towards DataBuffer device, only if we're not in workspace
-            if (_workspace == nullptr) {
-                if (Environment::getInstance().isCPU())
-                    sd::memory::MemoryCounter::getInstance().countOut(_deviceId, getLenInBytes());
+    // count out towards DataBuffer device, only if we're not in workspace
+    if (_workspace == nullptr) {
+      if (Environment::getInstance().isCPU())
+        sd::memory::MemoryCounter::getInstance().countOut(_deviceId, getLenInBytes());
 
-                sd::memory::MemoryCounter::getInstance().countOut(sd::memory::MemoryType::HOST, getLenInBytes());
-            }
-        }
+      sd::memory::MemoryCounter::getInstance().countOut(sd::memory::MemoryType::HOST, getLenInBytes());
     }
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////
-    void DataBuffer::deleteBuffers() {
-        if(_primaryBuffer != nullptr)
-            deletePrimary();
-        if(_specialBuffer != nullptr)
-            deleteSpecial();
-        _lenInBytes = 0;
-    }
+void DataBuffer::deleteBuffers() {
+  if(_primaryBuffer != nullptr)
+    deletePrimary();
+  if(_specialBuffer != nullptr)
+    deleteSpecial();
+  _lenInBytes = 0;
+}
 
 ////////////////////////////////////////////////////////////////////////
-    DataBuffer::~DataBuffer() { deleteBuffers(); }
+DataBuffer::~DataBuffer() { deleteBuffers(); }
 
-    void DataBuffer::setPrimaryBuffer(void* buffer, size_t length) {
-        if (_primaryBuffer != nullptr && _isOwnerPrimary) {
-            deletePrimary();
-        }
+void DataBuffer::setPrimaryBuffer(void* buffer, size_t length) {
+  if (_primaryBuffer != nullptr && _isOwnerPrimary) {
+    deletePrimary();
+  }
 
-        _primaryBuffer = buffer;
-        _isOwnerPrimary = false;
-        _lenInBytes = length * DataTypeUtils::sizeOf(_dataType);
-    }
+  _primaryBuffer = buffer;
+  _isOwnerPrimary = false;
+  _lenInBytes = length * DataTypeUtils::sizeOf(_dataType);
+}
 
-    void DataBuffer::setSpecialBuffer(void* buffer, size_t length) {
-        if (_specialBuffer != nullptr && _isOwnerSpecial) {
-            deleteSpecial();
-        }
+void DataBuffer::setSpecialBuffer(void* buffer, size_t length) {
+  if (_specialBuffer != nullptr && _isOwnerSpecial) {
+    deleteSpecial();
+  }
 
-        this->setSpecial(buffer, false);
-        _lenInBytes = length * DataTypeUtils::sizeOf(_dataType);
-    }
+  this->setSpecial(buffer, false);
+  _lenInBytes = length * DataTypeUtils::sizeOf(_dataType);
+}
 
-    void DataBuffer::setDataType(DataType dataType) { _dataType = dataType; }
+void DataBuffer::setDataType(DataType dataType) { _dataType = dataType; }
 
-    int DataBuffer::deviceId() const { return _deviceId.load(); }
+int DataBuffer::deviceId() const { return _deviceId.load(); }
 
-    void DataBuffer::close() { this->deleteBuffers(); }
+void DataBuffer::close() { this->deleteBuffers(); }
 
-    void DataBuffer::setDeviceId(int deviceId) { _deviceId = deviceId; }
+void DataBuffer::setDeviceId(int deviceId) { _deviceId = deviceId; }
 }  // namespace sd
