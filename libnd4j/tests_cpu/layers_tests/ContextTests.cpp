73a4e80e8835e293499b65a786965528d3813c28
Abdelrauf <rauf@konduit.ai>
2022-06-12 10:17:52 +0900

    Veda can be only set using InteropDataBuffer. Therefore we changed tests for it

--- libnd4j/tests_cpu/layers_tests/ContextTests.cpp
+++ libnd4j/tests_cpu/layers_tests/ContextTests.cpp
@@ -312,11 +312,20 @@ TEST_F(ContextTests, test_short_context_2) {
 
   auto exp = NDArrayFactory::create<float>('c', {3, 2}, {2.f, 4.f, 6.f, 8.f, 10.f, 12.f});
   Context ctx(1);
+#if defined(HAVE_VEDA)
+  // veda should be set using InteropDataBuffer
+  InteropDataBuffer i0(array0.dataBuffer());
+  InteropDataBuffer i1(array1.dataBuffer());
+  InteropDataBuffer o0(z.dataBuffer());
+  ctx.setInputArray(0, &i0, array0.shapeInfo(), array0.specialShapeInfo());
+  ctx.setInputArray(1, &i1, array1.shapeInfo(), array1.specialShapeInfo());
+  ctx.setOutputArray(0, &o0, z.shapeInfo(), z.specialShapeInfo());
 
+#else
   ctx.setInputArray(0, array0.buffer(), array0.shapeInfo(), array0.specialBuffer(), array0.specialShapeInfo());
   ctx.setInputArray(1, array1.buffer(), array1.shapeInfo(), array1.specialBuffer(), array1.specialShapeInfo());
   ctx.setOutputArray(0, z.buffer(), z.shapeInfo(), z.specialBuffer(), z.specialShapeInfo());
-
+#endif
   ASSERT_EQ(2, ctx.width());
 
   sd::ops::add op;
