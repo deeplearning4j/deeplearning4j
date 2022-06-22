ba65da5e63411637ee2e763702732ce525357ac1
Adam Gibson <adam@konduit.ai>
2022-06-22 22:28:32 +0900

    Update ImportReflectionCache.kt

--- nd4j/samediff-import/samediff-import-api/src/main/kotlin/org/nd4j/samediff/frameworkimport/reflect/ImportReflectionCache.kt
+++ nd4j/samediff-import/samediff-import-api/src/main/kotlin/org/nd4j/samediff/frameworkimport/reflect/ImportReflectionCache.kt
@@ -33,14 +33,16 @@ import org.nd4j.shade.protobuf.ProtocolMessageEnum
 
 object ImportReflectionCache {
 
-    val scannedClasses =   ClassGraph().enableAllInfo().
-    disableModuleScanning() // added for GraalVM
-     .disableDirScanning() // added for GraalVM
-     .disableNestedJarScanning() // added for GraalVM
-     .disableRuntimeInvisibleAnnotations() // added for GraalVM
+    val scannedClasses =   ClassGraph()
+        .enableAnnotationInfo()
+        .enableClassInfo()
+        .disableModuleScanning() // added for GraalVM
+    .disableDirScanning() // added for GraalVM
+    .disableNestedJarScanning() // added for GraalVM
+    .disableRuntimeInvisibleAnnotations() // added for GraalVM
     .addClassLoader(ClassLoader.getSystemClassLoader()) // see
     // https://github.com/oracle/graal/issues/470#issuecomment-401022008
-        .scan()
+    .scan()
 
     //all relevant node names relevant for
     val preProcessRuleImplementationsByNode: Table<String,String,MutableList<PreImportHook>> = TreeBasedTable.create()
