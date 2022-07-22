bf6abca8da12ab71226b553a7ad2ca0be133ae3f
Adam Gibson <adam@konduit.ai>
2022-07-22 10:21:36 +0900

    Remove hard coded test, add deepwalk import

--- platform-tests/src/test/java/org/deeplearning4j/graph/models/deepwalk/TestDeepWalk.java
+++ platform-tests/src/test/java/org/deeplearning4j/graph/models/deepwalk/TestDeepWalk.java
@@ -48,8 +48,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Disabled("Permissions issues on CI")
 @NativeTag
