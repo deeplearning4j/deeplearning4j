9f727c97d9321d4cd31ca95b4e2d020ecd3bf772
Adam Gibson <adam@konduit.ai>
2022-08-03 06:57:56 +0900

    Update build.sh

--- .github/actions/build-centos/build.sh
+++ .github/actions/build-centos/build.sh
@@ -2,9 +2,10 @@
 env
 ## Based on the javacpp presets github actions centos 7 build at https://github.com/bytedeco/javacpp-presets/
 SCL_ENABLE="devtoolset-7"
-yum -y update && yum -y install wget centos-release-scl-rh epel-release
-wget -o /opt/zulu11.58.15-ca-jdk11.0.16-linux_x64.tar.gz https://cdn.azul.com/zulu/bin/zulu11.58.15-ca-jdk11.0.16-linux_x64.tar.gz
-cd /opt && tar xvf zulu11.58.15-ca-jdk11.0.16-linux_x64.tar.gz
+yum -y update && yum -y install wget unzip centos-release-scl-rh epel-release
+echo "Downloading java from azul"
+wget -o /opt/zulu11.58.15-ca-jdk11.0.16-linux_x64.zip https://cdn.azul.com/zulu/bin/zulu11.58.15-ca-jdk11.0.16-linux_x64.zip
+cd /opt && unzip zulu11.58.15-ca-jdk11.0.16-linux_x64.zip
 #zulu11.58.15-ca-jdk11.0.16-linux_x64
 yum -y install $SCL_ENABLE rh-java-common-ant boost-devel ccache clang gcc-c++ gcc-gfortran  ant python python36-devel python36-pip swig file which wget unzip tar bzip2 gzip xz patch autoconf-archive automake make libtool bison flex perl nasm alsa-lib-devel freeglut-devel gtk2-devel libusb-devel libusb1-devel curl-devel expat-devel gettext-devel openssl-devel bzip2-devel zlib-devel SDL-devel libva-devel libxkbcommon-devel libxkbcommon-x11-devel xcb-util* fontconfig-devel libffi-devel ragel ocl-icd-devel GeoIP-devel pcre-devel ssdeep-devel yajl-devel
 sed -i 's/_mm512_abs_pd (__m512 __A)/_mm512_abs_pd (__m512d __A)/g' /opt/rh/devtoolset-7/root/usr/lib/gcc/x86_64-redhat-linux/7/include/avx512fintrin.h
