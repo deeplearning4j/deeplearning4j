cf71e1266815d00b1af2206ef2a00c06153cd6a1
Adam Gibson <adam@konduit.ai>
2022-09-28 14:50:26 +0900

    Update veda_helper.h

--- libnd4j/include/ops/declarable/platform/vednn/veda_helper.h
+++ libnd4j/include/ops/declarable/platform/vednn/veda_helper.h
@@ -234,7 +234,7 @@ inline VEDAresult vedaArgsSetLocal(VEDAargs args, const int idx, const VEDAstack
 
 inline VEDAresult __vedaLaunchKernelLocal(VEDAfunction func, VEDAstream stream, uint64_t* result, VEDAargs args,
                                           const int idx) {
-  return vedaLaunchKernelEx(func, stream, result,args);
+  return vedaLaunchKernelEx(func, stream, result);
 }
 
 template <typename T, typename... Args>
