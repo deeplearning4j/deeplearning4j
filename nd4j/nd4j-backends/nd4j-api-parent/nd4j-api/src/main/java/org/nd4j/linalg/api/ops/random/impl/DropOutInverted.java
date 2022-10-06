8ff8092198bb207dc8018a32eee61c91b5e4380e
Adam Gibson <adam@konduit.ai>
2022-10-06 20:44:08 +0900

    Add back dropout inverted for legacy tests/code

---
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/ops/random/impl/DropOutInverted.java
@@ -0,0 +1,103 @@
+/*
+ *  ******************************************************************************
+ *  *
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Apache License, Version 2.0 which is available at
+ *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *
+ *  *  See the NOTICE file distributed with this work for additional
+ *  *  information regarding copyright ownership.
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  * License for the specific language governing permissions and limitations
+ *  * under the License.
+ *  *
+ *  * SPDX-License-Identifier: Apache-2.0
+ *  *****************************************************************************
+ */
+
+package org.nd4j.linalg.api.ops.random.impl;
+
+import lombok.NonNull;
+import onnx.Onnx;
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.OpContext;
+import org.nd4j.linalg.api.ops.random.BaseRandomOp;
+import org.nd4j.linalg.api.shape.LongShapeDescriptor;
+import org.tensorflow.framework.AttrValue;
+import org.tensorflow.framework.GraphDef;
+import org.tensorflow.framework.NodeDef;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class DropOutInverted extends BaseRandomOp {
+
+    private double p;
+
+    public DropOutInverted() {
+    }
+
+    public DropOutInverted(SameDiff sameDiff, SDVariable input, double p) {
+        super(sameDiff, input);
+        this.p = p;
+        this.extraArgs = new Object[]{p};
+    }
+
+    public DropOutInverted(@NonNull INDArray x, double p) {
+        this(x, x, p);
+    }
+
+    public DropOutInverted(@NonNull INDArray x, @NonNull INDArray z, double p) {
+        super(x,null,z);
+        this.p = p;
+        this.extraArgs = new Object[] {p};
+    }
+
+    @Override
+    public int opNum() {
+        return 2;
+    }
+
+    @Override
+    public String opName() {
+        return "dropout_inverted";
+    }
+
+    @Override
+    public void initFromTensorFlow(NodeDef nodeDef, SameDiff initWith, Map<String, AttrValue> attributesForNode, GraphDef graph) {
+        super.initFromTensorFlow(nodeDef, initWith, attributesForNode, graph);
+    }
+
+    @Override
+    public void initFromOnnx(Onnx.NodeProto node, SameDiff initWith, Map<String, Onnx.AttributeProto> attributesForNode, Onnx.GraphProto graph) {
+        super.initFromOnnx(node, initWith, attributesForNode, graph);
+    }
+
+    @Override
+    public String onnxName() {
+        return "Dropout";
+    }
+
+    @Override
+    public List<SDVariable> doDiff(List<SDVariable> f1) {
+        throw new UnsupportedOperationException("DropOutInverted does not have a derivative.");
+    }
+
+    @Override
+    public List<LongShapeDescriptor> calculateOutputShape(OpContext oc) {
+        return calculateOutputShape();
+    }
+
+    @Override
+    public List<LongShapeDescriptor> calculateOutputShape() {
+        LongShapeDescriptor longShapeDescriptor = LongShapeDescriptor.fromShape(shape,dataType);
+        return Arrays.asList(longShapeDescriptor);
+    }
+
+}
