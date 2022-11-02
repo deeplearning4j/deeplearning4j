4727b0403a6980945a35c026596dc7caf90bd5b5
agibsonccc <adam@konduit.ai>
2022-11-02 16:06:33 +0900

    Fixes #9835

--- platform-tests/src/test/java/org/eclipse/deeplearning4j/dl4jcore/datasets/iterator/DataSetSplitterTests.java
+++ platform-tests/src/test/java/org/eclipse/deeplearning4j/dl4jcore/datasets/iterator/DataSetSplitterTests.java
@@ -21,7 +21,11 @@
 package org.eclipse.deeplearning4j.dl4jcore.datasets.iterator;
 
 import lombok.val;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.writable.IntWritable;
 import org.deeplearning4j.BaseDL4JTest;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIteratorSplitter;
 import org.eclipse.deeplearning4j.dl4jcore.datasets.iterator.tools.DataSetGenerator;
 import org.junit.jupiter.api.Tag;
@@ -31,6 +35,7 @@ import org.nd4j.common.tests.tags.TagNames;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.exception.ND4JIllegalStateException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -401,4 +406,19 @@ public class DataSetSplitterTests extends BaseDL4JTest {
         }
         assertEquals(5, valCnt);
     }
+
+
+    @Test
+    public void testSplitter9835() {
+        RecordReader reader = new CollectionRecordReader(Collections.nCopies(4, Collections.nCopies(4, new IntWritable(1))));
+        DataSetIterator baseIterator = new RecordReaderDataSetIterator(reader, 1, 3, 3, true);
+        DataSetIteratorSplitter splitter = new DataSetIteratorSplitter(baseIterator, 4, 0.5);
+        List<DataSetIterator> iterators = splitter.getIterators(); // throws exception
+        DataSetIterator iterator0 = iterators.get(0);
+        DataSetIterator iterator1 = iterators.get(1);
+        assertNotNull(iterator0);
+        assertNotNull(iterator1);
+
+    }
+
 }
