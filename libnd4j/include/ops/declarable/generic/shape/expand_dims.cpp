217f04eb338fb4629f7cf5b949ac64e7c1c1d328
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2022-11-15 17:01:39 +0900

    Fixes https://github.com/deeplearning4j/deeplearning4j/issues/9847 (#9849)

--- libnd4j/include/ops/declarable/generic/shape/expand_dims.cpp
+++ libnd4j/include/ops/declarable/generic/shape/expand_dims.cpp
@@ -34,7 +34,7 @@ CUSTOM_OP_IMPL(expand_dims, 1, 1, false, 0, -2) {
 
   if (axis < 0) axis += input->rankOf() + 1;
 
-  REQUIRE_TRUE(axis >= 0 && axis <= input->rankOf() + 1, 0,
+  REQUIRE_TRUE(axis >= 0 && axis <= input->rankOf(), 0,
                "ExpandDims: axis should be in range of 0...%i in this case, but got %i instead", input->rankOf() + 1,
                axis);
 
@@ -63,10 +63,14 @@ DECLARE_SHAPE_FN(expand_dims) {
     return SHAPELIST(newShape);
   }
 
+  auto input = INPUT_VARIABLE(0);
   auto x_rank = shape::rank(inShape);
   char order = shape::order(inShape);
 
   sd::LongType axis = block.numI() > 0 ? INT_ARG(0) : INPUT_VARIABLE(1)->e<int>(0);
+  REQUIRE_TRUE(axis >= 0 && axis <= input->rankOf(), 0,
+               "ExpandDims: axis should be in range of 0...%i in this case, but got %i instead", input->rankOf() + 1,
+               axis);
 
   if (axis < 0) axis += x_rank + 1;
   std::vector<sd::LongType> shape;
