c120d8628215dbbd6696060994b37fc36b9e7435
Adam Gibson <adam@konduit.ai>
2023-01-22 11:29:06 +0900

    Fix save npy signatures Add data type validation for invalid data types Remove extra prints Fix formatting nits fix up merge conflict issues Fix numpy cnpy conflict Remove extra print statements

--- libnd4j/include/math/templatemath.h
+++ libnd4j/include/math/templatemath.h
@@ -592,7 +592,6 @@ namespace sd {
             }
 
             auto diff = static_cast<double>(sd::math::sd_abs<T>(d1 - d2));
-            printf("Diff was %f\n",diff);
             // works well except in the range of very large numbers
             if (diff <= eps) return true;
 
