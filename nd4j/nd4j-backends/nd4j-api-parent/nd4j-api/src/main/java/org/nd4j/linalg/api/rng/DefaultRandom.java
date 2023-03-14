2c54670f3b07f14e8e56219dbf0c4a2e8d94b774
agibsonccc <adam@konduit.ai>
2023-03-14 09:15:16 +0900

    Add minor comment

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/rng/DefaultRandom.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/api/rng/DefaultRandom.java
@@ -266,7 +266,7 @@ public class DefaultRandom implements Random, RandomGenerator {
 
     @Override
     public void close() {
-        //
+        //mainly implemented down stream
     }
 
     /**
