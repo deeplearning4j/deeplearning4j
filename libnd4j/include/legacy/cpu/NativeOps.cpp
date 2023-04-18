3e27f21c22186500cc0712cd68bd112a670f84de
agibsonccc <adam@konduit.ai>
2023-04-18 21:11:21 +0900

    Remove print statements

--- libnd4j/include/legacy/cpu/NativeOps.cpp
+++ libnd4j/include/legacy/cpu/NativeOps.cpp
@@ -185,7 +185,7 @@ void printOpTrace() {
       sd_printf("Op name: %s\n", curr->opName->c_str());
     }
     sd_printf(" Input buffers:\n",0);
-   if(curr->inputShapeBuffers == nullptr || curr->inputShapeBuffers->size() == 0) {
+    if(curr->inputShapeBuffers == nullptr || curr->inputShapeBuffers->size() == 0) {
       sd_printf("No input buffers\n",0);
       continue;
     } else {
@@ -403,14 +403,10 @@ void  setGraphContextInputBuffers(OpaqueContext* ptr, int numArrays, OpaqueDataB
 void setGraphContextOutputBuffers(OpaqueContext* ptr, int numArrays, OpaqueDataBuffer** buffer, sd::Pointer* shapeInfo,
                                   sd::Pointer * specialShapeInfo) {
   auto inputShapeBuffers = (void **) shapeInfo;
-  sd_printf("After input shape buffers resolution\n",0);
   for(int i = 0; i < numArrays; i++) {
-    sd_printf("Context: Setting output array %d\n",i);
     if(buffer != nullptr && buffer[i] != nullptr) {
-      sd_printf("Context: Before Setting output array %d\n",i);
       setGraphContextOutputBuffer(ptr, i, buffer[i], inputShapeBuffers[i],
                                   specialShapeInfo != nullptr ? specialShapeInfo[i] : nullptr);
-      sd_printf("Context: After Setting output array %d\n",i);
     } else {
       setGraphContextOutputBuffer(ptr,i, nullptr,inputShapeBuffers[i],specialShapeInfo);
     }
