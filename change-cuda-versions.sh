250444dd3dc5884f9354bc0d5404aa9ac8c766c5
agibsonccc <adam@konduit.ai>
2023-06-06 21:18:02 +0900

    update javacpp versions

--- change-cuda-versions.sh
+++ change-cuda-versions.sh
@@ -26,7 +26,7 @@
 
 set -e
 
-VALID_VERSIONS=( 9.2 10.0 10.1 10.2 11.0 11.1 11.2 11.4 11.6 11.8)
+VALID_VERSIONS=( 9.2 10.0 10.1 10.2 11.0 11.1 11.2 11.4 11.6 11.8 12.1)
 
 usage() {
   echo "Usage: $(basename $0) [-h|--help] <cuda version to be used>
@@ -53,6 +53,10 @@ check_cuda_version() {
 check_cuda_version "$VERSION"
 
 case $VERSION in
+ 12.1)
+    VERSION2="8.9"
+    VERSION3="1.5.9"
+    ;;
  11.8)
     VERSION2="8.6"
     VERSION3="1.5.8"
