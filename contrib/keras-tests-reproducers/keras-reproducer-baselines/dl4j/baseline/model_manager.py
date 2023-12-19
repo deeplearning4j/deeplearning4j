Add first bidirectional test Fix minor model manager issues with computing gradients.

contrib/keras-tests-reproducers/keras-reproducer-baselines/dl4j/baseline/model_manager.py
@@ -46,7 +46,7 @@ class ModelManager(abc.ABC):
         if not model_state:
             raise ValueError(f"Model not found for model: {model_name}")
 
-        model_state.outputs = model_state.model.predict(model_state.inputs)
+        model_state.outputs = model_state.model(model_state.inputs)
 
     def compute_gradients(self, model_name: str) -> None:
         model_state = self.models.get(model_name)
@@ -57,6 +57,8 @@ class ModelManager(abc.ABC):
                 with tf.GradientTape() as tape:
                     tape.watch(model_state.model.trainable_variables)
                     predictions = model_state.model(input_, training=True)
+                    # keras can technically accept strings but we expect an instance of a loss here
+                    assert type(model_state.model.loss) is not str
                     loss = model_state.model.loss(tf.zeros_like(predictions), predictions)
                 grads = tape.gradient(loss, model_state.model.trainable_variables)
                 model_state.gradients.append(grads)

contrib/keras-tests-reproducers/keras-reproducer-baselines/dl4j/baseline/tests/bidirectional_test.py
@@ -17,6 +17,9 @@ class BidirectionalModelManager(ModelManager):
         inshape = (3, 10, 6)
         input_array = np.linspace(0, 1, num=np.prod(inshape), dtype=np.float32)
         in_ = tf.constant(input_array.reshape(inshape))
+        self.set_inputs('model0', [in_])
+        self.set_inputs('model1', [in_])
+        self.set_inputs('model2', [in_])
 
 
     def model_builder(self) -> None:
@@ -25,17 +28,18 @@ class BidirectionalModelManager(ModelManager):
             model.add(Bidirectional(
                 SimpleRNN(10, return_sequences=True, activation='tanh', kernel_initializer=GlorotNormal(),
                           recurrent_initializer=GlorotNormal())))
-            model.compile(loss='mean_squared_error', optimizer=Adam())
+            model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=Adam())
             self.add_model('model0', model)
 
             # 'model1'
             model = Sequential()
             model.add(SimpleRNN(10, return_sequences=True, activation='tanh', kernel_initializer=GlorotNormal(),
                                 recurrent_initializer=GlorotNormal()))
-            model.compile(loss='mean_squared_error', optimizer=Adam())
+            model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=Adam())
             self.add_model('model1', model)
 
             model3 = Sequential()
             model3.add(SimpleRNN(10, return_sequences=True, activation='tanh', kernel_initializer=GlorotNormal(),
                                  recurrent_initializer=GlorotNormal()))
-            model3.compile(loss='mean_squared_error', optimizer=Adam())
+            model3.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=Adam())
+            self.add_model('model2', model)