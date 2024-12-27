888f7a74d1f75eb2a0ce1cb5a09e91d5ee1d7d68
agibsonccc <adam@konduit.ai>
2024-12-27 17:14:58 +0900

    more reversions

--- contrib/benchmarking_nd4j/run-benchmark.sh
+++ contrib/benchmarking_nd4j/run-benchmark.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-#mvn -Djavacpp.platform=linux-x86_64 clean package
+#mvn clean package
  java \
-  -jar target/benchmarks.jar \
-    -jvm ../../platform-tests/bin/java \
-  org.nd4j.Large_NDArray
+  -jar /home/agibsonccc/Documents/GitHub/deeplearning4j/contrib/benchmarking_nd4j/target/benchmarks.jar \
+  -jvmArgs   "-agentpath:/home/agibsonccc/YourKit-JavaProfiler-2022.9-b167/YourKit-JavaProfiler-2022.9/bin/linux-x86-64/libyjpagent.so=disablestacktelemetry,exceptions=disable,delay=30000" \
+  -jvm /home/agibsonccc/Documents/GitHub/deeplearning4j/platform-tests/bin/java \
+  org.nd4j.SmallNDArrays
 
