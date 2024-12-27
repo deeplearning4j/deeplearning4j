1bae3fd17c24db86531abbff34aa91a55bc7145e
agibsonccc <adam@konduit.ai>
2024-12-27 17:20:15 +0900

    fix cuda chunking issues

--- libnd4j/include/helpers/cuda/ConstantShapeHelper.cu
+++ libnd4j/include/helpers/cuda/ConstantShapeHelper.cu
@@ -15,63 +15,6 @@
 * SPDX-License-Identifier: Apache-2.0
 ******************************************************************************/
 
-//
-//  @author raver119@gmail.com
-//
-
-#ifdef __CUDABLAS__
-#include <array/PrimaryPointerDeallocator.h>
-#include <helpers/ConstantShapeHelper.h>
-#include <helpers/ShapeBuilders.h>
-#include <helpers/ShapeUtils.h>
-#include <helpers/logger.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-namespace sd {
-
-// Part 1: Constructor, Destructor, and Basic Instance Methods
-
-ConstantShapeHelper::~ConstantShapeHelper() {
-}
-
-ConstantShapeHelper::ConstantShapeHelper() {
-}
-
-ConstantShapeHelper& ConstantShapeHelper::getInstance() {
- static ConstantShapeHelper instance;
- return instance;
-}
-
-const sd::LongType * ConstantShapeHelper::emptyShapeInfoWithShape(const sd::DataType dataType, std::vector<sd::LongType> &shape) {
- auto descriptor = ShapeBuilders::createShapeInfo(dataType,'c', shape, nullptr);
- ArrayOptions::setPropertyBit(descriptor, ARRAY_EMPTY);
- auto existing = createFromExisting(descriptor);
- return existing;
-}
-
-} // namespace sd
-
-#endif/* ******************************************************************************
-*
-* This program and the accompanying materials are made available under the
-* terms of the Apache License, Version 2.0 which is available at
-* https://www.apache.org/licenses/LICENSE-2.0.
-*
-*  See the NOTICE file distributed with this work for additional
-*  information regarding copyright ownership.
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*
-* SPDX-License-Identifier: Apache-2.0
-******************************************************************************/
-
-//
-//  @author raver119@gmail.com
-//
 
 #ifdef __CUDABLAS__
 #include <array/PrimaryPointerDeallocator.h>
