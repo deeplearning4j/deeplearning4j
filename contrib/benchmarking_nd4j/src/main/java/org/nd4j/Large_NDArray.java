54fb9ee229797e4e193afc8183654a76d91bbc67
agibsonccc <adam@konduit.ai>
2024-12-27 16:52:15 +0900

    restore benchmarks

--- contrib/benchmarking_nd4j/src/main/java/org/nd4j/Large_NDArray.java
+++ contrib/benchmarking_nd4j/src/main/java/org/nd4j/Large_NDArray.java
@@ -1,23 +1,3 @@
-
-/*
- *  ******************************************************************************
- *  *
- *  *
- *  * This program and the accompanying materials are made available under the
- *  * terms of the Apache License, Version 2.0 which is available at
- *  * https://www.apache.org/licenses/LICENSE-2.0.
- *  *
- *  *  See the NOTICE file distributed with this work for additional
- *  *  information regarding copyright ownership.
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  * License for the specific language governing permissions and limitations
- *  * under the License.
- *  *
- *  * SPDX-License-Identifier: Apache-2.0
- *  *****************************************************************************
- */
 package org.nd4j;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -30,18 +10,67 @@ public class Large_NDArray {
 
     @State(Scope.Thread)
     public static class SetupState {
-        public INDArray array1 = Nd4j.ones(2000000).reshape(1000000, 2);
-        public INDArray row = Nd4j.ones(2);
+        public INDArray array1 = Nd4j.ones(1<<28);
+        public INDArray array2 = Nd4j.ones(1<<28);
 
-
+        static {
+            // Only needed for mkl on RC3.8
+            //System.loadLibrary("mkl_rt");
+        }
     }
-
 
 
     @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public void addiRowVector(SetupState state) {
-        state.array1.addiRowVector(state.row);
+    public void sumNumber(SetupState state) {
+        state.array1.sumNumber().doubleValue();
     }
 
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void add(SetupState state) {
+        state.array1.add(state.array2);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void addi(SetupState state) {
+        state.array1.addi(state.array2);
+    }
+
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void sub(SetupState state) {
+        state.array1.sub(state.array2);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void subi(SetupState state) {
+        state.array1.subi(state.array2);
+    }
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void mul(SetupState state) {
+        state.array1.mul(state.array2);
+    }
+
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void muli(SetupState state) {
+        state.array1.muli(state.array2);
+    }
+
+    /*@Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void cumsum(SetupState state) {
+        state.array1.cumsum(0);
+    }
+
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void cumsumi(SetupState state) {
+        state.array1.cumsumi(0);
+    }*/
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void assign(SetupState state) {
+        state.array1.assign(state.array2);
+    }
 
 }
