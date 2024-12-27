b06943ded37608aea1a62ed233ed52ee52280c1b
agibsonccc <adam@konduit.ai>
2024-12-27 17:04:44 +0900

    fix cuda chunking issues

--- libnd4j/include/helpers/cuda/ConstantShapeHelper.cu
+++ libnd4j/include/helpers/cuda/ConstantShapeHelper.cu
@@ -169,39 +169,7 @@ bool ConstantShapeHelper::checkBufferExistenceForShapeInfo(ShapeDescriptor *desc
  return _shapeTrie.exists(descriptor->toShapeInfo());
 }
 
-} // namespace sd
 
-#endif/* ******************************************************************************
-*
-* This program and the accompanying materials are made available under the
-* terms of the Apache License, Version 2.0 which is available at
-* https://www.apache.org/licenses/LICENSE-2.0.
-*
-*  See the NOTICE file distributed with this work for additional
-*  information regarding copyright ownership.
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*
-* SPDX-License-Identifier: Apache-2.0
-******************************************************************************/
-
-//
-//  @author raver119@gmail.com
-//
-
-#ifdef __CUDABLAS__
-#include <array/PrimaryPointerDeallocator.h>
-#include <helpers/ConstantShapeHelper.h>
-#include <helpers/ShapeBuilders.h>
-#include <helpers/ShapeUtils.h>
-#include <helpers/logger.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-namespace sd {
 
 // Part 3: Shape Info Creation Methods
 
@@ -280,39 +248,7 @@ const LongType* ConstantShapeHelper::createFromExisting(sd::LongType* shapeInfo,
  return result;
 }
 
-} // namespace sd
 
-#endif/* ******************************************************************************
-*
-* This program and the accompanying materials are made available under the
-* terms of the Apache License, Version 2.0 which is available at
-* https://www.apache.org/licenses/LICENSE-2.0.
-*
-*  See the NOTICE file distributed with this work for additional
-*  information regarding copyright ownership.
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*
-* SPDX-License-Identifier: Apache-2.0
-******************************************************************************/
-
-//
-//  @author raver119@gmail.com
-//
-
-#ifdef __CUDABLAS__
-#include <array/PrimaryPointerDeallocator.h>
-#include <helpers/ConstantShapeHelper.h>
-#include <helpers/ShapeBuilders.h>
-#include <helpers/ShapeUtils.h>
-#include <helpers/logger.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-namespace sd {
 
 // Part 4: Type Casting and Shape Transformation Methods
 
