283563a3df82c62618656b0c444e7bbf184b53bb
agibsonccc <adam@konduit.ai>
2025-06-14 09:04:38 +0900

    more windows cuda

--- libnd4j/include/loops/cpu/reduce/reduce_long.hpp
+++ libnd4j/include/loops/cpu/reduce/reduce_long.hpp
@@ -8,9 +8,9 @@
 *  See the NOTICE file distributed with this work for additional
 *  information regarding copyright ownership.
 * Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
-* the License for the specific language governing permissions and limitations
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
 * under the License.
 *
 * SPDX-License-Identifier: Apache-2.0
@@ -219,7 +219,7 @@ void SD_HOST ReduceLongFunction<X, Z>::exec(sd::memory::Workspace *workspace, co
    convertedExtraParamsPtr = convertedExtraParams;
  }
 
- // Fix: Use ReductionLongLoops directly without #ifdef guards - the implementation will handle inlining
+ // Call ReductionLongLoops properly
  sd::ReductionLongLoops<X, Z>::template innerloopReduce<OpType>(workspace, x, xShapeInfo, z, zShapeInfo, dims, convertedExtraParamsPtr);
 }
 
