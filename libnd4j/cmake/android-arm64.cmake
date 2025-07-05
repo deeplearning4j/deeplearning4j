ebb7220edc1f1759b7250bf4c36f52cb420d0d9b
agibsonccc <adam@konduit.ai>
2025-07-06 06:39:30 +0900

    arm64 toolchain

--- libnd4j/cmake/android-arm64.cmake
+++ libnd4j/cmake/android-arm64.cmake
@@ -19,7 +19,7 @@
 # CMake toolchain to build for Android 5.0 or newer. Sample usage:
 
 set(CMAKE_SYSTEM_NAME Android)
-set(CMAKE_ANDROID_ARCH_ABI armv8-a)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 set(CMAKE_ANDROID_NDK "$ENV{ANDROID_NDK}")
 set(CMAKE_ANDROID_STL_TYPE c++_static)
 set(CMAKE_SYSTEM_VERSION  "$ENV{ANDROID_VERSION}")
