c9e5b6d68bad0c0e71908303b931599bccea153d
agibsonccc <adam@konduit.ai>
2025-07-06 08:00:44 +0900

    android arm64

--- libnd4j/cmake/android-arm64.cmake
+++ libnd4j/cmake/android-arm64.cmake
@@ -5,11 +5,8 @@ set(CMAKE_ANDROID_STL_TYPE c++_static)
 set(CMAKE_SYSTEM_VERSION 21)
 set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION clang)
 
-# Use the modern sysroot approach
-set(CMAKE_SYSROOT "$ENV{ANDROID_NDK}/toolchains/llvm/prebuilt/linux-aarch64/sysroot")
-
 set(ANDROID TRUE)
-set(CMAKE_C_COMPILER "$ENV{ANDROID_NDK}/toolchains/llvm/prebuilt/linux-aarch64/bin/aarch64-linux-android21-clang")
+set(CMAKE_C_COMPILER   "$ENV{ANDROID_NDK}/toolchains/llvm/prebuilt/linux-aarch64/bin/aarch64-linux-android21-clang")
 set(CMAKE_CXX_COMPILER "$ENV{ANDROID_NDK}/toolchains/llvm/prebuilt/linux-aarch64/bin/aarch64-linux-android21-clang++")
 
 add_definitions(-D__ANDROID_API__=21 -DANDROID -fPIC -ffunction-sections -funwind-tables -fstack-protector-strong)
