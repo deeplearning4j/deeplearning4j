45ade58a3ed075e2a2a095d9767357528b504d98
agibsonccc <adam@konduit.ai>
2026-06-15 19:35:19 +0900

    fix: correct NDArrayFactory::create argument order in loha/lokr/multi_lora
    - loha_matmul, lokr_matmul, multi_lora_matmul: fix create() argument
      order to match (char order, vector shape, DataType, LaunchContext*)

--- libnd4j/include/ops/declarable/generic/nn/multi_lora_matmul.cpp
+++ libnd4j/include/ops/declarable/generic/nn/multi_lora_matmul.cpp
@@ -78,10 +78,10 @@ CUSTOM_OP_IMPL(multi_lora_matmul, 5, 1, false, 0, 0) {
         auto loraB = loraBSub->reshape('c', loraBShape);
 
         // LoRA: X @ A @ B * alpha
-        auto intermediate = NDArrayFactory::create(input->dataType(), 'c', {1, rank}, input->getContext());
+        auto intermediate = NDArrayFactory::create('c', std::vector<sd::LongType>{1, rank}, input->dataType(), input->getContext());
         MmulHelper::mmul(inputRow, loraA, intermediate, 1.0f, 0.0f);
 
-        auto loraOutput = NDArrayFactory::create(input->dataType(), 'c', {1, outFeatures}, input->getContext());
+        auto loraOutput = NDArrayFactory::create('c', std::vector<sd::LongType>{1, outFeatures}, input->dataType(), input->getContext());
         MmulHelper::mmul(intermediate, loraB, loraOutput, alpha, 0.0f);
 
         // Add to base output
